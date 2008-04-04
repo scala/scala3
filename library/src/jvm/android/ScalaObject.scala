023adb86207d3ccac1cc0b41d17aabb63c2ec660
Geoffrey Washburn <geoffrey.washburn@epfl.ch>
2008-04-04 12:09:06 +0000

    The great library reorg!
    The standard library will now be laid out approximately like this
    
     library/scala
            /jvm/scala
            /jvm/jvm1.4/scala
            /jvm/jvm1.5/scala
            /jvm/android/scala
            /jvm/cldc/scala
            /dotnet/scala
    
    To build the standard library for a given target you start by copying
    the root library/scala tree to a staging area, then move down in the
    hierarchy copying the nested "scala" trees on top in the staging area.
    So if you wanted to build for cldc, for example, you would do something
    like the following:
    
      rsync -avz library/scala                staging/
      rsync -avz library/scala/jvm/scala      staging/
      rsync -avz library/scala/jvm/cldc/scala staging/
    
    The ant build files will be updated to do this for you automagically,
    and there will soon be shell script to do this if you want to compile
    some parts of the standard library manually.

---
+++ library/src/jvm/android/ScalaObject.scala
@@ -0,0 +1,23 @@
+/*                     __                                               *\
+**     ________ ___   / /  ___     Scala API                            **
+**    / __/ __// _ | / /  / _ |    (c) 2002-2008, LAMP/EPFL             **
+**  __\ \/ /__/ __ |/ /__/ __ |    http://scala-lang.org/               **
+** /____/\___/_/ |_/____/_/ | |                                         **
+**                          |/                                          **
+\*                                                                      */
+
+// $Id$
+
+
+package scala
+
+import Predef._
+
+trait ScalaObject extends AnyRef {
+
+  /** This method is needed for optimizing pattern matching expressions
+   *  which match on constructors of case classes.
+   */
+  def $tag(): Int = 0
+
+}
