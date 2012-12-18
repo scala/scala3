4481a057fb5906c002788642aaad34a6cf1124ef
Martin Odersky <odersky@gmail.com>
2012-12-18 15:08:39 +0100

    Added rudimentary definitions for Positions

---
+++ src/dotty/tools/dotc/core/Positions.scala
@@ -0,0 +1,22 @@
+package dotty.tools.dotc.core
+
+object Positions {
+
+  /** The bit position of the end part of a range position */
+  private val Shift = 32
+
+  class Position(val coords: Long) extends AnyVal {
+    def isRange = coords < 0
+    def point: Int = if (isRange) start else coords.toInt
+    def start: Int = coords.abs.toInt
+    def end: Int = if (isRange) start else (coords.abs >>> Shift).toInt
+  }
+
+  def rangePos(start: Int, end: Int) =
+    new Position(-(start + (end.toLong << Shift)))
+
+  def offsetPosition(point: Int) =
+    new Position(point.toLong)
+
+
+}
