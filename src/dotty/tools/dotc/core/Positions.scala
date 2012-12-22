c88f31f494bf4aa5f8210b603d34a5c70a274186
Martin Odersky <odersky@gmail.com>
2012-12-22 18:03:08 +0100

    Fixes to Position.

--- src/dotty/tools/dotc/core/Positions.scala
+++ src/dotty/tools/dotc/core/Positions.scala
@@ -9,14 +9,14 @@ object Positions {
     def isRange = coords < 0
     def point: Int = if (isRange) start else coords.toInt
     def start: Int = coords.abs.toInt
-    def end: Int = if (isRange) start else (coords.abs >>> Shift).toInt
+    def end: Int = (if (isRange) coords.abs >>> Shift else coords).toInt
   }
 
   def rangePos(start: Int, end: Int) =
     new Position(-(start + (end.toLong << Shift)))
 
-  def offsetPosition(point: Int) =
+  def offsetPos(point: Int) =
     new Position(point.toLong)
 
-
+  val NoPosition = new Position(-1L)
 }
