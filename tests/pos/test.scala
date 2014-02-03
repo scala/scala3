4e34962fba727975c071d6288913d89b4692a08f
Martin Odersky <odersky@gmail.com>
2014-02-03 22:40:54 +0100

    Commenting out unused alternative isSubType implementation.

--- tests/pos/test.scala
+++ tests/pos/test.scala
@@ -1,7 +1,11 @@
 object test {
-  import scala.collection.breakOut
- 
-  def name(i: Int) = i.toString
-
-  (0 until 10).map(name)(breakOut)
+  
+  import java.util.concurrent.{ Future, Callable, ExecutorService }
+  def callable[T](body: => T): Callable[T] = new Callable[T] { override def call() = body }
+  
+  
+  def xsubmit(x: Runnable): Future[_] = ???
+  //def xsubmit[T](x: Runnable, y: T): Future[T] = ???
+  def xsubmit[U](x: Callable[U]): Future[U] = ???
+  def spawn0[V](body: V): Future[V]         = xsubmit(callable(body))
 }
