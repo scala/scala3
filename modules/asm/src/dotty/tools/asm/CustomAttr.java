95b263a7c9df202f74ae95a9e3d120e71c47fc44
Lukas Rytz <lukas.rytz@epfl.ch>
2014-02-12 20:53:05 +0100

    glue ASM <-> GenASM
    the only part of scala.tools.asm to maintain manually.
    
    same commit as
    https://github.com/scala/scala/commit/f9943432a6ce13c770927c4ffc184887daa101f2

---
+++ modules/asm/src/dotty/tools/asm/CustomAttr.java
@@ -0,0 +1,20 @@
+/* NSC -- new Scala compiler
+ * Copyright 2005-2012 LAMP/EPFL
+ */
+
+package dotty.tools.asm;
+
+import dotty.tools.asm.Attribute;
+
+/**
+ * A subclass of ASM's Attribute for the sole purpose of accessing a protected field there.
+ *
+ */
+public class CustomAttr extends Attribute {
+
+    public CustomAttr(final String type, final byte[] value) {
+        super(type);
+        super.value = value;
+    }
+
+}
