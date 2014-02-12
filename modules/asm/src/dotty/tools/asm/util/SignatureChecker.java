ffa2e989cc5f2a43a1b5fef2a3266a6aafc31074
Lukas Rytz <lukas.rytz@epfl.ch>
2014-02-12 20:53:05 +0100

    glue ASM <-> GenASM
    the only part of scala.tools.asm to maintain manually.
    
    same commit as
    https://github.com/scala/scala/commit/f9943432a6ce13c770927c4ffc184887daa101f2

---
+++ modules/asm/src/dotty/tools/asm/util/SignatureChecker.java
@@ -0,0 +1,47 @@
+/* NSC -- new Scala compiler
+ * Copyright 2005-2012 LAMP/EPFL
+ */
+
+package dotty.tools.asm.util;
+
+import dotty.tools.asm.util.CheckMethodAdapter;
+import dotty.tools.asm.MethodVisitor;
+
+/**
+ * A subclass of ASM's CheckMethodAdapter for the sole purpose of accessing some protected methods there.
+ *
+ */
+public class SignatureChecker extends CheckMethodAdapter {
+
+    public SignatureChecker(final MethodVisitor mv) {
+        super(mv);
+    }
+
+    /**
+     * Checks a class signature.
+     *
+     * @param signature a string containing the signature that must be checked.
+     */
+    public static void checkClassSignature(final String signature) {
+      CheckMethodAdapter.checkClassSignature(signature);
+    }
+
+    /**
+     * Checks a method signature.
+     *
+     * @param signature a string containing the signature that must be checked.
+     */
+    public static void checkMethodSignature(final String signature) {
+      CheckMethodAdapter.checkMethodSignature(signature);
+    }
+
+    /**
+     * Checks a field signature.
+     *
+     * @param signature a string containing the signature that must be checked.
+     */
+    public static void checkFieldSignature(final String signature) {
+      CheckMethodAdapter.checkFieldSignature(signature);
+    }
+
+}
