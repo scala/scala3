b2dd56c2b5f049f84635e62fc41197ec84c8d3e4
Dmitry Petrashko <dmitry.petrashko@gmail.com>
2014-03-03 16:41:45 +0100

    For some tests context is required, it should be passed to checker phase.

--- test/test/SamplePhaseTest.scala
+++ test/test/SamplePhaseTest.scala
@@ -6,7 +6,8 @@ class SamplePhaseTest extends DottyTest {
 
   @Test
   def testTypechekingSimpleClass = checkCompile("frontend", "class A{}") {
-    tree =>
+    (tree, context) =>
+      implicit val ctx = context
       Assert.assertTrue("can typecheck simple class",
         tree.toString == "PackageDef(Ident(<empty>),List(TypeDef(Modifiers(,,List()),A,Template(DefDef(Modifiers(,,List()),<init>,List(),List(List()),TypeTree[TypeRef(ThisType(module class scala),Unit)],EmptyTree),List(Apply(Select(New(TypeTree[TypeRef(ThisType(module class lang),Object)]),<init>),List())),ValDef(Modifiers(private,,List()),_,EmptyTree,EmptyTree),List()))))"
       )
