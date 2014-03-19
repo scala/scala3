DarkDimius/dotty

LazyVals phase.
Creates accessors for lazy vals:
1) lazy local vals are rewritten to dotty.runtime.Lazy*** holders
2) for a non-volatile field lazy val create a non-thread-safe accessor and flag:
2.a) if lazy val type indicates that val is not nullable, uses null value as a flag
2.b) else uses boolean flag for sake of performance, method size, and
allowing more jvm optimizations
3) for a volatile field lazy val use double locking scheme, that guaranties no
spurious deadlocks, using long bits as bitmaps and creating companion
objects to store offsets needed for unsafe methods.

src/dotty/runtime/LazyHolders.scala
@@ -1,0 +1,43 @@
+package dotty.runtime
+
+/**
+ * Classes used as holders for local lazy vals
+ */
+class LazyInt(init : =>Int) {
+  lazy val value = init
+}
+
+class LazyLong(init : =>Long) {
+  lazy val value = init
+}
+
+class LazyBoolean(init : =>Boolean) {
+  lazy val value = init
+}
+
+class LazyDouble(init : =>Double) {
+  lazy val value = init
+}
+
+class LazyFloat(init : =>Float) {
+  lazy val value = init
+}
+
+class LazyByte(init : =>Byte) {
+  lazy val value = init
+}
+
+class LazyRef(init : =>AnyRef) {
+  lazy val value = init
+}
+
+class LazyShort(init : =>Short) {
+  lazy val value = init
+}
+
+class LazyChar(init : =>Char) {
+  lazy val value = init
+}
+
+
+

scala/scala3

Conflicts:
	test/dotc/tests.scala

src/dotty/runtime/LazyHolders.scala
@@ -1,0 +1,40 @@
+package dotty.runtime
+
+/**
+ * Classes used as holders for local lazy vals
+ */
+class LazyInt(init: => Int) {
+  lazy val value = init
+}
+
+class LazyLong(init: => Long) {
+  lazy val value = init
+}
+
+class LazyBoolean(init: => Boolean) {
+  lazy val value = init
+}
+
+class LazyDouble(init: => Double) {
+  lazy val value = init
+}
+
+class LazyFloat(init: => Float) {
+  lazy val value = init
+}
+
+class LazyByte(init: => Byte) {
+  lazy val value = init
+}
+
+class LazyRef(init: => AnyRef) {
+  lazy val value = init
+}
+
+class LazyShort(init: => Short) {
+  lazy val value = init
+}
+
+class LazyChar(init: => Char) {
+  lazy val value = init
+}