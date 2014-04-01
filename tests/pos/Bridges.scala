0b50b29cb9e8b43fff1d012dd53561b5ac0a437c
Dmitry Petrashko <dmitry.petrashko@gmail.com>
2014-04-01 23:27:54 +0200

    Bridge generation in erasure implemented.
    Relies on meaning of "override" to see which bridges are required.
    Doesn't take in account value classes for now
    Uses 'adapt' used by erasure for converting arguments and return value.

---
+++ tests/pos/Bridges.scala
@@ -0,0 +1,11 @@
+abstract class X[T]{
+  def go2(x:T)(y:T = x): T = y
+  def go: T 
+  def go1(x: T) = x
+}
+
+class Y extends X[Int] {
+  override def go2(x: Int)(z: Int) = 2
+  override def go = 0
+  override def go1(x: Int) = x
+}
