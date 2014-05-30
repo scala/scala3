e43c3aaa61e570de1cb80b1766fb3e3988c444a0
Martin Odersky <odersky@gmail.com>
2014-05-30 14:42:59 +0200

    Reclassifying tests

--- tests/neg/t2994.scala
+++ tests/neg/t2994.scala
@@ -25,3 +25,11 @@ object Naturals {
   }
 
 }
+
+object Test {
+  trait Bar[X[_]]
+  trait Baz[S[_] <: Bar[S]] {
+    type Apply[T]
+  }
+  trait Foo[V[_] <: Bar[V]] extends Bar[Baz[V]#Apply]
+}
--- tests/pending/pos/t2994b.scala
+++
@@ -1,7 +0,0 @@
-object Test {
-  trait Bar[X[_]]
-  trait Baz[S[_] <: Bar[S]] {
-    type Apply[T]
-  }
-  trait Foo[V[_] <: Bar[V]] extends Bar[Baz[V]#Apply]
-}
