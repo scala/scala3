601b11eb5c8c43304f83310d66fe7943fc1cb0a5
Dmitry Petrashko <dmitry.petrashko@gmail.com>
2014-07-22 13:55:10 +0200

    Add test for tailcall
    Test for order of type parameters in recursive call

--- tests/pos/tailcall/tailcall.scala
+++ tests/pos/tailcall/tailcall.scala
@@ -3,3 +3,7 @@ class tailcall {
   final def fact(x: Int, acc: Int = 1): Int = if (x == 0) acc else fact(x - shift, acc * x)
   def id[T <: AnyRef](x: T): T = if (x eq null) x else id(x)
 }
+
+class TypedApply[T2]{
+  private def firstDiff[T <: TypedApply[T2]](xs: List[T]): Int = firstDiff(xs)
+}
