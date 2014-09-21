0a04924f8b900bdddd54fe4e43d72a23c36f3782
Martin Odersky <odersky@gmail.com>
2014-09-21 14:40:41 +0200

    Fix handling of type params in secondary constructors
    Params are already added by Desugar. No special treatment needed here.
    Besides primaryConstructor.typeParams is always empty, because term symbols
    do not have type parameters.
    
    The fix turns t2660.scala into an error. I believe the error is correct, hence
    the test was moved with a comment to neg.

---
+++ tests/neg/t2660.scala
@@ -0,0 +1,47 @@
+// Dotty deviation. The calls here now are classified as ambiguous.
+
+package hoho
+
+class G
+
+class H extends G
+
+class A[T](x: T) {
+
+  def this(y: G, z: T) = {
+    this(z)
+    print(1)
+  }
+
+  def this(z: H, h: T) = {
+    this(h)
+    print(2)
+  }
+}
+
+object T {
+  def main(args: Array[String]): Unit = {
+    implicit def g2h(g: G): H = new H
+    new A[Int](new H, 23)
+      // in the context here, either secondary constructor is applicable
+      // to the other, due to the implicit in scope. So the call is ambiguous.
+  }
+}
+
+
+// A version of t2660 which does not use constructors
+
+object X {
+  def f[T](x: T) = ???
+  def f[T](y: G, z: T) = ???
+  def f[T](z: H, h: T) = ???
+}
+
+object T2 {
+  def main(args: Array[String]): Unit = {
+    implicit def g2h(g: G): H = new H
+    X.f(new H, 23)
+  }
+}
+
+
