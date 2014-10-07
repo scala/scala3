b4e4bc5a01319ebb8c7cf8c2d2fbfaec46b46ae2
Martin Odersky <odersky@gmail.com>
2014-10-07 14:23:38 +0200

    New tests

---
+++ tests/pending/pos/vararg-pattern.scala
@@ -0,0 +1,12 @@
+object Test {
+
+  List(1, 2, 3, 4) match {
+    case List(1, 2, xs: _*) =>
+      val ys: Seq[Int] = xs
+      println(ys)
+  }
+  val List(1, 2, x: _*) = List(1, 2, 3, 4)
+
+}
+
+
