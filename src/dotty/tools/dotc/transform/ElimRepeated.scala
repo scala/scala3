34fa82cc3f0e4895b99128385a9708b14e8cf285
Dmitry Petrashko <dmitry.petrashko@gmail.com>
2014-10-11 12:21:12 +0200

    Fix elimRepeated not transforming annotations.

--- src/dotty/tools/dotc/transform/ElimRepeated.scala
+++ src/dotty/tools/dotc/transform/ElimRepeated.scala
@@ -11,6 +11,7 @@ import Contexts.Context
 import Symbols._
 import Denotations._, SymDenotations._
 import Decorators.StringInterpolators
+import dotty.tools.dotc.core.Annotations.ConcreteAnnotation
 import scala.collection.mutable
 import DenotTransformers._
 import Names.Name
@@ -24,6 +25,34 @@ class ElimRepeated extends MiniPhaseTransform with InfoTransformer { thisTransfo
   import ast.tpd._
 
   override def phaseName = "elimRepeated"
+
+  object annotTransformer extends TreeMap {
+    override def transform(tree: Tree)(implicit ctx: Context): Tree = super.transform(tree) match {
+        case x @(_: Ident|_ :Select|_: Apply| _: TypeApply| _: DefDef) => transformTypeOfTree(x)
+        case x => x
+      }
+  }
+
+  /**
+   * Overriden to solve a particular problem with <repeated> not being eliminated inside annotation trees
+   * Dmitry: this should solve problem for now,
+   *         following YAGNI principle I am convinced that we shouldn't make a solution
+   *         for a generalized problem(transforming annotations trees)
+   *         that manifests itself only here.
+   */
+  override def transform(ref: SingleDenotation)(implicit ctx: Context): SingleDenotation = {
+    val info1 = transformInfo(ref.info, ref.symbol)
+
+    ref match {
+      case ref: SymDenotation =>
+        val annotTrees = ref.annotations.map(_.tree)
+        val annotTrees1 = annotTrees.mapConserve(annotTransformer.transform)
+        val annots1 = if(annotTrees eq annotTrees1) ref.annotations else annotTrees1.map(new ConcreteAnnotation(_))
+        if ((info1 eq ref.info) && (annots1 eq ref.annotations)) ref
+        else ref.copySymDenotation(info = info1, annotations = annots1)
+      case _ => if (info1 eq ref.info) ref else ref.derivedSingleDenotation(ref.symbol, info1)
+    }
+  }
 
   def transformInfo(tp: Type, sym: Symbol)(implicit ctx: Context): Type =
     elimRepeated(tp)
