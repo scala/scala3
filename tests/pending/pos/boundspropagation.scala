1c6ce936550a08b3d505375548c4a19efd9fead3
Martin Odersky <odersky@gmail.com>
2014-11-17 16:17:54 +0100

    Updated comment in test

---
+++ tests/pending/pos/boundspropagation.scala
@@ -0,0 +1,27 @@
+// scalac fails for test2/3
+// dotc used to fail for all three
+object test1 {
+  class Tree[-T >: Null]
+
+
+  def f(x: Any): Tree[Null] = x match {
+    case y: Tree[_] => y
+  }
+}
+/*object test2 {
+  class Tree[T >: Null]
+
+
+  def f(x: Any): Tree[Null] = x match {
+    case y: Tree[_] => y
+  }
+}
+object test3 {
+  class Tree[+T >: Null]
+
+
+  def f(x: Any): Tree[Null] = x match {
+    case y: Tree[_] => y
+   }
+}
+*/
