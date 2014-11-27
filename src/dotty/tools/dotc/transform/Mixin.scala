0b73ed1ce8747ab8b69dffa48f4bc6b264f9e939
Dmitry Petrashko <dmitry.petrashko@gmail.com>
2014-11-27 16:18:26 +0100

    Mixing now also changes Scala2 traits into interfaces.

--- src/dotty/tools/dotc/transform/Mixin.scala
+++ src/dotty/tools/dotc/transform/Mixin.scala
@@ -73,7 +73,12 @@ class Mixin extends MiniPhaseTransform with SymTransformer { thisTransform =>
   override def transformSym(sym: SymDenotation)(implicit ctx: Context): SymDenotation =
     if (sym.is(Accessor, butNot = Deferred) && sym.owner.is(Trait))
       sym.copySymDenotation(initFlags = sym.flags | Deferred)
-    else
+    else if(sym.is(Scala2x) && sym.is(Trait, JavaDefined)) { // fixup scala2 traits to become interfaces
+      val info = sym.asClass.classInfo
+      val parents: List[TypeRef] = defn.ObjectType.asInstanceOf[TypeRef] :: info.parents // new superClass is object
+      val newInfo = info.derivedClassInfo(classParents = parents)
+      sym.copySymDenotation(initFlags = sym.flags | JavaInterface, info = newInfo)
+    } else
       sym
 
   private def initializer(sym: Symbol)(implicit ctx: Context): TermSymbol = {
