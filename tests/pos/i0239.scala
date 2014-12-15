413bcebf5a87102b1f1b2840b92b6a8017aa16df
Martin Odersky <odersky@gmail.com>
2014-12-15 12:57:13 +0100

    Fix to asSeenFrom for package object members
    Like TypeAssigner, asSeenFrom needs to insert a package object if the
    prefix is a package but the class enclosing the type is not.

---
+++ tests/pos/i0239.scala
@@ -0,0 +1,24 @@
+package p {
+  class C[A] {
+    implicit def foo: M[A] = ???
+  }
+
+  object `package` extends C[String]
+
+  object test0 {
+    def compute[A](implicit m: M[A]): A = ???
+    val v = compute
+    val v1: String = v
+  }
+}
+
+trait M[A]
+
+object test1 {
+
+  def compute[A](implicit m: M[A]): A = ???
+
+  import p._
+  val v = compute
+  val v1: String = v
+}
