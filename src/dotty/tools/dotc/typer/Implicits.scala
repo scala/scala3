0a35baa8a1ed91a71398887ae7a1a08910e4faf0
Martin Odersky <odersky@gmail.com>
2015-01-08 11:36:32 +0100

    New scheme for subtyping refined types.
    - Instead of rebasing, use the DeBrujn level of a RefiendThis.
    - Make sure lower type is a singleton by skolemizing it if necessary.
    - Do the correct rebinding of the upper type's RefinedThis.
    
    Remarks:
    
     - The new scheme discovered quite a lot of errors which are mostly fixded in
    other commits of this branch. i0268 (GADT matching) still does not work, moved to pending.
    
     - Some optimizations are currently missing:
       (1) fast path refined subtyping
       (2) faster operations for substituting refined thistypes which
           explot the fact that RefinedThis is relatively rare.

--- src/dotty/tools/dotc/typer/Implicits.scala
+++ src/dotty/tools/dotc/typer/Implicits.scala
@@ -450,7 +450,10 @@ trait Implicits { self: Typer =>
     private def nestedContext = ctx.fresh.setMode(ctx.mode &~ Mode.ImplicitsEnabled)
 
     private def implicitProto(resultType: Type, f: Type => Type) =
-      if (argument.isEmpty) f(resultType) else ViewProto(f(argument.tpe.widen), f(resultType))
+      if (argument.isEmpty) f(resultType) else ViewProto(f(argument.tpe/*.widen*/), f(resultType)) 
+        // !!! TODO: check performance implications
+        // If we do the widen, SyntheticMethods, line 66 fails to compile
+        // val synthetic = sym.copy(...)
 
     assert(argument.isEmpty || argument.tpe.isValueType || argument.tpe.isInstanceOf[ExprType],
         d"found: ${argument.tpe}, expected: $pt")
