3a86797d43a5d00cd8008607ab7f2d6d02b3823d
Martin Odersky <odersky@gmail.com>
2015-02-12 11:28:35 +0100

    Disallow refinements of types or methods that do not appear in parent.
    We planned this for a long time but never implemented it. Instead, we sometimes
    issued an erro in Splitter, namely if reflection would have been needed to access the
    member. It turns out that some tests (e.g. neg/t625) fail -Ycheck (we knew that before
    and disabled) but also fail Pickling because they generate orhpan PolyParams. So rather
    than patching this up it seems now is a good time to enforce the restriction for real.

--- tests/neg/t1131.scala
+++
@@ -1,4 +0,0 @@
-trait A { self: Any { def p: Any } =>
-  def f(b: => Unit): Unit = {}
-  f { p } // error: cannot access member 'p' from structural type
-}
--- tests/pos/t2810.scala
+++
@@ -1,8 +0,0 @@
-
-
-
-
-object Test {
-  val closeable1: { def close(): Unit } = new scala.io.Source { val iter: Iterator[Char] = "".iterator }
-  val closeable2: { def close(): Unit } = new java.io.Closeable { def close() = {} }
-}
