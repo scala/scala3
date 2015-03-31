00b4fceb8e15f816b517226bd4b1c609d03e5aa6
Guillaume Martres <smarter@ubuntu.com>
2015-03-31 23:50:17 +0200

    Don't consider the temporary refinement classes as derived value classes

--- src/dotty/tools/dotc/transform/ValueClasses.scala
+++ src/dotty/tools/dotc/transform/ValueClasses.scala
@@ -13,6 +13,7 @@ import StdNames._
 object ValueClasses {
 
   def isDerivedValueClass(d: SymDenotation)(implicit ctx: Context) = {
+    !d.isRefinementClass &&
     d.isValueClass &&
     (d.initial.symbol ne defn.AnyValClass) && // Compare the initial symbol because AnyVal does not exist after erasure
     !d.isPrimitiveValueClass
