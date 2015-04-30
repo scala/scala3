be61c7813fbf7cab90b5d70ef6e506e296c510dc
Dmitry Petrashko <dmitry.petrashko@gmail.com>
2015-04-30 16:43:40 +0200

    Add tests for #499

---
+++ tests/run/byNameVarargs/i499.scala
@@ -0,0 +1,28 @@
+object Test {
+  def foo(a: => Any*) = ()
+  def bar(a: => Any*) = foo(a : _*)
+  def baz(a: => Seq[Any]) = foo(a : _*)
+  bar(???, ???)
+  baz(Seq(???, ???))
+  baz(Array(???, ???))
+
+  def foo1(a => Any, b: => Any*) = ()
+  foo1(???)
+  foo1(???, ???, ???)
+
+  def assertFails(a => Any) = {
+   var failed = false 
+   try {
+     a
+   } catch {
+     case e => failed = true
+   }
+   assert(failed)
+  }
+
+  def forceLength(b: => Any*) = b.length
+  assertFails(forceLength(???))
+
+  def forceHead(b: => Any*) = b(0)
+  assertFails(forceHead(1, ???))
+}
