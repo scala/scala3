744ffb5d7b5325c2ac14a27c493a6c2749e741d4
Dmitry Petrashko <dmitry.petrashko@gmail.com>
2015-05-01 16:28:52 +0200

    Introduce VCArray.clone in scala.
    It can be done with non-abstract rhs of method.

--- src/dotty/runtime/vc/VCArrayClone.java
+++
@@ -1,9 +0,0 @@
-package dotty.runtime.vc;
-
-/**
- *  Exists for the sole reason of Scala not accepting such trait
- */
-
-interface VCArrayClone {
-    Object clone();
-}
--- src/dotty/runtime/vc/VCPrototype.scala
+++ src/dotty/runtime/vc/VCPrototype.scala
@@ -4,8 +4,9 @@ abstract class VCPrototype {
 }
 
 
-abstract class VCArrayPrototype[T <: VCPrototype] extends Object with Cloneable with VCArrayClone {
+abstract class VCArrayPrototype[T <: VCPrototype] extends Object with Cloneable {
   def apply(idx: Int): Object
   def update(idx: Int, el: T): Unit
   def length: Int
+  override def clone: Object = super.clone()
 }
