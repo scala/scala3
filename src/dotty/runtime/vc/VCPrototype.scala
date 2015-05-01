68d12d3745e8e7f71d98d9101e9aaa348070c3b0
Dmitry Petrashko <dmitry.petrashko@gmail.com>
2015-05-01 14:59:31 +0200

    Remove VCCompanionPrototype: it is not used.

--- src/dotty/runtime/vc/VCPrototype.scala
+++ src/dotty/runtime/vc/VCPrototype.scala
@@ -3,10 +3,6 @@ package dotty.runtime.vc
 abstract class VCPrototype {
 }
 
-abstract class VCCompanionPrototype {
-
-}
-
 abstract class VCArrayPrototype[T <: VCPrototype] {
   def apply(idx: Int): Object
   def update(idx: Int, el: T): Unit
