220fe53ad0638c33572a9e11db0b6dc3aabd6c27
Martin Odersky <odersky@gmail.com>
2015-05-03 16:41:29 +0200

    Fix #522.
    We were missing a substitution in full parameterization. Embarraingly, this
    made even the example in the doc comment of `fullyParameterizedDef` fail.

--- tests/pos/extmethods.scala
+++ tests/pos/extmethods.scala
@@ -5,3 +5,13 @@ class T[A, This <: That1[A]](val x: Int) extends AnyVal {
   final def loop(x: This, cnt: Int): Int = loop(x, cnt + 1)
   def const[B](): Boolean = return true
 }
+
+final class TraversableOnceOps[+A](val collection: TraversableOnce[A]) extends AnyVal {
+  def reduceLeftOption[B >: A](op: (B, A) => B): Option[B] =
+    if (collection.isEmpty) None else Some(collection.reduceLeft[B](op))
+}
+
+class Foo[+A <: AnyRef](val xs: List[A]) extends AnyVal {
+  def baz[B >: A](x: B): List[B] = ???
+}
+
