090a6a606f22ceb0b2800556f5d5de825a3941b8
Martin Odersky <odersky@gmail.com>
2015-05-03 19:01:04 +0200

    Moved tests from pending

--- tests/pos/extmethods.scala
+++ tests/pos/extmethods.scala
@@ -6,11 +6,6 @@ class T[A, This <: That1[A]](val x: Int) extends AnyVal {
   def const[B](): Boolean = return true
 }
 
-final class TraversableOnceOps[+A](val collection: TraversableOnce[A]) extends AnyVal {
-  def reduceLeftOption[B >: A](op: (B, A) => B): Option[B] =
-    if (collection.isEmpty) None else Some(collection.reduceLeft[B](op))
-}
-
 class Foo[+A <: AnyRef](val xs: List[A]) extends AnyVal {
   def baz[B >: A](x: B): List[B] = ???
 }
