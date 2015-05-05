c0d265a52c34ddfd77f4237d215a085675b25a57
Dmitry Petrashko <dmitry.petrashko@gmail.com>
2015-05-05 20:12:15 +0200

    Give implicit ClassTags in DottyPredef explicit types.

--- src/dotty/DottyPredef.scala
+++ src/dotty/DottyPredef.scala
@@ -8,20 +8,18 @@ object DottyPredef {
   /** implicits for ClassTag and TypeTag. Should be implemented with macros */
   implicit def classTag[T]: ClassTag[T] = scala.Predef.???
   implicit def typeTag[T]: TypeTag[T] = scala.Predef.???
-  
-  implicit val IntClassTag     = ClassTag.Int
-  implicit val ByteClassTag    = ClassTag.Byte
-  implicit val ShortClassTag   = ClassTag.Short
-  implicit val CharClassTag    = ClassTag.Char
-  implicit val LongClassTag    = ClassTag.Long
-  implicit val FloatClassTag   = ClassTag.Float
-  implicit val DoubleClassTag  = ClassTag.Double
-  implicit val BooleanClassTag = ClassTag.Boolean
-  implicit val UnitClassTag    = ClassTag.Unit
-  implicit val AnyClassTag     = ClassTag.Any
-  implicit val AnyRefClassTag  = ClassTag.AnyRef
-  implicit val AnyValClassTag  = ClassTag.AnyVal
-  implicit val ObjectClassTag  = ClassTag.Object
-  implicit val NullClassTag    = ClassTag.Null
-  implicit val NothingClassTag = ClassTag.Nothing
+
+
+  /** ClassTags for final classes */
+  implicit val IntClassTag: ClassTag[Int]         = ClassTag.Int
+  implicit val ByteClassTag: ClassTag[Byte]       = ClassTag.Byte
+  implicit val ShortClassTag: ClassTag[Short]     = ClassTag.Short
+  implicit val CharClassTag: ClassTag[Char]       = ClassTag.Char
+  implicit val LongClassTag: ClassTag[Long]       = ClassTag.Long
+  implicit val FloatClassTag: ClassTag[Float]     = ClassTag.Float
+  implicit val DoubleClassTag: ClassTag[Double]   = ClassTag.Double
+  implicit val BooleanClassTag: ClassTag[Boolean] = ClassTag.Boolean
+  implicit val UnitClassTag: ClassTag[Unit]       = ClassTag.Unit
+  implicit val NullClassTag: ClassTag[Null]       = ClassTag.Null
+  implicit val NothingClassTag: ClassTag[Nothing] = ClassTag.Nothing
 }
