5d02fe14e07a66ab219258565b10ff51baada5a0
Martin Odersky <odersky@gmail.com>
2015-05-23 10:32:07 +0200

    Uncomment tests

--- tests/pos/harmonize.scala
+++ tests/pos/harmonize.scala
@@ -3,7 +3,7 @@ object Test {
   def main(args: Array[String]) = {
     val x = true
     val n = 1
-/*    val y = if (x) 'A' else n
+    val y = if (x) 'A' else n
     val z: Int = y
 
     val yy = n match {
@@ -20,7 +20,7 @@ object Test {
       case ex: Error => 3L
     }
     val b: Long = a
-*/
+
     val xs = List(1.0, n, 'c')
     val ys: List[Double] = xs
   }
