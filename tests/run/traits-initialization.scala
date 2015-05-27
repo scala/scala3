79516efe0e3c43af2a7084bc2c61316ad7879f91
Dmitry Petrashko <dmitry.petrashko@gmail.com>
2015-05-27 17:09:04 +0200

    Add super calls to trait initialization test)

--- tests/run/traits-initialization.scala
+++ tests/run/traits-initialization.scala
@@ -11,15 +11,15 @@ trait B extends A {
  str += '2'
 }
 
-class D extends A {
+class D(sup: =>String) extends A {
  str += 'd'
  override val s = str += 'D'
  str += '3'
 }
 
-object Test extends D with B {
+object Test extends D({Test.str += "Z"; Test.str}) with B {
  // should only have 2 fields
  str += 'E'
- def main(args: Array[String]) = assert(str == "aA1dD3bB2E4")
+ def main(args: Array[String]) = assert(str == "aA1dD3bB2E4", str)
  str += '4'
 }
