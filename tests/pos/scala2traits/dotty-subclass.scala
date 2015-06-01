5aaa40607f65d0f3c1af121df226d715c0d02673
Martin Odersky <odersky@gmail.com>
2015-06-01 15:34:19 +0200

    Add test case.
    We are still lacking the setup to do this right for mixed Scala 2/Dotty runtime tests. So I checked into `pos` for now.

---
+++ tests/pos/scala2traits/dotty-subclass.scala
@@ -0,0 +1,27 @@
+// This is supposed to be compiled by Dotty
+class Sub extends T
+
+class A extends S2T with S2Tprivate {
+  val a: Int = 3
+  var b = 2
+}
+
+object Main {
+  def main(args: Array[String]): Unit = {
+    val sub = new Sub
+    println(sub.d)
+    println(sub.v)
+    println(sub.O)
+    println(sub.w)
+
+    val a = new A
+    a.x += a.y
+    println(a.x)
+    println(a.f(a.a + a.b))
+
+    a.xx += a.yy
+    println(a.x)
+    println(a.ff(a.xx))
+  }
+}
+
