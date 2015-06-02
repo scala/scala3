367c9d36622dd0a37304efd042374c20944d75bd
Dmitry Petrashko <dmitry.petrashko@gmail.com>
2015-06-02 14:49:09 +0200

    Move test from lambdaLift.scala to a separate file in pending.

--- tests/pos/lambdalift.scala
+++ tests/pos/lambdalift.scala
@@ -23,24 +23,3 @@ object test {
 
   }
 }
-
-class Super(x: Int)
-
-class Sub extends Super({
-  def foo3(x: Int) = {
-
-    class C {
-      def this(name: String) = this()
-
-      def bam(y: Int): String => Int = {
-        def baz = x + y
-        z => baz * z.length
-      }
-    }
-
-    val fun = new C("dummy").bam(1)
-    fun("abc")
-
-  }
-  foo3(22)
-})
