8c28293fd52213616ed8ca3a2f600eb1e19debe0
Martin Odersky <odersky@gmail.com>
2015-06-11 19:09:06 +0200

    Implement checking for illegal parent trait constructor calls.
    A parent trait may not be parameterized (as in T()) if the calling class does not
    directly implement that trait.

--- test/dotc/tests.scala
+++ test/dotc/tests.scala
@@ -138,6 +138,7 @@ class tests extends CompilerTest {
   @Test def neg_instantiateAbstract = compileFile(negDir, "instantiateAbstract", xerrors = 8)
   @Test def neg_selfInheritance = compileFile(negDir, "selfInheritance", xerrors = 5)
   @Test def neg_shadowedImplicits = compileFile(negDir, "arrayclone-new", xerrors = 2)
+  @Test def neg_traitParamsTyper = compileFile(negDir, "traitParamsTyper", xerrors = 5)
 
   @Test def run_all = runFiles(runDir)
 
---
+++ tests/neg/traitParamsTyper.scala
@@ -0,0 +1,16 @@
+trait T(x: Int) {
+  def f = x
+}
+
+class C(x: Int) extends T() // error
+
+trait U extends C with T
+
+trait V extends C(1) with T(2) // two errors
+
+trait W extends T(3) // error
+
+
+class E extends T(0)
+class F extends E with T(1) // error
+
