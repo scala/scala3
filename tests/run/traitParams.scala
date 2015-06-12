da773fb91df35684034f106544fe59f197b4ec80
Martin Odersky <odersky@gmail.com>
2015-06-12 11:09:19 +0200

    Extend test to verify initialization order
    Verify that the initilialization order described in #640 is correctly implemented.

--- tests/run/traitParams.scala
+++ tests/run/traitParams.scala
@@ -1,19 +1,26 @@
+object State {
+  var s: Int = 0
+}
+
 trait T(x: Int, val y: Int) {
   def f = x
 }
 
 trait U extends T {
+  State.s += 1
   override def f = super.f + y
 }
+import State._
+class C(x: Int) extends U with T(x, x * x + s)
+class C2(x: Int) extends T(x, x * x + s) with U
 
-class C(x: Int) extends U with T(x, x * x)
-
-class D extends C(10) with T {
-
-}
+class D extends C(10) with T
+class D2 extends C2(10) with T
 
 object Test {
-  def main(args: Array[String]): Unit =
+  def main(args: Array[String]): Unit = {
     assert(new D().f == 110)
+    assert(new D2().f == 111)
+  }
 }
 
