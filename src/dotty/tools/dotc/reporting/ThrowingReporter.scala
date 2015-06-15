2e33ba9dd22913b5e7aab472f70dc2f645079636
Martin Odersky <odersky@gmail.com>
2015-06-15 17:08:49 +0200

    Fix doc comment

--- src/dotty/tools/dotc/reporting/ThrowingReporter.scala
+++ src/dotty/tools/dotc/reporting/ThrowingReporter.scala
@@ -7,7 +7,8 @@ import collection.mutable
 import Reporter._
 
 /**
- * This class implements a Reporter that stores all messages
+ * This class implements a Reporter that throws all errors and sends warnings and other
+ * info to the underlying reporter.
  */
 class ThrowingReporter(reportInfo: Reporter) extends Reporter {
   protected def doReport(d: Diagnostic)(implicit ctx: Context): Unit = d match {
