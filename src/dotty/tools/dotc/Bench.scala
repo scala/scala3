de8368d5db436bb126e2f327d58fa1882e427e51
Martin Odersky <odersky@gmail.com>
2015-06-15 17:09:12 +0200

    Remove -resident setting

--- src/dotty/tools/dotc/Bench.scala
+++ src/dotty/tools/dotc/Bench.scala
@@ -8,19 +8,7 @@ package dotc
 import core.Contexts.Context
 import reporting.Reporter
 
-/* To do:
- *   - simplify hk types
- *   - have a second look at normalization (leave at method types if pt is method type?)
- *   - Don't open package objects from class files if they are present in source
- *   - Revise the way classes are inherited - when not followed by [...] or (...),
- *     assume the unparameterized type and forward type parameters as we do now for the synthetic head class.
- */
 object Bench extends Driver {
-  def resident(compiler: Compiler): Reporter = unsupported("resident") /*loop { line =>
-    val command = new CompilerCommand(line split "\\s+" toList, new Settings(scalacError))
-    compiler.reporter.reset()
-    new compiler.Run() compile command.files
-  }*/
 
   private var numRuns = 1
 
@@ -29,17 +17,13 @@ object Bench extends Driver {
   private def ntimes(n: Int)(op: => Reporter): Reporter =
     (emptyReporter /: (0 until n)) ((_, _) => op)
 
-  override def doCompile(compiler: Compiler, fileNames: List[String], reporter: Option[Reporter] = None)
-      (implicit ctx: Context): Reporter =
-    if (new config.Settings.Setting.SettingDecorator[Boolean](ctx.base.settings.resident).value(ctx))
-      resident(compiler)
-    else
-      ntimes(numRuns) {
-        val start = System.nanoTime()
-        val r = super.doCompile(compiler, fileNames, reporter)
-        ctx.println(s"time elapsed: ${(System.nanoTime - start) / 1000000}ms")
-        r
-      }
+  override def doCompile(compiler: Compiler, fileNames: List[String])(implicit ctx: Context): Reporter =
+    ntimes(numRuns) {
+      val start = System.nanoTime()
+      val r = super.doCompile(compiler, fileNames)
+      println(s"time elapsed: ${(System.nanoTime - start) / 1000000}ms")
+      r
+    }
 
   def extractNumArg(args: Array[String], name: String, default: Int = 1): (Int, Array[String]) = {
     val pos = args indexOf name
@@ -47,11 +31,11 @@ object Bench extends Driver {
     else (args(pos + 1).toInt, (args take pos) ++ (args drop (pos + 2)))
   }
 
-  override def process(args: Array[String], rootCtx: Context, reporter: Option[Reporter] = None): Reporter = {
+  override def process(args: Array[String], rootCtx: Context): Reporter = {
     val (numCompilers, args1) = extractNumArg(args, "#compilers")
     val (numRuns, args2) = extractNumArg(args1, "#runs")
     this.numRuns = numRuns
-    ntimes(numCompilers)(super.process(args2, rootCtx, reporter))
+    ntimes(numCompilers)(super.process(args2, rootCtx))
   }
 }
 
