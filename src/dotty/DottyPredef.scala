204158d4141254d1148534e14d3d26b361b7bc12
Martin Odersky <odersky@gmail.com>
2015-06-18 15:07:25 +0200

    Stratify classTags in DottyPredef
    Test case in pending/run/array-addition.scala. The problem was that
    implicit search failed to insert `genericArrayOps` around `Array()`.
    The reason was that the implicit parameter for `genericArrayOps` was
    ambiguous, multiple `classTag` methods matched the expected type
    `ClassTag[?T >: Nothing]`.
    
    Stratifying ClassTags in DottyPredef into layers according
    to implicit priority avoids this problem. This is a temporary solution
    until we have proper ClassTag handling in place.

--- src/dotty/DottyPredef.scala
+++ src/dotty/DottyPredef.scala
@@ -4,22 +4,34 @@ import scala.reflect.ClassTag
 import scala.reflect.runtime.universe.TypeTag
 import scala.Predef.???  // this is currently ineffective, because of #530
 
-object DottyPredef {
-  /** implicits for ClassTag and TypeTag. Should be implemented with macros */
+abstract class I1 {
   implicit def classTag[T]: ClassTag[T] = scala.Predef.???
   implicit def typeTag[T]: TypeTag[T] = scala.Predef.???
-
-
-  /** ClassTags for final classes */
-  implicit val IntClassTag: ClassTag[Int]         = ClassTag.Int
-  implicit val ByteClassTag: ClassTag[Byte]       = ClassTag.Byte
-  implicit val ShortClassTag: ClassTag[Short]     = ClassTag.Short
-  implicit val CharClassTag: ClassTag[Char]       = ClassTag.Char
-  implicit val LongClassTag: ClassTag[Long]       = ClassTag.Long
-  implicit val FloatClassTag: ClassTag[Float]     = ClassTag.Float
   implicit val DoubleClassTag: ClassTag[Double]   = ClassTag.Double
+}
+abstract class I2 extends I1 {
+  implicit val FloatClassTag: ClassTag[Double]   = ClassTag.Double
+}
+abstract class I3 extends I2 {
+  implicit val LongClassTag: ClassTag[Long]       = ClassTag.Long
+}
+abstract class I4 extends I3 {
+  implicit val IntClassTag: ClassTag[Int]         = ClassTag.Int
+}
+abstract class I5 extends I4 {
+  implicit val ShortClassTag: ClassTag[Short]     = ClassTag.Short
+}
+abstract class I6 extends I5 {
+  implicit val ByteClassTag: ClassTag[Byte]       = ClassTag.Byte
+  implicit val CharClassTag: ClassTag[Char]       = ClassTag.Char
   implicit val BooleanClassTag: ClassTag[Boolean] = ClassTag.Boolean
   implicit val UnitClassTag: ClassTag[Unit]       = ClassTag.Unit
   implicit val NullClassTag: ClassTag[Null]       = ClassTag.Null
+}
+
+/** implicits for ClassTag and TypeTag. Should be implemented with macros */
+object DottyPredef extends I6 {
+
+  /** ClassTags for final classes */
   implicit val NothingClassTag: ClassTag[Nothing] = ClassTag.Nothing
 }
