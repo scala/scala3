d012514d2f6176c311e7f09b82e416f23505b656
Dmitry Petrashko <dmitry.petrashko@gmail.com>
2015-08-17 15:46:30 +0200

    Scala2 supecraccessors are pickled as private, but compiled as public expanded
    Need to do the very same transformation in AugmentScala2Traits.

--- src/dotty/tools/dotc/transform/AugmentScala2Traits.scala
+++ src/dotty/tools/dotc/transform/AugmentScala2Traits.scala
@@ -24,7 +24,7 @@ import ast.Trees._
  *   - an implementation class which defines a trait constructor and trait method implementations
  *   - trait setters for vals defined in traits
  *
- *  Furthermore, it expands the names of all private getters and setters in the trait and makes
+ *  Furthermore, it expands the names of all private getters and setters as well as super accessors in the trait and makes
  *  them not-private.
  */
 class AugmentScala2Traits extends MiniPhaseTransform with IdentityDenotTransformer with FullParameterization { thisTransform =>
@@ -89,8 +89,9 @@ class AugmentScala2Traits extends MiniPhaseTransform with IdentityDenotTransform
           else if (!sym.is(Deferred) && !sym.setter.exists &&
                    !sym.info.resultType.isInstanceOf[ConstantType])
             traitSetter(sym.asTerm).enteredAfter(thisTransform)
-        if (sym.is(PrivateAccessor, butNot = ExpandedName) &&
+        if ((sym.is(PrivateAccessor, butNot = ExpandedName) &&
           (sym.isGetter || sym.isSetter)) // strangely, Scala 2 fields are also methods that have Accessor set.
+          || sym.is(SuperAccessor)) // scala2 superaccessors are pickled as private, but are compiler as public expanded
           sym.ensureNotPrivate.installAfter(thisTransform)
       }
       ctx.log(i"Scala2x trait decls of $mixin = ${mixin.info.decls.toList.map(_.showDcl)}%\n %")
