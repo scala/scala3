869bdb0503628e8665292073087b08b91399a6c9
Dmitry Petrashko <dmitry.petrashko@gmail.com>
2015-08-24 17:46:20 +0200

    Test behaviour of final vals.

---
+++ tests/run/final-fields.scala
@@ -0,0 +1,18 @@
+trait T {
+
+ val f1: Int = {println("T.f1"); -1}
+ val f2: Int = {println("T.f2"); -2}
+ val f3: Int = {println("T.f3"); -3}
+ val f4: Int = {println("T.f4"); -4}
+
+ println(s"$f1 $f2 $f3 $f4")
+}
+
+object Test extends T {
+ override final val f1 = /*super.f1*/ 1 + f2
+ override final val f2 = 2
+ override final val f3 = {println(3); 3}
+ override val f4 = 4
+
+ def main(args: Array[String]): Unit = {}
+}
