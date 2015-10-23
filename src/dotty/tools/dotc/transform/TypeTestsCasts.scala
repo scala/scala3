14d20d9bacb713da046ada786673935e4ff4a8c2
Dmitry Petrashko <dmitry.petrashko@gmail.com>
2015-10-23 10:41:47 +0200

    Revert "Workaround #840"
    This reverts commit acd7b9d31cd0a2fbc285921bdc5a7bdc9c1d2872.

--- src/dotty/tools/dotc/transform/TypeTestsCasts.scala
+++ src/dotty/tools/dotc/transform/TypeTestsCasts.scala
@@ -47,7 +47,7 @@ trait TypeTestsCasts {
 
         def transformIsInstanceOf(expr:Tree, argType: Type): Tree = {
           def argCls = argType.classSymbol
-          if (false && expr.tpe <:< argType)
+          if ((expr.tpe <:< argType) && isPureExpr(expr))
             Literal(Constant(true)) withPos tree.pos
           else if (argCls.isPrimitiveValueClass)
             if (qualCls.isPrimitiveValueClass) Literal(Constant(qualCls == argCls)) withPos tree.pos
