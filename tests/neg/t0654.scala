24ef49b2aba670aa1ad9211c05f7f511e6eb7ed6
Martin Odersky <odersky@gmail.com>
2015-12-22 14:06:01 +0100

    Fix off by 2 error for symbol positions

--- tests/neg/t0654.scala
+++
@@ -1,5 +0,0 @@
-object Test {
-  class Foo[T]
-  type C[T] = Foo[_ <: T]   // error: parameter type T of type alias does not appear as type argument of the aliased class Foo
-  val a: C[AnyRef] = new Foo[AnyRef]
-}
