f20f2a0d25eb4d063b62993afe0bb3fbd3257312
Martin Odersky <odersky@gmail.com>
2016-01-10 16:04:41 +0100

    Make some operations surivive partial named parameter lists
    Types#underlyingClassRef and PostTyper#normalizeTree need to be changed so they can
    deal with partial named parameter lists.

--- src/dotty/tools/dotc/typer/Checking.scala
+++ src/dotty/tools/dotc/typer/Checking.scala
@@ -49,12 +49,22 @@ object Checking {
   def checkBounds(args: List[tpd.Tree], poly: PolyType)(implicit ctx: Context): Unit =
     checkBounds(args, poly.paramBounds, _.substParams(poly, _))
 
-  /** Check all AppliedTypeTree nodes in this tree for legal bounds */
+  /** Check all AppliedTypeTree nodes in this tree for legal bounds @@@ */
   val typeChecker = new TreeTraverser {
     def traverse(tree: Tree)(implicit ctx: Context) = {
       tree match {
         case AppliedTypeTree(tycon, args) =>
-          val tparams = tycon.tpe.typeSymbol.typeParams
+          // If `args` is a list of named argiments, return corresponding type parameters,
+          // otherwise return type parameters unchanged
+          def matchNamed(tparams: List[TypeSymbol], args: List[Tree]): List[TypeSymbol] = args match {
+            case Nil => Nil
+            case NamedArg(name, arg) :: args1 =>
+              val (matchingParam :: Nil, others) = tparams.partition(_.name == name)
+              matchingParam :: matchNamed(others, args1)
+            case _ =>
+              tparams
+          }
+          val tparams = matchNamed(tycon.tpe.typeSymbol.typeParams, args)
           val bounds = tparams.map(tparam =>
             tparam.info.asSeenFrom(tycon.tpe.normalizedPrefix, tparam.owner.owner).bounds)
           checkBounds(args, bounds, _.substDealias(tparams, _))
