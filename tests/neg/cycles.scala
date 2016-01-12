dbf04a67bbd06c5beebb958d0012ae300a20d0d1
Martin Odersky <odersky@gmail.com>
2016-01-12 11:37:21 +0100

    Annotate test with // error indications
    We had same fleyness in number of errors of cycle.scala
    which prompted this.

--- tests/neg/cycles.scala
+++ tests/neg/cycles.scala
@@ -1,21 +1,22 @@
-class Foo[T <: U, U <: T] // error: cycle
+class Foo[T <: U, U <: T] // error: illegal cyclic reference: upper bound U of type T refers back to the type itself
 
-class Bar[T >: T] // error: cycle
+class Bar[T >: T] // error: illegal cyclic reference: lower bound T of type T refers back to the type itself
+
 
 class A {
   val x: T = ???
-  type T <: x.type // error: cycle
+  type T <: x.type // error: cyclic reference involving value x
 }
 
 class B {
-  type T <: x.type // error: cycle
-  final val x: T = ???
+  type T <: x.type // error: illegal cyclic reference: upper bound B.this.T(B.this.x) of type T refers back to the type itself
+  val x: T = ???
 }
 
 class C {
   final val x: D#T = ???
   class D {
-    type T <: x.type // error: cycle
+    type T <: x.type // error: cyclic reference involving value x
     val z: x.type = ???
   }
 }
