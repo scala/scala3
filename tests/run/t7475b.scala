2042adc79cff35cdafef4130b74ed90d0d5841bf
Martin Odersky <odersky@gmail.com>
2016-01-19 19:13:31 +0100

    Extend test
    We verified that before the combination abstract/concrete for `x`
    also led to AMEs. So we test it here explicitly, too.

--- tests/run/t7475b.scala
+++ tests/run/t7475b.scala
@@ -1,11 +1,16 @@
 trait A { private val x = 1 }
 trait B { val x = 2 }
+trait C { val x: Int }
 trait C1 extends B with A { println(x) }
 trait C2 extends A with B { println(x) }
+trait C3 extends C with B { println(x) }
+trait C4 extends B with C { println(x) }
 
 object Test {
   def main(args: Array[String]): Unit = {
     new C1 { }
     new C2 { }
+    new C3 { }
+    new C4 { }
   }
 }
