2e3e67f87cefb2e5c3135b524d41a98e6acbf688
Martin Odersky <odersky@gmail.com>
2016-02-04 18:50:45 +0100

    Fix doc comment

--- src/dotty/tools/dotc/typer/ProtoTypes.scala
+++ src/dotty/tools/dotc/typer/ProtoTypes.scala
@@ -379,19 +379,6 @@ object ProtoTypes {
     }
   }
 
-  /** Like wildApprox, but assume that all parameters of `disregarded` are not
-   *  bound in current constraint. This is necessary because an instance of
-   *  the type of a polymorphic function might already be part of the current
-   *  constraint when we try to check another occurrence of the same polymorphic
-   *  function for eligibility. An example is in i1044.scala where one expression
-   *  requires two implicit calls of the same refArrayOps method. When approximating
-   *  the parameter types of such a method we should always ignore type parameters of
-   *  the method itself, even though some of these parameters might still be
-   *  bound in the constraint.
-   */
-  final def wildApprox(tp: Type, disregarded: PolyType)(implicit ctx: Context): Type =
-    wildApprox(tp, new WildApproxMap(disregarded))
-
   /** Approximate occurrences of parameter types and uninstantiated typevars
    *  by wildcard types.
    */
@@ -441,6 +428,19 @@ object ProtoTypes {
     case _ =>
       (if (theMap != null) theMap else new WildApproxMap).mapOver(tp)
   }
+
+  /** Like unary wildApprox, but assume that all parameters of `disregarded` are not
+   *  bound in current constraint. This is necessary because an instance of
+   *  the type of a polymorphic function might already be part of the current
+   *  constraint when we try to check another occurrence of the same polymorphic
+   *  function for eligibility. An example is in i1044.scala where one expression
+   *  requires two implicit calls of the same refArrayOps method. When approximating
+   *  the parameter types of such a method we should always map the type parameters of
+   *  the method itself to wildcards, even though some of these parameters might still be
+   *  bound in the constraint.
+   */
+  final def wildApprox(tp: Type, disregarded: PolyType)(implicit ctx: Context): Type =
+    wildApprox(tp, new WildApproxMap(disregarded))
 
   private[ProtoTypes] class WildApproxMap(val disregarded: Type = NoType)(implicit ctx: Context) extends TypeMap {
     def apply(tp: Type) = wildApprox(tp, this)
