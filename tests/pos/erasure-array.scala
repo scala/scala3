e384b3af6859c7e2d8481288e293ee9d86a377b9
Dmitry Petrashko <dmitry.petrashko@gmail.com>
2016-02-08 15:04:07 +0100

    Test that #1065 is fixed.

---
+++ tests/pos/erasure-array.scala
@@ -0,0 +1,12 @@
+// https://github.com/lampepfl/dotty/issues/1065
+package hello
+
+object world {
+  def mkArray(atype: Int): Array[_ <: AnyVal] = {
+    (if (atype == 1) new Array[Int](10) else new Array[Float](10))
+  }
+
+  def main(args: Array[String]): Unit = {
+    println(mkArray(1))
+  }
+}
