b6decfaeddcac550517d4803ab88091a48fc5054
Martin Odersky <odersky@gmail.com>
2016-02-18 23:00:34 +0100

    Fix ClassTag.arrayTag
    Cannot simply use `wrap` as it does the wrong thing for
    Array[Unit] (what a mess!).

--- src/dotty/DottyPredef.scala
+++ src/dotty/DottyPredef.scala
@@ -2,14 +2,18 @@ package dotty
 
 import scala.reflect.runtime.universe.TypeTag
 import scala.reflect.ClassTag
-import scala.Predef.???
+import scala.Predef.{???, implicitly}
 
 /** unimplemented implicit for TypeTag */
 object DottyPredef {
   implicit def typeTag[T]: TypeTag[T] = ???
 
-  implicit def arrayTag[T](implicit ctag: ClassTag[T]): ClassTag[Array[T]] =
-    ctag.wrap
+  implicit def arrayTag[T](implicit ctag: ClassTag[T]): ClassTag[Array[T]] = {
+    val ctag1 =
+      if (ctag == ClassTag.Unit) implicitly[ClassTag[scala.runtime.BoxedUnit]]
+      else ctag
+    ctag1.wrap.asInstanceOf[ClassTag[Array[T]]]
+  }
 
   def classOf[T](implicit ctag: ClassTag[T]): Class[T] =
     ctag.runtimeClass.asInstanceOf[Class[T]]
