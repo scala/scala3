c5ac21b65f997d3dd4b491d9ba0f06cf65320e33
Martin Odersky <odersky@gmail.com>
2016-02-18 17:23:50 +0100

    Fix search of Array classTags.
    If a classtag for `T` is available, a classtag
    for `Array[T]` can also be generated.

--- src/dotty/DottyPredef.scala
+++ src/dotty/DottyPredef.scala
@@ -1,13 +1,13 @@
 package dotty
 
 import scala.reflect.runtime.universe.TypeTag
+import scala.reflect.ClassTag
 import scala.Predef.???
 
 /** unimplemented implicit for TypeTag */
 object DottyPredef {
   implicit def typeTag[T]: TypeTag[T] = ???
 
-// not yet:
-// def classOf[T](implicit ctag: ClassTag[T]): Class[T] =
-//    ctag.runtimeClass
+  implicit def arrayTag[T](implicit ctag: ClassTag[T]): ClassTag[Array[T]] =
+    ctag.wrap
 }
