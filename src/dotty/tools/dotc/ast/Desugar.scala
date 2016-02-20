b54bc22516b97f977aeffd4a2c5b2e2e3a3c727a
Martin Odersky <odersky@gmail.com>
2016-02-20 19:05:20 +0100

    Set type of default getter method
    Copy type from default parameter to default getter method.

--- src/dotty/tools/dotc/ast/Desugar.scala
+++ src/dotty/tools/dotc/ast/Desugar.scala
@@ -176,7 +176,7 @@ object desugar {
             name = meth.name.defaultGetterName(n),
             tparams = meth.tparams.map(tparam => dropContextBound(toDefParam(tparam))),
             vparamss = takeUpTo(normalizedVparamss, n),
-            tpt = TypeTree(),
+            tpt = vparam.tpt,
             rhs = vparam.rhs
           ).withMods(Modifiers(mods.flags & AccessFlags, mods.privateWithin))
         val rest = defaultGetters(vparams :: vparamss1, n + 1)
