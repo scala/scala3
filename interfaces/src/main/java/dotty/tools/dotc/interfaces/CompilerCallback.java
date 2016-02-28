smarter/dotty

Add a `dotty-interfaces` package
We introduce a new entry point for the compiler in
`dotty.tools.dotc.Driver`:
```
def process(args: Array[String], simple: interfaces.SimpleReporter,
  callback: interfaces.CompilerCallback): interfaces.ReporterResult
```
Except for `args` which is just an array, the argument types and return
type of this method are Java interfaces defined in a new package called
`dotty-interfaces` which has a stable ABI. This means that you can
programmatically run a compiler with a custom reporter and callbacks
without having to recompile it against every version of dotty: you only
need to have `dotty-interfaces` present at compile-time and call the
`process` method using Java reflection.

See `test/test/InterfaceEntryPointTest.scala` for a concrete example.

This design is based on discussions with the IntelliJ IDEA Scala plugin
team. Thanks to Nikolay Tropin for the discussions and his PR
proposal (see #1011).

interfaces/src/main/java/dotty/tools/dotc/interfaces/CompilerCallback.java
@@ -1,0 +1,30 @@
+package dotty.tools.dotc.interfaces;
+
+import java.io.File;
+
+/** Set of callbacks called in response to events during the compilation process.
+ *
+ *  You should implement this interface if you want to react to one or more of
+ *  these events.
+ *
+ *  @see the method `process` of `dotty.tools.dotc.Driver` for more information.
+ */
+public interface CompilerCallback {
+  /** Called when a class has been generated.
+   *
+   *  @param source         The source file corresponding to this class.
+   *                        Example: ./src/library/scala/collection/Seq.scala
+   *  @param generatedClass The generated classfile for this class.
+   *                        Example: ./scala/collection/Seq$.class
+   *  @param className      The name of this class.
+   *                        Example: scala.collection.Seq$
+   */
+  default void onClassGenerated(File source, File generatedClass, String className) {};
+
+  /** Called when every class for this file has been generated.
+   *
+   *  @param source         The source file.
+   *                        Example: ./src/library/scala/collection/Seq.scala
+   */
+  default void onSourceCompiled(File source) {};
+}

test/test/InterfaceEntryPointTest.scala
@@ -1,0 +1,61 @@
+package test
+
+import org.junit.Test
+import org.junit.Assert._
+import dotty.tools.dotc.interfaces._
+import java.io.File
+import scala.collection.mutable.ListBuffer
+
+/** Test that demonstrates how to use dotty-interfaces
+ *
+ *  This test requires:
+ *  - dotty-interfaces to be present at compile-time
+ *  - dotty-interfaces and dotty to be present at run-time
+ *
+ *  Since the ABI of dotty-interfaces is stable, this means that users can write
+ *  code that works with multiple versions of dotty without recompilation.
+ *
+ *  @see [[OtherEntryPointsTest]]
+ */
+class InterfaceEntryPointTest {
+  @Test def runCompilerFromInterface = {
+    val sources = List("./tests/pos/HelloWorld.scala")
+    val args = sources ++ List("-d", "./out/")
+
+    val mainClass = Class.forName("dotty.tools.dotc.Main")
+    val process = mainClass.getMethod("process",
+      classOf[Array[String]], classOf[SimpleReporter], classOf[CompilerCallback])
+
+    val reporter = new CustomSimpleReporter
+    val callback = new CustomCompilerCallback
+
+    // Run the compiler by calling dotty.tools.dotc.Main.process
+    process.invoke(null, args.toArray, reporter, callback)
+
+    assertEquals("Number of errors", 0, reporter.errorCount)
+    assertEquals("Number of warnings", 0, reporter.warningCount)
+    assertEquals("Compiled sources", sources, callback.paths)
+  }
+
+  private class CustomSimpleReporter extends SimpleReporter {
+    var errorCount = 0
+    var warningCount = 0
+
+    def report(diag: Diagnostic): Unit = {
+      if (diag.level == Diagnostic.ERROR)
+        errorCount += 1
+      if (diag.level == Diagnostic.WARNING)
+        warningCount += 1
+    }
+  }
+
+  private class CustomCompilerCallback extends CompilerCallback {
+    private val pathsBuffer = new ListBuffer[String]
+    def paths = pathsBuffer.toList
+
+    override def onSourceCompiled(source: File): Unit = {
+      pathsBuffer += source.getPath
+    }
+  }
+}
+

test/test/OtherEntryPointsTest.scala
@@ -1,0 +1,65 @@
+package test
+
+import org.junit.Test
+import org.junit.Assert._
+import dotty.tools.dotc.Main
+import dotty.tools.dotc.interfaces.CompilerCallback
+import dotty.tools.dotc.reporting._
+import dotty.tools.dotc.core.Contexts._
+import java.io.File
+import scala.collection.mutable.ListBuffer
+
+/** Test the compiler entry points that depend on dotty
+ *
+ *  This file also serve as an example for using [[dotty.tools.dotc.Driver#process]].
+ *
+ *  @see [[InterfaceEntryPointTest]]
+ */
+class OtherEntryPointsTest {
+  @Test def runCompiler = {
+    val sources = List("./tests/pos/HelloWorld.scala")
+    val args = sources ++ List("-d", "./out/")
+
+    val reporter = new CustomReporter
+    val callback = new CustomCompilerCallback
+
+    Main.process(args.toArray, reporter, callback)
+
+    assertEquals("Number of errors", false, reporter.hasErrors)
+    assertEquals("Number of warnings", false, reporter.hasWarnings)
+    assertEquals("Compiled sources", sources, callback.paths)
+  }
+
+  @Test def runCompilerWithContext = {
+    val sources = List("./tests/pos/HelloWorld.scala")
+    val args = sources ++ List("-d", "./out/")
+
+    val reporter = new CustomReporter
+    val callback = new CustomCompilerCallback
+    val context = (new ContextBase).initialCtx.fresh
+      .setReporter(reporter)
+      .setCompilerCallback(callback)
+
+    Main.process(args.toArray, context)
+
+    assertEquals("Number of errors", false, reporter.hasErrors)
+    assertEquals("Number of warnings", false, reporter.hasWarnings)
+    assertEquals("Compiled sources", sources, callback.paths)
+  }
+
+  private class CustomReporter extends Reporter
+      with UniqueMessagePositions
+      with HideNonSensicalMessages {
+    def doReport(d: Diagnostic)(implicit ctx: Context): Unit = {
+    }
+  }
+
+  private class CustomCompilerCallback extends CompilerCallback {
+    private val pathsBuffer = new ListBuffer[String]
+    def paths = pathsBuffer.toList
+
+    override def onSourceCompiled(source: File): Unit = {
+      pathsBuffer += source.getPath
+    }
+  }
+}

scala/scala3

interfaces/src/main/java/dotty/tools/dotc/interfaces/CompilerCallback.java
@@ -1,0 +1,28 @@
+package dotty.tools.dotc.interfaces;
+
+/** Set of callbacks called in response to events during the compilation process.
+ *
+ *  You should implement this interface if you want to react to one or more of
+ *  these events.
+ *
+ *  @see the method `process` of `dotty.tools.dotc.Driver` for more information.
+ */
+public interface CompilerCallback {
+  /** Called when a class has been generated.
+   *
+   *  @param source         The source file corresponding to this class.
+   *                        Example: ./src/library/scala/collection/Seq.scala
+   *  @param generatedClass The generated classfile for this class.
+   *                        Example: ./scala/collection/Seq$.class
+   *  @param className      The name of this class.
+   *                        Example: scala.collection.Seq$
+   */
+  default void onClassGenerated(SourceFile source, AbstractFile generatedClass, String className) {};
+
+  /** Called when every class for this file has been generated.
+   *
+   *  @param source         The source file.
+   *                        Example: ./src/library/scala/collection/Seq.scala
+   */
+  default void onSourceCompiled(SourceFile source) {};
+}

test/test/InterfaceEntryPointTest.scala
@@ -1,0 +1,61 @@
+package test
+
+import org.junit.Test
+import org.junit.Assert._
+import dotty.tools.dotc.interfaces._
+import scala.collection.mutable.ListBuffer
+
+/** Test that demonstrates how to use dotty-interfaces
+ *
+ *  This test requires:
+ *  - dotty-interfaces to be present at compile-time
+ *  - dotty-interfaces and dotty to be present at run-time
+ *
+ *  Since the ABI of dotty-interfaces is stable, this means that users can write
+ *  code that works with multiple versions of dotty without recompilation.
+ *
+ *  @see [[OtherEntryPointsTest]]
+ */
+class InterfaceEntryPointTest {
+  @Test def runCompilerFromInterface = {
+    val sources = List("./tests/pos/HelloWorld.scala")
+    val args = sources ++ List("-d", "./out/")
+
+    val mainClass = Class.forName("dotty.tools.dotc.Main")
+    val process = mainClass.getMethod("process",
+      classOf[Array[String]], classOf[SimpleReporter], classOf[CompilerCallback])
+
+    val reporter = new CustomSimpleReporter
+    val callback = new CustomCompilerCallback
+
+    // Run the compiler by calling dotty.tools.dotc.Main.process
+    process.invoke(null, args.toArray, reporter, callback)
+
+    assertEquals("Number of errors", 0, reporter.errorCount)
+    assertEquals("Number of warnings", 0, reporter.warningCount)
+    assertEquals("Compiled sources", sources, callback.paths)
+  }
+
+  private class CustomSimpleReporter extends SimpleReporter {
+    var errorCount = 0
+    var warningCount = 0
+
+    def report(diag: Diagnostic): Unit = {
+      if (diag.level == Diagnostic.ERROR)
+        errorCount += 1
+      if (diag.level == Diagnostic.WARNING)
+        warningCount += 1
+    }
+  }
+
+  private class CustomCompilerCallback extends CompilerCallback {
+    private val pathsBuffer = new ListBuffer[String]
+    def paths = pathsBuffer.toList
+
+    override def onSourceCompiled(source: SourceFile): Unit = {
+      if (source.jfile.isPresent)
+        pathsBuffer += source.jfile.get.getPath
+    }
+  }
+}
+

test/test/OtherEntryPointsTest.scala
@@ -1,0 +1,66 @@
+package test
+
+import org.junit.Test
+import org.junit.Assert._
+import dotty.tools.dotc.Main
+import dotty.tools.dotc.interfaces.{CompilerCallback, SourceFile}
+import dotty.tools.dotc.reporting._
+import dotty.tools.dotc.core.Contexts._
+import java.io.File
+import scala.collection.mutable.ListBuffer
+
+/** Test the compiler entry points that depend on dotty
+ *
+ *  This file also serve as an example for using [[dotty.tools.dotc.Driver#process]].
+ *
+ *  @see [[InterfaceEntryPointTest]]
+ */
+class OtherEntryPointsTest {
+  @Test def runCompiler = {
+    val sources = List("./tests/pos/HelloWorld.scala")
+    val args = sources ++ List("-d", "./out/")
+
+    val reporter = new CustomReporter
+    val callback = new CustomCompilerCallback
+
+    Main.process(args.toArray, reporter, callback)
+
+    assertEquals("Number of errors", false, reporter.hasErrors)
+    assertEquals("Number of warnings", false, reporter.hasWarnings)
+    assertEquals("Compiled sources", sources, callback.paths)
+  }
+
+  @Test def runCompilerWithContext = {
+    val sources = List("./tests/pos/HelloWorld.scala")
+    val args = sources ++ List("-d", "./out/")
+
+    val reporter = new CustomReporter
+    val callback = new CustomCompilerCallback
+    val context = (new ContextBase).initialCtx.fresh
+      .setReporter(reporter)
+      .setCompilerCallback(callback)
+
+    Main.process(args.toArray, context)
+
+    assertEquals("Number of errors", false, reporter.hasErrors)
+    assertEquals("Number of warnings", false, reporter.hasWarnings)
+    assertEquals("Compiled sources", sources, callback.paths)
+  }
+
+  private class CustomReporter extends Reporter
+      with UniqueMessagePositions
+      with HideNonSensicalMessages {
+    def doReport(d: Diagnostic)(implicit ctx: Context): Unit = {
+    }
+  }
+
+  private class CustomCompilerCallback extends CompilerCallback {
+    private val pathsBuffer = new ListBuffer[String]
+    def paths = pathsBuffer.toList
+
+    override def onSourceCompiled(source: SourceFile): Unit = {
+      if (source.jfile.isPresent)
+        pathsBuffer += source.jfile.get.getPath
+    }
+  }
+}