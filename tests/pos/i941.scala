e2cdfa3123d311773d34b10f5979181cecd8acf1
Martin Odersky <odersky@gmail.com>
2016-04-07 17:59:44 +0200

    Augment test case

--- tests/pos/i941.scala
+++ tests/pos/i941.scala
@@ -7,4 +7,18 @@ object Test {
     def foo(x: A): Unit = ()
 
   }
+  class D[A](x: A) {
+
+    var f = x
+
+    def this(y1: A, y2: A) = {
+      this(y1)
+      f = y2
+      val g: A = f
+      foo(y2)
+    }
+
+    def foo(x: A): Unit = ()
+
+  }
 }
