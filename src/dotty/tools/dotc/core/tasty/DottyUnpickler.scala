5aa59ac17e6f48719f8b52048e0b86570e9399e3
Martin Odersky <odersky@gmail.com>
2016-04-09 11:16:10 +0200

    Simplify handling of sourcefiles in Tasty info
    Instead of separate source file sections, pickle SourceFile as an
    annotation of all toplevel classes.
    
    We represent it like this anyway when reading back Tasty-defined classes.

--- src/dotty/tools/dotc/core/tasty/DottyUnpickler.scala
+++ src/dotty/tools/dotc/core/tasty/DottyUnpickler.scala
@@ -6,7 +6,6 @@ package tasty
 import Contexts._, SymDenotations._, Symbols._
 import dotty.tools.dotc.ast.tpd
 import TastyUnpickler._, TastyBuffer._
-import dotty.tools.dotc.core.tasty.DottyUnpickler.{SourceFileUnpickler, TreeSectionUnpickler, PositionsSectionUnpickler}
 import util.Positions._
 import util.{SourceFile, NoSource}
 import PositionUnpickler._
@@ -17,13 +16,6 @@ object DottyUnpickler {
 
   /** Exception thrown if classfile is corrupted */
   class BadSignature(msg: String) extends RuntimeException(msg)
-
-  class SourceFileUnpickler extends SectionUnpickler[SourceFile]("Sourcefile") {
-    def unpickle(reader: TastyReader, tastyName: TastyName.Table) = {
-      val TastyName.Simple(sourceName) = tastyName(reader.readNameRef())
-      new SourceFile(sourceName.toString, Seq())
-    }
-  }
 
   class TreeSectionUnpickler extends SectionUnpickler[TreeUnpickler]("ASTs") {
     def unpickle(reader: TastyReader, tastyName: TastyName.Table) =
@@ -41,27 +33,24 @@ object DottyUnpickler {
  */
 class DottyUnpickler(bytes: Array[Byte]) extends ClassfileParser.Embedded {
   import tpd._
+  import DottyUnpickler._
 
   val unpickler = new TastyUnpickler(bytes)
   private val treeUnpickler = unpickler.unpickle(new TreeSectionUnpickler).get
-  private val source = unpickler.unpickle(new SourceFileUnpickler).getOrElse(NoSource)
 
   /** Enter all toplevel classes and objects into their scopes
    *  @param roots          a set of SymDenotations that should be overwritten by unpickling
    */
-  def enter(roots: Set[SymDenotation])(implicit ctx: Context): Unit = {
+  def enter(roots: Set[SymDenotation])(implicit ctx: Context): Unit =
     treeUnpickler.enterTopLevel(roots)
-    if (source.exists)
-      for (root <- roots) root.addAnnotation(Annotation.makeSourceFile(source.path))
-  }
 
   /** The unpickled trees, and the source file they come from
    *  @param readPositions if true, trees get decorated with position information.
    */
-  def body(readPositions: Boolean = false)(implicit ctx: Context): (List[Tree], SourceFile) = {
+  def body(readPositions: Boolean = false)(implicit ctx: Context): List[Tree] = {
     if (readPositions)
       for ((totalRange, positions) <- unpickler.unpickle(new PositionsSectionUnpickler))
         treeUnpickler.usePositions(totalRange, positions)
-    (treeUnpickler.unpickle(), source)
+    treeUnpickler.unpickle()
   }
 }
