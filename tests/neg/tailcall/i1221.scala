d20314fd9a741e1a3203dbacf0080e9e923559ca
Dmitry Petrashko <dmitry.petrashko@gmail.com>
2016-04-19 10:56:04 +0200

    Fix test failures in tailcall due to name clashes.

--- tests/neg/tailcall/i1221.scala
+++ tests/neg/tailcall/i1221.scala
@@ -1,6 +1,6 @@
 import annotation.tailrec
 
-object Test{
+object I1221{
   final def foo(a: Int): Int = {
   if ((foo(a - 1): @tailrec) > 0) // error: not in tail position
     foo(a - 1): @tailrec
--- tests/pos/tailcall/i1221.scala
+++ tests/pos/tailcall/i1221.scala
@@ -1,6 +1,6 @@
 import annotation.tailrec
 
-object Test{
+object i1221{
   final def foo(a: Int): Int = {
   if (foo(a - 1) > 0) 
     foo(a - 1): @tailrec 
