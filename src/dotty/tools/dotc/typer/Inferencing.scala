05bfe78c6f881884272e31cfd1eea993a1ad9f8a
Martin Odersky <odersky@gmail.com>
2016-04-22 17:33:12 +0200

    Comment out debug println

--- src/dotty/tools/dotc/typer/Inferencing.scala
+++ src/dotty/tools/dotc/typer/Inferencing.scala
@@ -81,7 +81,7 @@ object Inferencing {
         force.appliesTo(tvar) && {
           val direction = instDirection(tvar.origin)
           if (direction != 0) {
-            if (direction > 0) println(s"inst $tvar dir = up")
+            //if (direction > 0) println(s"inst $tvar dir = up")
             instantiate(tvar, direction < 0)
           }
           else {
