9dcd6d8d43792e485a5a61e21a558609ccb7b89c
Martin Odersky <odersky@gmail.com>
2016-04-26 11:58:38 +0200

    Eq and EqClass definitions
    Trait definitions supporting equality

---
+++ src/scala/EqClassOf.scala
@@ -0,0 +1,7 @@
+package scala
+
+/** A class providing a specialized notion of equality, allowing
+ *  only values in the same equality class to be compared.
+ */
+trait EqClassOf[-T] extends EqClass
+
