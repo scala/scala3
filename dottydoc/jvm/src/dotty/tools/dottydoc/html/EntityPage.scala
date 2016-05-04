cb42a04bfcd7548261cda15c489671b8ca09cd7e
Felix Mulder <felix.mulder@gmail.com>
2016-05-04 09:46:13 +0200

    Filter out ProductX when X > 3 in package view

--- dottydoc/jvm/src/dotty/tools/dottydoc/html/EntityPage.scala
+++ dottydoc/jvm/src/dotty/tools/dottydoc/html/EntityPage.scala
@@ -85,13 +85,19 @@ case class EntityPage(entity: Entity, packages: Map[String, Package]) {
     cls := "mdl-list packages",
     {
       val keys: Seq[String] = packages.keys.toSeq.sorted
+      val productReg = """^Product[0-9]+$""".r
       keys.flatMap { k =>
         val pack = packages(k)
         val children =
           pack.children
             .sortBy(_.name)
             .filterNot { ent =>
-              ent.kind == "package" || (ent.kind == "object" && companion(ent).isDefined)
+              // Filter out ProductX where X > 3
+              productReg.findFirstIn(ent.name).map(_.slice(7, 99).toInt > 3).getOrElse(false) ||
+              // Filter out packages
+              ent.kind == "package" ||
+              // Filter out objects that have companions
+              (ent.kind == "object" && companion(ent).isDefined)
             }
             .map { entity =>
               val comp = companion(entity)
