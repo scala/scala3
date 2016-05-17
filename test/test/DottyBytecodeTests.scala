eaf2520b63fcc9fe65cf02d88c96849c4caf8177
Felix Mulder <felix.mulder@gmail.com>
2016-05-17 11:01:08 +0200

    Add bytecode checking infrastructure

---
+++ test/test/DottyBytecodeTests.scala
@@ -0,0 +1,25 @@
+package test
+
+import org.junit.Assert._
+import org.junit.Test
+
+class TestBCode extends DottyBytecodeTest {
+  @Test def nullChecks = {
+    val source = """
+                 |class Foo {
+                 |  def foo(x: AnyRef): Int = {
+                 |    val bool = x == null
+                 |    if (x != null) 1
+                 |    else 0
+                 |  }
+                 |}
+                 """.stripMargin
+
+    checkBCode(source) { file =>
+      val clsIn      = file.lookupName("Foo.class", directory = false).input
+      val clsNode    = loadClassNode(clsIn)
+      val methodNode = getMethod(clsNode, "foo")
+      correctNumberOfNullChecks(2, methodNode.instructions)
+    }
+  }
+}
