fa81e54db9ebb36c7f76cbedfa98115e50a8f88c
Felix Mulder <felix.mulder@gmail.com>
2016-05-26 11:41:19 +0200

    Take side-effects into account during rewriting

--- src/dotty/tools/dotc/transform/IsInstanceOfEvaluator.scala
+++ src/dotty/tools/dotc/transform/IsInstanceOfEvaluator.scala
@@ -91,11 +91,16 @@ class IsInstanceOfEvaluator extends MiniPhaseTransform { thisTransformer =>
      *
      *  `scrutinee.isInstanceOf[Selector]` if `scrutinee eq null`
      */
-    def rewrite(tree: Select, to: Boolean): Tree =
-      if (!to || !tree.qualifier.tpe.widen.derivesFrom(defn.AnyRefAlias))
-        Literal(Constant(to))
-      else
-        Apply(tree.qualifier.select(defn.Object_ne), List(Literal(Constant(null))))
+    def rewrite(tree: Select, to: Boolean): Tree = {
+      val expr =
+        if (!to || !tree.qualifier.tpe.widen.derivesFrom(defn.AnyRefAlias))
+          Literal(Constant(to))
+        else
+          Apply(tree.qualifier.select(defn.Object_ne), List(Literal(Constant(null))))
+
+      if (!isPureExpr(tree.qualifier)) Block(List(tree.qualifier), expr)
+      else expr
+    }
 
     /** Attempts to rewrite TypeApply to either `scrutinee ne null` or a
      *  constant
