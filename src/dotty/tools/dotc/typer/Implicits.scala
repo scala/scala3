044d29dd6b133bfb28468366ee92046b77adf6f9
Guillaume Martres <smarter@ubuntu.com>
2016-05-31 15:43:01 +0200

    Don't compute implicit scopes for synthetic Lambda traits

--- src/dotty/tools/dotc/typer/Implicits.scala
+++ src/dotty/tools/dotc/typer/Implicits.scala
@@ -284,10 +284,13 @@ trait ImplicitRunInfo { self: RunInfo =>
       override implicit protected val ctx: Context = liftingCtx
       override def stopAtStatic = true
       def apply(tp: Type) = tp match {
+        case tp: TypeRef if tp.symbol.isLambdaTrait =>
+          defn.AnyType
         case tp: TypeRef if tp.symbol.isAbstractOrAliasType =>
           val pre = tp.prefix
           def joinClass(tp: Type, cls: ClassSymbol) =
-            AndType.make(tp, cls.typeRef.asSeenFrom(pre, cls.owner))
+            if (cls.isLambdaTrait) tp
+            else AndType.make(tp, cls.typeRef.asSeenFrom(pre, cls.owner))
           val lead = if (tp.prefix eq NoPrefix) defn.AnyType else apply(tp.prefix)
           (lead /: tp.classSymbols)(joinClass)
         case tp: TypeVar =>
