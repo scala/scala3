6abde38668acd76cb8b4ef15b62d6015938f483c
Martin Odersky <odersky@gmail.com>
2016-06-29 17:34:29 +0200

    Get rid of BindingKind
    Under direct hk encoding this is no longer needed.
    Also, drop now redundant pieces of widenForMatchSelector.

--- src/dotty/tools/dotc/core/tasty/TastyFormat.scala
+++ src/dotty/tools/dotc/core/tasty/TastyFormat.scala
@@ -130,7 +130,7 @@ Standard-Section: "ASTs" TopLevelStat*
                   SUPERtype      Length this_Type underlying_Type
                   REFINEDtype    Length underlying_Type refinement_NameRef info_Type
                   APPLIEDtype    Length tycon_Type arg_Type*
-                  TYPEBOUNDS     Length low_Type high_Type bindingKind_Nat?
+                  TYPEBOUNDS     Length low_Type high_Type
                   TYPEALIAS      Length alias_Type (COVARIANT | CONTRAVARIANT)?
                   ANNOTATED      Length underlying_Type fullAnnotation_Term
                   ANDtype        Length left_Type right_Type
