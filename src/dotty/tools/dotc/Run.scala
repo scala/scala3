8a39160e8375dd1707f686e13a2a796aed310ef7
Edmund Noble <edmundnoble@gmail.com>
2016-07-02 14:24:17 -0400

    Implement -print as -Xprint:(last phase)

--- src/dotty/tools/dotc/Run.scala
+++ src/dotty/tools/dotc/Run.scala
@@ -62,7 +62,8 @@ class Run(comp: Compiler)(implicit ctx: Context) {
         units = phase.runOn(units)
         def foreachUnit(op: Context => Unit)(implicit ctx: Context): Unit =
           for (unit <- units) op(ctx.fresh.setPhase(phase.next).setCompilationUnit(unit))
-        if (ctx.settings.Xprint.value.containsPhase(phase))
+        if (ctx.settings.Xprint.value.containsPhase(phase) ||
+           (ctx.settings.print.value && phase == ctx.allPhases.last))
           foreachUnit(printTree)
         ctx.informTime(s"$phase ", start)
       }
