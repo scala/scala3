9e9b9807b0f272ef586a5a92e6fc87de46c6da9d
Clemens Winter <clemenswinter1@gmail.com>
2016-07-25 16:08:29 +0200

    Add tests for unbound wildcard types

---
+++ tests/pending/neg/unboundWildcard.scala
@@ -0,0 +1,21 @@
+object unboundWildcard {
+
+  // TODO: move this to tests/neg once it doesn't crash the compiler anymore
+  val wildcardVal: _ = 0  // error: unbound wildcard type
+
+  val annotated: _ @unchecked = 0  // error: unbound wildcard type
+
+  def wildcardArg(x: _): Int = 0  // error: unbound wildcard type
+
+  def wildcardResult(x: Int): _ = 0  // error: unbound wildcard type
+
+  val singletonTuple: (((((((_))))))) = ???  // error: unbound wildcard type
+
+  val wildcardBoundedTypeArgL: List[_ <: _] = List(0)  // error: unbound wildcard type
+  val wildcardBoundedTypeArgU: List[_ >: _] = List(0)  // error: unbound wildcard type
+
+  def wildcardBoundedTypeParamL[T <: _](x: T): T = x  // error: unbound wildcard type
+  def wildcardBoundedTypeParamU[T >: _](x: T): T = x  // error: unbound wildcard type
+
+  val _1403: (_ <: Any) = 1  // error: unbound wildcard type
+}
