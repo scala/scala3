290200037ba9633d1dc23dc02750c1396ee11045
Martin Odersky <odersky@gmail.com>
2016-07-29 20:37:38 +0200

    Index members of a class before evaluating its parents
    Avoids missing member in tangledCompanion.scala, which is a minimization
    of intermittent failures in CollectionStrawMan6. Intermittent, because it
    depended on order of compilation. CollectionTests have to be compiled together with
    but before CollectionStrawMan6 (this was _sometimes_ the case because partest did not
    honor indicated compilation order so far). I.e.
    
        dotc CollectionTests_2.scala CollectionStrawMan6_1.scala
    
    would trigger the error. tangledCompanion.scala captures the dependencies
    in a single file.

---
+++ tests/pos/tangledCompanion.scala
@@ -0,0 +1,26 @@
+object Test {
+
+  import Coll._
+  import LazyList.#::
+
+  val xs = LazyList.Empty
+
+}
+
+object Coll {
+
+  trait IterableFactory[+C[X]]
+
+  class LazyList[+A](expr: => LazyList.Evaluated[A])
+
+  object LazyList extends IterableFactory[LazyList] {
+
+    type Evaluated[+A] = Option[(A, LazyList[A])]
+
+    object Empty extends LazyList[Nothing](None)
+
+    object #:: {
+      def unapply[A](s: LazyList[A]): Evaluated[A] = ???
+    }
+  }
+}
