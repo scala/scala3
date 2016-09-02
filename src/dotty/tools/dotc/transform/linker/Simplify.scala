aa854ec35b77af677e017159a975049ccb53d443
Dmitry Petrashko <dmitry.petrashko@gmail.com>
2016-09-02 15:49:07 +0200

    Simplify: add vilify transformation

--- src/dotty/tools/dotc/transform/linker/Simplify.scala
+++ src/dotty/tools/dotc/transform/linker/Simplify.scala
@@ -104,6 +104,7 @@ class Simplify extends MiniPhaseTransform with IdentityDenotTransformer {
     inlineCaseIntrinsics
     ,inlineOptions
     ,inlineLabelsCalledOnce
+    ,valify
     ,devalify
     ,jumpjump
     ,dropGoodCasts
@@ -743,6 +744,77 @@ class Simplify extends MiniPhaseTransform with IdentityDenotTransformer {
       else tree
     }
     ("inlineLabelsCalledOnce", BeforeAndAfterErasure, visitor, transformer)
+  }}
+
+  val valify: Optimization = { (ctx0: Context) => {
+    implicit val ctx: Context = ctx0
+     // either a duplicate or a read through series of immutable fields
+    val defined: mutable.Map[Symbol, ValDef] = mutable.Map()
+    val firstRead: mutable.Map[Symbol, RefTree] = mutable.Map()
+    val firstWrite: mutable.Map[Symbol, Assign] = mutable.Map()
+    val secondWrite: mutable.Map[Symbol, Assign] = mutable.Map()
+    val visitor: Visitor = {
+      case t: ValDef if t.symbol.is(Flags.Mutable, Flags.Lazy) && !t.symbol.is(Flags.Method) && !t.symbol.owner.isClass =>
+        if (tpd.isPureExpr(t.rhs))
+          defined(t.symbol) = t
+      case t: RefTree if !t.symbol.is(Flags.Method) && !t.symbol.owner.isClass =>
+        if (!firstWrite.contains(t.symbol)) firstRead(t.symbol) = t
+      case t @ Assign(l, expr) if  !l.symbol.is(Flags.Method) && !l.symbol.owner.isClass =>
+        if (!firstRead.contains(l.symbol)) {
+          if (firstWrite.contains(l.symbol)) {
+            if (!secondWrite.contains(l.symbol))
+              secondWrite(l.symbol) = t
+          } else if (!expr.existsSubTree(x => x match {
+            case tree: RefTree if x.symbol == l.symbol => firstRead(l.symbol) = tree; true
+            case _ => false
+          })) {
+            firstWrite(l.symbol) = t
+          }
+        }
+      case _ =>
+    }
+
+    val transformer: Transformer = () => localCtx => {
+      val transformation: Tree => Tree = {
+        case t: Block => // drop non-side-effecting stats
+          val valdefs = t.stats.filter(x => x match  {
+            case t: ValDef if defined.contains(t.symbol) => true
+            case _ => false
+          }).asInstanceOf[List[ValDef]]
+          val assigns = t.stats.filter(x => x match {
+            case t @ Assign(lhs, r) =>
+              firstWrite.contains(lhs.symbol) && !secondWrite.contains(lhs.symbol)
+            case _ => false
+          })
+
+          val pairs = valdefs.flatMap(x => assigns.find(y => y.asInstanceOf[Assign].lhs.symbol == x.symbol) match {
+            case Some(y: Assign) => List((x, y))
+            case _ => Nil
+          })
+
+          val valsToDrop = pairs.map(x => x._1).toSet
+          val assignsToReplace: Map[Assign, ValDef] = pairs.map(x => (x._2, x._1)).toMap
+
+          val newStats = t.stats.mapConserve {
+            case x: ValDef if valsToDrop.contains(x) => EmptyTree
+            case t: Assign => assignsToReplace.get(t) match {
+              case Some(vd) =>
+                val newD = vd.symbol.asSymDenotation.copySymDenotation(initFlags = vd.symbol.flags.&~(Flags.Mutable))
+                newD.installAfter(this)
+                tpd.ValDef(vd.symbol.asTerm, t.rhs)
+              case None => t
+            }
+            case x => x
+          }
+
+          if (newStats eq t.stats) t
+          else cpy.Block(t)(newStats, t.expr)
+        case tree => tree
+      }
+
+      transformation
+    }
+    ("valify", BeforeAndAfterErasure, visitor, transformer)
   }}
 
   val devalify: Optimization = { (ctx0: Context) => {
