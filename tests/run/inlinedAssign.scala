d96cb84c35bfe47718850c1b8db435e67d512a0e
Martin Odersky <odersky@gmail.com>
2016-09-05 18:21:20 +0200

    Test case for closure inlining
    This test works, but closures are not currently inlined.
    That's a still to do.

---
+++ tests/run/inlinedAssign.scala
@@ -0,0 +1,18 @@
+object Test {
+
+  @dotty.annotation.inline
+  def swap[T](x: T, x_= : T => Unit, y: T, y_= : T => Unit) = {
+    val t = x
+    x_=(y)
+    y_=(t)
+  }
+
+  def main(args: Array[String]) = {
+    var x = 1
+    var y = 2
+    @dotty.annotation.inline def setX(z: Int) = x = z
+    @dotty.annotation.inline def setY(z: Int) = y = z
+    swap[Int](x, setX, y, setY)
+    assert(x == 2 && y == 1)
+  }
+}
