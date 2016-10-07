0fde698b0e7890353b42c7d050ec3ff45b3e778f
Nicolas Stucki <nicolas.stucki@gmail.com>
2016-10-07 14:19:45 +0200

    Add AnnotatedTypes to caches to have coherent hashCode.

--- src/dotty/tools/dotc/core/Types.scala
+++ src/dotty/tools/dotc/core/Types.scala
@@ -3243,7 +3243,7 @@ object Types {
 
   /** An annotated type tpe @ annot */
   case class AnnotatedType(tpe: Type, annot: Annotation)
-      extends UncachedProxyType with ValueType {
+      extends CachedProxyType with ValueType {
     // todo: cache them? but this makes only sense if annotations and trees are also cached.
     override def underlying(implicit ctx: Context): Type = tpe
     def derivedAnnotatedType(tpe: Type, annot: Annotation) =
@@ -3253,6 +3253,8 @@ object Types {
     override def stripTypeVar(implicit ctx: Context): Type =
       derivedAnnotatedType(tpe.stripTypeVar, annot)
     override def stripAnnots(implicit ctx: Context): Type = tpe.stripAnnots
+
+    override def computeHash: Int = doHash(annot, tpe)
   }
 
   object AnnotatedType {
