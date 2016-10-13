6e78c7aff351de5e5c8ccddc319744a4f976d79c
Guillaume Martres <smarter@ubuntu.com>
2016-10-13 02:05:30 +0200

    Fix erasure of "def foo[T]: Unit", do not box the return type

---
+++ tests/run/unit_erasure.scala
@@ -0,0 +1,16 @@
+class A {
+  def foo1[T]: Unit = {}
+  def foo2[T](): Unit = {}
+  def foo3[T]()(): Unit = {}
+  def foo4: Unit = {}
+  def foo5(): Unit = {}
+  def foo6()(): Unit = {}
+}
+
+object Test {
+  def main(args: Array[String]): Unit = {
+    classOf[A].getMethods.toList.filter(_.getName.startsWith("foo")).foreach { m =>
+      assert(m.getGenericReturnType == Void.TYPE, s"Method does not return void: `${m}`")
+    }
+  }
+}
