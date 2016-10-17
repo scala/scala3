fc6b8fba13a4ac1823763410568a48e46e4b2192
Martin Odersky <odersky@gmail.com>
2016-10-18 00:23:15 +0200

    Mention Inlined transformation in docs

--- src/dotty/tools/dotc/ast/Trees.scala
+++ src/dotty/tools/dotc/ast/Trees.scala
@@ -502,7 +502,9 @@ object Trees {
 
   /** A tree representing inlined code.
    *
-   *  @param  call      The original call that was inlined
+   *  @param  call      Info about the original call that was inlined
+   *                    Until PostTyper, this is the full call, afterwards only
+   *                    a reference to the toplevel class from which the call was inlined.
    *  @param  bindings  Bindings for proxies to be used in the inlined code
    *  @param  expansion The inlined tree, minus bindings.
    *
