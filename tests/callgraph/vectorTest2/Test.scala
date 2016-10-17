75c7d6f404db787708d201da4cc351e8649d4457
Nicolas Stucki <nicolas.stucki@gmail.com>
2016-10-17 16:08:01 +0200

    Add test infrastructure.

---
+++ tests/callgraph/vectorTest2/Test.scala
@@ -0,0 +1,11 @@
+import java.util
+
+import scala.util.control.Breaks
+import scala.util.control.ControlThrowable
+import scala.util.control.NoStackTrace
+
+object Test {
+  def main(args: Array[String]): Unit = {
+    scala.collection.immutable.Vector(1, 2, 3)
+  }
+}
