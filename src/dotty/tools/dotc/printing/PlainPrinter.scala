0781f7f7ec4766d7766aba556c31dc08a28a65e0
Nicolas Stucki <nicolas.stucki@gmail.com>
2016-10-18 10:17:07 +0200

    Add java aloccated types to callgraph

--- src/dotty/tools/dotc/printing/PlainPrinter.scala
+++ src/dotty/tools/dotc/printing/PlainPrinter.scala
@@ -11,6 +11,8 @@ import java.lang.Integer.toOctalString
 import config.Config.summarizeDepth
 import scala.annotation.switch
 
+import dotty.tools.dotc.transform.Summaries.JavaAllocatedType
+
 class PlainPrinter(_ctx: Context) extends Printer {
   protected[this] implicit def ctx: Context = _ctx.addMode(Mode.Printing)
 
@@ -256,6 +258,8 @@ class PlainPrinter(_ctx: Context) extends Printer {
         else "{...}.this" // TODO move underlying type to an addendum, e.g. ... z3 ... where z3: ...
       case tp: SkolemType =>
         if (homogenizedView) toText(tp.info) else tp.repr
+      case tp: JavaAllocatedType =>
+        toText(tp.underlying)
     }
   }
 
