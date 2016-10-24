d5f5f844e490e00a0bdb569b4cc1bece781fef83
Nicolas Stucki <nicolas.stucki@gmail.com>
2016-10-24 16:22:09 +0200

    Fix CG field init calls in constructor.

--- tests/callgraph/0-printlnTest1/Test.scala
+++ tests/callgraph/0-printlnTest1/Test.scala
@@ -1,8 +1,3 @@
-import java.util
-
-import scala.util.control.Breaks
-import scala.util.control.ControlThrowable
-import scala.util.control.NoStackTrace
 
 object Test {
   def main(args: Array[String]): Unit = {
