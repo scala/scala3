49fa9a5c70416f9d0b4073c70d4563723cc2280b
Romain <romain.beguet@epfl.ch>
2016-10-25 15:55:24 +0200

    updates testrunner

--- runCallGraphTests.sh
+++ runCallGraphTests.sh
@@ -1,51 +1,98 @@
 #!/bin/sh
 
-function dotcf() {
-cat /Users/nicolasstucki/GitHub/dotty/test/dotc/scala-collections.whitelist|grep -v '#'| grep -v "^$"| sed 's/.\//\/Users\/nicolasstucki\/GitHub\/dotty\//' | xargs dotc -language:Scala2 $@
+source ~/.bash_profile
+
+callSite=`pwd`
+
+function findScalaSources() {
+  echo `(ls ../*.scala) 2> /dev/null`
 }
 
-alias dottyr='java -cp .:/Users/nicolasstucki/.ivy2/cache/org.scala-lang/scala-library/jars/scala-library-2.11.5.jar:/Users/nicolasstucki/GitHub/dotty/target/scala-2.11/dotty_2.11-0.1-SNAPSHOT.jar'
+function findJavaSources() {
+  echo `(ls ../*.java) 2> /dev/null`
+}
 
-if [ $# -eq 0 ]
-then
-    FILES=tests/callgraph/*
+function findAuxJavaSources() {
+  echo `(ls ../java/*.java) 2> /dev/null`
+}
+
+
+function compile() {
+  sc_srcs=`findScalaSources`
+  jv_srcs=`findJavaSources`
+  jv_aux_srcs=`findAuxJavaSources`
+
+  # if there are java sources but no java compilation command, issue a warning
+  if ([ -n "$jv_srcs" ] || [ -n "$jv_aux_srcs" ]) && [ -z "$jv_cmp_cmd" ]; then
+    echo "warning: java sources were found ($jv_srcs) but the java compilation command was no specified (use option -j)" 1>&2
+  fi
+
+  # if there are java sources and a java compilation command is given
+  if [ -n "$jv_srcs" ] && [ -n "$jv_cmp_cmd" ]; then
+    ( $sc_cmp_cmd "$sc_srcs $jv_srcs" && $jv_cmp_cmd $jv_srcs ) > compile-log.txt 2> compile-errors.txt || return 1
+  else
+    $sc_cmp_cmd "$sc_srcs" > compile-log.txt 2> compile-errors.txt || return 1
+  fi
+
+  # if there are auxiliary java sources and a java compilation command is given
+  if [ -n "$jv_aux_srcs" ] && [ -n "$jv_cmp_cmd" ]; then
+    $jv_cmp_cmd $jv_aux_srcs > compile-log.txt 2> compile-errors.txt || return 1
+  fi
+}
+
+function run() {
+  dottyr Test > run-log.txt 2> run-errors.txt
+}
+
+if [ $# -ge 2 ] && [ $1 == "-j" ]; then
+  jv_cmp_cmd="$2"; shift; shift
 else
-    FILES=$@
+  jv_cmp_cmd=""
 fi
 
-CODE=0
+if [ $# -lt 2 ]; then
+  echo "Usage: `basename $0` [-j javaCompileCmd] scalaCompileCmd runCmd [test1 test2 ...]" 1>&2
+  exit 1
+else
+  sc_cmp_cmd="$1"; shift
+  run_cmd="$1"; shift
+fi
 
-for f in $FILES
-do
-  echo "Testing" $f
-  pushd $f > /dev/null
+if [ $# -eq 0 ]; then
+  test_dirs=tests/callgraph/*
+else
+  test_dirs=$@
+fi
+
+code=0
+
+for test_dir in $test_dirs; do
+
+  echo "Testing" $test_dir
+  pushd $test_dir > /dev/null
   rm -R out > /dev/null 2> /dev/null
   mkdir -p out
   cd out > /dev/null
-  dotcf ../Test.scala > compile-log.txt 2> compile-errors.txt && dottyr Test > run-log.txt 2> run-errors.txt || { CODE=1;  }
+  compile "$test_dir/out/" && run || { echo "... failed"; code=1; }
   popd > /dev/null
 
-  if [ -e "$f/out/run-log.txt" ]
+  if [ -e "$test_dir/out/run-log.txt" ]
   then
-    if [ -s "$f/out/run-errors.txt" ]
+    if [ -s "$test_dir/out/run-errors.txt" ]
     then
       echo "... failed run";
-      cat $f/out/run-errors.txt
+      cat $test_dir/out/run-errors.txt
     else
-      diff -y $f/out/run-log.txt $f/check.txt > $f/out/run-log-diff.txt && echo "... passed" || { echo "... diff failed"; cat $f/out/run-log-diff.txt; CODE=1; }
+      diff -y $test_dir/out/run-log.txt $test_dir/check.txt > $test_dir/out/run-log-diff.txt && echo "... passed" || { echo "... diff failed"; cat $test_dir/out/run-log-diff.txt; code=1; }
     fi
   else
     echo "... failed compilation";
-    cat $f/out/compile-errors.txt
+    cat $test_dir/out/compile-errors.txt
   fi
-
 
   echo ""
 done
 
-echo "Finished testing"
-
-if [ $CODE -eq 0 ]
-then
-    exit 1;
+if [ $code -eq 0 ]; then
+  exit 1
 fi
