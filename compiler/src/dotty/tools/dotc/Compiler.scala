0c717cf169750a9e92fba2958858a3c38b53b053
Nicolas Stucki <nicolas.stucki@gmail.com>
2016-10-27 15:24:47 +0200

    Fix CG with overrides in mixins.

--- compiler/src/dotty/tools/dotc/Compiler.scala
+++ compiler/src/dotty/tools/dotc/Compiler.scala
@@ -87,14 +87,14 @@ class Compiler {
            new Constructors,        // Collect initialization code in primary constructors
                                        // Note: constructors changes decls in transformTemplate, no InfoTransformers should be added after it
            new FunctionalInterfaces,// Rewrites closures to implement @specialized types of Functions.
-           new GetClass,           // Rewrites getClass calls on primitive types.
+           new GetClass,            // Rewrites getClass calls on primitive types.
            new DeadCodeElimination),
       List(new LambdaLift,          // Lifts out nested functions to class scope, storing free variables in environments
                                        // Note: in this mini-phase block scopes are incorrect. No phases that rely on scopes should be here
            new ElimStaticThis,      // Replace `this` references to static objects by global identifiers
            new Flatten,             // Lift all inner classes to package scope
            new RestoreScopes),      // Repair scopes rendered invalid by moving definitions in prior phases of the group
-      List( new DeadCodeElimination,
+      List(new DeadCodeElimination,
            new ExpandPrivate,       // Widen private definitions accessed from nested classes
            new SelectStatic,        // get rid of selects that would be compiled into GetStatic
            new CollectEntryPoints,  // Find classes with main methods
