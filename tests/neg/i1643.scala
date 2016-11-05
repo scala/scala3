cf351a20fcd9de79eedeca76fad59bad55d5a5ec
Martin Odersky <odersky@gmail.com>
2016-11-05 15:45:03 +0100

    Disallow wildcard arguments in new
    These might lead to bad bounds if unchecked.
    Scalac disallows them also, but with a confusing error message
    ("class type expected" on the class).

---
+++ tests/neg/i1643.scala
@@ -0,0 +1,19 @@
+trait T extends Array {
+  def t1(as: String*): Array[String] = { varargs1(as: _*) } // error
+  def t2(as: String*): Array[String] = { super.varargs1(as: _*) } // error
+}
+class C extends Base_1 { // error
+  def c1(as: String*): Array[String] = { varargs1(as: _*) } // error
+  def c2(as: String*): Array[String] = { super.varargs1(as: _*) } // error
+}
+object Test extends App {
+  val t = new T {}  // error
+  println(t.t1("a", "b").mkString(","))
+  println(t.t2("a", "b").mkString(","))
+  val c = new C {}
+  println(c.c1("a", "b").mkString(","))
+  println(c.c2("a", "b").mkString(","))
+
+  class CC[T]
+  val x = new CC[_]  // error
+}
