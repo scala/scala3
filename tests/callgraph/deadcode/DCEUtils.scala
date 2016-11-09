b239f2884c2c0e0eded5de66ba8ea1dbcf7f0c5c
Nicolas Stucki <nicolas.stucki@gmail.com>
2016-11-09 10:20:15 +0100

    Modify deadcode tests.

---
+++ tests/callgraph/deadcode/DCEUtils.scala
@@ -0,0 +1,10 @@
+object DCEUtils {
+  @DoNotDCE
+  def shouldDCE(expr: => Any): Unit = try {
+    expr
+    throw new Exception("Expected DCE")
+  } catch {
+    case dce: dotty.runtime.DeadCodeEliminated =>
+        // TODO: check stack trace to see if the DCE was in the fist call of expr
+  }
+}
--- tests/callgraph/deadcode/Test1.scala
+++ tests/callgraph/deadcode/Test1.scala
@@ -1,25 +1,13 @@
+
 object Main {
-  class A {
-    def foo(x: Int) = x
-    def bar = foo(42)
-  }
-  
-  def foo = 2
-  def bar = foo
+
+  def foo() = 2
+  def bar() = foo
   
   @DoNotDCE
   def testEntry: Unit = {
-    val x = new A
-    try {
-      x.foo(3)
-    } catch {
-      case _: dotty.runtime.DeadCodeEliminated => System.out.println("Main.A.foo")
-      case _: Throwable => ()
-    }
-    /*Utils.checkDCE("Main.A.foo", x.foo(3))
-    Utils.checkDCE("Main.A.bar", x.bar)
-    Utils.checkDCE("Main.bar", bar)
-    Utils.checkNotDCE("Main.foo", foo)*/
+    DCEUtils.shouldDCE(bar())
+    foo()
   }
   
   def main(args: Array[String]): Unit = {
