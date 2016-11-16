ec06a0c5c51f905ee9a90a8a057f40456c224ee3
Guillaume Martres <smarter@ubuntu.com>
2016-11-16 21:57:19 +0100

    checkNoPrivateLeaks: handle references to companion members
    Previously Outer2#Inner#foo failed to compile with:
    ```
        non-private method foo refers to private value x in its type signature
    ```
    
    This should compile because the boundary of `foo` is `class Outer2`
    and the boundary of `x` is `object Outer2`. This commit fixes this by
    also considering the linked boundary in `checkNoPrivateLeaks`.

---
+++ tests/pos/leaks.scala
@@ -0,0 +1,17 @@
+class Outer1 {
+  private val x: Int = 1
+
+  private class Inner {
+    def foo: x.type = x // OK
+  }
+}
+
+object Outer2 {
+  private val x: Int = 1
+}
+
+class Outer2 {
+  private class Inner {
+    def foo: Outer2.x.type = Outer2.x // OK
+  }
+}
