b99edff13f63a8040c82a997b5ae093c523eb07e
liu fengyun <liu@fengy.me>
2016-11-16 17:59:58 +0100

    fix #1701: disqualify package name in type name resolution

--- src/dotty/tools/dotc/typer/Typer.scala
+++ src/dotty/tools/dotc/typer/Typer.scala
@@ -126,9 +126,10 @@ class Typer extends Namer with TypeAssigner with Applications with Implicits wit
      *  case x :: xs in class List would return the :: method).
      */
     def qualifies(denot: Denotation): Boolean =
-      reallyExists(denot) && !(
-         pt.isInstanceOf[UnapplySelectionProto] &&
-         (denot.symbol is (Method, butNot = Accessor)))
+      reallyExists(denot) &&
+        !(name.isTypeName && denot.symbol.is(Package)) &&
+        !(pt.isInstanceOf[UnapplySelectionProto] &&
+          (denot.symbol is (Method, butNot = Accessor)))
 
     /** Find the denotation of enclosing `name` in given context `ctx`.
      *  @param previous    A denotation that was found in a more deeply nested scope,
