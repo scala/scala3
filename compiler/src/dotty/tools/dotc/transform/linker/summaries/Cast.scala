b5a72e323e6aa580705510c8c581b3fda27bf44b
Nicolas Stucki <nicolas.stucki@gmail.com>
2016-11-17 13:04:59 +0100

    Check that casts are not done on PolyTypes

--- compiler/src/dotty/tools/dotc/transform/linker/summaries/Cast.scala
+++ compiler/src/dotty/tools/dotc/transform/linker/summaries/Cast.scala
@@ -1,9 +1,13 @@
 package dotty.tools.dotc.transform.linker.summaries
 
 import dotty.tools.dotc.core.Contexts.Context
-import dotty.tools.dotc.core.Types.Type
+import dotty.tools.dotc.core.Types.{PolyType, Type}
 
 case class Cast(from: Type, to: Type)(implicit ctx: Context) {
+
+  assert(!from.widenDealias.isInstanceOf[PolyType])
+  assert(!to.widenDealias.isInstanceOf[PolyType])
+
   override def equals(other: Any): Boolean = {
     other match {
       case Cast(a, b) =>
