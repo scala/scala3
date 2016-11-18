894897821a92e354c1f584ff4ff56c947d55572f
Nicolas Stucki <nicolas.stucki@gmail.com>
2016-11-18 11:02:36 +0100

    Add flags to enable dead code elimination and CG visual output.

--- compiler/src/dotty/tools/dotc/Compiler.scala
+++ compiler/src/dotty/tools/dotc/Compiler.scala
@@ -47,8 +47,8 @@ class Compiler {
       List(new PostTyper),          // Additional checks and cleanups after type checking
       List(new sbt.ExtractAPI),     // Sends a representation of the API of classes to sbt via callbacks
       List(new Pickler),            // Generate TASTY info
-      List(new CollectSummaries),
-      List(new BuildCallGraph),
+      List(new CollectSummaries),   // Collects method summaries for the call graph construction
+      List(new BuildCallGraph),     // Builds the call graph
       List(new FirstTransform,      // Some transformations to put trees into a canonical form
            new CheckReentrant),     // Internal use only: Check that compiled program has no data races involving global vars
       List(new RefChecks,           // Various checks mostly related to abstract members and overriding
@@ -76,7 +76,7 @@ class Compiler {
            new ResolveSuper,        // Implement super accessors and add forwarders to trait methods
            new ArrayConstructors),  // Intercept creation of (non-generic) arrays and intrinsify.
       List(new Erasure),            // Rewrite types to JVM model, erasing all type parameters, abstract types and refinements.
-      List(new ElimErasedValueType, // Expand erased value types to their underlying implmementation types
+      List(new ElimErasedValueType, // Expand erased value types to their underlying implementation types
            new VCElideAllocations,  // Peep-hole optimization to eliminate unnecessary value class allocations
            new Mixin,               // Expand trait fields and trait initializers
            new LazyVals,            // Expand lazy vals
@@ -94,7 +94,7 @@ class Compiler {
            new ElimStaticThis,      // Replace `this` references to static objects by global identifiers
            new Flatten,             // Lift all inner classes to package scope
            new RestoreScopes),      // Repair scopes rendered invalid by moving definitions in prior phases of the group
-      List(new DeadCodeElimination,
+      List(new DeadCodeElimination, // Replaces dead code by a `throw new DeadCodeEliminated`
            new ExpandPrivate,       // Widen private definitions accessed from nested classes
            new SelectStatic,        // get rid of selects that would be compiled into GetStatic
            new CollectEntryPoints,  // Find classes with main methods
