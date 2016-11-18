b1b4835366b54edb920a2ae3183fbea506248972
Nicolas Stucki <nicolas.stucki@gmail.com>
2016-11-18 14:06:28 +0100

    Only track CallWithContext out edges when needed.

--- src/dotty/tools/dotc/transform/linker/summaries/CallWithContext.scala
+++ src/dotty/tools/dotc/transform/linker/summaries/CallWithContext.scala
@@ -5,11 +5,34 @@ import dotty.tools.dotc.core.Types.Type
 
 import scala.collection.mutable
 
+
 class CallWithContext(call: Type, targs: List[Type], argumentsPassed: List[Type], val outerTargs: OuterTargs,
     val parent: CallWithContext, val callee: CallInfo)(implicit ctx: Context)
     extends CallInfo(call, targs, argumentsPassed) {
 
-  val outEdges = mutable.HashMap[CallInfo, List[CallWithContext]]().withDefault(x => Nil)
+  private val outEdgesOpt =
+    if (trackOutEdges) Some(mutable.HashMap[CallInfo, List[CallWithContext]]().withDefault(x => Nil))
+    else None
+
+  def outEdgesIterator: Iterator[(CallInfo, List[CallWithContext])] =
+    outEdgesOpt.fold[Iterator[(CallInfo, List[CallWithContext])]](Iterator.empty)(_.iterator)
+
+  def getOutEdges(callSite: CallInfo): List[CallWithContext] =
+    outEdgesOpt.fold(List.empty[CallWithContext])(_.apply(callSite))
+
+  def addOutEdges(callSite: CallInfo, edges: Traversable[CallWithContext]): Unit = {
+    outEdgesOpt.foreach { outEdges =>
+      var es = outEdges(callSite)
+      for (e <- edges) {
+        if (!es.contains(e))
+          es = e :: es
+      }
+      outEdges(callSite) = es
+    }
+  }
+
+  def edgeCount: Int =
+    outEdgesOpt.fold(0)(_.values.foldLeft(0)(_ + _.size))
 
   override def hashCode(): Int = super.hashCode() ^ outerTargs.hashCode()
 
@@ -21,6 +44,9 @@ class CallWithContext(call: Type, targs: List[Type], argumentsPassed: List[Type]
       case _ => false
     }
   }
+
+  private def trackOutEdges: Boolean =
+    ctx.settings.linkVis.value
 
   override def toString: String = s"CallWithContext($call, $targs, $argumentsPassed, $outerTargs, $parent, $callee)"
 }
