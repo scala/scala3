d25c28eba009d652f555d2c1eaea81a9f26e4095
Nicolas Stucki <nicolas.stucki@gmail.com>
2016-11-26 14:41:27 +0100

    Add CG tests for code called from java.

---
+++ tests/link/dce-failing/link-code-from-java-3/DCETest.scala
@@ -0,0 +1,64 @@
+import java.util.Observable
+
+import scala.annotation.internal.DoNotDCE
+
+object DCETest {
+  @DoNotDCE def dceTest: Unit = {
+    val a = new A[AA]
+    a.hasNext
+    a.next()
+    val aa = new AA
+    Test.shouldDCE(aa.f)
+    a.toString
+
+    val b = new B[BB] {}
+    b.hasNext
+    b.next()
+    val bb = new BB
+    Test.shouldDCE(bb.f)
+    b.toString
+
+    val c = new C[CC] {}
+    c.hasNext
+    c.next()
+    val cc = new CC
+    Test.shouldDCE(cc.f)
+    c.toString
+  }
+
+  @scala.export def entryPoint(): Unit = {
+    System.out.print(new A[AA])
+    System.out.print(new B[BB] {})
+    System.out.print(new C[CC] {})
+  }
+}
+
+class AA {
+  def f = 42
+  override def toString: String = super.toString
+}
+
+class BB {
+  def f = 43
+  override def toString: String = super.toString
+}
+
+class CC {
+  def f = 44
+  override def toString: String = super.toString
+}
+
+class A[E] extends java.util.Iterator[E] {
+  override def next(): E = null.asInstanceOf[E]
+  override def hasNext: Boolean = false
+}
+
+abstract class B[E] extends java.util.Iterator[E] {
+  override def next(): E = null.asInstanceOf[E]
+  override def hasNext(): Boolean = false
+}
+
+trait C[E] extends java.util.Iterator[E] {
+  override def next(): E = null.asInstanceOf[E]
+  override def hasNext(): Boolean = false
+}
