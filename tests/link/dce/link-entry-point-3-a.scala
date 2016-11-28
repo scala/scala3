6f64655cd6a21777fe8ce29e41fdf659bbe6c8a9
Nicolas Stucki <nicolas.stucki@gmail.com>
2016-11-28 15:13:02 +0100

    Fix CG entry point module initialization.

--- tests/link/dce/link-entry-point-3-a.scala
+++ tests/link/dce/link-entry-point-3-a.scala
@@ -15,16 +15,6 @@ object Test {
 
   @internal.link.AssertNotReachable
   @internal.link.DoNotDeadCodeEliminate
-  def shouldDCE(expr: => Any): Unit = try {
-    expr
-    throw new Exception("Expected DCE")
-  } catch {
-    case dce: dotty.runtime.DeadCodeEliminated =>
-    // TODO: check stack trace to see if the DCE was in the fist call of expr
-  }
-
-  @internal.link.AssertNotReachable
-  @internal.link.DoNotDeadCodeEliminate
   def dceTest: Unit = {
     Foo.bar()
     Foo.foo()
