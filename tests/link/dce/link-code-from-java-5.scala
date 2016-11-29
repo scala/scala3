e55bb3aefed0622709690fd939c492c2c1d1e77d
Nicolas Stucki <nicolas.stucki@gmail.com>
2016-11-29 23:21:40 +0100

    Fix HKApply in JavaAllocatedTypes.

--- tests/link/dce/link-code-from-java-5.scala
+++ tests/link/dce/link-code-from-java-5.scala
@@ -3,7 +3,8 @@ import scala.math.Ordering
 object Test {
   def main(args: Array[String]): Unit = {
     val arr = new Array[Object](1)
-    arr(0) = 1
+    arr(0) = 42
     java.util.Arrays.sort(arr, Ordering.Int.asInstanceOf[Ordering[Object]]) // extracted from SeqLike.sorted
+    System.out.println(arr(0))
   }
 }
