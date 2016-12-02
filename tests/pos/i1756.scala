5175f1e145cd02185a02a8fae06bd1b52d2bff85
Martin Odersky <odersky@gmail.com>
2016-12-02 09:14:03 +0100

    Add original test to test case

--- tests/pos/i1756.scala
+++ tests/pos/i1756.scala
@@ -1,5 +1,4 @@
-/*
- * class A { { val x = this } }
+class A { { val x = this } }
 class B(x: Int) {
   class C(x: Int)
       extends B({
@@ -13,7 +12,7 @@ class B(x: Int) {
     }
   }
 }
-*/
+
 // Minimized version
 class D(x: Int) {
   class E(x: Int) extends D({val test = D.this; x})
