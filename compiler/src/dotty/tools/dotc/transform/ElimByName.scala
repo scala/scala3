56f658a31587af9d2a97ee940f3dc1dbacc99291
Guillaume Martres <smarter@ubuntu.com>
2016-12-02 20:14:52 +0100

    Hacky fix for the TreeTypeMap issue from the previous commit
    With this commit, tests/pos/treetypemap-miniphase.scala now compiles,
    but this is only a hack: there are other instances of TreeTypeMap in the
    compiler and they would all need to be run with "atGroupEnd", this is
    tricky since we are not always calling TreeTypeMap in a context where we
    have access to the current TreeTransformer (for example when we create a
    TreeTypeMap inside TypeMap to deal with annotations), ideas and
    alternative fixes welcome.

--- compiler/src/dotty/tools/dotc/transform/ElimByName.scala
+++ compiler/src/dotty/tools/dotc/transform/ElimByName.scala
@@ -80,7 +80,9 @@ class ElimByName extends MiniPhaseTransform with InfoTransformer { thisTransform
             val inSuper = if (ctx.mode.is(Mode.InSuperCall)) InSuperCall else EmptyFlags
             val meth = ctx.newSymbol(
                 ctx.owner, nme.ANON_FUN, Synthetic | Method | inSuper, MethodType(Nil, Nil, argType))
-            Closure(meth, _ => arg.changeOwner(ctx.owner, meth))
+            atGroupEnd { implicit ctx: Context =>
+              Closure(meth, _ => arg.changeOwner(ctx.owner, meth))
+            }
         }
         ref(defn.dummyApply).appliedToType(argType).appliedTo(argFun)
       case _ =>
