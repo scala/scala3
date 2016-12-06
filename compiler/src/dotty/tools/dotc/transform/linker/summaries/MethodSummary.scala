d3cb5ba3bb18ad4c3ac0819c503b9e7fff8d9ae0
Nicolas Stucki <nicolas.stucki@gmail.com>
2016-12-06 22:30:19 +0100

    Make MethodSummary immutable and add MethodSummaryBuilder.

--- compiler/src/dotty/tools/dotc/transform/linker/summaries/MethodSummary.scala
+++ compiler/src/dotty/tools/dotc/transform/linker/summaries/MethodSummary.scala
@@ -3,13 +3,11 @@ package dotty.tools.dotc.transform.linker.summaries
 import dotty.tools.dotc.core.Symbols.Symbol
 import dotty.tools.dotc.core.Types.Type
 
-import scala.collection.mutable
-
 case class MethodSummary(methodDef: Symbol,
-                         var thisAccessed: Boolean,
-                         methodsCalled: mutable.Map[Type, List[CallInfo]],
+                         thisAccessed: Boolean,
+                         methodsCalled: Map[Type, List[CallInfo]],
                          // allocatedLambdas
-                         var accessedModules: List[Symbol],
+                         accessedModules: List[Symbol],
                          argumentReturned: Byte, // -1 if not known
-                         var argumentStoredToHeap: List[Boolean] // not currently collected
+                         argumentStoredToHeap: List[Boolean] // not currently collected
                         )
