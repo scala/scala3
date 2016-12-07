072af0012ac10b5d24d91954911825be543a6517
liu fengyun <liu@fengy.me>
2016-12-07 23:09:06 +0100

    fix #1773: handle patterns in interpolated string

---
+++ tests/run/i1773.scala
@@ -0,0 +1,14 @@
+object Test {
+  implicit class Foo(sc: StringContext) {
+    object q {
+      def unapply(arg: Any): Option[(Any, Any)] =
+        Some((sc.parts(0), sc.parts(1)))
+    }
+  }
+
+  def main(args: Array[String]): Unit = {
+    val q"class ${name: String} extends ${parent: String}" = new Object
+    println(name)
+    println(parent)
+  }
+}
