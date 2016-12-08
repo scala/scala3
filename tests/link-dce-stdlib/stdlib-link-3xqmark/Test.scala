4a45551eb4010cf0e0df14dbf3b28aac18fdb855
Nicolas Stucki <nicolas.stucki@gmail.com>
2016-12-08 17:37:08 +0100

    Put back tests.

--- tests/link-dce-stdlib/stdlib-link-3xqmark/Test.scala
+++ tests/link-dce-stdlib/stdlib-link-3xqmark/Test.scala
@@ -1,7 +1,7 @@
 import scala.annotation.internal
 
 object Test {
-  @internal.link.CallGraphBounds(reachableClasses = 101, classesWithReachableMethods = 12, reachableMethods = 61)
+  @internal.link.CallGraphBounds(reachableClasses = 294, classesWithReachableMethods = 248, reachableMethods = 472)
   def main(args: Array[String]): Unit = {
     try {
       ???
