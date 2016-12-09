a28265ad162fa676812bffbbc01e1d6b82401265
Nicolas Stucki <nicolas.stucki@gmail.com>
2016-12-09 11:42:47 +0100

    Remove context from CallInfos.

--- compiler/src/dotty/tools/dotc/transform/linker/summaries/CallInfo.scala
+++ compiler/src/dotty/tools/dotc/transform/linker/summaries/CallInfo.scala
@@ -1,14 +1,13 @@
 package dotty.tools.dotc.transform.linker.summaries
 
 import dotty.tools.dotc.core.Contexts.Context
-import dotty.tools.dotc.core.Types.{PolyType, Type, TypeBounds}
+import dotty.tools.dotc.core.Types.Type
 
-case class CallInfo(call: Type, // this is type of method, that includes full type of reciever, eg: TermRef(reciever, Method)
-                    targs: List[Type],
-                    argumentsPassed: List[Type],
-                    source: Option[CallInfo] = None // When source is not None this call was generated as part of a call to source
-                   )(implicit ctx: Context) extends AbstractCallInfo {
+/* When source is not None this call was generated as part of a call to source */
+case class CallInfo(call: Type, targs: List[Type], argumentsPassed: List[Type], source: Option[CallInfo] = None) extends AbstractCallInfo
 
-  checkAbstractCallInfoAssertions()
-
+object CallInfo {
+  def check(info: CallInfo)(implicit ctx: Context): Unit = {
+    AbstractCallInfo.check(info)
+  }
 }
