aa790fa5b7c609425de791c414632102abf7aac2
Felix Mulder <felix.mulder@gmail.com>
2016-12-14 15:23:45 +0100

    Refactor transformations to be more idiomatic

--- compiler/src/dotty/tools/dotc/transform/SpecializeFunction1.scala
+++ compiler/src/dotty/tools/dotc/transform/SpecializeFunction1.scala
@@ -5,185 +5,151 @@ package transform
 import TreeTransforms.{ MiniPhaseTransform, TransformerInfo }
 import core._
 import Contexts.Context, Types._, Decorators._, Symbols._, DenotTransformers._
-import Denotations._, SymDenotations._, Scopes._, StdNames._
+import Denotations._, SymDenotations._, Scopes._, StdNames._, NameOps._, Names._
 
 class SpecializeFunction1 extends MiniPhaseTransform with DenotTransformer {
   import ast.tpd._
 
   val phaseName = "specializeFunction1"
 
-  // TODO: remove me, just used for debug during dev
-  def debug[A](str: A*) = {
-    str.foreach(println)
-    System.exit(1)
-  }
-
   /** Transforms all classes extending `Function1[-T1, +R]` so that
-    * they instead extend the specialized version `JFunction...`
+    * they instead extend the specialized version `JFunction$mp...`
     */
-  def transform(ref: SingleDenotation)(implicit ctx: Context) = {
-    def containsFunction1(xs: List[TypeRef]): Boolean =
-      xs.map(_.typeSymbol.showFullName).contains("scala.Function1")
+  def transform(ref: SingleDenotation)(implicit ctx: Context) = ref match {
+    case ShouldTransformDenot(cref, t1, r, func1) =>
+      transformDenot(cref, t1, r, func1)
+    case _ => ref
+  }
 
-    // TODO: go away **please**
-    def blacklisted(cref: ClassDenotation): Boolean = {
-      "JFunction1" :: "AbstractFunction1" :: "PartialFunction" :: "AbstractPartialFunction" :: Nil
-    } contains (cref.name.show)
+  override def transformTemplate(tree: Template)(implicit ctx: Context, info: TransformerInfo) =
+    tree match {
+      case ShouldTransformTree(func1, t1, r) => transformTree(tree, func1, t1, r)
+      case _ => tree
+    }
 
+  private[this] val functionName = "JFunction1".toTermName
+  private[this] val functionPkg  = "scala.compat.java8.".toTermName
+  private[this] var Function1: Symbol = _
+  private[this] var argTypes: Set[Symbol] = _
+  private[this] var returnTypes: Set[Symbol] = _
+  private[this] var blacklisted: Set[Symbol] = _
 
-    ref match {
-      case cref: ClassDenotation if containsFunction1(cref.classParents) && !blacklisted(cref) =>
-        specializeFunction1(cref)
-      case ref => ref
+  /** Do setup of `argTypes` and `returnTypes` */
+  override def prepareForUnit(tree: Tree)(implicit ctx: Context) = {
+    argTypes = Set(defn.DoubleClass,
+                   defn.FloatClass,
+                   defn.IntClass,
+                   defn.LongClass,
+                   defn.UnitClass,
+                   defn.BooleanClass)
+
+    returnTypes = Set(defn.DoubleClass,
+                      defn.FloatClass,
+                      defn.IntClass,
+                      defn.LongClass)
+
+    Function1 = ctx.requiredClass("scala.Function1")
+
+    blacklisted = Set(
+      "scala.compat.java8.JFunction1",
+      "scala.runtime.AbstractFunction1",
+      "scala.PartialFunction",
+      "scala.runtime.AbstractPartialFunction"
+    ).map(ctx.requiredClass(_))
+
+    this
+  }
+
+  private object ShouldTransformDenot {
+    def unapply(cref: ClassDenotation)(implicit ctx: Context): Option[(ClassDenotation, Type, Type, Type)] = {
+      def collectFunc1(xs: List[Type])(implicit ctx: Context): Option[(Type, Type, Type)] =
+        xs.collect {
+          case func1 @ RefinedType(RefinedType(parent, _, t1), _, r)
+            if func1.isRef(Function1) => (t1, r, func1)
+        }.headOption
+
+      collectFunc1(cref.info.parentsWithArgs).flatMap { case (t1, r, func1) =>
+        if (
+          !argTypes.contains(t1.typeSymbol) ||
+          !returnTypes.contains(r.typeSymbol) ||
+          blacklisted.exists(sym => cref.symbol.derivesFrom(sym))
+        ) None
+        else Some((cref, t1, r, func1))
+      }
     }
   }
 
-  def specializeFunction1(cref: ClassDenotation)(implicit ctx: Context): SingleDenotation = {
-    def JFunction(t1: String, r: String): TypeRef = ctx.requiredClassRef {
-      "scala.compat.java8.JFunction1$mc" + s"$r$t1" + "$sp"
-    }
+  private object ShouldTransformTree {
+    def unapply(tree: Template)(implicit ctx: Context): Option[(Tree, Type, Type)] =
+      tree.parents
+      .map { t => (t.tpe, t) }
+      .collect {
+        case (tp @ RefinedType(RefinedType(parent, _, t1), _, r), func1)
+        if tp.isRef(Function1) => (func1, t1, r)
+      }
+      .headOption
+  }
 
-    def typeMember(name: String): ClassSymbol =
-      cref.classInfo.decls.lookup(name.toTypeName).info.typeSymbol.asClass
+  private def specializedName(name: Name, t1: Type, r: Type)(implicit ctx: Context): Name =
+    name.specializedFor(List(t1, r), List(t1, r).map(_.typeSymbol.name))
 
-    def t1Member = typeMember("scala$Function1$$T1")
-    def rMember  = typeMember("scala$Function1$$R")
-
-    // This Symbol -> String map contains the equaivalent types for
-    // scalac-specialized classes
-    val typeAbbr = Map(
-      defn.DoubleClass  -> "D",
-      defn.FloatClass   -> "F",
-      defn.IntClass     -> "I",
-      defn.LongClass    -> "J",
-      defn.UnitClass    -> "V",
-      defn.BooleanClass -> "Z"
-    )
+  def transformDenot(cref: ClassDenotation, t1: Type, r: Type, func1: Type)(implicit ctx: Context): SingleDenotation = {
+    val specializedFunction: TypeRef =
+      ctx.requiredClassRef(functionPkg ++ specializedName(functionName, t1, r))
 
     def replaceFunction1(in: List[TypeRef]): List[TypeRef] =
       in.foldRight(List.empty[TypeRef]) { (tp, acc) =>
-        val curr =
-          // TODO: remove .showFullName
-          if (tp.typeSymbol.showFullName == "scala.Function1") {
-            // If there exists a specialization, both type members will be in
-            // `typeAbbr`
-            typeAbbr.get(t1Member) -> typeAbbr.get(rMember) match {
-              case (Some(t1), Some(r)) => JFunction(t1, r)
-              case _ => tp
-            }
-          }
+        val newTp =
+          if (tp.isRef(Function1)) specializedFunction
           else tp
-
-        curr :: acc
+        newTp :: acc
       }
 
-    def specializeApply(scope: Scope): Scope =  {
-      import core.StdNames._
-      def specializeApply(sym: Symbol, t1: String, r: String): Symbol = {
-        val specializedMethodName = ("apply$mc" + t1 + r + "$sp").toTermName
-        val specializedApply = JFunction(t1, r).info.decls.lookup(specializedMethodName)
-
-        val specSym = ctx.newSymbol(
+    def specializeApply(scope: Scope): Scope = {
+      def specializedApply: Symbol = {
+        val specializedMethodName = specializedName(nme.apply, t1, r)
+        ctx.newSymbol(
           cref.symbol.owner,
           specializedMethodName,
           Flags.Override | Flags.Method,
-          specializedApply.info
-        )
-
-        specSym.enteredAfter(this)
+          specializedFunction.info.decls.lookup(specializedMethodName).info
+        ).enteredAfter(this)
       }
 
-      typeAbbr.get(t1Member) -> typeAbbr.get(rMember) match {
-        case (Some(t1), Some(r)) =>
-          scope.foldRight(newScope) { (sym, newScope) =>
-            newScope.enter {
-              if (sym.name eq nme.apply) specializeApply(sym, t1, r) else sym
-            }
-            newScope
-          }
-        case _ => scope
+      val alteredScope = newScope
+      scope.foreach { sym =>
+        alteredScope.enter(if (sym.name eq nme.apply) specializedApply else sym)
       }
+      alteredScope
     }
 
-    val ClassInfo(prefix, cls, parents, decls, info) = cref.info
-    val res = cref.copySymDenotation(
-      info = ClassInfo(prefix, cls, replaceFunction1(in = parents), specializeApply(decls), info)
-    )
-
-    res
+    val ClassInfo(prefix, cls, parents, decls, info) = cref.classInfo
+    val newInfo = ClassInfo(prefix, cls, replaceFunction1(in = parents), specializeApply(decls), info)
+    cref.copySymDenotation(info = newInfo)
   }
 
-  override def transformTemplate(tree: Template)(implicit ctx: Context, info: TransformerInfo) = {
-    // types as abbreviated in scalac specialized class file names
-    val typeAbbr = Map(
-      defn.DoubleClass  -> "D",
-      defn.FloatClass   -> "F",
-      defn.IntClass     -> "I",
-      defn.LongClass    -> "J",
-      defn.UnitClass    -> "V",
-      defn.BooleanClass -> "Z"
-    )
+  private def transformTree(tmpl: Template, func1: Tree, t1: Type, r: Type)(implicit ctx: Context) = {
+    val specializedFunc1 =
+      TypeTree(ctx.requiredClassRef(functionPkg ++ specializedName(functionName, t1, r)))
 
-    def typeMember(name: String): ClassSymbol =
-      ctx.owner.info.asInstanceOf[ClassInfo].decls.lookup(name.toTypeName).info.typeSymbol.asClass
-
-    def t1Member = typeMember("scala$Function1$$T1")
-    def rMember  = typeMember("scala$Function1$$R")
-
-    val parentAndTypes =
-      tree.parents
-      // map super-classes to a tuple of type and tree
-      .map { t => (t.tpe, t) }
-      // collect the parent that corresponds to Function1 and its type params
-      .collect {
-        case (RefinedType(RefinedType(parent, _, t1), _, r), function1)
-          if parent.typeSymbol.showFullName == "scala.Function1" =>
-            (function1, t1, r)
-      }
-      .headOption
-
-    def replaceParent(parent: Tree, in: List[Tree], withTree: Tree) =
-      in.foldRight(List.empty[Tree]) { (t, trees) =>
-        (if (parent eq t) withTree else t) :: trees
-      }
-
-    def specializedParent(t1: Type, r: Type, orig: Tree): Option[Tree] =
-      // get the required class via the abbreviations in `typeAbbr`, if they
-      // don't both exist there, there is no specialization for the combination
-      typeAbbr.get(t1Member) -> typeAbbr.get(rMember) match {
-        case (Some(t1Abbr), Some(rAbbr)) =>
-          Some(TypeTree(ctx.requiredClassRef("scala.compat.java8.JFunction1$mc" + rAbbr + t1Abbr +  "$sp")))
-        case _ => None
-      }
-
-    def withRenamedApply(parent: Tree): List[Tree] = {
-      (typeAbbr.get(t1Member), typeAbbr.get(rMember)) match {
-        case (Some(t1Abbr), Some(rAbbr)) =>
-          tree.body.foldRight(List.empty[Tree]) {
-            case (tree: DefDef, acc) if tree.name == nme.apply =>
-              val specializedMethodName = ("apply$mc" + t1Abbr + rAbbr + "$sp").toTermName
-              val specializedApply = ctx.owner.info.decls.lookup(specializedMethodName).asTerm
-
-              val newDefDef = polyDefDef(specializedApply, trefs => vrefss => {
-                tree.rhs
-                  .changeOwner(tree.symbol, specializedApply)
-                  .subst(tree.vparamss.flatten.map(_.symbol), vrefss.flatten.map(_.symbol))
-              })
-
-              newDefDef :: acc
-            case (tree, acc) =>
-              tree :: acc
-          }
-        case x => tree.body
-      }
+    val parents = tmpl.parents.foldRight(List.empty[Tree]) { (t, trees) =>
+      (if (func1 eq t) specializedFunc1 else t) :: trees
     }
 
-    parentAndTypes.flatMap { case (parent, t1, r) =>
-      specializedParent(t1, r, parent).map { newPar =>
-        val parents = replaceParent(parent, in = tree.parents, withTree = newPar)
-        val body = withRenamedApply(newPar)
-        cpy.Template(tree)(parents = parents, body = body)
+    val body = tmpl.body.foldRight(List.empty[Tree]) {
+      case (tree: DefDef, acc) if tree.name == nme.apply => {
+        val specializedMethodName = specializedName(nme.apply, t1, r)
+        val specializedApply = ctx.owner.info.decls.lookup(specializedMethodName).asTerm
+
+        polyDefDef(specializedApply, trefs => vrefss => {
+          tree.rhs
+            .changeOwner(tree.symbol, specializedApply)
+            .subst(tree.vparamss.flatten.map(_.symbol), vrefss.flatten.map(_.symbol))
+        }) :: acc
       }
-    } getOrElse (tree)
+      case (tree, acc) => tree :: acc
+    }
+
+    cpy.Template(tmpl)(parents = parents, body = body)
   }
 }
