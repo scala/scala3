90ce8875c874f011b335390b3b41422793e1c7aa
Martin Odersky <odersky@gmail.com>
2016-12-14 17:38:21 +0100

    More tests and other odds and end
     - Add tests that work to pos/neg, tests that don't work yet to pending/pos/neg.
     - Also, change .gitignore to allow for a local directory.
     - Also add a draft page to the docs.

---
+++ tests/pos/i1777.scala
@@ -0,0 +1,9 @@
+object Main extends App {
+  import scala.collection.immutable._
+  case class Foo(s: String)
+  {
+    implicit val orderingS: Ordering[String] = Ordering[String] // Crash
+    val tree = TreeMap.empty ++ (1 to 100).map { i => Foo(i.toString) -> i }
+    println(tree.getClass)
+  }
+}
