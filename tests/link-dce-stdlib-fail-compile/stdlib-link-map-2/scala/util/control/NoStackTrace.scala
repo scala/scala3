a3fb5306f4838456977b7dbc47571d944768f068
Nicolas Stucki <nicolas.stucki@gmail.com>
2016-12-19 17:33:35 +0100

    Update CG tests.

---
+++ tests/link-dce-stdlib-fail-compile/stdlib-link-map-2/scala/util/control/NoStackTrace.scala
@@ -0,0 +1,32 @@
+/*                     __                                               *\
+**     ________ ___   / /  ___     Scala API                            **
+**    / __/ __// _ | / /  / _ |    (c) 2003-2013, LAMP/EPFL             **
+**  __\ \/ /__/ __ |/ /__/ __ |    http://scala-lang.org/               **
+** /____/\___/_/ |_/____/_/ | |                                         **
+**                          |/                                          **
+\*                                                                      */
+
+package scala
+package util.control
+
+/** A trait for exceptions which, for efficiency reasons, do not
+ *  fill in the stack trace.  Stack trace suppression can be disabled
+ *  on a global basis via a system property wrapper in
+ *  [[scala.sys.SystemProperties]].
+ *
+ *  @author   Paul Phillips
+ *  @since    2.8
+ */
+trait NoStackTrace extends Throwable {
+  override def fillInStackTrace(): Throwable =
+    if (NoStackTrace.noSuppression) super.fillInStackTrace()
+    else this
+}
+
+object NoStackTrace {
+  final def noSuppression = _noSuppression
+
+  // two-stage init to make checkinit happy, since sys.SystemProperties.noTraceSupression.value calls back into NoStackTrace.noSuppression
+  // FIXME: had to remove the two-stage init, was failing at runtime not finding field _noSuppression
+  final private val _noSuppression = sys.SystemProperties.noTraceSupression.value
+}
