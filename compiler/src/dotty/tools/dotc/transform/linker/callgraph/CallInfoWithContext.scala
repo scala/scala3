a43445f1c4c88b43f8d746f653ab090d1e095c9b
Nicolas Stucki <nicolas.stucki@gmail.com>
2016-12-19 11:41:04 +0100

    Add checks

--- compiler/src/dotty/tools/dotc/transform/linker/callgraph/CallInfoWithContext.scala
+++ compiler/src/dotty/tools/dotc/transform/linker/callgraph/CallInfoWithContext.scala
@@ -1,13 +1,16 @@
 package dotty.tools.dotc.transform.linker.callgraph
 
 import dotty.tools.dotc.core.Contexts.Context
-import dotty.tools.dotc.core.Types.Type
+import dotty.tools.dotc.core.Types.{PolyParam, Type}
 import dotty.tools.dotc.transform.linker.summaries.{AbstractCallInfo, CallInfo}
 
 import scala.collection.mutable
 
 case class CallInfoWithContext(call: Type, targs: List[Type], argumentsPassed: List[Type], outerTargs: OuterTargs,
-    parent: Option[CallInfoWithContext], callee: Option[CallInfo]) extends AbstractCallInfo {
+    parent: Option[CallInfoWithContext], callee: Option[CallInfo])(implicit ctx: Context) extends AbstractCallInfo {
+
+  assert(!call.widenDealias.isInstanceOf[PolyParam], call.widenDealias)
+  assert(call.termSymbol.isTerm, call)
 
   private val outEdges = mutable.HashMap[CallInfo, List[CallInfoWithContext]]().withDefault(x => Nil)
 
