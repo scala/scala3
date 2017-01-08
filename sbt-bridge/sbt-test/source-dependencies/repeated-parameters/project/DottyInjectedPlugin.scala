0bca6b76ee6c7c9555707d03551593dd04b5fe81
Guillaume Martres <smarter@ubuntu.com>
2017-01-08 20:04:52 +0100

    Bump version from 0.1-SNAPSHOT to 0.1.1-SNAPSHOT
    This is useful for two reasons:
    - All published Scala versions are of the form a.b.c and some tooling
      expect that, like sbt CrossVersion API.
    - Using 0.1.1 instead of 0.1.0 means that we match the version number of
      dotty-sbt-bridge, this is simpler and means that in the future sbt
      could automatically choose the correct version of dotty-sbt-bridge so
      that the user does not need to specify scalaCompilerBridgeSource in
      his build.sbt
    
    Note: it's awful that we have hardcoded paths to jars and that I had to
    change them, but I won't fix that now.

--- sbt-bridge/sbt-test/source-dependencies/repeated-parameters/project/DottyInjectedPlugin.scala
+++ sbt-bridge/sbt-test/source-dependencies/repeated-parameters/project/DottyInjectedPlugin.scala
@@ -6,12 +6,12 @@ object DottyInjectedPlugin extends AutoPlugin {
   override def trigger = allRequirements
 
   override val projectSettings = Seq(
-    scalaVersion := "0.1-SNAPSHOT",
+    scalaVersion := "0.1.1-SNAPSHOT",
     scalaOrganization := "ch.epfl.lamp",
     scalacOptions += "-language:Scala2",
     scalaBinaryVersion  := "2.11",
     autoScalaLibrary := false,
     libraryDependencies ++= Seq("org.scala-lang" % "scala-library" % "2.11.5"),
-    scalaCompilerBridgeSource := ("ch.epfl.lamp" % "dotty-sbt-bridge" % "0.1.1-SNAPSHOT" % "component").sources()
+    scalaCompilerBridgeSource := ("ch.epfl.lamp" % "dotty-sbt-bridge" % scalaVersion.value % "component").sources()
   )
 }
