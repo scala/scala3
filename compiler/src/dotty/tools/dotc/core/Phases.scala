f56a8b3246adca23ed76beefb0eb5102e70f6d97
Felix Mulder <felix.mulder@gmail.com>
2017-01-23 17:17:55 +0100

    Add @OlivierBlanvillain to the core devs

--- compiler/src/dotty/tools/dotc/core/Phases.scala
+++ compiler/src/dotty/tools/dotc/core/Phases.scala
@@ -269,7 +269,7 @@ object Phases {
     /** A name given to the `Phase` that can be used to debug the compiler. For
       * instance, it is possible to print trees after a given phase using:
       *
-      * ```none
+      * ```bash
       * $ ./bin/dotc -Xprint:<phaseNameHere> sourceFile.scala
       * ```
       */
