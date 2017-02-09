c7a557c43221c1fbad4117e63b6cd8c255ddc8f7
liu fengyun <liu@fengy.me>
2017-02-09 17:24:47 +0100

    add more debug files to the tests

---
+++ tests/debug/function.scala
@@ -0,0 +1,14 @@
+object Test {
+  def main(args: Array[String]): Unit = {
+    val a = 1 + 2
+    val b = a * 9        // [break] [step: plus] [step: c = plus]
+    val plus = (x: Int, y: Int) => {    // [cont: x * x]
+      val a = x * x                     // [break] [step: y * y]
+      val b = y * y                     // [step: a + b]
+      a + b                             // [next] [next]
+    }
+    val c = plus(a, b)   // [next: print]
+    print(c)             // [cont]
+  }
+
+}
