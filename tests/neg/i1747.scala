aa561a034cb9205d05737a602dc2268ec798fa53
Martin Odersky <odersky@gmail.com>
2017-02-15 14:41:02 +0100

    Fix test case

--- tests/neg/i1747.scala
+++ tests/neg/i1747.scala
@@ -1,3 +1,3 @@
 class Coll[E] extends java.util.Collection[E] {
-  def toArray[T](a: Array[T]): Array[T] = ??? // error: cannot override
+  def toArray[T](a: Array[T]): Array[T] = ??? // error: cannot override // error
 }
