5e53ff36f1d9ac2bd433fa01e28a194810c1be74
Martin Odersky <odersky@gmail.com>
2017-02-21 23:28:46 +0100

    Fix test ^ 2

--- compiler/test/dotc/tests.scala
+++ compiler/test/dotc/tests.scala
@@ -184,7 +184,7 @@ class tests extends CompilerTest {
   @Test def neg_autoTupling = compileFile(negCustomArgs, "autoTuplingTest", args = "-language:noAutoTupling" :: Nil)
   @Test def neg_i1050 = compileFile(negCustomArgs, "i1050", List("-strict"))
   @Test def neg_i1240 = compileFile(negCustomArgs, "i1240")(allowDoubleBindings)
-  @Test def neg_i2002 = compileFile(negCustomArgs, "i2002", Nil)(allowDoubleBindings)
+  @Test def neg_i2002 = compileFile(negCustomArgs, "i2002")(allowDoubleBindings)
 
   val negTailcallDir = negDir + "tailcall/"
   @Test def neg_tailcall_t1672b = compileFile(negTailcallDir, "t1672b")
