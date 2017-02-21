1b9c6877549664c3f4281581df2f56863ebafbeb
Martin Odersky <odersky@gmail.com>
2017-02-21 18:07:03 +0100

    Change pretyping of argument closure
    Follow scalac in taking the lub of all formal parameters
    instead of requiring that they are all the same.
    
    This conforms to the spec change in
    
      https://github.com/scala/scala/pull/5307/files#diff-5d74b019862ed901d7800b4ace79fb5b

--- compiler/src/dotty/tools/dotc/typer/Applications.scala
+++ compiler/src/dotty/tools/dotc/typer/Applications.scala
@@ -1366,13 +1366,8 @@ trait Applications extends Compatibility { self: Typer with Dynamic =>
             val formalParamTypessForArg: List[List[Type]] =
               decomposedFormalsForArg.map(_.get._1)
             if (isUniform(formalParamTypessForArg)((x, y) => x.length == y.length)) {
-              val commonParamTypes = formalParamTypessForArg.transpose.map(ps =>
-                // Given definitions above, for i = 1,...,m,
-                //   ps(i) = List(p_i_1, ..., p_i_n)  -- i.e. a column
-                // If all p_i_k's are the same, assume the type as formal parameter
-                // type of the i'th parameter of the closure.
-                if (isUniform(ps)(ctx.typeComparer.isSameTypeWhenFrozen(_, _))) ps.head
-                else WildcardType)
+              val commonParamTypes = formalParamTypessForArg.transpose
+                .map(ps => ps.reduceLeft(_ | _))
               val commonFormal = defn.FunctionOf(commonParamTypes, WildcardType)
               overload.println(i"pretype arg $arg with expected type $commonFormal")
               pt.typedArg(arg, commonFormal)
