62b4eb87aa72c3b4e40a9c847ec61dabe467dabd
Martin Odersky <odersky@gmail.com>
2017-03-02 11:41:22 +0100

    Fix neg test error count
    Previous expansion caused 3 spurious follow-on errors which are now
    avoided.

--- tests/neg/enums.scala
+++ tests/neg/enums.scala
@@ -1,7 +1,7 @@
 enum List[+T] {
   case Cons(x: T, xs: List[T])
   case Nil // error: illegal enum value
-  case Snoc[U](xs: List[U], x: U) // error: case with type parameters needs extends clause // error // error // error
+  case Snoc[U](xs: List[U], x: U) // error: case with type parameters needs extends clause
 }
 
 enum class X {
