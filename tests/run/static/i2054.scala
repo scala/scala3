804b2544c370a3f4a1985203d2a0f0b64f2bacfd
Dmitry Petrashko <dmitry.petrashko@gmail.com>
2017-03-07 11:01:05 +0100

    Test that #2054 is fixed.

---
+++ tests/run/static/i2054.scala
@@ -0,0 +1,13 @@
+import scala.annotation.static
+
+class Test
+
+object Test {
+  @static def test(n: Int): Int =  {
+    List(3).map(_ + 2)
+    n
+  }
+
+  def main(args: Array[String]): Unit = test(3)
+}
+
