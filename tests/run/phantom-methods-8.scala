1fd91bef96743f7ae96e7fa7ecde4e594b08be21
Nicolas Stucki <nicolas.stucki@gmail.com>
2017-03-22 13:47:20 +0100

    Extra tests for Phantoms 1.

---
+++ tests/run/phantom-methods-8.scala
@@ -0,0 +1,26 @@
+/* Run this test with
+ *   `run tests/run/xyz.scala -Xprint-diff-del -Xprint:arrayConstructors,phantomRefErasure,phantomErasure,erasure`
+ * to see the the diffs after PhantomRefErasure, PhantomDeclErasure and Erasure.
+ */
+
+object Test {
+  import Boo._
+
+  def main(args: Array[String]): Unit = {
+    pacFun4(inky)
+  }
+
+  def pacFun4(clyde: Inky) = {
+    println("pacFun4")
+  }
+
+  def inky: Inky = {
+    println("inky")
+    Boo.boo[Inky]
+  }
+}
+
+object Boo extends Phantom {
+  type Inky <: Boo.Any
+  def boo[B <: Boo.Any]: B = assume[B]
+}
