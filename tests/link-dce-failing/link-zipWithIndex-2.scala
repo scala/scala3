32957e4c67205843c05cc752fca204be05123b07
Nicolas Stucki <nicolas.stucki@gmail.com>
2017-03-23 09:56:22 +0100

    Add second test case for zipWithIndex.

--- tests/link-dce-failing/link-zipWithIndex-2.scala
+++ tests/link-dce-failing/link-zipWithIndex-2.scala
@@ -4,11 +4,9 @@ object Test {
  // @internal.link.CallGraphBounds(reachableClasses = 31, classesWithReachableMethods = 7, reachableMethods = 55)
   def main(args: Array[String]): Unit = {
     val list = List('a', 'b', 'c')
-    for ((elem, idx) <- list.zipWithIndex) {
-      System.out.println(elem + " " + idx)
+    list.zipWithIndex.map { case (pname, idx) =>
+      System.out.println(pname + " " + idx)
     }
-//    val s = list.zip(list)
-//    s.withFilter(_ => true)
   }
 
 }
---
+++ tests/link-dce/link-zipWithIndex-1.scala
@@ -0,0 +1,12 @@
+import scala.annotation.internal
+
+object Test {
+ // @internal.link.CallGraphBounds(reachableClasses = 31, classesWithReachableMethods = 7, reachableMethods = 55)
+  def main(args: Array[String]): Unit = {
+    val list = List('a', 'b', 'c')
+    for ((elem, idx) <- list.zipWithIndex) {
+      System.out.println(elem + " " + idx)
+    }
+  }
+
+}
