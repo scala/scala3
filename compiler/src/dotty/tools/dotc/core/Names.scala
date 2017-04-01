9f7012e886aa13f22e6ffc417050f9f6fc5b8003
Martin Odersky <odersky@gmail.com>
2017-04-01 11:32:03 +0200

    Memoize toSimpleName
    toSimpleName is called a lot from the backend, so it makes
    sense to memoize it. It would be even better to communicate
    with the backend using strings, because then we would not
    have to enter all these simple names in the name table.

--- compiler/src/dotty/tools/dotc/core/Names.scala
+++ compiler/src/dotty/tools/dotc/core/Names.scala
@@ -339,7 +339,12 @@ object Names {
 
     def isSimple = false
     def asSimpleName = throw new UnsupportedOperationException(s"$debugString is not a simple name")
-    def toSimpleName = termName(toString)
+
+    private var simpleName: SimpleTermName = null
+    def toSimpleName = {
+      if (simpleName == null) simpleName = termName(toString)
+      simpleName
+    }
 
     def rewrite(f: PartialFunction[Name, Name]): ThisName =
       if (f.isDefinedAt(this)) likeSpaced(f(this))
