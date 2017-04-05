18d0ae5e08e66c90377ca6818b9d6f1af7f259ac
Martin Odersky <odersky@gmail.com>
2017-04-05 17:27:09 +0200

    Another test fixed

--- tests/run/enum-Color.scala
+++ tests/run/enum-Color.scala
@@ -6,6 +6,6 @@ object Test {
   def main(args: Array[String]) =
     for (color <- Color.enumValues) {
       println(s"$color: ${color.enumTag}")
-      assert(Color.valueOf(color.enumTag) eq color)
+      assert(Color.enumValue(color.enumTag) eq color)
     }
 }
