04dc2b6adca40eb2dc7ea5bf569721cc22b4ab81
Martin Odersky <odersky@gmail.com>
2017-04-21 18:05:10 +0200

    Also hoist complex arguments out of this-calls
    Also hoist complex arguments out of this-calls
    of secondary constructors.

--- compiler/src/dotty/tools/dotc/transform/HoistSuperArgs.scala
+++ compiler/src/dotty/tools/dotc/transform/HoistSuperArgs.scala
@@ -15,7 +15,7 @@ import core.Names.TermName
 import core.NameKinds.SuperArgName
 import SymUtils._
 
-/** This phase hoists complex arguments to supercalls out of the enclosing class.
+/** This phase hoists complex arguments of supercalls and this-calls out of the enclosing class.
  *  Example:
  *
  *      class B(y: Int) extends A({ def f(x: Int) = x * x; f(y)})
@@ -47,131 +47,160 @@ class HoistSuperArgs extends MiniPhaseTransform with IdentityDenotTransformer { 
     // There's an interaction with by name closures in that the <cbn-arg> marker
     // application should not be hoisted, but be left at the point of call.
 
-  /** Hoist complex arguments in super call `parent` out of the class.
-   *  @return A pair consisting of the transformed super call and a list of super argument
-   *          defininitions.
+  /** Defines methods for hoisting complex supercall arguments out of
+   *  parent super calls and constructor definitions.
+   *  Hoisted superarg methods are collected in `superArgDefs`
    */
-  def hoistSuperArgs(parent: Tree, cls: Symbol)(implicit ctx: Context): (Tree, List[DefDef]) = {
-    lazy val constr = cls.primaryConstructor
-    lazy val allParams = // The parameters that can be accessed in the supercall
-      cls.info.decls.filter(d => d.is(TypeParam) || d.is(TermParamAccessor))
+  class Hoister(cls: Symbol)(implicit ctx: Context) {
     val superArgDefs = new mutable.ListBuffer[DefDef]
 
-    /** The parameter references defined by the primary constructor info */
-    def allParamRefs(tp: Type): List[ParamRef] = tp match {
-      case tp: LambdaType => tp.paramRefs ++ allParamRefs(tp.resultType)
-      case _ => Nil
-    }
+    /** If argument is complex, hoist it out into its own method and refer to the
+     *  method instead.
+     *  @param   arg   The argument that might be hoisted
+     *  @param   cdef  The definition of the constructor from which the call is made
+     *  @return  The argument after possible hoisting
+     */
+    private def hoistSuperArg(arg: Tree, cdef: DefDef): Tree = {
+      val constr = cdef.symbol
+      lazy val origParams = // The parameters that can be accessed in the supercall
+        if (constr == cls.primaryConstructor)
+          cls.info.decls.filter(d => d.is(TypeParam) || d.is(TermParamAccessor))
+        else
+          (cdef.tparams ::: cdef.vparamss.flatten).map(_.symbol)
 
-    /** Splice `restpe` in final result type position of `tp` */
-    def replaceResult(tp: Type, restpe: Type): Type = tp match {
-      case tp: LambdaType =>
-        tp.derivedLambdaType(resType = replaceResult(tp.resultType, restpe))
-      case _ => restpe
-    }
+      /** The parameter references defined by the constructor info */
+      def allParamRefs(tp: Type): List[ParamRef] = tp match {
+        case tp: LambdaType => tp.paramRefs ++ allParamRefs(tp.resultType)
+        case _              => Nil
+      }
 
-    /** A method representing a hoisted supercall argument */
-    def newSuperArgMethod(argType: Type) = {
-      val (staticFlag, methOwner) =
-        if (cls.owner.is(Package)) (JavaStatic, cls) else (EmptyFlags, cls.owner)
-      val argTypeWrtConstr = argType.subst(allParams, allParamRefs(constr.info))
+      /** Splice `restpe` in final result type position of `tp` */
+      def replaceResult(tp: Type, restpe: Type): Type = tp match {
+        case tp: LambdaType =>
+          tp.derivedLambdaType(resType = replaceResult(tp.resultType, restpe))
+        case _ => restpe
+      }
+
+      /** A method representing a hoisted supercall argument */
+      def newSuperArgMethod(argType: Type) = {
+        val (staticFlag, methOwner) =
+          if (cls.owner.is(Package)) (JavaStatic, cls) else (EmptyFlags, cls.owner)
+        val argTypeWrtConstr = argType.subst(origParams, allParamRefs(constr.info))
         // argType with references to paramRefs of the primary constructor instead of
         // local parameter accessors
-      val meth = ctx.newSymbol(
+        val meth = ctx.newSymbol(
           owner = methOwner,
           name = SuperArgName.fresh(cls.name.toTermName),
           flags = Synthetic | Private | Method | staticFlag,
           info = replaceResult(constr.info, argTypeWrtConstr),
           coord = constr.coord)
-      if (methOwner.isClass) meth.enteredAfter(thisTransform) else meth
+        if (methOwner.isClass) meth.enteredAfter(thisTransform) else meth
+      }
+
+      /** Type of a reference implies that it needs to be hoisted */
+      def refNeedsHoist(tp: Type): Boolean = tp match {
+        case tp: ThisType => !tp.cls.isStaticOwner && tp.cls != cls
+        case tp: TermRef  => refNeedsHoist(tp.prefix)
+        case _            => false
+      }
+
+      /** Super call argument is complex, needs to be hoisted */
+      def needsHoist(tree: Tree) = tree match {
+        case _: DefDef            => true
+        case _: Template          => true
+        case _: New               => !tree.tpe.typeSymbol.isStatic
+        case _: RefTree | _: This => refNeedsHoist(tree.tpe)
+        case _                    => false
+      }
+
+      // begin hoistSuperArg
+      arg match {
+        case Apply(fn, arg1 :: Nil) if fn.symbol == defn.cbnArg =>
+          cpy.Apply(arg)(fn, hoistSuperArg(arg1, cdef) :: Nil)
+        case _ if (arg.existsSubTree(needsHoist)) =>
+          val superMeth = newSuperArgMethod(arg.tpe)
+          val superArgDef = polyDefDef(superMeth, trefs => vrefss => {
+            val paramSyms = trefs.map(_.typeSymbol) ::: vrefss.flatten.map(_.symbol)
+            val tmap = new TreeTypeMap(
+              typeMap = new TypeMap {
+                def apply(tp: Type) = tp match {
+                  case tp: NamedType
+                  if (tp.symbol.owner == cls || tp.symbol.owner == constr) &&
+                     tp.symbol.is(ParamOrAccessor) =>
+                    val mappedSym = origParams.zip(paramSyms).toMap.apply(tp.symbol)
+                    if (tp.symbol.isType) mappedSym.typeRef else mappedSym.termRef
+                  case _ =>
+                    mapOver(tp)
+                }
+              },
+              treeMap = {
+                case tree: RefTree if paramSyms.contains(tree.symbol) =>
+                  cpy.Ident(tree)(tree.name).withType(tree.tpe)
+                case tree =>
+                  tree
+              })
+            tmap(arg).changeOwnerAfter(constr, superMeth, thisTransform)
+          })
+          superArgDefs += superArgDef
+          def termParamRefs(tp: Type, params: List[Symbol]): List[List[Tree]] = tp match {
+            case tp: PolyType =>
+              termParamRefs(tp.resultType, params)
+            case tp: MethodType =>
+              val (thisParams, otherParams) = params.splitAt(tp.paramNames.length)
+              thisParams.map(ref) :: termParamRefs(tp.resultType, otherParams)
+            case _ =>
+              Nil
+          }
+          val (typeParams, termParams) = origParams.span(_.isType)
+          val res = ref(superMeth)
+            .appliedToTypes(typeParams.map(_.typeRef))
+            .appliedToArgss(termParamRefs(constr.info, termParams))
+          ctx.log(i"hoist $arg, cls = $cls = $res")
+          res
+        case _ => arg
+      }
     }
 
-    def refNeedsHoist(tp: Type): Boolean = tp match {
-      case tp: ThisType => !tp.cls.isStaticOwner && tp.cls != cls
-      case tp: TermRef => refNeedsHoist(tp.prefix)
-      case _ => false
+    /** Hoist complex arguments in super call out of the class. */
+    def hoistSuperArgsFromCall(superCall: Tree, cdef: DefDef): Tree = superCall match {
+      case Apply(fn, args) =>
+        cpy.Apply(superCall)(hoistSuperArgsFromCall(fn, cdef), args.mapconserve(hoistSuperArg(_, cdef)))
+      case _ =>
+        superCall
     }
 
-    /** Super call argument is complex, needs to be hoisted */
-    def needsHoist(tree: Tree) = tree match {
-      case _: DefDef => true
-      case _: Template => true
-      case _: New => !tree.tpe.typeSymbol.isStatic
-      case _: RefTree | _: This => refNeedsHoist(tree.tpe)
-      case _ => false
+    /** Hoist complex arguments in this-constructor call of secondary constructor out of the class. */
+    def hoistSuperArgsFromConstr(stat: Tree): Tree = stat match {
+      case stat: DefDef if stat.symbol.isClassConstructor =>
+        cpy.DefDef(stat)(rhs =
+          stat.rhs match {
+            case Block(superCall :: stats, expr) =>
+              val superCall1 = hoistSuperArgsFromCall(superCall, stat)
+              if (superCall1 eq superCall) stat.rhs
+              else cpy.Block(stat.rhs)(superCall1 :: stats, expr)
+            case _ =>
+              hoistSuperArgsFromCall(stat.rhs, stat)
+          })
+      case _ =>
+        stat
     }
-
-    /** If argument is complex, hoist it out into its own method and refer to the
-     *  method instead.
-     *  @return  The argument after possible hoisting
-     *  Might append a method definition to `superArgs` as a side effect.
-     */
-    def hoistSuperArg(arg: Tree): Tree = arg match {
-      case Apply(fn, arg1 :: Nil) if fn.symbol == defn.cbnArg =>
-        cpy.Apply(arg)(fn, hoistSuperArg(arg1) :: Nil)
-      case _ if (arg.existsSubTree(needsHoist)) =>
-        val superMeth = newSuperArgMethod(arg.tpe)
-        val superArgDef = polyDefDef(superMeth, trefs => vrefss => {
-          val paramSyms = trefs.map(_.typeSymbol) ::: vrefss.flatten.map(_.symbol)
-          val tmap = new TreeTypeMap(
-            typeMap = new TypeMap {
-              def apply(tp: Type) = tp match {
-                case tp: NamedType if tp.symbol.owner == cls && tp.symbol.is(ParamOrAccessor) =>
-                  val mappedSym = allParams.zip(paramSyms).toMap.apply(tp.symbol)
-                  if (tp.symbol.isType) mappedSym.typeRef else mappedSym.termRef
-                case _ =>
-                  mapOver(tp)
-              }
-            },
-            treeMap = {
-              case tree: RefTree if paramSyms.contains(tree.symbol) =>
-                cpy.Ident(tree)(tree.name).withType(tree.tpe)
-              case tree =>
-                tree
-            }
-          )
-          tmap(arg).changeOwnerAfter(constr, superMeth, thisTransform)
-        })
-        superArgDefs += superArgDef
-        def termParamRefs(tp: Type): List[List[Tree]] = tp match {
-          case tp: PolyType =>
-            termParamRefs(tp.resultType)
-          case tp: MethodType =>
-            def paramRef(name: TermName) =
-              ref(cls.info.decl(name).suchThat(_.is(ParamAccessor)).symbol)
-            tp.paramNames.map(paramRef) :: termParamRefs(tp.resultType)
-          case _  =>
-            Nil
-        }
-        val res = ref(superMeth)
-          .appliedToTypes(cls.typeParams.map(_.typeRef))
-          .appliedToArgss(termParamRefs(constr.info))
-        ctx.log(i"hoist $arg, cls = $cls = $res")
-        res
-      case _ => arg
-    }
-
-    def recur(tree: Tree): Tree = tree match {
-      case Apply(fn, args) => cpy.Apply(tree)(recur(fn), args.mapconserve(hoistSuperArg))
-      case _ => tree
-    }
-    (recur(parent), superArgDefs.toList)
   }
 
   override def transformTypeDef(tdef: TypeDef)(implicit ctx: Context, info: TransformerInfo): Tree =
     tdef.rhs match {
-      case impl @ Template(_, superCall :: others, _, _) =>
-        val cls = tdef.symbol
-        val (hoisted, superArgDefs) = hoistSuperArgs(superCall, cls)
-        if (superArgDefs.isEmpty) tdef
+      case impl @ Template(cdef, superCall :: others, _, _) =>
+        val hoist = new Hoister(tdef.symbol)
+        val hoistedSuperCall = hoist.hoistSuperArgsFromCall(superCall, cdef)
+        val hoistedBody = impl.body.mapconserve(hoist.hoistSuperArgsFromConstr)
+        if (hoist.superArgDefs.isEmpty) tdef
         else {
           val (staticSuperArgDefs, enclSuperArgDefs) =
-            superArgDefs.partition(_.symbol.is(JavaStatic))
+            hoist.superArgDefs.toList.partition(_.symbol.is(JavaStatic))
           flatTree(
               cpy.TypeDef(tdef)(
                   rhs = cpy.Template(impl)(
-                      parents = hoisted :: others,
-                      body = impl.body ++ staticSuperArgDefs)) ::
+                      parents = hoistedSuperCall :: others,
+                      body = hoistedBody ++ staticSuperArgDefs)) ::
               enclSuperArgDefs)
         }
       case _ =>
