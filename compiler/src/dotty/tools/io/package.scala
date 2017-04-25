bf7704fd4b71aaae10639b5cd420d14f657131a0
Felix Mulder <felix.mulder@gmail.com>
2017-04-25 19:00:53 +0200

    Remove unused files and type aliases

--- compiler/src/dotty/tools/io/package.scala
+++ compiler/src/dotty/tools/io/package.scala
@@ -21,38 +21,10 @@ package object io {
   type ManifestResources = scala.reflect.io.ManifestResources
   type Path = scala.reflect.io.Path
   val Path = scala.reflect.io.Path
-  val Streamable = scala.reflect.io.Streamable
   type VirtualDirectory = scala.reflect.io.VirtualDirectory
   type VirtualFile = scala.reflect.io.VirtualFile
   type ZipArchive = scala.reflect.io.ZipArchive
-  val ZipArchive = scala.reflect.io.ZipArchive
   type FileZipArchive = scala.reflect.io.FileZipArchive
   type JManifest = java.util.jar.Manifest
   type JFile = java.io.File
-
-  implicit def enrichManifest(m: JManifest): Jar.WManifest = Jar.WManifest(m)
-  private lazy val daemonThreadPool = DaemonThreadFactory.newPool()
-
-  def runnable(body: => Unit): Runnable       = new Runnable { override def run() = body }
-  def callable[T](body: => T): Callable[T]    = new Callable[T] { override def call() = body }
-  def spawn[T](body: => T): Future[T]         = daemonThreadPool submit callable(body)
-  def submit(runnable: Runnable)              = daemonThreadPool submit runnable
-
-  // Create, start, and return a daemon thread
-  def daemonize(body: => Unit): Thread = newThread(_ setDaemon true)(body)
-  def newThread(f: Thread => Unit)(body: => Unit): Thread = {
-    val thread = new Thread(runnable(body))
-    f(thread)
-    thread.start
-    thread
-  }
-
-  // Set a timer to execute the given code.
-  def timer(seconds: Int)(body: => Unit): Timer = {
-    val alarm = new Timer(true) // daemon
-    val tt    = new TimerTask { def run() = body }
-
-    alarm.schedule(tt, seconds * 1000)
-    alarm
-  }
 }
