6a92606407d51b7a1d039b69bfdb3417f3a237f6
Nicolas Stucki <nicolas.stucki@gmail.com>
2017-04-28 13:16:14 +0200

    Cleanup

--- project/Build.scala
+++ project/Build.scala
@@ -997,7 +997,7 @@ object DottyInjectedPlugin extends AutoPlugin {
 
   private def projectChecks(): Unit = {
     val submodules = List(new File("scala-backend"), new File("scala-scala"), new File("collection-strawman"))
-    if (!submodules.forall(_.exists)) {
+    if (!submodules.forall(f => f.exists && f.listFiles().nonEmpty)) {
       println(
         s"""[WARNING] Missing some of the submodules
            |You can initialize the modules with:
