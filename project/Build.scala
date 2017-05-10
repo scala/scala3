c58fe5cccd83c1ae5df3980d1a10a8510d03239b
Guillaume Martres <smarter@ubuntu.com>
2017-05-10 15:41:58 +0200

    Add missing scala-library dependency
    This had been missing for a while, but was only discovered now because
    dotty-compiler used to depend on scala-reflect which pulled in the
    correct scala-library.

--- project/Build.scala
+++ project/Build.scala
@@ -828,7 +828,14 @@ object DottyInjectedPlugin extends AutoPlugin {
   // depend on the dotty-library
   lazy val `scala-library` = project.
     dependsOn(`dotty-library-bootstrapped`).
-    settings(commonDummySettings)
+    settings(commonDummySettings).
+    settings(
+      // Need a direct dependency on the real scala-library even though we indirectly
+      // depend on it via dotty-library, because sbt may rewrite dependencies
+      // (see https://github.com/sbt/sbt/pull/2634), but won't rewrite the direct
+      // dependencies of scala-library (see https://github.com/sbt/sbt/pull/2897)
+      libraryDependencies += "org.scala-lang" % "scala-library" % scalacVersion
+    )
 
   lazy val `scala-compiler` = project.
     settings(commonDummySettings)
