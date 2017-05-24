4b5bd1f34ae0defa4a75cc364e3910a5ff86697d
Nicolas Stucki <nicolas.stucki@gmail.com>
2017-05-24 17:24:31 +0200

    Add post-condidtion to CheckPhantomCast

--- compiler/src/dotty/tools/dotc/transform/CheckPhantomCast.scala
+++ compiler/src/dotty/tools/dotc/transform/CheckPhantomCast.scala
@@ -16,6 +16,14 @@ class CheckPhantomCast extends MiniPhaseTransform { thisTransformer =>
 
   override def phaseName = "checkPhantomCast"
 
+  override def checkPostCondition(tree: tpd.Tree)(implicit ctx: Context): Unit = {
+    tree match {
+      case TypeApply(fun, targs) if fun.symbol eq defn.Any_asInstanceOf => assert(!containsPhantom(targs.head.tpe))
+      case Bind(_, Typed(_, tpt)) => assert(!containsPhantom(tpt.tpe))
+      case _ =>
+    }
+  }
+
   override def transformTypeApply(tree: tpd.TypeApply)(implicit ctx: Context, info: TransformerInfo): tpd.Tree = {
     if (tree.fun.symbol eq defn.Any_asInstanceOf)
       checkNoPhantoms(tree.args.head)
@@ -31,13 +39,12 @@ class CheckPhantomCast extends MiniPhaseTransform { thisTransformer =>
   }
 
   private def checkNoPhantoms(tpTree: tpd.Tree)(implicit ctx: Context): Unit = {
-    val checker = new TypeTraverser() {
-      override def traverse(tp: Type): Unit = {
-        if (tp.isPhantom) ctx.error("Cannot cast type containing a phantom type", tpTree.pos)
-        else traverseChildren(tp)
-      }
-    }
-    checker.traverse(tpTree.tpe)
+    if (containsPhantom(tpTree.tpe))
+      ctx.error("Cannot cast type containing a phantom type", tpTree.pos)
   }
+
+  private def containsPhantom(tp: Type)(implicit ctx: Context): Boolean = new TypeAccumulator[Boolean] {
+    override def apply(x: Boolean, tp: Type): Boolean = x || tp.isPhantom || foldOver(false, tp)
+  }.apply(x = false, tp)
 
 }
