a3a91216b9395cd12cc3e96491640f5ebf840d49
Martin Odersky <odersky@gmail.com>
2017-06-15 13:41:15 +0200

    Harden TreeAccumulator for IDE

--- compiler/src/dotty/tools/dotc/ast/Trees.scala
+++ compiler/src/dotty/tools/dotc/ast/Trees.scala
@@ -1305,8 +1305,10 @@ object Trees {
             this(this(x, arg), annot)
           case Thicket(ts) =>
             this(x, ts)
-          case _ if ctx.reporter.errorsReported =>
+          case _ if ctx.mode.is(Mode.Interactive) =>
             // in case of errors it may be that typed trees point to untyped ones.
+            // The can still traverse inside such trees, either in the run where errors
+            // are reported, or in subsequent ones.
             x
         }
       }
