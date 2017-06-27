cb59a0e8963d748b62419a8951882626a7d2059a
Dmitry Petrashko <dmitry.petrashko@gmail.com>
2017-06-27 16:01:55 +0200

    AllocationStats: pass checkReentrant

--- compiler/src/dotty/tools/dotc/reporting/AllocationStats.scala
+++ compiler/src/dotty/tools/dotc/reporting/AllocationStats.scala
@@ -6,6 +6,7 @@ import java.lang.ref.WeakReference
 import java.util.concurrent.atomic.{AtomicInteger, AtomicReferenceArray}
 
 import dotty.tools.dotc.typer.FrontEnd
+import dotty.tools.sharable
 
 
 /** Collect collection allocation statistics in the entire compiler.
@@ -15,8 +16,8 @@ import dotty.tools.dotc.typer.FrontEnd
 object AllocationStats {
   final val collect = false
 
-  private val counts = collection.concurrent.TrieMap[(Class[_], Class[_]), Int]()
-  private val lastPhase = new ThreadLocal[Class[_]]()
+  @sharable private val counts = collection.concurrent.TrieMap[(Class[_], Class[_]), Int]()
+  @sharable private val lastPhase = new ThreadLocal[Class[_]]()
 
   def fixPhase(x: Class[_]): Class[_] = {
     if (x eq null) {
@@ -59,13 +60,13 @@ object AllocationStats {
     val data = counts.readOnlySnapshot()
     clear()
     val longestNameLength = data.keys.map(x => x._2.getName.length).max
-    val total = data.groupBy( x => x._1._2).map(x => (x._1, x._2.map(_._2).sum))
+    val total = data.groupBy( x => x._1._2).map(x => (x._1, x._2.map(_._2).sum)).toSeq.sortBy(-_._2)
     val byPhase = data.groupBy(x => x._1._1).map(x => (x._1, x._2.map(x => (x._1._2, x._2)))).toSeq.sortBy(x => -x._2.map(_._2).sum)
 
     "Total allocations:\n" + total.map{ x =>
          "   " + x._1.getName + " -> " + x._2
     }.mkString("\n") +
-      "Class allocations by phase:\n" + byPhase.map { x =>
+      "\n\n\nClass allocations by phase:\n" + byPhase.map { x =>
       val phaseName = x._1.getSimpleName
       val subtrees = x._2.toSeq.sortBy(-_._2).map(x => s"${x._1.getName.padTo(longestNameLength, " ").mkString} -> ${x._2}").mkString("\n   ", "\n   ","\n")
       phaseName ++ subtrees
