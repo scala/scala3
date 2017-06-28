c9e36fb3c12d722038d6d0054b8600a82ab35674
Martin Odersky <odersky@gmail.com>
2017-06-28 14:24:09 +0200

    Generalize isNotNull

--- compiler/src/dotty/tools/dotc/core/Types.scala
+++ compiler/src/dotty/tools/dotc/core/Types.scala
@@ -218,8 +218,15 @@ object Types {
      *  For the moment this is only true for modules, but it could
      *  be refined later.
      */
-    final def isNotNull(implicit ctx: Context): Boolean =
-      classSymbol is ModuleClass
+    final def isNotNull(implicit ctx: Context): Boolean = this match {
+      case tp: ConstantType => tp.value.value != null
+      case tp: ClassInfo => !tp.cls.isNullableClass && tp.cls != defn.NothingClass
+      case tp: TypeBounds => tp.lo.isNotNull
+      case tp: TypeProxy => tp.underlying.isNotNull
+      case AndType(tp1, tp2) => tp1.isNotNull || tp2.isNotNull
+      case OrType(tp1, tp2) => tp1.isNotNull && tp2.isNotNull
+      case _ => false
+    }
 
     /** Is this type produced as a repair for an error? */
     final def isError(implicit ctx: Context): Boolean = stripTypeVar match {
