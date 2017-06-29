ccdb36529ae5a83ae5a61dfecc550ee85be09a37
Martin Odersky <odersky@gmail.com>
2017-06-29 18:38:15 +0200

    Documentation and code cleanups

--- compiler/src/dotty/tools/dotc/transform/TypeTestsCasts.scala
+++ compiler/src/dotty/tools/dotc/transform/TypeTestsCasts.scala
@@ -27,20 +27,20 @@ trait TypeTestsCasts {
 
   def interceptTypeApply(tree: TypeApply)(implicit ctx: Context): Tree = ctx.traceIndented(s"transforming ${tree.show}", show = true) {
     tree.fun match {
-      case fun @ Select(qual, selector) =>
+      case fun @ Select(expr, selector) =>
         val sym = tree.symbol
 
         def isPrimitive(tp: Type) = tp.classSymbol.isPrimitiveValueClass
 
-        def derivedTree(qual1: Tree, sym: Symbol, tp: Type) =
-          cpy.TypeApply(tree)(qual1.select(sym).withPos(qual.pos), List(TypeTree(tp)))
+        def derivedTree(expr1: Tree, sym: Symbol, tp: Type) =
+          cpy.TypeApply(tree)(expr1.select(sym).withPos(expr.pos), List(TypeTree(tp)))
 
-        def foundCls = qual.tpe.widen.classSymbol
+        def foundCls = expr.tpe.widen.classSymbol
         // println(i"ta $tree, found = $foundCls")
 
         def inMatch =
           fun.symbol == defn.Any_typeTest ||  // new scheme
-          qual.symbol.is(Case)                // old scheme
+          expr.symbol.is(Case)                // old scheme
 
         def transformIsInstanceOf(expr:Tree, testType: Type, flagUnrelated: Boolean): Tree = {
           def testCls = testType.classSymbol
@@ -50,28 +50,40 @@ trait TypeTestsCasts {
               if (inMatch) ctx.error(em"this case is unreachable since $why", expr.pos)
               else ctx.warning(em"this will always yield false since $why", expr.pos)
 
-          def checkSensical: Boolean = {
-            val foundCls = erasure(qual.tpe.widen).typeSymbol
-            val testCls = testType.typeSymbol
-            !foundCls.isClass ||
-            !testCls.isClass ||
-            foundCls.isPrimitiveValueClass != testCls.isPrimitiveValueClass ||
-            ValueClasses.isDerivedValueClass(foundCls) ||
-            ValueClasses.isDerivedValueClass(testCls) ||
-            foundCls == defn.ObjectClass ||
-            foundCls.derivesFrom(testCls) || {
+          /** Are `foundCls` and `testCls` classes that allow checks
+           *  whether a test would be always false?
+           */
+          def isCheckable =
+            foundCls.isClass && testCls.isClass &&
+            foundCls.isPrimitiveValueClass == testCls.isPrimitiveValueClass &&
+               // if `found` is primitive but `test` is not, it's illegal anyway
+               // if `test` is primitive but `found` is not, we might have a case like
+               // found = java.lang.Integer, test = Int, which could be true
+               // (not sure why that is so, but scalac behaves the same way)
+            !isDerivedValueClass(foundCls) && !isDerivedValueClass(testCls) &&
+               // we don't have the logic to handle derived value classes
+            foundCls != defn.ObjectClass
+               // if `foundCls == Object`, it could have been `Any` before erasure.
+
+          /** Check whether a runtime test that a value of `foundCls` can be a `testCls`
+           *  can be true in some cases. Issure a warning or an error if that's not the case.
+           */
+          def checkSensical: Boolean =
+            if (!isCheckable) true
+            else if (!foundCls.derivesFrom(testCls)) {
               if (foundCls.is(Final)) {
                 unreachable(i"$foundCls is not a subclass of $testCls")
                 false
               }
               else if (!testCls.derivesFrom(foundCls) &&
-                       (testCls.is(Final) || !testCls.is(Trait) && !foundCls.is(Trait))) {
+                       (testCls.is(Final) ||
+                        !testCls.is(Trait) && !foundCls.is(Trait))) {
                 unreachable(i"$foundCls and $testCls are unrelated")
                 false
               }
               else true
             }
-          }
+            else true
 
           if (expr.tpe <:< testType)
             if (expr.tpe.isNotNull) {
@@ -88,32 +100,25 @@ trait TypeTestsCasts {
               constant(expr, Literal(Constant(foundCls == testCls)))
             else
               transformIsInstanceOf(expr, defn.boxedType(testCls.typeRef), flagUnrelated)
-          else testType.dealias match {
-            case _: SingletonType =>
-              val cmpOp =
-                if (testType derivesFrom defn.AnyValClass) defn.Any_equals
-                else defn.Object_eq
-              expr.select(cmpOp).appliedTo(singleton(testType))
-            case _ =>
-              derivedTree(expr, defn.Any_isInstanceOf, testType)
-          }
+          else
+            derivedTree(expr, defn.Any_isInstanceOf, testType)
         }
 
         def transformAsInstanceOf(testType: Type): Tree = {
           def testCls = testType.widen.classSymbol
-          if (qual.tpe <:< testType)
-            Typed(qual, tree.args.head)
+          if (expr.tpe <:< testType)
+            Typed(expr, tree.args.head)
           else if (foundCls.isPrimitiveValueClass) {
-            if (testCls.isPrimitiveValueClass) primitiveConversion(qual, testCls)
-            else derivedTree(box(qual), defn.Any_asInstanceOf, testType)
+            if (testCls.isPrimitiveValueClass) primitiveConversion(expr, testCls)
+            else derivedTree(box(expr), defn.Any_asInstanceOf, testType)
           }
           else if (testCls.isPrimitiveValueClass)
-            unbox(qual.ensureConforms(defn.ObjectType), testType)
+            unbox(expr.ensureConforms(defn.ObjectType), testType)
           else if (isDerivedValueClass(testCls)) {
-            qual // adaptToType in Erasure will do the necessary type adaptation
+            expr // adaptToType in Erasure will do the necessary type adaptation
           }
           else
-            derivedTree(qual, defn.Any_asInstanceOf, testType)
+            derivedTree(expr, defn.Any_asInstanceOf, testType)
         }
 
         /** Transform isInstanceOf OrType
@@ -124,32 +129,35 @@ trait TypeTestsCasts {
          *  The transform happens before erasure of `testType`, thus cannot be merged
          *  with `transformIsInstanceOf`, which depends on erased type of `testType`.
          */
-        def transformTypeTest(qual: Tree, testType: Type, flagUnrelated: Boolean): Tree = testType.dealias match {
+        def transformTypeTest(expr: Tree, testType: Type, flagUnrelated: Boolean): Tree = testType.dealias match {
+          case _: SingletonType =>
+            val cmpOp =
+              if (testType derivesFrom defn.AnyValClass) defn.Any_equals else defn.Object_eq
+              expr.select(cmpOp).appliedTo(singleton(testType))
           case OrType(tp1, tp2) =>
-            evalOnce(qual) { fun =>
-              transformTypeTest(fun, tp1, flagUnrelated = false)
-                .select(defn.Boolean_||)
-                .appliedTo(transformTypeTest(fun, tp2, flagUnrelated = false))
+            evalOnce(expr) { e =>
+              transformTypeTest(e, tp1, flagUnrelated = false)
+                .or(transformTypeTest(e, tp2, flagUnrelated = false))
             }
           case AndType(tp1, tp2) =>
-            evalOnce(qual) { fun =>
-              transformTypeTest(fun, tp1, flagUnrelated)
-                .select(defn.Boolean_&&)
-                .appliedTo(transformTypeTest(fun, tp2, flagUnrelated))
+            evalOnce(expr) { e =>
+              transformTypeTest(e, tp1, flagUnrelated)
+                .and(transformTypeTest(e, tp2, flagUnrelated))
             }
           case defn.MultiArrayOf(elem, ndims) if isUnboundedGeneric(elem) =>
             def isArrayTest(arg: Tree) =
               ref(defn.runtimeMethodRef(nme.isArray)).appliedTo(arg, Literal(Constant(ndims)))
-            if (ndims == 1) isArrayTest(qual)
-            else evalOnce(qual) { qual1 =>
-              derivedTree(qual1, defn.Any_isInstanceOf, qual1.tpe) and isArrayTest(qual1)
+            if (ndims == 1) isArrayTest(expr)
+            else evalOnce(expr) { e =>
+              derivedTree(e, defn.Any_isInstanceOf, e.tpe)
+                .and(isArrayTest(e))
             }
           case _ =>
-            transformIsInstanceOf(qual, erasure(testType), flagUnrelated)
+            transformIsInstanceOf(expr, erasure(testType), flagUnrelated)
         }
 
         if ((sym eq defn.Any_isInstanceOf) || (sym eq defn.Any_typeTest))
-          transformTypeTest(qual, tree.args.head.tpe, flagUnrelated = true)
+          transformTypeTest(expr, tree.args.head.tpe, flagUnrelated = true)
         else if (sym eq defn.Any_asInstanceOf)
           transformAsInstanceOf(erasure(tree.args.head.tpe))
         else tree
