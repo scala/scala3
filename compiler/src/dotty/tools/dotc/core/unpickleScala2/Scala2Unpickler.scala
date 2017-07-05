fdaf1c6838229275b5a329fc96c0091fdada0e70
Guillaume Martres <smarter@ubuntu.com>
2017-07-05 13:39:14 +0200

    Fix #2803: Scala2Unpickler failure when using cats_2.11

--- compiler/src/dotty/tools/dotc/core/unpickleScala2/Scala2Unpickler.scala
+++ compiler/src/dotty/tools/dotc/core/unpickleScala2/Scala2Unpickler.scala
@@ -745,7 +745,7 @@ class Scala2Unpickler(bytes: Array[Byte], classRoot: ClassDenotation, moduleClas
           else TypeRef(pre, sym.name.asTypeName)
         val args = until(end, readTypeRef)
         if (sym == defn.ByNameParamClass2x) ExprType(args.head)
-        else if (args.nonEmpty) tycon.safeAppliedTo(EtaExpandIfHK(sym.typeParams, args))
+        else if (args.nonEmpty) tycon.safeAppliedTo(EtaExpandIfHK(sym.typeParams, args.map(translateTempPoly)))
         else if (sym.typeParams.nonEmpty) tycon.EtaExpand(sym.typeParams)
         else tycon
       case TYPEBOUNDStpe =>
