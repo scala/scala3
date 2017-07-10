a707a74943c3ea886c50bd1ade1c3db2b96c2118
Guillaume Martres <smarter@ubuntu.com>
2017-07-10 19:38:30 +0200

    Fix test i2797
    Fork_1.java failed to compile before because the class name did not
    match the filename.

--- tests/pos/i2797/Fork_1.java
+++ tests/pos/i2797/Fork_1.java
@@ -1,7 +1,7 @@
 // Test is pending because we have no good way to test it.
 // We need to: Compile Fork.java, and then compile Test.scala
 // with Fork.class on the classpath.
-public @interface Fork {
+public @interface Fork_1 {
   int value() default -1;
   String[] jvmArgs() default { "nope" };
 }
--- tests/pos/i2797/Test_2.scala
+++ tests/pos/i2797/Test_2.scala
@@ -1,5 +1,5 @@
 // Test is pending because we have no good way to test it.
 // We need to: Compile Fork.java, and then compile Test.scala
 // with Fork.class on the classpath.
-@Fork(jvmArgs = Array("I'm", "hot"))
+@Fork_1(jvmArgs = Array("I'm", "hot"))
 class Test
