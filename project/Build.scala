52d81179dd8c14164fe50396692a0d927ac1ebb6
Guillaume Martres <smarter@ubuntu.com>
2017-07-12 16:08:25 +0200

    Release sbt-dotty 0.1.4

--- project/Build.scala
+++ project/Build.scala
@@ -899,7 +899,7 @@ object Build {
 
 
       sbtPlugin := true,
-      version := "0.1.3",
+      version := "0.1.4",
       ScriptedPlugin.scriptedSettings,
       ScriptedPlugin.sbtTestDirectory := baseDirectory.value / "sbt-test",
       ScriptedPlugin.scriptedBufferLog := false,
