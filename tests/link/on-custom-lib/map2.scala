f52541e85a3d266397ff1d5f51d2cb6ff531987e
Nicolas Stucki <nicolas.stucki@gmail.com>
2017-08-04 14:56:26 +0200

    Update tests

--- tests/link/on-custom-lib/map2.scala
+++ tests/link/on-custom-lib/map2.scala
@@ -1,7 +1,9 @@
+import strawman.collection._
+
 object Test {
   def main(args: Array[String]): Unit = {
-    // FIMXE
-//    val map2 = new Map2[Int] {}
-//    println(map2.foo)
+    // FIXME: Issue when loading type bindings from Tasty
+    // val map2 = new Map2[Int] {}
+    // println(map2.foo)
   }
 }
