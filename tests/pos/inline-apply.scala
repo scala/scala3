53e35e0e2fb32a8715abd62bd76349b7fb22a8bf
Martin Odersky <odersky@gmail.com>
2017-08-14 21:56:51 +0200

    Avoid insertion of spurious applies when inlining
    The test case crashed before, because it encountered
    a selection with an inserted apply, which should be typed
    but was not. The fix prevents inserting the apply by
    overriding the usual behavior in ReTyper.

---
+++ tests/pos/inline-apply.scala
@@ -0,0 +1,11 @@
+class Context
+
+object Test {
+
+  def transform()(implicit ctx: Context) = {
+    @inline def withLocalOwner[T](op: Context => T) = op(ctx)
+
+    withLocalOwner { implicit ctx => }
+
+  }
+}
