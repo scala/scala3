64862a0ccaa4061ddac9b7b7b0e06c3eb2ef8cc3
Felix Mulder <felix.mulder@gmail.com>
2017-08-17 16:04:36 +0200

    Temporarily disable console in sbt bridge

--- sbt-bridge/src/xsbt/ConsoleInterface.scala
+++ sbt-bridge/src/xsbt/ConsoleInterface.scala
@@ -6,8 +6,9 @@ package xsbt
 import xsbti.Logger
 
 import dotty.tools.dotc.core.Contexts.Context
-import dotty.tools.dotc.repl.REPL
-import dotty.tools.dotc.repl.REPL.Config
+// TODO: decide what to do with sbt REPL interface
+// import dotty.tools.dotc.repl.REPL
+// import dotty.tools.dotc.repl.REPL.Config
 
 class ConsoleInterface {
   def commandArguments(
@@ -27,43 +28,43 @@ class ConsoleInterface {
           bindValues: Array[Any],
           log: Logger
   ): Unit = {
-    val completeArgs =
-      args                                    :+
-      "-bootclasspath" :+ bootClasspathString :+
-      "-classpath"     :+ classpathString
+    //val completeArgs =
+    //  args                                    :+
+    //  "-bootclasspath" :+ bootClasspathString :+
+    //  "-classpath"     :+ classpathString
 
-    println("Starting dotty interpreter...")
-    val repl = ConsoleInterface.customRepl(
-      initialCommands :: Nil,
-      cleanupCommands :: Nil,
-      bindNames zip bindValues,
-      loader
-    )
-    repl.process(completeArgs)
+    //println("Starting dotty interpreter...")
+    //val repl = ConsoleInterface.customRepl(
+    //  initialCommands :: Nil,
+    //  cleanupCommands :: Nil,
+    //  bindNames zip bindValues,
+    //  loader
+    //)
+    //repl.process(completeArgs)
   }
 }
 
 object ConsoleInterface {
-  def customConfig(
-    initCmds: List[String],
-    cleanupCmds: List[String],
-    boundVals: Array[(String, Any)],
-    loader: ClassLoader
-  ) = new Config {
-    override val initialCommands: List[String] = initCmds
-    override val cleanupCommands: List[String] = cleanupCmds
-    override val boundValues: Array[(String, Any)] = boundVals
-    override val classLoader: Option[ClassLoader] = Option(loader)
-  }
+  //def customConfig(
+  //  initCmds: List[String],
+  //  cleanupCmds: List[String],
+  //  boundVals: Array[(String, Any)],
+  //  loader: ClassLoader
+  //) = new Config {
+  //  override val initialCommands: List[String] = initCmds
+  //  override val cleanupCommands: List[String] = cleanupCmds
+  //  override val boundValues: Array[(String, Any)] = boundVals
+  //  override val classLoader: Option[ClassLoader] = Option(loader)
+  //}
 
-  def customRepl(cfg: Config): REPL = new REPL {
-    override lazy val config = cfg
-  }
+  //def customRepl(cfg: Config): REPL = new REPL {
+  //  override lazy val config = cfg
+  //}
 
-  def customRepl(
-    initCmds: List[String],
-    cleanupCmds: List[String],
-    boundVals: Array[(String, Any)],
-    loader: ClassLoader
-  ): REPL = customRepl(customConfig(initCmds, cleanupCmds, boundVals, loader))
+  //def customRepl(
+  //  initCmds: List[String],
+  //  cleanupCmds: List[String],
+  //  boundVals: Array[(String, Any)],
+  //  loader: ClassLoader
+  //): REPL = customRepl(customConfig(initCmds, cleanupCmds, boundVals, loader))
 }
