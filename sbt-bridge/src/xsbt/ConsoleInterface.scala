d601fdf889cba007180d617b643c499e20cf0c3a
Felix Mulder <felix.mulder@gmail.com>
2017-08-22 19:53:05 +0200

    Add instructions on how to add `initialCommands` to REPL

--- sbt-bridge/src/xsbt/ConsoleInterface.scala
+++ sbt-bridge/src/xsbt/ConsoleInterface.scala
@@ -19,6 +19,24 @@ class ConsoleInterface {
   def run(args: Array[String],
           bootClasspathString: String,
           classpathString: String,
+          // TODO: initial commands needs to be run under some form of special
+          // "silent" mode in the REPL. I.e. the effects should be had without
+          // any visual output.
+          //
+          // To do this we can use the `run` interface to the `ReplDriver` and
+          // pass it a special instance of `ParseResult` like `Silently(res: ParseResult)`
+          // and then observe the effects without printing to `ReplDriver#out`
+          //
+          // This way, the REPL can offer feedback on invalid commands but
+          // still function without stringly logic.
+          //
+          // This same principle can be applied to `cleanupCommands` and
+          // `bindValues`
+          //
+          // Steps:
+          //
+          // 1. Introduce `case class Silent(res: ParseResult) extends ParseResult`
+          // 2. Perform all steps in `interpret` as usual without printing to `out`
           initialCommands: String,
           cleanupCommands: String,
           loader: ClassLoader,
