ae85afbb24757f0e625c9a0c46e3feb72ca91387
Felix Mulder <felix.mulder@gmail.com>
2017-08-23 13:05:25 +0200

    Fix compilation of dotty sbt scripted tests

--- sbt-dotty/sbt-test/source-dependencies/inherited_type_params/A.scala
+++ sbt-dotty/sbt-test/source-dependencies/inherited_type_params/A.scala
@@ -3,5 +3,5 @@ trait Equal[-A] {
   def equal(a1: A, a2: A): Boolean
 }
 object Test {
-  implicit def TraversableEqual[CC[X] <: collection.TraversableLike[X, CC[X]] with Traversable[X], A: Equal]: Equal[CC[A]] = error("")
+  implicit def TraversableEqual[CC[X] <: collection.TraversableLike[X, CC[X]] with Traversable[X], A: Equal]: Equal[CC[A]] = sys.error("")
 }
