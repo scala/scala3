59ce28ffa2ba0c4be358d1c33584b85dd2077e8d
Martin Odersky <odersky@gmail.com>
2017-08-27 13:30:09 +0200

    Hash-cons applied types in their own table

--- compiler/src/dotty/tools/dotc/core/Contexts.scala
+++ compiler/src/dotty/tools/dotc/core/Contexts.scala
@@ -613,15 +613,19 @@ object Contexts {
     /** A table for hash consing unique refined types */
     private[dotc] val uniqueRefinedTypes = new RefinedUniques // @!!! replace with uniqueAppliedTypes
 
+    /** A table for hash consing unique refined types */
+    private[dotc] val uniqueAppliedTypes = new AppliedUniques
+
     /** A table for hash consing unique named types */
     private[core] val uniqueNamedTypes = new NamedTypeUniques
 
     /** A table for hash consing unique type bounds */
-    private[core] val uniqueTypeAliases = new TypeAliasUniques
+    private[core] val uniqueTypeAliases = new TypeAliasUniques // @!!! replace
 
     private def uniqueSets = Map(
         "uniques" -> uniques,
         "uniqueRefinedTypes" -> uniqueRefinedTypes,
+        "uniqueAppliedTypes" -> uniqueAppliedTypes,
         "uniqueNamedTypes" -> uniqueNamedTypes,
         "uniqueTypeAliases" -> uniqueTypeAliases)
 
