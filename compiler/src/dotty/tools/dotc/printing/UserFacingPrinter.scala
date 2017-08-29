e0139bf8fae53456c9a117f8e9f02d57214ca02c
Felix Mulder <felix.mulder@gmail.com>
2017-08-29 09:26:25 +0200

    Fix #3025: remove duplicated logic from `UserFacingPrinter`

--- compiler/src/dotty/tools/dotc/printing/UserFacingPrinter.scala
+++ compiler/src/dotty/tools/dotc/printing/UserFacingPrinter.scala
@@ -1,29 +1,14 @@
 package dotty.tools.dotc
 package printing
 
-import ast.Trees.{ Untyped, Tree }
 import core._
-import Annotations.Annotation
-import Constants.Constant
-import Contexts.Context
-import Denotations.{ Denotation, MultiDenotation, SingleDenotation }
-import Flags._
-import TypeApplications.{ AppliedType, EtaExpansion }
-import Names._
-import NameOps._
-import StdNames._
-import Decorators._
-import Scopes.Scope
-import Symbols.{ Symbol, ClassSymbol, defn }
-import SymDenotations.NoDenotation
-import Types._
-import Texts._
+import Constants.Constant, Contexts.Context, Denotations._, Flags._, Names._
+import NameOps._, StdNames._, Decorators._, Scopes.Scope, Types._, Texts._
+import SymDenotations.NoDenotation, Symbols.{ Symbol, ClassSymbol, defn }
 import typer.Implicits.SearchResult
 import typer.ImportInfo
 
 class UserFacingPrinter(_ctx: Context) extends RefinedPrinter(_ctx) {
-
-  private def panic(msg: String): Nothing = throw new AssertionError(msg)
 
   private[this] def getPkgCls(path: String) =
     _ctx.requiredPackage(path).moduleClass.asClass
@@ -45,7 +30,7 @@ class UserFacingPrinter(_ctx: Context) extends RefinedPrinter(_ctx) {
   def wellKnownPkg(pkgSym: Symbol) = standardPkg(pkgSym) || wrappedName(pkgSym)
 
   override protected def keyString(sym: Symbol): String = {
-    val flags = sym.flags
+    val flags = sym.flagsUNSAFE
     if (flags is Package) ""
     else if (sym.isPackageObject) "package object"
     else if (flags.is(Module) && flags.is(Case)) "case object"
@@ -68,7 +53,7 @@ class UserFacingPrinter(_ctx: Context) extends RefinedPrinter(_ctx) {
     toText(sym) ~ {
       if (sym.is(Method)) toText(sym.info)
       else if (sym.isClass) ""
-      else if (sym.isType && sym.info.isInstanceOf[TypeAlias]) " =" ~~ toText(sym.info)
+      else if (sym.isType && sym.info.isInstanceOf[TypeAlias]) toText(sym.info)
       else if (sym.isType) ""
       else {
         ":" ~~ toText(sym.info)
@@ -78,47 +63,16 @@ class UserFacingPrinter(_ctx: Context) extends RefinedPrinter(_ctx) {
   override def toText(const: Constant): Text = Str(const.value.toString)
 
   override def toText(tp: Type): Text = tp match {
-    case tp: AnnotatedType => toText(tp.tpe) ~~ toText(tp.annot)
     case tp: ConstantType => toText(tp.value)
-    case tp: TypeAlias => toText(tp.underlying)
     case ExprType(result) => ":" ~~ toText(result)
-    case TypeBounds(lo, hi) =>
-      { if (lo != defn.NothingType) toText(lo) ~~ ">: _" else Str("_") } ~~
-      { if (hi != defn.AnyType) "<:" ~~ toText(hi) else Text() }
     case tp: TypeRef => tp.info match {
       case TypeAlias(alias) => toText(alias)
       case _ => toText(tp.info)
     }
     case tp: ParamRef => {
-      val name = tp.paramName.unexpandedName.invariantName.toString
+      val name = ParamRefNameString(tp.paramName)
       if (tp.isInstanceOf[TermParamRef]) name ~ ".type"
       else name
-    }
-    case EtaExpansion(tycon) => toText(tycon)
-    case PolyType(params, res) =>
-      "[" ~ Fluid(params.map(tl => toText(tl.toArg)).intersperse(Str(", "))) ~ "]" ~ toText(res)
-    case tp: MethodType => {
-      def paramText(name: TermName, tp: Type) = toText(name) ~ ": " ~ toText(tp)
-      changePrec(GlobalPrec) {
-        (if (tp.isImplicit) "(implicit " else "(") ~
-          Text((tp.paramNames, tp.paramInfos).zipped map paramText, ", ") ~
-        (if (tp.resultType.isInstanceOf[MethodType]) ")" else "): ") ~
-        toText(tp.resultType)
-      }
-    }
-    case AppliedType(tycon, args) => {
-      def toTextInfixType(tycon: Type, args: List[Type]): Text = {
-        // TODO: blatant copy from `RefinedPrinter`
-        val l :: r :: Nil = args
-        val isRightAssoc = tycon.typeSymbol.name.endsWith(":")
-        val leftArg = if (isRightAssoc && l.isInfixType) "(" ~ toText(l) ~ ")" else toText(l)
-        val rightArg = if (!isRightAssoc && r.isInfixType) "(" ~ toText(r) ~ ")" else toText(r)
-        leftArg ~~ atPrec(DotPrec) { tycon.toText(this) } ~~ rightArg
-      }
-      if (tp.isInfixType) toTextInfixType(tycon, args)
-      else {
-        toText(tycon) ~ "[" ~ Fluid(args.reverse.map(toText).intersperse(Str(", "))) ~ "]"
-      }
     }
     case tp: ClassInfo => {
       if (wellKnownPkg(tp.cls.owner))
