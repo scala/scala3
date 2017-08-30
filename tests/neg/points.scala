41d6d9eaca11b098f645e2437769d0c0c7b05fdd
Martin Odersky <odersky@gmail.com>
2017-08-30 22:46:20 +0200

    Update "good bounds" checks
    These need to be special-cased for base types now, since
    conflicting base types do not need any longer to public
    type members with conflicting bounds. A Test case where this
    shows up is points.scala. Another is Iter3.scala which
    got moved from pos to neg.
    
    These test cases passed in the old scheme, probably because comflicting
    aliases were not coalesced into bad type bounds. points.scala failed at
    ElimErasedValueTypes because it reported a double definition, but
    I think that one was not correct either and came from the same root
    cause of conflicting aliases.

--- tests/neg/points.scala
+++ tests/neg/points.scala
@@ -1,8 +1,18 @@
+trait Comparable[T] {
+  def compareTo(other: T): Int
+}
+
 class Point extends Comparable[Point] {
-  override def compareTo(other: Point): Int = ???
+  override def compareTo(other: Point): Int = 1
 }
 
-class ColoredPoint extends Point with Comparable[ColoredPoint] {
-  override def compareTo(other: ColoredPoint): Int = ??? // error: overridden method has different signature
+class ColoredPoint extends Point with Comparable[ColoredPoint] { // error: cannot be instantiated
+  override def compareTo(other: ColoredPoint): Int = -1
 }
 
+object Test extends App {
+  val c: Point = new ColoredPoint
+  def cmp[T <: Comparable[T]](p1: Comparable[T], p2: T) =
+    p1.compareTo(p2)
+  println(cmp(c, c))
+}
