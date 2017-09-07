7b81aee6fc03b6b3ba7da3bf8d049411520b0e99
liu fengyun <liu@fengy.me>
2017-09-07 11:34:01 +0200

    reenable exhaustivity check

--- compiler/test/dotty/tools/dotc/CompilationTests.scala
+++ compiler/test/dotty/tools/dotc/CompilationTests.scala
@@ -169,8 +169,7 @@ class CompilationTests extends ParallelTesting {
     compileFile("../tests/neg/customArgs/noimports.scala", defaultOptions.and("-Yno-imports")) +
     compileFile("../tests/neg/customArgs/noimports2.scala", defaultOptions.and("-Yno-imports")) +
     compileFile("../tests/neg/customArgs/overloadsOnAbstractTypes.scala", allowDoubleBindings) +
-    // currently compiles without warning. TODO e-enable when exhaustiveness is fixed.
-    // compileFile("../tests/neg/customArgs/xfatalWarnings.scala", defaultOptions.and("-Xfatal-warnings")) +
+    compileFile("../tests/neg/customArgs/xfatalWarnings.scala", defaultOptions.and("-Xfatal-warnings")) +
     compileFile("../tests/neg/customArgs/pureStatement.scala", defaultOptions.and("-Xfatal-warnings")) +
     compileFile("../tests/neg/customArgs/phantom-overload.scala", allowDoubleBindings) +
     compileFile("../tests/neg/customArgs/phantom-overload-2.scala", allowDoubleBindings) +
