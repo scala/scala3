ce2b4d0c324d3abecfa3c053564781c7eb543359
Allan Renucci <allan.renucci@gmail.com>
2017-09-13 09:52:49 +0200

    Don't serve stale JAR metadata from cache in classpath implementation
    Adapted from scalac commit bcf47b165ccfd8e1827188f70aeb24e2cecfb80f by
    Jason Zaugg in scala/scala#6064:
    
    Use the last modified timestamp and the file inode to help detect
    when the file has been overwritten (as is common in SBT builds
    with `exportJars := true`, or when using snapshot dependencies).
    
    Fixes scala/bug#10295

--- compiler/src/dotty/tools/dotc/classpath/ZipAndJarFileLookupFactory.scala
+++ compiler/src/dotty/tools/dotc/classpath/ZipAndJarFileLookupFactory.scala
@@ -5,6 +5,9 @@ package dotty.tools.dotc.classpath
 
 import java.io.File
 import java.net.URL
+import java.nio.file.Files
+import java.nio.file.attribute.{BasicFileAttributes, FileTime}
+
 import scala.annotation.tailrec
 import dotty.tools.io.{AbstractFile, ClassPath, ClassRepresentation, FileZipArchive, ManifestResources}
 import dotty.tools.dotc.core.Contexts.Context
@@ -19,23 +22,17 @@ import FileUtils._
  * when there are a lot of projects having a lot of common dependencies.
  */
 sealed trait ZipAndJarFileLookupFactory {
-  private val cache = collection.mutable.Map.empty[AbstractFile, ClassPath]
+  private val cache = new FileBasedCache[ClassPath]
 
   def create(zipFile: AbstractFile)(implicit ctx: Context): ClassPath = {
-    if (ctx.settings.YdisableFlatCpCaching.value) createForZipFile(zipFile)
+    if (ctx.settings.YdisableFlatCpCaching.value || zipFile.file == null) createForZipFile(zipFile)
     else createUsingCache(zipFile)
   }
 
   protected def createForZipFile(zipFile: AbstractFile): ClassPath
 
-  private def createUsingCache(zipFile: AbstractFile)(implicit ctx: Context): ClassPath = cache.synchronized {
-    def newClassPathInstance = {
-      if (ctx.settings.verbose.value || ctx.settings.Ylogcp.value)
-        println(s"$zipFile is not yet in the classpath cache")
-      createForZipFile(zipFile)
-    }
-    cache.getOrElseUpdate(zipFile, newClassPathInstance)
-  }
+  private def createUsingCache(zipFile: AbstractFile): ClassPath =
+    cache.getOrCreate(zipFile.file.toPath, () => createForZipFile(zipFile))
 }
 
 /**
@@ -178,4 +175,30 @@ object ZipAndJarSourcePathFactory extends ZipAndJarFileLookupFactory {
   }
 
   override protected def createForZipFile(zipFile: AbstractFile): ClassPath = ZipArchiveSourcePath(zipFile.file)
+}
+
+final class FileBasedCache[T] {
+  private case class Stamp(lastModified: FileTime, fileKey: Object)
+  private val cache = collection.mutable.Map.empty[java.nio.file.Path, (Stamp, T)]
+
+  def getOrCreate(path: java.nio.file.Path, create: () => T): T = cache.synchronized {
+    val attrs = Files.readAttributes(path, classOf[BasicFileAttributes])
+    val lastModified = attrs.lastModifiedTime()
+    // only null on some platforms, but that's okay, we just use the last modified timestamp as our stamp
+    val fileKey = attrs.fileKey()
+    val stamp = Stamp(lastModified, fileKey)
+    cache.get(path) match {
+      case Some((cachedStamp, cached)) if cachedStamp == stamp => cached
+      case _ =>
+        val value = create()
+        cache.put(path, (stamp, value))
+        value
+    }
+  }
+
+  def clear(): Unit = cache.synchronized {
+    // TODO support closing
+    // cache.valuesIterator.foreach(_.close())
+    cache.clear()
+  }
 }
