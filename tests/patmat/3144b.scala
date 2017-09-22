c53d6ec35b1b16b05fd4a6e2ff79f188b0471cf0
liu fengyun <liu@fengy.me>
2017-09-22 15:12:54 +0200

    address review: update test case

--- tests/patmat/3144b.scala
+++ tests/patmat/3144b.scala
@@ -1,6 +1,11 @@
 class Test {
   def f(x: Any): Int = x match {
     case xs: List[Int] @unchecked => xs.head
+    case xs: Array[List[Int]] => 3
+    case _ => 0
+  }
+
+  def g(x: Any): Int = x match {
     case xs: List[Int @unchecked] => xs.head
     case xs: Array[List[Int]] => 3
     case _ => 0
