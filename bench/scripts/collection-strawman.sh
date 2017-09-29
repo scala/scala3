96f7e30d41cb00c2f2bb606c28b277704a159eca
Guillaume Martres <smarter@ubuntu.com>
2017-09-29 23:15:46 +0200

    Make the bench scripts more useful
    - Use the bootstrapped compiler
    - Use a fixed amount of RAM (2G)

--- bench/scripts/collection-strawman.sh
+++ bench/scripts/collection-strawman.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-find collection-strawman/src/main/ -type f \( -name "*.scala" -or -name "*.java" \) -exec echo "dotty-bench/jmh:run" {} + | sbt
+find collection-strawman/src/main/ -type f \( -name "*.scala" -or -name "*.java" \) -exec echo "dotty-bench-bootstrapped/jmh:run" {} + | sbt
--- bench/scripts/library-cold.sh
+++ bench/scripts/library-cold.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-find library/src/ -type f \( -name "*.scala" -or -name "*.java" \) -exec echo "dotty-bench/jmh:run 0 1 10" {} + | sbt
+find library/src/ -type f \( -name "*.scala" -or -name "*.java" \) -exec echo "dotty-bench-bootstrapped/jmh:run 0 1 10" {} + | sbt
