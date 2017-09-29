e5088e217b84a40a782ad14aa4a18f9c612d7c0e
Martin Odersky <odersky@gmail.com>
2017-09-29 13:29:52 +0200

    Clean up imports

--- compiler/src/dotty/tools/dotc/transform/ElimOuterSelect.scala
+++ compiler/src/dotty/tools/dotc/transform/ElimOuterSelect.scala
@@ -2,27 +2,12 @@ package dotty.tools.dotc
 package transform
 
 import core._
-import TreeTransforms._
+import TreeTransforms.{MiniPhaseTransform, TransformerInfo}
 import Contexts.Context
-import Flags._
-import SymUtils._
-import Symbols._
-import SymDenotations._
 import Types._
 import Decorators._
-import DenotTransformers._
-import StdNames._
-import NameOps._
 import NameKinds.OuterSelectName
 import ast.Trees._
-import dotty.tools.dotc.ast.tpd
-import util.Positions._
-import Names._
-
-import collection.mutable
-import ResolveSuper._
-
-import scala.collection.immutable.::
 
 /** This phase rewrites outer selects `E.n_<outer>` which were introduced by
  *  inlining to outer paths.
