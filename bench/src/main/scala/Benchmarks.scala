bf7249f99d2c14010d431b89366c7f15781e5bee
Nicolas Stucki <nicolas.stucki@gmail.com>
2017-10-12 16:17:28 +0200

    Make MaxInlineLevel larger to unblock JVM inlining

--- bench/src/main/scala/Benchmarks.scala
+++ bench/src/main/scala/Benchmarks.scala
@@ -37,7 +37,7 @@ object Bench {
     val libs = System.getProperty("BENCH_CLASS_PATH")
 
     val opts = new OptionsBuilder()
-               .jvmArgsPrepend("-Xbootclasspath/a:" + libs + ":", "-Xms2G", "-Xmx2G")
+               .jvmArgsPrepend("-Xbootclasspath/a:" + libs + ":", "-Xms2G", "-Xmx2G", "-XX:MaxInlineLevel=35")
                .mode(Mode.AverageTime)
                .timeUnit(TimeUnit.MILLISECONDS)
                .warmupIterations(warmup)
