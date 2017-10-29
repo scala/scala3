6bad77e65e7e4d048502dfa38495ed961ac109a1
Allan Renucci <allan.renucci@gmail.com>
2017-10-29 21:19:42 +0100

    Update test cases

--- tests/pos-scala2/i3246.scala
+++ tests/pos-scala2/i3246.scala
@@ -4,4 +4,8 @@ class Test {
   def baz = 1
   def bam: () => Int = baz _
   def ban: () => Int = 1 _
+
+  def titi(fun: () => Unit) = ???
+  def toto(fun: => Int) = titi(fun _)
+  titi(1 _) // rejected by scalac
 }
