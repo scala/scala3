vendethiel/dotty

Make `ThisName` recursive on `self.ThisName`

compiler/src/dotty/tools/dotc/core/Names.scala
@@ -40,10 +40,10 @@ object Names {
    *  3. Names are intended to be encoded strings. @see dotc.util.NameTransformer.
    *     The encoding will be applied when converting a string to a name.
    */
-  abstract class Name extends DotClass with PreName {
+  abstract class Name extends DotClass with PreName { self =>
 
     /** A type for names of the same kind as this name */
-    type ThisName <: Name
+    type ThisName <: Name { type ThisName = self.ThisName }
 
     /** Is this name a type name? */
     def isTypeName: Boolean

scala/scala3

compiler/src/dotty/tools/dotc/core/Names.scala
@@ -35,10 +35,10 @@ object Names {
    *  in a name table. A derived term name adds a tag, and possibly a number
    *  or a further simple name to some other name.
    */
-  abstract class Name extends Designator with PreName {
+  abstract class Name extends Designator with PreName { self =>
 
     /** A type for names of the same kind as this name */
-    type ThisName <: Name
+    type ThisName <: Name { type ThisName = self.ThisName }
 
     /** Is this name a type name? */
     def isTypeName: Boolean