73038981e04d90ea569cc598baabb4189d57a909
Martin Odersky <odersky@gmail.com>
2017-12-04 15:37:34 +0100

    Cleanup of ElimJavaPackages
    Just came by this accidentally and noticed that it could be more idiomatic and
    terminology could be improved.

--- compiler/src/dotty/tools/dotc/transform/ElimJavaPackages.scala
+++ compiler/src/dotty/tools/dotc/transform/ElimJavaPackages.scala
@@ -1,46 +1,31 @@
-package dotty.tools.dotc.transform
+package dotty.tools.dotc
+package transform
 
-import dotty.tools.dotc.ast.tpd._
-import dotty.tools.dotc.core.Contexts.Context
-import dotty.tools.dotc.core.Flags._
-import dotty.tools.dotc.core.Types.{Type, TypeRef}
-import dotty.tools.dotc.transform.MegaPhase.MiniPhase
+import core._
+import Decorators._, Flags._, Types._, Contexts._, Symbols._
+import ast.tpd._
+import Flags._
+import MegaPhase.MiniPhase
 
-/**
-  * Eliminates syntactic references to Java packages, so that there's no chance
-  * they accidentally end up in the backend.
-  */
-class ElimJavaPackages extends MiniPhase {
+/** Eliminates syntactic references to package terms as prefixes of classes, so that there's no chance
+ *  they accidentally end up in the backend.
+ */
+class ElimPackagePrefixes extends MiniPhase {
 
-  override def phaseName: String = "elimJavaPackages"
+  override def phaseName: String = "elimPackagePrefixes"
 
-  override def transformSelect(tree: Select)(implicit ctx: Context): Tree = {
-    if (isJavaPackage(tree)) {
-      assert(tree.tpe.isInstanceOf[TypeRef], s"Expected tree with type TypeRef, but got ${tree.tpe.show}")
-      Ident(tree.tpe.asInstanceOf[TypeRef])
-    } else {
-      tree
-    }
+  override def transformSelect(tree: Select)(implicit ctx: Context): Tree =
+    if (isPackageClassRef(tree)) Ident(tree.tpe.asInstanceOf[TypeRef]) else tree
+
+  override def checkPostCondition(tree: Tree)(implicit ctx: Context): Unit = tree match {
+    case tree: Select =>
+      assert(!isPackageClassRef(tree), i"Unexpected reference to package in $tree")
+    case _ =>
   }
 
-  override def checkPostCondition(tree: Tree)(implicit ctx: Context): Unit = {
-    tree match {
-      case tree: Select =>
-        assert(!isJavaPackage(tree), s"Unexpected reference to Java package in ${tree.show}")
-      case _ => ()
-    }
-  }
-
-  /**
-    * Is the given tree a syntactic reference to a Java package?
-    */
-  private def isJavaPackage(tree: Select)(implicit ctx: Context): Boolean = {
-    tree.tpe match {
-      case TypeRef(prefix, _) =>
-        val flags = prefix.termSymbol.flags
-        // Testing for each flag separately is more efficient than using FlagConjunction.
-        flags.is(Package) && flags.is(JavaDefined)
-      case _ => false
-    }
+  /** Is the given tree a reference to a type in a package? */
+  private def isPackageClassRef(tree: Select)(implicit ctx: Context): Boolean = tree.tpe match {
+    case TypeRef(prefix, _) => prefix.termSymbol.is(Package)
+    case _ => false
   }
 }
