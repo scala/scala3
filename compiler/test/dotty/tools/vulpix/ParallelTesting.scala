d56c45c880ae97fefe21e950a367707a15bad9d3
Martin Odersky <odersky@gmail.com>
2017-12-08 11:31:20 +0100

    Reduce running time of testPickling
    testPickling pickled directories recursively. It seems this was unintentional
    as it caused testPickling to test most files of the dotty compiler three times,
    including two runs where basically the whole compiler was tested together. This
    was very slow as pickling (and retaining pickled info) caused very high memory pressure.
    
    We now compile root compiler directories non-recursively.

--- compiler/test/dotty/tools/vulpix/ParallelTesting.scala
+++ compiler/test/dotty/tools/vulpix/ParallelTesting.scala
@@ -1113,13 +1113,16 @@ trait ParallelTesting extends RunnerOrchestration { self =>
    *  By default, files are compiled in alphabetical order. An optional seed
    *  can be used for randomization.
    */
-  def compileDir(f: String, flags: TestFlags, randomOrder: Option[Int] = None)(implicit testGroup: TestGroup): CompilationTest = {
+  def compileDir(f: String, flags: TestFlags, randomOrder: Option[Int] = None, recursive: Boolean = true)(implicit testGroup: TestGroup): CompilationTest = {
     val outDir = defaultOutputDir + testGroup + "/"
     val sourceDir = new JFile(f)
     checkRequirements(f, sourceDir, outDir)
 
     def flatten(f: JFile): Array[JFile] =
-      if (f.isDirectory) f.listFiles.flatMap(flatten)
+      if (f.isDirectory) {
+        val files = f.listFiles
+        if (recursive) files.flatMap(flatten) else files
+      }
       else Array(f)
 
     // Sort files either alphabetically or randomly using the provided seed:
