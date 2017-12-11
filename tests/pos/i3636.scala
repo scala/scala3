c2bbff3a6543818261a4508e8c98147618434625
Martin Odersky <odersky@gmail.com>
2017-12-11 22:57:32 +0100

    Fix #3636: Better disambiguation in withPrefix
    Use the kind of disambiguation in withPrefix that we
    apply already in memberDenot.

---
+++ tests/pos/i3636.scala
@@ -0,0 +1,15 @@
+trait Iterable[A] {
+  def concat[B >: A](that: Iterable[B]): Iterable[B] = ???
+  @`inline` final def ++ [B >: A](that: Iterable[B]): Iterable[B] = concat(that)
+}
+
+class BitSet extends Iterable[Int] {
+  def concat(that: Iterable[Int]): BitSet = ???
+  @`inline` final def ++ (that: Iterable[Int]): BitSet = concat(that)
+}
+
+class Test {
+  def test(x: BitSet, y: Iterable[Int]): Unit = {
+    val foo = x ++ y
+  }
+}
