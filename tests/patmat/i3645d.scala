fecb6d0022f30cf3c1b3b39f976e9c92bbd4d4f8
liu fengyun <liu@fengy.me>
2017-12-11 01:45:28 +0100

    add more tests for variance handling

---
+++ tests/patmat/i3645d.scala
@@ -0,0 +1,29 @@
+object App {
+  def main(args: Array[String]): Unit = {
+    trait FooT {
+      type T
+      def subst[F[_]](fa: F[T]): F[Int]
+    }
+    val Foo: FooT = new FooT {
+      type T = Int
+
+      def subst[F[_]](fa: F[T]): F[Int] = fa
+    }
+    type Foo = Foo.T
+    type Bar = Foo
+
+    sealed abstract class K[-A]
+    final case object K1 extends K[Int]
+    final case object K2 extends K[Foo]
+    final case object K3 extends K[Bar]
+
+    val foo: K[Int] = Foo.subst[K](K2)
+    def get(k: K[Int]): Unit = k match {
+      case K1 => ()
+      // case K2 => ()
+      // case K3 => ()
+    }
+
+    get(foo)
+  }
+}
