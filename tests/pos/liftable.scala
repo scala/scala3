21ad0dbb8978e3c147f14fed8639c537d8f53ac7
Martin Odersky <odersky@gmail.com>
2017-12-19 14:08:14 +0100

    Fix test

--- tests/pos/liftable.scala
+++ tests/pos/liftable.scala
@@ -2,7 +2,7 @@ import scala.quoted._
 
 object Test {
 
-  implicit def IntIsQuotable: Quotable[Int] = new {
+  implicit def IntIsLiftable: Liftable[Int] = new {
     def toExpr(n: Int): Expr[Int] = n match {
       case Int.MinValue    => '(Int.MinValue)
       case _ if n < 0      => '(-(~toExpr(n)))
@@ -12,14 +12,14 @@ object Test {
     }
   }
 
-  implicit def BooleanIsQuotable: Quotable[Boolean] = new {
+  implicit def BooleanIsLiftable: Liftable[Boolean] = new {
     implicit def toExpr(b: Boolean) =
       if (b) '(true) else '(false)
   }
 
-  implicit def ListIsQuotable[T: Quotable]: Quotable[List[T]] = new {
+  implicit def ListIsLiftable[T: Liftable]: Liftable[List[T]] = new {
     def toExpr(xs: List[T]): Expr[List[T]] = xs match {
-      case x :: xs1 => '{ ~implicitly[Quotable[T]].toExpr(x) :: ~toExpr(xs1) }
+      case x :: xs1 => '{ ~implicitly[Liftable[T]].toExpr(x) :: ~toExpr(xs1) }
       case Nil => '(Nil: List[T])
     }
   }
