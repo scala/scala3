efff78b5e68347b20f82ca75516fcffdcca6df8c
Nicolas Stucki <nicolas.stucki@gmail.com>
2017-12-23 10:45:53 +0100

    Avoid running the compiler for ConstantExpr

--- compiler/src/dotty/tools/dotc/quoted/Runners.scala
+++ compiler/src/dotty/tools/dotc/quoted/Runners.scala
@@ -1,16 +1,22 @@
 package dotty.tools.dotc.quoted
 
 import scala.quoted.Expr
+import scala.quoted.Liftable.ConstantExpr
 import scala.runtime.quoted._
 
 /** Default runners for quoted expressions */
 object Runners {
+
   implicit def runner[T]: Runner[T] = new Runner[T] {
 
-    def run(expr: Expr[T]): T =
-      new QuoteDriver().run(expr)
+    def run(expr: Expr[T]): T = expr match {
+      case expr: ConstantExpr[T] => expr.value
+      case _ => new QuoteDriver().run(expr)
+    }
 
-    def show(expr: Expr[T]): String =
-      new QuoteDriver().show(expr)
+    def show(expr: Expr[T]): String = expr match {
+      case expr: ConstantExpr[T] => expr.value.toString
+      case _ => new QuoteDriver().show(expr)
+    }
   }
 }
