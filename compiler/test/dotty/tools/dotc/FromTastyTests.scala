b80d85e12baf1050947097c19f0b1ce9cc8e5cf7
Nicolas Stucki <nicolas.stucki@gmail.com>
2017-12-24 18:15:01 +0100

    Add decompiler tests

--- compiler/test/dotty/tools/dotc/FromTastyTests.scala
+++ compiler/test/dotty/tools/dotc/FromTastyTests.scala
@@ -26,7 +26,7 @@ class FromTastyTests extends ParallelTesting {
     // > dotc -Ythrough-tasty -Ycheck:all <source>
 
     implicit val testGroup: TestGroup = TestGroup("posTestFromTasty")
-    val (step1, step2) = compileTastyInDir("../tests/pos", defaultOptions,
+    val (step1, step2, step3) = compileTastyInDir("../tests/pos", defaultOptions,
       blacklist = Set(
         "NoCyclicReference.scala",
         "depfuntype.scala",
@@ -53,7 +53,8 @@ class FromTastyTests extends ParallelTesting {
     )
     step1.checkCompile() // Compile all files to generate the class files with tasty
     step2.checkCompile() // Compile from tasty
-    (step1 + step2).delete()
+    step3.checkCompile() // Decompile from tasty
+    (step1 + step2 + step3).delete()
   }
 
   @Test def runTestFromTasty: Unit = {
@@ -63,7 +64,7 @@ class FromTastyTests extends ParallelTesting {
     // > dotr Test
 
     implicit val testGroup: TestGroup = TestGroup("runTestFromTasty")
-    val (step1, step2) = compileTastyInDir("../tests/run", defaultOptions,
+    val (step1, step2, step3) = compileTastyInDir("../tests/run", defaultOptions,
        blacklist = Set(
          "Course-2002-13.scala",
          "bridges.scala",
@@ -93,7 +94,8 @@ class FromTastyTests extends ParallelTesting {
     )
     step1.checkCompile() // Compile all files to generate the class files with tasty
     step2.checkRuns() // Compile from tasty and run the result
-    (step1 + step2).delete()
+    step3.checkCompile() // Decompile from tasty
+    (step1 + step2 + step3).delete()
   }
 
   private implicit class tastyCompilationTuples(tup: (CompilationTest, CompilationTest)) {
