71a9b4e8983f16f3d189b66ff1a546510a8ffc7f
Martin Odersky <odersky@gmail.com>
2017-12-29 21:35:06 +0100

    Fix test

--- tests/neg/i3702.scala
+++ tests/neg/i3702.scala
@@ -2,7 +2,7 @@ object test {
   class annot extends scala.annotation.Annotation
   def foo = {
     def bar(i: Int): Int = i
-    @annot class Silly {}
+    @annot class Silly {} // error: not found
     bar(5)
   }
 }
