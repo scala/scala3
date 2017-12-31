81efdfaa2f56f13c24ed50aec29b8b5673d25035
Guillaume Martres <smarter@ubuntu.com>
2017-12-31 17:46:40 +0100

    Improve traitNoInit test to actually test if $init$ is present

--- tests/run/traitNoInit.scala
+++ tests/run/traitNoInit.scala
@@ -1,12 +1,29 @@
-trait Foo {
-  //println("hello")
+trait NoInit {
+  def meth(x: Int): Int
+}
+
+trait WithInit {
+  val i = 1
   def meth(x: Int): Int
 }
 
 trait Bar(x: Int)
 
-class C extends Foo() with Bar(1) {
+class NoInitClass extends NoInit() with Bar(1) {
   def meth(x: Int) = x
 }
 
-object Test extends C with App
+class WithInitClass extends WithInit() with Bar(1) {
+  def meth(x: Int) = x
+}
+
+object Test {
+  def hasInit(cls: Class[_]) = cls.getMethods.map(_.toString).exists(_.contains("$init$"))
+  def main(args: Array[String]): Unit = {
+    val noInit = new NoInitClass {}
+    val withInit = new WithInitClass {}
+
+    assert(!hasInit(noInit.getClass))
+    assert(hasInit(withInit.getClass))
+  }
+}
