ad96b032a3d8d3e7e228f4b99b65ffa28bb64c83
Martin Odersky <odersky@gmail.com>
2018-01-03 19:06:13 +0100

    Fix #3736: Search all imports for language features

--- compiler/src/dotty/tools/dotc/core/TypeOps.scala
+++ compiler/src/dotty/tools/dotc/core/TypeOps.scala
@@ -292,16 +292,16 @@ trait TypeOps { this: Context => // TODO: Make standalone object.
       if (!sym.exists || (sym eq defn.LanguageModuleClass)) ""
       else toPrefix(sym.owner) + sym.name + "."
     def featureName = toPrefix(owner) + feature
-    def hasImport(implicit ctx: Context): Boolean = {
-      if (ctx.importInfo == null || (ctx.importInfo.site.widen.typeSymbol ne owner)) false
-      else if (ctx.importInfo.excluded.contains(feature)) false
-      else if (ctx.importInfo.originals.contains(feature)) true
-      else {
-        var c = ctx.outer
-        while (c.importInfo eq ctx.importInfo) c = c.outer
-        hasImport(c)
+    def hasImport(implicit ctx: Context): Boolean =
+      ctx.importInfo != null && {
+        ctx.importInfo.site.widen.typeSymbol == owner &&
+        !ctx.importInfo.excluded.contains(feature) &&
+        ctx.importInfo.originals.contains(feature) || {
+          var c = ctx.outer
+          while (c.importInfo eq ctx.importInfo) c = c.outer
+          hasImport(c)
+        }
       }
-    }
     def hasOption = ctx.base.settings.language.value exists (s => s == featureName || s == "_")
     hasImport(ctx.withPhase(ctx.typerPhase)) || hasOption
   }
