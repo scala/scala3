6129ac500d30a461ce9369d37e5025275fd06622
Nicolas Stucki <nicolas.stucki@gmail.com>
2018-01-11 19:36:26 +0100

    Add test

--- tests/neg-custom-args/i2333.scala
+++ tests/neg-custom-args/i2333.scala
@@ -1,5 +1,8 @@
-@deprecated("bla", "2.11.0") class Foo
+@deprecated("bla", "2.11.0") class Foo {
+  def this(x: Int) = this()
+}
 
 object Test {
-  new Foo // error
+  new Foo // error: deprecated
+  new Foo(1) // error: deprecated
 }
