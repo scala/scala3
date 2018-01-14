9b48d071f982123ccd81c33a8de9a3bf79dcb9d8
Martin Odersky <odersky@gmail.com>
2018-01-14 01:12:54 +0100

    More test to pos
    It succeeds now, still worth keeping.

--- tests/pos/i3703.scala
+++ tests/pos/i3703.scala
@@ -3,9 +3,7 @@ package bar {
   class S[XS[_] <: M[XS], A](val x: XS[A])
   object S {
     def apply[X[_] <: M[X], A](x: X[A]): S[X, A] = S[X, A](x)
-    def unapply[X[_] <: M[X], A](p: S[X, A]): S[X, A] = S[X, A](p.x)
-      // type annotation required for dotc. scalac accepts also `= S(p.x)`.
-  }
+    def unapply[X[_] <: M[X], A](p: S[X, A]): S[X, A] = S(p.x)
 }
 
 
