798b0d3f63552bd04472ac4ea885ad5384d60e65
liu fengyun <liu@fengy.me>
2018-01-18 14:30:49 +0100

    remove noise in test code

--- tests/neg/i3812.scala
+++ tests/neg/i3812.scala
@@ -1,11 +1,7 @@
 object Test {
   def main(args: Array[String]): Unit = {
-    case class Box(v: Int)
-
     val x = 42
     val Y = "42"
-    var Z1 = Box(4)
-    val Z2 = Box(4)
 
     x match { case { 42 }           => () } // error
     x match { case { "42".toInt }   => () } // error
