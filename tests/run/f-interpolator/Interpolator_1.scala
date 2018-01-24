982e354f9abfef62eccbea49a37cc360ca4a7dbb
Nicolas Stucki <nicolas.stucki@gmail.com>
2018-01-24 08:19:31 +0100

    Workaround #3898

--- tests/run/f-interpolator/Interpolator_1.scala
+++ tests/run/f-interpolator/Interpolator_1.scala
@@ -3,7 +3,10 @@ import scala.quoted._
 object FInterpolation {
 
   implicit class FInterpolatorHelper(val sc: StringContext) extends AnyVal {
-    inline def ff(args: Any*): String = ~fInterpolation(sc, '(args))
+    inline def ff(arg1: Any): String = ~fInterpolation(sc, '(Seq(arg1)))
+    inline def ff(arg1: Any, arg2: Any): String = ~fInterpolation(sc, '(Seq(arg1, arg2)))
+    inline def ff(arg1: Any, arg2: Any, arg3: Any): String = ~fInterpolation(sc, '(Seq(arg1, arg2, arg3)))
+    // ...
   }
 
   def fInterpolation(sc: StringContext, args: Expr[Seq[Any]]): Expr[String] = {
--- tests/run/f-interpolator/Test_2.scala
+++ tests/run/f-interpolator/Test_2.scala
@@ -4,5 +4,7 @@ import FInterpolation._
 object Test {
   def main(args: Array[String]): Unit = {
     println(ff"abc${5}")
+    println(ff"abc${5}asf${"l"}")
+    println(ff"abc${5}${6}asf${8}")
   }
 }
