e6219ab69706c5f45a5ce18b8ac95b1da4c5e1f1
Nicolas Stucki <nicolas.stucki@gmail.com>
2018-01-24 11:59:01 +0100

    Add tests files from benchmarks

---
+++ tests/pos/power-macro/PowerInlined-1_2.scala
@@ -0,0 +1,5 @@
+object PowerInlined1 {
+  import PowerMacro._
+
+  power(1, 5.0) // 1 quotes to unpickle
+}
---
+++ tests/pos/power-macro/PowerInlined-1k_2.scala
@@ -0,0 +1,12 @@
+object PowerInlined1k {
+  import PowerMacro._
+
+  power(9223372036854775807L, 5.0) // 125 quotes to unpickle
+  power(9223372036854775807L, 5.0) // 125 quotes to unpickle
+  power(9223372036854775807L, 5.0) // 125 quotes to unpickle
+  power(9223372036854775807L, 5.0) // 125 quotes to unpickle
+  power(9223372036854775807L, 5.0) // 125 quotes to unpickle
+  power(9223372036854775807L, 5.0) // 125 quotes to unpickle
+  power(9223372036854775807L, 5.0) // 125 quotes to unpickle
+  power(9223372036854775807L, 5.0) // 125 quotes to unpickle
+}
