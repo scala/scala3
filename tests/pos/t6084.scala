07ced9d9f2bb304ea40f254a8ac40768f08502e5
Martin Odersky <odersky@gmail.com>
2018-02-20 13:50:09 +0100

    Be more careful when constraining parameter types wrt scrutinees
    i3989a.scala gives an example which is rejected under the new scheme.
    This would pass and fail at runtime under the previous scheme.

--- tests/pos/t6084.scala
+++ tests/pos/t6084.scala
@@ -1,10 +1,13 @@
 package object foo { type X[T, U] = (T => U) }
 
 package foo {
-  abstract class Foo[T, U](val d: T => U) extends (T => U) {
+  // Note that Foo must be final because of #3989.
+  final class Foo[T, U](val d: T => U) extends (T => U) {
     def f1(r: X[T, U])           = r match { case x: Foo[_,_] => x.d }  // inferred ok
     def f2(r: X[T, U]): (T => U) = r match { case x: Foo[_,_] => x.d }  // dealiased ok
     def f3(r: X[T, U]): X[T, U]  = r match { case x: Foo[_,_] => x.d }  // alias not ok
+
+    def apply(x: T): U = d(x)
 
     // x.d : foo.this.package.type.X[?scala.reflect.internal.Types$NoPrefix$?.T, ?scala.reflect.internal.Types$NoPrefix$?.U] ~>scala.this.Function1[?scala.reflect.internal.Types$NoPrefix$?.T, ?scala.reflect.internal.Types$NoPrefix$?.U]
     //  at scala.Predef$.assert(Predef.scala:170)
