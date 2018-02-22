1a522f3a2bbec61f9542f6654213cdc6bccb5313
Martin Odersky <odersky@gmail.com>
2018-02-22 17:57:18 +0100

    Add reference documentation

--- tests/pos/opaque.scala
+++ tests/pos/opaque.scala
@@ -26,7 +26,7 @@ object usesites {
   val l = Logarithm(1.0)
   val l2 = Logarithm(2.0)
   val l3 = l * l2
-  val l4 = l * l2
+  val l4 = l + l2
   val d = l3.toDouble
   val l4: Logarithm = (1.0).asInstanceOf[Logarithm]
 }
