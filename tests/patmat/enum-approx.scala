21e572155140f5e1f0de1e1b19f4a62959ddad6b
liu fengyun <liu@fengy.me>
2018-03-01 16:24:51 +0100

    handle binder type symbols in extractor patterns

---
+++ tests/patmat/enum-approx.scala
@@ -0,0 +1,31 @@
+enum Fun[-T, +U >: Null] {
+  def f: T => U = this match {
+    case Identity(g) => g
+    case ConstNull => (_ => null)
+    case ConstNullClass(y) => (_ => null)
+    case ConstNullSimple => null
+  }
+
+  case Identity[T, U >: Null](g: T => U) extends Fun[T, U]
+  case ConstNull
+  case ConstNullClass(x: T)
+  case ConstNullSimple
+}
+
+object Test {
+  def main(args: Array[String]) = {
+    val x: Null = Fun.ConstNull.f("abc")
+    val y: Null = Fun.ConstNullClass("hello").f("abc")
+    assert(Fun.ConstNullSimple.f == null)
+  }
+
+  import Fun._
+
+  def f[T, U >: Null](f: Fun[T, U]): T => U = f match {
+    case Identity(g) => g
+    case ConstNull => (_ => null)
+    case ConstNullClass(y: Int) => (_ => null)
+    case ConstNullSimple => null
+  }
+}
+
