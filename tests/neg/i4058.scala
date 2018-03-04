62997855ad2f75c2a775ddc4648d63cbdbfd6077
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2018-03-04 23:44:53 +0100

    Test a few extra variants
    This might be overkill, since these modifiers aren't expected here.

--- tests/neg/i4058.scala
+++ tests/neg/i4058.scala
@@ -1,3 +1,9 @@
 class A(sealed val a: Int) // error
 class B(lazy val a: Int) // error
 class C(abstract val a: Int) // error
+class D {
+  def f(sealed a: Int) = 0 // error
+  def g(lazy a: Int) = 0 // error
+  def g(override a: Int) = 0 // error
+  def g(abstract a: Int) = 0 // error
+}
