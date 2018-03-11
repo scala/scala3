345a80822a5631057ef43a8799aa328e6f1d1b8e
Nicolas Stucki <nicolas.stucki@gmail.com>
2018-03-11 12:13:58 +0100

    Add error reporter for inlined quotes

---
+++ tests/neg/quote-exception/Macro_1.scala
@@ -0,0 +1,8 @@
+import quoted._
+
+object Macro_1 {
+  inline def foo(inline b: Boolean): Unit = ~fooImpl(b)
+  def fooImpl(b: Boolean): Expr[Unit] =
+    if (b) '(println("foo(true)"))
+  else ???
+}
