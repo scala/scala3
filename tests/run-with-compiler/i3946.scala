44259c6a84709b7d48286c2c6da4c6f2a3eb111b
Nicolas Stucki <nicolas.stucki@gmail.com>
2018-03-16 10:40:04 +0100

    Remove quoted.Liftable for Unit
    To lift a unit we only need to write `'()` which will still
    become a ValueExpr by some optimization in ReifyQuotes.

--- tests/run-with-compiler/i3946.scala
+++ tests/run-with-compiler/i3946.scala
@@ -2,7 +2,7 @@ import dotty.tools.dotc.quoted.Toolbox._
 import scala.quoted._
 object Test {
   def main(args: Array[String]): Unit = {
-    val u: Expr[Unit] = ()
+    val u: Expr[Unit] = '()
     println(u.show)
     println(u.run)
   }
--- tests/run-with-compiler/quote-show-blocks.scala
+++ tests/run-with-compiler/quote-show-blocks.scala
@@ -11,14 +11,14 @@ object Test {
       if (n == 0) x
       else a(n - 1, '{ println(~n.toExpr); ~x })
 
-    println(a(5, ()).show)
+    println(a(5, '()).show)
 
 
     def b(n: Int, x: Expr[Unit]): Expr[Unit] =
       if (n == 0) x
       else b(n - 1, '{ ~x; println(~n.toExpr) })
 
-    println(b(5, ()).show)
+    println(b(5, '()).show)
   }
 
 }
