scala/scala3

commit 21ab9a1355036aa953db4e1f87c8f0f9a06506b5

Get rid of ExpandedName flag

compiler/src/dotty/tools/backend/jvm/DottyBackendInterface.scala
@@ -421,7 +421,7 @@ class DottyBackendInterface(outputDirectory: AbstractFile, val superCallsMap: Ma
   val Flag_METHOD: Flags = Flags.Method.bits
   val ExcludedForwarderFlags: Flags = {
       Flags.Specialized | Flags.Lifted | Flags.Protected | Flags.JavaStatic |
-     Flags.ExpandedName | Flags.Bridge | Flags.VBridge | Flags.Private | Flags.Macro
+      Flags.Bridge | Flags.VBridge | Flags.Private | Flags.Macro
   }.bits
 
 

compiler/src/dotty/tools/dotc/ast/Desugar.scala
@@ -244,7 +244,7 @@ object desugar {
   def typeDef(tdef: TypeDef)(implicit ctx: Context): Tree = {
     if (tdef.mods is PrivateLocalParam) {
       val tparam = cpy.TypeDef(tdef)(name = tdef.name.expandedName(ctx.owner))
-        .withMods(tdef.mods &~ PrivateLocal | ExpandedName)
+        .withMods(tdef.mods &~ PrivateLocal)
       val alias = cpy.TypeDef(tdef)(rhs = refOfDef(tparam))
         .withMods(tdef.mods & VarianceFlags | PrivateLocalParamAccessor | Synthetic)
       Thicket(tparam, alias)

compiler/src/dotty/tools/dotc/core/Definitions.scala
@@ -67,7 +67,7 @@ class Definitions {
     enterTypeField(cls, name, flags | ClassTypeParamCreationFlags, scope)
 
   private def enterSyntheticTypeParam(cls: ClassSymbol, paramFlags: FlagSet, scope: MutableScope, suffix: String = "T0") =
-    enterTypeParam(cls, suffix.toTypeName.expandedName(cls), ExpandedName | paramFlags, scope)
+    enterTypeParam(cls, suffix.toTypeName.expandedName(cls), paramFlags, scope)
 
   // NOTE: Ideally we would write `parentConstrs: => Type*` but SIP-24 is only
   // implemented in Dotty and not in Scala 2.

compiler/src/dotty/tools/dotc/core/Flags.scala
@@ -272,9 +272,6 @@ object Flags {
    */
   final val Synthetic = commonFlag(18, "<synthetic>")
 
-  /** Symbol's name is expanded */
-  final val ExpandedName = commonFlag(19, "<expandedname>")
-
   /** A covariant type variable / an outer accessor */
   final val CovariantOrOuter = commonFlag(20, "")
   final val Covariant = typeFlag(20, "<covariant>")
@@ -308,7 +305,6 @@ object Flags {
   final val CaseAccessor = termFlag(25, "<caseaccessor>")
 
   /** A binding for a type parameter of a base class or trait.
-   *  TODO: Replace with combination of isType, ExpandedName, and Override?
    */
   final val BaseTypeArg = typeFlag(25, "<basetypearg>")
 
@@ -409,9 +405,6 @@ object Flags {
   final val Scala2ExistentialCommon = commonFlag(55, "<existential>")
   final val Scala2Existential = Scala2ExistentialCommon.toTypeFlags
 
-  /** An overloaded symbol (Scala 2.x only) */
-  final val Scala2Overloaded = termFlag(56, "<overloaded>")
-
   /** A module variable (Scala 2.x only) */
   final val Scala2ModuleVar = termFlag(57, "<modulevar>")
 
@@ -423,6 +416,13 @@ object Flags {
 
   /** A method that is known to have inherited default parameters */
   final val InheritedDefaultParams = termFlag(60, "<inherited-default-param>")
+
+  /** Translation of Scala2's EXPANDEDNAME flag. This flag is never stored in
+   *  symbols, is only used locally when reading the flags of a Scala2 symbol.
+   *  It's therefore safe to share the code with `InheritedDefaultParams` because
+   *  the latter is never present in Scala2 unpickle info.
+   */
+  final val Scala2ExpandedName = InheritedDefaultParams.toCommonFlags
 
   /** A method that is known to have no default parameters */
   final val NoDefaultParams = termFlag(61, "<no-default-param>")
@@ -475,7 +475,7 @@ object Flags {
 
   /** Flags that are passed from a type parameter of a class to a refinement symbol
     * that sets the type parameter */
-  final val RetainedTypeArgFlags = VarianceFlags | ExpandedName | Protected | Local
+  final val RetainedTypeArgFlags = VarianceFlags | Protected | Local
 
   /** Modules always have these flags set */
   final val ModuleCreationFlags = ModuleVal | Lazy | Final | Stable
@@ -502,7 +502,7 @@ object Flags {
     */
   final val RetainedModuleValAndClassFlags: FlagSet =
     AccessFlags | Package | Case |
-    Synthetic | ExpandedName | JavaDefined | JavaStatic | Artifact |
+    Synthetic | JavaDefined | JavaStatic | Artifact |
     Erroneous | Lifted | MixedIn | Specialized
 
   /** Flags that can apply to a module val */
@@ -550,9 +550,6 @@ object Flags {
   /** A private accessor */
   final val PrivateAccessor = allOf(Private, Accessor)
 
-  /** A type parameter with synthesized name */
-  final val ExpandedTypeParam = allOf(ExpandedName, TypeParam)
-
   /** An inline method */
   final val InlineMethod = allOf(Inline, Method)
 
@@ -578,7 +575,7 @@ object Flags {
   final val FinalOrInline = Final | Inline
 
   /** If symbol of a type alias has these flags, prefer the alias */
-  final val AliasPreferred = TypeParam | BaseTypeArg | ExpandedName
+  final val AliasPreferred = TypeParam | BaseTypeArg
 
   /** A covariant type parameter instance */
   final val LocalCovariant = allOf(Local, Covariant)

compiler/src/dotty/tools/dotc/core/NameExtractors.scala
@@ -45,7 +45,7 @@ object NameExtractors {
 
   class PrefixNameExtractor(tag: Int, prefix: String, infoString: String) extends ClassifiedNameExtractor(tag, infoString) {
     def mkString(underlying: TermName, info: ThisInfo) =
-      underlying.mapLast(n => termName(prefix + n)).toString
+      underlying.mapLast(n => termName(prefix + n.toString)).toString
   }
 
   class SuffixNameExtractor(tag: Int, suffix: String, infoString: String) extends ClassifiedNameExtractor(tag, infoString) {
@@ -99,7 +99,7 @@ object NameExtractors {
 
   object QualifiedName   extends QualifiedNameExtractor(QUALIFIED, ".", "Qualified")
   object FlattenedName   extends QualifiedNameExtractor(FLATTENED, "$", "Flattened")
-  object XpandedName    extends QualifiedNameExtractor(EXPANDED, str.EXPAND_SEPARATOR, "Expanded")
+  object ExpandedName    extends QualifiedNameExtractor(EXPANDED, str.EXPAND_SEPARATOR, "Expanded")
   object TraitSetterName extends QualifiedNameExtractor(TRAITSETTER, str.TRAIT_SETTER_SEPARATOR, "TraitSetter")
 
   object DefaultGetterName extends NumberedNameExtractor(DEFAULTGETTER, "DefaultGetter") {
@@ -146,6 +146,6 @@ object NameExtractors {
   val separatorToQualified: Map[String, QualifiedNameExtractor] =
     Map("." -> QualifiedName,
       "$" -> FlattenedName,
-      str.EXPAND_SEPARATOR -> XpandedName,
+      str.EXPAND_SEPARATOR -> ExpandedName,
       str.TRAIT_SETTER_SEPARATOR -> TraitSetterName)
 }

compiler/src/dotty/tools/dotc/core/NameOps.scala
@@ -138,7 +138,7 @@ object NameOps {
     /** The expanded name of `name` relative to given class `base`.
      */
     def expandedName(base: Symbol, separator: Name)(implicit ctx: Context): N =
-      expandedName(if (base is Flags.ExpandedName) base.name else base.fullNameSeparated("$"), separator)
+      expandedName(if (base.name.is(ExpandedName)) base.name else base.fullNameSeparated("$"), separator)
 
     def expandedName(base: Symbol)(implicit ctx: Context): N = expandedName(base, nme.EXPAND_SEPARATOR)
 
@@ -161,7 +161,7 @@ object NameOps {
 
     /** Revert the expanded name. */
     def unexpandedName: N = likeTyped {
-      name.rewrite { case XpandedName(_, unexp) => unexp }
+      name.rewrite { case ExpandedName(_, unexp) => unexp }
     }
 
     def unexpandedNameOfMangled: N = likeTyped {
@@ -175,8 +175,8 @@ object NameOps {
       if (idx < 0) name else (name drop (idx + nme.EXPAND_SEPARATOR.length))
     }
 
-    def expandedPrefix: N = likeTyped { name.exclude(XpandedName) }
- 
+    def expandedPrefix: N = likeTyped { name.exclude(ExpandedName) }
+
     def expandedPrefixOfMangled: N = {
       val idx = name.lastIndexOfSlice(nme.EXPAND_SEPARATOR)
       assert(idx >= 0)
@@ -188,7 +188,7 @@ object NameOps {
         val unmangled = unexpandedNameOfMangled
         if (name eq unmangled) name
         else likeTyped(
-          XpandedName(expandedPrefixOfMangled.toTermName, unmangled.asSimpleName))
+          ExpandedName(expandedPrefixOfMangled.toTermName, unmangled.asSimpleName))
       }
       else name
 

compiler/src/dotty/tools/dotc/core/Signature.scala
@@ -34,6 +34,14 @@ import scala.annotation.tailrec
 case class Signature(paramsSig: List[TypeName], resSig: TypeName) {
   import Signature._
 
+/* FIXME does not compile under dotty, we get a missing param error
+  def checkUnqual(name: TypeName) = name mapParts { part =>
+    assert(!part.contains('.'), name)
+    part
+  }
+  paramsSig.foreach(checkUnqual)
+  checkUnqual(resSig)
+*/
   /** Two names are consistent if they are the same or one of them is tpnme.Uninstantiated */
   private def consistent(name1: TypeName, name2: TypeName) =
     name1 == name2 || name1 == tpnme.Uninstantiated || name2 == tpnme.Uninstantiated

compiler/src/dotty/tools/dotc/core/SymDenotations.scala
@@ -367,7 +367,7 @@ object SymDenotations {
 
     /** The expanded name of this denotation. */
     final def expandedName(implicit ctx: Context) =
-      if (is(ExpandedName) || isConstructor) name
+      if (name.is(ExpandedName) || isConstructor) name
       else {
         def legalize(name: Name): Name = // JVM method names may not contain `<' or `>' characters
           if (is(Method)) name.replace('<', '(').replace('>', ')') else name
@@ -1210,9 +1210,7 @@ object SymDenotations {
     /** If denotation is private, remove the Private flag and expand the name if necessary */
     def ensureNotPrivate(implicit ctx: Context) =
       if (is(Private))
-        copySymDenotation(
-          name = expandedName,
-          initFlags = this.flags &~ Private | ExpandedName)
+        copySymDenotation(name = expandedName, initFlags = this.flags &~ Private)
       else this
   }
 

commit 12e6ae339d88d0b2701db41e2a1fbf049bd45466

Fix #4138: Do not emit forwarders for methods with expanded names
This was accidentally broken in 21ab9a1355036aa953db4e1f87c8f0f9a06506b5
when the ExpandedName symbol flag was replaced by a name kind.

compiler/src/dotty/tools/backend/jvm/DottyBackendInterface.scala
@@ -33,7 +33,7 @@ import tpd._
 import scala.tools.asm
 import StdNames.{nme, str}
 import NameOps._
-import NameKinds.DefaultGetterName
+import NameKinds.{DefaultGetterName, ExpandedName}
 import dotty.tools.dotc.core
 import dotty.tools.dotc.core.Names.TypeName
 
@@ -677,6 +677,7 @@ class DottyBackendInterface(outputDirectory: AbstractFile, val superCallsMap: Ma
     def isType: Boolean = sym.isType
     def isAnonymousClass: Boolean = toDenot(sym).isAnonymousClass
     def isConstructor: Boolean = toDenot(sym).isConstructor
+    def isExpanded: Boolean = sym.name.is(ExpandedName)
     def isAnonymousFunction: Boolean = toDenot(sym).isAnonymousFunction
     def isMethod: Boolean = sym is Flags.Method
     def isPublic: Boolean =  sym.flags.is(Flags.EmptyFlags, Flags.Private | Flags.Protected)

tests/run/forwarder.check
@@ -1,0 +1,1 @@
+public static int Foo.hi()

tests/run/forwarder.scala
@@ -1,0 +1,20 @@
+class Foo
+object Foo extends Bar
+
+trait Bar {
+  def hi: Int = 1
+  private def foo: Int = 1
+
+  class Inner {
+    val a = foo // Force foo to be expanded by ExpandPrivate
+  }
+}
+
+object Test {
+  def main(args: Array[String]): Unit = {
+    println(
+      classOf[Foo].getMethods
+        .filter(m => (m.getModifiers & java.lang.reflect.Modifier.STATIC) != 0)
+        .mkString("\n"))
+  }
+}