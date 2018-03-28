480f6a2d52b73d7246468620c69e3043f9893d3f
Martin Duhem <martin.duhem@gmail.com>
2018-03-28 10:52:16 +0200

    Typo: competion -> completion

--- language-server/test/dotty/tools/languageserver/CompletionTest.scala
+++ language-server/test/dotty/tools/languageserver/CompletionTest.scala
@@ -6,7 +6,7 @@ import dotty.tools.languageserver.util.Code._
 
 class CompletionTest {
 
-  @Test def competion0: Unit = {
+  @Test def completion0: Unit = {
     code"class Foo { val xyz: Int = 0; def y: Int = xy$m1 }".withSource
       .completion(m1, List(("xyz", "Field", "Int")))
   }
