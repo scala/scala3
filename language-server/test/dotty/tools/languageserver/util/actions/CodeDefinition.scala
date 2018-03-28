c1d6a8acacb52657d8e61ccaac51f6ff20c7fbd7
Martin Duhem <martin.duhem@gmail.com>
2018-03-28 21:07:07 +0200

    Use `JavaConverters` in `CodeDefinition`

--- language-server/test/dotty/tools/languageserver/util/actions/CodeDefinition.scala
+++ language-server/test/dotty/tools/languageserver/util/actions/CodeDefinition.scala
@@ -3,6 +3,8 @@ package dotty.tools.languageserver.util.actions
 import dotty.tools.languageserver.util._
 import dotty.tools.languageserver.util.embedded.CodeMarker
 
+import scala.collection.JavaConverters._
+
 /**
  * An action requesting for the definition of the symbol inside `range`.
  * This action corresponds to the `textDocument/definition` method of the Language Server Protocol.
@@ -15,8 +17,9 @@ class CodeDefinition(override val range: CodeRange, expected: Seq[CodeRange]) ex
   override def onMarker(marker: CodeMarker): Exec[Unit] = {
     val results = server.definition(fix(marker.toTextDocumentPositionParams)).get()
     assert(results.size == expected.size, s"Expected ${expected.size} matches, found ${results.size}")
-    (0 until results.size).foreach { i =>
-      assert(results.get(i) == expected(i).toLocation, s"Expected ${expected(i).toLocation}, found ${results.get(i)}.")
+    results.asScala.zip(expected).foreach {
+      case (result, expected) =>
+        assert(result == expected.toLocation, s"Expected ${expected.toLocation}, found $result.")
     }
   }
 
