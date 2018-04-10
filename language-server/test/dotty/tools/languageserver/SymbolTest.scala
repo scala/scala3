2a511eadf889551834d33764ce8be00487a48dbf
Martin Duhem <martin.duhem@gmail.com>
2018-04-10 09:24:11 +0200

    Revive commented-out test

--- language-server/test/dotty/tools/languageserver/SymbolTest.scala
+++ language-server/test/dotty/tools/languageserver/SymbolTest.scala
@@ -7,8 +7,10 @@ import dotty.tools.languageserver.util.Code._
 
 class SymbolTest {
 
-//  @Test def symbol0: Unit =
-//    code"class ${m1}Foo$m2".withSource.symbol("Foo", ("Foo", "Class", m1 to m2))
+ @Test def symbol0: Unit = {
+   val Foo = (m1 to m2).withCode("Foo")
+   withSources(code"class $Foo").symbol("Foo", Foo.range.symInfo("Foo", SymbolKind.Class))
+ }
 
   @Test def symbol1: Unit = {
     val Foo = (m1 to m2).withCode("Foo")
