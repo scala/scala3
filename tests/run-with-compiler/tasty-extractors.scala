f8d198af9e9d290086bb63bd1160bd3c35b47693
Nicolas Stucki <nicolas.stucki@gmail.com>
2018-04-12 10:58:18 +0200

    Partial implementation of tasty tree traverser

--- tests/run-with-compiler/tasty-extractors.scala
+++ tests/run-with-compiler/tasty-extractors.scala
@@ -1,8 +1,14 @@
 
 import scala.quoted._
+
 import scala.tasty.constants.Constant
-import scala.tasty.statements._
-import scala.tasty.terms._
+import scala.tasty.names
+import scala.tasty.patterns
+import scala.tasty.statements
+import scala.tasty.terms
+import scala.tasty.typetrees
+import scala.tasty.types
+
 import dotty.tools.dotc.quoted.Toolbox._
 import dotty.tools.dotc.tasty.Toolbox._
 
@@ -67,129 +73,211 @@ object Test {
     '{ type Foo >: Null <: Object },
   )
 
-//  def str(arg: TopLevelStatement): String = arg match {
-//    case arg: Term => str(arg)
-//    case arg: Definition => str(arg)
-//    case Package(pkg, body) => s"Package(${str(pkg)}, ${body.map(str)})"
-//    case Import(expr, selectors) => s"Import(${str(expr)}, ${selectors.map(str)})"
-//  }
-//
-//  def str(arg: Definition): String = arg match {
-//    case ValDef(name, tpt, rhs, mods) => s"ValDef(${str(name)}, ${str(tpt)}, ${rhs.map(str)}, ${mods.map(str)})"
-//    case DefDef(name, typeParams, paramss, returnTpt, rhs, mods) =>
-//      s"DefDef(${str(name)}, ${typeParams.map(str)}, ${paramss.map(_.map(str))}, ${str(returnTpt)}, ${rhs.map(str)}, ${mods.map(str)})"
-//    case TypeDef(name, rhs, mods) => s"TypeDef(${str(name)}, ${str(rhs)}, ${mods.map(str)})"
-//    case ClassDef(name, constructor, parents, self, body, mods) =>
-//      s"ClassDef(${str(name)}, ${str(constructor)}, ${parents.map(str)}, ${self.map(str)}, ${body.map(str)}, ${mods.map(str)})"
-//    case _ => "###"
-//  }
-//
-//  def str(arg: Term): String = arg match {
-//    case Ident(name) => s"Ident(${str(name)})"
-//    case Select(qual, name) => s"Select(${str(qual)}, ${str(name)})"
-//    case Literal(const) => s"Literal(${str(const)})"
-//    case New(tpt) => s"New(${str(tpt)})"
-//    case NamedArg(name, arg) => s"NamedArg(${str(name)}, ${str(arg)})"
-//    case Apply(fn, args) => s"Apply(${str(fn)}, ${args.map(str)})"
-//    case TypeApply(fn, args) => s"TypeApply(${str(fn)}, ${args.map(str)})"
-//    case Super(qual, mixin) => s"Super(${str(qual)}, ${mixin.map(str)})"
-//    case Typed(expr, tpt) => s"Typed(${str(expr)}, ${str(tpt)})"
-//    case Assign(lhs, rhs) => s"Assign(${str(lhs)}, ${str(rhs)})"
-//    case Block(stats, expr) => s"Block(${stats.map(str)}, ${str(expr)})"
-//    case Lambda(meth, tpt) => s"Lambda(${str(meth)}, ${tpt.map(str)})"
-//    case If(cond, thenp, elsep) => s"If(${str(cond)}, ${str(thenp)}, ${str(elsep)})"
-//    case Match(selector, cases) => s"Match(${str(selector)}, ${cases.map(str)})"
-//    case Try(body, catches, finalizer) => s"Try(${str(body)}, ${catches.map(str)}, ${finalizer.map(str)})"
-//    case Return(expr) => s"Return(${str(expr)})"
-//    case Repeated(args) => s"Repeated(${args.map(str)})"
-//    case _ => "###"
-//  }
-//
-//  def str(arg: scala.tasty.pattern.CaseDef): String = {
-//    import scala.tasty.pattern.CaseDef
-//    arg match {
-//      case CaseDef(pat, guard, body) => s"CaseDef(${str(pat)}, ${guard.map(str)}, ${str(body)})"
-//      case _ => "###"
-//    }
-//  }
-//
-//  def str(arg: scala.tasty.pattern.Pattern): String = {
-//    import scala.tasty.pattern._
-//    arg match {
-//      case Value(v) => s"Value(${str(v)})"
-//      case Bind(name, body) => s"Bind(${str(name)}, ${str(body)})"
-//      case Unapply(fun, implicits, patterns) => s"Unapply(${str(fun)}, ${implicits.map(str)}, ${patterns.map(str)})"
-//      case Alternative(patterns) => s"Alternative(${patterns.map(str)})"
-//      case TypeTest(tpt) => s"TypeTest(${str(tpt)})"
-//      case Wildcard() => s"Wildcard()"
-//      case _ => "###"
-//    }
-//  }
-//
-//  def str(arg: TermName): String = {
-//    import scala.tasty._
-//    arg match {
-//      case Simple(name) => name.toString // TODO
-//      case _ => "###"
-//    }
-//  }
-//
-//  def str(arg: PossiblySignedName): String = arg match {
-//    case arg: TermName => str(arg)
-//    case _ => "###"
-//  }
-//
-//  def str(arg: scala.tasty.typetree.TypeTree): String = {
-//    import scala.tasty.typetree._
-//    arg match {
-//      case Synthetic() => "Synthetic()"
-//      case Ident(name) => s"Ident(${str(name)})"
-//      case Select(qual, name) => s"Select(${str(qual)}, ${str(name)})"
-//      case Singleton(ref) => s"Singleton(${str(ref)})"
-//      //      case Refined(tpt, refinements) => s"Refined($ref, ${list(refinements)})"
-//      case Applied(tycon, args) => s"Applied(${str(tycon)}, ${args.map(str)})"
-//      case TypeBounds(lo, hi) => s"TypeBounds(${str(lo)}, ${str(hi)})"
-//      case Annotated(arg, annot) => s"Annotated(${str(arg)}, ${str(annot)})"
-//      case And(left, right) => s"And(${str(left)}, ${str(right)})"
-//      case Or(left, right) => s"Or(${str(left)}, ${str(right)})"
-//      case ByName(tpt) => s"ByName(${str(tpt)})"
-//      case _ => "###"
-//    }
-//  }
-//
-//  def str(arg: scala.tasty.TypeName): String = arg match {
-//    case _ => arg.toString // TODO
-//    case _ => "###"
-//  }
-//
-//  def str(arg: scala.tasty.Id): String = s"Id(${arg.name})"
-//
-//  def str(arg: Modifier): String = arg match {
-//    case _ => ???
-//  }
-//
-//  def str(arg: Import.ImportSelector): String = arg match {
-//    case Import.ImportSelector.Simple(id) => s"Simple(${str(id)})"
-//    case Import.ImportSelector.Rename(id1, id2) => s"Rename(${str(id1)}, ${str(id2)})"
-//    case Import.ImportSelector.Omit(id) => s"Omit(${str(id)})"
-//    case _ => "###"
-//  }
-//
-//  def str(arg: Constant): String = {
-//    import scala.tasty._
-//    arg match {
-//      case Unit() => "Unit()"
-//      case Null() => "Null()"
-//      case Boolean(value) => s"Boolean($value)"
-//      case Byte(value) => s"Byte($value)"
-//      case Short(value) => s"Short($value)"
-//      case Char(value) => s"Char('$value')"
-//      case Int(value) => s"Int($value)"
-//      case Long(value) => s"Long($value)"
-//      case Float(value) => s"Float($value)"
-//      case Double(value) => s"Double($value)"
-//      case String(value) => s"""String("$value")"""
-//      case _ => "###"
-//    }
-//  }
+  class TreeTraverser {
+
+    def traverse(arg: statements.TopLevelStatement): Unit = {
+      import statements._
+      arg match {
+        case arg: terms.Term =>
+          traverse(arg)
+        case arg: Definition =>
+          traverse(arg)
+        case Package(pkg, body) =>
+          traverse(pkg)
+          body.map(traverse)
+        case Import(expr, selectors) =>
+          traverse(expr)
+          selectors.map(traverse)
+        case _ =>
+      }
+    }
+
+    def traverse(arg: statements.Definition): Unit = {
+      import statements._
+      arg match {
+        case ValDef(name, tpt, rhs, mods) =>
+          traverse(name)
+          traverse(tpt)
+          rhs.foreach(traverse)
+          mods.foreach(traverse)
+        case DefDef(name, typeParams, paramss, returnTpt, rhs, mods) =>
+          traverse(name)
+          typeParams.map(traverse)
+          paramss.map(_.map(traverse))
+          traverse(returnTpt)
+          rhs.map(traverse)
+          mods.map(traverse)
+        case TypeDef(name, rhs, mods) =>
+          traverse(name)
+          traverse(rhs)
+          mods.map(traverse)
+        case ClassDef(name, contraverseuctor, parents, self, body, mods) =>
+          traverse(name)
+          traverse(contraverseuctor)
+          parents.map(traverse)
+          self.map(traverse)
+          body.map(traverse)
+          mods.map(traverse)
+        case _ =>
+      }
+    }
+
+    def traverse(arg: terms.Term): Unit = {
+      import terms._
+      arg match {
+        case Ident(name) =>
+          traverse(name)
+        case Select(qual, name) =>
+          traverse(qual)
+          traverse(name)
+        case Literal(const) =>
+          traverse(const)
+        case New(tpt) =>
+          traverse(tpt)
+        case NamedArg(name, arg) =>
+          traverse(name)
+          traverse(arg)
+        case Apply(fn, args) =>
+          traverse(fn)
+          args.map(traverse)
+        case TypeApply(fn, args) =>
+          traverse(fn)
+          args.map(traverse)
+        case Super(qual, mixin) =>
+          traverse(qual)
+          mixin.map(traverse)
+        case Typed(expr, tpt) =>
+          traverse(expr)
+          traverse(tpt)
+        case Assign(lhs, rhs) =>
+          traverse(lhs)
+          traverse(rhs)
+        case Block(stats, expr) =>
+          stats.map(traverse)
+          traverse(expr)
+        case Lambda(meth, tpt) =>
+          traverse(meth)
+          tpt.map(traverse)
+        case If(cond, thenp, elsep) =>
+          traverse(cond)
+          traverse(thenp)
+          traverse(elsep)
+        case Match(selector, cases) =>
+          traverse(selector)
+          cases.map(traverse)
+        case Try(body, catches, finalizer) =>
+          traverse(body)
+          catches.map(traverse)
+          finalizer.map(traverse)
+        case Return(expr) =>
+          traverse(expr)
+        case Repeated(args) =>
+          args.map(traverse)
+        case _ =>
+      }
+    }
+
+    def traverse(arg: patterns.CaseDef): Unit = {
+      import patterns.CaseDef
+      arg match {
+        case CaseDef(pat, guard, body) =>
+          traverse(pat)
+          guard.foreach(traverse)
+          traverse(body)
+        case _ =>
+      }
+    }
+
+    def traverse(arg: patterns.Pattern): Unit = {
+      import patterns._
+      arg match {
+        case Value(v) =>
+          traverse(v)
+        case Bind(name, body) =>
+          traverse(name)
+          traverse(body)
+        case Unapply(fun, implicits, patterns) =>
+          traverse(fun)
+          implicits.map(traverse)
+          patterns.map(traverse)
+        case Alternative(patterns) =>
+          patterns.map(traverse)
+        case TypeTest(tpt) =>
+          traverse(tpt)
+        case Wildcard() =>
+        case _ =>
+      }
+    }
+
+    def traverse(arg: names.TermName): Unit = {
+      import names._
+      arg match {
+        case Simple(name) =>
+        case _ => ???
+      }
+    }
+
+    def traverse(arg: names.PossiblySignedName): Unit = {
+      arg match {
+        case _ => ???
+      }
+    }
+
+    def traverse(arg: typetrees.TypeTree): Unit = {
+      import typetrees._
+      arg match {
+        case Synthetic() =>
+        case Ident(name) => traverse(name)
+        case Select(qual, name) =>
+          traverse(qual)
+          traverse(name)
+        case Singleton(ref) => traverse(ref)
+        //      case Refined(tpt, refinements) =>
+        //        traverse(ref)
+        //        refinements.foreach(traverse)
+        case Applied(tycon, args) =>
+          traverse(tycon)
+          args.map(traverse)
+        case TypeBounds(lo, hi) =>
+          traverse(lo)
+          traverse(hi)
+        case Annotated(arg, annot) =>
+          traverse(arg)
+          traverse(annot)
+        case And(left, right) =>
+          traverse(left)
+          traverse(right)
+        case Or(left, right) =>
+          traverse(left)
+          traverse(right)
+        case ByName(tpt) =>
+          traverse(tpt)
+        case _ =>
+      }
+    }
+
+    def traverse(arg: names.TypeName): Unit = arg match {
+      case _ => arg.toString // TODO
+      case _ =>
+    }
+
+    def traverse(arg: scala.tasty.Id): Unit = ()
+
+    def traverse(arg: Modifier): Unit = ()
+
+    def traverse(arg: statements.Import.ImportSelector): Unit = {
+      import statements.Import.ImportSelector._
+      arg match {
+        case Simple(id) =>
+          traverse(id)
+        case Rename(id1, id2) =>
+          traverse(id1)
+          traverse(id2)
+        case Omit(id) =>
+          traverse(id)
+      }
+    }
+
+    def traverse(arg: Constant): Unit = ()
+  }
 }
