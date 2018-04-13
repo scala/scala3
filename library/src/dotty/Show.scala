df7c55901943f0e62e9ca9175e09c489a0d3d167
Nicolas Stucki <nicolas.stucki@gmail.com>
2018-04-13 11:29:42 +0200

    Workaround bug in String.valueOf(null) in some VMs

--- library/src/dotty/Show.scala
+++ library/src/dotty/Show.scala
@@ -6,7 +6,7 @@ trait Show[T] {
 
 trait LowPrioShow {
   implicit def defaultShow[T]: Show[T] = new Show[T] {
-    def show(x: T) = String.valueOf(x)
+    def show(x: T) = if (x == null) "null" else x.toString
   }
 }
 
