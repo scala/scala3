acbe8f18d2e2ef91de8669a264132158357a3ef0
Allan Renucci <allan.renucci@gmail.com>
2018-04-24 18:32:12 +0200

    Refine test case for unchecked warnings

--- tests/pos-special/isInstanceOf/3324h.scala
+++ tests/pos-special/isInstanceOf/3324h.scala
@@ -2,7 +2,11 @@ object Test {
   trait Marker
   def foo[T](x: T) = x match {
     case _: (T & Marker)       => // no warning
-    case _: T with String      => // scalac emits a warning
+    case _ =>
+  }
+
+  def foo2[T](x: T) = x match {
+    case _: T with Marker      => // scalac emits a warning
     case _ =>
   }
 }
