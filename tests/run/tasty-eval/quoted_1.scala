5b51f3ffd63132fbd5f3ac5802e3b98bb97d86ae
Nicolas Stucki <nicolas.stucki@gmail.com>
2018-05-08 16:10:11 +0200

    Add Valuable test case

---
+++ tests/run/tasty-eval/quoted_1.scala
@@ -0,0 +1,36 @@
+import scala.quoted._
+
+import scala.tasty.Universe
+import scala.tasty.util.{TastyPrinter, TreeTraverser}
+
+object Macros {
+
+  implicit inline def foo(i: Int): String =
+    ~impl('(i))(Universe.compilationUniverse) // FIXME infer Universe.compilationUniverse within top level ~
+
+  def impl(i: Expr[Int])(implicit u: Universe): Expr[String] = {
+    value(i).toString.toExpr
+  }
+
+  inline implicit def value[X](e: Expr[X])(implicit u: Universe, ev: Valuable[X]): Option[X] = ev.value(e)
+
+  trait Valuable[X] {
+    def value(e: Expr[X])(implicit u: Universe): Option[X]
+  }
+
+  implicit def intIsEvalable: Valuable[Int] = new Valuable[Int] {
+    override def value(e: Expr[Int])(implicit u: Universe): Option[Int] = {
+      import u._
+      import u.tasty._
+      e.toTasty.tpe match {
+        case SymRef(ValDef(_, tpt, _), pre) =>
+          tpt.tpe match {
+            case ConstantType(IntConstant(i)) => Some(i)
+            case _ => None
+          }
+        case ConstantType(IntConstant(i)) => Some(i)
+        case _ => None
+      }
+    }
+  }
+}
