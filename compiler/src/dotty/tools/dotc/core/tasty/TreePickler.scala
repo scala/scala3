a669acc585adb94d219c428b9bbec28d687febf6
Martin Odersky <odersky@gmail.com>
2018-05-29 12:31:30 +0200

    Pickle Enum flag
    It's useful to know something was an enum when it started, and it's necessary to pickle
    this when serializing untyped trees.

--- compiler/src/dotty/tools/dotc/core/tasty/TreePickler.scala
+++ compiler/src/dotty/tools/dotc/core/tasty/TreePickler.scala
@@ -601,6 +601,7 @@ class TreePickler(pickler: TastyPickler) {
     if (flags is Macro) writeByte(MACRO)
     if (flags is JavaStatic) writeByte(STATIC)
     if (flags is Module) writeByte(OBJECT)
+    if (flags is Enum) writeByte(ENUM)
     if (flags is Local) writeByte(LOCAL)
     if (flags is Synthetic) writeByte(SYNTHETIC)
     if (flags is Artifact) writeByte(ARTIFACT)
