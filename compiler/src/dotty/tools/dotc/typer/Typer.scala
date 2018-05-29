7814a446bc2743fb8dbf5ec549417142509d4d12
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2018-05-29 16:37:47 +0200

    Typer: Refine position when catching top-level TypeError
    Ensures stackoverflows aren't hidden by errors, without needing #4511.
    
    Also add relevant testcase from
    https://github.com/lampepfl/dotty/pull/4385#issuecomment-387847237; I confirmed
    this fails without the position fix.

--- compiler/src/dotty/tools/dotc/typer/Typer.scala
+++ compiler/src/dotty/tools/dotc/typer/Typer.scala
@@ -1862,7 +1862,13 @@ class Typer extends Namer
       assertPositioned(tree)
       try adapt(typedUnadapted(tree, pt, locked), pt, locked)
       catch {
-        case ex: TypeError => errorTree(tree, ex.toMessage)
+        case ex: TypeError =>
+          // This is equivalent to errorTree(tree, ex.toMessage) but
+          // uses tree.pos.focus instead of tree.pos. We use this because:
+          // - since tree can be a top-level definition, tree.pos can point to the whole definition
+          // - that would in turn hide all other type errors inside tree.
+          // TODO: might be even better to store positions inside TypeErrors.
+          tree withType errorType(ex.toMessage, tree.pos.focus)
       }
     }
 
