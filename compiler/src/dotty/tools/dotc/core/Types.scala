0281bc7ca6d3b789ca5bb04d72e514b6a8ef165a
Allan Renucci <allan.renucci@gmail.com>
2018-06-26 18:46:00 +0200

    Fix #4611:
    class and trait with a single abstract method which result type is an
    implicit function type are not considered SAM types anymore. The reason
    is because the abstract member desugar to mutiple ones. E.g.
    
    ```scala
    trait Foo {
      def foo(): implicit Int => Int
    }
    
    // becomes
    trait Foo {
      def foo(): implicit Int => Int
      def foo$direct(implicit x: Int): Int
    }
    ```

--- compiler/src/dotty/tools/dotc/core/Types.scala
+++ compiler/src/dotty/tools/dotc/core/Types.scala
@@ -3868,7 +3868,7 @@ object Types {
    *  A type is a SAM type if it is a reference to a class or trait, which
    *
    *   - has a single abstract method with a method type (ExprType
-   *     and PolyType not allowed!)
+   *     and PolyType not allowed!) whose result type is not an implicit function type
    *   - can be instantiated without arguments or with just () as argument.
    *
    *  The pattern `SAMType(sam)` matches a SAM type, where `sam` is the
@@ -3910,7 +3910,8 @@ object Types {
         // println(s"absMems: ${absMems map (_.show) mkString ", "}")
         if (absMems.size == 1)
           absMems.head.info match {
-            case mt: MethodType if !mt.isParamDependent =>
+            case mt: MethodType if !mt.isParamDependent &&
+                !defn.isImplicitFunctionType(mt.resultType) =>
               val cls = tp.classSymbol
 
               // Given a SAM type such as:
