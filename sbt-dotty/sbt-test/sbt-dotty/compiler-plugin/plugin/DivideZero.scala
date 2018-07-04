cf7b5890295c033a05cd91748e1e9b2b7b01b968
Fengyun Liu <liu@fengy.me>
2018-07-04 10:56:52 +0200

    Add documentation for compiler plugins (#4658)

--- sbt-dotty/sbt-test/sbt-dotty/compiler-plugin/plugin/DivideZero.scala
+++ sbt-dotty/sbt-test/sbt-dotty/compiler-plugin/plugin/DivideZero.scala
@@ -22,7 +22,7 @@ class DivideZero extends PluginPhase with StandardPlugin {
   override val runsAfter = Set(Pickler.name)
   override val runsBefore = Set(LinkAll.name)
 
-  override def init(options: List[String]): List[PluginPhase] = this :: Nil
+  def init(options: List[String]): List[PluginPhase] = this :: Nil
 
   private def isNumericDivide(sym: Symbol)(implicit ctx: Context): Boolean = {
     def test(tpe: String): Boolean =
