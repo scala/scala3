7c2a61c54bfb8accd4518915e0a574dd85feeb9a
Nicolas Stucki <nicolas.stucki@gmail.com>
2018-07-15 13:39:51 +0200

    Add back whitelisted list

--- compiler/test/dotty/tools/StdLibSources.scala
+++ compiler/test/dotty/tools/StdLibSources.scala
@@ -11,7 +11,17 @@ object StdLibSources {
 
   def blacklistFile: String = "compiler/test/dotc/scala-collections.blacklist"
 
+  def whitelisted: List[String] = all.diff(blacklisted)
   def blacklisted: List[String] = loadList(blacklistFile)
+
+  def all: List[String] = {
+    def collectAllFilesInDir(dir: File, acc: List[String]): List[String] = {
+      val files = dir.listFiles()
+      val acc2 = files.foldLeft(acc)((acc1, file) => if (file.isFile && file.getPath.endsWith(".scala")) file.getPath :: acc1 else acc1)
+      files.foldLeft(acc2)((acc3, file) => if (file.isDirectory) collectAllFilesInDir(file, acc3) else acc3)
+    }
+    collectAllFilesInDir(new File(stdLibPath), Nil)
+  }
 
   private def loadList(path: String): List[String] = Source.fromFile(path, "UTF8").getLines()
     .map(_.trim) // allow identation
