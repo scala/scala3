2c9e845a72a9bcb95063e83ba5d28882a2947233
Allan Renucci <allan.renucci@gmail.com>
2018-07-18 17:14:33 +0200

    Add test that exercises a cycle reference when playing with SeqType

---
+++ tests/pos/seqtype-cycle/Test2.scala
@@ -0,0 +1,8 @@
+package object scala {
+  // needed for some reasons
+  type Throwable = java.lang.Throwable
+  type IndexOutOfBoundsException = java.lang.IndexOutOfBoundsException
+
+  type Seq[+A] = scala.collection.Seq[A]
+  val Seq = scala.collection.Seq
+}
