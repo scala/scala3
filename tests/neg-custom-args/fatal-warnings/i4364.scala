5685a9993cf2b68f7e88f7bbf13000b2f8ca9999
Jaemin Hong <hjm0901@gmail.com>
2018-07-23 16:59:43 +0200

    fix test, change val name, & change warning point

--- tests/neg-custom-args/fatal-warnings/i4364.scala
+++ tests/neg-custom-args/fatal-warnings/i4364.scala
@@ -1,14 +1,9 @@
 object Test {
-  def foo(c: java.util.function.Consumer[String]) = c.accept("")
-
-  def f(x: String): Unit = ()
+  def foo(c: java.util.function.Consumer[Integer]) = c.accept(0)
   def f(x: Int): Unit = ()
 
   def main(args: Array[String]) = {
     foo(f) // Ok: Consumer is @FunctionalInterface
-
-    val oos = new java.io.ObjectOutputStream(f) // error: OutputStream is not @FunctionalInterface
-    oos.write(0)
-    oos.close()
+    new java.io.ObjectOutputStream(f) // error: OutputStream is not @FunctionalInterface
   }
 }
