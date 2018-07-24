4356c73d7ada843fb6f76ad162671e469a4f470e
Allan Renucci <allan.renucci@gmail.com>
2018-07-24 11:48:27 +0200

    Some refactoring

--- compiler/src/dotty/tools/dotc/transform/ExtensionMethods.scala
+++ compiler/src/dotty/tools/dotc/transform/ExtensionMethods.scala
@@ -9,7 +9,6 @@ import dotty.tools.dotc.transform.MegaPhase._
 import ValueClasses._
 import dotty.tools.dotc.ast.{Trees, tpd}
 import scala.collection.{ mutable, immutable }
-import mutable.ListBuffer
 import core._
 import dotty.tools.dotc.core.Phases.Phase
 import Types._, Contexts._, Constants._, Names._, NameOps._, Flags._, DenotTransformers._
@@ -158,13 +157,7 @@ class ExtensionMethods extends MiniPhase with DenotTransformer with FullParamete
       assert(staticClass.exists, s"$origClass lacks companion, ${origClass.owner.definedPeriodsString} ${origClass.owner.info.decls} ${origClass.owner.info.decls}")
       val extensionMeth = extensionMethod(origMeth)
       ctx.log(s"Value class $origClass spawns extension method.\n  Old: ${origMeth.showDcl}\n  New: ${extensionMeth.showDcl}")
-      val store: ListBuffer[Tree] = extensionDefs.get(staticClass) match {
-        case Some(x) => x
-        case None =>
-          val newC = new ListBuffer[Tree]()
-          extensionDefs(staticClass) = newC
-          newC
-      }
+      val store = extensionDefs.getOrElseUpdate(staticClass, new mutable.ListBuffer[Tree])
       store += fullyParameterizedDef(extensionMeth, tree)
       cpy.DefDef(tree)(rhs = forwarder(extensionMeth, tree))
     } else tree
