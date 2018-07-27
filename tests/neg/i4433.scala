tgodzik/dotty

Change top-level ~ evaluation scheme
* top-level ~ must contain a call to a static method
  * arguments must be quoted
  * inline arguments can be passed directly
* removed dependency on the original inlined code (removed in PostTyper)
* ReifyQuotes is no longer an InfoTransformer
* Splicer implements both checking and evaluation of splices in a
  single abstraction
* Fix #4773
* Fix #4735

tests/neg/i4433.scala
@@ -1,7 +1,7 @@
 
 object Foo {
-  transparent def g(transparent p: Int => Boolean): Boolean = ~{
-    if(p(5)) '(true) // error
+  transparent def g(inline p: Int => Boolean): Boolean = ~{ // error
+    if(p(5)) '(true)
     else '(false)
   }
 }

tests/neg/quote-macro-complex-arg-0.scala
@@ -1,0 +1,6 @@
+import scala.quoted._
+
+object Macros {
+  inline def foo(inline i: Int, dummy: Int, j: Int): Int = ~bar(i + 1, '(j)) // error: i + 1 is not a parameter or field reference
+  def bar(x: Int, y: Expr[Int]): Expr[Int] = '{ ~x.toExpr + ~y }
+}

tests/run/i4735/Macro_1.scala
@@ -1,0 +1,34 @@
+import scala.annotation.tailrec
+import scala.quoted._
+
+object Macro {
+
+  inline def unrolledForeach(inline unrollSize: Int, seq: Array[Int], f: => Int => Unit): Unit = // or f: Int => Unit
+    ~unrolledForeachImpl(unrollSize, '(seq), '(f))
+
+  private def unrolledForeachImpl(unrollSize: Int, seq: Expr[Array[Int]], f: Expr[Int => Unit]): Expr[Unit] = '{
+    val size = (~seq).length
+    assert(size % (~unrollSize.toExpr) == 0) // for simplicity of the implementation
+    var i = 0
+    while (i < size) {
+      println("<log> start loop")
+      ~{
+        for (j <- new UnrolledRange(0, unrollSize)) '{
+          val element = (~seq)(i + ~j.toExpr)
+          ~f('(element)) // or `(~f)(element)` if `f` should not be inlined
+        }
+      }
+      i += ~unrollSize.toExpr
+    }
+
+  }
+
+  private class UnrolledRange(start: Int, end: Int) {
+    def foreach(f: Int => Expr[Unit]): Expr[Unit] = {
+      @tailrec def loop(i: Int, acc: Expr[Unit]): Expr[Unit] =
+        if (i >= 0) loop(i - 1, '{ ~f(i); ~acc })
+      else acc
+      loop(end - 1, '())
+    }
+  }
+}

scala/scala3

tests/neg/i4433.scala
@@ -1,7 +1,7 @@
 
 object Foo {
-  transparent def g(transparent p: Int => Boolean): Boolean = ~{
-    if(p(5)) '(true) // error
+  transparent def g(transparent p: Int => Boolean): Boolean = ~{ // error
+    if(p(5)) '(true)
     else '(false)
   }
 }

tests/neg/quote-macro-complex-arg-0.scala
@@ -1,0 +1,6 @@
+import scala.quoted._
+
+object Macros {
+  transparent def foo(transparent i: Int, dummy: Int, j: Int): Int = ~bar(i + 1, '(j)) // error: i + 1 is not a parameter or field reference
+  def bar(x: Int, y: Expr[Int]): Expr[Int] = '{ ~x.toExpr + ~y }
+}

tests/run/i4735/Macro_1.scala
@@ -1,0 +1,34 @@
+import scala.annotation.tailrec
+import scala.quoted._
+
+object Macro {
+
+  transparent def unrolledForeach(transparent unrollSize: Int, seq: Array[Int], f: => Int => Unit): Unit = // or f: Int => Unit
+    ~unrolledForeachImpl(unrollSize, '(seq), '(f))
+
+  private def unrolledForeachImpl(unrollSize: Int, seq: Expr[Array[Int]], f: Expr[Int => Unit]): Expr[Unit] = '{
+    val size = (~seq).length
+    assert(size % (~unrollSize.toExpr) == 0) // for simplicity of the implementation
+    var i = 0
+    while (i < size) {
+      println("<log> start loop")
+      ~{
+        for (j <- new UnrolledRange(0, unrollSize)) '{
+          val element = (~seq)(i + ~j.toExpr)
+          ~f('(element)) // or `(~f)(element)` if `f` should not be inlined
+        }
+      }
+      i += ~unrollSize.toExpr
+    }
+
+  }
+
+  private class UnrolledRange(start: Int, end: Int) {
+    def foreach(f: Int => Expr[Unit]): Expr[Unit] = {
+      @tailrec def loop(i: Int, acc: Expr[Unit]): Expr[Unit] =
+        if (i >= 0) loop(i - 1, '{ ~f(i); ~acc })
+      else acc
+      loop(end - 1, '())
+    }
+  }
+}