0a24170c5160d20d24935ead7b1da00c5733ed29
Martin Odersky <odersky@gmail.com>
2018-08-14 16:11:41 +0200

    Fix TupleXXL equals
    Does not rely anymore on `deep`, which is dropped in Scala 2.14.

--- library/src/scala/TupleXXL.scala
+++ library/src/scala/TupleXXL.scala
@@ -1,10 +1,11 @@
 package scala
+import java.util.Arrays.deepEquals
 
 final class TupleXXL private (es: Array[Object]) {
   override def toString = elems.mkString("(", ",", ")")
   override def hashCode = getClass.hashCode * 41 + elems.deep.hashCode
   override def equals(that: Any) = that match {
-    case that: TupleXXL => this.elems.deep.equals(that.elems.deep)
+    case that: TupleXXL => deepEquals(this.elems, that.elems)
     case _ => false
   }
   def elems: Array[Object] = es
