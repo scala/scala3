bf358a423f3728f5f94868f24be0f965aed8fb83
Martin Odersky <odersky@gmail.com>
2018-08-14 17:56:12 +0200

    Erase *: to tuple classes
    Was erased to Object before, but this loses precision and breaks
    binary compatibility with Scala 2.

--- library/src/scala/TupleXXL.scala
+++ library/src/scala/TupleXXL.scala
@@ -1,9 +1,9 @@
 package scala
-import java.util.Arrays.deepEquals
+import java.util.Arrays.{deepEquals, deepHashCode}
 
 final class TupleXXL private (es: Array[Object]) {
   override def toString = elems.mkString("(", ",", ")")
-  override def hashCode = getClass.hashCode * 41 + elems.deep.hashCode
+  override def hashCode = getClass.hashCode * 41 + deepHashCode(elems)
   override def equals(that: Any) = that match {
     case that: TupleXXL => deepEquals(this.elems, that.elems)
     case _ => false
