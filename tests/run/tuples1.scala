5f49c78282be155da72b9ddca1fceed84396f47b
Martin Odersky <odersky@gmail.com>
2018-08-16 17:24:35 +0200

    Test pattern matching of tuples >= 23.

--- tests/run/tuples1.scala
+++ tests/run/tuples1.scala
@@ -41,6 +41,10 @@ object Test extends App {
               Int, Int, Int, Int, Int,
               Int, Int, Int)
   val x23c: T23 = x23
+  x23 match {
+    case (x1, x2, x3, x4, x5, x6, x7, x8, x9, x10, x11, x12, x13, x14, x15, x16, x17, x18, x19, x20, x21, x22, x23) =>
+      println(x1 + x2 + x3 + x4 + x5 + x6 + x7 + x8 + x9 + x10 + x11 + x12 + x13 + x14 + x15 + x16 + x17 + x18 + x19 + x20 + x21 + x22 + x23)
+  }
   println(x23)
   assert(x23(0) == 1)
   assert(x23(22) == 23)
