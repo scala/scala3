c5399da391cad5b1f646663c2c75380113a96795
Nicolas Stucki <nicolas.stucki@gmail.com>
2018-08-22 14:07:46 +0200

    Reformat to avoid long line

--- compiler/src/dotty/tools/dotc/tastyreflect/TastyImpl.scala
+++ compiler/src/dotty/tools/dotc/tastyreflect/TastyImpl.scala
@@ -2,4 +2,21 @@ package dotty.tools.dotc.tastyreflect
 
 import dotty.tools.dotc.core._
 
-class TastyImpl(val rootContext: Contexts.Context) extends scala.tasty.Tasty with TastyCoreImpl with CaseDefOpsImpl with ConstantOpsImpl with ContextOpsImpl with IdOpsImpl with ImportSelectorOpsImpl with QuotedOpsImpl with PatternOpsImpl with PositionOpsImpl with PrintersImpl with SignatureOpsImpl with StandardDefinitions with SymbolOpsImpl with TreeOpsImpl with TypeOrBoundsTreesOpsImpl with TypeOrBoundsOpsImpl
+class TastyImpl(val rootContext: Contexts.Context)
+    extends scala.tasty.Tasty
+    with TastyCoreImpl
+    with CaseDefOpsImpl
+    with ConstantOpsImpl
+    with ContextOpsImpl
+    with IdOpsImpl
+    with ImportSelectorOpsImpl
+    with QuotedOpsImpl
+    with PatternOpsImpl
+    with PositionOpsImpl
+    with PrintersImpl
+    with SignatureOpsImpl
+    with StandardDefinitions
+    with SymbolOpsImpl
+    with TreeOpsImpl
+    with TypeOrBoundsTreesOpsImpl
+    with TypeOrBoundsOpsImpl
--- library/src/scala/tasty/Tasty.scala
+++ library/src/scala/tasty/Tasty.scala
@@ -2,4 +2,20 @@ package scala.tasty
 
 import scala.tasty.reflect._
 
-abstract class Tasty extends TastyCore with CaseDefOps with ConstantOps with ContextOps with IdOps with ImportSelectorOps with QuotedOps with PatternOps with PositionOps with Printers with SignatureOps with StandardDefinitions with SymbolOps with TreeOps with TypeOrBoundsTreeOps with TypeOrBoundsOps
+abstract class Tasty
+    extends TastyCore
+    with CaseDefOps
+    with ConstantOps
+    with ContextOps
+    with IdOps
+    with ImportSelectorOps
+    with QuotedOps
+    with PatternOps
+    with PositionOps
+    with Printers
+    with SignatureOps
+    with StandardDefinitions
+    with SymbolOps
+    with TreeOps
+    with TypeOrBoundsTreeOps
+    with TypeOrBoundsOps
