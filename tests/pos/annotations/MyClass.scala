65f6d66bee1423c33b94217681368938212ac69d
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2018-08-26 14:52:19 +0200

    Requested tests for #2973 fix
    See https://github.com/lampepfl/dotty/pull/4932#discussion_r209450910.

--- tests/pos/annotations/MyClass.scala
+++ tests/pos/annotations/MyClass.scala
@@ -2,3 +2,10 @@ class MyClass {
   @MyAnnotation(Name="Blah", foBaskaLi = Array(1,2,3))
   def method = ???
 }
+
+class MyClass2 extends AnyRef @MyAnnotation(Name="Foo", foBaskaLi = Array(1,2,3))
+
+class MyClass3 extends AnyRef @MyAnnotation(Name="Foo", foBaskaLi = Array(1,2,3)) {
+  @MyAnnotation(Name="Blah", foBaskaLi = Array(1,2,3))
+  def method = ???
+}
