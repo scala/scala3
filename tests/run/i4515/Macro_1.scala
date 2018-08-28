ef22195761a3f635a33e4d366650d415eb97bb52
Nicolas Stucki <nicolas.stucki@gmail.com>
2018-08-28 09:09:17 +0200

    More fixes after rebase

--- tests/run/i4515/Macro_1.scala
+++ tests/run/i4515/Macro_1.scala
@@ -1,5 +1,5 @@
 
 object Macro {
-  transparent def foo[X](x: X): Unit = ~fooImpl('(x))
+  rewrite def foo[X](x: X): Unit = ~fooImpl('(x))
   def fooImpl[X: quoted.Type](x: quoted.Expr[X]): quoted.Expr[Unit] = '()
 }
