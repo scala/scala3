8ffaea3d7442cf132c771ca3798e3550fafe8139
Abel Nieto <abel.nieto90@gmail.com>
2018-08-30 18:04:05 -0400

    Moar easy test fixes

--- tests/pos-java-interop/1576/Test.scala
+++ tests/pos-java-interop/1576/Test.scala
@@ -1,5 +1,5 @@
 object Test {
-  val v: TagAnnotation = null
+  val v: TagAnnotation = ???
   println(v.value) // error: value value in class TagAnnotation cannot be accessed as a
                    // member of TagAnnotation(Test.v) from module class Test$.
 }
