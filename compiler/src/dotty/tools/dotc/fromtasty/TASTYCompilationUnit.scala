54cc1756114d3abad6592a1e9f6e9727c37ef099
Martin Duhem <martin.duhem@gmail.com>
2018-09-05 10:02:22 +0200

    Generate documentation from TASTY in sbt plugin

--- compiler/src/dotty/tools/dotc/fromtasty/TASTYCompilationUnit.scala
+++ compiler/src/dotty/tools/dotc/fromtasty/TASTYCompilationUnit.scala
@@ -1,8 +1,20 @@
 package dotty.tools.dotc.fromtasty
 
 import dotty.tools.dotc.CompilationUnit
+import dotty.tools.dotc.core.Contexts.Context
 import dotty.tools.dotc.util.NoSource
+import dotty.tools.io.Path
 
 class TASTYCompilationUnit(val className: String) extends CompilationUnit(NoSource) {
   override def toString: String = s"class file $className"
+}
+object TASTYCompilationUnit {
+  def apply(className: String)(implicit ctx: Context): Option[TASTYCompilationUnit] = {
+    if (Path(className).exists) {
+      ctx.inform(s"Ignoring $className: cannot create a `TASTYCompilationUnit` for a source file.")
+      None
+    } else {
+      Some(new TASTYCompilationUnit(className))
+    }
+  }
 }
