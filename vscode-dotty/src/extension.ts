4789db007821c187aec60085b2d92eb263f5fc42
Martin Duhem <martin.duhem@gmail.com>
2018-09-21 10:35:36 +0200

    Run `configureIDE` with sbt server

--- vscode-dotty/src/extension.ts
+++ vscode-dotty/src/extension.ts
@@ -6,6 +6,8 @@ import * as path from 'path';
 import * as cpp from 'child-process-promise';
 import * as compareVersions from 'compare-versions';
 
+import { ChildProcess } from "child_process";
+
 import { ExtensionContext } from 'vscode';
 import * as vscode from 'vscode';
 import { LanguageClient, LanguageClientOptions, RevealOutputChannelOn,
@@ -14,9 +16,15 @@ import { enableOldServerWorkaround } from './compat'
 
 import * as worksheet from './worksheet'
 
+import * as rpc from 'vscode-jsonrpc'
+import * as sbtserver from './sbt-server'
+
 let extensionContext: ExtensionContext
 let outputChannel: vscode.OutputChannel
 export let client: LanguageClient
+
+/** The sbt process that may have been started by this extension */
+let sbtProcess: ChildProcess
 
 const sbtVersion = "1.2.3"
 const sbtArtifact = `org.scala-sbt:sbt-launch:${sbtVersion}`
@@ -71,25 +79,77 @@ export function activate(context: ExtensionContext) {
     })
 
   } else {
-    // Check whether `.dotty-ide-artifact` exists. If it does, start the language server,
-    // otherwise, try propose to start it if there's no build.sbt
-    if (fs.existsSync(languageServerArtifactFile)) {
-      runLanguageServer(coursierPath, languageServerArtifactFile)
-    } else if (isUnconfiguredProject()) {
-      vscode.window.showInformationMessage(
-          "This looks like an unconfigured Scala project. Would you like to start the Dotty IDE?",
-          "Yes", "No"
+    let configuredProject: Thenable<void> = Promise.resolve()
+    if (isUnconfiguredProject()) {
+      configuredProject = vscode.window.showInformationMessage(
+        "This looks like an unconfigured Scala project. Would you like to start the Dotty IDE?",
+        "Yes", "No"
       ).then(choice => {
-        if (choice == "Yes") {
-          fetchAndConfigure(coursierPath, sbtArtifact, buildSbtFileSource, dottyPluginSbtFileSource).then(() => {
-            runLanguageServer(coursierPath, languageServerArtifactFile)
-          })
-        } else {
+        if (choice === "Yes") {
+          bootstrapSbtProject(buildSbtFileSource, dottyPluginSbtFileSource)
+          return Promise.resolve()
+        } else if (choice === "No") {
           fs.appendFile(disableDottyIDEFile, "", _ => {})
+          return Promise.reject()
         }
       })
     }
+
+    configuredProject
+      .then(_ => withProgress("Configuring Dotty IDE...", configureIDE(coursierPath)))
+      .then(_ => runLanguageServer(coursierPath, languageServerArtifactFile))
   }
+}
+
+export function deactivate() {
+  // If sbt was started by this extension, kill the process.
+  // FIXME: This will be a problem for other clients of this server.
+  if (sbtProcess) {
+    sbtProcess.kill()
+  }
+}
+
+/**
+ * Display a progress bar with title `title` while `op` completes.
+ *
+ * @param title The title of the progress bar
+ * @param op The thenable that is monitored by the progress bar.
+ */
+function withProgress<T>(title: string, op: Thenable<T>): Thenable<T> {
+  return vscode.window.withProgress({
+    location: vscode.ProgressLocation.Window,
+    title: title
+  }, _ => op)
+}
+
+/** Connect to an sbt server and run `configureIDE`. */
+function configureIDE(coursierPath: string): Thenable<sbtserver.ExecResult> {
+
+  function offeringToRetry(client: rpc.MessageConnection, command: string): Thenable<sbtserver.ExecResult> {
+    return sbtserver.tellSbt(outputChannel, client, command)
+      .then(success => Promise.resolve(success),
+        _ => {
+          outputChannel.show()
+          return vscode.window.showErrorMessage("IDE configuration failed (see logs for details)", "Retry?")
+            .then(retry => {
+              if (retry) return offeringToRetry(client, command)
+              else return Promise.reject()
+            })
+        })
+  }
+
+  return withSbtInstance(outputChannel, coursierPath)
+    .then(client => {
+      // `configureIDE` is a command, which means that upon failure, sbt won't tell us anything
+      // until sbt/sbt#4370 is fixed.
+      // We run `compile` and `test:compile` first because they're tasks (so we get feedback from sbt
+      // in case of failure), and we're pretty sure configureIDE will pass if they passed.
+      return offeringToRetry(client, "compile").then(_ => {
+        return offeringToRetry(client, "test:compile").then(_ => {
+          return offeringToRetry(client, "configureIDE")
+        })
+      })
+    })
 }
 
 function runLanguageServer(coursierPath: string, languageServerArtifactFile: string) {
@@ -109,10 +169,34 @@ function runLanguageServer(coursierPath: string, languageServerArtifactFile: str
   })
 }
 
-function fetchAndConfigure(coursierPath: string, sbtArtifact: string, buildSbtFileSource: string, dottyPluginSbtFileSource: string) {
-    return fetchWithCoursier(coursierPath, sbtArtifact).then((sbtClasspath) => {
-        return configureIDE(sbtClasspath, buildSbtFileSource, dottyPluginSbtFileSource)
+/**
+ * Connects to an existing sbt server, or boots up one instance and connects to it.
+ */
+function withSbtInstance(log: vscode.OutputChannel, coursierPath: string): Thenable<rpc.MessageConnection> {
+  const serverSocketInfo = path.join(workspaceRoot, "project", "target", "active.json")
+
+  if (!fs.existsSync(serverSocketInfo)) {
+    fetchWithCoursier(coursierPath, sbtArtifact).then((sbtClasspath) => {
+      sbtProcess = cpp.spawn("java", [
+        "-Dsbt.log.noformat=true",
+        "-classpath", sbtClasspath,
+        "xsbt.boot.Boot"
+      ]).childProcess
+
+      // Close stdin, otherwise in case of error sbt will block waiting for the
+      // user input to reload or exit the build.
+      sbtProcess.stdin.end()
+
+      sbtProcess.stdout.on('data', data => {
+        log.appendLine(data.toString())
+      })
+      sbtProcess.stderr.on('data', data => {
+        log.appendLine(data.toString())
+      })
     })
+  }
+
+  return sbtserver.connectToSbtServer(log)
 }
 
 function fetchWithCoursier(coursierPath: string, artifact: string, extra: string[] = []) {
@@ -150,54 +234,12 @@ function fetchWithCoursier(coursierPath: string, artifact: string, extra: string
     })
 }
 
-function configureIDE(sbtClasspath: string,
-                      buildSbtFileSource: string,
-                      dottyPluginSbtFileSource: string) {
-
-  return vscode.window.withProgress({
-    location: vscode.ProgressLocation.Window,
-    title: 'Configuring the IDE for Dotty...'
-  }, _ => {
-
-    // Bootstrap an sbt build
+function bootstrapSbtProject(buildSbtFileSource: string,
+                             dottyPluginSbtFileSource: string) {
     fs.mkdirSync(sbtProjectDir)
     fs.appendFileSync(sbtBuildPropertiesFile, `sbt.version=${sbtVersion}`)
     fs.copyFileSync(buildSbtFileSource, sbtBuildSbtFile)
     fs.copyFileSync(dottyPluginSbtFileSource, path.join(sbtProjectDir, "plugins.sbt"))
-
-    // Run sbt to configure the IDE.
-    const sbtPromise =
-      cpp.spawn("java", [
-        "-Dsbt.log.noformat=true",
-        "-classpath", sbtClasspath,
-        "xsbt.boot.Boot",
-        "configureIDE"
-      ])
-
-    const sbtProc = sbtPromise.childProcess
-    // Close stdin, otherwise in case of error sbt will block waiting for the
-    // user input to reload or exit the build.
-    sbtProc.stdin.end()
-
-    sbtProc.stdout.on('data', (data: Buffer) => {
-      let msg = data.toString().trim()
-      outputChannel.appendLine(msg)
-    })
-    sbtProc.stderr.on('data', (data: Buffer) => {
-      let msg = data.toString().trim()
-      outputChannel.appendLine(msg)
-    })
-
-    sbtProc.on('close', (code: number) => {
-      if (code != 0) {
-        const msg = "Configuring the IDE failed."
-        outputChannel.appendLine(msg)
-        throw new Error(msg)
-      }
-    })
-
-    return sbtPromise
-  })
 }
 
 function run(serverOptions: ServerOptions, isOldServer: boolean) {
