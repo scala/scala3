8ce1fccee489e758c958fe122b4abe9a7f089f24
Nicolas Stucki <nicolas.stucki@gmail.com>
2018-09-24 21:16:45 -0500

    Fix #5152: Use context of the target location

---
+++ tests/run-with-compiler/i5152.scala
@@ -0,0 +1,17 @@
+import scala.quoted._
+
+object Test {
+  implicit val toolbox: scala.quoted.Toolbox = scala.quoted.Toolbox.make
+
+  def eval1(ff: Expr[Int => Int]): Expr[Int => Int] = '(identity)
+
+  def peval1(): Expr[Unit] = '{
+    lazy val f: Int => Int = ~eval1('((y: Int) => f(y)))
+  }
+
+  def main(args: Array[String]): Unit = {
+    val p = peval1()
+    println(p.show)
+  }
+
+}
