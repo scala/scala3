14621a2903dad4efc248faa8b919424b82650762
Guillaume Martres <smarter@ubuntu.com>
2018-09-25 17:00:17 -0500

    Fix incorrect cast under Java 9+

--- compiler/src/dotty/tools/dotc/interactive/InteractiveDriver.scala
+++ compiler/src/dotty/tools/dotc/interactive/InteractiveDriver.scala
@@ -119,10 +119,11 @@ class InteractiveDriver(val settings: List[String]) extends Driver {
 
   private val (zipClassPaths, dirClassPaths) = currentCtx.platform.classPath(currentCtx) match {
     case AggregateClassPath(cps) =>
-      val (zipCps, dirCps) = cps.partition(_.isInstanceOf[ZipArchiveFileLookup[_]])
-      // This will be wrong if any other subclass of ClassPath is either used,
-      // like `JrtClassPath` once we get Java 9 support
-      (zipCps.asInstanceOf[Seq[ZipArchiveFileLookup[_]]], dirCps.asInstanceOf[Seq[JFileDirectoryLookup[_]]])
+      // FIXME: We shouldn't ignore other subclasses of ClassPath, like
+      // JrtClassPath on Java 9+.
+      val zipCps = cps.collect { case cp: ZipArchiveFileLookup[_] => cp }
+      val dirCps = cps.collect { case cp: JFileDirectoryLookup[_] => cp }
+      (zipCps, dirCps)
     case _ =>
       (Seq(), Seq())
   }
