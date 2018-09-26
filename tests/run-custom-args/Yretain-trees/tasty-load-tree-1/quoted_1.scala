AndreasKostler/dotty

TASTy reflect load complete definition trees
Currently only works with -Yretain-tree.

Also fix an issue with TASTy reflect

tests/run-custom-args/Yretain-trees/tasty-load-tree-1/quoted_1.scala
@@ -1,0 +1,26 @@
+import scala.quoted._
+
+import scala.tasty._
+
+object Foo {
+
+  transparent def inspectBody(i: => Int): String =
+    ~inspectBodyImpl('(i))(TopLevelSplice.tastyContext) // FIXME infer TopLevelSplice.tastyContext within top level ~
+
+  def inspectBodyImpl(x: Expr[Int])(implicit tasty: Tasty): Expr[String] = {
+    import tasty._
+
+    def definitionString(tree: Tree): Expr[String] = tree.symbol.tree match {
+      case Some(definition) => definition.show.toExpr
+      case None => '("NO DEFINTION")
+    }
+
+    x.toTasty match {
+      case Term.Inlined(None, Nil, arg) => definitionString(arg)
+      case arg => definitionString(arg) // TODO should all by name parameters be in an inline node
+    }
+  }
+
+  def foo: Int = 1 + 2
+  val bar: Int = 2 + 3
+}

scala/scala3

tests/run-custom-args/Yretain-trees/tasty-load-tree-1/quoted_1.scala
@@ -1,0 +1,26 @@
+import scala.quoted._
+
+import scala.tasty._
+
+object Foo {
+
+  inline def inspectBody(i: => Int): String =
+    ~inspectBodyImpl('(i))
+
+  def inspectBodyImpl(x: Expr[Int])(implicit tasty: Tasty): Expr[String] = {
+    import tasty._
+
+    def definitionString(tree: Tree): Expr[String] = tree.symbol.tree match {
+      case Some(definition) => definition.show.toExpr
+      case None => '("NO DEFINTION")
+    }
+
+    x.toTasty match {
+      case Term.Inlined(None, Nil, arg) => definitionString(arg)
+      case arg => definitionString(arg) // TODO should all by name parameters be in an inline node
+    }
+  }
+
+  def foo: Int = 1 + 2
+  val bar: Int = 2 + 3
+}