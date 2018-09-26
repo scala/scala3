f11f18497880d1ccecd8ae0abfa5d416712c76ca
Martin Duhem <martin.duhem@gmail.com>
2018-09-26 16:03:22 +0200

    Add `needsTerminal` param to `JLineTerminal`
    This parameter configures the JLine terminal so that it throws an
    exception if `needsTerminal` is set but no terminal is connected.
    
    The goal is to reuse that interface from the worksheet, where no
    terminal is attached.

--- compiler/src/dotty/tools/repl/ReplDriver.scala
+++ compiler/src/dotty/tools/repl/ReplDriver.scala
@@ -102,8 +102,8 @@ class ReplDriver(settings: Array[String],
    *  observable outside of the CLI, for this reason, most helper methods are
    *  `protected final` to facilitate testing.
    */
-  final def runUntilQuit(initialState: State = initialState): State = {
-    val terminal = new JLineTerminal()
+  final def runUntilQuit(needsTerminal: Boolean, initialState: State = initialState): State = {
+    val terminal = new JLineTerminal(needsTerminal)
 
     /** Blockingly read a line, getting back a parse result */
     def readLine(state: State): ParseResult = {
