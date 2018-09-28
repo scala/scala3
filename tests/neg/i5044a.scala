91eaf201d97e5e774cea98bb43918e143fd022e3
Jasper Moeys <jaspermoeys@hotmail.com>
2018-09-28 16:45:03 +0200

    address review

--- tests/neg/i5044a.scala
+++
@@ -1,7 +0,0 @@
-class I0 {
-  class I1
-  def i3 = {
-    val i4 = new i5.I1 // error: `i5` is a forward reference extending over the definition of `i4`
-    val i5 = new I0
-  }
-}
--- tests/neg/i5044b.scala
+++
@@ -1,8 +0,0 @@
-class I0 {
-  class I1
-  def i3 = {
-    type T = i5.I1
-    val i4 = new T // error: `i5` is a forward reference extending over the definition of `i4`
-    val i5 = new I0
-  }
-}
