8f686f75c4b9fc54ddc289e3d12a30a801944ec8
Nicolas Stucki <nicolas.stucki@gmail.com>
2018-09-28 22:09:37 -0500

    Allow None and Some to be inline parameters

---
+++ tests/run/inline-option/Macro_1.scala
@@ -0,0 +1,13 @@
+
+import scala.quoted._
+
+object Macros {
+
+  def impl(opt: Option[Int]): Expr[Int] = opt match {
+    case Some(i) => i.toExpr
+    case None => '(-1)
+  }
+
+  def impl2(opt: Option[Option[Int]]): Expr[Int] = impl(opt.flatten)
+
+}
