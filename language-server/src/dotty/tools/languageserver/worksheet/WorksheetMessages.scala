0689ef31702e613fae809203561644f19069574c
Martin Duhem <martin.duhem@gmail.com>
2018-10-03 09:13:48 +0200

    Add empty secondary ctors to worksheet messages
    They are used when deserializing from JSON.

--- language-server/src/dotty/tools/languageserver/worksheet/WorksheetMessages.scala
+++ language-server/src/dotty/tools/languageserver/worksheet/WorksheetMessages.scala
@@ -3,13 +3,25 @@ package dotty.tools.languageserver.worksheet
 import java.net.URI
 
 /** The parameter for the `worksheet/exec` request. */
-case class WorksheetExecParams(uri: URI)
+case class WorksheetExecParams(uri: URI) {
+  // Used for deserialization
+  // see https://github.com/lampepfl/dotty/pull/5102#discussion_r222055355
+  def this() = this(null)
+}
 
 /** The response to a `worksheet/exec` request. */
-case class WorksheetExecResponse(success: Boolean)
+case class WorksheetExecResponse(success: Boolean) {
+  // Used for deserialization
+  // see https://github.com/lampepfl/dotty/pull/5102#discussion_r222055355
+  def this() = this(false)
+}
 
 /**
  * A notification that tells the client that a line of a worksheet
  * produced the specified output.
  */
-case class WorksheetExecOutput(uri: URI, line: Int, content: String)
+case class WorksheetExecOutput(uri: URI, line: Int, content: String) {
+  // Used for deserialization
+  // see https://github.com/lampepfl/dotty/pull/5102#discussion_r222055355
+  def this() = this(null, 0, null)
+}
