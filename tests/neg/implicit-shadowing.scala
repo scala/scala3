cf1f290d9baa2c0425608142bd5bbc6ea1b22d71
Martin Odersky <odersky@gmail.com>
2018-10-10 11:24:10 +0200

    Fix #5224: Make implicit shadowing conform to the rules in the spec
    The previous version of implicit shadowing considered a nested reference
    as a shadow _only_ if it matched the prototype of the implicit search.
    This is not backed up by the spec, and is different to what scalac does.
    Arguably, the spec is right here: We should consider an implicit ineligible
    if it is not visible as a simple name because a nested reference has the
    same name, no matter what the type of the nested reference is.

--- tests/neg/implicit-shadowing.scala
+++ tests/neg/implicit-shadowing.scala
@@ -5,8 +5,25 @@ object Test {
   def outer(implicit c: C) = {
 
     def f(c: C) = implicitly[C]   // error: shadowing
-    def g(c: Int) = implicitly[C] // ok since type is different
+    def g(c: Int) = implicitly[C] // error: shadowing (even though type is different)
 
     f(new C)
+  }
+
+  class C1[X]
+  class C2[X]
+
+  def f[T: C1] = {
+    def g[U: C2] = {
+      implicitly[C1[T]]    // OK: no shadowing for evidence parameters
+      implicitly[C2[U]]
+    }
+  }
+
+  def h[T]: implicit C1[T] => Unit = {
+    def g[U]: implicit C2[U] => Unit = {
+      implicitly[C1[T]]    // OK: no shadowing for evidence parameters
+      implicitly[C2[U]]
+    }
   }
 }
