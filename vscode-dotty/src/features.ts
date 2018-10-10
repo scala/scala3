0f465e52d5819628ceada3891d4e59780358e162
Guillaume Martres <smarter@ubuntu.com>
2018-10-10 14:59:25 +0200

    Refactor worksheet support, make it a dynamic feature
    - Only enable worksheet features on the client if the server supports
    them using the LSP support for dynamic feature registration.
    - Replace the global functions and state in worksheet.ts by a
    WorksheetProvider class, move some of the global functions specific to
    one worksheet into Worksheet

---
+++ vscode-dotty/src/features.ts
@@ -0,0 +1,62 @@
+import * as vscode from 'vscode'
+import {
+  BaseLanguageClient, ClientCapabilities, DynamicFeature, ServerCapabilities,
+  TextDocumentFeature, TextDocumentRegistrationOptions,
+} from 'vscode-languageclient'
+import { generateUuid } from 'vscode-languageclient/lib/utils/uuid'
+import { DocumentSelector } from 'vscode-languageserver-protocol'
+import { Disposable } from 'vscode-jsonrpc'
+
+import { WorksheetExecRequest } from './protocol'
+import { WorksheetProvider } from './worksheet'
+
+// Remove this if
+// https://github.com/Microsoft/vscode-languageserver-node/issues/423 is fixed.
+function ensure<T, K extends keyof T>(target: T, key: K): T[K] {
+  if (target[key] === void 0) {
+    target[key] = {} as any;
+  }
+  return target[key];
+}
+
+export interface WorksheetClientCapabilities {
+  worksheet?: {
+    exec?: {
+      dynamicRegistration?: boolean
+    }
+  }
+}
+
+export interface WorksheetServerCapabilities {
+  /**
+   * The server provides support for running worksheets.
+   */
+  worksheetRunProvider?: boolean
+}
+
+export class WorksheetExecFeature extends TextDocumentFeature<TextDocumentRegistrationOptions> {
+  constructor(client: BaseLanguageClient) {
+    super(client, WorksheetExecRequest.type)
+  }
+
+  public fillClientCapabilities(capabilities: ClientCapabilities & WorksheetClientCapabilities): void {
+    ensure(ensure(capabilities, "worksheet")!, "exec")!.dynamicRegistration = true
+  }
+
+  public initialize(capabilities: ServerCapabilities & WorksheetServerCapabilities, documentSelector: DocumentSelector): void {
+    if (!capabilities.worksheetRunProvider) {
+      return
+    }
+
+    const selector: DocumentSelector = documentSelector || [ { language: 'scala', pattern: '**/*.sc' } ]
+    this.register(this.messages, {
+      id: generateUuid(),
+      registerOptions: { documentSelector: selector }
+    })
+  }
+
+  protected registerLanguageProvider(options: TextDocumentRegistrationOptions): Disposable {
+    let client = this._client
+    return new WorksheetProvider(client, options.documentSelector!)
+  }
+}
