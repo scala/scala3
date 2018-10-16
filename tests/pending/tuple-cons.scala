095fbfe68188b139b2cb983f89e96364bd10030f
Olivier Blanvillain <olivier.blanvillain@gmail.com>
2018-10-16 18:20:06 +0200

    Add tuple >22 test cases from #2199
    4 of these tests are failing, I put them in pending for now.
    Interestingly each of them fails in different ways:
    
    Compiler crash
    tests/pending/tuple-patmat-extract.scala
    
    Compilation error
    tests/pending/tuple-for-comprehension.scala
    
    Runtime error
    tests/pending/tuple-erased.scala
    
    Doesn't pass Ycheck
    tests/pending/tuple-cons.scala

---
+++ tests/pending/tuple-cons.scala
@@ -0,0 +1,182 @@
+import dotty._
+
+object Test {
+  def main(args: Array[String]) = {
+    val t1: *:[Int, *:[Int, Unit]] = (1, 2)
+
+    val t2: *:[Int, *:[Int, *:[Int, Unit]]] = 0 *: t1
+
+    assert(t2.head == 0)
+    assert(t2.tail.head == 1)
+    assert(t2.tail.tail.head == 2)
+
+    t2 match {
+      case e1 *: e2 *: e3 *: () =>
+        assert(e1 == 0)
+        assert(e2 == 1)
+        assert(e3 == 2)
+    }
+
+    val t3: *:[Int, *:[Int, *:[Int, *:[Int, Unit]]]] = -1 *: t2
+
+    assert(t3.head == -1)
+    assert(t3.tail.head == 0)
+    assert(t3.tail.tail.head == 1)
+    assert(t3.tail.tail.tail.head == 2)
+
+    t3 match {
+      case e1 *: e2 *: e3 *: e4 *: () =>
+        assert(e1 == -1)
+        assert(e2 == 0)
+        assert(e3 == 1)
+        assert(e4 == 2)
+    }
+
+    type I = Int
+
+    //       1   2   3   4   5   6   7   8   9   10  11  12  13  14  15  16  17  18  19  20
+    val t20: *:[I,*:[I,*:[I,*:[I,*:[I,*:[I,*:[I,*:[I,*:[I,*:[I,*:[I,*:[I,*:[I,*:[I,*:[I,*:[I,*:[I,*:[I,*:[I,*:[I,Unit]]]]]]]]]]]]]]]]]]]] = 1 *: 2 *: 3 *: 4 *: 5 *: 6 *: 7 *: 8 *: 9 *: 10 *: 11 *: 12 *: 13 *: 14 *: 15 *: 16 *: 17 *: 18 *: 19 *: 20 *: ()
+
+    t20 match {
+      case e1 *: e2 *: e3 *: e4 *: e5 *: e6 *: e7 *: e8 *: e9 *: e10 *: e11 *: e12 *: e13 *: e14 *: e15 *: e16 *: e17 *: e18 *: e19 *: e20 *: () =>
+      assert(e1 == 1)
+      assert(e2 == 2)
+      assert(e3 == 3)
+      assert(e4 == 4)
+      assert(e5 == 5)
+      assert(e6 == 6)
+      assert(e7 == 7)
+      assert(e8 == 8)
+      assert(e9 == 9)
+      assert(e10 == 10)
+      assert(e11 == 11)
+      assert(e12 == 12)
+      assert(e13 == 13)
+      assert(e14 == 14)
+      assert(e15 == 15)
+      assert(e16 == 16)
+      assert(e17 == 17)
+      assert(e18 == 18)
+      assert(e19 == 19)
+      assert(e20 == 20)
+    }
+
+    //       1   2   3   4   5   6   7   8   9   10  11  12  13  14  15  16  17  18  19  20  21
+    val t21: *:[I,*:[I,*:[I,*:[I,*:[I,*:[I,*:[I,*:[I,*:[I,*:[I,*:[I,*:[I,*:[I,*:[I,*:[I,*:[I,*:[I,*:[I,*:[I,*:[I,*:[I,Unit]]]]]]]]]]]]]]]]]]]]] = 21 *: t20
+
+    t21 match {
+      case e21 *: e1 *: e2 *: e3 *: e4 *: e5 *: e6 *: e7 *: e8 *: e9 *: e10 *: e11 *: e12 *: e13 *: e14 *: e15 *: e16 *: e17 *: e18 *: e19 *: e20 *: () =>
+      assert(e1 == 1)
+      assert(e2 == 2)
+      assert(e3 == 3)
+      assert(e4 == 4)
+      assert(e5 == 5)
+      assert(e6 == 6)
+      assert(e7 == 7)
+      assert(e8 == 8)
+      assert(e9 == 9)
+      assert(e10 == 10)
+      assert(e11 == 11)
+      assert(e12 == 12)
+      assert(e13 == 13)
+      assert(e14 == 14)
+      assert(e15 == 15)
+      assert(e16 == 16)
+      assert(e17 == 17)
+      assert(e18 == 18)
+      assert(e19 == 19)
+      assert(e20 == 20)
+      assert(e21 == 21)
+    }
+
+    //       1   2   3   4   5   6   7   8   9   10  11  12  13  14  15  16  17  18  19  20  21  22
+    val t22: *:[I,*:[I,*:[I,*:[I,*:[I,*:[I,*:[I,*:[I,*:[I,*:[I,*:[I,*:[I,*:[I,*:[I,*:[I,*:[I,*:[I,*:[I,*:[I,*:[I,*:[I,*:[I,Unit]]]]]]]]]]]]]]]]]]]]]] = 22 *: t21
+
+    t22 match {
+      case e22 *: e21 *: e1 *: e2 *: e3 *: e4 *: e5 *: e6 *: e7 *: e8 *: e9 *: e10 *: e11 *: e12 *: e13 *: e14 *: e15 *: e16 *: e17 *: e18 *: e19 *: e20 *: () =>
+      assert(e1 == 1)
+      assert(e2 == 2)
+      assert(e3 == 3)
+      assert(e4 == 4)
+      assert(e5 == 5)
+      assert(e6 == 6)
+      assert(e7 == 7)
+      assert(e8 == 8)
+      assert(e9 == 9)
+      assert(e10 == 10)
+      assert(e11 == 11)
+      assert(e12 == 12)
+      assert(e13 == 13)
+      assert(e14 == 14)
+      assert(e15 == 15)
+      assert(e16 == 16)
+      assert(e17 == 17)
+      assert(e18 == 18)
+      assert(e19 == 19)
+      assert(e20 == 20)
+      assert(e21 == 21)
+      assert(e22 == 22)
+    }
+
+    //       1   2   3   4   5   6   7   8   9   10  11  12  13  14  15  16  17  18  19  20  21  22  23
+    val t23: *:[I,*:[I,*:[I,*:[I,*:[I,*:[I,*:[I,*:[I,*:[I,*:[I,*:[I,*:[I,*:[I,*:[I,*:[I,*:[I,*:[I,*:[I,*:[I,*:[I,*:[I,*:[I,*:[I,Unit]]]]]]]]]]]]]]]]]]]]]]] = 23 *: t22
+
+    t23 match {
+      case e23 *: e22 *: e21 *: e1 *: e2 *: e3 *: e4 *: e5 *: e6 *: e7 *: e8 *: e9 *: e10 *: e11 *: e12 *: e13 *: e14 *: e15 *: e16 *: e17 *: e18 *: e19 *: e20 *: () =>
+      assert(e1 == 1)
+      assert(e2 == 2)
+      assert(e3 == 3)
+      assert(e4 == 4)
+      assert(e5 == 5)
+      assert(e6 == 6)
+      assert(e7 == 7)
+      assert(e8 == 8)
+      assert(e9 == 9)
+      assert(e10 == 10)
+      assert(e11 == 11)
+      assert(e12 == 12)
+      assert(e13 == 13)
+      assert(e14 == 14)
+      assert(e15 == 15)
+      assert(e16 == 16)
+      assert(e17 == 17)
+      assert(e18 == 18)
+      assert(e19 == 19)
+      assert(e20 == 20)
+      assert(e21 == 21)
+      assert(e22 == 22)
+      assert(e23 == 23)
+    }
+
+    //       1   2   3   4   5   6   7   8   9   10  11  12  13  14  15  16  17  18  19  20  21  22  23  24
+    val t24: *:[I,*:[I,*:[I,*:[I,*:[I,*:[I,*:[I,*:[I,*:[I,*:[I,*:[I,*:[I,*:[I,*:[I,*:[I,*:[I,*:[I,*:[I,*:[I,*:[I,*:[I,*:[I,*:[I,*:[I,Unit]]]]]]]]]]]]]]]]]]]]]]]] = 24 *: t23
+
+    t24 match {
+      case e24 *: e23 *: e22 *: e21 *: e1 *: e2 *: e3 *: e4 *: e5 *: e6 *: e7 *: e8 *: e9 *: e10 *: e11 *: e12 *: e13 *: e14 *: e15 *: e16 *: e17 *: e18 *: e19 *: e20 *: () =>
+      assert(e1 == 1)
+      assert(e2 == 2)
+      assert(e3 == 3)
+      assert(e4 == 4)
+      assert(e5 == 5)
+      assert(e6 == 6)
+      assert(e7 == 7)
+      assert(e8 == 8)
+      assert(e9 == 9)
+      assert(e10 == 10)
+      assert(e11 == 11)
+      assert(e12 == 12)
+      assert(e13 == 13)
+      assert(e14 == 14)
+      assert(e15 == 15)
+      assert(e16 == 16)
+      assert(e17 == 17)
+      assert(e18 == 18)
+      assert(e19 == 19)
+      assert(e20 == 20)
+      assert(e21 == 21)
+      assert(e22 == 22)
+      assert(e23 == 23)
+      assert(e24 == 24)
+    }
+  }
+}
---
+++ tests/run/tuple-accessors.scala
@@ -0,0 +1,61 @@
+import dotty._
+
+case class X2[A, B](a: A, b: B)
+case class X3[A, B, C](a: A, b: B, c: C)
+case class X4[A, B, C, D](a: A, b: B, c: C, d: D)
+case class X5[A, B, C, D, E](a: A, b: B, c: C, d: D, e: E)
+case class X6[A, B, C, D, E, F](a: A, b: B, c: C, d: D, e: E, f: F)
+
+object Test {
+  def main(args: Array[String]) = {
+
+    val x2 = X2("a", 2)
+    val X2(a2, b2) = x2
+
+    assert(a2 == "a")
+    assert(b2 == 2)
+
+    val x3 = X3("a", 2, "b")
+    val X3(a3, b3, c3) = x3
+
+    assert(a3 == "a")
+    assert(b3 == 2)
+    assert(c3 == "b")
+
+    val x4 = X4("a", 2, "b", 3)
+    val X4(a4, b4, c4, d4) = x4
+
+    assert(a4 == "a")
+    assert(b4 == 2)
+    assert(c4 == "b")
+    assert(d4 == 3)
+
+    val x5 = X5("a", 2, "b", 3, "c")
+    val X5(a5, b5, c5, d5, e5) = x5
+
+    assert(a5 == "a")
+    assert(b5 == 2)
+    assert(c5 == "b")
+    assert(d5 == 3)
+    assert(e5 == "c")
+
+    val x6 = X6("a", 2, "b", 3, "c", 4)
+    val X6(a6, b6, c6, d6, e6, f6) = x6
+
+    assert(a6 == "a")
+    assert(b6 == 2)
+    assert(c6 == "b")
+    assert(d6 == 3)
+    assert(e6 == "c")
+    assert(f6 == 4)
+  }
+
+  def any = {
+    val x: Any = null
+    val X2(a2, b2) = x
+    val X3(a3, b3, c3) = x
+    val X4(a4, b4, c4, d4) = x
+    val X5(a5, b5, c5, d5, e5) = x
+    val X6(a6, b6, c6, d6, e6, f6) = x
+  }
+}
