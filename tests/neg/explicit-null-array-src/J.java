09ea9a1456739b61c6f1d51a9c9fe75330c36d9b
Abel Nieto <abel.nieto90@gmail.com>
2018-10-22 12:13:45 -0400

    Add two explicit null tests

---
+++ tests/neg/explicit-null-array-src/J.java
@@ -0,0 +1,3 @@
+class J {
+	void foo(String[] ss) {}
+}
---
+++ tests/neg/explicit-null-varargs-src/J.java
@@ -0,0 +1,12 @@
+class J {
+	void foo(String... ss) {}
+}
+
+class Animal {}
+
+class Dog extends Animal {}
+
+class J2 {
+	void foo(Animal... animal) {}
+}
+
