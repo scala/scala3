70c03b85eb9ae75da890ce812456419c18d4a82d
Guillaume Martres <smarter@ubuntu.com>
2018-10-24 19:30:35 +0200

    vscode-dotty: upgrade the default version of dotty

--- project/Build.scala
+++ project/Build.scala
@@ -43,7 +43,7 @@ object Build {
   // This should be the latest published releases.
   // TODO: Have the vscode extension fetch these numbers from the Internet
   // instead of hardcoding them ?
-  val publishedDottyVersion = "0.11.0-bin-20181017-3253921-NIGHTLY" // Using a nightly for now to get worksheet support
+  val publishedDottyVersion = "0.11.0-bin-20181031-5a465af-NIGHTLY" // Using a nightly for now to get worksheet support
   val publishedSbtDottyVersion = "0.2.5"
 
 
