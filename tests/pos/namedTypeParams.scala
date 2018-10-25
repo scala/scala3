d73424b031f32f5247a7acebfb233b72c99dcf69
Guillaume Martres <smarter@ubuntu.com>
2018-10-25 18:27:24 +0200

    More details on named type arguments

--- tests/pos/namedTypeParams.scala
+++ tests/pos/namedTypeParams.scala
@@ -6,4 +6,10 @@ object Test {
   f[X = Int, Y = String](1, "")
   f[X = Int](1, "")
   f[Y = String](1, "")
+
+  f[X = Int][Y = String](1, "")
+  f[X = Int][String](1, "")
+
+  f[Y = String][X = Int](1, "")
+  f[Y = String][Int](1, "")
 }
