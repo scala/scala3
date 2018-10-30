7ada2642d53b745b6efbe50aa37bae13d18ccbce
Guillaume Martres <smarter@ubuntu.com>
2018-10-30 16:04:16 +0100

    Fix #5333: Invalid entries in the baseType cache
    Provisional types should not have their baseType cached, it may change
    as type variables get refined and instantiated.
    
    The seqtype-cycle test had to be adjusted to avoid some missing
    reference errors, probably due to the completion order changing.

--- compiler/src/dotty/tools/dotc/core/SymDenotations.scala
+++ compiler/src/dotty/tools/dotc/core/SymDenotations.scala
@@ -1631,7 +1631,10 @@ object SymDenotations {
           Stats.record("basetype cache entries")
           if (!baseTp.exists) Stats.record("basetype cache NoTypes")
         }
-        btrCache.put(tp, baseTp)
+        if (!tp.isProvisional)
+          btrCache.put(tp, baseTp)
+        else
+          btrCache.remove(tp) // Remove any potential sentinel value
       }
 
       def ensureAcyclic(baseTp: Type) = {
@@ -1717,8 +1720,6 @@ object SymDenotations {
               val baseTp = recur(superTp)
               tp match {
                 case tp: CachedType if baseTp.exists && inCache(superTp) =>
-                  // Note: This also works for TypeVars: If they are not instantiated, their supertype
-                  // is a TypeParamRef, which is never cached. So uninstantiated TypeVars are not cached either.
                   record(tp, baseTp)
                 case _ =>
               }
