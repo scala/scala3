58f7832b008359a598d028af60a104f1f7d635ba
Guillaume Martres <smarter@ubuntu.com>
2018-11-01 14:40:51 +0100

    vscode-dotty: bump to 0.1.10-snapshot

--- project/Build.scala
+++ project/Build.scala
@@ -987,7 +987,7 @@ object Build {
     settings(commonSettings).
     settings(
       EclipseKeys.skipProject := true,
-      version := "0.1.9", // Keep in sync with package.json
+      version := "0.1.10-snapshot", // Keep in sync with package.json
       autoScalaLibrary := false,
       publishArtifact := false,
       includeFilter in unmanagedSources := NothingFilter | "*.ts" | "**.json",
