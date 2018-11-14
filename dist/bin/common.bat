6688ea66fe976c534c5ea4d822ea9fcfc428bc0c
michelou <stephane.micheloud@gmail.com>
2018-11-14 17:56:23 +0100

    added batch scripts for a new PR

---
+++ dist/bin/common.bat
@@ -0,0 +1,63 @@
+rem ##########################################################################
+rem ## Code common to dotc.bat, dotd.bat and dotr.bat
+
+if defined JAVACMD (
+    set _JAVACMD=%JAVACMD%
+    if %_DEBUG%==1 echo [%_BASENAME%] Using environment variable JAVACMD
+) else if defined JAVA_HOME (
+    set _JAVACMD=%JAVA_HOME%\bin\java.exe
+    if %_DEBUG%==1 echo [%_BASENAME%] Using environment variable JAVA_HOME
+) else if defined JDK_HOME (
+    set _JAVACMD=%JDK_HOME%\bin\java.exe
+    if %_DEBUG%==1 echo [%_BASENAME%] Using environment variable JDK_HOME
+) else (
+    where /q java.exe
+    if !ERRORLEVEL!==0 (
+        for /f "delims=" %%i in ('where /f java.exe') do set _JAVA_BIN_DIR=%%~dpsi
+        rem we ignore Oracle path for java executable
+        if "!_JAVA_BIN_DIR!"=="!_JAVA_BIN_DIR:javapath=!" set _JAVACMD=!_JAVA_BIN_DIR!\java.exe
+    )
+    if not defined _JAVACMD (
+        set _PATH=C:\Progra~1\Java
+        for /f %%f in ('dir /ad /b "!_PATH!\jre*" 2^>NUL') do set _JAVA_HOME=!_PATH!\%%f
+        if not defined _JAVA_HOME (
+           set _PATH=C:\opt
+           for /f %%f in ('dir /ad /b "!_PATH!\jdk*" 2^>NUL') do set _JAVA_HOME=!_PATH!\%%f\jre
+        )
+        if defined _JAVA_HOME (
+            if %_DEBUG%==1 echo [%_BASENAME%] Using default Java installation directory !_JAVA_HOME!
+            set _JAVACMD=!_JAVA_HOME!\bin\java.exe
+        )
+    )
+)
+if not exist "%_JAVACMD%" (
+   echo Error: Java executable not found ^(%_JAVACMD%^) 1>&2
+   set _EXITCODE=1
+   goto :eof
+)
+
+if defined DOTTY_HOME (
+    set _LIB_DIR=%DOTTY_HOME%\lib
+) else (
+    if not defined _PROG_HOME (
+        for %%f in ("%~dp0..") do set _PROG_HOME=%%~sf
+    )
+    set _LIB_DIR=!_PROG_HOME!\lib
+)
+
+set _PSEP=;
+
+for /f %%f in ('dir /a-d /b "%_LIB_DIR%\*dotty-compiler*"')       do set _DOTTY_COMP=%_LIB_DIR%\%%f
+for /f %%f in ('dir /a-d /b "%_LIB_DIR%\*dotty-interfaces*"')     do set _DOTTY_INTF=%_LIB_DIR%\%%f
+for /f %%f in ('dir /a-d /b "%_LIB_DIR%\*dotty-library*"')        do set _DOTTY_LIB=%_LIB_DIR%\%%f
+for /f %%f in ('dir /a-d /b "%_LIB_DIR%\*scala-asm*"')            do set _SCALA_ASM=%_LIB_DIR%\%%f
+for /f %%f in ('dir /a-d /b "%_LIB_DIR%\*scala-library*"')        do set _SCALA_LIB=%_LIB_DIR%\%%f
+for /f %%f in ('dir /a-d /b "%_LIB_DIR%\*scala-xml*"')            do set _SCALA_XML=%_LIB_DIR%\%%f
+for /f %%f in ('dir /a-d /b "%_LIB_DIR%\*compiler-interface*"')   do set _SBT_INTF=%_LIB_DIR%\%%f
+for /f %%f in ('dir /a-d /b "%_LIB_DIR%\*jline-reader-3*"')       do set _JLINE_READER=%_LIB_DIR%\%%f
+for /f %%f in ('dir /a-d /b "%_LIB_DIR%\*jline-terminal-3*"')     do set _JLINE_TERMINAL=%_LIB_DIR%\%%f
+for /f %%f in ('dir /a-d /b "%_LIB_DIR%\*jline-terminal-jna-3*"') do set _JLINE_TERMINAL_JNA=%_LIB_DIR%\%%f
+for /f %%f in ('dir /a-d /b "%_LIB_DIR%\*jna-4*"')                do set _JNA=%_LIB_DIR%\%%f
+
+rem debug
+set _DEBUG_STR=-agentlib:jdwp=transport=dt_socket,server=y,suspend=y,address=5005
---
+++ dist/bin/dotd.bat
@@ -0,0 +1,104 @@
+@echo off
+setlocal enabledelayedexpansion
+
+rem only for interactive debugging !
+set _DEBUG=0
+
+rem ##########################################################################
+rem ## Environment setup
+
+set _EXITCODE=0
+
+set _BASENAME=%~n0
+
+for %%f in ("%~dp0..") do set _PROG_HOME=%%~sf
+
+call %_PROG_HOME%\bin\common.bat
+if not %_EXITCODE%==0 goto end
+
+rem ##########################################################################
+rem ## Main
+
+call :javaClassPath
+
+if %_DEBUG%==1 echo [%_BASENAME%] "%_JAVACMD%" -Dscala.usejavacp=true -classpath "%_CLASS_PATH%" dotty.tools.dottydoc.Main %*
+"%_JAVACMD%" -Dscala.usejavacp=true -classpath "%_CLASS_PATH%" dotty.tools.dottydoc.Main %*
+if not %ERRORLEVEL%==0 (
+    if %_DEBUG%==1 echo [%_BASENAME%] Dottydoc execution failed
+    set _EXITCODE=1
+    goto end
+)
+goto end
+
+rem ##########################################################################
+rem ## Subroutines
+
+rem output parameter: _CLASS_PATH
+:javaClassPath
+set __LIB_DIR=%_PROG_HOME%\lib
+
+rem Set dotty-doc dep:
+for /f %%f in ('dir /a-d /b "%__LIB_DIR%\*dotty-doc*"') do set _DOTTY_DOC_LIB=%__LIB_DIR%\%%f
+
+rem Set flexmark deps:
+for /f %%f in ('dir /a-d /b "%__LIB_DIR%\*flexmark-0*"')                     do set _FLEXMARK_LIBS=%__LIB_DIR%\%%f%_PSEP%
+for /f %%f in ('dir /a-d /b "%__LIB_DIR%\*flexmark-ext-anchorlink*"')        do set _FLEXMARK_LIBS=%_FLEXMARK_LIBS%%__LIB_DIR%\%%f%_PSEP%
+for /f %%f in ('dir /a-d /b "%__LIB_DIR%\*flexmark-ext-autolink*"')          do set _FLEXMARK_LIBS=%_FLEXMARK_LIBS%%__LIB_DIR%\%%f%_PSEP%
+for /f %%f in ('dir /a-d /b "%__LIB_DIR%\*flexmark-ext-emoji*"')             do set _FLEXMARK_LIBS=%_FLEXMARK_LIBS%%__LIB_DIR%\%%f%_PSEP%
+for /f %%f in ('dir /a-d /b "%__LIB_DIR%\*flexmark-ext-gfm-strikethrough*"') do set _FLEXMARK_LIBS=%_FLEXMARK_LIBS%%__LIB_DIR%\%%f%_PSEP%
+for /f %%f in ('dir /a-d /b "%__LIB_DIR%\*flexmark-ext-gfm-tables*"')        do set _FLEXMARK_LIBS=%_FLEXMARK_LIBS%%__LIB_DIR%\%%f%_PSEP%
+for /f %%f in ('dir /a-d /b "%__LIB_DIR%\*flexmark-ext-gfm-tasklist*"')      do set _FLEXMARK_LIBS=%_FLEXMARK_LIBS%%__LIB_DIR%\%%f%_PSEP%
+for /f %%f in ('dir /a-d /b "%__LIB_DIR%\*flexmark-ext-ins*"')               do set _FLEXMARK_LIBS=%_FLEXMARK_LIBS%%__LIB_DIR%\%%f%_PSEP%
+for /f %%f in ('dir /a-d /b "%__LIB_DIR%\*flexmark-ext-superscript*"')       do set _FLEXMARK_LIBS=%_FLEXMARK_LIBS%%__LIB_DIR%\%%f%_PSEP%
+for /f %%f in ('dir /a-d /b "%__LIB_DIR%\*flexmark-ext-tables*"')            do set _FLEXMARK_LIBS=%_FLEXMARK_LIBS%%__LIB_DIR%\%%f%_PSEP%
+for /f %%f in ('dir /a-d /b "%__LIB_DIR%\*flexmark-ext-wikilink*"')          do set _FLEXMARK_LIBS=%_FLEXMARK_LIBS%%__LIB_DIR%\%%f%_PSEP%
+for /f %%f in ('dir /a-d /b "%__LIB_DIR%\*flexmark-ext-yaml-front-matter*"') do set _FLEXMARK_LIBS=%_FLEXMARK_LIBS%%__LIB_DIR%\%%f%_PSEP%
+for /f %%f in ('dir /a-d /b "%__LIB_DIR%\*flexmark-formatter*"')             do set _FLEXMARK_LIBS=%_FLEXMARK_LIBS%%__LIB_DIR%\%%f%_PSEP%
+for /f %%f in ('dir /a-d /b "%__LIB_DIR%\*flexmark-jira-converter*"')        do set _FLEXMARK_LIBS=%_FLEXMARK_LIBS%%__LIB_DIR%\%%f%_PSEP%
+for /f %%f in ('dir /a-d /b "%__LIB_DIR%\*flexmark-util*"')                  do set _FLEXMARK_LIBS=%_FLEXMARK_LIBS%%__LIB_DIR%\%%f%_PSEP%
+
+rem Set jackson deps:
+for /f %%f in ('dir /a-d /b "%__LIB_DIR%\*jackson-annotations*"')     do set _JACKSON_LIBS=%__LIB_DIR%\%%f%_PSEP%
+for /f %%f in ('dir /a-d /b "%__LIB_DIR%\*jackson-core*"')            do set _JACKSON_LIBS=%_JACKSON_LIBS%%__LIB_DIR%\%%f%_PSEP%
+for /f %%f in ('dir /a-d /b "%__LIB_DIR%\*jackson-databind*"')        do set _JACKSON_LIBS=%_JACKSON_LIBS%%__LIB_DIR%\%%f%_PSEP%
+for /f %%f in ('dir /a-d /b "%__LIB_DIR%\*jackson-dataformat-yaml*"') do set _JACKSON_LIBS=%_JACKSON_LIBS%%__LIB_DIR%\%%f%_PSEP%
+
+rem Set liqp dep:
+for /f %%f in ('dir /a-d /b "%__LIB_DIR%\*liqp*"') do set _LIQP_LIB=%__LIB_DIR%\%%f%_PSEP%
+
+rem Set ANTLR dep:
+for /f %%f in ('dir /a-d /b "%__LIB_DIR%\*antlr-3*"') do set _ANTLR_LIB=%__LIB_DIR%\%%f%_PSEP%
+for /f %%f in ('dir /a-d /b "%__LIB_DIR%\*antlr-runtime-3*"') do set _ANTLR_RUNTIME_LIB=%__LIB_DIR%\%%f%_PSEP%
+
+rem Set autolink dep:
+rem conflict with flexmark-ext-autolink-0.11
+for /f %%f in ('dir /a-d /b "%__LIB_DIR%\*autolink-0.6*"') do set _AUTOLINK_LIB=%__LIB_DIR%\%%f
+
+rem Set snakeyaml dep:
+for /f %%f in ('dir /a-d /b "%__LIB_DIR%\*snakeyaml*"') do set _SNAKEYAML_LIB=%__LIB_DIR%\%%f%_PSEP%
+
+rem Set ST4 dep:
+for /f %%f in ('dir /a-d /b "%__LIB_DIR%\*ST4*"') do set _ST4_LIB=%__LIB_DIR%\%%f%_PSEP%
+
+rem Set jsoup dep:
+for /f %%f in ('dir /a-d /b "%__LIB_DIR%\*jsoup*"') do set _JSOUP_LIB=%__LIB_DIR%\%%f%_PSEP%
+
+set _CLASS_PATH=%_DOTTY_LIB%%_PSEP%%_DOTTY_COMP%%_PSEP%%_DOTTY_DOC_LIB%%_PSEP%%_DOTTY_INTF%%_PSEP%%_SBT_INTF%
+set _CLASS_PATH=%_CLASS_PATH%%_PSEP%%_SCALA_LIB%
+set _CLASS_PATH=%_CLASS_PATH%%_PSEP%%_FLEXMARK_LIBS%
+set _CLASS_PATH=%_CLASS_PATH%%_PSEP%%_JACKSON_LIBS%
+set _CLASS_PATH=%_CLASS_PATH%%_PSEP%%_LIQP_LIB%
+set _CLASS_PATH=%_CLASS_PATH%%_PSEP%%_ANTLR_LIB%%_PSEP%%_ANTLR_RUNTIME_LIB%
+set _CLASS_PATH=%_CLASS_PATH%%_PSEP%%_AUTOLINK_LIB%
+set _CLASS_PATH=%_CLASS_PATH%%_PSEP%%_SNAKEYAML_LIB%
+set _CLASS_PATH=%_CLASS_PATH%%_PSEP%%_ST4_LIB%
+set _CLASS_PATH=%_CLASS_PATH%%_PSEP%%_JSOUP_LIB%
+goto :eof
+
+rem ##########################################################################
+rem ## Cleanups
+
+:end
+if %_DEBUG%==1 echo [%_BASENAME%] _EXITCODE=%_EXITCODE%
+exit /b %_EXITCODE%
+endlocal
