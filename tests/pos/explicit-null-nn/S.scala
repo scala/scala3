f611bf4ef0bd5d66d9454b77cfc82d3a59938fa2
Abel Nieto <abel.nieto90@gmail.com>
2018-11-26 16:04:41 -0500

    Test that we can call .nn on a non-null type

--- tests/pos/explicit-null-nn/S.scala
+++ tests/pos/explicit-null-nn/S.scala
@@ -4,4 +4,12 @@ class S {
   // nullability from a type.
   val s: String = j.foo.nn
   val a: Array[String|Null] = j.bar.nn
+
+  // We can also call .nn on non-nullable types.
+  val x: String = ???
+  val y: String = x.nn
+
+  // And on other Scala code.
+  val x2: String|Null = null
+  val y2: String = x2.nn
 }
