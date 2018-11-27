7e75c4301b471079c973e7da7543b672603189b6
Aggelos Biboudis <biboudis@gmail.com>
2018-11-27 17:28:04 +0100

    Handle simple method calls

--- tests/run-with-compiler/tasty-interpreter/Foo.scala
+++ tests/run-with-compiler/tasty-interpreter/Foo.scala
@@ -1,13 +1,41 @@
 object Foo {
   def main(args: Array[String]): Unit = {
-      val x1 = 42
-      println(x1)
+    val x1 = 42
+    println(x1)
+    println()
 
-      lazy val x2 = println("Hello")
-      x2
-      x2
+    lazy val x2 = println("Hello")
+    x2
+    x2
+    println()
 
-      def x3 = 42
-      println(x3)
+    def x3 = 42
+    println(x3)
+    println()
+
+    var x4: Int = 42
+    x4 = 43
+    println(x4)
+    println()
+
+    if(x1 == 42)
+      println("if")
+    else
+      println("else")
+    println()
+
+    var x5 = 5
+    while(x5 > 0){
+      println(x5)
+      x5 = x5 - 1
+    }
+    println()
+
+    def meth() = 42
+    println(meth())
+    println()
+
+    def methP(i: Int) = i
+    println(methP(55))
   }
 }
--- tests/run-with-compiler/tasty-interpreter/Test.scala
+++ tests/run-with-compiler/tasty-interpreter/Test.scala
@@ -8,15 +8,32 @@ object Test {
 }
 
 trait Ref {
-  def value: Any
+  def get: Any
 }
-class Eager(val value: Any) extends Ref
+class Eager(val get: Any) extends Ref
 class Lazy(thunk: => Any) extends Ref {
-  lazy val value: Any = thunk
+  lazy val get: Any = thunk
 }
+class Var(private var value: Any) extends Ref {
+  def get = value
+
+  def update(rhs: Any): Unit = {
+    value = rhs
+  }
+}
+
 
 class Interpreter[R <: Reflection & Singleton](val reflect: R)(implicit ctx: reflect.Context) {
   import reflect._
+
+  object Call {
+    def unapply(arg: Tree): Option[(Tree, List[Tree])] = arg match {
+      case fn@ Term.Select(_, _, _) => Some((fn, Nil))
+      case Term.Apply(Call(fn, args1), args2) => Some((fn, args1 ::: args2))
+      case Term.TypeApply(Call(fn, args), _) => Some((fn, args))
+      case _ => None
+    }
+  }
 
   type Env = Map[Symbol, Ref]
 
@@ -28,26 +45,60 @@ class Interpreter[R <: Reflection & Singleton](val reflect: R)(implicit ctx: ref
       case Term.Apply(Term.Ident("println"), List(arg)) =>
         println(eval(arg))
 
-      case Term.Apply(Term.Select(a, "+", Some(sig)), List(b)) =>
-        eval(a).asInstanceOf[Int] + eval(b).asInstanceOf[Int]
+      case Term.Apply(Term.Ident("println"), List()) =>
+        println()
+
+      case Term.Apply(Term.Select(a, op, Some(sig)), List(b)) =>
+        op match {
+          case "+" => eval(a).asInstanceOf[Int] + eval(b).asInstanceOf[Int]
+          case "-" => eval(a).asInstanceOf[Int] - eval(b).asInstanceOf[Int]
+          case ">" => eval(a).asInstanceOf[Int] > eval(b).asInstanceOf[Int]
+          case "==" => eval(a).asInstanceOf[Int] == eval(b).asInstanceOf[Int]
+        }
+
+      // TODO: handle multiple parameters
+      case Term.Apply(meth@Term.Ident(_), args) =>
+        meth.symbol match {
+          case IsDefSymbol(sym) =>
+            val evaluatedArgs = args.map(arg => new Eager(eval(arg)))
+            val env1 = env ++ sym.tree.paramss.head.map(_.symbol).zip(evaluatedArgs)
+
+            eval(sym.tree.rhs.get)(env1)
+        }
 
       case Term.Ident(_) =>
-
         tree.symbol match {
           case IsDefSymbol(sym) =>
             eval(sym.tree.rhs.get)
           case _ =>
-            env(tree.symbol).value
+            env(tree.symbol).get
+        }
+
+      case Term.Assign(lhs, rhs) =>
+        env(lhs.symbol) match {
+          case varf: Var => varf.update(eval(rhs))
+        }
+
+      case Term.If(cond, thenp, elsep) =>
+        if(eval(cond).asInstanceOf[Boolean])
+          eval(thenp)
+        else
+          eval(elsep)
+
+      case Term.While(cond, expr) =>
+        while(eval(cond).asInstanceOf[Boolean]){
+          eval(expr)
         }
 
       case Term.Block(stats, expr) =>
-
         val newEnv = stats.foldLeft(env)((accEnv, stat) => stat match {
           case ValDef(name, tpt, Some(rhs)) =>
 
             val evalRef: Ref =
               if (stat.symbol.flags.isLazy)
-                new Lazy(eval(rhs)(accEnv))
+                new Lazy(eval(rhs)(accEnv)) // do not factor out rhs from here (laziness)
+              else if (stat.symbol.flags.isMutable)
+                new Var(eval(rhs)(accEnv))
               else
                 new Eager(eval(rhs)(accEnv))
 
@@ -82,8 +133,6 @@ class TastyInterpreter extends TastyConsumer {
         // TODO: check the correct sig and object enclosement for main
         case DefDef("main", _, _, _, Some(rhs)) =>
           val interpreter = new Interpreter(reflect)
-
-          println("Found main")
 
           interpreter.eval(rhs)(Map.empty)
         // TODO: recurse only for PackageDef, ClassDef
