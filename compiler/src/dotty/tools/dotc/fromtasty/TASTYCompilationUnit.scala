0688420a30585dac7b7deb0fc8837c44af783cbb
Martin Duhem <martin.duhem@gmail.com>
2018-11-28 15:31:51 +0100

    Address review comment

--- compiler/src/dotty/tools/dotc/fromtasty/TASTYCompilationUnit.scala
+++ compiler/src/dotty/tools/dotc/fromtasty/TASTYCompilationUnit.scala
@@ -3,14 +3,14 @@ package dotty.tools.dotc.fromtasty
 import dotty.tools.dotc.CompilationUnit
 import dotty.tools.dotc.core.Contexts.Context
 import dotty.tools.dotc.util.NoSource
-import dotty.tools.io.Path
+import dotty.tools.io.File
 
 class TASTYCompilationUnit(val className: String) extends CompilationUnit(NoSource) {
   override def toString: String = s"class file $className"
 }
 object TASTYCompilationUnit {
   def apply(className: String)(implicit ctx: Context): Option[TASTYCompilationUnit] = {
-    if (Path(className).exists) {
+    if (File(className).exists) {
       ctx.inform(s"Ignoring $className: cannot create a `TASTYCompilationUnit` for a source file.")
       None
     } else {
