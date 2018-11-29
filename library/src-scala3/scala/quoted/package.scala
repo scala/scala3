8c0178e85e6f82b3fc3372ce513e082e8a837d3d
Nicolas Stucki <nicolas.stucki@gmail.com>
2018-11-29 17:07:45 +0100

    Add List[Expr[T]] to Expr[List[T]] conversion in the library

--- library/src-scala3/scala/quoted/package.scala
+++ library/src-scala3/scala/quoted/package.scala
@@ -6,4 +6,13 @@ package object quoted {
     def toExpr(implicit ev: Liftable[T]): Expr[T] = ev.toExpr(x)
   }
 
+  implicit class ListOfExprOps[T](val list: List[Expr[T]]) extends AnyVal {
+    def toExprOfList(implicit ev: Type[T]): Expr[List[T]] = {
+      def rec(list: List[Expr[T]]): Expr[List[T]] = list match {
+        case x :: xs  => '{ (~x) :: (~rec(xs)) }
+        case Nil => '(Nil)
+      }
+      rec(list)
+    }
+  }
 }
