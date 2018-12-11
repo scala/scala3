67b3f72117ba9f20a6e5cee96c1ff1799119858d
Allan Renucci <allan.renucci@gmail.com>
2018-12-11 15:47:11 +0100

    Bump sbt, scala and zinc version

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -10,7 +10,7 @@ object Dependencies {
   val `jackson-dataformat-yaml` =
     "com.fasterxml.jackson.dataformat" % "jackson-dataformat-yaml" % jacksonVersion
 
-  private val zincVersion = "1.2.2"
+  private val zincVersion = "1.2.5"
   val `compiler-interface` = "org.scala-sbt" % "compiler-interface" % zincVersion
   val `zinc-api-info` = "org.scala-sbt" %% "zinc-apiinfo" % zincVersion
 }
