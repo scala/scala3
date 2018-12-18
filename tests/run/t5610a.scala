fedb58708120660c34d2fa14ec497af57f1bc430
Abel Nieto <abel.nieto90@gmail.com>
2018-12-18 16:09:59 -0500

    fix test

--- tests/run/t5610a.scala
+++ tests/run/t5610a.scala
@@ -1,13 +1,13 @@
 object Test extends dotty.runtime.LegacyApp {
-  class Result(_str: => String) {
+  class Result(_str: => String|Null) {
     lazy val str = _str
   }
 
-  def foo(str: => String)(i: Int) = new Result(str)
+  def foo(str: => String|Null)(i: Int) = new Result(str)
 
   def bar(f: Int => Result) = f(42)
 
-  var test: String = null
+  var test: String|Null = null
   val result = bar(foo(test))
   test = "bar"
 
