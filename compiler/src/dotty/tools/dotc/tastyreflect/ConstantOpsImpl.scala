8a60c20413b673e4d3fb9f3321d083b40ec33a63
Nicolas Stucki <nicolas.stucki@gmail.com>
2019-01-03 15:53:50 +0100

    Add missing tasty.reflect.Constant constructors

--- compiler/src/dotty/tools/dotc/tastyreflect/ConstantOpsImpl.scala
+++ compiler/src/dotty/tools/dotc/tastyreflect/ConstantOpsImpl.scala
@@ -12,14 +12,17 @@ trait ConstantOpsImpl extends scala.tasty.reflect.ConstantOps with CoreImpl {
   object Constant extends ConstantModule {
 
     object Unit extends UnitModule {
+      def apply(): Constant = Constants.Constant(())
       def unapply(x: Constant): Boolean = x.tag == Constants.UnitTag
     }
 
     object Null extends NullModule {
+      def apply(): Constant = Constants.Constant(null)
       def unapply(x: Constant): Boolean = x.tag == Constants.NullTag
     }
 
     object Boolean extends BooleanModule {
+      def apply(x: Boolean): Constant = Constants.Constant(x)
       def unapply(x: Constant): Option[Boolean] = x match {
         case x: Constants.Constant if x.tag == Constants.BooleanTag => Some(x.booleanValue)
         case _ => None
@@ -27,6 +30,7 @@ trait ConstantOpsImpl extends scala.tasty.reflect.ConstantOps with CoreImpl {
     }
 
     object Byte extends ByteModule {
+      def apply(x: Byte): Constant = Constants.Constant(x)
       def unapply(x: Constant): Option[Byte] = x match {
         case x: Constants.Constant if x.tag == Constants.ByteTag => Some(x.byteValue)
         case _ => None
@@ -34,6 +38,7 @@ trait ConstantOpsImpl extends scala.tasty.reflect.ConstantOps with CoreImpl {
     }
 
     object Short extends ShortModule {
+      def apply(x: Short): Constant = Constants.Constant(x)
       def unapply(x: Constant): Option[Short] = x match {
         case x: Constants.Constant if x.tag == Constants.ShortTag => Some(x.shortValue)
         case _ => None
@@ -41,6 +46,7 @@ trait ConstantOpsImpl extends scala.tasty.reflect.ConstantOps with CoreImpl {
     }
 
     object Char extends CharModule {
+      def apply(x: Char): Constant = Constants.Constant(x)
       def unapply(x: Constant): Option[Char] = x match {
         case x: Constants.Constant if x.tag == Constants.CharTag => Some(x.charValue)
         case _ => None
@@ -48,6 +54,7 @@ trait ConstantOpsImpl extends scala.tasty.reflect.ConstantOps with CoreImpl {
     }
 
     object Int extends IntModule {
+      def apply(x: Int): Constant = Constants.Constant(x)
       def unapply(x: Constant): Option[Int] = x match {
         case x: Constants.Constant if x.tag == Constants.IntTag => Some(x.intValue)
         case _ => None
@@ -55,6 +62,7 @@ trait ConstantOpsImpl extends scala.tasty.reflect.ConstantOps with CoreImpl {
     }
 
     object Long extends LongModule {
+      def apply(x: Long): Constant = Constants.Constant(x)
       def unapply(x: Constant): Option[Long] = x match {
         case x: Constants.Constant if x.tag == Constants.LongTag => Some(x.longValue)
         case _ => None
@@ -62,6 +70,7 @@ trait ConstantOpsImpl extends scala.tasty.reflect.ConstantOps with CoreImpl {
     }
 
     object Float extends FloatModule {
+      def apply(x: Float): Constant = Constants.Constant(x)
       def unapply(x: Constant): Option[Float] = x match {
         case x: Constants.Constant if x.tag == Constants.FloatTag => Some(x.floatValue)
         case _ => None
@@ -69,6 +78,7 @@ trait ConstantOpsImpl extends scala.tasty.reflect.ConstantOps with CoreImpl {
     }
 
     object Double extends DoubleModule {
+      def apply(x: Double): Constant = Constants.Constant(x)
       def unapply(x: Constant): Option[Double] = x match {
         case x: Constants.Constant if x.tag == Constants.DoubleTag => Some(x.doubleValue)
         case _ => None
@@ -76,6 +86,7 @@ trait ConstantOpsImpl extends scala.tasty.reflect.ConstantOps with CoreImpl {
     }
 
     object String extends StringModule {
+      def apply(x: String): Constant = Constants.Constant(x)
       def unapply(x: Constant): Option[String] = x match {
         case x: Constants.Constant if x.tag == Constants.StringTag => Some(x.stringValue)
         case _ => None
@@ -83,6 +94,7 @@ trait ConstantOpsImpl extends scala.tasty.reflect.ConstantOps with CoreImpl {
     }
 
     object ClassTag extends ClassTagModule {
+      def apply[T](implicit x: scala.reflect.ClassTag[T]): Constant = Constants.Constant(x)
       def unapply(x: Constant): Option[Type] = x match {
         case x: Constants.Constant if x.tag == Constants.ClazzTag => Some(x.typeValue)
         case _ => None
@@ -90,6 +102,7 @@ trait ConstantOpsImpl extends scala.tasty.reflect.ConstantOps with CoreImpl {
     }
 
     object Symbol extends SymbolModule {
+      def apply(x: scala.Symbol): Constant = Constants.Constant(x)
       def unapply(x: Constant): Option[scala.Symbol] = x match {
         case x: Constants.Constant if x.tag == Constants.ScalaSymbolTag => Some(x.scalaSymbolValue)
         case _ => None
