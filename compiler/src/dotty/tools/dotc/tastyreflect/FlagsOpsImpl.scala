5e7f54b85fc0d6de821e5a398976b213e96b2485
Nicolas Stucki <nicolas.stucki@gmail.com>
2019-01-03 16:20:51 +0100

    Add constructors for TASTy reflect flags

---
+++ compiler/src/dotty/tools/dotc/tastyreflect/FlagsOpsImpl.scala
@@ -0,0 +1,38 @@
+package dotty.tools.dotc
+package tastyreflect
+
+import dotty.tools.dotc.core.Decorators._
+
+trait FlagsOpsImpl extends scala.tasty.reflect.FlagsOps with CoreImpl {
+
+  object Flags extends FlagsModule {
+    def Protected: FlagSet = new FlagSet(core.Flags.Protected)
+    def Abstract: FlagSet = new FlagSet(core.Flags.Abstract)
+    def Final: FlagSet = new FlagSet(core.Flags.Final)
+    def Sealed: FlagSet = new FlagSet(core.Flags.Sealed)
+    def Case: FlagSet = new FlagSet(core.Flags.Case)
+    def Implicit: FlagSet = new FlagSet(core.Flags.Implicit)
+    def Erased: FlagSet = new FlagSet(core.Flags.Erased)
+    def Lazy: FlagSet = new FlagSet(core.Flags.Lazy)
+    def Override: FlagSet = new FlagSet(core.Flags.Override)
+    def Inline: FlagSet = new FlagSet(core.Flags.Inline)
+    def Macro: FlagSet = new FlagSet(core.Flags.Macro)
+    def Static: FlagSet = new FlagSet(core.Flags.JavaStatic)
+    def Object: FlagSet = new FlagSet(core.Flags.Module)
+    def Trait: FlagSet = new FlagSet(core.Flags.Trait)
+    def Local: FlagSet = new FlagSet(core.Flags.Local)
+    def Synthetic: FlagSet = new FlagSet(core.Flags.Synthetic)
+    def Artifact: FlagSet = new FlagSet(core.Flags.Artifact)
+    def Mutable: FlagSet = new FlagSet(core.Flags.Mutable)
+    def FieldAccessor: FlagSet = new FlagSet(core.Flags.Accessor)
+    def CaseAcessor: FlagSet = new FlagSet(core.Flags.CaseAccessor)
+    def Covariant: FlagSet = new FlagSet(core.Flags.Covariant)
+    def Contravariant: FlagSet = new FlagSet(core.Flags.Contravariant)
+    def Scala2X: FlagSet = new FlagSet(core.Flags.Scala2x)
+    def DefaultParameterized: FlagSet = new FlagSet(core.Flags.DefaultParameterized)
+    def Stable: FlagSet = new FlagSet(core.Flags.Stable)
+    def Param: FlagSet = new FlagSet(core.Flags.Param)
+    def ParamAccessor: FlagSet = new FlagSet(core.Flags.ParamAccessor)
+  }
+
+}
