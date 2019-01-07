69811e5184e71897bde0fa80c99cb901f2ab3da6
Abel Nieto <abel.nieto90@gmail.com>
2019-01-07 10:16:49 -0500

    Add implicit conversions from nullable array

--- library/src-bootstrapped/scala/NonNull.scala
+++ library/src-bootstrapped/scala/NonNull.scala
@@ -13,5 +13,8 @@ object NonNull {
     // TODO(abeln) add additional versions of these a la FunctionN?
     implicit def toNullable1[T](a: Array[T]): Array[T|Null] = a.asInstanceOf[Array[T|Null]]
     implicit def toNullable2[T](a: Array[Array[T]]): Array[Array[T|Null]|Null] = a.asInstanceOf[Array[Array[T|Null]|Null]] 
+
+    implicit def fromNullable1[T](a: Array[T|Null]): Array[T] = a.asInstanceOf[Array[T]]
+    implicit def fromNullable2[T](a: Array[Array[T|Null]|Null]): Array[Array[T]] = a.asInstanceOf[Array[Array[T]]]
   }
 }
