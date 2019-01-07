486dda4fd90c640dc578bddf8af4dfa9ef95384c
Abel Nieto <abel.nieto90@gmail.com>
2019-01-07 14:58:41 -0500

    fix test

--- tests/run/t8177f.scala
+++ tests/run/t8177f.scala
@@ -7,7 +7,7 @@ class A[T](final val x: Thing { type A = T }) {
   def x3: x.A = x.p
 }
 // all result types should be inferred as Int
-class B extends A[Int](null) {
+class B extends A[Int](new Thing { type A = Int }) {
   def y1 = x1
   def y2 = x2
   val y3 = x3 // before SI-8177, this lead to a signature that erased to java.lang.Object
@@ -15,6 +15,14 @@ class B extends A[Int](null) {
 
 
 object Test extends dotty.runtime.LegacyApp {
-  val methods = classOf[B].getDeclaredMethods.sortBy(_.getName)
-  assert(methods.forall(_.toGenericString.startsWith("public int")))
+  val methods = classOf[B].getDeclaredMethods.sortBy(_.nn.getName.nn)
+  // B's methods are
+  //   private static Thing B.B$superArg$1()
+  //   public int B.y1()
+  //   public int B.y2()
+  //   public int B.y3()
+  assert(methods.forall { m =>
+    val name = m.nn.toGenericString
+    name.startsWith("private") || name.startsWith("public int")
+  })
 }
