ab9a60720a848b8e32051a758e026a2d72a2dafd
Nicolas Stucki <nicolas.stucki@gmail.com>
2019-01-08 18:00:03 +0100

    Add infrastructure to interpret `run` test

--- tests/run-with-compiler-custom-args/tasty-interpreter/Test.scala
+++ tests/run-with-compiler-custom-args/tasty-interpreter/Test.scala
@@ -1,6 +1,10 @@
-import java.io.{ByteArrayOutputStream, PrintStream}
+import java.io.{ByteArrayOutputStream, File, PrintStream}
 
+import dotty.tools.dotc.core.Contexts
+import dotty.tools.dotc.reporting.Reporter
+import dotty.tools.dotc.reporting.diagnostic.MessageContainer
 import dotty.tools.dotc.util.DiffUtil
+import dotty.tools.io.Path
 
 import scala.io.Source
 import scala.tasty.file._
@@ -9,12 +13,8 @@ import scala.tasty.Reflection
 
 object Test {
   def main(args: Array[String]): Unit = {
-    val ps = new ByteArrayOutputStream()
-    try scala.Console.withOut(ps) {
-        ConsumeTasty("", List("IntepretedMain", "InterpretedBar"), new TastyInterpreter)
-    } catch {
-      case e: Throwable => throw new Exception(ps.toString, e)
-    }
+
+    val actualOutput = interpret("")("IntepretedMain", "InterpretedBar")
     val expectedOutput =
       """42
         |
@@ -54,12 +54,45 @@ object Test {
         |68
         |""".stripMargin
 
-    val actualOutput = ps.toString
-
     assert(expectedOutput == actualOutput,
       "\n>>>>>>>>>>>>>>>>>>\n" +
       DiffUtil.mkColoredCodeDiff(actualOutput, expectedOutput, true) +
       "<<<<<<<<<<<<<<<<<<"
     )
+
+//    compileAndInterpret("HelloWorld")
+//    compileAndInterpret("i3518")
+//    compileAndInterpret("withIndex")
+  }
+
+  def compileAndInterpret(testName: String) = {
+    val reproter = new Reporter {
+      def doReport(m: MessageContainer)(implicit ctx: Contexts.Context): Unit = println(m)
+    }
+    val out = java.nio.file.Paths.get("out/interpreted")
+    if (!java.nio.file.Files.exists(out))
+      java.nio.file.Files.createDirectory(out)
+    dotty.tools.dotc.Main.process(Array("-classpath", System.getProperty("java.class.path"), "-d", out.toString, "tests/run/" + testName + ".scala"), reproter)
+
+    val actualOutput = interpret(out.toString)("Test")
+
+    val checkFile = java.nio.file.Paths.get("tests/run/" + testName + ".check")
+    val expectedOutput = Source.fromFile(checkFile.toFile).getLines().mkString("\n")
+
+    assert(expectedOutput == actualOutput,
+      "\n>>>>>>>>>>>>>>>>>>\n" +
+        DiffUtil.mkColoredCodeDiff(actualOutput, expectedOutput, true) +
+        "<<<<<<<<<<<<<<<<<<"
+    )
+  }
+
+  def interpret(classpath: String*)(interpretedClasses: String*): String = {
+    val ps = new ByteArrayOutputStream()
+    try scala.Console.withOut(ps) {
+      ConsumeTasty(classpath.mkString(java.io.File.pathSeparatorChar.toString), interpretedClasses.toList, new TastyInterpreter)
+    } catch {
+      case e: Throwable => throw new Exception(ps.toString, e)
+    }
+    ps.toString
   }
 }
