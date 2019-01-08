f5fcaa0e1c869383a83fbcdf10f3cf08387b99d6
Abel Nieto <abel.nieto90@gmail.com>
2019-01-08 13:46:35 -0500

    fix typo

--- library/src-bootstrapped/scala/NonNull.scala
+++ library/src-bootstrapped/scala/NonNull.scala
@@ -3,7 +3,7 @@ package scala
 object NonNull {
   implicit class NonNull[T](x: T|Null) {
     def nn: T = if (x == null) {
-      throw new NullPointerException("tried cast away nullability, but value is null")
+      throw new NullPointerException("tried to cast away nullability, but value is null")
     } else {
       x.asInstanceOf[T]
     }
