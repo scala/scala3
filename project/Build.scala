e28740351db635a06c5e303157efb01ec7b7d6c1
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2019-01-16 01:09:10 +0100

    Avoid pulling broken Maven artifact
    I just created https://github.com/eclipse/xtext-lib/issues/146 with details.

--- project/Build.scala
+++ project/Build.scala
@@ -874,7 +874,8 @@ object Build {
       fork in run := true,
       fork in Test := true,
       libraryDependencies ++= Seq(
-        "org.eclipse.lsp4j" % "org.eclipse.lsp4j" % "0.5.0",
+        "org.eclipse.xtend" % "org.eclipse.xtend.lib" % "2.16.0",
+        "org.eclipse.lsp4j" % "org.eclipse.lsp4j" % "0.5.0" exclude ("org.eclipse.xtend", "org.eclipse.xtend.lib"),
         Dependencies.`jackson-databind`
       ),
       javaOptions := (javaOptions in `dotty-compiler-bootstrapped`).value,
