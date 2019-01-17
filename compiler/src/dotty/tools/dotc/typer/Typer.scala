e46529fb1a8908277284d8a478b95a245a9f098b
Martin Odersky <odersky@gmail.com>
2019-01-17 18:02:25 +0100

    Optimization: Avoid hot lazy vals in adaptNoArgs

--- compiler/src/dotty/tools/dotc/typer/Typer.scala
+++ compiler/src/dotty/tools/dotc/typer/Typer.scala
@@ -2567,17 +2567,19 @@ class Typer extends Namer
 
     def adaptNoArgs(wtp: Type): Tree = {
       val ptNorm = underlyingApplied(pt)
-      lazy val functionExpected = defn.isFunctionType(ptNorm)
-      lazy val resultMatch = constrainResult(tree.symbol, wtp, followAlias(pt))
+      def functionExpected = defn.isFunctionType(ptNorm)
+      def resultMatch = constrainResult(tree.symbol, wtp, followAlias(pt))
       def needsEta = pt match {
         case _: SingletonType => false
         case IgnoredProto(_: FunOrPolyProto) => false
         case _ => true
       }
+      var resMatch: Boolean = false
       wtp match {
         case wtp: ExprType =>
           readaptSimplified(tree.withType(wtp.resultType))
-        case wtp: MethodType if wtp.isImplicitMethod && (resultMatch || !functionExpected) =>
+        case wtp: MethodType if wtp.isImplicitMethod &&
+          ({ resMatch = resultMatch; resMatch } || !functionExpected) =>
           if (resultMatch || ctx.mode.is(Mode.ImplicitsEnabled)) adaptNoArgsImplicitMethod(wtp)
           else {
             // Don't proceed with implicit search if result type cannot match - the search
@@ -2586,8 +2588,9 @@ class Typer extends Namer
             err.typeMismatch(tree, pt)
           }
         case wtp: MethodType if needsEta =>
+          val funExpected = functionExpected
           val arity =
-            if (functionExpected)
+            if (funExpected)
               if (!isFullyDefined(pt, ForceDegree.none) && isFullyDefined(wtp, ForceDegree.none))
                 // if method type is fully defined, but expected type is not,
                 // prioritize method parameter types as parameter types of the eta-expanded closure
@@ -2598,7 +2601,7 @@ class Typer extends Namer
               if (nparams > 0 || pt.eq(AnyFunctionProto)) nparams
               else -1 // no eta expansion in this case
             }
-            adaptNoArgsUnappliedMethod(wtp, functionExpected, arity)
+            adaptNoArgsUnappliedMethod(wtp, funExpected, arity)
         case _ =>
           adaptNoArgsOther(wtp)
       }
