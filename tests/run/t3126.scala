8823c43d60c8a903c2b6cbc9490c4806a46404cc
Hoang Phan <phan.minhhoang2606@gmail.com>
2019-01-22 07:48:48 -0500

    Modified test

--- tests/run/t3126.scala
+++ tests/run/t3126.scala
@@ -1,9 +1,12 @@
 object Test {
   case class C(x: Int)
-  val v: Some[Int] = null
+  val v1: Some[Int] | Null = null
+  val v2: Some[Int] = null.asInstanceOf[Some[Int]]
 
   def main(args: Array[String]): Unit = {
-    try C.unapply(null) catch { case _: MatchError => }
-    try v match { case Some(1) => } catch { case _: MatchError => }
+    try C.unapply(null.asInstanceOf[C]) catch { case _: MatchError => }
+    try (v1: @unchecked) match { case Some(1) => } catch { case _: MatchError => }
+    try (v2: @unchecked) match { case Some(1) => } catch { case _: NullPointerException => }
   }
 }
+
