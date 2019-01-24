9f68f86f93e5bceb84157b44ffd6f3e272439968
Nicolas Stucki <nicolas.stucki@gmail.com>
2019-01-24 07:28:00 +0100

    Add more tests for #209

--- tests/run/i209.scala
+++ tests/run/i209.scala
@@ -6,6 +6,15 @@ case class Bar(a: Int, b: Int) {
   def copy(i: Int = 4, j: Int = 6): Bar = Bar(i, j)
 }
 
+case class Baz(a: Int) {
+  def copy(i: Int): Baz = Baz(2 * i)
+}
+
+case class PBaz(a: Int) {
+  private def copy(i: Int): PBaz = PBaz(2 * i)
+  def copy2(i: Int): PBaz = copy(i)
+}
+
 object Test {
   def main(args: Array[String]): Unit = {
     val a = Foo(2)
@@ -19,5 +28,13 @@ object Test {
     assert(b.copy(5) == Bar(5, 6))
     assert(b.copy(j = 5) == Bar(4, 5))
     assert(b.copy() == Bar(4, 6))
+
+    val c = Baz(2)
+    assert(c == Baz(2))
+    assert(c.copy(3) == Baz(6))
+
+    val d = PBaz(2)
+    assert(d == PBaz(2))
+    assert(d.copy2(3) == PBaz(6))
   }
 }
