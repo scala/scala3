ee41fbc47437c9f9e933d3b35022fe2738804c61
Dotty CI <stephane.micheloud@gmail.com>
2019-01-29 17:00:33 +0100

    removed change in ScriptedTests.scala

--- compiler/test/dotty/tools/repl/ScriptedTests.scala
+++ compiler/test/dotty/tools/repl/ScriptedTests.scala
@@ -60,16 +60,6 @@ class ScriptedTests extends ReplTest with MessageRendering {
         case nonEmptyLine => nonEmptyLine :: Nil
       }
 
-    def printOutput(prefix: String, output: String): Unit = {
-      val bytes = output.getBytes
-      println(prefix+
-        " (#chars="+output.length+
-        ", #CR="+(bytes.count(_ == 13))+
-        ", #LF="+(bytes.count(_ == 10))+
-        ") ========>"+EOL+
-        output.replaceAll("\r", "<CR>").replaceAll("\n", "<LF>"+EOL))
-    }
- 
     val expectedOutput =
       Source.fromFile(f, "UTF-8").getLines().flatMap(filterEmpties).mkString(EOL)
     val actualOutput = {
@@ -89,8 +79,10 @@ class ScriptedTests extends ReplTest with MessageRendering {
     }
 
     if (expectedOutput != actualOutput) {
-      printOutput("expected", expectedOutput)
-      printOutput("actual", actualOutput)
+      println("expected =========>")
+      println(expectedOutput)
+      println("actual ===========>")
+      println(actualOutput)
 
       fail(s"Error in file $f, expected output did not match actual")
     }
