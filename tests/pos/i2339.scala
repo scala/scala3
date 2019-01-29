075f52f696a1cd2606a36de266922c6e49e80a5d
Guillaume Martres <smarter@ubuntu.com>
2019-01-29 19:53:32 +0100

    Fix build: ImplicitConverter has been renamed Conversion

--- tests/pos/i2339.scala
+++ tests/pos/i2339.scala
@@ -1,6 +1,6 @@
 import scala.collection.mutable
 
-case class Foo[K, V]()(implicit conv: ImplicitConverter[V, Ordered[V]])
+case class Foo[K, V]()(implicit conv: Conversion[V, Ordered[V]])
 extends mutable.HashMap[K,V] {
 
   val a = this.toSeq.sortWith { case ((_, v1), (_, v2)) => v1 > v2 }
