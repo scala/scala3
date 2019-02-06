7b7838dc0012e8c4d6de49f2944c324c0d50c659
Liu Fengyun <liu@fengy.me>
2019-02-06 14:24:48 +0100

    Add test for Type[T].show

---
+++ tests/run/type-show/Macro_1.scala
@@ -0,0 +1,11 @@
+import scala.quoted._
+import scala.tasty._
+
+object TypeToolbox {
+  inline def show[A]: String = ~showImpl('[A])
+  private def showImpl[A, B](a: Type[A])(implicit refl: Reflection): Expr[String] = {
+    import refl._
+    import scala.quoted.Toolbox.Default._
+    a.show.toExpr
+  }
+}
---
+++ tests/run/type-show/Test_2.scala
@@ -0,0 +1,21 @@
+
+object Test {
+  import TypeToolbox._
+  def main(args: Array[String]): Unit = {
+    val x = 5
+    println(show[Nil.type])
+    println(show[Int => Int])
+    println(show[(Int, String)])
+    println(show[x.type])
+    assert(show[x.type] == "x.type")
+    assert(show[Nil.type] == "scala.Nil.type")
+    assert(show[Int] == "scala.Int")
+    assert(show[Int => Int] == "scala.Function1[scala.Int, scala.Int]")
+    assert(show[(Int, String)] == "scala.Tuple2[scala.Int, scala.Predef.String]")
+
+    // TODO: more complex types:
+    //  - implicit function types
+    //  - dependent function types
+    //  - refinement types
+  }
+}
