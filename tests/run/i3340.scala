2793b75971ff9c7e874a5c57c6b6c8be96e6d15b
Nicolas Stucki <nicolas.stucki@gmail.com>
2019-02-07 16:53:13 +0100

    Fix #3340: Always throw when casting to scala.Nothing
    Also fix #4410

---
+++ tests/run/i3340.scala
@@ -0,0 +1,45 @@
+object Test {
+  def main(args: Array[String]): Unit = {
+    printlnStackLine(f1)
+    printlnStackLine(f2)
+    printlnStackLine(f3)
+    printlnStackLine(f4)
+    printlnStackLine(f5)
+  }
+
+  def f1: Unit = {
+    val a: Nothing =
+      null.asInstanceOf[Nothing] // throws here
+  }
+
+  def f2: Unit = {
+    null.asInstanceOf[Nothing] // throws here
+  }
+
+  def f3: Unit = {
+    null.asInstanceOf[Nothing] // throws here
+    ()
+  }
+
+
+  def f4: Unit = {
+    val n: Any = null
+    n.asInstanceOf[Nothing] // throws here
+    ()
+  }
+
+  def f5: Unit = {
+    val n: Any = null
+    val a: Nothing =
+      n.asInstanceOf[Nothing] // throws here
+    ()
+  }
+
+  def printlnStackLine(t: => Any): Unit = {
+    try t
+    catch {
+      case e: ClassCastException =>
+        println(e.getStackTrace.head)
+    }
+  }
+}
