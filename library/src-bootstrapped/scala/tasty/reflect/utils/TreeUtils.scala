7e433cf08b322c80c834d8bbaceed9f518796714
Liu Fengyun <liu@fengy.me>
2019-02-14 16:15:38 +0100

    Fix rebase errors

--- library/src-bootstrapped/scala/tasty/reflect/utils/TreeUtils.scala
+++ library/src-bootstrapped/scala/tasty/reflect/utils/TreeUtils.scala
@@ -9,23 +9,17 @@ trait TreeUtils {
   import reflect._
 
   /** Bind the `rhs` to a `val` and use it in `body` */
-  def let(rhs: Term)(bodyType: Type)(body: Term.Ident => Term): Term = {
-    // Recover all lost type information
+  def let(rhs: Term)(body: Term.Ident => Term): Term = {
     type T // TODO probably it is better to use the Sealed contruct rather than let the user create their own existential type
-    type U // TODO probably it is better to use the Sealed contruct rather than let the user create their own existential type
-    implicit val bodyTpe: quoted.Type[U] = bodyType.seal.asInstanceOf[quoted.Type[U]]
     implicit val rhsTpe: quoted.Type[T] = rhs.tpe.seal.asInstanceOf[quoted.Type[T]]
     val rhsExpr = rhs.seal[T]
-
-    let[T, U](rhsExpr) { x =>
-      val id = ('(x)).unseal.asInstanceOf[Term.Ident]
-      id.withPos(rhs.pos)
-      body(id).seal[U]
-    }.unseal
-  }
-
-  private def let[T: quoted.Type, U: quoted.Type](rhs: Expr[T])(in: Expr[T] => Expr[U]): Expr[U] = '{
-    val x = ~rhs
-    ~in('(x))
+    val expr = '{
+      val x = ~rhsExpr
+      ~{
+        val id = ('(x)).unseal.asInstanceOf[Term.Ident]
+        body(id).seal[Any]
+      }
+    }
+    expr.unseal
   }
 }
