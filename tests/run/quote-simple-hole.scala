26ca22111d7fbc8ea3cc2c2209f0d403e1c3a425
Nicolas Stucki <nicolas.stucki@gmail.com>
2019-02-18 12:55:08 +0100

    Add regression tests

--- tests/run/quote-simple-hole.scala
+++ tests/run/quote-simple-hole.scala
@@ -3,8 +3,12 @@ object Test {
     val x = '{0}
     val y = '{$x}
     val z = '{${'{$y}}}
+    val a = '{{{$x}}}
+    val b = '{{${{'{{$y}}}}}}
     assert(x eq y)
     assert(x eq z)
+    assert(x eq a)
+    assert(x eq b)
 
     val i = '[Int]
     val j = '[$i]
