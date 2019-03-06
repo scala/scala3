8827eff59fd82a2b1d4af522434d39e441d419bc
Olivier Blanvillain <olivier.blanvillain@gmail.com>
2019-03-06 11:01:22 +0100

    Revert "Flag ChildrenQueried in hasAnonymousChild"
    This reverts commit bb1515eb6514eaf2277e9dbe872fc0a07bc4d914.

--- compiler/src/dotty/tools/dotc/transform/SymUtils.scala
+++ compiler/src/dotty/tools/dotc/transform/SymUtils.scala
@@ -145,11 +145,8 @@ class SymUtils(val self: Symbol) extends AnyVal {
     }.reverse
   }
 
-  def hasAnonymousChild(implicit ctx: Context): Boolean = {
-    if (self.isType)
-      self.setFlag(ChildrenQueried)
+  def hasAnonymousChild(implicit ctx: Context): Boolean =
     children.exists(_ `eq` self)
-  }
 
   /** Is symbol directly or indirectly owned by a term symbol? */
   @tailrec final def isLocal(implicit ctx: Context): Boolean = {
