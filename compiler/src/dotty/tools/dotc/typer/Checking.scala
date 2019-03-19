127ac60a72a0e613c612bc3cce4fa19f25cbd2b5
Martin Odersky <odersky@gmail.com>
2019-03-19 17:34:27 +0100

    Make `hasSimpleKind` follow aliases

--- compiler/src/dotty/tools/dotc/typer/Checking.scala
+++ compiler/src/dotty/tools/dotc/typer/Checking.scala
@@ -799,7 +799,7 @@ trait Checking {
 
   /** Check that `tpt` does not define a higher-kinded type */
   def checkSimpleKinded(tpt: Tree)(implicit ctx: Context): Tree =
-    if (!tpt.tpe.dealias.hasSimpleKind && !ctx.compilationUnit.isJava) {
+    if (!tpt.tpe.hasSimpleKind && !ctx.compilationUnit.isJava) {
         // be more lenient with missing type params in Java,
         // needed to make pos/java-interop/t1196 work.
       errorTree(tpt, MissingTypeParameterFor(tpt.tpe))
