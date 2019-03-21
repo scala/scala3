a31502d9c0ff04eb23fe5f0b01094ed52c01c2dd
Martin Odersky <odersky@gmail.com>
2019-03-21 11:37:08 +0100

    Drop FunProtoState#evalState
    With the new context merging technique it's no longer needed. We now
    propagate any updates to the context from a FuncProto.typedArg immediately
    into the caller context. So no need to check later whether the update
    is present.

--- compiler/src/dotty/tools/dotc/typer/ProtoTypes.scala
+++ compiler/src/dotty/tools/dotc/typer/ProtoTypes.scala
@@ -232,9 +232,6 @@ object ProtoTypes {
     /** A map in which typed arguments can be stored to be later integrated in `typedArgs`. */
     var typedArg: SimpleIdentityMap[untpd.Tree, Tree] = SimpleIdentityMap.Empty
 
-    /** A map recording the typer states and constraints in which arguments stored in myTypedArg were typed */
-    var evalState: SimpleIdentityMap[untpd.Tree, (TyperState, Constraint)] = SimpleIdentityMap.Empty
-
     /** The tupled version of this prototype, if it has been computed */
     var tupled: Type = NoType
 
@@ -265,25 +262,10 @@ object ProtoTypes {
 
     override def notApplied: Type = WildcardType
 
-    /** Forget the types of any arguments that have been typed producing a constraint
-     *    - that is in a typer state that is not yet committed into the one of the current context `ctx`,
-     *    - or that has been retracted from its typestate because oif a failed operation.
-     *  This is necessary to avoid "orphan" TypeParamRefs that are referred to from
-     *  type variables in the typed arguments, but that are not registered in the
-     *  current constraint. Test cases are pos/t1756.scala and pos/i3538.scala.
-     *  @return True if all arguments have types (in particular, no types were forgotten).
+    /** @return True if all arguments have types.
      */
-    def allArgTypesAreCurrent()(implicit ctx: Context): Boolean = {
-      state.evalState foreachBinding { (arg, tstateConstr) =>
-        if ((tstateConstr._1.uncommittedAncestor.constraint `ne` ctx.typerState.constraint) ||
-            tstateConstr._2.isRetracted) {
-          typr.println(i"need to invalidate $arg / ${state.typedArg(arg)}, ${tstateConstr._2}, current = ${ctx.typerState.constraint}")
-          state.typedArg = state.typedArg.remove(arg)
-          state.evalState = state.evalState.remove(arg)
-        }
-      }
+    def allArgTypesAreCurrent()(implicit ctx: Context): Boolean =
       state.typedArg.size == args.length
-    }
 
     private def isUndefined(tp: Type): Boolean = tp match {
       case _: WildcardType => true
@@ -307,15 +289,8 @@ object ProtoTypes {
             targ = arg.withType(WildcardType)
           case _ =>
             targ = typerFn(arg)
-            if (!ctx.reporter.hasUnreportedErrors) {
-              // FIXME: This can swallow warnings by updating the typerstate from a nested
-              // context that gets discarded later. But we do have to update the
-              // typerstate if there are no errors. If we also omitted the next two lines
-              // when warning were emitted, `pos/t1756.scala` would fail when run with -feature.
-              // It would produce an orphan type parameter for CI when pickling.
+            if (!ctx.reporter.hasUnreportedErrors)
               state.typedArg = state.typedArg.updated(arg, targ)
-              state.evalState = state.evalState.updated(arg, (ctx.typerState, ctx.typerState.constraint))
-            }
         }
       }
       targ
