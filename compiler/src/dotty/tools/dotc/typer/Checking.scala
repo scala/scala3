861e59bf51144e272de3598ca76691f1563b2c75
Martin Odersky <odersky@gmail.com>
2019-03-23 18:25:55 +0100

    Revert "Use substApprox for F-bounds instantiation"
    This reverts commit 48f7ed677d9256ee19b09377b839a5292438dd2d.

--- compiler/src/dotty/tools/dotc/typer/Checking.scala
+++ compiler/src/dotty/tools/dotc/typer/Checking.scala
@@ -75,12 +75,8 @@ object Checking {
     }.getOrElse(TypeTree(tparam.paramRef))
     val orderedArgs = if (hasNamedArg(args)) tparams.map(argNamed) else args
     val bounds = tparams.map(_.paramInfoAsSeenFrom(tree.tpe).bounds)
-    def instantiate(bound: Type, args: List[Type]) = tparams match {
-      case (_: Symbol) :: _ if args.exists(_.isInstanceOf[TypeBounds]) =>
-        bound.substApprox(tparams.asInstanceOf[List[Symbol]], args)
-      case _ =>
-        HKTypeLambda.fromParams(tparams, bound).appliedTo(args)
-    }
+    def instantiate(bound: Type, args: List[Type]) =
+      HKTypeLambda.fromParams(tparams, bound).appliedTo(args)
     if (boundsCheck) checkBounds(orderedArgs, bounds, instantiate)
 
     def checkWildcardApply(tp: Type): Unit = tp match {
