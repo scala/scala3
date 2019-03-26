f4fdb221b798bdab47c0eed4932f8cb216b85eba
Nicolas Stucki <nicolas.stucki@gmail.com>
2019-03-26 14:54:36 +0100

    Add String interpolation position regression test

---
+++ tests/neg/tasty-string-interpolator-position-a/Macro_1.scala
@@ -0,0 +1,21 @@
+import scala.quoted._
+import scala.tasty.Reflection
+import scala.language.implicitConversions
+import scala.quoted.Exprs.LiftedExpr
+import scala.quoted.Toolbox.Default._
+
+object Macro {
+
+  implicit inline def (strCtx: => StringContext) f2 (args: =>Any*): String = ${FIntepolator.apply('strCtx, 'args)}
+
+}
+
+object FIntepolator {
+
+  def apply(strCtxExpr: Expr[StringContext], argsExpr: Expr[Seq[Any]])(implicit reflect: Reflection): Expr[String] = {
+    import reflect._
+    error("there are no parts", strCtxExpr.unseal.underlyingArgument.pos)
+    '{ ($strCtxExpr).s($argsExpr: _*) }
+  }
+
+}
