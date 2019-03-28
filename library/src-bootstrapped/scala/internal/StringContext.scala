89070aebc7df4ec5e3c374711cd5c365305bdcac
Nicolas Stucki <nicolas.stucki@gmail.com>
2019-03-28 09:06:25 +0100

    Fix implementation of f interpolation macro

--- library/src-bootstrapped/scala/internal/StringContext.scala
+++ library/src-bootstrapped/scala/internal/StringContext.scala
@@ -7,9 +7,15 @@ object StringContext {
   inline def f(sc: => scala.StringContext)(args: Any*): String = ${ fImpl('sc, 'args) }
 
   private def fImpl(sc: Expr[StringContext], args: Expr[Seq[Any]]): Expr[String] = {
-    // TODO implement f interpolation checks and addapt sc.parts
+    // TODO implement f interpolation checks and generate optimal code
     //   See https://github.com/alemannosara/f-interpolators-in-Dotty-macros
-    '{ $sc.parts.mkString.format($args: _*) }
+    '{
+      // Purely runtime implementation of the f interpolation without any checks
+      val parts = $sc.parts.toList
+      assert(parts.nonEmpty, "StringContext should have non empty parts")
+      val parts2 = parts.head :: parts.tail.map(x => if (x.startsWith("%s")) x else "%s" + x)
+      parts2.mkString.format($args: _*)
+    }
   }
 
 }
--- tests/run-with-compiler/f-interpolator.scala
+++ tests/run-with-compiler/f-interpolator.scala
@@ -2,6 +2,7 @@
 object Test {
   def main(args: Array[String]): Unit = {
     println(f"abc")
-    println(f"Hello ${"world"}%s")
+    println(f"Hello ${"world"}%s!")
+    println(f"Hello ${"world"}!")
   }
 }
