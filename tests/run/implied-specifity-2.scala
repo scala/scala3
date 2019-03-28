f307da22d4dfe1bae306d3176f727d47431a5d9a
Martin Odersky <odersky@gmail.com>
2019-03-28 17:42:33 +0100

    Add test cases

--- tests/run/implied-specifity-2.scala
+++ tests/run/implied-specifity-2.scala
@@ -36,9 +36,20 @@ class Bam(val str: String)
 implied lo given Low for Bam("lo")
 implied hi given High given Arg for Bam("hi")
 
+class Bam2(val str: String)
+implied lo2 given Low for Bam2("lo")
+implied mid2 given High given Arg for Bam2("mid")
+implied hi2 for Bam2("hi")
+
+class Red(val str: String)
+implied normal given Arg for Red("normal")
+implied reduced given Arg given Low for Red("reduced")
+
 object Test extends App {
   assert(Foo[Int] == 0)
   assert(Foo[Bar[Int]] == 3)
   assert(Foo[Bar[Baz]] == 5)
   assert(the[Bam].str == "hi")
+  assert(the[Bam2].str == "hi")
+  //assert(the[Red].str == "normal")
 }
