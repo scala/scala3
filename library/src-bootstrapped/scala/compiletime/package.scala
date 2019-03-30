3530ea734e00f2edca697c6f9f4fde04e366973b
Martin Odersky <odersky@gmail.com>
2019-03-30 10:59:03 +0100

    Add findImplied to compiletime package

--- library/src-bootstrapped/scala/compiletime/package.scala
+++ library/src-bootstrapped/scala/compiletime/package.scala
@@ -11,4 +11,9 @@ package object compiletime {
   inline def constValue[T]: T = ???
 
   type S[X <: Int] <: Int
+
+  inline def findImplied[T] given (ev: (T | Null) = null): Option[ev.type] = inline ev match {
+    case _: Null => None
+    case _ => Some(ev)
+  }
 }
