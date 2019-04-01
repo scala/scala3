ee907d04a1ac17c0d1901ebf7031766cac645fbb
Martin Odersky <odersky@gmail.com>
2019-04-01 21:14:26 +0200

    Fix pruning when adding a constraint
    The variance in the code was exactly the opposite of what was claimed
    in the comment. The reasoning in the comment was right.

--- tests/run/implicit-functors.scala
+++ tests/run/implicit-functors.scala
@@ -7,19 +7,19 @@ import Utils._
 
 class Instances[F[_[_]], T[_]]
 
-trait Functor[F[_]]
+class Functor[F[_]](val kind: String)
 
 object Functor {
-  implicit val functorId: Functor[Id] = { println("functorId"); null }
-  implicit def functorGen[F[_]](implicit inst: Instances[Functor, F]): Functor[F] = { println("funcorGen"); null }
-  implicit def functorConst[T]: Functor[Const[T]] =  { println("funcorConst"); null }
+  implicit val functorId: Functor[Id] = new Functor[Id]("id")
+  implicit def functorGen[F[_]](implicit inst: Instances[Functor, F]): Functor[F] = new Functor[F]("gen")
+  implicit def functorConst[T]: Functor[Const[T]] = new Functor[Const[T]]("const")
 }
 
 case class Sm[A](value: A)
 object Sm {
-  implicit def smInstances[F[_[_]]]: Instances[F, Sm] = { println("smInstances"); null }
+  implicit def smInstances[F[_[_]]]: Instances[F, Sm] = null
 }
 
 object Test extends App {
-  implicitly[Functor[Sm]]
+  assert(implicitly[Functor[Sm]].kind == "gen")
 }
