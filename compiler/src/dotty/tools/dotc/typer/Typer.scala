1a9ec2357ec78e00efe135114736f75d561a0ba6
Martin Odersky <odersky@gmail.com>
2019-04-02 11:30:51 +0200

    Simplification: Inline givenReflection

--- compiler/src/dotty/tools/dotc/typer/Typer.scala
+++ compiler/src/dotty/tools/dotc/typer/Typer.scala
@@ -1948,7 +1948,7 @@ class Typer extends Namer
             fun = ref(defn.QuotedMatcher_unapplyR).appliedToType(patType),
             implicits =
               ref(defn.InternalQuoted_exprQuoteR).appliedToType(shape.tpe).appliedTo(shape) ::
-              givenReflection(tree.span) :: Nil,
+              implicitArgTree(defn.TastyReflectionType, tree.span) :: Nil,
             patterns = splicePat :: Nil,
             proto = pt)
         }
@@ -1978,9 +1978,6 @@ class Typer extends Namer
   /** A hole the shape pattern of a quoted.Matcher.unapply, representing a splice */
   def patternHole(splice: Tree)(implicit ctx: Context): Tree =
     ref(defn.InternalQuoted_patternHoleR).appliedToType(splice.tpe).withSpan(splice.span)
-
-  def givenReflection(span: Span)(implicit ctx: Context): Tree =
-    implicitArgTree(defn.TastyReflectionType, span)
 
   /** Translate `${ t: Expr[T] }` into expression `t.splice` while tracking the quotation level in the context */
   def typedSplice(tree: untpd.Splice, pt: Type)(implicit ctx: Context): Tree = track("typedSplice") {
