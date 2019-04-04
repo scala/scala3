aa10a37afe13384f0776977c3723bae3a8c53a4e
Aleksander Boruch-Gruszecki <aleksander.boruch-gruszecki@epfl.ch>
2019-04-04 18:03:51 +0200

    Fix #4523: ensure we generate no bytecode for inline

---
+++ tests/run/i4523.scala
@@ -0,0 +1,16 @@
+class C {
+  inline def one = 1
+  def foo = {
+    def notfive(i: Int) = println(i)
+    inline def five = one + 4
+    Seq(5).map { i =>
+      i + five
+    }
+  }
+}
+
+object Test {
+  def main(args: Array[String]): Unit = {
+    classOf[C].getDeclaredMethods.map(_.toString).sorted.foreach(println)
+  }
+}
