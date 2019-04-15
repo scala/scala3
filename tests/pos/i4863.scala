ebfb238ba1f80a77479f96e93571e748bee4a655
Liu Fengyun <liu@fengy.me>
2019-04-15 17:10:22 +0200

    Fix #4863: add tests

---
+++ tests/pos/i4863.scala
@@ -0,0 +1,13 @@
+sealed trait Nat
+case class S(n: Nat) extends Nat
+case object Z extends Nat
+
+inline def pred(n: Nat) = inline n match {
+  case S(m) => m
+  case Z =>
+    compiletime.error("n cannot be Z")
+}
+
+class Test {
+  pred(S(Z))
+}
