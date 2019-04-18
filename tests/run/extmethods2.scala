3eb8a788c19c6a70d98b382a22ec1c07dd88bcf1
Martin Odersky <odersky@gmail.com>
2019-04-18 17:59:45 +0200

    Fix test

--- tests/run/extmethods2.scala
+++ tests/run/extmethods2.scala
@@ -5,12 +5,12 @@ object Test extends App {
   implied StringListOps given TC {
     type T = List[String]
     def (x: T) foo (y: T) = (x ++ y, the[TC])
-    def (x: T) bar (y: Int) = (x(y), the[TC])
+    def (x: T) bar (y: Int) = (x(0)(y), the[TC])
   }
 
   def test given TC = {
     assert(List("abc").foo(List("def"))._1 == List("abc", "def"))
-    assert(List("abc").bar(2)._1 == "c")
+    assert(List("abc").bar(2)._1 == 'c')
   }
 
   test given TC()
