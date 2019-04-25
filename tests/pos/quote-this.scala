ed9858189f843eeb25eb345a628a7e60e00ca431
Nicolas Stucki <nicolas.stucki@gmail.com>
2019-04-25 17:58:22 +0200

    Re-enable tests

--- tests/pos/quote-this.scala
+++ tests/pos/quote-this.scala
@@ -1,24 +1,25 @@
 import scala.quoted._
 
 class Foo {
-//  def a: Expr[Int] = '{1}
-//  def b: Expr[Int] = '{
-//    ${ this.a }
-//  }
-//
-//  def d: Expr[Expr[Int]] = '{ '{1} }
-//  def e: Expr[Expr[Int]] = '{
-//    '{${${this.d}}}
-//  }
-//
-//  def foo[T](x: T): T = x
-//
-//  def f = '{
-//    ${ foo[this.type](this).a }
-//  }
+  def a: Expr[Int] = '{1}
+  def b: Expr[Int] = '{
+    ${ this.a }
+  }
+
+  def d: Expr[Expr[Int]] = '{ '{1} }
+  def e: Expr[Expr[Int]] = '{
+    '{${${this.d}}}
+  }
+
+  def foo[T](x: T): T = x
+
+  def f = '{
+    ${ foo[this.type](this).a }
+  }
 
   inline def g(): Unit = ${ Foo.impl[this.type](1) }
   inline def h(): Unit = ${ Foo.impl[Any]('this) }
+  // FIXME
 //  inline def i(that: Foo): Unit = ${ Foo.impl[that.type](1) }
 
 }
