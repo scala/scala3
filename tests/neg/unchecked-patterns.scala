ce35deb37ab7bb91ce973cfaf6f8f956563b6808
Martin Odersky <odersky@gmail.com>
2019-04-28 21:17:56 +0200

    Error on narrowing pattern definitions

--- tests/neg/unchecked-patterns.scala
+++ tests/neg/unchecked-patterns.scala
@@ -1,4 +1,6 @@
 object Test {
   val (y1: Some[Int] @unchecked) = Some(1): Option[Int]  // OK
   val y2: Some[Int] @unchecked = Some(1): Option[Int]    // error
+
+  val x :: xs = List(1, 2, 3)  // error
 }
