deeb2b9be14b1fcad0e19bd1b1e861f12ec57374
Nicolas Stucki <nicolas.stucki@gmail.com>
2019-05-03 17:03:07 +0200

    Revert "Fix #4369: Add regression test"
    This reverts commit 5ee64d5746095176cfcd204d648cfd987dd81935.

--- tests/neg/i4369.scala
+++
@@ -1,4 +0,0 @@
-trait X[R <: Z, Z >: X[R, R]] {
-   def foo(x: X[R, R]): Z = x // error: Found: X[R, R](x)    Required: Z
-}
-class Z extends X[Z, Z]
--- tests/neg/i4369c.scala
+++
@@ -1,5 +0,0 @@
-trait X { self =>
-  type R <: Z
-  type Z >: X { type R = self.R; type Z = self.R }
-}
-class Foo extends X { type R = Foo; type Z = Foo } // error
