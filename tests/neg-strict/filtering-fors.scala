af921ed582122bb719005874b61bf2c24ececd22
Martin Odersky <odersky@gmail.com>
2019-05-04 15:57:21 +0200

    Add doc page

--- tests/neg-strict/filtering-fors.scala
+++ tests/neg-strict/filtering-fors.scala
@@ -16,6 +16,9 @@ object Test {
   for (case (x: String) <- xs; (y, z) <- xs) do () // error
   for ((x: String) <- xs; case (y, z) <- xs) do () // error
 
+  val pairs: List[Any] = List((1, 2), (3, 4))
+  for ((x, y) <- pairs) yield (y, x) // error
+
   for (case x: String <- xs) do ()      // OK
   for (case (x: String) <- xs) do ()    // OK
   for (case y@ (x: String) <- xs) do () // OK
@@ -24,4 +27,6 @@ object Test {
   for (case (x: String) <- xs if x.isEmpty) do ()   // OK
   for (case (x: String) <- xs; y = x) do ()          // OK
   for (case (x: String) <- xs; case (y, z) <- xs) do ()   // OK
+
+  for (case (x, y) <- pairs) yield (y, x) // OK
 }
