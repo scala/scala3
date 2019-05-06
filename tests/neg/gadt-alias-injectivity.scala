966d9e08ba6e194ba78bac422be31bcd36bc59ff
Aleksander Boruch-Gruszecki <aleksander.boruch-gruszecki@epfl.ch>
2019-05-06 09:47:01 +0200

    Also check injectivity handling for abstract types

--- tests/neg/gadt-alias-injectivity.scala
+++ tests/neg/gadt-alias-injectivity.scala
@@ -31,4 +31,18 @@ object Test {
       case Refl() => x // error
     }
   }
+
+  trait E {
+    type Foo[+X] <: Int | (X, X)
+    def foo[X, Y](x: X, eq: EQ[Foo[X], Foo[Y]]): Y = eq match {
+      case Refl() => x // error
+    }
+  }
+
+  trait F {
+    type Foo[X] >: Int | (X, X)
+    def foo[X, Y](x: X, eq: EQ[Foo[X], Foo[Y]]): Y = eq match {
+      case Refl() => x // error
+    }
+  }
 }
