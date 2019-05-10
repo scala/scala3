3e52756f18b982ae8e641fd6e84707bbe0c73d3c
Bryan Abate <bryan.abate@gmail.com>
2019-05-10 13:08:09 +0200

    Done TODO in Main

--- tastydoc/src/dotty/tastydoc/Main.scala
+++ tastydoc/src/dotty/tastydoc/Main.scala
@@ -5,8 +5,16 @@ import scala.tasty.file._
 
 object Main {
   def main(args: Array[String]): Unit = {
-    val extraClasspath = "." // TODO allow to set it from the args with -classpath XYZ
-    val classes = args.toList
+
+    val (extraClasspath, classes) = {
+      val idx = args.indexOf("-classpath")
+      if(idx >= 0 && args.size > idx + 1){
+        (args(idx + 1), args.filter(_!=(args(idx + 1))).toList)
+      }else{
+        (".", args.toList)
+      }
+    }
+
     if (args.isEmpty) {
       println("Dotty Tastydoc: No classes where passed as argument")
     } else {
--- tastydoc/test/dotty/tastydoc/Tests.scala
+++ tastydoc/test/dotty/tastydoc/Tests.scala
@@ -51,21 +51,21 @@ class Tests {
   // }
   @Test def testDocumentation(): Unit = {
     ConsumeTasty(
-      "tastydoc/out/bootstrap/dotty-tastydoc-input/scala-0.14/classes",
+      "tastydoc/out/bootstrap/dotty-tastydoc-input/scala-0.15/classes",
       List("example.level2.Documentation"),
       new dotty.tastydoc.TastydocConsumer
     )
   }
   @Test def testReturnTypeClass(): Unit = {
     ConsumeTasty(
-      "tastydoc/out/bootstrap/dotty-tastydoc-input/scala-0.14/classes",
+      "tastydoc/out/bootstrap/dotty-tastydoc-input/scala-0.15/classes",
       List("example.ReturnTypeClass"),
       new dotty.tastydoc.TastydocConsumer
     )
   }
   // @Test def testListFromLib(): Unit = {
   //   ConsumeTasty(
-  //     "tastydoc/out/bootstrap/dotty-tastydoc-input/scala-0.14/classes",
+  //     "tastydoc/out/bootstrap/dotty-tastydoc-input/scala-0.15/classes",
   //     List("scala.collection.immutable.List"),
   //     new dotty.tastydoc.TastydocConsumer
   //   )
