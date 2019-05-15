2ddbb6328edf63500800341edca0c0693715f431
Aleksander Boruch-Gruszecki <aleksander.boruch-gruszecki@epfl.ch>
2019-05-15 11:54:26 +0200

    [CI only] Demonstrate asploding TreeAccumulator

---
+++ tests/run-with-compiler/tasty-consumer-check/Test.scala
@@ -0,0 +1,28 @@
+import scala.tasty.Reflection
+import scala.tasty.file._
+
+object Test {
+  def main(args: Array[String]): Unit = {
+    ConsumeTasty("", List("scala.collection.immutable.List"), new DBConsumer)
+  }
+}
+
+class DBConsumer extends TastyConsumer {
+
+  final def apply(reflect: Reflection)(root: reflect.Tree): Unit = {
+    import reflect._
+    object Traverser extends TreeTraverser {
+
+      override def traverseTree(tree: Tree)(implicit ctx: Context): Unit = tree match {
+        case IsClassDef(tree) =>
+          // println(tree.symbol.methods)
+          super.traverseTree(tree)
+        case tree =>
+          super.traverseTree(tree)
+      }
+
+    }
+    Traverser.traverseTree(root)(reflect.rootContext)
+  }
+
+}
