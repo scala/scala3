2d001bfb0b01f5dc66acce621316bc094461a105
Guillaume Martres <smarter@ubuntu.com>
2019-05-22 16:14:16 +0200

    Fix error message when overriding with a lazy val

--- compiler/src/dotty/tools/dotc/typer/RefChecks.scala
+++ compiler/src/dotty/tools/dotc/typer/RefChecks.scala
@@ -388,7 +388,7 @@ object RefChecks {
       } else if (member.is(ModuleVal) && !other.isRealMethod && !other.is(Deferred | Lazy)) {
         overrideError("may not override a concrete non-lazy value")
       } else if (member.is(Lazy, butNot = Module) && !other.isRealMethod && !other.is(Lazy) &&
-                 !ctx.testScala2Mode("may not override a non-lazy value", member.sourcePos)) {
+                 !ctx.testScala2Mode(overrideErrorMsg("may not override a non-lazy value"), member.sourcePos)) {
         overrideError("may not override a non-lazy value")
       } else if (other.is(Lazy) && !other.isRealMethod && !member.is(Lazy)) {
         overrideError("must be declared lazy to override a lazy value")
