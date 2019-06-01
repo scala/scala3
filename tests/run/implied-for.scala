c8ad8ecd90c7a9408dd23a8b437e198d443b39cc
Martin Odersky <odersky@gmail.com>
2019-06-01 12:40:40 +0200

    Add docs
    Explain by-type imports on the doc page.
    Also, add a test for wildcards in bounds.

--- tests/run/implied-for.scala
+++ tests/run/implied-for.scala
@@ -4,22 +4,22 @@ object A {
 
   class B extends T
   class C extends T
-  class D
+  class D[T]
 
   implied b for B
   implied c for C
   implied t for T
-  implied d for D
+  implied d for D[Int]
 }
 
 object Test extends App {
   import A._
-  import implied A.{t, for B, D}
+  import implied A.{t, for B, D[_]}
 
   val x1: B = b
   val x2: T = t
-  val x3: D = d
+  val x3: D[Int] = d
 
   assert(the[T].isInstanceOf[B])
-  assert(the[D].isInstanceOf[D])
+  assert(the[D[Int]].isInstanceOf[D[_]])
 }
