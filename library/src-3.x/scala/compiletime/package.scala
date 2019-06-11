3142de6ced83f6035183fcb19bc29b3400a1c4ea
Rodrigo Fernandes <rtfrodrigo@gmail.com>
2019-06-11 14:04:53 +0100

    Fix #6622: Add `code` interpolation
    Allows to get string representations for code passed in the interpolated
    values
    
    ```scala
    inline def logged(p1: => Any) = {
      val c = code"code: $p1"
      val res = p1
      (c, p1)
    }
    logged(indentity("foo"))
    ```
    
    is equivalent to:
    ```scala
    ("code: indentity("foo")", indentity("foo"))
    ```

--- library/src-3.x/scala/compiletime/package.scala
+++ library/src-3.x/scala/compiletime/package.scala
@@ -4,7 +4,23 @@ package object compiletime {
 
   erased def erasedValue[T]: T = ???
 
-  inline def error(inline msg: String, objs: Any*): Nothing = ???
+  inline def error(inline msg: String): Nothing = ???
+
+  /** Returns the string representations for code passed in the interpolated values
+   *  ```scala
+   *  inline def logged(p1: => Any) = {
+   *    val c = code"code: $p1"
+   *    val res = p1
+   *    (c, p1)
+   *  }
+   *  logged(indentity("foo"))
+   *  ```
+   *  is equivalent to:
+   *  ```scala
+   *  ("code: indentity("foo")", indentity("foo"))
+   *  ```
+   */
+  inline def (self: => StringContext) code (args: => Any*): String = ???
 
   inline def constValueOpt[T]: Option[T] = ???
 
