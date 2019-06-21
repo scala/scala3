263e02f68aa30d0e7b7930bee7ea9bd1340106ee
Liu Fengyun <liu@fengy.me>
2019-06-21 07:46:13 +0200

    Port tests from Scala2 #5207
    https://github.com/scala/scala/pull/5207

--- tests/run-bootstrapped/i6710.scala
+++ tests/run-bootstrapped/i6710.scala
@@ -19,5 +19,36 @@ object Test {
     while(x > 10.0) { x = x + 1; println(x) }
     do { x = x + 1; println(x) } while(x < 10.0)
     do { x = x + 1; println(x) } while(x > 10.0)
+
+    // tests from https://github.com/scala/scala/pull/5207
+    {
+      val n = Double.NaN
+      def ne(x: Double, y: Double) = x != y
+      val fs: List[(Double, Double) => Boolean] = List(_ < _, _ <= _, _ > _, _ >= _,  _ == _, (x, y) => !ne(x, y))
+      val vs = List[Double](n, 1, -1, 0)
+      for (f <- fs; v <- vs; (x, y) <- List((n, v), (v, n))) assert(!f(x, y))
+    }
+
+    {
+      val n = Float.NaN
+      def ne(x: Float, y: Float) = x != y
+      val fs: List[(Float, Float) => Boolean] = List(_ < _, _ <= _, _ > _, _ >= _,  _ == _, (x, y) => !ne(x, y))
+      val vs = List[Float](n, 1, -1, 0)
+      for (f <- fs; v <- vs; (x, y) <- List((n, v), (v, n))) assert(!f(x, y))
+    }
+
+    {
+      def a[T](x: T, y: T) = x == y
+      def b[T](x: T, y: T) = x != y
+      val n = Double.NaN
+      (a(n, n) :: a(n, 0) :: a (0, n) :: !b(n, n) :: !b(n, 0) :: !b(0, n) :: Nil).foreach(b => assert(!b))
+    }
+
+    {
+      def a[T](x: T, y: T) = x == y
+      def b[T](x: T, y: T) = x != y
+      val n = Float.NaN
+      (a(n, n) :: a(n, 0) :: a (0, n) :: !b(n, n) :: !b(n, 0) :: !b(0, n) :: Nil).foreach(b => assert(!b))
+    }
   }
 }
