3262acc2288eee587a467f817e99d4631198f096
Liu Fengyun <liu@fengy.me>
2019-06-29 17:21:15 +0200

    Fix tests

--- tests/neg/i2146.scala
+++ tests/neg/i2146.scala
@@ -1,8 +1,5 @@
-object Test {
-  case class A()
-  case class B()
-
-  def foo[A, B]: given A => given B => Int = { given b: B =>
-    42 // error: found Int, required: given A => given B => Int
+class Test {
+  def foo[A, B]: given A => given B => Int = { given b: B =>  // error: found Int, required: given A => given B => Int
+    42
   }
 }
