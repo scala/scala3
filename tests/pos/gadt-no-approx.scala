93379747148844634c4a3654605a39db3f813ac7
Liu Fengyun <liu@fengy.me>
2019-07-01 13:01:51 +0200

    Fix tests
    Now the code type checks as expected.

--- tests/pos/gadt-no-approx.scala
+++ tests/pos/gadt-no-approx.scala
@@ -2,9 +2,8 @@ object `gadt-no-approx` {
   def fo[U](u: U): U =
     (0 : Int) match {
       case _: u.type =>
-        val i: Int = (??? : U) // error
-        // potentially could compile
-        // val i2: Int = u
+        val i: Int = (??? : U)
+        val i2: Int = u
         u
     }
 }
