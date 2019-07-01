6ca6062accfd223214b484266da726efab80f215
odersky <odersky@gmail.com>
2019-07-01 22:57:08 +0200

    Update compiler/src/dotty/tools/dotc/typer/Typer.scala
    Co-Authored-By: Guillaume Martres <smarter@ubuntu.com>

--- compiler/src/dotty/tools/dotc/typer/Typer.scala
+++ compiler/src/dotty/tools/dotc/typer/Typer.scala
@@ -2209,7 +2209,7 @@ class Typer extends Namer
     if (!tree.denot.isOverloaded &&
           // for overloaded trees: resolve overloading before simplifying
         !tree.isInstanceOf[Applications.IntegratedTypeArgs]
-          // don't interpolatie in the middle of an extension method application
+          // don't interpolate in the middle of an extension method application
        )
       if (!tree.tpe.widen.isInstanceOf[MethodOrPoly] // wait with simplifying until method is fully applied
           || tree.isDef)                             // ... unless tree is a definition
