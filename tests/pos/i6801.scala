9c4df91f4cbfef95f937dfd4180f46437d61e93e
Martin Odersky <odersky@gmail.com>
2019-07-04 14:04:02 +0200

    Fix #6801: Add test

---
+++ tests/pos/i6801.scala
@@ -0,0 +1,5 @@
+delegate MyNumericOps[T] {
+  def (x: T) +(y: T) given (n: Numeric[T]): T = n.plus(x,y)
+}
+def foo[T: Numeric](x: T) = 1f + x // error: no implicit argument of type Numeric[Any]
+def bar[T: Numeric](x: T) = x + 1f // error: no implicit argument of type Numeric[Any]
