9a431dc0aa0ab8bc0cf103de6c35f3a2464b8158
Olivier Blanvillain <olivier.blanvillain@gmail.com>
2019-07-10 17:41:37 +0200

    Address review

--- tests/run-macros/erased-arg-macro/2.scala
+++ tests/run-macros/erased-arg-macro/2.scala
@@ -1,10 +1,12 @@
 object Test {
-  assert(Macro.foo1(1) == 0)
-  assert(Macro.foo2(1) == 0)
-  assert(Macro.foo3(1) == 0)
-  assert(Macro.foo4(1) == 0)
-  assert(Macro.foo5(1) == 0)
-  assert(Macro.foo6(1) == 0)
-  assert(Macro.foo7(1) == 0)
-  assert(Macro.foo8(1) == 0)
+  def main(args: Array[String]): Unit = {
+    assert(Macro.foo1(1) == 0)
+    assert(Macro.foo2(1) == 0)
+    assert(Macro.foo3(1) == 0)
+    assert(Macro.foo4(1) == 0)
+    assert(Macro.foo5(1) == 0)
+    assert(Macro.foo6(1) == 0)
+    assert(Macro.foo7(1) == 0)
+    assert(Macro.foo8(1) == 0)
+  }
 }
