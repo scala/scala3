7dd000ac71e46fa3ce2a33560d4210f87798dfa5
Liu Fengyun <liu@fengy.me>
2019-07-15 12:47:27 +0200

    Add test

---
+++ tests/patmat/i6490.scala
@@ -0,0 +1,10 @@
+sealed class Foo(val value: Int)
+
+object Foo {
+  def unapplySeq(foo: Foo): Seq[Int] = List(foo.value)
+}
+
+def foo(x: Foo): Unit =
+  x match {
+    case Foo(x, _: _*) => assert(x == 3)
+  }
---
+++ tests/run/i6490b.scala
@@ -0,0 +1,14 @@
+final class Foo(val value: Int)
+
+object Foo {
+  def unapplySeq(foo: Foo): (Int, Seq[Int]) = (foo.value, Nil)
+}
+
+object Test {
+  def main(args: Array[String]): Unit = {
+    (new Foo(3)) match {
+      case Foo(x, _: _*) =>
+        assert(x == 3)
+    }
+  }
+}
