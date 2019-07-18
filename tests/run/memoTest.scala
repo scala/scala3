3404445e6fa45c0d394cccfd5423a2caf73a6caa
Martin Odersky <odersky@gmail.com>
2019-07-18 23:37:07 +0200

    Expand test

--- tests/run/memoTest.scala
+++ tests/run/memoTest.scala
@@ -12,4 +12,24 @@ object Test extends App {
 
   assert(foo(1) + foo(2) == 4)
   assert(bar(1) + bar(2) == 4)
+
+  class Context(val n: Int)
+  def f(c: Context): Context = {
+    println("computing f")
+    Context(c.n + 1)
+  }
+  given as Context(0)
+
+  locally {
+    given as Context given (c: Context) = memo(f(c))
+    println(the[Context].n)
+    println(the[Context].n)
+  }
+
+  val ctx = f(the[Context])
+  locally {
+    given as Context = ctx
+    println(the[Context].n)
+    println(the[Context].n)
+  }
 }
