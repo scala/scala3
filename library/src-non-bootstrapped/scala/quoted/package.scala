81dd055aedff3dae85b1b17e88212450bd1cf98d
Nicolas Stucki <nicolas.stucki@gmail.com>
2019-07-26 09:22:57 +0200

    Use reuse toExprOfSeq and toExprOfList

--- library/src-non-bootstrapped/scala/quoted/package.scala
+++ library/src-non-bootstrapped/scala/quoted/package.scala
@@ -10,5 +10,11 @@ package object quoted {
 
   implicit object ExprOps {
     def (x: T) toExpr[T: Liftable] given QuoteContext: Expr[T] = the[Liftable[T]].toExpr(x)
+
+    def (seq: Seq[Expr[T]]) toExprOfSeq[T] given (tp: Type[T], qctx: QuoteContext): Expr[Seq[T]] =
+      throw new Exception("Non bootsrapped library")
+
+    def (list: List[Expr[T]]) toExprOfList[T] given Type[T], QuoteContext: Expr[List[T]] =
+      throw new Exception("Non bootsrapped library")
   }
 }
--- library/src/scala/quoted/Liftable.scala
+++ library/src/scala/quoted/Liftable.scala
@@ -53,17 +53,13 @@ object Liftable {
   }
 
   given [T: Type: Liftable] as Liftable[Seq[T]] = new Liftable[Seq[T]] {
-    def toExpr(seq: Seq[T]): given QuoteContext => Expr[Seq[T]] = given qctx => {
-      import qctx.tasty._
-      Repeated(seq.map(x => the[Liftable[T]].toExpr(x).unseal).toList, the[quoted.Type[T]].unseal).seal.asInstanceOf[Expr[Seq[T]]]
-    }
+    def toExpr(xs: Seq[T]): given QuoteContext => Expr[Seq[T]] =
+      xs.map(the[Liftable[T]].toExpr).toExprOfSeq
   }
 
   given [T: Type: Liftable] as Liftable[List[T]] = new Liftable[List[T]] {
-    def toExpr(x: List[T]): given QuoteContext => Expr[List[T]] = x match {
-      case x :: xs  => '{ (${xs.toExpr}).::[T](${x.toExpr}) }
-      case Nil => '{ Nil: List[T] }
-    }
+    def toExpr(xs: List[T]): given QuoteContext => Expr[List[T]] =
+      xs.map(the[Liftable[T]].toExpr).toExprOfList
   }
 
   given [T: Type: Liftable] as Liftable[Set[T]] = new Liftable[Set[T]] {
