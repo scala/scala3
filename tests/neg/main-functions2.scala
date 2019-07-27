ffea9f416c53ffc9d2af27eccc1f11bba3a685e3
Martin Odersky <odersky@gmail.com>
2019-07-27 13:56:15 +0200

    Add test

---
+++ tests/neg/main-functions2.scala
@@ -0,0 +1,11 @@
+object Test2 {
+  @main val x = 2  // error: @main annotation cannot be applied to value x
+}
+
+class Foo {
+  @main def f = () // error: method f cannot be a @main method since it cannot be accessed statically
+}
+
+@main def g(x: Int*)(y: Int*) = () // error: varargs parameter of @main method must come last
+
+@main def h[T: util.FromString](x: T) = () // error: @main method cannot have type parameters
