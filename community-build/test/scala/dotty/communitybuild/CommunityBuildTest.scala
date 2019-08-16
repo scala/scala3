b2f1eed5919a01904737070251446abf08850a5f
Jamie Thompson <bishbashboshjt@gmail.com>
2019-08-16 15:32:15 +0200

    drop scalaPB until type inference issue is resolved

--- community-build/test/scala/dotty/communitybuild/CommunityBuildTest.scala
+++ community-build/test/scala/dotty/communitybuild/CommunityBuildTest.scala
@@ -139,11 +139,12 @@ class CommunityBuildTest {
     updateCommand = "dotty-community-build/update"
   )
 
-  @Test def ScalaPB = test(
-    project       = "ScalaPB",
-    testCommand   = "dotty-community-build/compile",
-    updateCommand = "dotty-community-build/update"
-  )
+  // removing for now as the project migrated to using fastparse 2, which does not play well with type inference
+  // @Test def ScalaPB = test(
+  //   project       = "ScalaPB",
+  //   testCommand   = "dotty-community-build/compile",
+  //   updateCommand = "dotty-community-build/update"
+  // )
 
   @Test def minitest = test(
     project       = "minitest",
