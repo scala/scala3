5e49b921a110f16552891ef1ac82a6f4ce8256ad
Liu Fengyun <liu@fengy.me>
2019-08-25 19:51:08 +0200

    Format test code

--- tests/run/i7086.scala
+++ tests/run/i7086.scala
@@ -1,24 +1,24 @@
 object Test1 {
-	class A {
-  	override def toString: String = "A"
-	}
-	class B(a: A) {
-  	export a._  // OK
-	}
+  class A {
+    override def toString: String = "A"
+  }
+  class B(a: A) {
+    export a._  // OK
+  }
 }
 
 object Test extends App {
-	trait T {
-		def foo: Int = 1
+  trait T {
+    def foo: Int = 1
     def bar: Int
-	}
-	class A extends T {
-  	override def foo = 2
+  }
+  class A extends T {
+    override def foo = 2
     override def bar = 2
-	}
-	class B(a: A) extends T {
-  	export a._
-	}
+  }
+  class B(a: A) extends T {
+    export a._
+  }
   val b = B(A())
   assert(b.foo == 1)
   assert(b.bar == 2)
