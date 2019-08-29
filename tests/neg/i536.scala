1d48a9308e46e8660d4f9046f3f4b3889d2900cf
Martin Odersky <odersky@gmail.com>
2019-08-29 15:42:47 +0200

    Reclassify test
    i536.scala previously compiled since the type argument was inferred to be Nothing.
    But that inference is useless; it just hides a runtime failure. The issue #536
    only complained that the compiler crashed, so having a negative outcome
    is permissible.

--- tests/neg/i536.scala
+++ tests/neg/i536.scala
@@ -8,5 +8,5 @@ object Max {
   val m1 = max(xs)
   val m2 = max(null)
 
-  java.util.Collections.max(null)
+  java.util.Collections.max(null)  // error: Type argument Comparable[_] does not conform to upper bound Comparable[_ >: LazyRef(Comparable[_])]
 }
