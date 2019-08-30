c447ecf62c67efd5831a4fc6bdec985da5de65d2
Anatolii <anatoliykmetyuk@gmail.com>
2019-08-30 11:58:38 +0200

    Better organize release checklist

--- docs/docs/contributing/checklist.sh
+++ docs/docs/contributing/checklist.sh
@@ -10,13 +10,14 @@ next_version="0.$next.0"
 stable_branch="0.$stable.x"
 rc_branch="0.$rc.x"
 
-LIST='- [ ] Merge branch `<stable-branch>` into `master` to guarantee that all of the `<stable-branch>` commits are propagated to `master`
+LIST='- [ ] Finalize the the stable and RC releases
+  - [ ] Look at the milestone of the RC version being released. Move all the open issues from it to the next milestone.
+  - [ ] Merge branch `<stable-branch>` into `master` to guarantee that all of the `<stable-branch>` commits are propagated to `master`
 - [ ] Publish artifacts to Maven via CI. Use the following command: `bash <(curl -sL https://raw.githubusercontent.com/lampepfl/dotty/master/docs/docs/contributing/release.sh) '$stable'`
   - [ ] On branch `<stable-branch>`, set `baseVersion` to `<stable-version>` and `git tag` it as `<stable-version>`. This will publish artefacts to Sonatype and GitHub Release
   - [ ] Create branch `<rc-branch>` from `master`
   - [ ] On `<rc-branch>`, set `baseVersion` to `<rc-version>` and `git tag` it as `<rc-version>`. This will publish artefacts to Sonatype and GitHub Release.
   - [ ] On `master`, set `baseVersion` to `<next-version>`
-- [ ] Look at the milestone of the RC version being released. Move all the open issues from it to the next milestone.
 - [ ] Update `scalaVersion` (and, if applicable, the `sbt-dotty` version) in the Dotty ecosystem projects
   - [ ] https://github.com/lampepfl/dotty-example-project [![Build Status](https://travis-ci.org/lampepfl/dotty-example-project.svg?branch=master)](https://travis-ci.org/lampepfl/dotty-example-project)
     - [ ] Committed to `master`
