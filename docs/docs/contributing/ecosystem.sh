f1bb34ba80af3ef838282fedcb4809398db512e8
Anatolii <anatoliykmetyuk@gmail.com>
2019-08-30 16:59:40 +0200

    G8 template release automated

--- docs/docs/contributing/ecosystem.sh
+++ docs/docs/contributing/ecosystem.sh
@@ -114,10 +114,27 @@
       echo mill root.run # TODO un-echo
     }
 
+  #class DottyG8 extends Project:
+    function deploy_dotty.g8 {
+      git clone https://github.com/lampepfl/dotty.g8
+    }
+
+    function update_dotty.g8 {
+      local what="val\s+dottyVersion\s*=\s*\".*\""
+      local with_what="val dottyVersion = \"$rc_version\""
+
+      replace "dotty.g8/src/main/g8/build.sbt" "$what" "$with_what"
+    }
+
+    function test_dotty.g8 {
+      echo 'sbt new file://./dotty.g8 --name=foo --description=bar && cd foo && sbt run'  # TODO unecho
+    }
+
 #object Main:
   PROJECTS='
   dotty-example-project
   dotty-example-project-mill
+  dotty.g8
   '
   function main {
     export -f process
