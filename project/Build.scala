2ba130a58062d96199ee2c048f6453c9992a42c1
Guillaume Martres <smarter@ubuntu.com>
2019-09-19 13:31:02 +0200

    Upgrade to scala-library 2.13.1
    This allows us to revert 3d13696b48f616d4eeda6df9061e0f5a5b9b988e and
    71a022c1d13262995d5e117fd64f22a931d70776.

--- project/Build.scala
+++ project/Build.scala
@@ -77,8 +77,8 @@ object Build {
    *  scala-library.
    */
   def stdlibVersion(implicit mode: Mode): String = mode match {
-    case NonBootstrapped => "2.13.0"
-    case Bootstrapped => "2.13.0"
+    case NonBootstrapped => "2.13.1"
+    case Bootstrapped => "2.13.1"
   }
 
   val dottyOrganization = "ch.epfl.lamp"
