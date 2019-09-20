69f79a33b9adc0a9a65bfabe739addf93981000c
Anatolii <anatoliykmetyuk@gmail.com>
2019-09-20 15:30:07 +0200

    Set baseVersion to 0.20.0

--- project/Build.scala
+++ project/Build.scala
@@ -60,7 +60,7 @@ object MyScalaJSPlugin extends AutoPlugin {
 object Build {
   val referenceVersion = "0.19.0-bin-20190917-d724f2b-NIGHTLY"
 
-  val baseVersion = "0.19.0"
+  val baseVersion = "0.20.0"
   val baseSbtDottyVersion = "0.3.5"
 
   // Versions used by the vscode extension to create a new project
