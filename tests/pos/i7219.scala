80880d5905f96886b7d210b33b73d5a860878156
Martin Odersky <odersky@gmail.com>
2019-09-26 13:25:01 +0200

    Simplify and fix type forwarders
    The previous scheme did not work correctly for forwarding classes.
    the previous commit fixed that for unparameterized classes, but
    parameterized classes still had a problem, The current scheme
    is far simpler since it always uses unparameterized aliases.

--- tests/pos/i7219.scala
+++ tests/pos/i7219.scala
@@ -7,3 +7,30 @@ class Foo {
   val a: MyEnum.Blue = ???
   a : Blue   // ok
 }
+
+object Test {
+  object Types {
+    type T <: AnyRef
+    type U = T
+    type TC[X] <: AnyRef
+    type UC[X] = TC[(X, X)]
+    class C
+    class D[X] extends C
+    def x1: T = ???
+    def x2: U = ???
+    def x3: TC[Int] = ???
+    def x4: UC[Int] = ???
+    def x5: C = C()
+    def x6: D[Int] = D()
+  }
+  export Types._
+  type D1 = Types.D
+  type U1 = Types.UC
+
+  val y1: T = x1
+  val y2: U = x2
+  val y3: TC[Int] = x3
+  val y4: UC[Int] = x4
+  val y5: C = x5
+  val y6: D[Int] = x6
+}
