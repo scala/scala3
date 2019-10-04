18ebd6b2adc2c672a441ec45359eff2de5757c0f
Antoine Brunner <antoine.brunner@epfl.ch>
2019-10-04 11:50:19 +0200

    Fix unreachable code in DynamicTuple.dynamiamicConcat

---
+++ tests/run/tuple-concat.scala
@@ -0,0 +1,13 @@
+
+object Test extends App {
+  val emptyTuple: Tuple = ()
+  val tuple1: Tuple = "e1" *: "e2" *: "e3" *: ()
+  val tuple2: Tuple = "e4" *: "e5" *: "e6" *: ()
+  val result: Tuple = "e1" *: "e2" *: "e3" *: "e4" *: "e5" *: "e6" *: ()
+
+  println("tuple1 ++ emptyTuple = " + (tuple1 ++ emptyTuple))
+  println("emptyTuple ++ tuple1 = " + (emptyTuple ++ tuple1))
+  println("tuple2 ++ emptyTuple = " + (tuple2 ++ emptyTuple))
+  println("emptyTuple ++ tuple2 = " + (emptyTuple ++ tuple2))
+  println("tuple1 ++ tuple2 = " + (tuple1 ++ tuple2))
+}
