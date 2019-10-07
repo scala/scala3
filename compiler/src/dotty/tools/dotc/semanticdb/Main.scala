6217c298cda54893f88c50f248820e535841f038
Olafur Pall Geirsson <lgeirsson@twitter.com>
2019-10-07 18:07:23 +0200

    Add helper method to write SemanticDB files

--- compiler/src/dotty/tools/dotc/semanticdb/Main.scala
+++ compiler/src/dotty/tools/dotc/semanticdb/Main.scala
@@ -1,44 +1,67 @@
 package dotty.tools.dotc.semanticdb
+
 import java.nio.file.Paths
 import java.nio.file.Files
 import java.nio.file.StandardOpenOption
+import java.nio.file.Path
+import java.security.MessageDigest
+import java.nio.charset.StandardCharsets
+import scala.collection.JavaConverters._
 
 object Main {
-  def main(args: Array[String]): Unit = {
-    if (args.isEmpty) {
-      println("missing <out>")
-    }
-    val docs = TextDocuments(
-      List(
-        TextDocument(
-          Schema.SEMANTICDB3,
-          Language.SCALA,
-          uri = "Main.scala",
-          "md5",
-          List(
-            SymbolOccurrence(
-              "scala/Predef.String#",
-              Some(Range(0, 0, 1, 1)),
-              SymbolOccurrence.Role.REFERENCE
-            )
-          )
-        )
-      )
+  def write(
+      source: Path,
+      contents: String,
+      sourceroot: Path,
+      targetroot: Path,
+      occurrences: Seq[SymbolOccurrence]
+  ): Unit = {
+    val relpath = sourceroot.relativize(source)
+    val reluri = relpath.iterator().asScala.mkString("/")
+    val outpath = targetroot
+      .resolve("META-INF")
+      .resolve("semanticdb")
+      .resolve(relpath)
+      .resolveSibling(source.getFileName().toString() + ".semanticdb")
+    Files.createDirectories(outpath.getParent())
+    val doc: TextDocument = TextDocument(
+      schema = Schema.SEMANTICDB4,
+      language = Language.SCALA,
+      uri = reluri,
+      md5 = MD5.compute(contents),
+      occurrences = occurrences
     )
-    println(docs.serializedSize)
-    val path = Paths.get(args(0)).toAbsolutePath()
-    val out = Files.newOutputStream(
-      path,
-      StandardOpenOption.CREATE,
-      StandardOpenOption.TRUNCATE_EXISTING
-    )
-    val stream = SemanticdbOutputStream.newInstance(out)
+    val docs = TextDocuments(List(doc))
+    val out = Files.newOutputStream(outpath)
     try {
+      val stream = SemanticdbOutputStream.newInstance(out)
       docs.writeTo(stream)
-    } finally {
       stream.flush()
+    } finally {
       out.close()
     }
-    println(path)
+  }
+
+  def main(args: Array[String]): Unit = {
+    if (args.isEmpty) {
+      println("missing <sourceroot>")
+      sys.exit(1)
+    }
+    val occurrences = List(
+      SymbolOccurrence(
+        "_empty_/Main.",
+        Some(Range(0, 8, 0, 12)),
+        SymbolOccurrence.Role.REFERENCE
+      )
+    )
+    val sourceroot = Paths.get(args(0))
+    val targetroot = sourceroot.resolve("out")
+    write(
+      sourceroot.resolve("Main.scala"),
+      "object Main",
+      sourceroot,
+      targetroot,
+      occurrences
+    )
   }
 }
