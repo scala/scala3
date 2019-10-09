dfe4d92a723e4cb68c9dbc07386d092e795c749c
Martin Odersky <odersky@gmail.com>
2019-10-09 10:50:58 +0200

    Add test case

---
+++ tests/new/semanticdb-extract.scala
@@ -0,0 +1,18 @@
+object A {
+
+  def foo(x: Int) = ()
+  def foo(): Unit = ()
+
+  foo(1)
+  foo()
+
+  "".substring(1)
+  "".substring(1, 2)
+
+  List(1, 2)
+  List.apply()
+  List.`apply`()
+  println(1 + 2)
+
+  case class Foo(x: Int)
+}
