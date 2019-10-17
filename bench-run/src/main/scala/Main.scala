05c0c954d80c85f3ced2e61a3f8fbee651a689cc
Antoine Brunner <antoine.brunner@epfl.ch>
2019-10-17 22:15:17 +0200

    Change benchmarks input format

--- bench-run/src/main/scala/Main.scala
+++ bench-run/src/main/scala/Main.scala
@@ -17,26 +17,31 @@ object Bench {
     val iterations = if (intArgs.length > 1) intArgs(1).toInt else 20
     val forks = if (intArgs.length > 2) intArgs(2).toInt else 1
 
-    val benchmarks = if (args1.length > 0) args1(0) else ".*"
-    val outputFile = if (args1.length > 2) args1(2) else "output.csv"
+    if (args1.isEmpty) {
+      println("You should specify which benchmarks to run.")
+      return
+    }
 
     var builder = new OptionsBuilder()
-               .shouldFailOnError(true)
-               .jvmArgs("-Xms2G", "-Xmx2G")
-               .mode(Mode.AverageTime)
-               .timeUnit(TimeUnit.NANOSECONDS)
-               .warmupIterations(warmup)
-               .warmupTime(TimeValue.milliseconds(750))
-               .measurementIterations(iterations)
-               .measurementTime(TimeValue.milliseconds(500))
-               .forks(forks)
-               .include(benchmarks)
-               .resultFormat(ResultFormatType.CSV)
-               .result(outputFile)
+      .shouldFailOnError(true)
+      .jvmArgs("-Xms2G", "-Xmx2G")
+      .mode(Mode.AverageTime)
+      .timeUnit(TimeUnit.NANOSECONDS)
+      .warmupIterations(warmup)
+      .warmupTime(TimeValue.milliseconds(750))
+      .measurementIterations(iterations)
+      .measurementTime(TimeValue.milliseconds(500))
+      .forks(forks)
+      .include(args1(0))
+      .resultFormat(ResultFormatType.CSV)
 
-    if (args1.length > 1) {
+    if (args1.length > 1 && args1(1) != "--") {
       for ((param, values) <- paramsFromFile(args1(1)))
         builder = builder.param(param, values: _*)
+    }
+
+    if (args1.length > 2) {
+      builder = builder.result(args1(2))
     }
 
     val runner = new Runner(builder.build) // full access to all JMH features, you can also provide a custom output Format here
