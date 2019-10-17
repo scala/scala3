6c898c77910af5b2d19e412de40e232d14b7837a
noti0na1 <noti0na1@users.noreply.github.com>
2019-10-17 14:38:04 -0400

    update comment on flags

--- compiler/src/dotty/tools/dotc/core/Flags.scala
+++ compiler/src/dotty/tools/dotc/core/Flags.scala
@@ -451,7 +451,10 @@ object Flags {
    */
   val AfterLoadFlags: FlagSet = commonFlags(
     FromStartFlags, AccessFlags, Final, AccessorOrSealed, LazyOrTrait, SelfName, JavaDefined,
-    // We cannot use JavaEnumValue here because it has not been initialized
+    // We would like to add JavaEnumValue to this set so that we can correctly
+    // detect it in JavaNullInterop. However, JavaEnumValue is not initialized at this
+    // point, so we just make sure that all the "primitive" flags contained in JavaEnumValue
+    // are mentioned here as well.
     Enum, StableRealizable)
 
   /** A value that's unstable unless complemented with a Stable flag */
