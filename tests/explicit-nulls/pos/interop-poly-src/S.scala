9d77d648206a8f62a0e7913d917003de32b96c40
noti0na1 <noti0na1@users.noreply.github.com>
2019-11-04 13:32:07 -0500

    fix typo in the test

--- tests/explicit-nulls/pos/interop-poly-src/S.scala
+++ tests/explicit-nulls/pos/interop-poly-src/S.scala
@@ -9,7 +9,7 @@ class Test {
   // because JavaCat, being a Java class, _already_ nullifies its
   // fields.
   val jc: JavaCat[String]|Null = J.getJavaCat[String]()
-  // ScalaCat is Java-defined, so we need the inner |Null.
+  // ScalaCat is Scala-defined, so we need the inner |Null.
   val sc: ScalaCat[String|Null]|Null = J.getScalaCat[String]()
 
   import java.util.List
