fe4eaa4d215e21c4c91ee8ae5bb4da523e4fe899
Antoine Brunner <antoine.brunner@epfl.ch>
2019-11-07 15:09:49 +0100

    Trying to see if tupleCons is too big for optimizations

--- library/src/scala/runtime/DynamicTuple.scala
+++ library/src/scala/runtime/DynamicTuple.scala
@@ -189,67 +189,17 @@ object DynamicTuple {
   def dynamicCons[H, This <: Tuple](x: H, self: This): H *: This = {
     type Result = H *: This
     val res = (self: Any) match {
-      case () =>
-        Tuple1(x)
-      case self: Tuple1[_] =>
-        Tuple2(x, self._1)
-      case self: Tuple2[_, _] =>
-        Tuple3(x, self._1, self._2)
-      case self: Tuple3[_, _, _] =>
-        Tuple4(x, self._1, self._2, self._3)
-      case self: Tuple4[_, _, _, _] =>
-        Tuple5(x, self._1, self._2, self._3, self._4)
-      case self: Tuple5[_, _, _, _, _] =>
-        Tuple6(x, self._1, self._2, self._3, self._4, self._5)
-      case self: Tuple6[_, _, _, _, _, _] =>
-        Tuple7(x, self._1, self._2, self._3, self._4, self._5, self._6)
-      case self: Tuple7[_, _, _, _, _, _, _] =>
-        Tuple8(x, self._1, self._2, self._3, self._4, self._5, self._6, self._7)
-      case self: Tuple8[_, _, _, _, _, _, _, _] =>
-        Tuple9(x, self._1, self._2, self._3, self._4, self._5, self._6, self._7, self._8)
-      case self: Tuple9[_, _, _, _, _, _, _, _, _] =>
-        Tuple10(x, self._1, self._2, self._3, self._4, self._5, self._6, self._7, self._8, self._9)
-      case self: Tuple10[_, _, _, _, _, _, _, _, _, _] =>
-        Tuple11(x, self._1, self._2, self._3, self._4, self._5, self._6, self._7, self._8, self._9, self._10)
-      case self: Tuple11[_, _, _, _, _, _, _, _, _, _, _] =>
-        Tuple12(x, self._1, self._2, self._3, self._4, self._5, self._6, self._7, self._8, self._9, self._10, self._11)
-      case self: Tuple12[_, _, _, _, _, _, _, _, _, _, _, _] =>
-        Tuple13(x, self._1, self._2, self._3, self._4, self._5, self._6, self._7, self._8, self._9, self._10, self._11, self._12)
-      case self: Tuple13[_, _, _, _, _, _, _, _, _, _, _, _, _] =>
-        Tuple14(x, self._1, self._2, self._3, self._4, self._5, self._6, self._7, self._8, self._9, self._10, self._11, self._12, self._13)
-      case self: Tuple14[_, _, _, _, _, _, _, _, _, _, _, _, _, _] =>
-        Tuple15(x, self._1, self._2, self._3, self._4, self._5, self._6, self._7, self._8, self._9, self._10, self._11, self._12, self._13, self._14)
-      case self: Tuple15[_, _, _, _, _, _, _, _, _, _, _, _, _, _, _] =>
-        Tuple16(x, self._1, self._2, self._3, self._4, self._5, self._6, self._7, self._8, self._9, self._10, self._11, self._12, self._13, self._14, self._15)
-      case self: Tuple16[_, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _] =>
-        Tuple17(x, self._1, self._2, self._3, self._4, self._5, self._6, self._7, self._8, self._9, self._10, self._11, self._12, self._13, self._14, self._15, self._16)
-      case self: Tuple17[_, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _] =>
-        Tuple18(x, self._1, self._2, self._3, self._4, self._5, self._6, self._7, self._8, self._9, self._10, self._11, self._12, self._13, self._14, self._15, self._16, self._17)
-      case self: Tuple18[_, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _] =>
-        Tuple19(x, self._1, self._2, self._3, self._4, self._5, self._6, self._7, self._8, self._9, self._10, self._11, self._12, self._13, self._14, self._15, self._16, self._17, self._18)
-      case self: Tuple19[_, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _] =>
-        Tuple20(x, self._1, self._2, self._3, self._4, self._5, self._6, self._7, self._8, self._9, self._10, self._11, self._12, self._13, self._14, self._15, self._16, self._17, self._18, self._19)
-      case self: Tuple20[_, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _] =>
-        Tuple21(x, self._1, self._2, self._3, self._4, self._5, self._6, self._7, self._8, self._9, self._10, self._11, self._12, self._13, self._14, self._15, self._16, self._17, self._18, self._19, self._20)
-      case self: Tuple21[_, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _] =>
-        Tuple22(x, self._1, self._2, self._3, self._4, self._5, self._6, self._7, self._8, self._9, self._10, self._11, self._12, self._13, self._14, self._15, self._16, self._17, self._18, self._19, self._20, self._21)
-      case self: Tuple22[_, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _] =>
-        val arr: Array[Object] = Array(
-          x.asInstanceOf[Object], self._1.asInstanceOf[Object], self._2.asInstanceOf[Object],
-          self._3.asInstanceOf[Object], self._4.asInstanceOf[Object], self._5.asInstanceOf[Object],
-          self._6.asInstanceOf[Object], self._7.asInstanceOf[Object], self._8.asInstanceOf[Object],
-          self._9.asInstanceOf[Object], self._10.asInstanceOf[Object], self._11.asInstanceOf[Object],
-          self._12.asInstanceOf[Object], self._13.asInstanceOf[Object], self._14.asInstanceOf[Object],
-          self._15.asInstanceOf[Object], self._16.asInstanceOf[Object], self._17.asInstanceOf[Object],
-          self._18.asInstanceOf[Object], self._19.asInstanceOf[Object], self._20.asInstanceOf[Object],
-          self._21.asInstanceOf[Object], self._22.asInstanceOf[Object],
-        )
-        TupleXXL.fromIArray(arr.asInstanceOf[IArray[Object]]).asInstanceOf[Result]
       case xxl: TupleXXL =>
         val arr = new Array[Object](xxl.productArity + 1)
         System.arraycopy(xxl.elems, 0, arr, 1, xxl.productArity)
         arr(0) = x.asInstanceOf[Object]
-        TupleXXL.fromIArray(arr.asInstanceOf[IArray[Object]]).asInstanceOf[Result]
+        TupleXXL.fromIArray(arr.asInstanceOf[IArray[Object]])
+      case () =>
+        Tuple1(x)
+      case _ =>
+        val arr = new Array[Object](self.size + 1)
+        itToArray(self.asInstanceOf[Product].productIterator, self.size, arr, 1)
+        dynamicFromIArray[Result](arr.asInstanceOf[IArray[Object]])
     }
     res.asInstanceOf[Result]
   }
@@ -295,48 +245,6 @@ object DynamicTuple {
     val res = (self: Any) match {
       case self: Tuple1[_] =>
         ()
-      case self: Tuple2[_, _] =>
-        Tuple1(self._2)
-      case self: Tuple3[_, _, _] =>
-        Tuple2(self._2, self._3)
-      case self: Tuple4[_, _, _, _] =>
-        Tuple3(self._2, self._3, self._4)
-      case self: Tuple5[_, _, _, _, _] =>
-        Tuple4(self._2, self._3, self._4, self._5)
-      case self: Tuple6[_, _, _, _, _, _] =>
-        Tuple5(self._2, self._3, self._4, self._5, self._6)
-      case self: Tuple7[_, _, _, _, _, _, _] =>
-        Tuple6(self._2, self._3, self._4, self._5, self._6, self._7)
-      case self: Tuple8[_, _, _, _, _, _, _, _] =>
-        Tuple7(self._2, self._3, self._4, self._5, self._6, self._7, self._8)
-      case self: Tuple9[_, _, _, _, _, _, _, _, _] =>
-        Tuple8(self._2, self._3, self._4, self._5, self._6, self._7, self._8, self._9)
-      case self: Tuple10[_, _, _, _, _, _, _, _, _, _] =>
-        Tuple9(self._2, self._3, self._4, self._5, self._6, self._7, self._8, self._9, self._10)
-      case self: Tuple11[_, _, _, _, _, _, _, _, _, _, _] =>
-        Tuple10(self._2, self._3, self._4, self._5, self._6, self._7, self._8, self._9, self._10, self._11)
-      case self: Tuple12[_, _, _, _, _, _, _, _, _, _, _, _] =>
-        Tuple11(self._2, self._3, self._4, self._5, self._6, self._7, self._8, self._9, self._10, self._11, self._12)
-      case self: Tuple13[_, _, _, _, _, _, _, _, _, _, _, _, _] =>
-        Tuple12(self._2, self._3, self._4, self._5, self._6, self._7, self._8, self._9, self._10, self._11, self._12, self._13)
-      case self: Tuple14[_, _, _, _, _, _, _, _, _, _, _, _, _, _] =>
-        Tuple13(self._2, self._3, self._4, self._5, self._6, self._7, self._8, self._9, self._10, self._11, self._12, self._13, self._14)
-      case self: Tuple15[_, _, _, _, _, _, _, _, _, _, _, _, _, _, _] =>
-        Tuple14(self._2, self._3, self._4, self._5, self._6, self._7, self._8, self._9, self._10, self._11, self._12, self._13, self._14, self._15)
-      case self: Tuple16[_, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _] =>
-        Tuple15(self._2, self._3, self._4, self._5, self._6, self._7, self._8, self._9, self._10, self._11, self._12, self._13, self._14, self._15, self._16)
-      case self: Tuple17[_, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _] =>
-        Tuple16(self._2, self._3, self._4, self._5, self._6, self._7, self._8, self._9, self._10, self._11, self._12, self._13, self._14, self._15, self._16, self._17)
-      case self: Tuple18[_, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _] =>
-        Tuple17(self._2, self._3, self._4, self._5, self._6, self._7, self._8, self._9, self._10, self._11, self._12, self._13, self._14, self._15, self._16, self._17, self._18)
-      case self: Tuple19[_, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _] =>
-        Tuple18(self._2, self._3, self._4, self._5, self._6, self._7, self._8, self._9, self._10, self._11, self._12, self._13, self._14, self._15, self._16, self._17, self._18, self._19)
-      case self: Tuple20[_, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _] =>
-        Tuple19(self._2, self._3, self._4, self._5, self._6, self._7, self._8, self._9, self._10, self._11, self._12, self._13, self._14, self._15, self._16, self._17, self._18, self._19, self._20)
-      case self: Tuple21[_, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _] =>
-        Tuple20(self._2, self._3, self._4, self._5, self._6, self._7, self._8, self._9, self._10, self._11, self._12, self._13, self._14, self._15, self._16, self._17, self._18, self._19, self._20, self._21)
-      case self: Tuple22[_, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _] =>
-        Tuple21(self._2, self._3, self._4, self._5, self._6, self._7, self._8, self._9, self._10, self._11, self._12, self._13, self._14, self._15, self._16, self._17, self._18, self._19, self._20, self._21, self._22)
       case xxl: TupleXXL =>
         if (xxl.productArity == 23) {
           val elems = xxl.elems
@@ -345,12 +253,18 @@ object DynamicTuple {
             elems(8), elems(9), elems(10), elems(11), elems(12), elems(13), elems(14),
             elems(15), elems(16), elems(17), elems(18), elems(19), elems(20),
             elems(21), elems(22)
-          ).asInstanceOf[Result]
+          )
         } else {
           val arr = new Array[Object](self.size - 1)
           System.arraycopy(xxl.elems, 1, arr, 0, self.size - 1)
-          TupleXXL.fromIArray(arr.asInstanceOf[IArray[Object]]).asInstanceOf[Result]
+          TupleXXL.fromIArray(arr.asInstanceOf[IArray[Object]])
         }
+      case _ =>
+        val arr = new Array[Object](self.size - 1)
+        val it = self.asInstanceOf[Product].productIterator
+        it.next()
+        itToArray(it, self.size - 1, arr, 0)
+        dynamicFromIArray[Result](arr.asInstanceOf[IArray[Object]])
     }
     res.asInstanceOf[Result]
   }
