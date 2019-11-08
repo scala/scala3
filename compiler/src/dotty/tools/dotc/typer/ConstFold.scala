d1981f82665b53278c882c9e43f0c2f7f0bdc0d1
Martin Odersky <odersky@gmail.com>
2019-11-08 18:00:21 +0100

    Constant fold null tests
    This is a temporary commit so that we can test the nullability analysis without
    having the full implementation of explicit nulls.
    
    Once we have explicit nulls, we could consider dropping this, as the benefits
    seem smallish.

--- compiler/src/dotty/tools/dotc/typer/ConstFold.scala
+++ compiler/src/dotty/tools/dotc/typer/ConstFold.scala
@@ -19,15 +19,16 @@ object ConstFold {
   /** If tree is a constant operation, replace with result. */
   def apply[T <: Tree](tree: T)(implicit ctx: Context): T = finish(tree) {
     tree match {
+      case ComparePathNull(ref, testEqual) if ctx.notNullRefs.containsRef(ref) =>
+        // TODO maybe drop once we have general Nullability?
+        Constant(!testEqual)
       case Apply(Select(xt, op), yt :: Nil) =>
-        xt.tpe.widenTermRefExpr.normalized match {
+        xt.tpe.widenTermRefExpr.normalized match
           case ConstantType(x) =>
-            yt.tpe.widenTermRefExpr match {
+            yt.tpe.widenTermRefExpr match
               case ConstantType(y) => foldBinop(op, x, y)
               case _ => null
-            }
           case _ => null
-        }
       case Select(xt, op) =>
         xt.tpe.widenTermRefExpr match {
           case ConstantType(x) => foldUnop(op, x)
