324822770d718a5c073ad866231fc9bf17578445
Antoine Brunner <antoine.brunner@epfl.ch>
2019-11-14 18:14:07 +0100

    Add a new folder for test files

---
+++ tests/run/tuple-tests/tuple-concat.scala
@@ -0,0 +1,27 @@
+
+object Test extends App {
+  val emptyTuple: Tuple = ()
+  val tuple1: Tuple = ("1", "2", "3", "4", "5")
+  val tuple2: Tuple = ("6", "7", "8", "9", "10")
+  val tupleXXL1: Tuple = ("11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31", "32", "33", "34", "35")
+  val tupleXXL2: Tuple = ("36", "37", "38", "39", "40", "41", "42", "43", "44", "45", "46", "47", "48", "49", "50", "51", "52", "53", "54", "55", "56", "57", "58", "59", "60")
+
+  // Try all possible kinds of concatenations (small === <=22):
+  // empty + empty
+  // small + empty
+  // empty + small
+  // xxl + empty
+  // empty + xxl
+  // small + small
+  // small + xxl
+  // xxl + small
+  // xxl + xxl
+  println(tuple1 ++ emptyTuple)
+  println(emptyTuple ++ tuple1)
+
+  // Concatenation with an empty tuple should be a no-op
+  assert((tuple1 ++ emptyTuple).asInstanceOf[AnyRef] eq tuple1.asInstanceOf[AnyRef])
+  assert((tupleXXL1 ++ emptyTuple).asInstanceOf[AnyRef] eq tupleXXL1.asInstanceOf[AnyRef])
+  assert((emptyTuple ++tuple1).asInstanceOf[AnyRef] eq tuple1.asInstanceOf[AnyRef])
+  assert((emptyTuple ++ tupleXXL1).asInstanceOf[AnyRef] eq tupleXXL1.asInstanceOf[AnyRef])
+}
