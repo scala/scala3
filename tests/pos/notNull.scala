434204f5890d3f43ce778fb20b5d1be8150f07ea
Sébastien Doeraene <sjrdoeraene@gmail.com>
2019-11-14 16:27:55 +0100

    Use a definition of `def notNull` that does not require `NotNull`.

--- tests/pos/notNull.scala
+++ tests/pos/notNull.scala
@@ -1,6 +1,6 @@
 trait Null extends Any
 object Test with
-  def notNull(x: Any): x.type & NotNull =
+  def notNull[A](x: A | Null): x.type & A =
     assert(x != null)
     x.asInstanceOf  // TODO: drop the .asInstanceOf when explicit nulls are implemented
 
@@ -9,25 +9,14 @@ object Test with
     val y = x; val _: Int | Null = y
   }
   locally {
-    val x: (Int | Null) & NotNull = ???
-    val y = identity(x); val yc: Int = y
-    val z = x; val zc: Int = z
-  }
-  locally {
     val x: Int | Null = ???
     val y = notNull(identity(x)); val yc: Int = y
     val z = notNull(x); val zc: Int = z
   }
-  locally {
-    val x: Int | Null = ???
-    val y = identity(x).$nn; val yc: Int = y
-    val z = x.$nn; val zc: Int = z
-  }
   class C { type T }
   locally {
     val x: C { type T = Int } = new C { type T = Int }
-    val y: x.$nn.T = 33
+    val xnn: x.type & C { type T = Int } = notNull(x)
+    val y: xnn.T = 33
     val z = y; val zc: Int = z
   }
-
-
