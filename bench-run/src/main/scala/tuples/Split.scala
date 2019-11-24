dfb8033406d6e5ff6d04dce6136b7e5809dad4f3
Antoine Brunner <antoine.brunner@epfl.ch>
2019-11-24 09:13:28 +0100

    Add benchmarks for tuple split

---
+++ bench-run/src/main/scala/tuples/Split.scala
@@ -0,0 +1,34 @@
+package dotty.tools.benchmarks.tuples
+
+import org.openjdk.jmh.annotations._
+import scala.runtime.DynamicTuple
+
+@State(Scope.Thread)
+class Split {
+  @Param(Array("0"))
+  var size: Int = _
+  var tuple: Tuple = _
+  var array: Array[Object] = _
+  var half: Int = _
+
+  @Setup
+  def setup(): Unit = {
+    tuple = ()
+    half = size / 2
+
+    for (i <- 1 to size)
+      tuple = "elem" *: tuple
+
+    array = new Array[Object](size)
+  }
+
+  @Benchmark
+  def tupleSplit(): (Tuple, Tuple) = {
+    DynamicTuple.dynamicSplitAt(tuple, half)
+  }
+
+  @Benchmark
+  def arraySplit(): (Array[Object], Array[Object]) = {
+    array.splitAt(half)
+  }
+}
