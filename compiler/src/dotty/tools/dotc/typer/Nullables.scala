91b54aa25f6832ea75497e4e384f1b246b786ec2
noti0na1 <noti0na1@users.noreply.github.com>
2019-12-04 16:28:56 -0500

    fix assert

--- compiler/src/dotty/tools/dotc/typer/Nullables.scala
+++ compiler/src/dotty/tools/dotc/typer/Nullables.scala
@@ -293,7 +293,7 @@ object Nullables with
     def computeAssignNullable()(given Context): tree.type = tree.lhs match
       case TrackedRef(ref) =>
         val rhstp = tree.rhs.typeOpt
-        if (rhstp.isNullType || rhstp.isNullableUnion)
+        if (rhstp.isNullType || (curCtx.explicitNulls && rhstp.isNullableUnion))
           // If the type of rhs is nullable (`T|Null` or `Null`), then the nullability of the
           // lhs variable is no longer trackable. We don't need to check whether the type `T`
           // is correct here, as typer will check it.
