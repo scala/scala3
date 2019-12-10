e2371420e32ae9e46acb89e2945065f1e249c936
Liu Fengyun <liu@fengy.me>
2019-12-10 10:27:49 +0100

    Add phase SetDefTree

---
+++ compiler/src/dotty/tools/dotc/transform/init/SetDefTree.scala
@@ -0,0 +1,33 @@
+package dotty.tools.dotc
+package transform
+package init
+
+import MegaPhase._
+import ast.tpd
+import core.Contexts.Context
+
+/** Set the `defTree` property of symbols */
+class SetDefTree extends MiniPhase {
+  import tpd._
+
+  override val phaseName: String = SetDefTree.name
+  override val runsAfter = Set("flatten", "lambdaLift", Erasure.name)
+
+    // don't allow plugins to change tasty
+    // research plugins can still change the phase plan at will
+
+  override def runOn(units: List[CompilationUnit])(implicit ctx: Context): List[CompilationUnit] = {
+    val ctx2 = ctx.fresh.setSetting(ctx.settings.YretainTrees, true)
+    super.runOn(units)(ctx2)
+  }
+
+  override def transformValDef(tree: ValDef)(implicit ctx: Context): Tree = tree.setDefTree
+
+  override def transformDefDef(tree: DefDef)(implicit ctx: Context): Tree = tree.setDefTree
+
+  override def transformTypeDef(tree: TypeDef)(implicit ctx: Context): Tree = tree.setDefTree
+}
+
+object SetDefTree {
+  val name: String = "SetDefTree"
+}
