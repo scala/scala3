ac2b9dc4084599038e22aa6560676386aee1bb35
Liu Fengyun <liu@fengy.me>
2019-12-15 01:11:55 +0100

    Add checker phase

---
+++ compiler/src/dotty/tools/dotc/transform/init/CheckingPhase.scala
@@ -0,0 +1,43 @@
+package dotty.tools.dotc
+package transform
+package init
+
+import MegaPhase._
+import ast.tpd
+import core._
+import Contexts.Context
+
+import scala.collection.mutable
+
+
+class Checker extends MiniPhase {
+  import tpd._
+
+  val phaseName = "initChecker"
+
+  // cache of class summary
+  private val baseEnv = Env(null, mutable.Map.empty)
+
+  override val runsAfter = Set(SetDefTree.name)
+
+  override def transformTypeDef(tree: TypeDef)(implicit ctx: Context): tpd.Tree = {
+    if (!tree.isClassDef) return tree
+    val cls = tree.symbol.asClass
+
+    // A concrete class may not be instantiated if the self type is not satisfied
+    if (!cls.isOneOf(Flags.AbstractOrTrait)) {
+      implicit val state = Checking.State(
+        visited = mutable.Set.empty,
+        path = Vector.empty,
+        thisClass = cls,
+        fieldsInited = mutable.Set.empty,
+        parentsInited = mutable.Set.empty,
+        env = baseEnv.withCtx(ctx)
+      )
+
+      Checking.checkClassBody(tree)
+    }
+
+    tree
+  }
+}
