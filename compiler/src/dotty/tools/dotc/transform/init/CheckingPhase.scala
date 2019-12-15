bf917a7c817debacfa305b64434a46a84b3cbb8b
Liu Fengyun <liu@fengy.me>
2019-12-15 04:14:20 +0100

    Ignore non-instantiable classes

--- compiler/src/dotty/tools/dotc/transform/init/CheckingPhase.scala
+++ compiler/src/dotty/tools/dotc/transform/init/CheckingPhase.scala
@@ -2,10 +2,17 @@ package dotty.tools.dotc
 package transform
 package init
 
-import MegaPhase._
+
+import dotty.tools.dotc._
 import ast.tpd
-import core._
+
+import dotty.tools.dotc.core._
 import Contexts.Context
+import Types._
+
+import dotty.tools.dotc.transform._
+import MegaPhase._
+
 
 import scala.collection.mutable
 
@@ -25,10 +32,20 @@ class Checker extends MiniPhase {
 
   override def transformTypeDef(tree: TypeDef)(implicit ctx: Context): tpd.Tree = {
     if (!tree.isClassDef) return tree
+
     val cls = tree.symbol.asClass
+    val instantiable: Boolean =
+      cls.is(Flags.Module) ||
+      !cls.isOneOf(Flags.AbstractOrTrait) && {
+        // see `Checking.checkInstantiable` in typer
+        val tp = cls.appliedRef
+        val stp = SkolemType(tp)
+        val selfType = cls.givenSelfType.asSeenFrom(stp, cls)
+        !selfType.exists || stp <:< selfType
+      }
 
     // A concrete class may not be instantiated if the self type is not satisfied
-    if (!cls.isOneOf(Flags.AbstractOrTrait)) {
+    if (instantiable) {
       implicit val state = Checking.State(
         visited = mutable.Set.empty,
         path = Vector.empty,
