f793e09c97bc143b276aa35fdb1e911106cb3513
Liu Fengyun <liu@fengy.me>
2019-12-16 13:51:26 +0100

    Change to new syntax

--- tests/run-macros/i6988/FirstArg_1.scala
+++ tests/run-macros/i6988/FirstArg_1.scala
@@ -2,32 +2,29 @@ package foo
 
 case class FirstArg(value: Any, source: String)
 object FirstArg {
-  inline given as FirstArg = ${Macros.argsImpl}
+  inline given FirstArg = ${Macros.argsImpl}
 }
 
 object Macros {
-  import language.experimental.macros
   import scala.quoted._
-  import scala.tasty._
 
-  def argsImpl given (qctx: QuoteContext): Expr[FirstArg] = {
-    import qctx.tasty._
-    var enclosingCls: ClassDefSymbol = rootContext.owner.asInstanceOf[ClassDefSymbol]
+  def argsImpl(given qctx: QuoteContext): Expr[FirstArg] = {
+    import qctx.tasty.{_, given}
+
+    var enclosingCls: Symbol = rootContext.owner
+
     def enclosingParamList(owner: Symbol): Seq[Seq[Symbol]] =
-      owner match {
-        case IsClassDefSymbol(x) =>
-          enclosingCls = x
-          x.tree.constructor.paramss map { _ map {
-            _.symbol
-          }}
-        case _ =>
-          enclosingParamList(owner.owner)
-      }
+      if owner.isClassDef then
+        owner.tree match
+          case tdef: ClassDef =>
+            tdef.constructor.paramss map { _ map {_.symbol }}
+      else enclosingParamList(owner.owner)
+
     def literal(value: String): Expr[String] =
       Literal(Constant(value)).seal.asInstanceOf[Expr[String]]
     val paramss = enclosingParamList(rootContext.owner)
     val firstArg = paramss.flatten.head
     val ref = Select.unique(This(enclosingCls), firstArg.name)
-    '{ FirstArg(${ref.seal}, ${firstArg.name.toExpr}) }
+    '{ FirstArg(${ref.seal}, ${Expr(firstArg.name)}) }
   }
 }
