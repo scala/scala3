95ef05306bb59eeabd0df52b0b0c209eb133e34a
Nicolas Stucki <nicolas.stucki@gmail.com>
2019-12-16 19:15:47 +0100

    Fix #7781: Avoid using nested package definition for Type
    Had to remove `sealed` from `Type`

---
+++ library/src/scala/internal/quoted/TreeType.scala
@@ -0,0 +1,14 @@
+package scala.internal.quoted
+
+/** An Type backed by a tree */
+final class TreeType[Tree](val typeTree: Tree, val scopeId: Int) extends scala.quoted.Type[Any] {
+  override def equals(that: Any): Boolean = that match {
+    case that: TreeType[_] => typeTree ==
+      // TastyTreeExpr are wrappers around trees, therfore they are equals if their trees are equal.
+      // All scopeId should be equal unless two different runs of the compiler created the trees.
+      that.typeTree && scopeId == that.scopeId
+    case _ => false
+  }
+  override def hashCode: Int = typeTree.hashCode
+  override def toString: String = s"Type(<tasty tree>)"
+}
