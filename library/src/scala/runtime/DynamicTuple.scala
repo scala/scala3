b3d10888e5b01a83af2da34fb67ac7ddb48c3b3f
Antoine Brunner <antoine.brunner@epfl.ch>
2019-12-17 20:15:04 +0100

    Remove unwanted change

--- library/src/scala/runtime/DynamicTuple.scala
+++ library/src/scala/runtime/DynamicTuple.scala
@@ -1,10 +1,8 @@
 package scala.runtime
 
 import scala.Tuple.{ Concat, Size, Head, Tail, Elem, Zip, Map }
-import scala.reflect.ClassTag
 
 object DynamicTuple {
-
   inline val MaxSpecialized = 22
   inline private val XXL = MaxSpecialized + 1
 
