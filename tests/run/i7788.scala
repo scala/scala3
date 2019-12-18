93258da0637aeed31f68b0cb11d6b530dc189d43
Martin Odersky <odersky@gmail.com>
2019-12-18 17:13:55 +0100

    Turn i7788.scala into a proper test

--- tests/run/i7788.scala
+++ tests/run/i7788.scala
@@ -8,7 +8,7 @@ given showEither[A,B]: (sA: Show[A]) => Show[B] => Show[Either[A,B]] =
   _.fold(a => s"Left(${summon[Show[A]].show(a)})", b => s"Right(${summon[Show[B]].show(b)})")
 given [A,B]: (sA: Show[A]) => (sB: Show[B]) => Show[(A,B)] = (a,b) => s"(${sA.show(a)}), ${sB.show(b)})"
 
-@main def ShowDemo =
+@main def Test =
   println(summon[Show[(Int, String)]].show(0 -> "hello"))
   println(summon[Show[Either[Int, String]]].show(Left(-1)))
   println(summon[Show[Either[Int, String]]].show(Right("success message")))
