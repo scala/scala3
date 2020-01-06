f7867bd391d6565346013d4193317cf6fc2aecb5
Martin Odersky <odersky@gmail.com>
2020-01-06 07:43:56 +0100

    Fix test case

--- tests/pos/i7893.scala
+++ tests/pos/i7893.scala
@@ -2,6 +2,6 @@ object Test {
   val l1 = List(Predef.identity[Int](_))
   val lc1: List[Int => Int] = l1
 
-  val l2 = List(Predef.identity[Int](_))
+  val l2 = List(Predef.identity[Int])
   val lc2: List[Int => Int] = l2
 }
