5ac5a34095c4e3166ef6743495d5eee9da47be8b
Nicolas Stucki <nicolas.stucki@gmail.com>
2020-01-06 11:48:19 +0100

    Fix printing of <repeated> types

--- library/src/scala/tasty/reflect/SourceCodePrinter.scala
+++ library/src/scala/tasty/reflect/SourceCodePrinter.scala
@@ -1467,7 +1467,7 @@ class SourceCodePrinter[R <: Reflection & Singleton](val tasty: R)(syntaxHighlig
 
     object ScalaPackage {
       def unapply(tpe: TypeOrBounds)(given ctx: Context): Boolean = tpe match {
-        case tpe: TermRef => tpe.termSymbol == defn.ScalaPackage
+        case tpe: Type => tpe.termSymbol == defn.ScalaPackage
         case _ => false
       }
     }
