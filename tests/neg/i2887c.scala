75775a29cfc5dddfb53e1d0bd96012c9fe81dc24
Nicolas Stucki <nicolas.stucki@gmail.com>
2020-01-09 10:59:19 +0100

    Fix #2887: Add regression test

---
+++ tests/neg/i2887c.scala
@@ -0,0 +1,13 @@
+trait A { type S[X[_,_], Y[_],_]; type I[_] }
+trait B { type S[X[_],Y[_]]; type I[_,_] }
+trait C { type M <: B }
+trait D { type M >: A }
+
+object Test {
+  def test(x: C with D): Unit = {
+    def foo(a: A, b: B)(z: a.S[b.I,a.I,b.S[a.I,a.I]]) = z
+    def bar(a: A, y: x.M) = foo(a,y)
+    def baz(a: A) = bar(a, a)
+    baz(new A { type S[X[_,_], Y[_], Z] = X[Z,Y[Z]]; type I[X] = X })(1) // error
+  }
+}
