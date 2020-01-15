EpamLifeSciencesTeam/dotty

Add error message for only functional erased or implicit types

compiler/src/dotty/tools/dotc/reporting/diagnostic/messages.scala
@@ -2401,4 +2401,18 @@ object messages {
       val msg: String = i"illegal cyclic type reference: ${where} ${hl(lastChecked.show)} of $sym refers back to the type itself"
       val explanation: String = ""
     }
+
+  case class ImplicitTypesCanOnlyBeFunctionTypes()(implicit val ctx: Context)
+    extends Message(ImplicitTypesCanOnlyBeFunctionTypesID) {
+    val kind: String = "Syntax"
+    val msg: String = "Types with implicit keyword can only be function types `given (...) => ...`"
+    val explanation: String = ""
+  }
+
+  case class ErasedTypesCanOnlyBeFunctionTypes()(implicit val ctx: Context)
+    extends Message(ErasedTypesCanOnlyBeFunctionTypesID) {
+    val kind: String = "Syntax"
+    val msg: String = "Types with erased keyword can only be function types `erased (...) => ...`"
+    val explanation: String = ""
+  }
 }

compiler/test/dotty/tools/dotc/reporting/ErrorMessagesTests.scala
@@ -1687,4 +1687,30 @@ class ErrorMessagesTests extends ErrorMessagesTest {
       assertEquals("alias", where)
       assertEquals("List[X]", lastChecked.show)
     }
+
+  @Test def implicitTypesCanOnlyBeFunctionTypesSuccess() =
+    checkMessagesAfter(RefChecks.name) ("def foo(f: (given Int) => Int): Int = 1")
+    .expectNoErrors
+
+  @Test def erasedTypesCanOnlyBeFunctionTypesSuccess() =
+    checkMessagesAfter(FrontEnd.name) ("def foo(f: (erased Int) => Int): Int = 1")
+      .expectNoErrors
+
+  @Test def implicitTypesCanOnlyBeFunctionTypesFailed() =
+    checkMessagesAfter(FrontEnd.name) ("def foo(f: (given Int)): Int = 1")
+      .expect { (ictx, messages) =>
+        implicit val ctx: Context = ictx
+        assertMessageCount(1, messages)
+        val ImplicitTypesCanOnlyBeFunctionTypes() :: Nil = messages
+        assertEquals("Types with implicit keyword can only be function types `given (...) => ...`", messages.head.msg)
+      }
+
+  @Test def erasedTypesCanOnlyBeFunctionTypesFailed() =
+    checkMessagesAfter(FrontEnd.name) ("def foo(f: (erased Int)): Int = 1")
+      .expect { (ictx, messages) =>
+        implicit val ctx: Context = ictx
+        assertMessageCount(1, messages)
+        val ErasedTypesCanOnlyBeFunctionTypes() :: Nil = messages
+        assertEquals("Types with erased keyword can only be function types `erased (...) => ...`", messages.head.msg)
+      }
 }

scala/scala3

compiler/src/dotty/tools/dotc/reporting/diagnostic/messages.scala
@@ -2381,4 +2381,18 @@ object messages {
       val msg: String = i"illegal cyclic type reference: ${where} ${hl(lastChecked.show)} of $sym refers back to the type itself"
       val explanation: String = ""
     }
+
+  case class ImplicitTypesCanOnlyBeFunctionTypes()(implicit val ctx: Context)
+    extends Message(ImplicitTypesCanOnlyBeFunctionTypesID) {
+    val kind: String = "Syntax"
+    val msg: String = "Types with given keyword can only be function types `given (...) => ...`"
+    val explanation: String = ""
+  }
+
+  case class ErasedTypesCanOnlyBeFunctionTypes()(implicit val ctx: Context)
+    extends Message(ErasedTypesCanOnlyBeFunctionTypesID) {
+    val kind: String = "Syntax"
+    val msg: String = "Types with erased keyword can only be function types `erased (...) => ...`"
+    val explanation: String = ""
+  }
 }

compiler/test/dotty/tools/dotc/reporting/ErrorMessagesTests.scala
@@ -1684,4 +1684,30 @@ class ErrorMessagesTests extends ErrorMessagesTest {
       assertEquals("alias", where)
       assertEquals("List[X]", lastChecked.show)
     }
+
+  @Test def implicitTypesCanOnlyBeFunctionTypesSuccess() =
+    checkMessagesAfter(RefChecks.name) ("def foo(f: (given Int) => Int): Int = 1")
+    .expectNoErrors
+
+  @Test def erasedTypesCanOnlyBeFunctionTypesSuccess() =
+    checkMessagesAfter(FrontEnd.name) ("def foo(f: (erased Int) => Int): Int = 1")
+      .expectNoErrors
+
+  @Test def implicitTypesCanOnlyBeFunctionTypesFailed() =
+    checkMessagesAfter(FrontEnd.name) ("def foo(f: (given Int)): Int = 1")
+      .expect { (ictx, messages) =>
+        implicit val ctx: Context = ictx
+        assertMessageCount(1, messages)
+        val ImplicitTypesCanOnlyBeFunctionTypes() :: Nil = messages
+        assertEquals("Types with given keyword can only be function types `given (...) => ...`", messages.head.msg)
+      }
+
+  @Test def erasedTypesCanOnlyBeFunctionTypesFailed() =
+    checkMessagesAfter(FrontEnd.name) ("def foo(f: (erased Int)): Int = 1")
+      .expect { (ictx, messages) =>
+        implicit val ctx: Context = ictx
+        assertMessageCount(1, messages)
+        val ErasedTypesCanOnlyBeFunctionTypes() :: Nil = messages
+        assertEquals("Types with erased keyword can only be function types `erased (...) => ...`", messages.head.msg)
+      }
 }