6f5e760faf6942503518ef1a9c0458c4eba8954f
Martin Odersky <odersky@gmail.com>
2020-01-30 14:32:12 +0100

    Simplify typedDependent

--- tests/pos/i8128.scala
+++ tests/pos/i8128.scala
@@ -1,4 +1,14 @@
 object Test {
   def id: (x: 1 | 0) => x.type = x => x
-  id(0): 0
+  id(0): 0   // fails
+
+  def id2: Function1[1 | 0, 1 | 0] {
+    def apply(x: 1 | 0): x.type
+  } = ???
+  id2(0): 0  // fails
+
+  def id3: Function1[1 | 0, Int] {
+    def apply(x: 1 | 0): x.type
+  } = ???
+  id3(0): 0  // ok
 }
