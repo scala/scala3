156f5342f6bd3677b7f28dce378b2ef526035513
Nicolas Stucki <nicolas.stucki@gmail.com>
2020-02-05 19:27:30 +0100

    Add test in scoped-quoted-expr-proto

--- tests/neg/scoped-quoted-expr-proto.scala
+++ tests/neg/scoped-quoted-expr-proto.scala
@@ -139,3 +139,89 @@ package c {
   }
 
 }
+
+package d {
+
+  import scala.compiletime.S
+
+  trait QCtx { qctx =>
+    type Expr[+T]
+    // type Type[T]
+    type Level <: Int
+    type NestedSCtx = SCtx {
+      type Level = S[qctx.Level]
+      type NestedQCtx = QCtx { type Expr[+T] >: qctx.Expr[T]; type Level = qctx.Level }
+    }
+  }
+
+  trait SCtx {
+    type NestedQCtx <: QCtx
+  }
+
+  opaque type Level[N <: Int] = Unit
+  object Level {
+    /*erased*/ given level[L] as Level[L] = ()
+    // given level[L](using QCtx { type }) as Level[L] = 0
+  }
+
+  /*Quote*/ def q[T, L <: Int](using qctx: QCtx { type Level = L }, lvl: Level[L])(x: (qctx.NestedSCtx, Level[S[L]]) ?=> T): qctx.Expr[T] = ???
+  /*Splice*/ def s[T, L <: Int](using sctx: SCtx { type Level = S[L] }, lvl: Level[S[L]])(x: (qctx: sctx.NestedQCtx, lvl: Level[L]) ?=> qctx.Expr[T]): T = ???
+  /*run*/ def r[T, L <: Int](using Level[L])(x: (qctx: QCtx { type Level = L }) ?=> qctx.Expr[T]): T = ???
+
+
+  val test: Any = {
+
+    def pow(using qctx: QCtx)(x: qctx.Expr[Double], n: Int): qctx.Expr[Double] =
+      if n == 0 then q{1.0} else q{ s{x} * s{pow(x, n - 1)} }
+
+    r {
+      q{ (x: Double) => s{pow(q{x}, 5)} }
+    }
+
+    r {
+      q{ (x: Double) =>
+        s{
+          val y = q{x}
+          pow(q{s{y}}, 5)
+        }
+      }
+    }
+
+    r {
+      val a = q{ 4.0 }
+      q{ (x: Double) =>
+        s{
+          pow(q{s{a}}, 5)
+        }
+      }
+    }
+
+
+    r {
+      q{
+        r {
+          q{ 1 }
+        }
+      }
+    }
+
+    r {
+      q{
+        q{ // error
+          1
+        }
+      }
+    }
+
+    r { qctx ?=>
+      var escaped: qctx.Expr[Double] = ???
+      q{ (x: Double) =>
+        s{
+          escaped = q{x} // error
+          pow(q{x}, 5)
+        }
+      }
+    }
+  }
+
+}
