0afdbcd0a96d81362c62ed9ca4dbc51d85cc5a90
gzoller <gzoller@gmail.com>
2020-02-07 10:32:18 -0600

    wording fix in comment

--- compiler/src/dotty/tools/dotc/fromtasty/NonTastyScalaCompilationUnit.scala
+++ compiler/src/dotty/tools/dotc/fromtasty/NonTastyScalaCompilationUnit.scala
@@ -5,8 +5,9 @@ import dotty.tools.dotc.util.NoSource
 
 /**
  * A marker CompilationUnit to return up the call stack from ReadTasty.  This will tell us that we've
- * encountered, and attempted to inspect, a Java class file.  We can't TASTy-inspect a Java class obviously,
- * but we want to return the fact we found it so that higher-up we can take appropriate action if desired.
+ * encountered, and attempted to inspect, a non-Tasty Scala class file (for example a legacy class pre-Scala 3).  
+ * In this case we still want to return the fact we found it so that higher-up we can take appropriate 
+ * action if desired.
  */
 class NonTastyScalaCompilationUnit(val className: String) extends CompilationUnit(NoSource) {
   override def toString: String = s"Non-Tasty Scala class file $className"
