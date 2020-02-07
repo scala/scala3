3988ba1cefd1e3a94e188596d6c900fadad2fbd3
Nicolas Stucki <nicolas.stucki@gmail.com>
2020-02-07 14:45:44 +0100

    Update to `given ..: T` to `given .. as T` syntax

--- library/src/scala/quoted/autolift.scala
+++ library/src/scala/quoted/autolift.scala
@@ -3,5 +3,5 @@ package scala.quoted
 /** Enable implicit conversion from a term of type `T` to an expression of type `Expr[T]` */
 object autolift {
   /** Implicit conversion from a term of type `T` to an expression of type `Expr[T]` */
-  given autoToExpr[T](using Liftable[T], QuoteContext): Conversion[T, Expr[T]] = Expr(_)
+  given autoToExpr[T](using Liftable[T], QuoteContext) as Conversion[T, Expr[T]] = Expr(_)
 }
