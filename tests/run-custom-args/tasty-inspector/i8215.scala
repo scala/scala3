5d7db95b7e715a964c940317f71bb03b37c71a51
gzoller <gzoller@gmail.com>
2020-02-10 13:56:35 -0600

    Attempt to fix build bug

--- tests/run-custom-args/tasty-inspector/i8215.scala
+++ tests/run-custom-args/tasty-inspector/i8215.scala
@@ -7,21 +7,21 @@ object Test {
   def main(args: Array[String]): Unit = {
 
     // Tasty Scala Class
-    val inspect1 = new TestInspector()
+    val inspect1 = new TestInspector_NonTasty()
     inspect1.inspect("", List("I8215"))
     assert(inspect1.isJava == false)
     assert(inspect1.isScala2 == false)
     assert(inspect1.className == "")
 
     // Java Class
-    val inspect2 = new TestInspector()
+    val inspect2 = new TestInspector_NonTasty()
     inspect2.inspect("", List("java.util.UUID"))
     assert(inspect2.isJava == true)
     assert(inspect2.isScala2 == false)
     assert(inspect2.className == "java.util.UUID")
 
     // Legacy non-Tasty Scala class
-    val inspect3 = new TestInspector()
+    val inspect3 = new TestInspector_NonTasty()
     inspect3.inspect("", List("scala.collection.immutable.RedBlackTree"))
     assert(inspect3.isJava == false)
     assert(inspect3.isScala2 == true)
@@ -29,7 +29,7 @@ object Test {
   }
 }
 
-class TestInspector() extends TastyInspector
+class TestInspector_NonTasty() extends TastyInspector
 
   var isJava: Boolean = false
   var isScala2: Boolean = false
