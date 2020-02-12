fa4eef686bc63ed99e36f393c40edf4486ef0f54
Nicolas Stucki <nicolas.stucki@gmail.com>
2020-02-12 15:52:19 +0100

    Fix typo

--- compiler/src/dotty/tools/dotc/core/StagingContext.scala
+++ compiler/src/dotty/tools/dotc/core/StagingContext.scala
@@ -14,7 +14,7 @@ object StagingContext {
   /** A key to be used in a context property that tracks the quoteation stack.
    *  Stack containing the QuoteContext references recieved by the surrounding quotes.
    */
-  private val QuoteQontextStack = new Property.Key[List[tpd.Tree]]
+  private val QuoteContextStack = new Property.Key[List[tpd.Tree]]
 
   /** All enclosing calls that are currently inlined, from innermost to outermost. */
   def level(implicit ctx: Context): Int =
@@ -26,9 +26,9 @@ object StagingContext {
 
   /** Context with an incremented quotation level and pushes a refecence to a QuoteContext on the quote context stack */
   def pushQuoteContext(qctxRef: tpd.Tree)(implicit ctx: Context): Context =
-    val old = ctx.property(QuoteQontextStack).getOrElse(List.empty)
+    val old = ctx.property(QuoteContextStack).getOrElse(List.empty)
     ctx.fresh.setProperty(QuotationLevel, level + 1)
-             .setProperty(QuoteQontextStack, qctxRef :: old)
+             .setProperty(QuoteContextStack, qctxRef :: old)
 
   /** Context with a decremented quotation level. */
   def spliceContext(implicit ctx: Context): Context =
@@ -40,9 +40,9 @@ object StagingContext {
   def popQuoteContext()(implicit ctx: Context): (Option[tpd.Tree], Context) =
     val ctx1 = ctx.fresh.setProperty(QuotationLevel, level - 1)
     val head =
-      ctx.property(QuoteQontextStack) match
+      ctx.property(QuoteContextStack) match
         case Some(x :: xs) =>
-          ctx1.setProperty(QuoteQontextStack, xs)
+          ctx1.setProperty(QuoteContextStack, xs)
           Some(x)
         case _ =>
           None // Splice at level 0 or lower
