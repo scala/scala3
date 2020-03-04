6b78d3f93d0aebd7415a8c0ad517a29e6aa14fe8
Martin Odersky <odersky@gmail.com>
2020-03-04 14:21:53 +0100

    Fix test

--- tests/neg/i4564a.scala
+++ tests/neg/i4564a.scala
@@ -3,4 +3,4 @@ class BaseCP[T] {
   def apply(x: T): ClashPoly = if (???) ClashPoly(1) else ???
 }
 object ClashPoly extends BaseCP[Int]
-case class ClashPoly private(x: Int) // private method apply cannot override method apply in class BaseCP
+case class ClashPoly private(x: Int) // error: private method apply cannot override method apply in class BaseCP
