3a502e53a91f0083388e848e78bb61f60101c1fa
Nicolas Stucki <nicolas.stucki@gmail.com>
2020-03-18 18:32:33 +0100

    Add string interpolator `apply` macro prototype
    This is just an example of how to encode String interpolator macros
    that used an object with and `apply` and `unapply` in the extension class.

---
+++ tests/run-macros/xml-interpolation-7/Macros_1.scala
@@ -0,0 +1,34 @@
+import scala.quoted._
+import scala.quoted.autolift
+
+import scala.language.implicitConversions
+
+case class Xml(parts: String, args: List[Any])
+
+object XmlQuote {
+
+  // Encoding for
+  //
+  // implicit class SCOps(s: StringContext) {
+  //   object xml {
+  //     def apply(exprs: Any*) = ...
+  //     def unapplySeq(...) = ...
+  //   }
+  // }
+  object XMLOps {
+    opaque type StringContext = scala.StringContext
+    def (ctx: scala.StringContext).xml: StringContext = ctx
+  }
+
+  inline def (inline ctx: XMLOps.StringContext).apply(inline args: Any*): Xml =
+    ${XmlQuote.impl('ctx, 'args)}
+  // inline def (inline ctx: SCOps.StringContext).unapplySeq(...): Xml = ...
+
+
+  def impl(receiver: Expr[XMLOps.StringContext], args: Expr[Seq[Any]])(using QuoteContext): Expr[Xml] = {
+    val string = receiver match {
+      case '{ XMLOps.xml(${Unlifted(sc)}) } => sc.parts.mkString("??")
+    }
+    '{new Xml(${string}, $args.toList)}
+  }
+}
