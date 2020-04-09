ba2b6784ed4635f0f12680dc28b9cfb45747650f
Liu Fengyun <liu@fengy.me>
2020-04-09 16:24:31 +0200

    Fix test
    We need to adapt the test, otherwise the exhaustivity warning,
    which comes before, will stop the compilation, the isInstanceOf
    warnings will not be issued.

--- tests/neg-custom-args/isInstanceOf/enum-approx2.scala
+++ tests/neg-custom-args/isInstanceOf/enum-approx2.scala
@@ -2,7 +2,7 @@ sealed trait Exp[T]
 case class Fun[A, B](f: Exp[A => B]) extends Exp[A => B]
 
 class Test {
-  def eval[T](e: Exp[T]) = e match {              // error
+  def eval(e: Fun[Int, Int]) = e match {
     case Fun(x: Fun[Int, Double]) => ???          // error
     case Fun(x: Exp[Int => String]) => ???        // error
   }
