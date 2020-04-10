bd874dfec074896fd80820474a26f33894aa812d
Martin Odersky <odersky@gmail.com>
2020-04-10 10:47:46 +0200

    Warn when using -language:Scala2Compat

--- library/src/scalaShadowing/language.scala
+++ library/src/scalaShadowing/language.scala
@@ -213,7 +213,7 @@ object language {
   }
 
   /** Where imported, a backwards compatibility mode for Scala2 is enabled */
-  object Scala2Compat
+  @deprecated object Scala2Compat
 
   /** Where imported, auto-tupling is disabled */
   object noAutoTupling
