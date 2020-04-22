7ecc2ed46f3547bd81d0cfdc322d434b55d75e96
Nicolas Stucki <nicolas.stucki@gmail.com>
2020-04-22 11:33:55 +0200

    Improve quoted pattern type inference
    Types a splice applied to some arguments `$f(arg1, ..., argn)` in a quote pattern.
    
     The tree is desugared into `$f.apply(arg1, ..., argn)` where the expression `$f`
     is expected to type as a function type `(T1, ..., Tn) => R`.
     `Ti` is the type of the argument `argi` and R if the type of the prototype.
     The prototype must be fully defined to be able to infer the type of `R`.

--- tests/run-macros/quote-matching-open/Macro_1.scala
+++ tests/run-macros/quote-matching-open/Macro_1.scala
@@ -6,9 +6,9 @@ object Macro {
 
   def impl(x: Expr[Any])(using QuoteContext): Expr[Any] = {
     x match {
-      case '{ (x: Int) => ($body: Int => Int)(x) } => UnsafeExpr.open(body) { (body, close) => close(body)(Expr(2)) }
-      case '{ (x1: Int, x2: Int) => ($body: (Int, Int) => Int)(x1, x2) } => UnsafeExpr.open(body) { (body, close) => close(body)(Expr(2), Expr(3)) }
-      case '{ (x1: Int, x2: Int, x3: Int) => ($body: (Int, Int, Int) => Int)(x1, x2, x3) } => UnsafeExpr.open(body) { (body, close) => close(body)(Expr(2), Expr(3), Expr(4)) }
+      case '{ (x: Int) => $body(x): Int } => UnsafeExpr.open(body) { (body, close) => close(body)(Expr(2)) }
+      case '{ (x1: Int, x2: Int) => $body(x1, x2): Int } => UnsafeExpr.open(body) { (body, close) => close(body)(Expr(2), Expr(3)) }
+      case '{ (x1: Int, x2: Int, x3: Int) => $body(x1, x2, x3): Int } => UnsafeExpr.open(body) { (body, close) => close(body)(Expr(2), Expr(3), Expr(4)) }
     }
   }
 
