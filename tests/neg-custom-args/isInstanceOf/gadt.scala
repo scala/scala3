77bd0cc937ffc2f17c48d3af098cf32e1470338c
Liu Fengyun <liu@fengy.me>
2020-04-28 13:07:05 +0200

    Add test

---
+++ tests/neg-custom-args/isInstanceOf/gadt.scala
@@ -0,0 +1,13 @@
+class Test {
+  trait A[+T]
+  class B[T] extends A[T]
+
+  class C
+  class D extends C
+
+  def quux(a: A[C]): Unit = a match {
+    case _: B[C] => // error!!
+  }
+
+  quux(new B[D])
+}
