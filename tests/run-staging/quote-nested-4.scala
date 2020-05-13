9118e59e9c01b2c921bbdac1281a6ca49e738e68
Nicolas Stucki <nicolas.stucki@gmail.com>
2020-05-13 12:01:08 +0200

    Update tests/run-staging/quote-nested-4.scala
    Co-authored-by: Fengyun Liu <liu@fengy.me>

--- tests/run-staging/quote-nested-4.scala
+++ tests/run-staging/quote-nested-4.scala
@@ -5,7 +5,7 @@ object Test {
   given Toolbox = Toolbox.make(getClass.getClassLoader)
   def main(args: Array[String]): Unit = withQuoteContext {
 
-    val q = '{ (qtx: QuoteContext) ?=>
+    val q = '{ (using qctx: QuoteContext) =>
       val t = '[String]
       t
     }
