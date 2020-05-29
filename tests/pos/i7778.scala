491a2449eab9f6dded666bc274bc35dcee3e15b6
Liu Fengyun <liu@fengy.me>
2020-05-29 16:46:23 +0200

    Add test for different arities

--- tests/pos/i7778.scala
+++ tests/pos/i7778.scala
@@ -1,5 +1,12 @@
 object Example extends App {
-  final case class Foo[A](run: A ?=> Int) {
-    // def copy[A]: this.A ?=> Int = (using a) => run
-  }
+  final case class Foo[A](run: A ?=> Int)
+}
+
+object Example2 extends App {
+  final case class Foo[A, B](run: (A, B) ?=> Int)
+}
+
+
+object Example3 extends App {
+  final case class Foo[A, B](run: () ?=> Int)
 }
