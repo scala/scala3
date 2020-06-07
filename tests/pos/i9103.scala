c7159a9f5ca2480ac5feaaaadb4e88fdf946ac73
Martin Odersky <odersky@gmail.com>
2020-06-07 11:40:29 +0200

    Also include companions of prefixes in implicit scope
    In that way, we include all of the implicit scope of Scala 2, except
    for package objects. The idea is that everything that is reasonable
    should be included in the implicit scope.
    
    Package objects were excluded since they had a dual purpose:
    
     1. as a way to define implicits that are in scope for a whole project
     2. as a prefix for implicits of all types coming out of that project
    
    It's often that one wants only (1) but then is surprised to also get (2).

--- tests/pos/i9103.scala
+++ tests/pos/i9103.scala
@@ -4,8 +4,25 @@ object a:
 
 val b = a
 
-def Test = summon[b.Foo[Unit]]
+def test1 = summon[b.Foo[Unit]]  // no ambiguity between b.given_Foo and a.given_Foo
 
 val n: Long = 1
 val total: BigInt = 2
-val remainder: BigInt = n % identity(total)
+val remainder = n % identity(total)  // object BigInt is in implicit scope of `total.type`
+
+trait Show[T] {def show(a: T): String}
+
+object S extends LowPriorityInstances {
+  class Permissions
+}
+
+sealed trait LowPriorityInstances
+object LowPriorityInstances {
+  given S.Permissions = ???
+  given Show[S.Permissions] = _ => "perms"
+}
+
+def test =
+  println(implicitly[S.Permissions])      // companions of base classes of S are in implicit scope
+  println(implicitly[Show[S.Permissions]])
+
