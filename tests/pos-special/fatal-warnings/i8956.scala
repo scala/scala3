7309b35f4d65a98602c16a46d58ffd48f448f62d
Jamie Thompson <bishbashboshjt@gmail.com>
2020-06-08 16:00:26 +0200

    move test to be fatal-warnings

--- tests/pos-special/fatal-warnings/i8956.scala
+++ tests/pos-special/fatal-warnings/i8956.scala
@@ -8,12 +8,6 @@ def check1(i: Double | Int | String): Unit = {
     case _ => println("categorical")
   }
 }
-/*
-  [warn] 25 |      case a:Numeric => println(s"numeric = $a")
-  [warn]    |           ^^^^^^^^^
-  [warn]    |  the type test for gg.SlidingIssue.Numeric cannot be checked at runtime
-  [warn] one warning found
-  */
 def check2(i: Double | Int | String): Unit = {
   i match {
     case a:Numeric => println(s"numeric = $a")
