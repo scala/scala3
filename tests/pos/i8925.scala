e9d7789f98afa4d60deaeced1107ee67101e6e73
Jamie Thompson <bishbashboshjt@gmail.com>
2020-06-08 10:39:58 +0200

    fix #8925: check for NamedArg when inserting implicit arguments

---
+++ tests/pos/i8925.scala
@@ -0,0 +1,17 @@
+trait Parent {
+  type Child
+}
+
+object Demo {
+  def params(arr: Int): Int = ???
+
+  def parametersOf[Parent, T]()(using m: String): Int = 0
+
+  def combineInternal(using p: Parent): Int = {
+    //this implicit needs to be available
+    implicit val s: String = ""
+
+    //parameter needs to be named
+    params(arr = parametersOf[Parent, p.Child]())
+  }
+}
