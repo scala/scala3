717a65a8eacd7b5acee481b65275a223f67872a6
Nicolas Stucki <nicolas.stucki@gmail.com>
2020-06-10 09:18:24 +0200

    Fix #7716: Always take prefix into account

---
+++ tests/run-macros/i7716.check
@@ -0,0 +1,3 @@
+Hello 1
+Hello 2
+Hello 3
---
+++ tests/run-macros/i7716/Macro_1.scala
@@ -0,0 +1,17 @@
+import scala.quoted._
+
+trait Foo:
+  def mcrImpl1(e: Expr[Any])(using ctx: QuoteContext): Expr[Any] =
+    '{println(s"Hello ${$e}")}
+
+object Foo extends Foo:
+  def mcrImpl2(e: Expr[Any])(using ctx: QuoteContext): Expr[Any] =
+    '{println(s"Hello ${$e}")}
+
+object Bar:
+  import Foo._
+  inline def mcr1(e: => Any) = ${mcrImpl1('e)}
+
+  inline def mcr2(e: => Any) = ${Foo.mcrImpl1('e)}
+
+  inline def mcr3(e: => Any) = ${mcrImpl2('e)}
