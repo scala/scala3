33354dfde452404901583f43806132643f0aa731
Anatolii Kmetiuk <anatoliykmetyuk@gmail.com>
2020-06-25 11:25:13 +0200

    Make Tuple.Filter tests more elaborate

--- tests/neg/tuple-filter.scala
+++ tests/neg/tuple-filter.scala
@@ -2,5 +2,9 @@ type P[x] <: Boolean = x match {
   case 3 => false
   case _ => true
 }
-val tup: (1, 2, 3) = (1, 2, 3)
-val tup2: Tuple.Filter[(1, 2, 3, 4), P] = tup  // error
+type RejectAll[x] = false
+
+def Test =
+  summon[Tuple.Filter[(1, 2, 3, 4), P] =:= EmptyTuple] // error
+  summon[Tuple.Filter[(1, 2, 3, 4), RejectAll] =:= (1, 2, 3)] // error
+  summon[Tuple.Filter[EmptyTuple, P] =:= (1, 2, 3)] // error
