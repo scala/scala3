c88399adeac7bfd77730eafafd18e469788a5b7c
Nicolas Stucki <nicolas.stucki@gmail.com>
2020-07-08 11:32:45 +0200

    Remove QuoteContext reporting implicit conversion
    Now use `Reporting.{error, warning, throwError}` directly.

--- tests/run-macros/BigFloat/BigFloatFromDigitsImpl_1.scala
+++ tests/run-macros/BigFloat/BigFloatFromDigitsImpl_1.scala
@@ -3,14 +3,14 @@ import scala.util.FromDigits
 import scala.quoted._
 
 object BigFloatFromDigitsImpl:
-  def apply(digits: Expr[String])(using ctx: QuoteContext): Expr[BigFloat] =
+  def apply(digits: Expr[String])(using QuoteContext): Expr[BigFloat] =
     digits match
       case Const(ds) =>
         try
           val BigFloat(m, e) = BigFloat(ds)
           '{BigFloat(${Expr(m)}, ${Expr(e)})}
         catch case ex: FromDigits.FromDigitsException =>
-          ctx.error(ex.getMessage)
+          Reporting.error(ex.getMessage)
           '{BigFloat(0, 0)}
       case digits =>
         '{BigFloat($digits)}
