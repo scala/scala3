114e0d282e552901f6284b9d013e1b15495a769b
Martin Odersky <odersky@gmail.com>
2020-07-09 14:41:34 +0200

    Convert core classes (3)

--- compiler/src/dotty/tools/dotc/core/GadtConstraint.scala
+++ compiler/src/dotty/tools/dotc/core/GadtConstraint.scala
@@ -15,46 +15,46 @@ import scala.annotation.internal.sharable
 /** Represents GADT constraints currently in scope */
 sealed abstract class GadtConstraint extends Showable {
   /** Immediate bounds of `sym`. Does not contain lower/upper symbols (see [[fullBounds]]). */
-  def bounds(sym: Symbol)(implicit ctx: Context): TypeBounds
+  def bounds(sym: Symbol)(using Context): TypeBounds
 
   /** Full bounds of `sym`, including TypeRefs to other lower/upper symbols.
    *
    * @note this performs subtype checks between ordered symbols.
    *       Using this in isSubType can lead to infinite recursion. Consider `bounds` instead.
    */
-  def fullBounds(sym: Symbol)(implicit ctx: Context): TypeBounds
+  def fullBounds(sym: Symbol)(using Context): TypeBounds
 
   /** Is `sym1` ordered to be less than `sym2`? */
-  def isLess(sym1: Symbol, sym2: Symbol)(implicit ctx: Context): Boolean
+  def isLess(sym1: Symbol, sym2: Symbol)(using Context): Boolean
 
   /** Add symbols to constraint, correctly handling inter-dependencies.
    *
    * @see [[ConstraintHandling.addToConstraint]]
    */
-  def addToConstraint(syms: List[Symbol])(implicit ctx: Context): Boolean
-  def addToConstraint(sym: Symbol)(implicit ctx: Context): Boolean = addToConstraint(sym :: Nil)
+  def addToConstraint(syms: List[Symbol])(using Context): Boolean
+  def addToConstraint(sym: Symbol)(using Context): Boolean = addToConstraint(sym :: Nil)
 
   /** Further constrain a symbol already present in the constraint. */
-  def addBound(sym: Symbol, bound: Type, isUpper: Boolean)(implicit ctx: Context): Boolean
+  def addBound(sym: Symbol, bound: Type, isUpper: Boolean)(using Context): Boolean
 
   /** Is the symbol registered in the constraint?
    *
    * @note this is true even if the symbol is constrained to be equal to another type, unlike [[Constraint.contains]].
    */
-  def contains(sym: Symbol)(implicit ctx: Context): Boolean
+  def contains(sym: Symbol)(using Context): Boolean
 
   def isEmpty: Boolean
   final def nonEmpty: Boolean = !isEmpty
 
   /** See [[ConstraintHandling.approximation]] */
-  def approximation(sym: Symbol, fromBelow: Boolean)(implicit ctx: Context): Type
+  def approximation(sym: Symbol, fromBelow: Boolean)(using Context): Type
 
   def fresh: GadtConstraint
 
   /** Restore the state from other [[GadtConstraint]], probably copied using [[fresh]] */
   def restore(other: GadtConstraint): Unit
 
-  def debugBoundsDescription(implicit ctx: Context): String
+  def debugBoundsDescription(using Context): String
 }
 
 final class ProperGadtConstraint private(
@@ -71,7 +71,7 @@ final class ProperGadtConstraint private(
   )
 
   /** Exposes ConstraintHandling.subsumes */
-  def subsumes(left: GadtConstraint, right: GadtConstraint, pre: GadtConstraint)(implicit ctx: Context): Boolean = {
+  def subsumes(left: GadtConstraint, right: GadtConstraint, pre: GadtConstraint)(using Context): Boolean = {
     def extractConstraint(g: GadtConstraint) = g match {
       case s: ProperGadtConstraint => s.constraint
       case EmptyGadtConstraint => OrderingConstraint.empty
@@ -79,7 +79,7 @@ final class ProperGadtConstraint private(
     subsumes(extractConstraint(left), extractConstraint(right), extractConstraint(pre))
   }
 
-  override def addToConstraint(params: List[Symbol])(implicit ctx: Context): Boolean = {
+  override def addToConstraint(params: List[Symbol])(using Context): Boolean = {
     import NameKinds.DepParamName
 
     val poly1 = PolyType(params.map { sym => DepParamName.fresh(sym.name.toTypeName) })(
@@ -87,7 +87,7 @@ final class ProperGadtConstraint private(
         // In bound type `tp`, replace the symbols in dependent positions with their internal TypeParamRefs.
         // The replaced symbols will be later picked up in `ConstraintHandling#addToConstraint`
         // and used as orderings.
-        def substDependentSyms(tp: Type, isUpper: Boolean)(implicit ctx: Context): Type = {
+        def substDependentSyms(tp: Type, isUpper: Boolean)(using Context): Type = {
           def loop(tp: Type) = substDependentSyms(tp, isUpper)
           tp match {
             case tp @ AndType(tp1, tp2) if !isUpper =>
@@ -128,7 +128,7 @@ final class ProperGadtConstraint private(
       .reporting(i"added to constraint: [$poly1] $params%, %\n$debugBoundsDescription", gadts)
   }
 
-  override def addBound(sym: Symbol, bound: Type, isUpper: Boolean)(implicit ctx: Context): Boolean = {
+  override def addBound(sym: Symbol, bound: Type, isUpper: Boolean)(using Context): Boolean = {
     @annotation.tailrec def stripInternalTypeVar(tp: Type): Type = tp match {
       case tv: TypeVar =>
         val inst = instType(tv)
@@ -165,10 +165,10 @@ final class ProperGadtConstraint private(
     }, gadts)
   }
 
-  override def isLess(sym1: Symbol, sym2: Symbol)(implicit ctx: Context): Boolean =
+  override def isLess(sym1: Symbol, sym2: Symbol)(using Context): Boolean =
     constraint.isLess(tvarOrError(sym1).origin, tvarOrError(sym2).origin)
 
-  override def fullBounds(sym: Symbol)(implicit ctx: Context): TypeBounds =
+  override def fullBounds(sym: Symbol)(using Context): TypeBounds =
     mapping(sym) match {
       case null => null
       case tv =>
@@ -176,7 +176,7 @@ final class ProperGadtConstraint private(
           .ensuring(containsNoInternalTypes(_))
     }
 
-  override def bounds(sym: Symbol)(implicit ctx: Context): TypeBounds =
+  override def bounds(sym: Symbol)(using Context): TypeBounds =
     mapping(sym) match {
       case null => null
       case tv =>
@@ -191,9 +191,9 @@ final class ProperGadtConstraint private(
           //.ensuring(containsNoInternalTypes(_))
     }
 
-  override def contains(sym: Symbol)(implicit ctx: Context): Boolean = mapping(sym) ne null
+  override def contains(sym: Symbol)(using Context): Boolean = mapping(sym) ne null
 
-  override def approximation(sym: Symbol, fromBelow: Boolean)(implicit ctx: Context): Type = {
+  override def approximation(sym: Symbol, fromBelow: Boolean)(using Context): Type = {
     val res = approximation(tvarOrError(sym).origin, fromBelow = fromBelow)
     gadts.println(i"approximating $sym ~> $res")
     res
@@ -222,21 +222,21 @@ final class ProperGadtConstraint private(
   override protected def constraint = myConstraint
   override protected def constraint_=(c: Constraint) = myConstraint = c
 
-  override def isSubType(tp1: Type, tp2: Type)(implicit ctx: Context): Boolean = ctx.typeComparer.isSubType(tp1, tp2)
-  override def isSameType(tp1: Type, tp2: Type)(implicit ctx: Context): Boolean = ctx.typeComparer.isSameType(tp1, tp2)
+  override def isSubType(tp1: Type, tp2: Type)(using Context): Boolean = ctx.typeComparer.isSubType(tp1, tp2)
+  override def isSameType(tp1: Type, tp2: Type)(using Context): Boolean = ctx.typeComparer.isSameType(tp1, tp2)
 
-   override def nonParamBounds(param: TypeParamRef)(implicit ctx: Context): TypeBounds =
+   override def nonParamBounds(param: TypeParamRef)(using Context): TypeBounds =
      constraint.nonParamBounds(param) match {
        case TypeAlias(tpr: TypeParamRef) => TypeAlias(externalize(tpr))
        case tb => tb
      }
 
-   override def fullLowerBound(param: TypeParamRef)(implicit ctx: Context): Type =
+   override def fullLowerBound(param: TypeParamRef)(using Context): Type =
      constraint.minLower(param).foldLeft(nonParamBounds(param).lo) {
        (t, u) => t | externalize(u)
      }
 
-   override def fullUpperBound(param: TypeParamRef)(implicit ctx: Context): Type =
+   override def fullUpperBound(param: TypeParamRef)(using Context): Type =
      constraint.minUpper(param).foldLeft(nonParamBounds(param).hi) { (t, u) =>
        val eu = externalize(u)
        // Any as the upper bound means "no bound", but if F is higher-kinded,
@@ -246,26 +246,26 @@ final class ProperGadtConstraint private(
 
   // ---- Private ----------------------------------------------------------
 
-  private def externalize(param: TypeParamRef)(implicit ctx: Context): Type =
+  private def externalize(param: TypeParamRef)(using Context): Type =
     reverseMapping(param) match {
       case sym: Symbol => sym.typeRef
       case null => param
     }
 
-  private def tvarOrError(sym: Symbol)(implicit ctx: Context): TypeVar =
+  private def tvarOrError(sym: Symbol)(using Context): TypeVar =
     mapping(sym).ensuring(_ ne null, i"not a constrainable symbol: $sym")
 
   private def containsNoInternalTypes(
     tp: Type,
     acc: TypeAccumulator[Boolean] = null
-  )(implicit ctx: Context): Boolean = tp match {
+  )(using Context): Boolean = tp match {
     case tpr: TypeParamRef => !reverseMapping.contains(tpr)
     case tv: TypeVar => !reverseMapping.contains(tv.origin)
     case tp =>
       (if (acc ne null) acc else new ContainsNoInternalTypesAccumulator()).foldOver(true, tp)
   }
 
-  private class ContainsNoInternalTypesAccumulator(implicit ctx: Context) extends TypeAccumulator[Boolean] {
+  private class ContainsNoInternalTypesAccumulator(using Context) extends TypeAccumulator[Boolean] {
     override def apply(x: Boolean, tp: Type): Boolean = x && containsNoInternalTypes(tp)
   }
 
@@ -275,7 +275,7 @@ final class ProperGadtConstraint private(
 
   override def toText(printer: Printer): Texts.Text = constraint.toText(printer)
 
-  override def debugBoundsDescription(implicit ctx: Context): String = {
+  override def debugBoundsDescription(using Context): String = {
     val sb = new mutable.StringBuilder
     sb ++= constraint.show
     sb += '\n'
@@ -287,25 +287,25 @@ final class ProperGadtConstraint private(
 }
 
 @sharable object EmptyGadtConstraint extends GadtConstraint {
-  override def bounds(sym: Symbol)(implicit ctx: Context): TypeBounds = null
-  override def fullBounds(sym: Symbol)(implicit ctx: Context): TypeBounds = null
+  override def bounds(sym: Symbol)(using Context): TypeBounds = null
+  override def fullBounds(sym: Symbol)(using Context): TypeBounds = null
 
-  override def isLess(sym1: Symbol, sym2: Symbol)(implicit ctx: Context): Boolean = unsupported("EmptyGadtConstraint.isLess")
+  override def isLess(sym1: Symbol, sym2: Symbol)(using Context): Boolean = unsupported("EmptyGadtConstraint.isLess")
 
   override def isEmpty: Boolean = true
 
-  override def contains(sym: Symbol)(implicit ctx: Context) = false
+  override def contains(sym: Symbol)(using Context) = false
 
-  override def addToConstraint(params: List[Symbol])(implicit ctx: Context): Boolean = unsupported("EmptyGadtConstraint.addToConstraint")
-  override def addBound(sym: Symbol, bound: Type, isUpper: Boolean)(implicit ctx: Context): Boolean = unsupported("EmptyGadtConstraint.addBound")
+  override def addToConstraint(params: List[Symbol])(using Context): Boolean = unsupported("EmptyGadtConstraint.addToConstraint")
+  override def addBound(sym: Symbol, bound: Type, isUpper: Boolean)(using Context): Boolean = unsupported("EmptyGadtConstraint.addBound")
 
-  override def approximation(sym: Symbol, fromBelow: Boolean)(implicit ctx: Context): Type = unsupported("EmptyGadtConstraint.approximation")
+  override def approximation(sym: Symbol, fromBelow: Boolean)(using Context): Type = unsupported("EmptyGadtConstraint.approximation")
 
   override def fresh = new ProperGadtConstraint
   override def restore(other: GadtConstraint): Unit =
     if (!other.isEmpty) sys.error("cannot restore a non-empty GADTMap")
 
-  override def debugBoundsDescription(implicit ctx: Context): String = "EmptyGadtConstraint"
+  override def debugBoundsDescription(using Context): String = "EmptyGadtConstraint"
 
   override def toText(printer: Printer): Texts.Text = "EmptyGadtConstraint"
 }
