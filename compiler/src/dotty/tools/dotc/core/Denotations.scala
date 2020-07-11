c5e4942af0b186fec90f35a826de240ad2a77894
Martin Odersky <odersky@gmail.com>
2020-07-11 12:44:41 +0200

    Drop redundant isContextualMethod comparisons
    isImplicitMethod is implied by isContextualMethod, no need to test the two separately.

--- compiler/src/dotty/tools/dotc/core/Denotations.scala
+++ compiler/src/dotty/tools/dotc/core/Denotations.scala
@@ -540,7 +540,7 @@ object Denotations {
         tp2 match
           case tp2: MethodType
           if ctx.typeComparer.matchingMethodParams(tp1, tp2)
-             && (tp1.isImplicitMethod || tp1.isContextualMethod) == (tp2.isImplicitMethod || tp2.isContextualMethod)
+             && tp1.isImplicitMethod == tp2.isImplicitMethod
              && tp1.isErasedMethod == tp2.isErasedMethod =>
             val resType = infoMeet(tp1.resType, tp2.resType.subst(tp2, tp1), safeIntersection)
             if resType.exists then
