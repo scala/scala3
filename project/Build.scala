5bc5c661325582e4d734a6b0507bd5fc0204bd52
Anatolii Kmetiuk <anatoliykmetyuk@gmail.com>
2020-07-22 14:47:08 +0200

    Release Dotty 0.25.0

--- project/Build.scala
+++ project/Build.scala
@@ -64,7 +64,7 @@ object MyScalaJSPlugin extends AutoPlugin {
 object Build {
   val referenceVersion = "0.24.0-RC1"
 
-  val baseVersion = "0.25.0-RC2"
+  val baseVersion = "0.25.0"
   val baseSbtDottyVersion = "0.4.2"
 
   // Versions used by the vscode extension to create a new project
