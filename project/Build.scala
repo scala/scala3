61c5049abca71b9af21a15cba25dd448d7595baa
Anatolii Kmetiuk <anatoliykmetyuk@gmail.com>
2020-07-24 13:52:54 +0200

    Upgrade Dotty to 0.26.0-RC1

--- project/Build.scala
+++ project/Build.scala
@@ -62,7 +62,7 @@ object MyScalaJSPlugin extends AutoPlugin {
 }
 
 object Build {
-  val referenceVersion = "0.26.0-bin-20200712-3456e85-NIGHTLY"
+  val referenceVersion = "0.26.0-RC1"
 
   val baseVersion = "0.27.0"
   val baseSbtDottyVersion = "0.4.2"
