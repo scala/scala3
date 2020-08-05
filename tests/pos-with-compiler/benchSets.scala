1418c04682dd7354b6ab5f835ea1f2bd31579258
Martin Odersky <odersky@gmail.com>
2020-08-05 17:11:52 +0200

    Make LinearIdentitySet use our own HashSet
    Looking at these benchmarks, our own HashSet is clearly the fastest
    so we should use it wherever possible.
    
    First tests without GC:
    ```
    java.util.HashMap          took 1977.537756 ms
    java.util.IdentityHashMap  took 2123.028843 ms
    scala.collection.HashMap   took 2116.062172 ms
    scala.collection.HashSet   took 1998.378418 ms
    dotty.tools.dotc.HashSet   took 1429.826866 ms
    ```
    Second tests with System.gc() run before every test:
    ```
    java.util.HashMap          took 1980.977419 ms
    java.util.IdentityHashMap  took 2082.178216 ms
    scala.collection.HashMap   took 2101.882151 ms
    scala.collection.HashSet   took 1967.379402 ms
    dotty.tools.dotc.HashSet   took 1450.755273 ms
    ```

---
+++ tests/pos-with-compiler/benchSets.scala
@@ -0,0 +1,146 @@
+type Elem = Object
+
+def newElem() = new Object()
+
+val MissFactor = 2
+
+val Runs = 100          // number of runs to warm-up, and then number of runs to test
+val ItersPerRun = 1000
+val elems = Array.fill(1024 * MissFactor)(newElem())
+
+def testJava =
+  val set = java.util.HashMap[Elem, Elem]()
+  var count = 0
+  var iter = 0
+  while iter < ItersPerRun do
+    var i = 0
+    while i < elems.length do
+      val e = elems(i)
+      if i % MissFactor == 0 then
+        set.put(e, e)
+      i += 1
+    while i > 0 do
+      i -= 1
+      if set.containsKey(elems(i)) then
+        count += 1
+    iter += 1
+  count
+
+def testJavaId =
+  val set = java.util.IdentityHashMap[Elem, Elem]()
+  var count = 0
+  var iter = 0
+  while iter < ItersPerRun do
+    var i = 0
+    while i < elems.length do
+      val e = elems(i)
+      if i % MissFactor == 0 then
+        set.put(e, e)
+      i += 1
+    while i > 0 do
+      i -= 1
+      if set.containsKey(elems(i)) then
+        count += 1
+    iter += 1
+  count
+
+def testScalaMap =
+  val set = scala.collection.mutable.HashMap[Elem, Elem]()
+  var count = 0
+  var iter = 0
+  while iter < ItersPerRun do
+    var i = 0
+    while i < elems.length do
+      val e = elems(i)
+      if i % MissFactor == 0 then
+        set.update(e, e)
+      i += 1
+    while i > 0 do
+      i -= 1
+      if set.contains(elems(i)) then
+        count += 1
+    iter += 1
+  count
+
+def testDottySet =
+  val set = dotty.tools.dotc.util.HashSet[Elem](64)
+  var count = 0
+  var iter = 0
+  while iter < ItersPerRun do
+    var i = 0
+    while i < elems.length do
+      val e = elems(i)
+      if i % MissFactor == 0 then
+        set += e
+      i += 1
+    while i > 0 do
+      i -= 1
+      if set.contains(elems(i)) then
+        count += 1
+    iter += 1
+  count
+
+def testScalaSet =
+  val set = scala.collection.mutable.HashSet[Elem]()
+  var count = 0
+  var iter = 0
+  while iter < ItersPerRun do
+    var i = 0
+    while i < elems.length do
+      val e = elems(i)
+      if i % MissFactor == 0 then
+        set += e
+      i += 1
+    while i > 0 do
+      i -= 1
+      if set.contains(elems(i)) then
+        count += 1
+    iter += 1
+  count
+
+def testLinearSet =
+  var set = dotty.tools.dotc.util.LinearIdentitySet.empty[Elem]
+  var count = 0
+  var iter = 0
+  while iter < ItersPerRun do
+    var i = 0
+    while i < elems.length do
+      val e = elems(i)
+      if i % MissFactor == 0 then
+        set += e
+      i += 1
+    while i > 0 do
+      i -= 1
+      if set.contains(elems(i)) then
+        count += 1
+    iter += 1
+  count
+
+
+val expected = (elems.size / MissFactor) * ItersPerRun
+
+def profile(name: String, op: => Int) =
+  for i <- 0 until 100 do assert(op == expected)
+  //System.gc()
+  val start = System.nanoTime()
+  var count = 0
+  for i <- 0 until 100 do count += op
+  //println(count)
+  assert(count == expected * Runs)
+  println(f"$name took ${(System.nanoTime().toDouble - start)/1_000_000} ms")
+
+@main def Test =
+  profile("java.util.HashMap         ", testJava)
+  profile("java.util.IdentityHashMap ", testJavaId)
+  profile("scala.collection.HashMap  ", testScalaMap)
+  profile("scala.collection.HashSet  ", testScalaSet)
+  profile("dotty.tools.dotc.HashSet  ", testDottySet)
+  profile("dotty.tools.dotc.LinearSet", testLinearSet)
+
+  profile("dotty.tools.dotc.LinearSet", testLinearSet)
+  profile("dotty.tools.dotc.HashSet  ", testDottySet)
+  profile("scala.collection.HashSet  ", testScalaSet)
+  profile("scala.collection.HashMap  ", testScalaMap)
+  profile("java.util.IdentityHashMap ", testJavaId)
+  profile("java.util.HashMap         ", testJava)
+
