d850991cd8f782c2f811b7cbe527123cf1d6b200
Guillaume Martres <smarter@ubuntu.com>
2020-09-01 00:40:24 +0200

    Record ordering constraint between HK type variables
    If `?F <: [X] => ?G[X]`, then the ordering part of the constraint should
    record `?F <: ?G`, otherwise constraints won't be propagated correctly.
    
    It's not clear to me if we should make a similar change in `stripParams`
    but note that doing so would require being a bit careful: since we use
    the upper bound of a type variable to determine its kind, we can't just
    replace it by `[X] => ?G[X]` by `AnyKind`.
    
    Fixes #9676.

--- compiler/src/dotty/tools/dotc/core/OrderingConstraint.scala
+++ compiler/src/dotty/tools/dotc/core/OrderingConstraint.scala
@@ -2,7 +2,7 @@ package dotty.tools
 package dotc
 package core
 
-import Types._, Contexts._, Symbols._, Decorators._
+import Types._, Contexts._, Symbols._, Decorators._, TypeApplications._
 import util.SimpleIdentityMap
 import collection.mutable
 import printing.Printer
@@ -360,13 +360,15 @@ class OrderingConstraint(private val boundsMap: ParamBounds,
    *    Q <: tp  implies  Q <: P      and isUpper = true, or
    *    tp <: Q  implies  P <: Q      and isUpper = false
    */
-  private def dependentParams(tp: Type, isUpper: Boolean): List[TypeParamRef] = tp match
+  private def dependentParams(tp: Type, isUpper: Boolean)(using Context): List[TypeParamRef] = tp match
     case param: TypeParamRef if contains(param) =>
       param :: (if (isUpper) upper(param) else lower(param))
     case tp: AndType if isUpper  =>
       dependentParams(tp.tp1, isUpper) | (dependentParams(tp.tp2, isUpper))
     case tp: OrType if !isUpper =>
       dependentParams(tp.tp1, isUpper).intersect(dependentParams(tp.tp2, isUpper))
+    case EtaExpansion(tycon) =>
+      dependentParams(tycon, isUpper)
     case _ =>
       Nil
 
