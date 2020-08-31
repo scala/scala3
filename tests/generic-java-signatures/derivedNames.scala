273068cf39267948da3363d05933eb5f1ab306f6
Liu Fengyun <liu@fengy.me>
2020-08-31 17:26:36 +0200

    Tolerate printing different on different platforms
    Note that Mac agrees with Windows in printing.
    The compiler generated signature is the same across platforms:
    
    signature for method m = ()LTest$Foo$A<LTest$Foo<TT1;>.B$;>;

--- tests/generic-java-signatures/derivedNames.scala
+++ tests/generic-java-signatures/derivedNames.scala
@@ -1,8 +1,11 @@
+import java.lang.reflect.ParameterizedType
+
 object Test {
   def main(args: Array[String]): Unit = {
     val objectB = classOf[Foo[Any]].getClasses
-    val returnType = objectB(1).getDeclaredMethod("m").getGenericReturnType
-    println(returnType)
+    val returnType = objectB(1).getDeclaredMethod("m").getGenericReturnType.asInstanceOf[ParameterizedType]
+    if (scala.util.isWin) assert(returnType.toString == "Test$Foo.Test$Foo$A<Test.Test$Foo<T1>.B$>")
+    else assert(returnType.toString == "Test$Foo$A<Test$Foo<T1>$B$>")
   }
   class Foo[T1] {
     class A[T2]
