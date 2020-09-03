29ddc17b94317c3c81cf3b5cf9fed786ed0ce0a7
Martin Odersky <odersky@gmail.com>
2020-09-03 10:45:57 +0200

    Convert sets in TreeChecker to util.HashSet

--- compiler/src/dotty/tools/dotc/transform/TreeChecker.scala
+++ compiler/src/dotty/tools/dotc/transform/TreeChecker.scala
@@ -160,8 +160,8 @@ class TreeChecker extends Phase with SymTransformer {
 
   class Checker(phasesToCheck: Seq[Phase]) extends ReTyper with Checking {
 
-    private val nowDefinedSyms = new mutable.HashSet[Symbol]
-    private val patBoundSyms = new mutable.HashSet[Symbol]
+    private val nowDefinedSyms = util.HashSet[Symbol]()
+    private val patBoundSyms = util.HashSet[Symbol]()
     private val everDefinedSyms = MutableSymbolMap[untpd.Tree]()
 
     // don't check value classes after typer, as the constraint about constructors doesn't hold after transform
