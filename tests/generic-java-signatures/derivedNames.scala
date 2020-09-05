19c66577c953de7a0c562932154806527280e23d
Liu Fengyun <liu@fengy.me>
2020-09-05 11:38:28 +0200

    Fix derivedNames test on Windows
    On Windows, both outcomes are possible.
    It might be related to JDK version on Windows.

--- tests/generic-java-signatures/derivedNames.scala
+++ tests/generic-java-signatures/derivedNames.scala
@@ -4,10 +4,12 @@ object Test {
   def main(args: Array[String]): Unit = {
     val objectB = classOf[Foo[Any]].getClasses
     val returnType = objectB(1).getDeclaredMethod("m").getGenericReturnType.asInstanceOf[ParameterizedType]
+    val out1 = "Test$Foo.Test$Foo$A<Test.Test$Foo<T1>.B$>" // Windows and OSX
+    val out2 = "Test$Foo$A<Test$Foo<T1>$B$>"               // Linux and sometimes Windows
     if (scala.util.Properties.isWin)
-      assert(returnType.toString == "Test$Foo.Test$Foo$A<Test.Test$Foo<T1>.B$>")
+      assert(returnType.toString == out1 || returnType.toString == out2)
     else
-      assert(returnType.toString == "Test$Foo$A<Test$Foo<T1>$B$>")
+      assert(returnType.toString == out2)
   }
   class Foo[T1] {
     class A[T2]
