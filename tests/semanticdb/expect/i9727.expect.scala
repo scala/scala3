b51151b9138bfa11eea427344c55b0bc9d8fa23a
bishabosha <bishbashboshjt@gmail.com>
2020-09-10 16:06:51 +0200

    fix #9727: use point span for primary constructor in semanticdb

---
+++ tests/semanticdb/expect/i9727.expect.scala
@@ -0,0 +1,5 @@
+package i9727
+
+class Test/*<-i9727::Test#*/(a/*<-i9727::Test#a.*/: Int/*->scala::Int#*/)
+val a = new T/*<-i9727::i9727$package.*//*<-i9727::i9727$package.a.*/est/*->i9727::Test#*/(1)
+val b/*<-i9727::i9727$package.b.*/ = new Test/*->i9727::Test#*/(2)
