3560062625291e9b7d4429d2f1a576bcbf5d5220
Nicolas Stucki <nicolas.stucki@gmail.com>
2020-09-15 16:04:07 +0200

    Update library/src/scala/tasty/reflect/Types.scala
    Co-authored-by: Fengyun Liu <liu@fengy.me>

--- library/src/scala/tasty/reflect/Types.scala
+++ library/src/scala/tasty/reflect/Types.scala
@@ -292,7 +292,7 @@ trait Types {
   /** Pattern representing `X | Y | ...` alternatives. */
   type Alternatives <: Tree
 
-  /** A type, kind, type bounds or NoPrefix */
+  /** A type, type constructors, type bounds or NoPrefix */
   type Type
 
   /** A singleton type representing a known constant value */
