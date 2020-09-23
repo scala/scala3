8590b1a2b80f6a6dd6290f4bc5a96c6e00fcfc19
Jamie Thompson <bishbashboshjt@gmail.com>
2020-09-23 15:23:12 +0200

    fix #9766: extensions cannot be extension_foo

--- tests/neg/illegal-extension.scala
+++ tests/neg/illegal-extension.scala
@@ -1,3 +1,5 @@
 trait A {
   def extension_n: String = "illegal method" // error: illegal method name: extension_n may not start with `extension_`
 }
+
+extension (x: Any) def extension_foo: String = "foo" // error: illegal method name: extension_foo may not start with `extension_`
