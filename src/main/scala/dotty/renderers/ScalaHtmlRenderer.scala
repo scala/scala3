9f167ac40d41880094a28848841c5d661df2a1d7
Guillaume Raffin <theelectronwill@gmail.com>
2020-10-01 02:10:53 +0200

    Fix HTML layout

--- src/main/scala/dotty/renderers/ScalaHtmlRenderer.scala
+++ src/main/scala/dotty/renderers/ScalaHtmlRenderer.scala
@@ -5,22 +5,27 @@ import org.jetbrains.dokka.pages._
 import org.jetbrains.dokka.model._
 import org.jetbrains.dokka._
 import scalatags.Text.all._
+import scalatags.Text.tags2.{title, main, nav}
 import scalatags.Text.TypedTag
 import collection.JavaConverters._
 import com.virtuslab.dokka.site.SiteRenderer
+import com.virtuslab.dokka.site.BaseStaticSiteProcessor
 import java.net.URI
+import java.util.{List => JList, Set => JSet}
 import kotlinx.html.FlowContent
 import kotlinx.html.stream.StreamKt
 import kotlinx.html.Gen_consumer_tagsKt
 
-
 class ScalaHtmlRenderer(ctx: DokkaContext) extends SiteRenderer(ctx) {
-    override def buildTable(f: FlowContent, node: ContentTable, pageContext: ContentPage, sourceSetRestriciton: java.util.Set[DisplaySourceSet]) = {
+
+    type FlowContentConsumer = kotlin.jvm.functions.Function1[? >: kotlinx.html.FlowContent, kotlin.Unit]
+
+    override def buildTable(f: FlowContent, node: ContentTable, pageContext: ContentPage, sourceSetRestriciton: JSet[DisplaySourceSet]) = {
         if(node.getStyle.asScala.toSet.contains(TableStyle.DescriptionList)) withHtml(f, buildDescriptionList(node, pageContext, sourceSetRestriciton))
         else super.buildTable(f, node, pageContext, sourceSetRestriciton)
     }
 
-    override def wrapGroup(f: FlowContent, node: ContentGroup, pageContext: ContentPage, childrenCallback: kotlin.jvm.functions.Function1[? >: kotlinx.html.FlowContent, kotlin.Unit]) = {
+    override def wrapGroup(f: FlowContent, node: ContentGroup, pageContext: ContentPage, childrenCallback: FlowContentConsumer) = {
         val additionalClasses = node.getStyle.asScala.map(_.toString.toLowerCase).mkString("", ",", "")
         def buildSymbol: String = div(cls := s"symbol $additionalClasses")(
             raw(
@@ -30,24 +35,24 @@ class ScalaHtmlRenderer(ctx: DokkaContext) extends SiteRenderer(ctx) {
         if node.getDci.getKind == ContentKind.Symbol && node.getStyle.asScala.toSet.contains(TextStyle.Monospace) then withHtml(f, buildSymbol) else super.wrapGroup(f, node, pageContext, childrenCallback)
     }
 
-    override def buildContentNode(f: FlowContent, node: ContentNode, pageContext: ContentPage, sourceSetRestriciton: java.util.Set[DisplaySourceSet]) = {
+    override def buildContentNode(f: FlowContent, node: ContentNode, pageContext: ContentPage, sourceSetRestriciton: JSet[DisplaySourceSet]) = {
         node match {
             case n: HtmlContentNode => withHtml(f, raw(n.body).toString)
             case other => super.buildContentNode(f, node, pageContext, sourceSetRestriciton)
         }
     }
 
-    def buildDescriptionList(node: ContentTable, pageContext: ContentPage, sourceSetRestriciton: java.util.Set[DisplaySourceSet]) = {
+    def buildDescriptionList(node: ContentTable, pageContext: ContentPage, sourceSetRestriciton: JSet[DisplaySourceSet]) = {
         val children = node.getChildren.asScala.toList.zipWithIndex
         dl(cls := "attributes")(
-            children.map((e, i) => 
-                if(i % 2 == 0) 
+            children.map((e, i) =>
+                if(i % 2 == 0)
                     dt(
                         raw(
                             buildWithKotlinx(e, pageContext, sourceSetRestriciton)
                         )
                     )
-                else 
+                else
                     dd(
                         raw(
                             buildWithKotlinx(e, pageContext, sourceSetRestriciton)
@@ -81,22 +86,98 @@ class ScalaHtmlRenderer(ctx: DokkaContext) extends SiteRenderer(ctx) {
         })
     }
 
-    private def buildWithKotlinx(node: ContentNode, pageContext: ContentPage, sourceSetRestriciton: java.util.Set[DisplaySourceSet]): String = {
+    override def buildHtml(page: PageNode, resources: JList[String], kotlinxContent: FlowContentConsumer): String =
+        val (pageTitle, pageResources, fromTemplate) = page match
+            case static: BaseStaticSiteProcessor.StaticPageNode =>
+                val res = if static.hasFrame then resources else static.resources
+                val title = static.getLoadedTemplate.getTemplateFile.title
+                (title, res, !static.hasFrame)
+            case _ =>
+                (page.getName, resources, false)
+        html(
+            head(
+                meta(charset := "utf-8"),
+                meta(name := "viewport", content := "width=device-width, initial-scale=1"),
+                title(pageTitle),
+                linkResources(page, pageResources.asScala).toSeq,
+                script(raw(s"""var pathToRoot = "${getLocationProvider.pathToRoot(page)}";"""))
+            ),
+            body(
+                if fromTemplate then
+                    raw(buildWithKotlinx(kotlinxContent))
+                else
+                    div(
+                        id := "container",
+                        div(
+                            id := "leftColumn",
+                            div(id := "logo"),
+                            div(id := "paneSearch"),
+                            nav(id := "sideMenu"),
+                        ),
+                        div(
+                            id := "main",
+                            div (
+                                id := "leftToggler",
+                                span(cls := "icon-toggler")
+                            ),
+                            div(id := "searchBar"),
+                            main(
+                                raw(buildWithKotlinx(kotlinxContent))
+                            ),
+                            footer(
+                                span(cls := "go-to-top-icon")(
+                                    a(href := "#container")(
+                                        span(cls:="icon-vertical_align_top"),
+                                        raw("&nbsp;Back to top")
+                                    )
+                                ),
+                                span(cls := "pull-right")(
+                                    raw("Generated by&nbsp;"),
+                                    a(href := "https://github.com/lampepfl/scala3doc")("Scala3doc")
+                                )
+                            )
+                        )
+                    ),
+                    script(`type` := "text/javascript", src := resolveRoot(page, "scripts/pages.js")),
+                    script(`type` := "text/javascript", src := resolveRoot(page, "scripts/main.js"))
+            )
+        ).toString
+
+    private def resolveRoot(page: PageNode, path: String) =
+        getLocationProvider.pathToRoot(page) + path
+
+    private def linkResources(page: PageNode, resources: Iterable[String]): Iterable[Frag] =
+        def fileExtension(url: String): String =
+            val param = url.indexOf('?')
+            val end = if param < 0 then url.length else param
+            val point = url.lastIndexOf('.', end)
+            url.substring(point+1, end)
+
+        def resolveLink(url: String): String =
+            if URI(url).isAbsolute then url else resolveRoot(page, url)
+
+        for res <- resources yield
+            fileExtension(res) match
+                case "css" => link(rel := "stylesheet", href := resolveLink(res))
+                case "js" => script(`type` := "text/javascript", src := resolveLink(res), defer)
+                case _ => raw(res)
+
+    private def buildWithKotlinx(node: ContentNode, pageContext: ContentPage, sourceSetRestriction: JSet[DisplaySourceSet]): String = {
         val res = Gen_consumer_tagsKt.div(
             StreamKt.createHTML(true, false),
-            null, 
-            (div) => {build(node, div, pageContext, sourceSetRestriciton); kotlin.Unit.INSTANCE}
+            null,
+            (div) => {build(node, div, pageContext, sourceSetRestriction); kotlin.Unit.INSTANCE}
         ).toString.stripPrefix("<div>").stripSuffix("</div>\n")
         res
     }
 
-    private def buildWithKotlinx(func: kotlin.jvm.functions.Function1[? >: kotlinx.html.FlowContent, kotlin.Unit]) = {
+    private def buildWithKotlinx(func: FlowContentConsumer): String = {
         val res = Gen_consumer_tagsKt.div(
             StreamKt.createHTML(true, false),
-            null, 
+            null,
             func
         ).toString.stripPrefix("<div>").stripSuffix("</div>\n")
         res
     }
-    
+
 }
