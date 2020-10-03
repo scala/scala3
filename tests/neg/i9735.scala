079b4f9c1209cdbfe92f2f7247226751c3239ea9
Martin Odersky <odersky@gmail.com>
2020-10-03 13:16:54 +0200

    Document restriction

--- tests/neg/i9735.scala
+++ tests/neg/i9735.scala
@@ -5,3 +5,8 @@ opaque type T = [A] =>> [B] =>> String // error: opaque type alias cannot have m
 opaque type S = [B] =>> String // ok
 opaque type IArray[+T] = Array[? <: T] // ok
 opaque type S2[B] = String // ok
+
+opaque type BadF[T] = [U] =>> (T, U)    // error
+opaque type BadG = [T] =>> [U] =>> (T, U) // error
+
+
