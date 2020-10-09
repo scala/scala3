knofz-qt/scala3doc

Add members from implicit conversions and extensions. Extend packages with extensions and givens.

src/main/scala/dotty/dokka/model/extras.scala
@@ -75,6 +75,30 @@ case class ClasslikeExtension(
 
 object ClasslikeExtension extends BaseKey[DClasslike, ClasslikeExtension]
 
+
+case class PackageExtension(
+  extensions: List[ExtensionGroup],
+  givens: List[Documentable]
+) extends ExtraProperty[DPackage]:
+  override def getKey = PackageExtension
+
+object PackageExtension extends BaseKey[DPackage, PackageExtension]:
+  def apply(ce: ClasslikeExtension): PackageExtension = PackageExtension(ce.extensions, ce.givens)
+
+case class ImplicitMembers(
+  methods: Map[DFunction, DRI] = Map.empty,
+  inheritedMethods: Map[DFunction, DRI] = Map.empty,
+  properties: Map[DProperty, DRI] = Map.empty,
+  givens: Map[Documentable, DRI] = Map.empty,
+  classlikes: Map[DClasslike, DRI] = Map.empty,
+  extensions: Map[ExtensionGroup, DRI] = Map.empty,
+  inheritedExtensions: Map[DFunction, DRI] = Map.empty,
+) extends ExtraProperty[DClasslike]:
+  override def getKey = ImplicitMembers
+
+object ImplicitMembers extends BaseKey[DClasslike, ImplicitMembers]
+
+
 case class SourceLinks(
   links: Map[DokkaConfiguration$DokkaSourceSet, String]
 ) extends ExtraProperty[Documentable]:
@@ -97,6 +121,11 @@ object PropertyExtension extends BaseKey[DProperty, PropertyExtension]
 
 case class AnnotationsInfo(val annotations: List[AnnotationsInfo.Annotation]) extends ExtraProperty[Documentable]:
     override def getKey = AnnotationsInfo
+
+case class ImplicitConversions(val conversions: List[ImplicitConversion]) extends ExtraProperty[WithScope]:
+  override def getKey = ImplicitConversions
+
+object ImplicitConversions extends BaseKey[WithScope, ImplicitConversions]
 
 object AnnotationsInfo extends BaseKey[Documentable, AnnotationsInfo]:
     case class Annotation(val dri: DRI, val params: List[AnnotationParameter])

src/main/scala/dotty/dokka/model/scalaModel.scala
@@ -61,3 +61,5 @@ case class HtmlContentNode(
   override def getChildren: JList[ContentNode] = Nil.asJava
   override def getExtra = extra
   override def withNewExtras(p: PropertyContainer[ContentNode]) = copy(extra = p)
+
+case class ImplicitConversion(conversion: DRI, from: DRI, to: DRI)

scala/scala3

src/main/scala/dotty/dokka/model/extras.scala
@@ -84,6 +84,28 @@ case class InheritedDefinitions(
 
 object ClasslikeExtension extends BaseKey[DClasslike, ClasslikeExtension]
 
+
+case class PackageExtension(
+  extensions: List[ExtensionGroup],
+  givens: List[Documentable]
+) extends ExtraProperty[DPackage]:
+  override def getKey = PackageExtension
+
+object PackageExtension extends BaseKey[DPackage, PackageExtension]:
+  def apply(ce: ClasslikeExtension): PackageExtension = PackageExtension(ce.extensions, ce.givens)
+
+case class ImplicitMembers(
+  methods: Map[DFunction, Documentable] = Map.empty,
+  inheritedMethods: Map[DFunction, Documentable] = Map.empty,
+  properties: Map[DProperty, Documentable] = Map.empty,
+  inheritedProperties: Map[DProperty, Documentable] = Map.empty,
+  inheritedExtensions: Map[DFunction, Documentable] = Map.empty
+) extends ExtraProperty[DClasslike]:
+  override def getKey = ImplicitMembers
+
+object ImplicitMembers extends BaseKey[DClasslike, ImplicitMembers]
+
+
 case class SourceLinks(
   links: Map[DokkaConfiguration$DokkaSourceSet, String]
 ) extends ExtraProperty[Documentable]:
@@ -106,6 +128,11 @@ object PropertyExtension extends BaseKey[DProperty, PropertyExtension]
 
 case class AnnotationsInfo(val annotations: List[AnnotationsInfo.Annotation]) extends ExtraProperty[Documentable]:
     override def getKey = AnnotationsInfo
+
+case class ImplicitConversions(val conversions: List[ImplicitConversion]) extends ExtraProperty[WithScope]:
+  override def getKey = ImplicitConversions
+
+object ImplicitConversions extends BaseKey[WithScope, ImplicitConversions]
 
 object AnnotationsInfo extends BaseKey[Documentable, AnnotationsInfo]:
     case class Annotation(val dri: DRI, val params: List[AnnotationParameter])

src/main/scala/dotty/dokka/model/scalaModel.scala
@@ -75,3 +75,4 @@ object ScalaTagWrapper {
     descTag: DocTag
   ) extends NamedTagWrapper(descTag, name, null)
 }
+case class ImplicitConversion(conversion: Documentable, from: DRI, to: DRI)