51fbf368bf207e05e51f4a7c6456956875813774
Martin Odersky <odersky@gmail.com>
2020-10-14 16:19:00 +0200

    Fix tests
    The tests in the commit were erroneously classified due to the bug in sameTypes

--- tests/neg-macros/quote-pattern-implemnetation-restrictions.scala
+++ tests/neg-macros/quote-pattern-implemnetation-restrictions.scala
@@ -4,9 +4,9 @@ def f(x: Expr[Any])(using QuoteContext) =
   x match {
     case '{ class Foo; () } => // error
     case '{ object Foo; () } => // error
-    case '{ 1 match { case _ => () } } => // error
-    case '{ try 1 finally () } => // error
-    case '{ try 1 catch { case _ => 4 } } => // error
+    case '{ 1 match { case _ => () } } => // error // error
+    case '{ try 1 finally () } => // error // error
+    case '{ try 1 catch { case _ => 4 } } => // error // error
     case '{ Nil.map({ case x: Int => () }) } => // error
     case '{ def f: Int = return 2 } => // error
   }
