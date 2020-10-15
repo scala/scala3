7c76651e6235bd7e9c0120e2258da5ffb0a3d301
Nicolas Stucki <nicolas.stucki@gmail.com>
2020-10-15 14:15:46 +0200

    Check if .jar exists first

--- compiler/src/dotty/tools/dotc/Driver.scala
+++ compiler/src/dotty/tools/dotc/Driver.scala
@@ -89,24 +89,23 @@ class Driver {
       // Resolve classpath and class names of tasty files
       val (classPaths, classNames) = fileNames0.flatMap { name =>
         val path = Paths.get(name)
-        if name.endsWith(".jar") then
+        if !name.endsWith(".jar") && !name.endsWith(".tasty") then // is class name
+          ("", name) :: Nil // TODO remove this case. We cannot rely on an expected tasty file beeing loaded.
+        else if !Files.exists(path) then
+          report.error(s"File does not exist: $name")
+          Nil
+        else if name.endsWith(".jar") then
           new dotty.tools.io.Jar(File(name)).toList.collect {
             case e if e.getName.endsWith(".tasty") =>
               (name, e.getName.stripSuffix(".tasty").replace("/", "."))
           }
-        else if (!name.endsWith(".tasty"))
-          ("", name) :: Nil
-        else if Files.exists(path) then
-          TastyFileUtil.getClassName(path) match {
-            case Some(res) => res:: Nil
+        else
+          assert(name.endsWith(".tasty"))
+          TastyFileUtil.getClassName(path) match
+            case Some(res) => res :: Nil
             case _ =>
               report.error(s"Could not load classname from: $name")
               Nil
-          }
-        else {
-          report.error(s"File does not exist: $name")
-          Nil
-        }
       }.unzip
       val ctx1 = ctx0.fresh
       val classPaths1 = classPaths.distinct.filter(_ != "")
