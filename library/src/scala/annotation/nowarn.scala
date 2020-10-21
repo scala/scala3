scala/scala

Add `@nowarn` annotation for local warning suppression
Integrate the fantastic [silencer](https://github.com/ghik/silencer)
compiler plugin by @ghik into the compiler, which allows suppressing
warnings locally using he `@nowarn` annotation.

The `@nowarn` annotation suppresses warnings within the scope covered by
the annotation.
  - `@nowarn def foo = ...`, `@nowarn class C { ... }` suppress
    warnings in a definition
  - `expression: @nowarn` suppress warnings in a specific expression

The annotation can be configured to filter selected warnings, for
example `@nowarn("cat=deprecation")` only suppresses deprecation
warnings. The filter configuration syntax is the same as in `-Wconf`.

MiMa exception for addition of `scala.annotation.nowarn`

Reporting warnings is now delayed until after typer, because the typer
collects `@nowarn` annotations.

If we stop before typer (e.g., because there are errors in the parser),
all warnings are shown, even if they are enclosed in `@silent`.

If a phase before typer has both errors and warnings, all errors are
printed before the warnings.

src/library/scala/annotation/nowarn.scala
@@ -1,0 +1,34 @@
+/*
+ * Scala (https://www.scala-lang.org)
+ *
+ * Copyright EPFL and Lightbend, Inc.
+ *
+ * Licensed under Apache License 2.0
+ * (http://www.apache.org/licenses/LICENSE-2.0).
+ *
+ * See the NOTICE file distributed with this work for
+ * additional information regarding copyright ownership.
+ */
+
+package scala.annotation
+
+/** An annotation for local warning suppression.
+  *
+  * The optional `value` parameter allows selectively silencing messages, see `scalac -Wconf:help`
+  * for help. Examples:
+  *
+  * {{{
+  *   def f = {
+  *     1: @nowarn // don't warn "a pure expression does nothing in statement position"
+  *     2
+  *   }
+  *
+  *   @nowarn def f = { 1; deprecated() } // don't warn
+  *
+  *   @nowarn("msg=pure expression does nothing")
+  *   def f = { 1; deprecated() } // show deprecation warning
+  * }}}
+  *
+  * To ensure that a `@nowarn` annotation actually suppresses a warning, enable `-Xlint:nowarn`.
+  */
+class nowarn(value: String = "") extends ConstantAnnotation

scala/scala3

(cherry picked from commit 5a93cba2a2d1ade8de6bcd0e42d4a74cab8e16c4)

library/src/scala/annotation/nowarn.scala
@@ -1,0 +1,34 @@
+/*
+ * Scala (https://www.scala-lang.org)
+ *
+ * Copyright EPFL and Lightbend, Inc.
+ *
+ * Licensed under Apache License 2.0
+ * (http://www.apache.org/licenses/LICENSE-2.0).
+ *
+ * See the NOTICE file distributed with this work for
+ * additional information regarding copyright ownership.
+ */
+
+package scala.annotation
+
+/** An annotation for local warning suppression.
+  *
+  * The optional `value` parameter allows selectively silencing messages, see `scalac -Wconf:help`
+  * for help. Examples:
+  *
+  * {{{
+  *   def f = {
+  *     1: @nowarn // don't warn "a pure expression does nothing in statement position"
+  *     2
+  *   }
+  *
+  *   @nowarn def f = { 1; deprecated() } // don't warn
+  *
+  *   @nowarn("msg=pure expression does nothing")
+  *   def f = { 1; deprecated() } // show deprecation warning
+  * }}}
+  *
+  * To ensure that a `@nowarn` annotation actually suppresses a warning, enable `-Xlint:nowarn`.
+  */
+class nowarn(value: String = "") extends ClassfileAnnotation