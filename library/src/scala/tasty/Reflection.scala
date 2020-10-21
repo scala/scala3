2b1c0eef181b3faf9983d07bdfcf3f7c05ad3159
Ruslan Shevchenko <ruslan@shevchenko.kiev.ua>
2020-10-21 16:12:26 +0300

    overload overloaded
    Co-authored-by: Nicolas Stucki <nicolas.stucki@gmail.com>

--- library/src/scala/tasty/Reflection.scala
+++ library/src/scala/tasty/Reflection.scala
@@ -514,6 +514,9 @@ trait Reflection { reflection =>
 
     // TODO rename, this returns an Apply and not a Select
     /** Call an overloaded method with the given type and term parameters */
+    def overloaded(qualifier: Term, name: String, targs: List[Type], args: List[Term]): Apply
+      
+    /** Call an overloaded method with the given type and term parameters */
     def overloaded(qualifier: Term, name: String, targs: List[Type], args: List[Term], returnType: Type): Apply
 
     def copy(original: Tree)(qualifier: Term, name: String): Select
