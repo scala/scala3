scala/scala

ListMap is reverse oriented, so use that for foldRight

src/library/scala/collection/immutable/ListMap.scala
@@ -44,6 +44,11 @@ object ListMap extends ImmutableMapFactory[ListMap] {
 
   @SerialVersionUID(-8256686706655863282L)
   private object EmptyListMap extends ListMap[Any, Nothing]
+
+  @tailrec private def foldRightInternal[A, B, Z](map: ListMap[A, B], prevValue: Z, op: ((A, B), Z) => Z): Z = {
+    if (map.isEmpty) prevValue
+    else foldRightInternal(map.init, op(map.last, prevValue), op)
+  }
 }
 
 /**
@@ -127,6 +132,7 @@ sealed class ListMap[A, +B] extends AbstractMap[A, B]
   protected def value: B = throw new NoSuchElementException("value of empty map")
   protected def next: ListMap[A, B] = throw new NoSuchElementException("next of empty map")
 
+  override def foldRight[Z](z: Z)(op: ((A, B), Z) => Z): Z = ListMap.foldRightInternal(this, z, op)
   override def stringPrefix = "ListMap"
 
   /**

scala/scala3

(cherry picked from commit 79231276356457d68d091d9ffe7681f7e358dfbf)

library/src/scala/collection/immutable/ListMap.scala
@@ -107,6 +107,7 @@ sealed class ListMap[K, +V]
   private[immutable] def value: V = throw new NoSuchElementException("value of empty map")
   private[immutable] def next: ListMap[K, V] = throw new NoSuchElementException("next of empty map")
 
+  override def foldRight[Z](z: Z)(op: ((K, V), Z) => Z): Z = ListMap.foldRightInternal(this, z, op)
   override protected[this] def className = "ListMap"
 
 }
@@ -272,6 +273,11 @@ object ListMap extends MapFactory[ListMap] {
     * @tparam V the map value type
     */
   def newBuilder[K, V]: ReusableBuilder[(K, V), ListMap[K, V]] = new ListMapBuilder[K, V]
+
+  @tailrec private def foldRightInternal[K, V, Z](map: ListMap[K, V], prevValue: Z, op: ((K, V), Z) => Z): Z = {
+    if (map.isEmpty) prevValue
+    else foldRightInternal(map.init, op(map.last, prevValue), op)
+  }
 }
 
 /** Builder for ListMap.