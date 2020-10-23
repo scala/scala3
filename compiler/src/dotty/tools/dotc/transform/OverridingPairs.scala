d46c5a2e0b079e709e132d9a0396edd1c78b4446
Martin Odersky <odersky@gmail.com>
2020-10-23 11:54:56 +0200

    Optimize OverridingPairs

--- compiler/src/dotty/tools/dotc/transform/OverridingPairs.scala
+++ compiler/src/dotty/tools/dotc/transform/OverridingPairs.scala
@@ -47,17 +47,14 @@ object OverridingPairs {
     private val decls = {
       val decls = newScope
       // fill `decls` with overriding shadowing overridden */
-      def fillDecls(bcs: List[Symbol], deferred: Boolean): Unit = bcs match {
-        case bc :: bcs1 =>
-          fillDecls(bcs1, deferred)
+      def fillDecls(bcs: List[Symbol], deferred: Boolean): Unit =
+        bcs.foreachReversed { bc =>
           var e = bc.info.decls.lastEntry
-          while (e != null) {
-            if (e.sym.is(Deferred) == deferred && !exclude(e.sym))
+          while e != null do
+            if e.sym.is(Deferred) == deferred && !exclude(e.sym) then
               decls.enter(e.sym)
             e = e.prev
-          }
-        case nil =>
-      }
+        }
       // first, deferred (this will need to change if we change lookup rules!
       fillDecls(base.info.baseClasses, deferred = true)
       // then, concrete.
