ce53d89d774faeefd67dd3a8155fe767861c3e65
Martin Odersky <odersky@gmail.com>
2020-11-05 13:24:38 +0100

    Add hint to error message
    In case of a double definition error that's due to an erasure clash,
    add a hint that one can resolve it with a @targetName annotation

--- compiler/src/dotty/tools/dotc/reporting/messages.scala
+++ compiler/src/dotty/tools/dotc/reporting/messages.scala
@@ -2079,7 +2079,10 @@ import transform.SymUtils._
             case MethodNotAMethodMatch =>
               "neither has parameters."
             case FullMatch =>
-              i"have the same$nameAnd type after erasure."
+              i"""have the same$nameAnd type after erasure.
+                 |
+                 |Consider adding a @targetName annotation to one of the conflicting definitions
+                 |for disambiguation."""
           }
         }
         else ""
