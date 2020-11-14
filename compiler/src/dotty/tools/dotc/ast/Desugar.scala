e4494c338a0bd6053ae03fb3a19d0c8506e8810c
Martin Odersky <odersky@gmail.com>
2020-11-14 10:53:57 +0100

    Drop unused attachment

--- compiler/src/dotty/tools/dotc/ast/Desugar.scala
+++ compiler/src/dotty/tools/dotc/ast/Desugar.scala
@@ -23,11 +23,6 @@ object desugar {
   import untpd._
   import DesugarEnums._
 
-  /** If a Select node carries this attachment, suppress the check
-   *  that its type refers to an acessible symbol.
-   */
-  val SuppressAccessCheck: Property.Key[Unit] = Property.Key()
-
   /** An attachment for companion modules of classes that have a `derives` clause.
    *  The position value indicates the start position of the template of the
    *  deriving class.
