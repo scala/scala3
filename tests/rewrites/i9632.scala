15acff871d21372be3ea4d98119c0342bd232b3e
Martin Odersky <odersky@gmail.com>
2020-11-16 12:40:31 +0100

    More test cases for refinements and anonymous classes

--- tests/rewrites/i9632.scala
+++ tests/rewrites/i9632.scala
@@ -5,3 +5,17 @@ def f(using scala.quoted.QuoteContext) =
         }
         x
      }
+
+type HasPath = {
+  def getPath: String
+}
+type HashPath2 = Any {
+  def getPath: String
+}
+val x = new {
+  val y = 1
+}
+val y = new AnyRef {
+  val y = 1
+}
+
