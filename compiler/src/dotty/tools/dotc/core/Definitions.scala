f4e1ca4f565c74fbe1aa56ea88ce6f59409ccdf0
Nicolas Stucki <nicolas.stucki@gmail.com>
2020-11-20 15:19:44 +0100

    Remove TupledFunction
    We remove from functions form 3.0 as it is not required for any other language feature. This can be added into 3.1 or later version.

--- compiler/src/dotty/tools/dotc/core/Definitions.scala
+++ compiler/src/dotty/tools/dotc/core/Definitions.scala
@@ -894,10 +894,6 @@ class Definitions {
     lazy val RuntimeTuple_isInstanceOfEmptyTuple: Symbol = RuntimeTupleModule.requiredMethod("isInstanceOfEmptyTuple")
     lazy val RuntimeTuple_isInstanceOfNonEmptyTuple: Symbol = RuntimeTupleModule.requiredMethod("isInstanceOfNonEmptyTuple")
 
-  @tu lazy val TupledFunctionTypeRef: TypeRef = requiredClassRef("scala.util.TupledFunction")
-  def TupledFunctionClass(using Context): ClassSymbol = TupledFunctionTypeRef.symbol.asClass
-  def RuntimeTupleFunctionsModule(using Context): Symbol = requiredModule("scala.runtime.TupledFunctions")
-
   // Annotation base classes
   @tu lazy val AnnotationClass: ClassSymbol = requiredClass("scala.annotation.Annotation")
   @tu lazy val ClassfileAnnotationClass: ClassSymbol = requiredClass("scala.annotation.ClassfileAnnotation")
