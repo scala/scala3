01dcb5952a20cad3b7c1eceab49f39cb74a50f00
Aleksander Boruch-Gruszecki <aleksander.boruch-gruszecki@epfl.ch>
2020-11-23 11:38:00 +0100

    Scala3doc: remove outdated extension name handling

--- scala3doc/src/dotty/dokka/tasty/NameNormalizer.scala
+++ scala3doc/src/dotty/dokka/tasty/NameNormalizer.scala
@@ -6,8 +6,7 @@ trait NameNormalizer { self: TastyParser =>
   import qctx.reflect._
   extension (s: Symbol) def normalizedName: String = {
     val withoutGivenPrefix = if s.isGiven then s.name.stripPrefix("given_") else s.name
-    val withoutExtensionPrefix = if s.isExtensionMethod then withoutGivenPrefix.stripPrefix("extension_") else withoutGivenPrefix
-    val withoutObjectSuffix = if s.flags.is(Flags.Object) then withoutExtensionPrefix.stripSuffix("$") else withoutExtensionPrefix
+    val withoutObjectSuffix = if s.flags.is(Flags.Object) then withoutGivenPrefix.stripSuffix("$") else withoutGivenPrefix
     val constructorNormalizedName = if s.isClassConstructor then "this" else withoutObjectSuffix
     constructorNormalizedName
   }
