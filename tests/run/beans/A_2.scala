81e7d81bfb1286160feb183cab4055621cd068d3
Paweł Marks <pmarks@virtuslab.com>
2020-12-02 13:56:10 +0100

    Add test for overriding bean property

--- tests/run/beans/A_2.scala
+++ tests/run/beans/A_2.scala
@@ -1,4 +1,4 @@
-class A {
+class A:
   @scala.beans.BeanProperty val x = 4
   @scala.beans.BooleanBeanProperty val y = true
   @scala.beans.BeanProperty var mutableOneWithLongName = "some text"
@@ -6,4 +6,12 @@ class A {
   @scala.beans.BeanProperty
   @beans.LibraryAnnotation_1
   val retainingAnnotation = 5
-}
+
+trait T:
+  @scala.beans.BeanProperty val x: Int
+
+class T1 extends T:
+  override val x = 5
+
+class T2 extends T1:
+  override val x = 10
