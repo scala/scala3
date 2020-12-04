c0220ba460e4a5aad2cd1f09ab56779bd4015532
Sébastien Doeraene <sjrdoeraene@gmail.com>
2020-12-04 16:12:28 +0100

    Remove the non implemented setting -Yparallel-pickler.

--- compiler/src/dotty/tools/dotc/config/ScalaSettings.scala
+++ compiler/src/dotty/tools/dotc/config/ScalaSettings.scala
@@ -176,7 +176,6 @@ class ScalaSettings extends Settings.SettingGroup with CommonScalaSettings {
   val YprintDebugOwners: Setting[Boolean] = BooleanSetting("-Yprint-debug-owners", "When printing trees, print owners of definitions.")
   val YshowPrintErrors: Setting[Boolean] = BooleanSetting("-Yshow-print-errors", "Don't suppress exceptions thrown during tree printing.")
   val YtestPickler: Setting[Boolean] = BooleanSetting("-Ytest-pickler", "Self-test for pickling functionality; should be used with -Ystop-after:pickler.")
-  val YparallelPickler: Setting[Boolean] = BooleanSetting("-Yparallel-pickler", "Run part of the pickling phase in parallel, disable because it breaks some tests.")
   val YcheckReentrant: Setting[Boolean] = BooleanSetting("-Ycheck-reentrant", "Check that compiled program does not contain vars that can be accessed from a global root.")
   val YdropComments: Setting[Boolean] = BooleanSetting("-Ydrop-comments", "Drop comments when scanning source files.")
   val YcookComments: Setting[Boolean] = BooleanSetting("-Ycook-comments", "Cook the comments (type check `@usecase`, etc.)")
