5a6542b3bbec44f8cda76e1c8792bebde075989a
Martin Odersky <odersky@gmail.com>
2020-12-05 20:03:17 +0100

    Update test

--- tests/neg-custom-args/fatal-warnings/opaque-match.scala
+++ tests/neg-custom-args/fatal-warnings/opaque-match.scala
@@ -3,6 +3,8 @@ case class C()
 object O:
   opaque type T <: C = C
   val x: T = C()
+  (??? : Any) match
+    case _: T => ??? // OK
 
 def Test[T] =
   O.x match
@@ -14,6 +16,8 @@ def Test[T] =
 
   (??? : Any) match
     case _: List[O.T] => ???  // error
+  (??? : Any) match
+    case _: List[O.T @unchecked] => ???  // OK 
   (??? : Any) match
     case _: List[T] => ???  // error
 
