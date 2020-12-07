9b7f7ab6edaa6bcf5dea3db3ae48d5d077fe20c9
Andrzej Ratajczak <andrzej.ratajczak98@gmail.com>
2020-12-07 16:31:52 +0100

    Apply requested changes to diagram tests

--- scala3doc/src/dotty/dokka/model/api/membersUtils.scala
+++ scala3doc/src/dotty/dokka/model/api/membersUtils.scala
@@ -5,23 +5,23 @@ import scala.jdk.CollectionConverters.{ListHasAsScala, SeqHasAsJava}
 import dotty.dokka.model.api._
 
 extension (m: DModule)
-    def visitMembers(callback: Member => Unit): Unit = 
-        def visitClasslike(c: Member, callback: Member => Unit): Unit =
-            callback(c)
-            c.allMembers.foreach(visitClasslike(_, callback))
-        m.getPackages.asScala.foreach(_.allMembers.foreach(visitClasslike(_, callback)))
+  def visitMembers(callback: Member => Unit): Unit = 
+    def visitClasslike(c: Member): Unit =
+      callback(c)
+      c.allMembers.foreach(visitClasslike(_))
+    m.getPackages.asScala.foreach(_.allMembers.foreach(visitClasslike(_)))
 
 extension (s: Signature)
-    def getName: String =
-        s.map { _ match
-            case s: String => s
-            case l: Link => l.name
-        }.mkString 
+  def getName: String =
+    s.map {
+      case s: String => s
+      case l: Link => l.name
+    }.mkString 
 
 extension (m: Member):
-    def getDirectParentsAsStrings: Seq[String] =
-        m.directParents.map(_.getName).sorted
-    def getParentsAsStrings: Seq[String] =
-        m.parents.map(_.signature.getName).sorted
-    def getKnownChildrenAsStrings: Seq[String] =   
-        m.knownChildren.map(_.signature.getName).sorted
+  def getDirectParentsAsStrings: Seq[String] =
+    m.directParents.map(_.getName).sorted
+  def getParentsAsStrings: Seq[String] =
+    m.parents.map(_.signature.getName).sorted
+  def getKnownChildrenAsStrings: Seq[String] =   
+    m.knownChildren.map(_.signature.getName).sorted
