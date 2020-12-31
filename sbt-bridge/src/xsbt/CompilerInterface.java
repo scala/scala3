70ef66a6e122fea6516a31f775cf7d75b77e47fe
Adrien Piquerez <adrien.piquerez@gmail.com>
2020-12-31 13:00:40 +0100

    Remove CompilerClassLoader

--- sbt-bridge/src/xsbt/CompilerInterface.java
+++ sbt-bridge/src/xsbt/CompilerInterface.java
@@ -6,17 +6,9 @@ package xsbt;
 import xsbti.AnalysisCallback;
 import xsbti.Logger;
 import xsbti.Reporter;
-import xsbti.Severity;
 import xsbti.compile.*;
 
 import java.io.File;
-
-import dotty.tools.dotc.core.Contexts.ContextBase;
-import dotty.tools.dotc.Main;
-import dotty.tools.dotc.interfaces.*;
-
-import java.lang.reflect.InvocationTargetException;
-import java.net.URLClassLoader;
 
 /**
  * The new compiler interface is [[dotty.tools.xsbt.CompilerBridge]] that extends the new `xsbti.CompilerInterface2`.
@@ -24,20 +16,7 @@ import java.net.URLClassLoader;
  */
 public final class CompilerInterface {
   public CachedCompiler newCompiler(String[] options, Output output, Logger initialLog, Reporter initialDelegate) {
-    // The classloader that sbt uses to load the compiler bridge is broken
-    // (see CompilerClassLoader#fixBridgeLoader for details). To workaround
-    // this we construct our own ClassLoader and then run the following code
-    // with it:
-    //   new CachedCompilerImpl(options, output)
-
-    try {
-      ClassLoader bridgeLoader = this.getClass().getClassLoader();
-      ClassLoader fixedLoader = CompilerClassLoader.fixBridgeLoader(bridgeLoader);
-      Class<?> cciClass = fixedLoader.loadClass("xsbt.CachedCompilerImpl");
-      return (CachedCompiler) cciClass.getConstructors()[0].newInstance(options, output);
-    } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | InvocationTargetException e) {
-      throw new RuntimeException(e);
-    }
+    return new CachedCompilerImpl(options, output);
   }
 
   public void run(File[] sources, DependencyChanges changes, AnalysisCallback callback, Logger log,
