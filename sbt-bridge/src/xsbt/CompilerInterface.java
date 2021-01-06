c93f13faeb0af10a01c4a31faedc022f09f8702b
Adrien Piquerez <adrien.piquerez@gmail.com>
2021-01-06 11:42:29 +0100

    Fallback on CompilerClassLoader

--- sbt-bridge/src/xsbt/CompilerInterface.java
+++ sbt-bridge/src/xsbt/CompilerInterface.java
@@ -8,6 +8,9 @@ import xsbti.Logger;
 import xsbti.Reporter;
 import xsbti.compile.*;
 
+import dotty.tools.dotc.Main;
+
+import java.lang.reflect.InvocationTargetException;
 import java.io.File;
 
 /**
@@ -15,8 +18,40 @@ import java.io.File;
  * This interface is kept for compatibility with Mill and the sbt 1.3.x series.
  */
 public final class CompilerInterface {
-  public CachedCompiler newCompiler(String[] options, Output output, Logger initialLog, Reporter initialDelegate) {
-    return new CachedCompilerImpl(options, output);
+  public CachedCompiler newCompiler(String[] options, Output output, Logger initialLog, Reporter initialDelegate) throws java.lang.Exception {
+    if(isClassLoaderValid()) {
+      return new CachedCompilerImpl(options, output);
+    } else {
+      initialLog.warn(() ->
+        "The compiler class loader is badly configured.\n" +
+        "Consider using a more recent version of your build tool:\n" +
+        " - sbt >= 1.4.0 and sbt-dotty >= 1.5.0\n" +
+        " - Mill >= 0.9.3-21-002361\n" +
+        " - Bloop >= 1.4.6-23-20a501bc"
+      );
+      // To workaround the wrong class loader, we construct our own and run
+      // the following code with it:
+      //   new CachedCompilerImpl(options, output)
+      try {
+        ClassLoader bridgeLoader = this.getClass().getClassLoader();
+        ClassLoader fixedLoader = CompilerClassLoader.fixBridgeLoader(bridgeLoader);
+        Class<?> cciClass = fixedLoader.loadClass("xsbt.CachedCompilerImpl");
+        return (CachedCompiler) cciClass.getConstructors()[0].newInstance(options, output);
+      } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | InvocationTargetException e) {
+        throw new RuntimeException(e);
+      }
+    }
+  }
+
+  private boolean isClassLoaderValid() {
+    // Check that the `xsbti.*` classes are loaded from the same class loader in the compiler and the bridge
+    ClassLoader compilerClassLoader = Main.class.getClassLoader();
+    Class<Logger> clazz = Logger.class;
+    try {
+      return compilerClassLoader.loadClass("xsbti.Logger") == clazz;
+    } catch (ClassNotFoundException e) {
+      throw new RuntimeException(e);
+    }
   }
 
   public void run(File[] sources, DependencyChanges changes, AnalysisCallback callback, Logger log,
