450863e7d99e0f114e8249bb4bcabeb295507122
Martin Odersky <odersky@gmail.com>
2021-02-01 14:54:43 +0100

    Use all available context info for healing ambiguous implicits
    When retrying after an ambiguous implicit, we now make use of all the
    information in the prototype, including ignored parts. We also try
    to match formal parameters with actually given arguments.
    
    Fixes #11243
    Fixes #5773, which previously was closed with a more detailed error message.

--- tests/pos/i5773b.scala
+++ tests/pos/i5773b.scala
@@ -10,7 +10,7 @@ object Semigroup {
 
   implicit def sumSemigroup[N](implicit N: Numeric[N]): Semigroup[N] = new {
     extension (lhs: N) override def append(rhs: N): N = N.plus(lhs, rhs)
-    extension (lhs: Int) def appendS(rhs: N): N = ??? // N.plus(lhs, rhs)
+    extension (lhs: Int) override def appendS(rhs: N): N = ??? // N.plus(lhs, rhs)
   }
 }
 
@@ -18,7 +18,7 @@ object Semigroup {
 object Main {
   import Semigroup.sumSemigroup // this is not sufficient
   def f1 = {
-    println(1 appendS 2) // error This should give the following error message:
+    println(1 appendS 2) // This used to give the following error message:
 /*
 21 |    println(1 appendS 2)
    |            ^^^^^^^^^
