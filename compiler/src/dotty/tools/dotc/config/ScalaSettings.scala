9fed88707da044f573fe95a28dd1adf2076f3e37
Guillaume Martres <smarter@ubuntu.com>
2021-02-02 17:16:48 +0100

    Upgrade to ASM 9.0
    Keep up with Scala 2: https://github.com/scala/scala/pull/9477

--- compiler/src/dotty/tools/dotc/config/ScalaSettings.scala
+++ compiler/src/dotty/tools/dotc/config/ScalaSettings.scala
@@ -72,8 +72,9 @@ trait CommonScalaSettings { self: Settings.SettingGroup =>
 }
 
 class ScalaSettings extends Settings.SettingGroup with CommonScalaSettings {
+  // Keep synchronized with `classfileVersion` in `BCodeIdiomatic`
   private val minTargetVersion = 8
-  private val maxTargetVersion = 15
+  private val maxTargetVersion = 16
 
   private def supportedTargetVersions: List[String] =
     (minTargetVersion to maxTargetVersion).toList.map(_.toString)
