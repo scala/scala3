0823a8f0a36c8adcb760bf3e293cb41ae14b36eb
Martin Odersky <odersky@gmail.com>
2021-02-07 18:46:33 +0100

    Fix test

--- tests/pos/i11214.scala
+++ tests/pos/i11214.scala
@@ -1,5 +1,5 @@
 trait Pet(val name: String)
 trait FeatheredPet extends Pet
 
-class Bird(name: String) extends FeatheredPet:
+class Bird(override val name: String) extends FeatheredPet:
    override def toString = s"bird name: $name"
