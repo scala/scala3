8829a9195139640820713d81ec0e1e8f5a4f58cb
Phil <philwalk9@gmail.com>
2021-02-08 06:21:09 -0700

    add test sources

---
+++ compiler/test-resources/scripting/hashBang.scala
@@ -0,0 +1,20 @@
+#!/usr/bin/env scala
+export STUFF=nada
+!#
+
+def main(args: Array[String]): Unit =
+  System.err.printf("mainClassFromStack: %s\n",mainFromStack)
+  //assert(mainFromStack.contains("HashBang"),s"fromStack[$mainFromStack]")
+
+  lazy val mainFromStack:String = {
+    val result = new java.io.StringWriter()
+    new RuntimeException("stack").printStackTrace(new java.io.PrintWriter(result))
+    val stack = result.toString.split("[\r\n]+").toList
+    for( s <- stack ){
+      System.err.printf("[%s]\n",s)
+    }
+    stack.filter { str => str.contains(".main(") }.map {
+      _.replaceAll(".*[(]","").
+      replaceAll("[:)].*","")
+    }.distinct.take(1).mkString("")
+  }
