thnii957/dotty

Add configurable social links to scala3doc

scala3doc/src/dotty/renderers/HtmlRenderer.scala
@@ -154,6 +154,16 @@ class HtmlRenderer(rootPackage: Member, val members: Map[DRI, Member])(using ctx
           )
     renderNested(navigablePage)._2
 
+  private def hasSocialLinks = !args.socialLinks.isEmpty
+
+  private def socialLinks(whiteIcon: Boolean = true, additionalClass: String = "") =
+    val icon = (link: SocialLinks) => if whiteIcon then link.whiteIconName else link.blackIconName
+    args.socialLinks.map { link =>
+      a(href := link.url)(
+        span(cls := s"social-icon $additionalClass", Attr("data-icon-path") := icon(link))
+      )
+    }
+
   private def mkFrame(link: Link, parents: Vector[Link], content: => AppliedTag): AppliedTag =
     val projectLogo =
       args.projectLogo.map { path =>
@@ -177,6 +187,9 @@ class HtmlRenderer(rootPackage: Member, val members: Map[DRI, Member])(using ctx
           ),
           span(
             args.projectVersion.map(v => div(cls:="projectVersion")(v)).toList
+          ),
+          div(id := "lhs-socials")(
+            socialLinks(additionalClass = "lhs-social")
           )
         ),
         div(id := "paneSearch"),
@@ -202,12 +215,18 @@ class HtmlRenderer(rootPackage: Member, val members: Map[DRI, Member])(using ctx
               raw("&nbsp;Back to top")
             )
           ),
-          raw("Generated by&nbsp;"),
-          a(href := "https://github.com/lampepfl/dotty/tree/master/scala3doc")(
-            img(
-              src := resolveRoot(link.dri, "images/scala3doc_logo.svg"),
-              alt := "Scala3doc",
-              cls := "scala3doc_logo"
+          div(id := "footer-socials")(
+            if hasSocialLinks then Seq(raw("Social links&nbsp;")) else Nil,
+            socialLinks(whiteIcon = false, additionalClass = "footer-social")
+          ),
+          div(id := "generated-by")(
+            raw("Generated by&nbsp;"),
+            a(href := "https://github.com/lampepfl/dotty/tree/master/scala3doc")(
+              img(
+                src := resolveRoot(link.dri, "images/scala3doc_logo.svg"),
+                alt := "Scala3doc",
+                cls := "scala3doc_logo"
+              )
             )
           )
         )

scala/scala3

scaladoc/src/dotty/tools/scaladoc/renderers/HtmlRenderer.scala
@@ -172,6 +172,16 @@ class HtmlRenderer(rootPackage: Member, val members: Map[DRI, Member])(using ctx
           )
     renderNested(navigablePage)._2
 
+  private def hasSocialLinks = !args.socialLinks.isEmpty
+
+  private def socialLinks(whiteIcon: Boolean = true) =
+    val icon = (link: SocialLinks) => if whiteIcon then link.whiteIconName else link.blackIconName
+    args.socialLinks.map { link =>
+      a(href := link.url)(
+        span(cls := s"social-icon", Attr("data-icon-path") := icon(link))
+      )
+    }
+
   private def mkFrame(link: Link, parents: Vector[Link], content: => AppliedTag): AppliedTag =
     val projectLogo =
       args.projectLogo.map { path =>
@@ -195,6 +205,9 @@ class HtmlRenderer(rootPackage: Member, val members: Map[DRI, Member])(using ctx
           ),
           span(
             args.projectVersion.map(v => div(cls:="projectVersion")(v)).toList
+          ),
+          div(cls := "socials")(
+            socialLinks()
           )
         ),
         div(id := "paneSearch"),
@@ -220,14 +233,20 @@ class HtmlRenderer(rootPackage: Member, val members: Map[DRI, Member])(using ctx
               raw("&nbsp;Back to top")
             )
           ),
-          raw("Generated by&nbsp;"),
-          a(href := "https://github.com/lampepfl/dotty/tree/master/scaladoc")(
-            img(
-              src := resolveRoot(link.dri, "images/scaladoc_logo.svg"),
-              alt := "scaladoc",
-              cls := "scaladoc_logo"
+          div(cls := "socials")(
+            if hasSocialLinks then Seq(raw("Social links&nbsp;")) else Nil,
+            socialLinks(whiteIcon = false)
+          ),
+          div(id := "generated-by")(
+            raw("Generated by&nbsp;"),
+            a(href := "https://github.com/lampepfl/dotty/tree/master/scaladoc")(
+              img(
+                src := resolveRoot(link.dri, "images/scaladoc_logo.svg"),
+                alt := "scaladoc",
+                cls := "scaladoc_logo"
+              )
             )
-          )
         )
       )
+    )
     )