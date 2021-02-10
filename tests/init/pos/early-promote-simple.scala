27f0fdafe555ec4301d3cbbd0a46b2cc57f572a5
Natsu Kagami <nki@nkagami.me>
2021-02-10 21:05:26 +0900

    Add subclasses case to test

--- tests/init/pos/early-promote-simple.scala
+++ tests/init/pos/early-promote-simple.scala
@@ -2,3 +2,13 @@ class P() {
     val a = 1
     List(this)
 }
+
+class Outer {
+    class Inner {
+        val b = a
+    }
+    val a = 5
+    val b = new Inner()
+    List(new Inner())
+    List(b)
+}
