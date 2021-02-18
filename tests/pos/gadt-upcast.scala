3a4d718c38c145dfa3a5658a66b437b95d545d9e
Yichen Xu <yichen.x@outlook.com>
2021-02-18 18:02:37 +0800

    Update GADT upcasting testcase

--- tests/pos/gadt-upcast.scala
+++ tests/pos/gadt-upcast.scala
@@ -1,7 +1,8 @@
 trait TagA[A]
 trait TagB[B]
 trait TagC[C]
-trait TriTag[A, B, C] extends TagA[A] with TagB[B] with TagC[C]
+trait ProxyTagC[C] extends TagC[C]
+trait TriTag[A, B, C] extends TagA[A] with TagB[B] with ProxyTagC[C]
 class IntStrCharTag extends TagA[Int] with TagB[String] with TagC[Char]
 
 def biget[A, B, C]: TriTag[A, B, C] => (A, B, C) = {
