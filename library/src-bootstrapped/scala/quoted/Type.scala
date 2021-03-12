ed985cad26a8196730ad40918a9bc6c90dc1d854
Nicolas Stucki <nicolas.stucki@gmail.com>
2021-03-12 13:42:56 +0100

    Add quotes.Type.valueOfConstant
    With quote pattern matching it is possible to match over the structure of a type, this add to possility to extract the values from constant types.
    
    This is esential to provide handle `Mirror.{MirroredLabel, MirroredElemLabels}` witthout needing reflection.

--- library/src-bootstrapped/scala/quoted/Type.scala
+++ library/src-bootstrapped/scala/quoted/Type.scala
@@ -13,11 +13,31 @@ object Type:
 
   /** Show a source code like representation of this type without syntax highlight */
   def show[T <: AnyKind](using Type[T])(using Quotes): String =
-    import quotes.reflect._
+    import quotes.reflect.*
     TypeTree.of[T].show
 
   /** Return a quoted.Type with the given type */
   @compileTimeOnly("Reference to `scala.quoted.Type.of` was not handled by PickleQuotes")
   given of[T <: AnyKind](using Quotes): Type[T] = ???
+
+
+  /** Extracts the value of singleton constant type, None otherwise.
+   *
+   *  Example usage:
+   *  ```scala
+   *  ... match
+   *    case '{ $mirrorExpr : Mirror.Sum { type MirroredLabel = label } } =>
+   *      Type.valueOfConstant[label] // Option[String]
+   *  }
+   *  ```
+   *  @syntax markdown
+   */
+  def valueOfConstant[T](using Type[T])(using Quotes): Option[T] =
+    import quotes.reflect.*
+    def valueOf(tpe: TypeRepr): Option[T] =
+      tpe.dealias.widenTermRefByName match
+        case ConstantType(const) => Some(const.value.asInstanceOf[T])
+        case _ => None
+    valueOf(TypeRepr.of[T])
 
 end Type
