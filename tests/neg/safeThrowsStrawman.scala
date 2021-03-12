7c4e3a94cd53073ffa882d7a1d57a4359ea4a132
Martin Odersky <odersky@gmail.com>
2021-03-12 13:13:36 +0100

    Make tests variance aware

--- tests/neg/safeThrowsStrawman.scala
+++ tests/neg/safeThrowsStrawman.scala
@@ -2,10 +2,10 @@ import language.experimental.erasedTerms
 import annotation.implicitNotFound
 
 object scalax:
-  @implicitNotFound("The capability to throw exception ${E} is missing.\nThe capability can be provided by one of the following:\n - A using clause `(using CanThrow[${E}])`\n - A throws clause in a result type `X throws ${E}`\n - an enclosing `try` that catches ${E}")
-  erased class CanThrow[E <: Exception]
+  @implicitNotFound("The capability to throw exception ${E} is missing.\nThe capability can be provided by one of the following:\n - A using clause `(using CanThrow[${E}])`\n - A throws clause in a result type such as `X throws ${E}`\n - an enclosing `try` that catches ${E}")
+  erased class CanThrow[-E <: Exception]
 
-  infix type throws[R, E <: Exception] = CanThrow[E] ?=> R
+  infix type throws[R, +E <: Exception] = CanThrow[E] ?=> R
 
   class Fail extends Exception
 
@@ -16,10 +16,14 @@ import scalax._
 def foo(x: Boolean): Int =
   if x then 1 else raise(Fail())  // error
 
+def bar: Int throws Exception =
+  raise(Fail())
+
 @main def Test =
   try
     erased given CanThrow[Fail] = ???
     println(foo(true))
     println(foo(false))
+    println(bar)        // error
   catch case ex: Fail =>
     println("failed")
--- tests/run/safeThrowsStrawman.scala
+++ tests/run/safeThrowsStrawman.scala
@@ -1,9 +1,9 @@
 import language.experimental.erasedTerms
 
 object scalax:
-  erased class CanThrow[E <: Exception]
+  erased class CanThrow[-E <: Exception]
 
-  infix type throws[R, E <: Exception] = CanThrow[E] ?=> R
+  infix type throws[R, +E <: Exception] = CanThrow[E] ?=> R
 
   class Fail extends Exception
 
@@ -14,13 +14,18 @@ import scalax._
 def foo(x: Boolean): Int throws Fail =
   if x then 1 else raise(Fail())
 
-def bar(x: Boolean)(using CanThrow[Fail]): Int =
-  if x then 1 else raise(Fail())
+def bar(x: Boolean)(using CanThrow[Fail]): Int = foo(x)
+def baz: Int throws Exception = foo(false)
 
 @main def Test =
   try
     given CanThrow[Fail] = ???
     println(foo(true))
     println(foo(false))
+  catch case ex: Fail =>
+    println("failed")
+  try
+    given CanThrow[Exception] = ???
+    println(baz)
   catch case ex: Fail =>
     println("failed")
