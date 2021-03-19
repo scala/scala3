59bae0beda00811bbc0bba1b65b37dccad8cbf1a
Tom Grigg <tomegrigg@gmail.com>
2021-03-18 17:25:38 -0700

    community build: run tests for cask

--- community-build/test/scala/dotty/communitybuild/CommunityBuildTest.scala
+++ community-build/test/scala/dotty/communitybuild/CommunityBuildTest.scala
@@ -87,6 +87,7 @@ end CommunityBuildTest
 @Category(Array(classOf[TestCategory]))
 class CommunityBuildTestA extends CommunityBuildTest:
   @Test def akka = projects.akka.run()
+  @Test def cask = projects.cask.run()
   @Test def endpoints4s = projects.endpoints4s.run()
   @Test def fansi = projects.fansi.run()
   @Test def fastparse = projects.fastparse.run()
