38baae8cf50ed2bac0446d80c24f0eafebcd8136
Yichen Xu <yichen.x@outlook.com>
2021-04-04 01:15:17 +0800

    Remove errors due to previous limitation in tests/neg/structural-gadt.scala

--- tests/neg/structural-gadt.scala
+++ tests/neg/structural-gadt.scala
@@ -11,15 +11,15 @@ object Test {
   def foo[A](e: Expr { type T = A }) = e match {
     case _: IntLit =>
       val a: A = 0 // error
-      val i: Int = ??? : A // limitation // error
+      val i: Int = ??? : A
 
     case _: Expr { type T <: Int } =>
       val a: A = 0 // error
       val i: Int = ??? : A
 
     case _: IntExpr =>
-      val a: A = 0 // limitation // error
-      val i: Int = ??? : A // limitation // error
+      val a: A = 0
+      val i: Int = ??? : A
 
     case _: Expr { type T = Int } =>
       val a: A = 0
@@ -36,7 +36,7 @@ object Test {
       val i: Int = ??? : A // error
 
     case _: IntExpr =>
-      val a: A = 0 // limitation // error
+      val a: A = 0
       val i: Int = ??? : A // error
 
     case _: Expr { type T = Int } =>
