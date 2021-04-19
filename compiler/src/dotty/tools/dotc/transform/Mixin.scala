43e286d97f5141637678e7b6cd9a6adcee54ebc7
Martin Odersky <odersky@gmail.com>
2021-04-19 14:28:56 +0200

    Fix trait setter validity
    Trait setters had a validity interval that started before they were created.
    This caused stale symbol errors in macro compilations.

--- compiler/src/dotty/tools/dotc/transform/Mixin.scala
+++ compiler/src/dotty/tools/dotc/transform/Mixin.scala
@@ -10,6 +10,7 @@ import SymUtils._
 import Symbols._
 import SymDenotations._
 import Types._
+import Periods._
 import Decorators._
 import DenotTransformers._
 import StdNames._
@@ -146,6 +147,7 @@ class Mixin extends MiniPhase with SymTransformer { thisPhase =>
         // !decl.isClass avoids forcing nested traits, preventing cycles
         if !decl.isClass && needsTraitSetter(decl) then
           val setter = makeTraitSetter(decl.asTerm)
+          setter.validFor = Period(ctx.runId, thisPhase.next.id, decl.validFor.lastPhaseId)
           decls1.enter(setter)
           modified = true
       if modified then
