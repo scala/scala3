1bf1b3fab3da0b6af7ac13036a45b47c0450e9f8
Liu Fengyun <liu@fengy.me>
2021-04-21 15:17:58 +0200

    Add another test: in case Map.map becomes non-overloaded (thanks @Jasper-M)

--- tests/pos/i11185.scala
+++ tests/pos/i11185.scala
@@ -3,3 +3,12 @@ class Test:
 
   Map(1 -> 2).map(foo _)
   Map(1 -> 2).map(foo)
+
+class Test2:
+  def foo(a: Int, b: Int) = a + b
+
+  def bar(f: ((Int, Int)) => Int) = "ok"
+  def bar(f: ((Int, Int)) => String)(using Int) = "ok"
+
+  bar(foo)
+  bar(foo _)
