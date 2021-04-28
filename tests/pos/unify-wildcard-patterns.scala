741e17e7e53487170d04d88bb231c433519cf3a1
Olivier Blanvillain <olivier.blanvillain@gmail.com>
2021-04-28 09:03:59 +0200

    Fix #12260: Add underscore to match type syntax

---
+++ tests/pos/unify-wildcard-patterns.scala
@@ -0,0 +1,13 @@
+// `case _ => expr` in a match expression should be equivalant to
+// `case _: Any => expr`. Likewise, in a match type, `case _ => T`
+// should be equivalant to `case Any => T`.
+
+object Test0 {
+  type M[X] =            X match { case String => Int   case Any => String }
+  def m[X](x: X): M[X] = x match { case _: String => 1  case _: Any => "s" }
+}
+
+object Test2 {
+  type M[X] =            X match { case String => Int   case _ => String   }
+  def m[X](x: X): M[X] = x match { case _: String => 1  case _: Any => "s" }
+}
