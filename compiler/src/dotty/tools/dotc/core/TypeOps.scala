44974cc2713e52d08b792a0893aaa08367b93d51
Martin Odersky <odersky@gmail.com>
2021-04-29 11:44:55 +0200

    Improve orDominator
    Merge applied type arguments also for abstract and opaque type constructors
    
    Fixes #12264

--- compiler/src/dotty/tools/dotc/core/TypeOps.scala
+++ compiler/src/dotty/tools/dotc/core/TypeOps.scala
@@ -343,7 +343,11 @@ object TypeOps:
 
     tp match {
       case tp: OrType =>
-        approximateOr(tp.tp1, tp.tp2)
+        (tp.tp1.dealias, tp.tp2.dealias) match
+          case (tp1 @ AppliedType(tycon1, args1), tp2 @ AppliedType(tycon2, args2)) if tycon1 eq tycon2 =>
+            mergeRefinedOrApplied(tp1, tp2)
+          case (tp1, tp2) =>
+            approximateOr(tp1, tp2)
       case _ =>
         tp
     }
