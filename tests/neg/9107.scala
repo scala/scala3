Linyxus/dotty

Fix #9107: Restore constraints after MT reduction
Because of `caseLambda = constrained(cas)` in `def matchCase`, subtyping
wrongly things that it's OK to further constrain types introduced in match
type patterns. This commit fixes the issue with a stronger version of
`inFrozenConstraint` around match type reduction.

tests/neg/9107.scala
@@ -1,0 +1,14 @@
+trait M[F[_]]
+trait Inv[T]
+
+object Test {
+  def ev[X] = implicitly[
+    (X match { case Inv[t] => Int }) =:=
+    (X match { case Inv[t] => t })
+  ] // error
+
+  def ev2[X] = implicitly[
+    (M[[t] =>> internal.MatchCase[Inv[t], Int]])  =:=
+    (M[[t] =>> internal.MatchCase[Inv[t], t]])
+  ] // error
+}

scala/scala3

tests/neg/9107.scala
@@ -1,0 +1,14 @@
+trait M[F[_]]
+trait Inv[T]
+
+object Test {
+  def ev[X] = implicitly[
+    (X match { case Inv[t] => Int }) =:=
+    (X match { case Inv[t] => t })
+  ] // error
+
+  def ev2[X] = implicitly[
+    (M[[t] =>> runtime.MatchCase[Inv[t], Int]])  =:=
+    (M[[t] =>> runtime.MatchCase[Inv[t], t]])
+  ] // error
+}