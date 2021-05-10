682a596454d9a0069e29b65b22c1616755e5c318
Liu Fengyun <liu@fengy.me>
2021-05-10 10:25:29 +0200

    Update test (thanks to @Kevin-Lee)

--- tests/patmat/i12337.scala
+++ tests/patmat/i12337.scala
@@ -1,12 +1,26 @@
 sealed trait Status
 object Status {
-  case object Active   extends Status
+  case class Active(since: Int) extends Status
   case object Inactive extends Status
 }
 
 case class Foo(status: Status)
-def bar(user: Foo): Unit = user match {
-  case Foo(Status.Active) =>
-    println("active")
-    // no compile-time warning for missing Status.Inactive case
+def bar(foo: Foo): Unit = foo match {
+  case Foo(Status.Active(since)) =>
+    println(s"active since $since")
 }
+// Expected:
+// warning: match may not be exhaustive.
+// It would fail on the following input: Foo(Inactive)
+// def bar(foo: Foo): Unit = foo match {
+
+def baz(foo: Foo): Unit = foo match {
+  case Foo(Status.Active(2000)) =>
+    println("active since 2000")
+  case Foo(Status.Inactive) =>
+    println("inactive")
+}
+// Expected:
+// warning: match may not be exhaustive.
+// It would fail on the following input: Foo(Active((x: Int forSome x not in 2000)))
+// def baz(foo: Foo): Unit = foo match {
