40fbede7aab670dd4ae8076e7e230b4a82fce44a
Martin Odersky <odersky@gmail.com>
2021-05-21 17:40:52 +0200

    Prefer old instance over extremal bound when refining
    Prefer old instance over extremal bound Nothing or Any when refining

--- tests/run/i7960.scala
+++ tests/run/i7960.scala
@@ -27,5 +27,24 @@ object Test {
       Future { A.a },
       Future { B.a },
     )), 1.seconds)
+    /*
+    On the other hand, this fails:
+    
+    Await.result(Future.sequence(Seq(
+      Future { A.a },
+      Future { B.a },
+    ))(
+      scala.collection.BuildFrom.buildFromIterableOps/*[Seq, Future[A], A]*/, global
+    )
+    , 1.seconds)
+
+	the problem here is that there is not enough info to instantiate the type parameters of
+	scala.collection.BuildFrom.buildFromIterableOps. If the BuildFrom is searched as an implicit,
+	enough type variables are instantiated to correctly determine the parameters. But if the
+	`buildFromIterableOps` is given explicitly, the problem is underconstrained and the type
+	parameters are instantiated to `Nothing`.
+
+    */
+    
   }
 }
