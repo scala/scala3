80de4d24fa526a5e85e03ec56cc2943911839d39
Fengyun Liu <liu@fengy.me>
2021-05-23 19:06:52 +0200

    Update compiler/src/dotty/tools/dotc/reporting/messages.scala
    Co-authored-by: Guillaume Martres <smarter@ubuntu.com>

--- compiler/src/dotty/tools/dotc/reporting/messages.scala
+++ compiler/src/dotty/tools/dotc/reporting/messages.scala
@@ -838,7 +838,7 @@ import transform.SymUtils._
 
   class MatchCaseOnlyNullWarning()(using Context)
   extends PatternMatchMsg(MatchCaseOnlyNullWarningID) {
-    def msg = em"""Unreachable case. If ${hl("null")} is matched, consider using ${hl("case null =>")}."""
+    def msg = em"""Unreachable case except for ${hl("null")} (if this is intentional, consider writing ${hl("case null =>")} instead)."""
     def explain = ""
   }
 
