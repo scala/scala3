4c8c7f522fe4034dbaf206a13735bd175f1aa303
Martin Odersky <odersky@gmail.com>
2021-05-25 18:44:24 +0200

    Make refiners conditional on -Yrefine-types
    Add refiner tests that exercise the setting

--- compiler/src/dotty/tools/dotc/typer/PreRefine.scala
+++ compiler/src/dotty/tools/dotc/typer/PreRefine.scala
@@ -3,7 +3,7 @@ package typer
 
 import core.Phases.Phase
 import core.DenotTransformers.IdentityDenotTransformer
-import core.Contexts.Context
+import core.Contexts.{Context, ctx}
 
 /** A phase that precedes the refiner and that allows installing
  *  completers for local symbols
@@ -11,6 +11,8 @@ import core.Contexts.Context
 class PreRefine extends Phase, IdentityDenotTransformer:
 
   def phaseName: String = "preRefine"
+
+  override def isEnabled(using Context) = ctx.settings.YrefineTypes.value
 
   def run(using Context): Unit =
     assert(next.isInstanceOf[RefineTypes],
