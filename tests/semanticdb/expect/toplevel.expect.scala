78c9bc58d330cd2945c26d98fb9ddc215fe93a11
tanishiking <rikiriki1238@gmail.com>
2021-05-25 23:06:46 +0900

    Add test for #11693
    https://github.com/lampepfl/dotty/issues/11693
    > scala3-compiler-bootstrapped/Test/runMain dotty.tools.dotc.semanticdb.updateExpect

--- tests/semanticdb/expect/toplevel.expect.scala
+++ tests/semanticdb/expect/toplevel.expect.scala
@@ -4,3 +4,9 @@ def combine/*<-_empty_::toplevel$package.combine(+1).*/(x/*<-_empty_::toplevel$p
 def combine/*<-_empty_::toplevel$package.combine(+2).*/ = 0
 def foo/*<-_empty_::toplevel$package.foo().*/ = "foo"
 /*<-_empty_::MyProgram#*//*->_empty_::toplevel$package.MyProgram().*//*->scala::util::CommandLineParser.parseArgument().*//*->_empty_::MyProgram#main().(args)*//*->scala::util::CommandLineParser.FromString.given_FromString_Int.*//*->scala::util::CommandLineParser.showError().*//*->local0*/@main/*->scala::main#*/ def MyProgram/*<-_empty_::toplevel$package.MyProgram().*/(times/*<-_empty_::toplevel$package.MyProgram().(times)*/: Int/*->scala::Int#*/): Unit/*->scala::Unit#*/ = (/*->scala::LowPriorityImplicits#intWrapper().*/1 to/*->scala::runtime::RichInt#to().*/ times/*->_empty_::toplevel$package.MyProgram().(times)*/) foreach/*->scala::collection::immutable::Range#foreach().*/ (_ => println/*->scala::Predef.println(+1).*/("hello"))
+
+trait Ord/*<-_empty_::Ord#*/[T/*<-_empty_::Ord#[T]*/]:
+   def compare/*<-_empty_::Ord#compare().*/(x/*<-_empty_::Ord#compare().(x)*/: T/*->_empty_::Ord#[T]*/, y/*<-_empty_::Ord#compare().(y)*/: T/*->_empty_::Ord#[T]*/): Int/*->scala::Int#*/
+
+given intOrd/*<-_empty_::toplevel$package.intOrd.*/: Ord/*->_empty_::Ord#*/[Int/*->scala::Int#*/] with
+   def compare/*<-_empty_::toplevel$package.intOrd.compare().*/(x/*<-_empty_::toplevel$package.intOrd.compare().(x)*/: Int/*->scala::Int#*/, y/*<-_empty_::toplevel$package.intOrd.compare().(y)*/: Int/*->scala::Int#*/) = ???/*->scala::Predef.`???`().*/
--- tests/semanticdb/expect/toplevel.scala
+++ tests/semanticdb/expect/toplevel.scala
@@ -4,3 +4,9 @@ def combine(x: Int, y: Int, z: Int) = x + y + z
 def combine = 0
 def foo = "foo"
 @main def MyProgram(times: Int): Unit = (1 to times) foreach (_ => println("hello"))
+
+trait Ord[T]:
+   def compare(x: T, y: T): Int
+
+given intOrd: Ord[Int] with
+   def compare(x: Int, y: Int) = ???
