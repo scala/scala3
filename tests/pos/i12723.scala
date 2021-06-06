917f6bb389ce0e14ce3442af46018878839d5582
Tom Grigg <tomegrigg@gmail.com>
2021-06-06 11:34:47 -0700

    Add regression test for #12723

---
+++ tests/pos/i12723.scala
@@ -0,0 +1,10 @@
+class Fun[|*|[_, _]] {
+  enum ->[A, B] {
+    case BiId[X, Y]() extends ((X |*| Y) -> (X |*| Y))
+  }
+
+  def go[A, B](f: A -> B): Unit =
+    f match {
+      case ->.BiId() => ()
+    }
+}
