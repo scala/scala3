07111310b953d5d93500a3eb3d431a8390b57ede
Martin Odersky <odersky@gmail.com>
2021-06-07 16:14:13 +0200

    Add missing case in type comparer

---
+++ tests/neg-custom-args/captures/capt1.scala
@@ -0,0 +1,32 @@
+class C
+def f(x: C holds *, y: C): () => C =
+  () => if x == null then y else y  // error
+
+def g(x: C holds *, y: C): Any =
+  () => if x == null then y else y  // error
+
+def h1(x: C holds *, y: C): Any holds x.type =
+  def f() = if x == null then y else y
+  () => f()  // ok
+
+def h2(x: C holds *): Any =
+  def f(y: Int) = if x == null then y else y
+  f  // error
+
+class A
+type Cap = C holds *
+
+def h3(x: Cap): A =
+  class F(y: Int) extends A:
+    def m() = if x == null then y else y
+  F(22)  // error
+
+def h4(x: Cap, y: Int): A =
+  new A:
+    def m() = if x == null then y else y  // error
+
+//def f1(c: Cap): (() => C holds c.type) = () => c // ok
+//def f2(c: Cap): (() => C) holds c.type = () => c
+
+//def h5(x: Cap): () => C =
+//  f(x)
