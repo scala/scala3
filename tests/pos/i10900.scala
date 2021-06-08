87d0ddcbcae4f5fabeed676c0fbeadb08ce5c4af
Liu Fengyun <liu@fengy.me>
2021-06-08 15:52:51 +0200

    Add original tests

--- tests/pos/i10900.scala
+++ tests/pos/i10900.scala
@@ -1,3 +1,23 @@
 import scala.collection.IterableOps
 def foo[CC[A] <: IterableOps[A, CC, CC[A]], A](collection: CC[A]) =
   collection == collection
+
+object Test1 {
+  import scala.collection.IterableOps
+  implicit class RichCollection[CC[A] <: IterableOps[A, CC, CC[A]], A](val collection: CC[A]) {
+    def awm(update: CC[A] => CC[A]): CC[A] = {
+      val newCollection = update(collection)
+      if (newCollection == collection) collection else newCollection.awm(update)
+    }
+  }
+}
+
+object Test2 {
+  import scala.collection.IterableOps
+  implicit class RichCollection[CC[A] <: IterableOps[A, CC, CC[A]], A](val collection: CC[A]) {
+    def awm(update: CC[A] => CC[A]): CC[A] = update(collection) match {
+      case `collection` => collection
+      case updated      => updated.awm(update)
+    }
+  }
+}
