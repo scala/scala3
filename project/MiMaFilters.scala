6d6714740c1d8d2588e7ca22762c5bb247f91f5a
Martin Odersky <odersky@gmail.com>
2021-06-09 19:35:08 +0200

    Update MiMa filters

--- project/MiMaFilters.scala
+++ project/MiMaFilters.scala
@@ -4,6 +4,15 @@ import com.typesafe.tools.mima.core.ProblemFilters._
 
 object MiMaFilters {
   val Library: Seq[ProblemFilter] = Seq(
+    // Experimental API for saferExceptions
+    exclude[MissingClassProblem]("scala.CanThrow"),
+    exclude[MissingClassProblem]("scala.CanThrow$package"),
+    exclude[MissingClassProblem]("scala.CanThrow$package$"),
+    exclude[MissingClassProblem]("scala.unsafeExceptions"),
+    exclude[MissingClassProblem]("scala.unsafeExceptions$"),
+    exclude[MissingFieldProblem]("scala.runtime.stdLibPatches.language#experimental.saferExceptions"),
+    exclude[MissingClassProblem]("scala.runtime.stdLibPatches.language$experimental$saferExceptions$"),
+
     // New APIs that will be introduced in 3.1.0
     exclude[ReversedMissingMethodProblem]("scala.quoted.Quotes#reflectModule.Wildcard"),
     exclude[ReversedMissingMethodProblem]("scala.quoted.Quotes#reflectModule.WildcardTypeTest"),
