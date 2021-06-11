472c1da800e926cfb9ddbd69f3e871d675160698
Martin Odersky <odersky@gmail.com>
2021-06-11 14:09:04 +0200

    Test curried tries

---
+++ tests/neg-custom-args/captures/try3.scala
@@ -0,0 +1,26 @@
+import java.io.IOException
+
+class CanThrow[E] extends Retains[*]
+type Top  = Any retains *
+
+def handle[E <: Exception, T <: Top](op: CanThrow[E] ?=> T)(handler: E => T): T =
+  val x: CanThrow[E] = ???
+  try op(using x)
+  catch case ex: E => handler(ex)
+
+def raise[E <: Exception](ex: E)(using CanThrow[E]): Nothing =
+  throw ex
+
+@main def Test: Int =
+  def f(a: Boolean) = // error
+    handle {
+      if !a then raise(IOException())
+      (b: Boolean) =>
+        if !b then raise(IOException())
+        0
+    } {
+      ex => (b: Boolean) => -1
+    }
+  val g = f(true)
+  g(false) // would raise an uncaught exception
+  f(true)(false) // would raise an uncaught exception
