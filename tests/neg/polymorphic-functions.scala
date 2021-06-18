73926bbaeb3356107126a25ee57cc8e65bad4786
Martin Odersky <odersky@gmail.com>
2021-06-18 13:06:12 +0200

    Fix printing of poly function types

---
+++ tests/neg/polymorphic-functions.scala
@@ -0,0 +1,2 @@
+val f: [T] => (x: T) => x.type = [T] => (x: Int) => x // error
+
--- tests/pos/polymorphic-functions.scala
+++ tests/pos/polymorphic-functions.scala
@@ -1,5 +1,5 @@
 object Test {
-  val pv0: [T] => List[T] = ???        // error
-  val pv1: Any = [T] => Nil            // error
-  val pv2: [T] => List[T] = [T] => Nil // error // error
+  val pv0: [T] => List[T] = ???
+  val pv1: Any = [T] => Nil
+  val pv2: [T] => List[T] = [T] => Nil
 }
