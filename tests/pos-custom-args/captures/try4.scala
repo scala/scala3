4e0d18de6b0b7a02316e1ac6ea34167094aa78c3
Martin Odersky <odersky@gmail.com>
2021-06-22 09:53:53 +0200

    More tests

--- tests/pos-custom-args/captures/try4.scala
+++ tests/pos-custom-args/captures/try4.scala
@@ -4,8 +4,10 @@ import java.io.IOException
 
 class CanThrow[E] extends Retains[*]
 type Top  = Any retains *
+infix type ==> [A, B] = (A => B) retains *
+class OtherCap extends Retains[*]
 
-def handle[E <: Exception, T <: Top](op: CanThrow[E] ?=> T)(handler: E => T): T =
+def handle[E <: Exception, T <: Top](op: (CanThrow[E] ?=> T))(handler: (E => T) retains T): T =
   val x: CanThrow[E] = ???
   try op(using x)
   catch case ex: E => handler(ex)
@@ -13,20 +15,21 @@ def handle[E <: Exception, T <: Top](op: CanThrow[E] ?=> T)(handler: E => T): T 
 def raise[E <: Exception](ex: E)(using CanThrow[E]): Nothing =
   throw ex
 
-def test1: Int =
-  def f(a: Boolean): Boolean => CanThrow[IOException] ?=> Int =
+def test2: Int =
+  def f(c: OtherCap, a: Boolean): Boolean => CanThrow[IOException] ?=> Int =
     handle {
       if !a then raise(IOException())
       (b: Boolean) => (_: CanThrow[IOException]) ?=>
         if !b then raise(IOException())
-        0
+        1
     } {
       ex => (b: Boolean) => (_: CanThrow[IOException]) ?=> -1
     }
   handle {
-    val g = f(true)
+    val c = OtherCap()
+    val g = f(c, true)
     g(false)
-    f(true)(false)
+    f(c, true)(false)
   } {
     ex => -1
   }
