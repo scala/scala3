d87b9c270dccf1783c529d555825f754259da698
Martin Odersky <odersky@gmail.com>
2021-06-24 12:55:16 +0200

    Add regression test
    Closes #12910

---
+++ tests/pos/i12910.scala
@@ -0,0 +1,27 @@
+trait Type[T]:
+  type Out
+
+type varchar
+
+given Type[varchar] with
+  type Out = String
+
+class Placeholder[T, U]
+
+object Placeholder:
+  def apply[T](using t: Type[T]): Placeholder[T, t.Out] = new Placeholder
+
+trait Encoder[P, X]:
+  def encode(x: X): String
+
+object Encoder:
+  def apply[P, X](placeholder: P)(using e: Encoder[P, X]): X => String = e.encode
+
+  given [T, X]: Encoder[Placeholder[T, X], X] with
+    def encode(x: X): String = ???
+
+def Test =
+  // the following compiles just fine
+  Encoder(new Placeholder[varchar, String])("hello")
+  // the following fails
+  Encoder(Placeholder[varchar])("hello")
