4887b897f442ffaba5c0cedcaf649d529af808c1
Fengyun Liu <fengyun.liu@oracle.com>
2021-06-25 00:35:55 +0200

    Synchronize Symbol.defTree before analysis

--- compiler/src/dotty/tools/dotc/transform/init/Checker.scala
+++ compiler/src/dotty/tools/dotc/transform/init/Checker.scala
@@ -10,15 +10,16 @@ import dotty.tools.dotc.core._
 import Contexts._
 import Types._
 import Symbols._
+import StdNames._
 
 import dotty.tools.dotc.transform._
-import MegaPhase._
+import Phases._
 
 
 import scala.collection.mutable
 
 
-class Checker extends MiniPhase {
+class Checker extends Phase {
   import tpd._
 
   val phaseName = "initChecker"
@@ -30,9 +31,34 @@ class Checker extends MiniPhase {
   override def isEnabled(using Context): Boolean =
     super.isEnabled && ctx.settings.YcheckInit.value
 
-  override def transformTypeDef(tree: TypeDef)(using Context): tpd.Tree = {
-    if (!tree.isClassDef) return tree
+  override def runOn(units: List[CompilationUnit])(using Context): List[CompilationUnit] =
+    units.foreach { unit => traverser.traverse(unit.tpdTree) }
+    super.runOn(units)
 
+  val traverser = new TreeTraverser {
+    override def traverse(tree: Tree)(using Context): Unit =
+      tree match {
+        case tdef: MemberDef =>
+          // self-type annotation ValDef has no symbol
+          if tdef.name != nme.WILDCARD then
+            tdef.symbol.defTree = tree
+        case _ =>
+          traverseChildren(tree)
+      }
+  }
+
+  override def run(using Context): Unit = {
+    val unit = ctx.compilationUnit
+    unit.tpdTree.foreachSubTree {
+      case tdef: TypeDef if tdef.isClassDef =>
+        transformTypeDef(tdef)
+
+      case _ =>
+    }
+  }
+
+
+  private def transformTypeDef(tree: TypeDef)(using Context): tpd.Tree = {
     val cls = tree.symbol.asClass
     val instantiable: Boolean =
       cls.is(Flags.Module) ||
