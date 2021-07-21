fc017e113fc121a9e36f3d6d863fd8c177d6a879
Lukas Rytz <lukas.rytz@gmail.com>
2021-07-21 16:16:19 +0200

    display setting:help messages for helping settings

--- compiler/src/dotty/tools/dotc/config/CompilerCommand.scala
+++ compiler/src/dotty/tools/dotc/config/CompilerCommand.scala
@@ -13,14 +13,17 @@ abstract class CompilerCommand extends CliCommand:
   type ConcreteSettings = ScalaSettings
 
   final def helpMsg(using settings: ScalaSettings)(using SettingsState, Context): String =
-    if (settings.help.value) usageMessage
-    else if (settings.Vhelp.value) vusageMessage
-    else if (settings.Whelp.value) wusageMessage
-    else if (settings.Xhelp.value) xusageMessage
-    else if (settings.Yhelp.value) yusageMessage
-    else if (settings.showPlugins.value) ctx.base.pluginDescriptions
-    else if (settings.XshowPhases.value) phasesMessage
-    else ""
+    settings.allSettings.find(isHelping) match
+      case Some(s) => s.description
+      case _ =>
+        if (settings.help.value) usageMessage
+        else if (settings.Vhelp.value) vusageMessage
+        else if (settings.Whelp.value) wusageMessage
+        else if (settings.Xhelp.value) xusageMessage
+        else if (settings.Yhelp.value) yusageMessage
+        else if (settings.showPlugins.value) ctx.base.pluginDescriptions
+        else if (settings.XshowPhases.value) phasesMessage
+        else ""
 
   final def isHelpFlag(using settings: ScalaSettings)(using SettingsState): Boolean =
     import settings._
