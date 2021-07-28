2ad04fb3dfe2b505cd8a9dc3d5a3c93dac0fc9b9
Krzysztof Romanowski <romanowski.kr@gmail.com>
2021-07-28 15:21:04 +0200

    More proper way to detect if context bound is used

--- scaladoc-testcases/src/tests/contextBounds.scala
+++ scaladoc-testcases/src/tests/contextBounds.scala
@@ -7,6 +7,9 @@ class A:
   def basic[A : ClassTag]: A
     = ???
 
+  def basic2[A : ClassTag, B : List]: A
+    = ???
+
   trait Build[X, Y]
   trait From[A, B]
   def b[T : ([T] =>> Build[From[T, T], T])](t: T): T
@@ -21,3 +24,16 @@ class A:
   // Tests not support multiline signatures
   def a[T <: String | Int : ([T] =>> T match { case String => A case Int => B })](t: T): T
     = t
+
+  def falsePositive[T](evidence$1: ClassTag[T]): Int 
+    = 1
+
+  // Scala spec stats that behaviour of names with `$` is undefined.
+  // Scaladoc documents definition below as `def falsePositive2[T: ClassTag]: Int`
+  // that is equivalent of methods below
+  // def falsePositive2[T](implicit evidence$3: ClassTag[T]): Int 
+  //   = 1
+
+  class Outer[A]:
+    def falsePositiveInner[T](implicit evidence$3: ClassTag[A]): Int 
+      = 1
