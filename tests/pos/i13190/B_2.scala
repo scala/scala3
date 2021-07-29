0d970d5eb880226be68b49d5098b2fdc7f851741
Martin Odersky <odersky@gmail.com>
2021-07-29 14:36:57 +0200

    Pick right type parameters in TreeUnpickler
    Fixes #13190

---
+++ tests/pos/i13190/B_2.scala
@@ -0,0 +1,15 @@
+import Opaque.*
+
+object Test {
+  type FindField[R <: scala.Tuple, K] = R match {
+    case FieldType[K, f] *: t => f
+    case _ *: t => FindField[t, K]
+  }
+
+  val f: FieldType["A", Int] = ???
+  val f1: Int = f
+  //val f2: Int = f
+
+  type R = FieldType["A", Int] *: FieldType["B", Double] *: FieldType["C", String] *: FieldType["D", Boolean] *: EmptyTuple
+  summon[FindField[R, "B"] =:= Double]
+}
