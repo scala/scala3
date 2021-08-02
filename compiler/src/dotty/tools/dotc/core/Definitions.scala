5ae473d5b9d45296e9438e0cdf4e93afd5a0ad1b
Martin Odersky <odersky@gmail.com>
2021-08-02 18:39:53 +0200

    Address review comments

--- compiler/src/dotty/tools/dotc/core/Definitions.scala
+++ compiler/src/dotty/tools/dotc/core/Definitions.scala
@@ -1210,8 +1210,9 @@ class Definitions {
   val AbstractFunctionClassPerRun: PerRun[Array[Symbol]] = new PerRun(AbstractFunctionType.map(_.symbol.asClass))
   def AbstractFunctionClass(n: Int)(using Context): Symbol = AbstractFunctionClassPerRun()(using ctx)(n)
 
-  @tu lazy val caseClassSynthesized: Set[Symbol] = Set(Any_toString, Product_canEqual,
-    Product_productArity, Product_productPrefix, Product_productElement, Product_productElementName)
+  @tu lazy val caseClassSynthesized: Set[Symbol] = Set(
+    Any_hashCode, Any_equals, Any_toString, Product_canEqual, Product_productArity,
+    Product_productPrefix, Product_productElement, Product_productElementName)
 
   val LazyHolder: PerRun[Map[Symbol, Symbol]] = new PerRun({
     def holderImpl(holderType: String) = requiredClass("scala.runtime." + holderType)
