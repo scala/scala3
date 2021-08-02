01f7cdd7acca61f2b0bb66d00f1c9134756e586f
Dale Wijnand <dale.wijnand@gmail.com>
2021-08-02 13:54:34 +0100

    Make i12840 a run test to test the bytecode

--- tests/pos/i12840/Main.scala
+++
@@ -1,5 +0,0 @@
-package example
-
-object Main {
-  new NamedImpl() {}
-}
--- tests/run/i12840/NamedImpl.java
+++ tests/run/i12840/NamedImpl.java
@@ -6,4 +6,6 @@ public class NamedImpl implements Named {
   public Class<? extends Annotation> annotationType() {
     return Named.class;
   }
+
+  public String toString() { return "NamedImpl"; }
 }
