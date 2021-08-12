9cde00352daeeebbc5f8a4a270186ef0b669bde7
Andrzej Ratajczak <andrzej.ratajczak98@gmail.com>
2021-08-12 11:19:52 +0200

    Remove spare checks from CollectEntryPoints

--- compiler/src/dotty/tools/dotc/transform/CollectEntryPoints.scala
+++ compiler/src/dotty/tools/dotc/transform/CollectEntryPoints.scala
@@ -20,25 +20,20 @@ import dotty.tools.backend.jvm.GenBCode
 
 /**
  * Small phase to be run to collect main classes and store them in the context.
- * The general rule to run this phase is either:
- * - The output of compilation is JarArchive and there is no `-Xmain-class` defined
- * - The compiler is run from sbt and is forced by flags forcing `ExtractorAPI`
+ * The general rule to run this phase is:
+ * - The output of compilation is JarArchive
+ * - There is no `-Xmain-class` defined
  *
  * The following flags affect this phase:
  *   -d path.jar
  *   -Xmain-class
- *   -Yforce-sbt-phases
- *   -Ydump-sbt-inc
  */
 class CollectEntryPoints extends MiniPhase:
   def phaseName: String = "Collect entry points"
 
   override def isRunnable(using Context): Boolean =
-    def forceRun = (ctx.settings.XmainClass.isDefault && ctx.settings.outputDir.value.isInstanceOf[JarArchive]) ||
-                    ctx.settings.YdumpSbtInc.value ||
-                    ctx.settings.YforceSbtPhases.value
-    super.isRunnable && (ctx.sbtCallback != null || forceRun)
-
+    def forceRun = ctx.settings.XmainClass.isDefault && ctx.settings.outputDir.value.isInstanceOf[JarArchive]
+    super.isRunnable && forceRun
 
   override def transformTypeDef(tree: tpd.TypeDef)(using Context): tpd.Tree =
     getEntryPoint(tree).map(registerEntryPoint)
