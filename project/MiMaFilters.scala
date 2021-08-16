57328b9b9f0cb45566d1ad4f025a94839c3cc83c
Nicolas Stucki <nicolas.stucki@gmail.com>
2021-08-16 16:34:15 +0200

    Update compiler base version to 3.1.0-RC1

--- project/MiMaFilters.scala
+++ project/MiMaFilters.scala
@@ -4,10 +4,5 @@ import com.typesafe.tools.mima.core.ProblemFilters._
 
 object MiMaFilters {
   val Library: Seq[ProblemFilter] = Seq(
-    // Internals added in 3.0.2
-    exclude[MissingClassProblem]("scala.annotation.internal.ProvisionalSuperClass"),
-
-    // New APIs marked @experimental in 3.0.2
-    exclude[MissingClassProblem]("scala.Selectable$WithoutPreciseParameterTypes")
   )
 }
