4b88b5a67424bac7faa6f1fe7f123c77fc84e2a7
Martin Odersky <odersky@gmail.com>
2021-08-17 13:02:09 +0200

    Cleanups

--- compiler/src/dotty/tools/dotc/Compiler.scala
+++ compiler/src/dotty/tools/dotc/Compiler.scala
@@ -103,8 +103,8 @@ class Compiler {
          new TupleOptimizations,     // Optimize generic operations on tuples
          new LetOverApply,           // Lift blocks from receivers of applications
          new ArrayConstructors) ::   // Intercept creation of (non-generic) arrays and intrinsify.
-    List(new PreRecheck) ::
-    List(new TestRecheck) ::
+    List(new PreRecheck) ::          // Preparations for recheck phase, enabled under -Yrecheck
+    List(new TestRecheck) ::         // Test rechecking, enabled under -Yrecheck
     List(new Erasure) ::             // Rewrite types to JVM model, erasing all type parameters, abstract types and refinements.
     List(new ElimErasedValueType,    // Expand erased value types to their underlying implmementation types
          new PureStats,              // Remove pure stats from blocks
