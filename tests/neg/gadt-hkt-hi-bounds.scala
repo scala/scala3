196033e7d0ee221e618ecf915d2c3fe44a34cf15
Yichen Xu <yichen.x@outlook.com>
2021-08-18 15:15:23 +0800

    Add neg test

---
+++ tests/neg/gadt-hkt-hi-bounds.scala
@@ -0,0 +1,8 @@
+type Const = [X] =>> Int
+
+trait Expr[-F[_]]
+case class ConstExpr() extends Expr[Const]
+
+def foo[F[_], A](e: Expr[F]) = e match
+  case _: ConstExpr =>
+    val i: Int = ??? : F[A]   // limitation  // error
