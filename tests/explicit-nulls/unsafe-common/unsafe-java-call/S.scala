8598031834939ff2de3fad95c56d30c1604316a0
noti0na1 <noti0na1@users.noreply.github.com>
2021-08-19 11:33:56 +0000

    Update test

--- tests/explicit-nulls/unsafe-common/unsafe-java-call/S.scala
+++ tests/explicit-nulls/unsafe-common/unsafe-java-call/S.scala
@@ -1,21 +1,33 @@
-import scala.language.unsafeNulls
+// Check Java calls have been cast to non-nullable.
 
 val j: J = new J
 
-val s1: String = j.f1()
+val s1: String = j.f1() // error
+
+val s1n: String | Null = j.f1()
 
 val i1: Int = j.f2()
 
-val s2: String = j.g1[String]()
+val s2: String = j.g1[String]()  // error
 
-val i2: Int = j.g1[Int]()
+val s2n: String | Null = j.g1[String]()
+
+val s3: String = j.g1[String | Null]()  // error
+
+val s3n: String | Null = j.g1[String | Null]()
+
+val i2: Int = j.g1[Int]()  // error
 
 val a1: Any = j.g1[Any]()
 
-val ar1: AnyRef = j.g1[AnyRef]()
+val ar1: AnyRef = j.g1[AnyRef]()  // error
 
 val n1: Null = j.g1[Null]()
 
-val ar2: AnyRef = j.g1[Null]()
+val ar2: AnyRef = j.g1[Null]()  // error
 
-def clo[T]: T = j.g1[T]()
+def clo1[T]: T = j.g1[T]()  // error
+
+def clo2[T <: AnyRef]: T = j.g1[T | Null]()  // error
+
+def clo3[T >: Null <: AnyRef | Null]: T = j.g1[T]()
