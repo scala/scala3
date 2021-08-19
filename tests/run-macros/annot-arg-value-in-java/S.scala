3dce00c23f97e418c326aae72a06d17fd442c090
noti0na1 <noti0na1@users.noreply.github.com>
2021-08-19 05:38:07 +0000

    Remove unnecessary comments

--- tests/run-macros/annot-arg-value-in-java/S.scala
+++ tests/run-macros/annot-arg-value-in-java/S.scala
@@ -1,9 +1,5 @@
 // Display annotation arguments in Java
 
-// inline def showAnnots(inline c: String): Unit =
-//   println(c + ":")
-//   annots(c).foreach(println)
-
 @main def Test =
   showAnnots("J")
   showAnnots("JOtherTypes")
