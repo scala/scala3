917ee4a0a29beb67d790de779f1e604506a869c2
Nicolas Stucki <nicolas.stucki@gmail.com>
2021-08-23 14:31:52 +0200

    Check for experimental annotations only in non-experimental scopes

--- tests/neg-custom-args/no-experimental/experimentalAnnot.scala
+++ tests/neg-custom-args/no-experimental/experimentalAnnot.scala
@@ -8,3 +8,15 @@ def test1: Unit = ()
 @experimental
 @myExperimentalAnnot
 def test2: Unit = ()
+
+@experimental
+class Foo {
+  @myExperimentalAnnot
+  def test3: Unit = ()
+
+  def test4: Unit = {
+    @myExperimentalAnnot
+    val f: Unit = ()
+    f
+  }
+}
