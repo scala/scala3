1ce54d23cc313f12041860d22ca525877df362b3
Rikito Taniguchi <rikiriki1238@gmail.com>
2021-08-25 02:25:30 +0900

    Exclude synthesized parseArgument(given_FromString_Int) by @main
    By checking if the template's owner module symbol has an Invisible flag.

--- tests/semanticdb/expect/toplevel.scala
+++ tests/semanticdb/expect/toplevel.scala
@@ -4,4 +4,5 @@ def combine(x: Int, y: Int, z: Int) = x + y + z
 def combine = 0
 def foo = "foo"
 @main def MyProgram(times: Int): Unit = (1 to times) foreach (_ => println("hello"))
+@main def readInts(ints: Int*): Unit = println(ints.mkString(","))
 def fooRef = toplevel$package.foo
