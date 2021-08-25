202b63e50dac73d14bbbe0f426eef49c3133d2ab
Dale Wijnand <dale.wijnand@gmail.com>
2021-08-25 18:19:04 +0100

    Change Space intersect so that the fallback for extractors matches the fallback for type spaces

--- tests/patmat/i12805-fallout.scala
+++ tests/patmat/i12805-fallout.scala
@@ -16,11 +16,15 @@ abstract class Tree[-T >: Untyped] {
   }
 }
 
-case class  Ident[-T >: Untyped]() extends Tree[T]
-case class DefDef[-T >: Untyped]() extends Tree[T]
+case class   Ident[-T >: Untyped]() extends Tree[T]
+case class  DefDef[-T >: Untyped]() extends Tree[T]
+case class Inlined[-T >: Untyped]() extends Tree[T]
+case class CaseDef[-T >: Untyped]() extends Tree[T]
 
 def test[T >: Untyped](tree: Tree[T], tp: Type) = tree.withType(tp) match {
-  case Ident()  => 1
-  case DefDef() => 2
-  case _        => 3
+  case Ident()       => 1
+  case DefDef()      => 2
+  case _: Inlined[_] => 3
+  case CaseDef()     => 4
+  case _             => 5
 }
