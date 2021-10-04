370280bcfa96d1ea15e8b0b59f272dbbb3a982b9
Paweł Marks <pmarks@virtuslab.com>
2021-10-04 12:43:05 +0200

    Set version to 3.1.0-RC3

--- project/Build.scala
+++ project/Build.scala
@@ -63,9 +63,9 @@ object DottyJSPlugin extends AutoPlugin {
 }
 
 object Build {
-  val referenceVersion = "3.1.0-RC1"
+  val referenceVersion = "3.1.0-RC2"
 
-  val baseVersion = "3.1.0-RC2"
+  val baseVersion = "3.1.0-RC3"
 
   // Versions used by the vscode extension to create a new project
   // This should be the latest published releases.
