6fc8c989db5a20dbfbd015f0ccf7c6c94bd3321c
Martin Odersky <odersky@gmail.com>
2021-10-05 11:12:27 +0200

    Special rule for {this} in capture sets of class members
    Consider the lazylists.scala test in pos-custom-args/captures:
    ```scala
    class CC
    type Cap = {*} CC
    
    trait LazyList[+A]:
      this: ({*} LazyList[A]) =>
    
      def isEmpty: Boolean
      def head: A
      def tail: {this} LazyList[A]
    
    object LazyNil extends LazyList[Nothing]:
      def isEmpty: Boolean = true
      def head = ???
      def tail = ???
    
    extension [A](xs: {*} LazyList[A])
      def map[B](f: {*} A => B): {xs, f} LazyList[B] =
        class Mapped extends LazyList[B]:
          this: ({xs, f} Mapped) =>
    
          def isEmpty = false
          def head: B = f(xs.head)
          def tail: {this} LazyList[B] = xs.tail.map(f)  // OK
        new Mapped
    ```
    Without this commit, the second to last line is an error since the right hand side
    has capture set `{xs, f}` but the required capture set is `this`.
    
    To fix this, we widen the expected type of the rhs `xs.tail.map(f)` from `{this}` to
    `{this, f, xs}`. That is, we add the declared captures of the self type to the expected
    type. The soundness argument for doing this is as follows:
    
    Since `tail` does not have parameters, the only thing it could capture are references that the
    receiver `this` captures as well. So `xs` and `f` must come via `this`. For instance, if
    the receiver `xs` of `xs.tail` happens to be pure, then `xs.tail` is pure as well.
    
    On the other hand, in the neg test `lazylists1.scala` we add the following line to `Mapped`:
    ```scala
          def concat(other: {f} LazyList[A]): {this} LazyList[A] = ??? : ({xs, f} LazyList[A]) // error
    ```
    Here, we cannot widen the expected type from `{this}` to `{this, xs, f}` since the result of concat
    refers to `f` independently of `this`, namely through its parameter `other`. Hence, if `ys: {f} LazyList[String]`
    then
    ```
       LazyNil.concat(ys)
    ```
    still refers to `f` even though `LazyNil` is pure. But if we would accept the definition of `concat`
    above, the type of `LazyNil.concat(ys)` would be `LazyList[String]`, which is unsound.
    
    The current implementation widens the expected type of class members if the class member does not
    have tracked parameters. We could potentially refine this to say we widen with all references in
    the expected type that are not subsumed by one of the parameter types.
    
    ## Changes:
    
    ### Refine rule for this widening
    
    We now widen the expected type of the right hand side of a class member as follows:
    
    Add all references of the declared type of this that are not subsumed by a capture set
    of a parameter type.
    
    ### Do expected type widening only in final classes
    
    Alex found a counter-example why this is required. See map5 in
    neg-customargs/captures/lazylists2.scala

---
+++ tests/pos-custom-args/captures/lazylists.scala
@@ -0,0 +1,42 @@
+class CC
+type Cap = {*} CC
+
+trait LazyList[+A]:
+  this: ({*} LazyList[A]) =>
+
+  def isEmpty: Boolean
+  def head: A
+  def tail: {this} LazyList[A]
+
+object LazyNil extends LazyList[Nothing]:
+  def isEmpty: Boolean = true
+  def head = ???
+  def tail = ???
+
+extension [A](xs: {*} LazyList[A])
+  def map[B](f: {*} A => B): {xs, f} LazyList[B] =
+    final class Mapped extends LazyList[B]:
+      this: ({xs, f} Mapped) =>
+
+      def isEmpty = false
+      def head: B = f(xs.head)
+      def tail: {this} LazyList[B] = xs.tail.map(f)  // OK
+      def concat(other: {f} LazyList[A]): {this, f} LazyList[A] = ??? : ({xs, f} LazyList[A]) // OK
+    if xs.isEmpty then LazyNil
+    else new Mapped
+
+def test(cap1: Cap, cap2: Cap) =
+  def f(x: String): String = if cap1 == cap1 then "" else "a"
+  def g(x: String): String = if cap2 == cap2 then "" else "a"
+
+  val xs =
+    class Initial extends LazyList[String]:
+      this: ({cap1} Initial) =>
+
+      def isEmpty = false
+      def head = f("")
+      def tail = LazyNil
+    new Initial
+  val xsc: {cap1} LazyList[String] = xs
+  val ys = xs.map(g)
+  val ysc: {cap1, cap2} LazyList[String] = ys
