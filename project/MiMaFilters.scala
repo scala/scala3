959aff90d991439134587c4cfc1db55faee82721
Timothée Loyck Andres <timothee.andres@epfl.ch>
2021-10-08 16:12:54 +0200

    Update MiMa filters

--- project/MiMaFilters.scala
+++ project/MiMaFilters.scala
@@ -4,6 +4,12 @@ import com.typesafe.tools.mima.core._
 object MiMaFilters {
   val Library: Seq[ProblemFilter] = Seq(
     // Experimental APIs that can be added in 3.2.0
+    ProblemFilters.exclude[MissingTypesProblem]("scala.main"),
+    ProblemFilters.exclude[DirectMissingMethodProblem]("scala.main.command"),
+    ProblemFilters.exclude[MissingClassProblem]("scala.annotation.MainAnnotation"),
+    ProblemFilters.exclude[MissingClassProblem]("scala.annotation.MainAnnotation$Command"),
+    ProblemFilters.exclude[DirectMissingMethodProblem]("scala.runtime.Tuples.init"),
+    ProblemFilters.exclude[DirectMissingMethodProblem]("scala.runtime.Tuples.last"),
     ProblemFilters.exclude[DirectMissingMethodProblem]("scala.runtime.Tuples.append"),
     ProblemFilters.exclude[ReversedMissingMethodProblem]("scala.quoted.Quotes#reflectModule#TypeReprMethods.substituteTypes"),
     ProblemFilters.exclude[DirectMissingMethodProblem]("scala.quoted.Quotes#reflectModule#TypeReprMethods.substituteTypes"),
