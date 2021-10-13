3b5ff7a44c9416aef6bbaf8bb0d439d80345ac32
Daniel Esik <e.danicheg@yandex.ru>
2021-10-14 00:13:13 +0300

    Add `init` and `last` methods to `NonEmptyTuple` (#13735)

--- project/MiMaFilters.scala
+++ project/MiMaFilters.scala
@@ -4,5 +4,7 @@ import com.typesafe.tools.mima.core.ProblemFilters._
 
 object MiMaFilters {
   val Library: Seq[ProblemFilter] = Seq(
+    ProblemFilters.exclude[DirectMissingMethodProblem]("scala.runtime.Tuples.init"),
+    ProblemFilters.exclude[DirectMissingMethodProblem]("scala.runtime.Tuples.last")
   )
 }
