ad291f275593a4403f6b692b08d6bf2e019c8e8d
Timothée Loyck Andres <timothee.andres@epfl.ch>
2021-10-14 13:05:26 +0200

    Add tests for return types and overloading

---
+++ tests/run/main-annotation-overload.scala
@@ -0,0 +1,31 @@
+// Sample main method
+object myProgram:
+
+  /** Adds two numbers */
+  @main def add(num: Int, inc: Int): Unit =
+    println(s"$num + $inc = ${num + inc}")
+
+  /** Adds one number (malformed, doesn't work) */
+  def add(num: Int): Unit =
+    ???
+
+  /** Adds zero numbers (malformed, doesn't work) */
+  def add(): Int =
+    ???
+
+end myProgram
+
+// Compiler generated code:
+// TODO remove once @main generation is operational
+object add extends main:
+  def main(args: Array[String]) =
+    val cmd = command(args)
+    val arg1 = cmd.argGetter[Int]("num")
+    val arg2 = cmd.argGetter[Int]("inc")
+    cmd.run(myProgram.add(arg1(), arg2()), "add", "Adds two numbers")
+end add
+
+object Test:
+  def main(args: Array[String]): Unit =
+    add.main(Array("2", "3"))
+end Test
---
+++ tests/run/main-annotation-return-type-2.scala
@@ -0,0 +1,30 @@
+class MyResult(val result: Int):
+  override def toString: String = result.toString
+
+// Sample main method
+object myProgram:
+
+  /** Adds two numbers and returns them */
+  @main def add(num: Int, inc: Int): MyResult =
+    println(f"$num + $inc = ${num + inc}")
+    MyResult(num + inc)
+
+end myProgram
+
+// Compiler generated code:
+// TODO remove once @main generation is operational
+object add extends main:
+  def main(args: Array[String]) =
+    val cmd = command(args)
+    val arg1 = cmd.argGetter[Int]("num")
+    val arg2 = cmd.argGetter[Int]("inc")
+    cmd.run(myProgram.add(arg1(), arg2()), "add", "Adds two numbers and returns them")
+end add
+
+object Test:
+  def main(args: Array[String]): Unit =
+    println("Direct call")
+    println(myProgram.add(2, 3))
+    println("Main call")
+    println(add.main(Array("2", "3")))
+end Test
