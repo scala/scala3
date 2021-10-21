443b03666b039fbcbb67acd24080d04a48c0b5ed
Nicolas Stucki <nicolas.stucki@gmail.com>
2021-10-21 10:33:06 +0200

    Improve message when -Xmax-inlines limit reached
    Fixes #13044

---
+++ tests/neg/i13044.scala
@@ -0,0 +1,51 @@
+import scala.deriving.Mirror
+import scala.compiletime._
+
+trait Schema[T] {
+  def build: T
+}
+
+object Schema extends SchemaDerivation {
+  implicit lazy val int: Schema[Int]   = ???
+  implicit def option[A](implicit ev: Schema[A]): Schema[Option[A]] = ???
+}
+
+trait SchemaDerivation {
+  inline def recurse[A <: Tuple]: List[Schema[Any]] =
+    inline erasedValue[A] match {
+      case _: (t *: ts) =>
+        val builder = summonInline[Schema[t]].asInstanceOf[Schema[Any]]
+        builder :: recurse[ts]
+      case _: EmptyTuple => Nil
+    }
+
+  inline def derived[A]: Schema[A] =
+    inline summonInline[Mirror.Of[A]] match {
+      case m: Mirror.SumOf[A] =>
+        lazy val subTypes = recurse[m.MirroredElemTypes]
+        new Schema[A] {
+          def build: A = ???
+        }
+
+      case m: Mirror.ProductOf[A] =>
+        lazy val fields = recurse[m.MirroredElemTypes]
+        new Schema[A] {
+          def build: A = ???
+        }
+    }
+
+  inline given gen[A]: Schema[A] = derived
+}
+
+case class H(i: Int)
+case class G(h: H)
+case class F(g: G)
+case class E(f: Option[F])
+case class D(e: E)
+case class C(d: D)
+case class B(c: C)
+case class A(a: A, b: B)
+
+object TestApp {
+   implicit def typeSchema: Schema[A] = Schema.gen // error // error
+}
