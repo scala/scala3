45f7ba2f4adb08214a451ed46b9889be03885593
Timothée Loyck Andres <timothee.andres@epfl.ch>
2021-10-26 15:36:48 +0200

    Add vararg and default to top-level test

--- tests/run/main-annotation-top-level.scala
+++ tests/run/main-annotation-top-level.scala
@@ -2,12 +2,31 @@
 @main def add(num: Int, inc: Int): Unit =
   println(s"$num + $inc = ${num + inc}")
 
+/** Adds any amount of numbers */
+@main def addAll(num: Int = 0, incs: Int*): Unit =
+  print(num)
+  if (incs.length > 0) {
+    print(" + ")
+    print(incs.mkString(" + "))
+  }
+  println(s" = ${num + incs.sum}")
+
 object Test:
-  def callMain(args: Array[String]): Unit =
+  def callMainAdd(args: Array[String]): Unit =
     val clazz = Class.forName("add")
     val method = clazz.getMethod("main", classOf[Array[String]])
     method.invoke(null, args)
 
+  def callMainAddAll(args: Array[String]): Unit =
+    val clazz = Class.forName("addAll")
+    val method = clazz.getMethod("main", classOf[Array[String]])
+    method.invoke(null, args)
+
   def main(args: Array[String]): Unit =
-    callMain(Array("2", "3"))
+    callMainAdd(Array("2", "3"))
+
+    callMainAddAll(Array("2", "3"))
+    callMainAddAll(Array("2"))
+    callMainAddAll(Array())
+    callMainAddAll(Array("1", "2", "3", "4"))
 end Test
