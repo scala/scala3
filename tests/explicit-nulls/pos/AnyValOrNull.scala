279d2f52dc54ec69801eb297f6b5b620cdd3d408
noti0na1 <noti0na1@users.noreply.github.com>
2021-10-27 13:55:35 -0400

    Update test

--- tests/explicit-nulls/pos/AnyValOrNull.scala
+++ tests/explicit-nulls/pos/AnyValOrNull.scala
@@ -1,16 +1,36 @@
-def test1 =
-  val v: AnyVal | Null = null
-  if v == null then
-    println("null")
-
-def test2 =
-  val v: Int | Null = 1
-  if v != null then
-    println(v)
-
 case class MyVal(i: Boolean) extends AnyVal
 
-def test3 =
-  val v: MyVal | Null = MyVal(false)
-  if v != null then
-    println(v)
+class Test1:
+
+  def test1 =
+    val v: AnyVal | Null = null
+    if v == null then
+      println("null")
+
+  def test2 =
+    val v: Int | Null = 1
+    if v != null then
+      println(v)
+
+  def test3 =
+    val v: MyVal | Null = MyVal(false)
+    if v != null then
+      println(v)
+
+class Test2:
+  import scala.language.unsafeNulls
+
+  def test1 =
+    val v: AnyVal | Null = null
+    if v == null then
+      println("null")
+
+  def test2 =
+    val v: Int | Null = 1
+    if v != null then
+      println(v)
+
+  def test3 =
+    val v: MyVal | Null = MyVal(false)
+    if v != null then
+      println(v)
