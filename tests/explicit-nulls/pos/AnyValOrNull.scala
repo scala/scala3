fdad2b8036ff74b1920fcf4e8cd74226659684d1
noti0na1 <noti0na1@users.noreply.github.com>
2021-10-27 02:08:48 -0400

    Fix comparing AnyVal | Null with Null

---
+++ tests/explicit-nulls/pos/AnyValOrNull.scala
@@ -0,0 +1,16 @@
+def test1 =
+  val v: AnyVal | Null = null
+  if v == null then
+    println("null")
+
+def test2 =
+  val v: Int | Null = 1
+  if v != null then
+    println(v)
+
+case class MyVal(val i: Boolean) extends AnyVal
+
+def test3 =
+  val v: MyVal | Null = MyVal(false)
+  if v != null then
+    println(v)
