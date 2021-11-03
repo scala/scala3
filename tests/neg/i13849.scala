abffac288cf4b380479b6de6de4a29b0434d9532
Martin Odersky <odersky@gmail.com>
2021-11-03 12:46:01 +0100

    Don't generate CanThrow capabilities for catches with guards

---
+++ tests/neg/i13849.scala
@@ -0,0 +1,16 @@
+import annotation.experimental
+import language.experimental.saferExceptions
+
+@experimental
+case class Ex(i: Int) extends Exception(s"Exception: $i")
+
+@experimental
+def foo(): Unit throws Ex = throw Ex(1)
+
+@experimental
+object Main:
+  def main(args: Array[String]): Unit =
+    try
+      foo() // error
+    catch
+      case _: Ex if false => println("Caught")
