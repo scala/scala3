88a76dfda766c4b93064da86d4feb666a34c3a19
Timothée Loyck Andres <timothee.andres@epfl.ch>
2021-11-14 00:35:33 +0100

    Factorize code of main
    - Now that main is final, the code can be factorized

--- library/src/scala/main.scala
+++ library/src/scala/main.scala
@@ -17,83 +17,19 @@ final class main extends scala.annotation.MainAnnotation:
   self =>
   import main._
 
-  protected sealed abstract trait Argument {
-    def name: String
-    def typeName: String
-    def doc: String
-
-    def usage: String
-  }
-  protected class SimpleArgument(val name: String, val typeName: String, val doc: String) extends Argument:
-    override def usage: String = s"[--$name] <$name>"
-  protected class OptionalArgument[T](val name: String, val typeName: String, val doc: String, val defaultValue: T)
-    extends Argument:
-    override def usage: String = s"[[--$name] <$name>]"
-  protected class VarArgument(val name: String, val typeName: String, val doc: String) extends Argument:
-    override def usage: String = s"[<$name> [<$name> [...]]]"
-
   override type ArgumentParser[T] = util.CommandLineParser.FromString[T]
   override type MainResultType = Any
 
-  /** Prints the main function's usage */
-  def usage(commandName: String, args: Seq[Argument]): Unit =
-    def wrappedArgumentUsages(argsUsage: List[String], maxLength: Int): List[String] =
-      def recurse(args: List[String], currentLine: String, acc: Vector[String]): Vector[String] =
-        (args, currentLine) match {
-          case (Nil, "") => acc
-          case (Nil, l) => (acc :+ l)
-          case (arg :: t, "") => recurse(t, arg, acc)
-          case (arg :: t, l) if l.length + 1 + arg.length <= maxLength => recurse(t, s"$l $arg", acc)
-          case (arg :: t, l) => recurse(t, arg, acc :+ l)
-        }
-
-      recurse(argsUsage, "", Vector()).toList
-    end wrappedArgumentUsages
-
-    val maxLineLength = 120 // TODO as parameter? As global Dotty parameter?
-
-    val usageBeginning = s"Usage: $commandName "
-    val argsOffset = usageBeginning.length
-    val argUsages = wrappedArgumentUsages(args.map(_.usage).toList, maxLineLength - argsOffset)
-
-    println(usageBeginning + argUsages.mkString("\n" + " " * argsOffset))
-
-  /** Prints an explanation about the function */
-  def explain(commandName: String, commandDoc: String, args: Seq[Argument]): Unit =
-    if (commandDoc.nonEmpty)
-      println(commandDoc)
-    if (args.nonEmpty) {
-      val argNameShift = 2
-      val argDocShift = argNameShift + 2
-
-      println("Arguments:")
-      for (arg <- args)
-        val argDoc = StringBuilder(" " * argNameShift)
-        argDoc.append(s"${arg.name} - ${arg.typeName}")
-
-        arg match {
-          case o: OptionalArgument[?] => argDoc.append(" (optional)")
-          case _ =>
-        }
-
-        if (arg.doc.nonEmpty) {
-          val shiftedDoc = arg.doc.split("\n").nn.map(" " * argDocShift + _).mkString("\n")
-          argDoc.append("\n").append(shiftedDoc)
-        }
-
-        println(argDoc)
-    }
-
-  /** Runs the command and handles its return value */
-  def run(f: => MainResultType): Unit =
-    f match
-      case ExitCode(n) => System.exit(n)
-      case _ =>
+  private enum ArgumentKind {
+    case SimpleArgument, OptionalArgument, VarArgument
+  }
 
   override def command(args: Array[String], commandName: String, docComment: String): Command =
     new Command:
-      /** A buffer of demanded arguments */
-      private var argInfos = new mutable.ListBuffer[self.Argument]
+      private var argNames = new mutable.ListBuffer[String]
+      private var argTypes = new mutable.ListBuffer[String]
+      private var argDocs = new mutable.ListBuffer[String]
+      private var argKinds = new mutable.ListBuffer[ArgumentKind]
 
       /** A buffer for all errors */
       private var errors = new mutable.ListBuffer[String]
@@ -120,11 +56,60 @@ final class main extends scala.annotation.MainAnnotation:
           case Some(t) => () => t
           case None => error(s"invalid argument for $argName: $arg")
 
+      private def argUsage(pos: Int): String =
+        val name = argNames(pos)
+
+        argKinds(pos) match {
+          case ArgumentKind.SimpleArgument => s"[--$name] <$name>"
+          case ArgumentKind.OptionalArgument => s"[[--$name] <$name>]"
+          case ArgumentKind.VarArgument => s"[<$name> [<$name> [...]]]"
+        }
+
       private def usage(): Unit =
-        self.usage(commandName, argInfos.toSeq)
+        def wrappedArgumentUsages(argsUsage: List[String], maxLength: Int): List[String] =
+          def recurse(args: List[String], currentLine: String, acc: Vector[String]): Vector[String] =
+            (args, currentLine) match {
+              case (Nil, "") => acc
+              case (Nil, l) => (acc :+ l)
+              case (arg :: t, "") => recurse(t, arg, acc)
+              case (arg :: t, l) if l.length + 1 + arg.length <= maxLength => recurse(t, s"$l $arg", acc)
+              case (arg :: t, l) => recurse(t, arg, acc :+ l)
+            }
+
+          recurse(argsUsage, "", Vector()).toList
+        end wrappedArgumentUsages
+
+        val maxLineLength = 120 // TODO as parameter? As global Dotty parameter?
+        val usageBeginning = s"Usage: $commandName "
+        val argsOffset = usageBeginning.length
+        val argUsages = wrappedArgumentUsages((0 until argNames.length).map(argUsage).toList, maxLineLength - argsOffset)
+
+        println(usageBeginning + argUsages.mkString("\n" + " " * argsOffset))
 
       private def explain(): Unit =
-        self.explain(commandName, docComment, argInfos.toSeq)
+        if (docComment.nonEmpty)
+          println(docComment)
+        if (argNames.nonEmpty) {
+          val argNameShift = 2
+          val argDocShift = argNameShift + 2
+
+          println("Arguments:")
+          for (pos <- 0 until argNames.length)
+            val argDoc = StringBuilder(" " * argNameShift)
+            argDoc.append(s"${argNames(pos)} - ${argTypes(pos)}")
+
+            argKinds(pos) match {
+              case ArgumentKind.OptionalArgument => argDoc.append(" (optional)")
+              case _ =>
+            }
+
+            if (argDocs(pos).nonEmpty) {
+              val shiftedDoc = argDocs(pos).split("\n").nn.map(" " * argDocShift + _).mkString("\n")
+              argDoc.append("\n").append(shiftedDoc)
+            }
+
+            println(argDoc)
+        }
 
       private def indicesOfArg(argName: String): Seq[Int] =
         def allIndicesOf(s: String): Seq[Int] =
@@ -150,16 +135,22 @@ final class main extends scala.annotation.MainAnnotation:
             error(s"more than one value for $argName: ${multValues.mkString(", ")}")
         }
 
+      private def registerArg(argName: String, argType: String, argDoc: String, argKind: ArgumentKind): Unit =
+        argNames += argName
+        argTypes += argType
+        argDocs += argDoc
+        argKinds += argKind
+
       override def argGetter[T](argName: String, argType: String, argDoc: String)(using p: ArgumentParser[T]): () => T =
-        argInfos += self.SimpleArgument(argName, argType, argDoc)
+        registerArg(argName, argType, argDoc, ArgumentKind.SimpleArgument)
         getArgGetter(argName, error(s"missing argument for $argName"))
 
       override def argGetterDefault[T](argName: String, argType: String, argDoc: String, defaultValue: => T)(using p: ArgumentParser[T]): () => T =
-        argInfos += self.OptionalArgument(argName, argType, argDoc, defaultValue)
+        registerArg(argName, argType, argDoc, ArgumentKind.OptionalArgument)
         getArgGetter(argName, () => defaultValue)
 
       override def argsGetter[T](argName: String, argType: String, argDoc: String)(using p: ArgumentParser[T]): () => Seq[T] =
-        argInfos += self.VarArgument(argName, argType, argDoc)
+        registerArg(argName, argType, argDoc, ArgumentKind.VarArgument)
         def remainingArgGetters(): List[() => T] = nextPositionalArg() match
           case Some(arg) => convert(argName, arg, p) :: remainingArgGetters()
           case None => Nil
@@ -174,7 +165,7 @@ final class main extends scala.annotation.MainAnnotation:
           case None =>
             for
               arg <- args
-              if arg.startsWith("--") && !argInfos.map(_.name).contains(arg.drop(2))
+              if arg.startsWith("--") && !argNames.contains(arg.drop(2))
             do
               error(s"unknown argument name: $arg")
         end flagUnused
@@ -188,8 +179,9 @@ final class main extends scala.annotation.MainAnnotation:
           if errors.nonEmpty then
             for msg <- errors do println(s"Error: $msg")
             usage()
-          else
-            self.run(f)
+          else f match
+            case ExitCode(n) => sys.exit(n)
+            case _ =>
       end run
   end command
 end main
