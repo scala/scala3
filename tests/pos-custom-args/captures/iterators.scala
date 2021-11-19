20cf766a23bc3fb55a76a9b33530bf623ee28c21
Martin Odersky <odersky@gmail.com>
2021-11-19 13:39:21 +0100

    Update iterators example

--- tests/pos-custom-args/captures/iterators.scala
+++ tests/pos-custom-args/captures/iterators.scala
@@ -1,17 +1,21 @@
 package cctest
 
 abstract class Iterator[T]:
-  thisIterator =>
+  thisIterator: ({*} Iterator[T]) =>
 
   def hasNext: Boolean
   def next: T
-  def map(f: {*} T => T): {f} Iterator[T] = new Iterator:
+  def map(f: {*} T => T): {f, this} Iterator[T] = new Iterator:
     def hasNext = thisIterator.hasNext
     def next = f(thisIterator.next)
 end Iterator
 
 class C
 type Cap = {*} C
+
+def map[T, U](it: {*} Iterator[T], f: {*} T => U): {it, f} Iterator[U] = new Iterator:
+  def hasNext = it.hasNext
+  def next = f(it.next)
 
 def test(c: Cap, d: Cap, e: Cap) =
   val it = new Iterator[Int]:
@@ -21,3 +25,4 @@ def test(c: Cap, d: Cap, e: Cap) =
 
   def f(x: Int): Int = if c == d then x else 10
   val it2 = it.map(f)
+  val it3 = map(it, f)
