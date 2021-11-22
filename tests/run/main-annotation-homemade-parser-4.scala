aff99de708cdd60ba1ef0e470a3ad38fd93fd85b
Timothée Loyck Andres <timothee.andres@epfl.ch>
2021-11-22 17:26:19 +0100

    Add test for Option[T] and Either[T]

---
+++ tests/run/main-annotation-homemade-parser-4.scala
@@ -0,0 +1,46 @@
+import scala.util.CommandLineParser.FromString
+
+given [T : FromString]: FromString[Option[T]] with
+  override def fromString(s: String) = Some(summon[FromString[T]].fromString(s))
+  override def fromStringOption(s: String) =
+    try {
+      Some(fromString(s))
+    }
+    catch {
+      case _: IllegalArgumentException => Some(None)
+    }
+
+given [T : FromString]: FromString[Either[T, String]] with
+  override def fromString(s: String) = Left(summon[FromString[T]].fromString(s))
+  override def fromStringOption(s: String) =
+    try {
+      Some(fromString(s))
+    }
+    catch {
+      case _: IllegalArgumentException => Some(Right(s"Unable to parse argument $s"))
+    }
+
+object myProgram:
+
+  @main def getOption(o: Option[Int] = Some[Int](42)) = o
+
+  @main def getEither(e: Either[Int, String] = Right[Int, String]("No argument given")) = e
+
+end myProgram
+
+
+object Test:
+  def call(className: String, args: Array[String]): Unit =
+    val clazz = Class.forName(className)
+    val method = clazz.getMethod("main", classOf[Array[String]])
+    method.invoke(null, args)
+
+  def main(args: Array[String]): Unit =
+    call("getOption", Array("7"))
+    call("getOption", Array())
+    call("getOption", Array("abc"))
+    println
+    call("getEither", Array("7"))
+    call("getEither", Array())
+    call("getEither", Array("abc"))
+end Test
