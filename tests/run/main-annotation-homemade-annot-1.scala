816116357ac11c24aac59620bc7a36f7b5ef53d3
Timothée Loyck Andres <timothee.andres@epfl.ch>
2021-12-04 10:35:31 +0100

    Add test with Future[_] as return type

---
+++ tests/run/main-annotation-homemade-annot-1.scala
@@ -0,0 +1,212 @@
+import scala.concurrent._
+import scala.annotation.MainAnnotation
+import scala.collection.mutable
+import ExecutionContext.Implicits.global
+import duration._
+
+@mainAwait def get(wait: Int): Future[Int] = Future{
+  Thread.sleep(1000 * wait)
+  42
+}
+
+object Test:
+  def callMain(args: Array[String]): Unit =
+    val clazz = Class.forName("get")
+    val method = clazz.getMethod("main", classOf[Array[String]])
+    method.invoke(null, args)
+
+  def main(args: Array[String]): Unit =
+    println(Await.result(get(1), Duration(2, SECONDS)))
+    callMain(Array("1"))
+end Test
+
+class mainAwait(timeout: Int = 2) extends MainAnnotation:
+  self =>
+
+  private val maxLineLength = 120
+
+  override type ArgumentParser[T] = util.CommandLineParser.FromString[T]
+  override type MainResultType = Future[Any]
+
+  private enum ArgumentKind {
+    case SimpleArgument, OptionalArgument, VarArgument
+  }
+
+  override def command(args: Array[String], commandName: String, docComment: String) =
+    new MainAnnotation.Command[ArgumentParser, MainResultType]:
+      private var argNames = new mutable.ArrayBuffer[String]
+      private var argTypes = new mutable.ArrayBuffer[String]
+      private var argDocs = new mutable.ArrayBuffer[String]
+      private var argKinds = new mutable.ArrayBuffer[ArgumentKind]
+
+      /** A buffer for all errors */
+      private var errors = new mutable.ArrayBuffer[String]
+
+      /** Issue an error, and return an uncallable getter */
+      private def error(msg: String): () => Nothing =
+        errors += msg
+        () => throw new AssertionError("trying to get invalid argument")
+
+      /** The next argument index */
+      private var argIdx: Int = 0
+
+      private def argAt(idx: Int): Option[String] =
+        if idx < args.length then Some(args(idx)) else None
+
+      private def nextPositionalArg(): Option[String] =
+        while argIdx < args.length && args(argIdx).startsWith("--") do argIdx += 2
+        val result = argAt(argIdx)
+        argIdx += 1
+        result
+
+      private def convert[T](argName: String, arg: String, p: ArgumentParser[T]): () => T =
+        p.fromStringOption(arg) match
+          case Some(t) => () => t
+          case None => error(s"invalid argument for $argName: $arg")
+
+      private def argUsage(pos: Int): String =
+        val name = argNames(pos)
+
+        argKinds(pos) match {
+          case ArgumentKind.SimpleArgument => s"[--$name] <${argTypes(pos)}>"
+          case ArgumentKind.OptionalArgument => s"[[--$name] <${argTypes(pos)}>]"
+          case ArgumentKind.VarArgument => s"[<${argTypes(pos)}> [<${argTypes(pos)}> [...]]]"
+        }
+
+      private def wrapLongLine(line: String, maxLength: Int): List[String] = {
+        def recurse(s: String, acc: Vector[String]): Seq[String] =
+          val lastSpace = s.trim.nn.lastIndexOf(' ', maxLength)
+          if ((s.length <= maxLength) || (lastSpace < 0))
+            acc :+ s
+          else {
+            val (shortLine, rest) = s.splitAt(lastSpace)
+            recurse(rest.trim.nn, acc :+ shortLine)
+          }
+
+        recurse(line, Vector()).toList
+      }
+
+      private def wrapArgumentUsages(argsUsage: List[String], maxLength: Int): List[String] = {
+        def recurse(args: List[String], currentLine: String, acc: Vector[String]): Seq[String] =
+          (args, currentLine) match {
+            case (Nil, "") => acc
+            case (Nil, l) => (acc :+ l)
+            case (arg :: t, "") => recurse(t, arg, acc)
+            case (arg :: t, l) if l.length + 1 + arg.length <= maxLength => recurse(t, s"$l $arg", acc)
+            case (arg :: t, l) => recurse(t, arg, acc :+ l)
+          }
+
+        recurse(argsUsage, "", Vector()).toList
+      }
+
+      private inline def shiftLines(s: Seq[String], shift: Int): String = s.map(" " * shift + _).mkString("\n")
+
+      private def usage(): Unit =
+        val usageBeginning = s"Usage: $commandName "
+        val argsOffset = usageBeginning.length
+        val argUsages = wrapArgumentUsages((0 until argNames.length).map(argUsage).toList, maxLineLength - argsOffset)
+
+        println(usageBeginning + argUsages.mkString("\n" + " " * argsOffset))
+
+      private def explain(): Unit =
+        if (docComment.nonEmpty)
+          println(wrapLongLine(docComment, maxLineLength).mkString("\n"))
+        if (argNames.nonEmpty) {
+          val argNameShift = 2
+          val argDocShift = argNameShift + 2
+
+          println("Arguments:")
+          for (pos <- 0 until argNames.length)
+            val argDoc = StringBuilder(" " * argNameShift)
+            argDoc.append(s"${argNames(pos)} - ${argTypes(pos)}")
+
+            argKinds(pos) match {
+              case ArgumentKind.OptionalArgument => argDoc.append(" (optional)")
+              case ArgumentKind.VarArgument => argDoc.append(" (vararg)")
+              case _ =>
+            }
+
+            if (argDocs(pos).nonEmpty) {
+              val shiftedDoc =
+                argDocs(pos).split("\n").nn
+                            .map(line => shiftLines(wrapLongLine(line.nn, maxLineLength - argDocShift), argDocShift))
+                            .mkString("\n")
+              argDoc.append("\n").append(shiftedDoc)
+            }
+
+            println(argDoc)
+        }
+
+      private def indicesOfArg(argName: String): Seq[Int] =
+        def allIndicesOf(s: String): Seq[Int] =
+          def recurse(s: String, from: Int): Seq[Int] =
+            val i = args.indexOf(s, from)
+            if i < 0 then Seq() else i +: recurse(s, i + 1)
+
+          recurse(s, 0)
+
+        val indices = allIndicesOf(s"--$argName")
+        indices.filter(_ >= 0)
+
+      private def getArgGetter[T](argName: String, getDefaultGetter: () => () => T)(using p: ArgumentParser[T]): () => T =
+        indicesOfArg(argName) match {
+          case s @ (Seq() | Seq(_)) =>
+            val argOpt = s.headOption.map(idx => argAt(idx + 1)).getOrElse(nextPositionalArg())
+            argOpt match {
+              case Some(arg) => convert(argName, arg, p)
+              case None => getDefaultGetter()
+            }
+          case s =>
+            val multValues = s.flatMap(idx => argAt(idx + 1))
+            error(s"more than one value for $argName: ${multValues.mkString(", ")}")
+        }
+
+      private def registerArg(argName: String, argType: String, argDoc: String, argKind: ArgumentKind): Unit =
+        argNames += argName
+        argTypes += argType
+        argDocs += argDoc
+        argKinds += argKind
+
+      override def argGetter[T](argName: String, argType: String, argDoc: String)(using p: ArgumentParser[T]): () => T =
+        registerArg(argName, argType, argDoc, ArgumentKind.SimpleArgument)
+        getArgGetter(argName, () => error(s"missing argument for $argName"))
+
+      override def argGetterDefault[T](argName: String, argType: String, argDoc: String, defaultValue: => T)(using p: ArgumentParser[T]): () => T =
+        registerArg(argName, argType, argDoc, ArgumentKind.OptionalArgument)
+        getArgGetter(argName, () => () => defaultValue)
+
+      override def argsGetter[T](argName: String, argType: String, argDoc: String)(using p: ArgumentParser[T]): () => Seq[T] =
+        registerArg(argName, argType, argDoc, ArgumentKind.VarArgument)
+        def remainingArgGetters(): List[() => T] = nextPositionalArg() match
+          case Some(arg) => convert(argName, arg, p) :: remainingArgGetters()
+          case None => Nil
+        val getters = remainingArgGetters()
+        () => getters.map(_())
+
+      override def run(f: => MainResultType): Unit =
+        def flagUnused(): Unit = nextPositionalArg() match
+          case Some(arg) =>
+            error(s"unused argument: $arg")
+            flagUnused()
+          case None =>
+            for
+              arg <- args
+              if arg.startsWith("--") && !argNames.contains(arg.drop(2))
+            do
+              error(s"unknown argument name: $arg")
+        end flagUnused
+
+        if args.contains("--help") then
+          usage()
+          println()
+          explain()
+        else
+          flagUnused()
+          if errors.nonEmpty then
+            for msg <- errors do println(s"Error: $msg")
+            usage()
+          else
+            println(Await.result(f, Duration(timeout, SECONDS)))
+      end run
+  end command
+end mainAwait
