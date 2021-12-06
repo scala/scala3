88137559e0869caed63a7657d9fd12e3edfc0463
Martin Odersky <odersky@gmail.com>
2021-12-06 14:47:53 +0100

    Harden `hasInnerErrors` and fix tests

--- tests/neg/i903.scala
+++
@@ -1,4 +0,0 @@
-object Test {
-  def test2 =
-    "".contains("", (_: Int)) // error
-}
--- tests/neg/zipped.scala
+++ tests/neg/zipped.scala
@@ -1,9 +1,8 @@
 object Test {
   val xs: List[Int] = ???
 
- // Does not work anynore since auto(un)tupling does not work after conversions
   xs.lazyZip(xs).lazyZip(xs)
-    .map( (x: (Int, Int, Int)) => x match { case (x, y, z) => x + y + z })     // error
+    .map( (x: (Int, Int, Int)) => x match { case (x, y, z) => x + y + z })     // ok
 
   xs.lazyZip(xs).lazyZip(xs)
     .map( x => x match { case (x, y, z) => x + y + z })     // error
