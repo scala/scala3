340a3b9e5f8ccbfd2c88da492c300e128a97d7d1
Martin Odersky <odersky@gmail.com>
2021-12-07 11:45:07 +0100

    Make export of default getters work also with named and given exports

--- tests/run/i14020.scala
+++ tests/run/i14020.scala
@@ -5,9 +5,31 @@ class B:
   val a = A()
   export a.*
 
+class C:
+  val a = A()
+  export a.greeting
+
 @main def Test =
   val b = B()
 
   println(b.a.greeting())      // works
   println(b.greeting("John"))  // works
   println(b.greeting())        // nope !
+
+  val c = C()
+
+  println(c.a.greeting())      // works
+  println(c.greeting("John"))  // works
+  println(c.greeting())        // nope !
+
+  val w = Wolf()
+  import w.given
+
+  println(summon[String]) // error: I found: w.bark(/* missing */summon[String])
+
+class Dog:
+  given bark(using msg: String = "Woof!"): String = s"bark: $msg"
+
+class Wolf:
+  private val dog = Dog()
+  export dog.given // needs to be `export dog.{given, *}` to export the default arguments
