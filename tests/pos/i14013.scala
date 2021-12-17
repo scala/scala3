f3f9e7946a4f67a13f70bdcedbb99545c8d02e6f
Martin Odersky <odersky@gmail.com>
2021-12-17 12:15:09 +0100

    Flesh out test

--- tests/pos/i14013.scala
+++ tests/pos/i14013.scala
@@ -1,3 +1,33 @@
+import LightTypeTagInheritance._
+
+trait LightTypeTagRef
+
+object LightTypeTagInheritance {
+  private final case class Ctx(self: LightTypeTagInheritance) {
+    def next(): Ctx = Ctx(self)
+  }
+  private implicit final class CtxExt(private val ctx: Ctx) extends AnyVal {
+    def isChild(selfT0: LightTypeTagRef, thatT0: LightTypeTagRef): Boolean = ctx.self.isChild(ctx.next())(selfT0, thatT0)
+  }
+}
+
+class LightTypeTagInheritance {
+
+  def isChild(s: LightTypeTagRef, t: LightTypeTagRef): Boolean = {
+    isChild(new Ctx(this))(s, t)
+  }
+
+  private def isChild(ctx: Ctx)(s: LightTypeTagRef, t: LightTypeTagRef): Boolean = {
+    ctx.isChild(s, t)
+  }
+
+}
+
+object App extends App {
+  println(LightTypeTagInheritance)
+}
+
+
 object Foo {
   case class Bar(i: Int)
 
@@ -10,6 +40,24 @@ class Foo {
   def bar = Foo.Bar(1).twice
 }
 
-object App extends App {
+object App2 extends App {
   println((new Foo).bar)
 }
+
+object Foo2 {
+  case class Bar(i: Int)
+
+  private given BarOps: AnyRef with {
+    extension (bar: Bar)
+      def twice: Bar = Bar(bar.i * 2)
+  }
+}
+
+class Foo2 {
+  def bar = Foo2.Bar(1).twice
+}
+
+object App3 extends App {
+  println((new Foo2).bar)
+}
+
