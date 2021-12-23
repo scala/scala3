0782259c308b3f30e0a070b770c883eb814150b0
Martin Odersky <odersky@gmail.com>
2021-12-23 13:27:23 +0100

    Allow empty capture sets in types
    This gives a more convenient override to disable capture set propagation
    in curried types than wrapping in a type alias. E.g. compare
    
        {x} A -> {} B -> C
    
    with
    
        {x} A -> Protect[B -> C]
    
    where
    
        type Protect[X] = X

--- tests/neg-custom-args/captures/curried-simplified.scala
+++ tests/neg-custom-args/captures/curried-simplified.scala
@@ -10,11 +10,12 @@ def test(x: Cap, y: Cap) =
   def x3: Cap -> Int -> Int = ???
   def y3: Cap -> Protect[Int -> Int] = x3 // error
   def x4: Cap -> Protect[Int -> Int] = ???
+  def y4: Cap -> {} Int -> Int = x4 // ok
   def x5: Cap -> {x} Int -> Int = ???
-  def y5: Cap -> Protect[Int -> Int] = x5 // error
+  def y5: Cap -> {} Int -> Int = x5 // error
   def x6: Cap -> Cap -> Int -> Int = ???
-  def y6: Cap -> Protect[Cap -> Protect[Int -> Int]] = x6 // error
+  def y6: Cap -> {} Cap -> Protect[Int -> Int] = x6 // error
   def x7: Cap -> (x: Cap) -> Int -> Int = ???
-  def y7: Cap -> Protect[Cap -> Protect[Int -> Int]] = x7 // error
+  def y7: Cap -> Protect[Cap -> {} Int -> Int] = x7 // error
 
 
