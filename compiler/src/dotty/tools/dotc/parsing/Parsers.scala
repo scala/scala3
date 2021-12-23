fa96111f2ac804bb6990e14b03a97288b57c4717
Martin Odersky <odersky@gmail.com>
2021-12-23 18:47:33 +0100

    Shorthand for curried functions
    Propagate capture sets to the right in curried functions. Example:
    
        {x} A -> B -> C
    
    is a shorthand for
    
        {x} A -> {x} B -> C
    
    or:
    
        (x: {*} A) -> B -> C
    
    is a shorthand for
    
        (x: {*} A) -> {x} B -> C
    
    or:
    
        ({*} A) -> B -> C
    
    is a shorthand for
    
        (x$0: {*} A) -> {x$0} B -> C
    
    Also: allow empty capture sets in types
    
    This gives a more convenient override to disable capture set propagation
    in curried types than wrapping in a type alias. E.g. compare
    
        {x} A -> {} B -> C
    
    with
    
        {x} A -> Protect[B -> C]
    
    where
    
        type Protect[X] = X
    
    Also: refactoring to move setup code from Rechecker and CheckCaptures into a joint
    class cc.Setup.

--- compiler/src/dotty/tools/dotc/parsing/Parsers.scala
+++ compiler/src/dotty/tools/dotc/parsing/Parsers.scala
@@ -905,6 +905,10 @@ object Parsers {
 
     def followingIsCaptureSet(): Boolean =
       val lookahead = in.LookaheadScanner()
+      def followingIsTypeStart() =
+        lookahead.nextToken()
+        canStartInfixTypeTokens.contains(lookahead.token)
+        || lookahead.token == LBRACKET
       def recur(): Boolean =
         (lookahead.isIdent || lookahead.token == THIS) && {
           lookahead.nextToken()
@@ -912,14 +916,10 @@ object Parsers {
             lookahead.nextToken()
             recur()
           else
-            lookahead.token == RBRACE && {
-              lookahead.nextToken()
-              canStartInfixTypeTokens.contains(lookahead.token)
-              || lookahead.token == LBRACKET
-            }
+            lookahead.token == RBRACE && followingIsTypeStart()
         }
       lookahead.nextToken()
-      recur()
+      if lookahead.token == RBRACE then followingIsTypeStart() else recur()
 
   /* --------- OPERAND/OPERATOR STACK --------------------------------------- */
 
@@ -1490,7 +1490,9 @@ object Parsers {
           else { accept(TLARROW); typ() }
         }
         else if in.token == LBRACE && followingIsCaptureSet() then
-          val refs = inBraces { commaSeparated(captureRef) }
+          val refs = inBraces {
+            if in.token == RBRACE then Nil else commaSeparated(captureRef)
+          }
           val t = typ()
           CapturingTypeTree(refs, t)
         else if (in.token == INDENT) enclosed(INDENT, typ())
