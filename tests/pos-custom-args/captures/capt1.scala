9cda65c2d84f3b787c6b428bd988c47a11c38e42
Martin Odersky <odersky@gmail.com>
2021-12-25 11:53:08 +0100

    Pure function types -> and ?->
     1. Allow `->` and `?->` and function operators, treated like `=>` and `?=>`.
     2. under -Ycc treat `->` and `?->` as immutable function types, whereas `A => B`
        is an alias of `{*} A -> B` and `A ?=> B` is an alias of `{*} A ?-> B`.
    
    Closures are unaffected, we still use `=>` for all closures where they are pure or not.
    Improve printing of capturing types
    
    Avoid explicit retains annotations also outside phase cc
    Generate "Impure" function aliases
    
    For every (possibly erased and/or context) function class
    XFunctionN, generate an alias ImpureXFunctionN in the Scala package defined as
    
        type ImpureXFunctionN[...] = {*} XFunctionN[...]
    
    Also:
    
     - Fix a bug in TypeComparer: glb has to test subCapture in a frozen state
     - Harden EventuallyCapturingType extractor to not crash on illegal capture sets
     - Cleanup transformation of inferred types

--- tests/pos-custom-args/captures/capt1.scala
+++ tests/pos-custom-args/captures/capt1.scala
@@ -1,14 +1,14 @@
 class C
 type Cap = {*} C
-def f1(c: Cap): {c} () => c.type = () => c // ok
+def f1(c: Cap): {c} () -> c.type = () => c // ok
 
 def f2: Int =
-  val g: {*} Boolean => Int = ???
+  val g: {*} Boolean -> Int = ???
   val x = g(true)
   x
 
 def f3: Int =
-  def g: {*} Boolean => Int = ???
+  def g: Boolean => Int = ???
   def h = g
   val x = g.apply(true)
   x
@@ -16,10 +16,10 @@ def f3: Int =
 def foo() =
   val x: {*} C = ???
   val y: {x} C = x
-  val x2: {x} () => C = ???
-  val y2: {x} () => {x} C = x2
+  val x2: {x} () -> C = ???
+  val y2: {x} () -> {x} C = x2
 
-  val z1: {*} () => Cap = f1(x)
+  val z1: () => Cap = f1(x)
   def h[X](a: X)(b: X) = a
 
   val z2 =
