16721395ee6f32cce6f38dd70d002700f21c836a
Martin Odersky <odersky@gmail.com>
2021-12-25 11:53:08 +0100

    Pure function types -> and ?->
     1. Allow `->` and `?->` and function operators, treated like `=>` and `?=>`.
     2. under -Ycc treat `->` and `?->` as immutable function types, whereas `A => B`
        is an alias of `{*} A -> B` and `A ?=> B` is an alias of `{*} A ?-> B`.
    
    Closures are unaffected, we still use `=>` for all closures where they are pure or not.
    Improve printing of capturing types
    
    Avoid explicit retains annotations also outside phase cc
    Generate "Impure" function aliases
    
    For every (possibly erased and/or context) function class
    XFunctionN, generate an alias ImpureXFunctionN in the Scala package defined as
    
        type ImpureXFunctionN[...] = {*} XFunctionN[...]
    
    Also:
    
     - Fix a bug in TypeComparer: glb has to test subCapture in a frozen state
     - Harden EventuallyCapturingType extractor to not crash on illegal capture sets
     - Cleanup transformation of inferred types

--- tests/pos-custom-args/captures/capt-depfun2.scala
+++ tests/pos-custom-args/captures/capt-depfun2.scala
@@ -3,6 +3,6 @@ type Cap = C @retains(*)
 
 def f(y: Cap, z: Cap) =
   def g(): C @retains(y, z) = ???
-  val ac: ((x: Cap) => Array[String @retains(x)]) = ???
+  val ac: ((x: Cap) -> Array[String @retains(x)]) = ???
   val dc: Array[? >: String <: {y, z} String] = ac(g()) // needs to be inferred
   val ec = ac(y)
