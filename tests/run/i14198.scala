0c5b5a5efc6e28ab5f47558c23f645245a7a963f
Som Snytt <som.snytt@gmail.com>
2022-01-03 04:59:36 -0800

    Clear temp var for captured var expr to permit GC

---
+++ tests/run/i14198.scala
@@ -0,0 +1,60 @@
+
+import java.lang.ref.WeakReference
+import java.util.concurrent.atomic.AtomicReference
+
+final class Mark
+
+object Test:
+
+  def main(args: Array[String]): Unit =
+    myTest
+    trying
+
+  final def myAssert(cond: => Boolean): Unit = assert(cond)
+
+  def terminally(cond: => Boolean): Unit =
+    System.gc()
+    var n = 10
+    while (n > 0 && !cond)
+    do
+      System.gc()
+      Thread.`yield`()
+      //print(".")
+      n -= 1
+    assert(cond)
+
+  def myTest: Unit =
+    val ref = new AtomicReference[WeakReference[AnyRef]]
+    var mark: AnyRef = null
+    assert(ref.compareAndSet(null, WeakReference(Mark())))
+    mark = ref.get().get()
+    myAssert(mark ne null) // in theory this could fail, but it isn't
+    mark = null
+    terminally(ref.get().get() == null)
+
+  def trying: Unit =
+    def ignore[A]: (Throwable => A) = _ => null.asInstanceOf[A]
+    var i: Int = 21
+    var s: String = "hello"
+    var r: WeakReference[String] = null
+    def f(n: => Int) = n + n + 1
+    def g(x: => String) =
+      r = WeakReference(x + "/" + x)
+      r.get()
+    i = try f(i) catch ignore
+    s = try g(s) catch ignore
+    assert(s == "hello/hello")
+    assert(r.get() == "hello/hello")
+    s = null
+    terminally(r.get() == null)
+    s = "bye"
+    s = try g(s) catch ignore
+    assert(s == "bye/bye")
+    assert(r.get() == "bye/bye")
+    s = null.asInstanceOf[String]
+    terminally(r.get() == null)
+    @volatile var z: String = "whoa"
+    z = try g(z) catch ignore
+    assert(r.get() == "whoa/whoa")
+    z = null
+    terminally(r.get() == null)
--- tests/run/liftedTry.scala
+++ tests/run/liftedTry.scala
@@ -16,7 +16,7 @@ object Test {
     assert(x == 1)
     assert(foo(2) == 2)
     assert(foo(try raise(3) catch handle) == 3)
-    Tr.foo
+    assert(Tr.foo == 3)
   }
 }
 
@@ -24,7 +24,26 @@ object Tr {
   def fun(a: Int => Unit) = a(2)
   def foo: Int = {
     var s = 1
-    s = try {fun(s = _); 3} catch{ case ex: Throwable => val x = 4; s = x; 5 }
+    s = try {fun(s = _); 3} catch { case ex: Throwable => val x = 4; s = x; 5 }
     s
   }
 }
+
+/* was:
+Caused by: java.lang.VerifyError: Inconsistent stackmap frames at branch target 33
+Exception Details:
+  Location:
+    Tr$.foo()I @30: goto
+  Reason:
+    Current frame's stack size doesn't match stackmap.
+  Current Frame:
+    bci: @30
+    flags: { }
+    locals: { 'Tr$', 'scala/runtime/IntRef', 'java/lang/Throwable', integer }
+    stack: { integer }
+  Stackmap Frame:
+    bci: @33
+    flags: { }
+    locals: { 'Tr$', 'scala/runtime/IntRef' }
+    stack: { top, integer }
+ */
