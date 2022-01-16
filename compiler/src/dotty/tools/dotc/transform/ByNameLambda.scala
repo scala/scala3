8fe40ca381e58975091f793a45d14254eb768357
Martin Odersky <odersky@gmail.com>
2022-01-16 18:57:44 +0100

    Avoid using changeOwner in ByNameLambda
    The regular changeOwner can cause cycles in transformations. Transformations like ByNameLambda should use
    the less demanding changeOwnerAfter.

--- compiler/src/dotty/tools/dotc/transform/ByNameLambda.scala
+++ compiler/src/dotty/tools/dotc/transform/ByNameLambda.scala
@@ -4,14 +4,16 @@ package transform
 
 import core._
 import Flags._
-import MegaPhase._
+import MegaPhase._, DenotTransformers.IdentityDenotTransformer
 import Symbols._, Contexts._, Types._, Decorators._
 import StdNames.nme
 import ast.Trees._
 import ast.TreeTypeMap
 
 /** Rewrite applications `<byname>(...)` to context closures `() ?=> ...` */
-class ByNameLambda extends MiniPhase:
+class ByNameLambda extends MiniPhase, IdentityDenotTransformer:
+  thisPhase =>
+
   import ast.tpd._
 
   def phaseName: String = ByNameLambda.name
@@ -22,18 +24,18 @@ class ByNameLambda extends MiniPhase:
         case Apply(Select(fn, nme.apply), Nil) if isPurePath(fn) && fn.tpe.widen.isByName =>
           fn
         case _ =>
-          ByNameLambda(body)
+          byNameClosure(body)
     case _ => app
+
+  def byNameClosure(body: Tree)(using Context): Block =
+    val restpe = body.tpe.widenIfUnstable.deskolemized
+    val meth = newAnonFun(ctx.owner, MethodType(Nil, restpe), coord = body.span)
+    Closure(meth, _ => body.changeOwnerAfter(ctx.owner, meth, thisPhase),
+      targetType = defn.ContextFunction0.typeRef.appliedTo(restpe))
 
 object ByNameLambda:
   import ast.tpd._
 
   val name = "byNameLambda"
-
-  def apply(body: Tree)(using Context): Block =
-    val restpe = body.tpe.widenIfUnstable.deskolemized
-    val meth = newAnonFun(ctx.owner, MethodType(Nil, restpe), coord = body.span)
-    Closure(meth, _ => body.changeOwner(ctx.owner, meth),
-      targetType = defn.ContextFunction0.typeRef.appliedTo(restpe))
 
 end ByNameLambda
