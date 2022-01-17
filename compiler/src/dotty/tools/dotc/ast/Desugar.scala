3ff1fc2d0ee348ad99f47c08c4476ac794649406
Nicolas Stucki <nicolas.stucki@gmail.com>
2022-01-17 14:39:35 +0100

    Properly desugar `inline given .. with ..`
    ```scala
    inline given Foo with { .. }
    ```
    
    now becomes
    ```scala
    inline given def given_Foo: Foo with {..} = new Foo
    class given_Foo extends Foo with {  }
    ```
    
    There are no creation of `lazy vals` when we have `inline`.
    We inline the `new givne_Foo` only, the class does not get inlined.
    This is useful to remove the instatiation of the given instance when
    the methods are inline themselfs.
    
    Fixes #14282
    Fixes #14177

--- compiler/src/dotty/tools/dotc/ast/Desugar.scala
+++ compiler/src/dotty/tools/dotc/ast/Desugar.scala
@@ -783,7 +783,7 @@ object desugar {
         DefDef(
           className.toTermName, joinParams(constrTparams, defParamss),
           classTypeRef, creatorExpr)
-          .withMods(companionMods | mods.flags.toTermFlags & GivenOrImplicit | Final)
+          .withMods(companionMods | mods.flags.toTermFlags & (GivenOrImplicit | Inline) | Final)
           .withSpan(cdef.span) :: Nil
       }
 
@@ -809,7 +809,7 @@ object desugar {
             Nil
         }
       }
-      val classMods = if mods.is(Given) then mods | Synthetic else mods
+      val classMods = if mods.is(Given) then mods &~ Inline | Synthetic else mods
       cpy.TypeDef(cdef: TypeDef)(
         name = className,
         rhs = cpy.Template(impl)(constr, parents1, clsDerived, self1,
