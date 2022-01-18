3aeeba3034841c75cb9803c5442c3623425c2595
Som Snytt <som.snytt@gmail.com>
2022-01-18 12:12:47 -0800

    Prefer context functions for ReplTests

--- compiler/test/dotty/tools/repl/TypeTests.scala
+++ compiler/test/dotty/tools/repl/TypeTests.scala
@@ -4,25 +4,26 @@ import org.junit.Assert._
 import org.junit.Test
 
 class TypeTests extends ReplTest {
-  @Test def typeOf1 = fromInitialState { implicit s =>
+  @Test def typeOf1 = initially {
     run(":type 1")
     assertEquals("Int", storedOutput().trim)
   }
 
-  @Test def typeOfBlock = fromInitialState { implicit s =>
+  @Test def typeOfBlock = initially {
     run(":type { /** omg omg omg */ 1 + 5; 1 }")
     assertEquals("Int", storedOutput().trim)
   }
 
   @Test def typeOfX =
-    fromInitialState { implicit s => run("val x = 5") }
-    .andThen { implicit s =>
+    initially {
+      run("val x = 5")
+    } andThen {
       storedOutput() // discard output
       run(":type x")
       assertEquals("Int", storedOutput().trim)
     }
 
-  @Test def typeOfEmpty = fromInitialState { implicit s =>
+  @Test def typeOfEmpty = initially {
     run(":type")
     assertEquals(":type <expression>", storedOutput().trim)
   }
