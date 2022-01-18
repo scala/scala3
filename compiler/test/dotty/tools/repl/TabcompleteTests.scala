46f334a020bf5b47e88c91932fb6d8ff0e31c1c0
Som Snytt <som.snytt@gmail.com>
2022-01-18 12:12:47 -0800

    Prefer context functions for ReplTests

--- compiler/test/dotty/tools/repl/TabcompleteTests.scala
+++ compiler/test/dotty/tools/repl/TabcompleteTests.scala
@@ -10,22 +10,22 @@ class TabcompleteTests extends ReplTest {
   private def tabComplete(src: String)(implicit state: State): List[String] =
     completions(src.length, src, state).map(_.value).sorted
 
-  @Test def tabCompleteList = fromInitialState { implicit s =>
+  @Test def tabCompleteList = initially {
     val comp = tabComplete("List.r")
     assertEquals(List("range"), comp.distinct)
   }
 
-  @Test def tabCompleteListInstance = fromInitialState { implicit s =>
+  @Test def tabCompleteListInstance = initially {
     val comp = tabComplete("(null: List[Int]).sli")
     assertEquals(List("slice", "sliding"), comp.distinct)
   }
 
-  @Test def tabCompleteModule = fromInitialState{ implicit s =>
+  @Test def tabCompleteModule = initially {
     val comp = tabComplete("scala.Pred")
     assertEquals(List("Predef"), comp)
   }
 
-  @Test def tabCompleteInClass = fromInitialState { implicit s =>
+  @Test def tabCompleteInClass = initially {
     val comp = tabComplete("class Foo { def bar: List[Int] = List.ap")
     assertEquals(List("apply"), comp)
   }
@@ -34,77 +34,70 @@ class TabcompleteTests extends ReplTest {
     val src1 = "class Foo { def bar(xs: List[Int]) = xs.map"
     val src2 = "class Foo { def bar(xs: List[Int]) = xs.mapC"
 
-    fromInitialState { implicit state =>
-      val comp = tabComplete(src1)
-      assertEquals(List("map", "mapConserve"), comp)
-      state
-    }
-    .andThen { implicit state =>
-      val comp = tabComplete(src2)
-      assertEquals(List("mapConserve"), comp)
+    initially {
+      val comp1 = tabComplete(src1)
+      assertEquals(List("map", "mapConserve"), comp1)
+      val comp2 = tabComplete(src2)
+      assertEquals(List("mapConserve"), comp2)
     }
   }
 
-  @Test def i3309 = fromInitialState { implicit s =>
+  @Test def i3309 = initially {
     // We make sure we do not crash
     List("\"", ")", "'", "¨", "£", ":", ",", ";", "@", "}", "[", "]", ".")
       .foreach(tabComplete(_))
   }
 
   @Test def completeFromPreviousState =
-    fromInitialState { implicit state  =>
+    initially {
       val src = "class Foo { def comp3 = 3; def comp1 = 1; def comp2 = 2 }"
       run(src)
-    }
-    .andThen { implicit state =>
+    } andThen {
       val expected = List("comp1", "comp2", "comp3")
       assertEquals(expected, tabComplete("(new Foo).comp"))
     }
 
   @Test def completeFromPreviousState2 =
-    fromInitialState { implicit state  =>
+    initially {
       val src = "def hello = 1"
       run(src)
-    }
-    .andThen { implicit state =>
+    } andThen {
       val expected = List("hello")
       assertEquals(expected, tabComplete("hel"))
     }
 
   @Test def tabCompleteFromPreviousImport =
-    fromInitialState { implicit state =>
+    initially {
       val src = "import java.io.FileDescriptor"
       run(src)
-    }
-    .andThen { implicit state =>
+    } andThen {
       val expected = List("FileDescriptor")
       assertEquals(expected, tabComplete("val foo: FileDesc"))
     }
 
   @Test def tabCompleteRenamedImport =
-    fromInitialState { implicit state =>
+    initially {
       val src = "import java.io.{FileDescriptor => Renamed}"
       run(src)
-    }
-    .andThen { implicit state =>
+    } andThen {
       val expected = List("Renamed")
       assertEquals(expected, tabComplete("val foo: Rena"))
     }
 
-  @Test def tabClosureComplete = fromInitialState { implicit s =>
+  @Test def tabClosureComplete = initially {
     assertEquals(List("map", "mapConserve"), tabComplete("Nil.map"))
     assertEquals(List("map", "mapConserve"), tabComplete("(x: Int => Int) => Nil.map"))
     assertEquals(List("apply"), tabComplete("(x: Int => Int) => x.ap"))
   }
 
-  @Test def importScala = fromInitialState { implicit s =>
+  @Test def importScala = initially {
     val comp = tabComplete("import scala.")
     // check that there are no special symbols leaked: <byname>, <special-ops>, ...
     assertEquals(Some("<:<"), comp.find(_.startsWith("<")))
     assert(!comp.contains("package"))
   }
 
-  @Test def `null` = fromInitialState { implicit s =>
+  @Test def `null` = initially {
     val comp = tabComplete("null.")
     assertEquals(
       List("!=", "##", "==", "asInstanceOf", "eq", "equals", "getClass", "hashCode",
@@ -112,7 +105,7 @@ class TabcompleteTests extends ReplTest {
       comp.distinct.sorted)
   }
 
-  @Test def anyRef = fromInitialState { implicit s =>
+  @Test def anyRef = initially {
     val comp = tabComplete("(null: AnyRef).")
     assertEquals(
       List("!=", "##", "->", "==", "asInstanceOf", "ensuring", "eq", "equals", "formatted",
@@ -120,20 +113,20 @@ class TabcompleteTests extends ReplTest {
       comp.distinct.sorted)
   }
 
-  @Test def `???` = fromInitialState { implicit s =>
+  @Test def `???` = initially {
     val comp = tabComplete("???.")
     assertEquals(Nil, comp)
   }
 
-  @Test def moduleCompletion = fromInitialState { implicit s =>
+  @Test def moduleCompletion = initially {
     assertEquals(List("Predef"), tabComplete("object Foo { type T = Pre"))
   }
 
-  @Test def i6415 = fromInitialState { implicit s =>
+  @Test def i6415 = initially {
     assertEquals(List("Predef"), tabComplete("object Foo { opaque type T = Pre"))
   }
 
-  @Test def i12600 = fromInitialState { implicit s =>
+  @Test def i12600 = initially {
     assertEquals(List("select", "show", "simplified", "substituteTypes"),
       tabComplete("import quoted.* ; def fooImpl(using Quotes): Expr[Int] = { import quotes.reflect.* ; TypeRepr.of[Int].s"))
   }
