d603c9171f4e62509c640c2b78c105be5e88eb80
Martin Odersky <odersky@gmail.com>
2022-01-21 14:27:43 +0100

    Updates for latest master
     - Fix rebase breakage
     - weaken test in TreePickler that was introduced in the meantime since the
       last rebase (this one needs follow up)
     - adapt to latest restrictions on rhs of erased definitions

--- tests/pos-custom-args/captures/try.scala
+++ tests/pos-custom-args/captures/try.scala
@@ -13,7 +13,7 @@ def foo(x: Boolean): Int throws Fail =
   if x then 1 else raise(Fail())
 
 def handle[E <: Exception, R](op: (erased CanThrow[E]) -> R)(handler: E -> R): R =
-  erased val x: CanThrow[E] = ???
+  erased val x: CanThrow[E] = ??? : CanThrow[E]
   try op(x)
   catch case ex: E => handler(ex)
 
