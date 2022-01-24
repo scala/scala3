26c863072abb2cd97fe39565ac1a7bcb2fbd6dcb
Nicolas Stucki <nicolas.stucki@gmail.com>
2022-01-24 13:57:18 +0100

    Fix test output
    The warning was removed in #12261
    
    Fixes #14332

--- compiler/test-resources/repl/i13208.scala
+++ compiler/test-resources/repl/i13208.scala
@@ -1,10 +1,5 @@
 // scalac: -source:future -deprecation
 scala> type M[X] = X match { case Int => String case _ => Int }
-1 warning found
--- Deprecation Warning: --------------------------------------------------------
-1 | type M[X] = X match { case Int => String case _ => Int }
-  |                                               ^
-  |        `_` is deprecated for wildcard arguments of types: use `?` instead
 scala> type N[X] = X match { case List[_] => Int }
 1 warning found
 -- Deprecation Warning: --------------------------------------------------------
