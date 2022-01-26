39b14f6fee7b9823f77fd27d3494cae23cb5899a
Martin Odersky <odersky@gmail.com>
2022-01-26 12:52:33 +0100

    Mark classes compiled under -Ycc with a CaptureChecked annotation

---
+++ library/src/scala/annotation/internal/CaptureChecked.scala
@@ -0,0 +1,8 @@
+package scala.annotation
+package internal
+
+/** A marker annotation on a toplevel class that indicates
+ *  that the class was checked under -Ycc
+ */
+class CaptureChecked extends StaticAnnotation
+
