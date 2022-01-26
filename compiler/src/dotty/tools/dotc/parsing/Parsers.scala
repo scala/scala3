eb75c725135a2bccdb004c341ff71c9b8c09e6a4
Martin Odersky <odersky@gmail.com>
2022-01-26 16:26:19 +0100

    Syntax change: allow capture sets in infix types

--- compiler/src/dotty/tools/dotc/parsing/Parsers.scala
+++ compiler/src/dotty/tools/dotc/parsing/Parsers.scala
@@ -1490,11 +1490,7 @@ object Parsers {
           else { accept(TLARROW); typ() }
         }
         else if in.token == LBRACE && followingIsCaptureSet() then
-          val refs = inBraces {
-            if in.token == RBRACE then Nil else commaSeparated(captureRef)
-          }
-          val t = typ()
-          CapturingTypeTree(refs, t)
+          CapturingTypeTree(captureSet(), typ())
         else if (in.token == INDENT) enclosed(INDENT, typ())
         else infixType()
 
@@ -1887,7 +1883,13 @@ object Parsers {
     def typeDependingOn(location: Location): Tree =
       if location.inParens then typ()
       else if location.inPattern then rejectWildcardType(refinedType())
+      else if in.token == LBRACE && followingIsCaptureSet() then
+        CapturingTypeTree(captureSet(), infixType())
       else infixType()
+
+    def captureSet(): List[Tree] = inBraces {
+      if in.token == RBRACE then Nil else commaSeparated(captureRef)
+    }
 
 /* ----------- EXPRESSIONS ------------------------------------------------ */
 
@@ -1958,7 +1960,7 @@ object Parsers {
      *                      |  ‘inline’ InfixExpr MatchClause
      *  Bindings          ::=  `(' [Binding {`,' Binding}] `)'
      *  Binding           ::=  (id | `_') [`:' Type]
-     *  Ascription        ::=  `:' InfixType
+     *  Ascription        ::=  `:' [CaptureSet] InfixType
      *                      |  `:' Annotation {Annotation}
      *                      |  `:' `_' `*'
      *  Catches           ::=  ‘catch’ (Expr | ExprCaseClause)
@@ -3944,7 +3946,7 @@ object Parsers {
       stats.toList
     }
 
-    /** TemplateStatSeq  ::= [id [`:' Type] `=>'] TemplateStat {semi TemplateStat}
+    /** TemplateStatSeq  ::= [SelfType] TemplateStat {semi TemplateStat}
      *  TemplateStat     ::= Import
      *                     | Export
      *                     | Annotations Modifiers Def
@@ -3954,6 +3956,8 @@ object Parsers {
      *                     |
      *  EnumStat         ::= TemplateStat
      *                     | Annotations Modifiers EnumCase
+     *  SelfType         ::= id [‘:’ [CaptureSet] InfixType] ‘=>’
+     *                     | ‘this’ ‘:’ [CaptureSet] InfixType ‘=>’
      */
     def templateStatSeq(): (ValDef, List[Tree]) = checkNoEscapingPlaceholders {
       var self: ValDef = EmptyValDef
