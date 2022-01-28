00992f7b4122a5429a7653bfa59a8d0a683731a5
Martin Odersky <odersky@gmail.com>
2022-01-28 23:00:32 +0100

    New scheme to reject root captures
    Reject root captures by considering unbox operations. This allows us
    to ignore root captures buried under type applications.

--- tests/neg-custom-args/captures/real-try.scala
+++ tests/neg-custom-args/captures/real-try.scala
@@ -6,9 +6,25 @@ class Ex2 extends Exception("Ex2")
 def foo(i: Int): (CanThrow[Ex1], CanThrow[Ex2]) ?-> Unit =
   if i > 0 then throw new Ex1 else throw new Ex2
 
+class Cell[+T](val x: T)
+
 def test() =
   try  // error
     () => foo(1)
   catch
     case _: Ex1 => ???
     case _: Ex2 => ???
+
+  try  // error
+    () => Cell(foo(1))
+  catch
+    case _: Ex1 => ???
+    case _: Ex2 => ???
+
+  val b = try  // ok here, but error on use
+    Cell(() => foo(1))//: Cell[box {ev} () => Unit] <: Cell[box {*} () => Unit]
+  catch
+    case _: Ex1 => ???
+    case _: Ex2 => ???
+
+  b.x // error
