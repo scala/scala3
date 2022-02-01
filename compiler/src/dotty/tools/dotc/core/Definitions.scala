f279ed863e8e7a84842e9bf6b3235748c30bb09a
Martin Odersky <odersky@gmail.com>
2022-02-01 14:58:19 +0100

    Capture-check exception capabilties
    Required exeption capability references are now preserved beyond typer
    in type ascriptions, so that they can be checked for escapes.

--- compiler/src/dotty/tools/dotc/core/Definitions.scala
+++ compiler/src/dotty/tools/dotc/core/Definitions.scala
@@ -968,6 +968,7 @@ class Definitions {
   @tu lazy val TargetNameAnnot: ClassSymbol = requiredClass("scala.annotation.targetName")
   @tu lazy val VarargsAnnot: ClassSymbol = requiredClass("scala.annotation.varargs")
   @tu lazy val SinceAnnot: ClassSymbol = requiredClass("scala.annotation.since")
+  @tu lazy val RequiresCapabilityAnnot: ClassSymbol = requiredClass("scala.annotation.internal.requiresCapability")
   @tu lazy val RetainsAnnot: ClassSymbol = requiredClass("scala.retains")
   @tu lazy val RetainsByNameAnnot: ClassSymbol = requiredClass("scala.retainsByName")
 
