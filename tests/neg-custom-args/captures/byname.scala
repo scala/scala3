1b4370bed97a0ba38ed13718ff0198f0353e7c07
Martin Odersky <odersky@gmail.com>
2022-02-01 11:14:42 +0100

    Change result type of by-name closures
    Make it the formal type rather than the actual one. This avoids messing up
    capture annotations.

--- tests/neg-custom-args/captures/byname.scala
+++ tests/neg-custom-args/captures/byname.scala
@@ -3,8 +3,11 @@
 def test(cap1: Cap, cap2: Cap) =
   def f() = if cap1 == cap1 then g else g
   def g(x: Int) = if cap2 == cap2 then 1 else x
+  def g2(x: Int) = if cap1 == cap1 then 1 else x
+  def f2() = if cap1 == cap1 then g2 else g2
   def h(ff: => {cap2} Int -> Int) = ff
-  h(f())  // error
+  h(f())  // ok
+  h(f2()) // error
 
 class I
 
