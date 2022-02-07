03abdedb7105197ca275ad916f784e243955192d
Nicolas Stucki <nicolas.stucki@gmail.com>
2022-02-07 17:26:43 +0100

    Re-enable missing cmdTests
    It might have been disabled by mistake because bootstrapCmdTests was
    assumed to be the set of tests for the bootstrapped compiler.
    The second is an extension for the second with tests that can only
    be run on the bootstrapped compiler. It is now renamed to
    bootstrappedOnlyCmdTests to make sure we avoid this potential confusion.
    
    Fixes #14428

--- tests/pos/i12973.scala
+++ tests/pos/i12973.scala
@@ -1,4 +1,4 @@
-// Compiled and placed on the REPL classpath in the bootstrapCmdTests script
+// Compiled and placed on the REPL classpath in the bootstrappedOnlyCmdTests script
 // to test that launching the REPL with `scala -cp <path>` works (issue #12973)
 case class Bug12973():
   def check = s"$productPrefix is fixed"
