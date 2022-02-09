8561b9e6dc08516ff4d8df0120f15ce3024555d5
Martin Odersky <odersky@gmail.com>
2022-02-09 08:33:37 +0100

    Test from doc page

--- tests/pos-custom-args/captures/lazylists-exceptions.scala
+++ tests/pos-custom-args/captures/lazylists-exceptions.scala
@@ -1,56 +1,56 @@
 import language.experimental.saferExceptions
-import annotation.unchecked.uncheckedVariance
+import scala.compiletime.uninitialized
 
-trait LazyList[+A]:
+trait LzyList[+A]:
   def isEmpty: Boolean
   def head: A
-  def tail: {this} LazyList[A]
+  def tail: {this} LzyList[A]
 
-object LazyNil extends LazyList[Nothing]:
-  def isEmpty: Boolean = true
+object LzyNil extends LzyList[Nothing]:
+  def isEmpty = true
   def head = ???
   def tail = ???
 
-final class LazyCons[+T](val x: T, val xs: () => {*} LazyList[T]) extends LazyList[T]:
-  var forced = false
-  var cache: {this} LazyList[T @uncheckedVariance] = compiletime.uninitialized
-
+final class LzyCons[+A](hd: A, tl: () => {*} LzyList[A]) extends LzyList[A]:
+  private var forced = false
+  private var cache: {this} LzyList[A] = uninitialized
   private def force =
-    if !forced then
-      cache = xs()
-      forced = true
+    if !forced then { cache = tl(); forced = true }
     cache
 
   def isEmpty = false
-  def head = x
-  def tail: {this} LazyList[T] = force
-end LazyCons
+  def head = hd
+  def tail: {this} LzyList[A] = force
+end LzyCons
 
-extension [A](xs: {*} LazyList[A])
-  def map[B](f: A => B): {xs, f} LazyList[B] =
-    if xs.isEmpty then LazyNil
-    else LazyCons(f(xs.head), () => xs.tail.map(f))
+extension [A](xs: {*} LzyList[A])
+  def map[B](f: A => B): {xs, f} LzyList[B] =
+    if xs.isEmpty then LzyNil
+    else LzyCons(f(xs.head), () => xs.tail.map(f))
 
-  def filter(p: A => Boolean): {xs, p} LazyList[A] =
-    if xs.isEmpty then LazyNil
+  def filter(p: A => Boolean): {xs, p} LzyList[A] =
+    if xs.isEmpty then LzyNil
     else if p(xs.head) then lazyCons(xs.head, xs.tail.filter(p))
     else xs.tail.filter(p)
 
-  def concat(ys: {*} LazyList[A]): {xs, ys} LazyList[A] =
+  def concat(ys: {*} LzyList[A]): {xs, ys} LzyList[A] =
     if xs.isEmpty then ys
     else xs.head #: xs.tail.concat(ys)
+
+  def drop(n: Int): {xs} LzyList[A] =
+    if n == 0 then xs else xs.tail.drop(n - 1)
 end extension
 
 extension [A](x: A)
-  def #:(xs1: => {*} LazyList[A]): {xs1} LazyList[A] =
-    LazyCons(x, () => xs1)
+  def #:(xs1: => {*} LzyList[A]): {xs1} LzyList[A] =
+    LzyCons(x, () => xs1)
 
-def lazyCons[A](x: A, xs1: => {*} LazyList[A]): {xs1} LazyList[A] =
-  LazyCons(x, () => xs1)
+def lazyCons[A](x: A, xs1: => {*} LzyList[A]): {xs1} LzyList[A] =
+  LzyCons(x, () => xs1)
 
 def tabulate[A](n: Int)(gen: Int => A) =
-  def recur(i: Int): {gen} LazyList[A] =
-    if i == n then LazyNil
+  def recur(i: Int): {gen} LzyList[A] =
+    if i == n then LzyNil
     else gen(i) #: recur(i + 1)
   recur(0)
 
@@ -58,7 +58,7 @@ class Ex1 extends Exception
 class Ex2 extends Exception
 
 def test(using cap1: CanThrow[Ex1], cap2: CanThrow[Ex2]) =
-  val xs = 1 #: LazyNil
+  val xs = 1 #: LzyNil
 
   def f(x: Int): Int throws Ex1 =
     if x < 0 then throw Ex1()
@@ -69,14 +69,21 @@ def test(using cap1: CanThrow[Ex1], cap2: CanThrow[Ex2]) =
     x * x
 
   def x1 = xs.map(f)
-  def x1c: {cap1} LazyList[Int] = x1
+  def x1c: {cap1} LzyList[Int] = x1
 
   def x2 = x1.concat(xs.map(g).filter(_ > 0))
-  def x2c: {cap1, cap2} LazyList[Int] = x2
+  def x2c: {cap1, cap2} LzyList[Int] = x2
 
   val x3 = tabulate(10) { i =>
       if i > 9 then throw Ex1()
       i * i
     }
-  val x3c: {cap1} LazyList[Int] = x3
+  val x3c: {cap1} LzyList[Int] = x3
 
+class LimitExceeded extends Exception
+
+def test2(n: Int)(using ct: CanThrow[LimitExceeded])  =
+  val xs = tabulate(10) { i =>
+    if i > 9 then throw LimitExceeded()
+    i * i
+  }
