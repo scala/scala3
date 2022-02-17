317b08c937e7b55e48d0541a4a752db2c67e0c44
adampauls <adpauls@microsoft.com>
2022-02-17 08:24:25 -0800

    Better error recovery in comma-separated lists

--- tests/neg/i1679.scala
+++ tests/neg/i1679.scala
@@ -1,5 +1,5 @@
 class A[T]
 object o {
   // Testing compiler crash, this test should be modified when named type argument are completely implemented
-  val x: A[T=Int, T=Int] = ??? // error: ']' expected, but '=' found // error
+  val x: A[T=Int, T=Int] = ??? // error: ']' expected, but '=' found // error: ']' expected, but '=' found
 }
