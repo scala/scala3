93b219296d0c7de802af67548267a17074a670af
odersky <odersky@gmail.com>
2022-03-02 18:57:52 +0100

    Refinements to capture checking and a collection strawman
    Fixes and refinements needed to compile a full collection strawman and tests with
    capture checking turned on.
    
     1. Exclude ResetPrivate symbols from overriding checks
    
        A symbol with the ResetPrivate flag should not take part in overriding checks.
        We have reset the Private just to enable refinements, but for the purpose
        of overriding checks it still should be treated as a private member.
    
     2. Refine conforms check
    
        The previous check did not work if the whole right hand side was a tracked
        variable. We now have a more robust check that works by augmenting the
        expected capture set rather than filtering the actual one.
    
     3. Keep self type around after FirstTransform
    
        Under -Ycc we need to keep the self type declaration until CheckCaptures, so that
        it can be checked for well-formedness. We can't keep the original self (which is
        a ValDef) since other phases assume it is empty after FirstTransform. Instead we
        copy the type `S` into a synthetic definition
    
            private[this] type $this = S
    
     4. Exclude some synthetic case class methods from checking, since their implementations
        do not allow for capture sets. We need to get back to them eventually, and annotate the
        methods in the correct way.
    
     5. Handle function type aliases in preTypeArgs
    
     6. Fixes for handling capture set ranges
    
         - We now accept `C[? >: C1 T1 <: C2 T2]` even if `C` is higher-kinded.
           Previously we widened the range to Nothing...Any.
         - Special case for handling the subtype test `C[? >: C1 T1 <: C2 T2] <: C3 T3`
           where `T1 =:= T2`: Unify capture sets C1, C2 and C3 in this case.
    
     7. Refine inferred variable scheme
    
         - More refined model when a type gets an inferred variable.
         - Add inferred variables to pattern matching lets
    
     8. A version of collection strawman that uses abstract types
    
        A version of the collection strawman that uses abstract types instead of
        type parameters for `Repr` and `C[_]`. This is necessary since we want to
        use `this` in the capture set of some of these types.

---
+++ tests/run-custom-args/captures/colltest5/Test_2.scala
@@ -0,0 +1,185 @@
+import Predef.{augmentString as _, wrapString as _, *}
+import scala.reflect.ClassTag
+
+object Test {
+  import colltest5.strawman.collections.*
+  import CollectionStrawMan5.*
+
+  def seqOps(xs: Seq[Int]) = { // try with {*} Seq[Int]
+    val strPlusInt: (String, Int) => String = _ + _
+    val intPlusStr: (Int, String) => String = _ + _
+    val isEven: Int => Boolean = _ % 2 == 0
+    val isNonNeg: Int => Boolean = _ > 0
+    val flips: Int => List[Int] = x => Cons(x, Cons(-x, Nil))
+    val x1 = xs.foldLeft("")(strPlusInt)
+    val y1: String = x1
+    val x2 = xs.foldRight("")(intPlusStr)
+    val y2: String = x2
+    val x3 = xs.indexWhere(isEven)
+    val y3: Int = x3
+    val x4 = xs.head
+    val y4: Int = x4
+    val x5 = xs.to(List)
+    val y5: List[Int] = x5
+    val (xs6, xs7) = xs.partition(isEven)
+    val ys6: Seq[Int] = xs6
+    val ys7: Seq[Int] = xs7
+    val xs8 = xs.drop(2)
+    val ys8: Seq[Int] = xs8
+    val xs9 = xs.map(isNonNeg)
+    val ys9: Seq[Boolean] = xs9
+    val xs10 = xs.flatMap(flips)
+    val ys10: Seq[Int] = xs10
+    val xs11 = xs ++ xs
+    val ys11: Seq[Int] = xs11
+    val xs12 = xs ++ Nil
+    val ys12: Seq[Int] = xs12
+    val xs13 = Nil ++ xs
+    val ys13: Seq[Int] = xs13
+    val xs14 = xs.++[Any](Cons("a", Nil)) // !!!
+    val ys14: Seq[Any] = xs14
+    val xs15 = xs.zip(xs9)
+    val ys15: Seq[(Int, Boolean)] = xs15
+    val xs16 = xs.reverse
+    val ys16: Seq[Int] = xs16
+    println("-------")
+    println(x1)
+    println(x2)
+    println(x3)
+    println(x4)
+    println(x5)
+    println(xs6)
+    println(xs7)
+    println(xs8)
+    println(xs9)
+    println(xs10)
+    println(xs11)
+    println(xs12)
+    println(xs13)
+    println(xs14)
+    println(xs15)
+    println(xs16)
+  }
+
+  def viewOps(xs: {*} View[Int]) = {
+    val strPlusInt: (String, Int) => String = _ + _
+    val intPlusStr: (Int, String) => String = _ + _
+    val isEven: Int => Boolean = _ % 2 == 0
+    val isNonNeg: Int => Boolean = _ > 0
+    val flips: Int => List[Int] = x => Cons(x, Cons(-x, Nil))
+    val x1 = xs.foldLeft("")(strPlusInt)
+    val y1: String = x1
+    val x2 = xs.foldRight("")(intPlusStr)
+    val y2: String = x2
+    val x3 = xs.indexWhere(isEven)
+    val y3: Int = x3
+    val x4 = xs.head
+    val y4: Int = x4
+    val x5 = xs.to(List)
+    val y5: {x5} List[Int] = x5 // !!!
+    val (xs6, xs7) = xs.partition(isEven)
+    val ys6: {xs6, isEven} View[Int] = xs6
+    val ys7: {xs7, isEven} View[Int] = xs7
+    val (xs6a, xs7a) = xs.partition(_ % 2 == 0)
+    val ys6a: {xs6} View[Int] = xs6
+    val ys7a: {xs7} View[Int] = xs7
+    val xs8 = xs.drop(2)
+    val ys8: {xs8} View[Int] = xs8
+    val xs9 = xs.map(isNonNeg)
+    val ys9: {xs9} View[Boolean] = xs9
+    val xs10 = xs.flatMap(flips)
+    val ys10: {xs10} View[Int] = xs10
+    val xs11 = xs ++ xs
+    val ys11: {xs11} View[Int] = xs11
+    val xs12 = xs ++ Nil
+    val ys12: {xs12} View[Int] = xs12
+    val xs13 = Nil ++ xs
+    val ys13: List[Int] = xs13
+    val xs14 = xs ++ Cons("a", Nil)
+    val ys14: {xs14} View[Any] = xs14
+    val xs15 = xs.zip(xs9)
+    val ys15: {xs15} View[(Int, Boolean)] = xs15
+    println("-------")
+    println(x1)
+    println(x2)
+    println(x3)
+    println(x4)
+    println(x5)
+    println(xs6.to(List))
+    println(xs7.to(List))
+    println(xs8.to(List))
+    println(xs9.to(List))
+    println(xs10.to(List))
+    println(xs11.to(List))
+    println(xs12.to(List))
+    println(xs13.to(List))
+    println(xs14.to(List))
+    println(xs15.to(List))
+  }
+
+  def stringOps(xs: String) = {
+    val x1 = xs.foldLeft("")(_ + _)
+    val y1: String = x1
+    val x2 = xs.foldRight("")(_ + _)
+    val y2: String = x2
+    val x3 = xs.indexWhere(_ % 2 == 0)
+    val y3: Int = x3
+    val x4 = xs.head
+    val y4: Int = x4
+    val x5 = xs.to(List)
+    val y5: List[Char] = x5
+    val (xs6, xs7) = xs.partition(_ % 2 == 0)
+    val ys6: String = xs6
+    val ys7: String = xs7
+    val xs8 = xs.drop(2)
+    val ys8: String = xs8
+    val xs9 = xs.map(_ + 1)
+    val ys9: Seq[Int] = xs9
+    val xs9a = xs.map(_.toUpper)
+    val ys9a: String = xs9a
+    val xs10 = xs.flatMap((x: Char) => s"$x,$x")
+    val ys10: String = xs10
+    val xs11 = xs ++ xs
+    val ys11: String = xs11
+    val xs11a = xs ++ List('x', 'y') // Cons('x', Cons('y', Nil))
+    val ys11a: String = xs11a
+    val xs12 = xs ++ Nil
+    val ys12: String = xs12
+    val xs13 = Nil ++ xs.iterator
+    val ys13: List[Char] = xs13
+    val xs14 = xs ++ Cons("xyz", Nil)
+    val ys14: Seq[Any] = xs14
+    val xs15 = xs.zip(xs9)
+    val ys15: Seq[(Char, Int)] = xs15
+    println("-------")
+    println(x1)
+    println(x2)
+    println(x3)
+    println(x4)
+    println(x5)
+    println(xs6)
+    println(xs7)
+    println(xs8)
+    println(xs9)
+    println(xs9a)
+    println(xs10)
+    println(xs11)
+    println(xs11a)
+    println(xs12)
+    println(xs13)
+    println(xs14)
+    println(xs15)
+  }
+
+  def main(args: Array[String]) = {
+    val ints = Cons(1, Cons(2, Cons(3, Nil)))
+    val intsBuf = ints.to(ArrayBuffer)
+    val intsListBuf = ints.to(ListBuffer)
+    val intsView = ints.view
+    seqOps(ints)
+    seqOps(intsBuf)
+    seqOps(intsListBuf)
+    viewOps(intsView)
+    stringOps("abc")
+  }
+}
