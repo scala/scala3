6ee3f0269b2fe2120d396a4a19e4f31978ddc93a
Ondrej Lhotak <olhotak@uwaterloo.ca>
2022-03-07 15:50:04 +0100

    add eq/ne extension for AnyRef|Null to Scala3RunTime

--- library/src/scala/runtime/Scala3RunTime.scala
+++ library/src/scala/runtime/Scala3RunTime.scala
@@ -19,4 +19,10 @@ object Scala3RunTime:
     if (isNull) throw new NullPointerException("tried to cast away nullability, but value is null")
     else x.asInstanceOf[x.type & T]
 
+  extension (inline x: AnyRef | Null)
+    inline def eq(inline y: AnyRef | Null): Boolean =
+      x.asInstanceOf[AnyRef] eq y.asInstanceOf[AnyRef]
+    inline def ne(inline y: AnyRef | Null): Boolean =
+      !(x eq y)
+
 end Scala3RunTime
