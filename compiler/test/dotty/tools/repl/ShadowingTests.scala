36300dcf1176e22b14475aea82e9fcc931f4c787
odersky <odersky@gmail.com>
2022-03-21 21:51:59 +0100

    Fix ShadowingTest try catch indentation

--- compiler/test/dotty/tools/repl/ShadowingTests.scala
+++ compiler/test/dotty/tools/repl/ShadowingTests.scala
@@ -32,7 +32,8 @@ object ShadowingTests:
   def createSubDir(name: String): Path =
     val subdir = dir.resolve(name)
     try Files.createDirectory(subdir)
-    catch case _: java.nio.file.FileAlreadyExistsException =>
+    catch
+      case _: java.nio.file.FileAlreadyExistsException =>
     assert(Files.isDirectory(subdir), s"failed to create shadowed subdirectory $subdir")
     subdir
 
