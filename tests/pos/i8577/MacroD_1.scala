2c6e613650da47a6e3eb11e7b5c54d658ec7b885
gorilskij <43991400@users.noreply.github.com>
2022-05-04 13:00:10 +0200

    minor change

--- tests/pos/i8577/MacroD_1.scala
+++ tests/pos/i8577/MacroD_1.scala
@@ -7,6 +7,6 @@ object MacroD:
   def apply(ctx: scala.StringContext): StringContext = ctx
   def unapply(ctx: StringContext): Option[scala.StringContext] = Some(ctx)
 
-def implUnapplyD[T, U](sc: Expr[MacroD.StringContext], input: Expr[T])
+def implUnapplyD[T](sc: Expr[MacroD.StringContext], input: Expr[T])
                    (using Type[T])(using Quotes): Expr[Option[Seq[T]]] =
   '{ Some(Seq(${input})) }
