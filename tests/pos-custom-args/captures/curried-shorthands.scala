a3f8130bb0a70f480bacc67f160a0cea1af2909a
odersky <odersky@gmail.com>
2022-05-06 20:34:13 +0200

    Fix locility condition for checking inferred types
    Fixes #15116

--- tests/pos-custom-args/captures/curried-shorthands.scala
+++ tests/pos-custom-args/captures/curried-shorthands.scala
@@ -1,23 +1,24 @@
-def map2(xs: List[Int])(f: Int => Int): List[Int] = xs.map(f)
-val f1 = map2
-val fc1: List[Int] -> (Int => Int) -> List[Int] = f1
+object Test:
+  def map2(xs: List[Int])(f: Int => Int): List[Int] = xs.map(f)
+  val f1 = map2
+  val fc1: List[Int] -> (Int => Int) -> List[Int] = f1
 
-def map3(f: Int => Int)(xs: List[Int]): List[Int] = xs.map(f)
-val f2 = map3
-val fc2: (Int => Int) -> List[Int] -> List[Int] = f2
+  def map3(f: Int => Int)(xs: List[Int]): List[Int] = xs.map(f)
+  private val f2 = map3
+  val fc2: (Int => Int) -> List[Int] -> List[Int] = f2
 
-val f3 = (f: Int => Int) =>
-  println(f(3))
-  (xs: List[Int]) => xs.map(_ + 1)
-val f3c: (Int => Int) -> {} List[Int] -> List[Int] = f3
+  val f3 = (f: Int => Int) =>
+    println(f(3))
+    (xs: List[Int]) => xs.map(_ + 1)
+  val f3c: (Int => Int) -> {} List[Int] -> List[Int] = f3
 
-class LL[A]:
-  def drop(n: Int): {this} LL[A] = ???
+  class LL[A]:
+    def drop(n: Int): {this} LL[A] = ???
 
-def test(ct: CanThrow[Exception]) =
-  def xs: {ct} LL[Int] = ???
-  val ys = xs.drop(_)
-  val ysc: Int -> {ct} LL[Int] = ys
+  def test(ct: CanThrow[Exception]) =
+    def xs: {ct} LL[Int] = ???
+    val ys = xs.drop(_)
+    val ysc: Int -> {ct} LL[Int] = ys
 
 
 
