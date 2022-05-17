434f7a7a924bafda2ff03a859494694495d9a08a
Dale Wijnand <dale.wijnand@gmail.com>
2022-05-17 12:11:19 +0100

    Match the original apply method name in tuple specialisation
    ... not the new name from the import.

--- compiler/src/dotty/tools/dotc/transform/SpecializeTuples.scala
+++ compiler/src/dotty/tools/dotc/transform/SpecializeTuples.scala
@@ -23,10 +23,10 @@ class SpecializeTuples extends MiniPhase:
 
   override def transformApply(tree: Apply)(using Context): Tree = tree match
     case Apply(TypeApply(fun: NameTree, targs), args)
-        if fun.name == nme.apply && fun.symbol.exists && defn.isSpecializableTuple(fun.symbol.owner.companionClass, targs.map(_.tpe)) =>
+        if fun.symbol.name == nme.apply && fun.symbol.exists && defn.isSpecializableTuple(fun.symbol.owner.companionClass, targs.map(_.tpe)) =>
       cpy.Apply(tree)(Select(New(defn.SpecialisedTuple(fun.symbol.owner.companionClass, targs.map(_.tpe)).typeRef), nme.CONSTRUCTOR), args).withType(tree.tpe)
     case Apply(TypeApply(fun: NameTree, targs), args)
-        if fun.name == nme.CONSTRUCTOR && fun.symbol.exists && defn.isSpecializableTuple(fun.symbol.owner, targs.map(_.tpe)) =>
+        if fun.symbol.name == nme.CONSTRUCTOR && fun.symbol.exists && defn.isSpecializableTuple(fun.symbol.owner, targs.map(_.tpe)) =>
       cpy.Apply(tree)(Select(New(defn.SpecialisedTuple(fun.symbol.owner, targs.map(_.tpe)).typeRef), nme.CONSTRUCTOR), args).withType(tree.tpe)
     case _ => tree
   end transformApply
--- tests/run/i11114.scala
+++ tests/run/i11114.scala
@@ -2,3 +2,9 @@
   println((1, 1).getClass)
   println(Tuple2.apply(1, 1).getClass)
   println(new Tuple2(1, 1).getClass)
+
+  import Tuple2.apply
+  println(apply(1, 4).getClass)
+
+  import Tuple2.{ apply => t2 }
+  println(t2(1, 5).getClass)
