fce673100006b9b6b4a2e28022c198fc42a35543
Jentsch <d@jentsch.berlin>
2022-06-09 17:51:13 +0200

    Use playground.scala

--- compiler/test/dotty/tools/dotc/Playground.scala
+++ compiler/test/dotty/tools/dotc/Playground.scala
@@ -4,10 +4,24 @@ import dotty.tools.vulpix._
 import org.junit.Test
 import org.junit.Ignore
 
-@Ignore class Playground:
+class Playground:
   import TestConfiguration._
   import CompilationTests._
 
+  implicit val testGroup: TestGroup = TestGroup("playground")
+
   @Test def example: Unit =
-    implicit val testGroup: TestGroup = TestGroup("playground")
     compileFile("tests/playground/example.scala", defaultOptions).checkCompile()
+
+
+  @Test def pos: Unit =
+    compileFile("tests/pos/namedPatternMatching.scala", defaultOptions).checkCompile()
+
+  @Test def negativeTests: Unit =
+    compileFile("tests/neg/negNamedPatternMatching.scala", defaultOptions).checkExpectedErrors()
+    compileFile("tests/neg/bad-unapplies.scala", defaultOptions).checkExpectedErrors()
+    compileFile("tests/neg/i10757.scala", defaultOptions).checkExpectedErrors()
+
+
+  @Test def executionTest: Unit =
+    compileFile("tests/run/runNamedPatternMatching.scala", defaultOptions).checkRuns()
