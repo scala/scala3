3a8e66996913cc85012c46a15ea2be619513643d
odersky <odersky@gmail.com>
2022-06-20 09:42:30 +0200

    Fix comment

--- compiler/src/dotty/tools/dotc/core/Types.scala
+++ compiler/src/dotty/tools/dotc/core/Types.scala
@@ -1685,9 +1685,14 @@ object Types {
       case _ => resultType
     }
 
-    /** Determine the expected function type from the prototype. If multiple
-     *  function types are found in a union or intersection, their intersection
-     *  is returned. If no function type is found, Any is returned.
+    /** Determine the expected function type from the prototype.
+     *  If no function type is found, Any is returned. If multiple
+     *  function types are found in an intersection, their intersection
+     *  is returned. This works since `&` invokes `TypeComparer.distributeAnd`, which
+     *  ensures that `(A1 => B1) & (A2 => B2)` simplifies to `(A1 | A2) => (B1 & B2)`,
+     *  so the result is again a function type. An analogous distribution mechanism
+     *  does not exist for `|`. Therefore, a union of function types also yields `NoType`,
+     *  since we cannot determine a single expected function type.
      */
     def findFunctionType(using Context): Type = dealias match
       case tp: AndType =>
