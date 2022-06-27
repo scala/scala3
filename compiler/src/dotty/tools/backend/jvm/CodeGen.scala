fa9648259d29a3c8bfcf8f1123bfa19b31931a84
Wojciech Mazur <wojciech.mazur95@gmail.com>
2022-06-27 16:22:15 +0200

    Port GenBCode refactor Scala#6012

---
+++ compiler/src/dotty/tools/backend/jvm/CodeGen.scala
@@ -0,0 +1,181 @@
+package dotty.tools.backend.jvm
+
+import scala.language.unsafeNulls
+
+import dotty.tools.dotc.CompilationUnit
+import dotty.tools.dotc.ast.Trees.{PackageDef, ValDef}
+import dotty.tools.dotc.ast.tpd
+import dotty.tools.dotc.core.Phases.Phase
+
+import scala.collection.mutable
+import scala.jdk.CollectionConverters._
+import dotty.tools.dotc.transform.SymUtils._
+import dotty.tools.dotc.interfaces
+import dotty.tools.dotc.report
+
+import java.util.Optional
+import dotty.tools.dotc.sbt.ExtractDependencies
+import dotty.tools.dotc.core._
+import Contexts._
+import Phases._
+import Symbols._
+import StdNames.nme
+
+import java.io.DataOutputStream
+import java.nio.channels.ClosedByInterruptException
+
+import dotty.tools.tasty.{ TastyBuffer, TastyHeaderUnpickler }
+
+import scala.tools.asm
+import scala.tools.asm.tree._
+import tpd._
+import dotty.tools.io.AbstractFile
+import dotty.tools.dotc.util.NoSourcePosition
+
+
+class CodeGen(val int: DottyBackendInterface, val primitives: DottyPrimitives)( val bTypes: BTypesFromSymbols[int.type]) { self =>
+  import DottyBackendInterface.symExtensions
+  import bTypes._
+  import int.given
+
+  private lazy val mirrorCodeGen = Impl.JMirrorBuilder()
+
+  def genUnit(unit: CompilationUnit): GeneratedDefs = {
+    val generatedClasses = mutable.ListBuffer.empty[GeneratedClass]
+    val generatedTasty = mutable.ListBuffer.empty[GeneratedTasty]
+
+    def genClassDef(cd: TypeDef): Unit =
+      try
+        val sym = cd.symbol
+        val sourceFile = unit.source.file
+
+        def registerGeneratedClass(classNode: ClassNode, isArtifact: Boolean): Unit =
+          generatedClasses += GeneratedClass(classNode, sourceFile, isArtifact, onFileCreated(classNode, sym, unit.source))
+
+        val plainC = genClass(cd, unit)
+        registerGeneratedClass(plainC, isArtifact = false)
+
+        val attrNode =
+          if !sym.isTopLevelModuleClass then plainC
+          else if sym.companionClass == NoSymbol then
+            val mirrorC = genMirrorClass(sym, unit)
+            registerGeneratedClass(mirrorC, isArtifact = true)
+            mirrorC
+          else
+            report.log(s"No mirror class for module with linked class: ${sym.fullName}", NoSourcePosition)
+            plainC
+
+        if sym.isClass then
+          genTastyAndSetAttributes(sym, attrNode)
+      catch
+        case ex: Throwable =>
+          ex.printStackTrace()
+          report.error(s"Error while emitting ${unit.source}\n${ex.getMessage}", NoSourcePosition)
+
+
+    def genTastyAndSetAttributes(claszSymbol: Symbol, store: ClassNode): Unit =
+      import Impl.createJAttribute
+      for (binary <- unit.pickled.get(claszSymbol.asClass)) {
+        generatedTasty += GeneratedTasty(store, binary)
+        val tasty =
+          val uuid = new TastyHeaderUnpickler(binary()).readHeader()
+          val lo = uuid.getMostSignificantBits
+          val hi = uuid.getLeastSignificantBits
+
+          // TASTY attribute is created but only the UUID bytes are stored in it.
+          // A TASTY attribute has length 16 if and only if the .tasty file exists.
+          val buffer = new TastyBuffer(16)
+          buffer.writeUncompressedLong(lo)
+          buffer.writeUncompressedLong(hi)
+          buffer.bytes
+
+        val dataAttr = createJAttribute(nme.TASTYATTR.mangledString, tasty, 0, tasty.length)
+        store.visitAttribute(dataAttr)
+      }
+
+    def genClassDefs(tree: Tree): Unit =
+      tree match {
+        case EmptyTree => ()
+        case PackageDef(_, stats) => stats foreach genClassDefs
+        case ValDef(_, _, _) => () // module val not emitted
+        case td: TypeDef =>  genClassDef(td)
+      }
+
+    genClassDefs(unit.tpdTree)
+    GeneratedDefs(generatedClasses.toList, generatedTasty.toList)
+  }
+
+  // Creates a callback that will be evaluated in PostProcessor after creating a file
+  private def onFileCreated(cls: ClassNode, claszSymbol: Symbol, sourceFile: interfaces.SourceFile): AbstractFile => Unit = clsFile => {
+    val (fullClassName, isLocal) = atPhase(sbtExtractDependenciesPhase) {
+      (ExtractDependencies.classNameAsString(claszSymbol), claszSymbol.isLocal)
+    }
+
+    val className = cls.name.replace('/', '.')
+    if (ctx.compilerCallback != null)
+      ctx.compilerCallback.onClassGenerated(sourceFile, convertAbstractFile(clsFile), className)
+
+    if (ctx.sbtCallback != null) {
+      val jSourceFile = sourceFile.jfile.orElse(null)
+      val cb = ctx.sbtCallback
+      if (isLocal) cb.generatedLocalClass(jSourceFile, clsFile.file)
+      else cb.generatedNonLocalClass(jSourceFile, clsFile.file, className, fullClassName)
+    }
+  }
+
+  /** Convert a `dotty.tools.io.AbstractFile` into a
+   *  `dotty.tools.dotc.interfaces.AbstractFile`.
+   */
+  private def convertAbstractFile(absfile: dotty.tools.io.AbstractFile): interfaces.AbstractFile =
+    new interfaces.AbstractFile {
+      override def name = absfile.name
+      override def path = absfile.path
+      override def jfile = Optional.ofNullable(absfile.file)
+    }
+
+  private def genClass(cd: TypeDef, unit: CompilationUnit): ClassNode = {
+    val b = new Impl.PlainClassBuilder(unit)
+    b.genPlainClass(cd)
+    val cls = b.cnode
+    checkForCaseConflict(cls.name, cd.symbol)
+    cls
+  }
+
+  private def genMirrorClass(classSym: Symbol, unit: CompilationUnit): ClassNode = {
+    val cls = mirrorCodeGen.genMirrorClass(classSym, unit)
+    checkForCaseConflict(cls.name, classSym)
+    cls
+  }
+
+  private val lowerCaseNames = mutable.HashMap.empty[String, Symbol]
+  private def checkForCaseConflict(javaClassName: String, classSymbol: Symbol) = {
+    val lowerCaseName = javaClassName.toLowerCase
+    lowerCaseNames.get(lowerCaseName) match {
+      case None =>
+        lowerCaseNames.put(lowerCaseName, classSymbol)
+      case Some(dupClassSym) =>
+        // Order is not deterministic so we enforce lexicographic order between the duplicates for error-reporting
+        val (cl1, cl2) =
+          if (classSymbol.effectiveName.toString < dupClassSym.effectiveName.toString) (classSymbol, dupClassSym)
+          else (dupClassSym, classSymbol)
+        val same = classSymbol.effectiveName.toString == dupClassSym.effectiveName.toString
+        atPhase(typerPhase) {
+          if same then
+             // FIXME: This should really be an error, but then FromTasty tests fail
+            report.warning(s"${cl1.show} and ${cl2.showLocated} produce classes that overwrite one another", cl1.sourcePos)
+          else
+            report.warning(s"${cl1.show} differs only in case from ${cl2.showLocated}. " +
+              "Such classes will overwrite one another on case-insensitive filesystems.", cl1.sourcePos)
+        }
+    }
+  }
+
+  sealed transparent trait ImplEarlyInit{
+    val int: self.int.type = self.int
+    val bTypes: self.bTypes.type = self.bTypes
+    protected val primitives: DottyPrimitives = self.primitives
+  }
+  object Impl extends ImplEarlyInit with BCodeSyncAndTry {
+    class PlainClassBuilder(unit: CompilationUnit) extends SyncAndTryBuilder(unit)
+  }
+}
--- compiler/src/dotty/tools/backend/jvm/CoreBTypes.scala
+++ compiler/src/dotty/tools/backend/jvm/CoreBTypes.scala
@@ -7,38 +7,58 @@ import dotty.tools.dotc.core.Symbols._
 import dotty.tools.dotc.transform.Erasure
 import scala.tools.asm.{Handle, Opcodes}
 import dotty.tools.dotc.core.StdNames
+import BTypes.InternalName
 
-/**
- * Core BTypes and some other definitions. The initialization of these definitions requies access
- * to symbols / types (global).
- *
- * The symbols used to initialize the ClassBTypes may change from one compiler run to the next. To
- * make sure the definitions are consistent with the symbols in the current run, the
- * `intializeCoreBTypes` method in BTypesFromSymbols creates a new instance of CoreBTypes in each
- * compiler run.
- *
- * The class BTypesFromSymbols does not directly reference CoreBTypes, but CoreBTypesProxy. The
- * reason is that having a `var bTypes: CoreBTypes` would not allow `import bTypes._`. Instead, the
- * proxy class holds a `CoreBTypes` in a variable field and forwards to this instance.
- *
- * The definitions in `CoreBTypes` need to be lazy vals to break an initialization cycle. When
- * creating a new instance to assign to the proxy, the `classBTypeFromSymbol` invoked in the
- * constructor will actucally go through the proxy. The lazy vals make sure the instance is assigned
- * in the proxy before the fields are initialized.
- *
- * Note: if we did not re-create the core BTypes on each compiler run, BType.classBTypeFromInternalNameMap
- * could not be a perRunCache anymore: the classes defeined here need to be in that map, they are
- * added when the ClassBTypes are created. The per run cache removes them, so they would be missing
- * in the second run.
- */
-class CoreBTypes[BTFS <: BTypesFromSymbols[_ <: DottyBackendInterface]](val bTypes: BTFS) {
+abstract class CoreBTypes {
+  val bTypes: BTypes
+  import bTypes._
+
+   def primitiveTypeMap: Map[Symbol, PrimitiveBType]
+
+   def boxedClasses: Set[ClassBType]
+
+   def boxedClassOfPrimitive: Map[PrimitiveBType, ClassBType]
+
+   def boxResultType: Map[Symbol, ClassBType]
+
+   def unboxResultType: Map[Symbol, PrimitiveBType]
+
+   def srNothingRef : ClassBType
+   def srNullRef    : ClassBType
+
+   def ObjectRef                   : ClassBType
+   def StringRef                   : ClassBType
+   def jlStringBuilderRef          : ClassBType
+   def jlStringBufferRef           : ClassBType
+   def jlCharSequenceRef           : ClassBType
+   def jlClassRef                  : ClassBType
+   def jlThrowableRef              : ClassBType
+   def jlCloneableRef              : ClassBType
+   def jiSerializableRef           : ClassBType
+   def jlClassCastExceptionRef     : ClassBType
+   def jlIllegalArgExceptionRef    : ClassBType
+   def jliSerializedLambdaRef      : ClassBType
+
+   def srBoxesRuntimeRef: ClassBType
+
+   def jliLambdaMetaFactoryMetafactoryHandle    : Handle
+   def jliLambdaMetaFactoryAltMetafactoryHandle : Handle
+   def jliLambdaDeserializeBootstrapHandle      : Handle
+   def jliStringConcatFactoryMakeConcatWithConstantsHandle: Handle
+
+   def asmBoxTo  : Map[BType, MethodNameAndType]
+   def asmUnboxTo: Map[BType, MethodNameAndType]
+
+   def typeOfArrayOp: Map[Int, BType]
+}
+
+abstract class CoreBTypesFromSymbols[I <: DottyBackendInterface] extends CoreBTypes {
+  val bTypes: BTypesFromSymbols[I]
+
   import bTypes._
   import int.given
   import DottyBackendInterface._
-
-  //import global._
-  //import rootMirror.{requiredClass, getClassIfDefined}
-  //import definitions._
+  import dotty.tools.dotc.core.Contexts.Context
 
   /**
    * Maps primitive types to their corresponding PrimitiveBType. The map is defined lexically above
@@ -56,31 +76,21 @@ class CoreBTypes[BTFS <: BTypesFromSymbols[_ <: DottyBackendInterface]](val bTyp
     defn.DoubleClass  -> DOUBLE
   )
 
-  private lazy val BOXED_UNIT    : ClassBType = classBTypeFromSymbol(requiredClass[java.lang.Void])
-  private lazy val BOXED_BOOLEAN : ClassBType = classBTypeFromSymbol(requiredClass[java.lang.Boolean])
-  private lazy val BOXED_BYTE    : ClassBType = classBTypeFromSymbol(requiredClass[java.lang.Byte])
-  private lazy val BOXED_SHORT   : ClassBType = classBTypeFromSymbol(requiredClass[java.lang.Short])
-  private lazy val BOXED_CHAR    : ClassBType = classBTypeFromSymbol(requiredClass[java.lang.Character])
-  private lazy val BOXED_INT     : ClassBType = classBTypeFromSymbol(requiredClass[java.lang.Integer])
-  private lazy val BOXED_LONG    : ClassBType = classBTypeFromSymbol(requiredClass[java.lang.Long])
-  private lazy val BOXED_FLOAT   : ClassBType = classBTypeFromSymbol(requiredClass[java.lang.Float])
-  private lazy val BOXED_DOUBLE  : ClassBType = classBTypeFromSymbol(requiredClass[java.lang.Double])
-
   /**
    * Map from primitive types to their boxed class type. Useful when pushing class literals onto the
    * operand stack (ldc instruction taking a class literal), see genConstant.
    */
   lazy val boxedClassOfPrimitive: Map[PrimitiveBType, ClassBType] = Map(
-    UNIT   -> BOXED_UNIT,
-    BOOL   -> BOXED_BOOLEAN,
-    BYTE   -> BOXED_BYTE,
-    SHORT  -> BOXED_SHORT,
-    CHAR   -> BOXED_CHAR,
-    INT    -> BOXED_INT,
-    LONG   -> BOXED_LONG,
-    FLOAT  -> BOXED_FLOAT,
-    DOUBLE -> BOXED_DOUBLE
-  )
+      UNIT   -> classBTypeFromSymbol(requiredClass[java.lang.Void]),
+      BOOL   -> classBTypeFromSymbol(requiredClass[java.lang.Boolean]),
+      BYTE   -> classBTypeFromSymbol(requiredClass[java.lang.Byte]),
+      SHORT  -> classBTypeFromSymbol(requiredClass[java.lang.Short]),
+      CHAR   -> classBTypeFromSymbol(requiredClass[java.lang.Character]),
+      INT    -> classBTypeFromSymbol(requiredClass[java.lang.Integer]),
+      LONG   -> classBTypeFromSymbol(requiredClass[java.lang.Long]),
+      FLOAT  -> classBTypeFromSymbol(requiredClass[java.lang.Float]),
+      DOUBLE -> classBTypeFromSymbol(requiredClass[java.lang.Double])
+    )
 
   lazy val boxedClasses: Set[ClassBType] = boxedClassOfPrimitive.values.toSet
 
@@ -114,33 +124,35 @@ class CoreBTypes[BTFS <: BTypesFromSymbols[_ <: DottyBackendInterface]](val bTyp
    * names of NothingClass and NullClass can't be emitted as-is.
    * TODO @lry Once there's a 2.11.3 starr, use the commented argument list. The current starr crashes on the type literal `scala.runtime.Nothing$`
    */
-  lazy val srNothingRef : ClassBType = classBTypeFromSymbol(requiredClass("scala.runtime.Nothing$")) // (requiredClass[scala.runtime.Nothing$])
-  lazy val srNullRef    : ClassBType = classBTypeFromSymbol(requiredClass("scala.runtime.Null$"))    // (requiredClass[scala.runtime.Null$])
+  lazy val srNothingRef : ClassBType = classBTypeFromSymbol(requiredClass("scala.runtime.Nothing$"))
+  lazy val srNullRef    : ClassBType = classBTypeFromSymbol(requiredClass("scala.runtime.Null$"))
 
-  lazy val ObjectRef   : ClassBType = classBTypeFromSymbol(defn.ObjectClass)
-  lazy val StringRef                   : ClassBType = classBTypeFromSymbol(defn.StringClass)
-  lazy val jlStringBuilderRef          : ClassBType = classBTypeFromSymbol(requiredClass[java.lang.StringBuilder])
-  lazy val jlStringBufferRef           : ClassBType = classBTypeFromSymbol(requiredClass[java.lang.StringBuffer])
-  lazy val jlCharSequenceRef           : ClassBType = classBTypeFromSymbol(requiredClass[java.lang.CharSequence])
-  lazy val jlClassRef                  : ClassBType = classBTypeFromSymbol(requiredClass[java.lang.Class[_]])
-  lazy val jlThrowableRef              : ClassBType = classBTypeFromSymbol(defn.ThrowableClass)
-  lazy val jlCloneableRef              : ClassBType = classBTypeFromSymbol(defn.JavaCloneableClass)        // java/lang/Cloneable
-  lazy val jioSerializableRef          : ClassBType = classBTypeFromSymbol(requiredClass[java.io.Serializable])     // java/io/Serializable
-  lazy val jlClassCastExceptionRef     : ClassBType = classBTypeFromSymbol(requiredClass[java.lang.ClassCastException])   // java/lang/ClassCastException
-  lazy val jlIllegalArgExceptionRef    : ClassBType = classBTypeFromSymbol(requiredClass[java.lang.IllegalArgumentException])
-  lazy val jliSerializedLambdaRef      : ClassBType = classBTypeFromSymbol(requiredClass[java.lang.invoke.SerializedLambda])
-  
-  lazy val srBoxesRunTimeRef: ClassBType = classBTypeFromSymbol(requiredClass[scala.runtime.BoxesRunTime])
+  lazy val ObjectRef : ClassBType = classBTypeFromSymbol(defn.ObjectClass)
+  lazy val StringRef : ClassBType = classBTypeFromSymbol(defn.StringClass)
 
-  private lazy val jliCallSiteRef              : ClassBType = classBTypeFromSymbol(requiredClass[java.lang.invoke.CallSite])
-  private lazy val jliLambdaMetafactoryRef     : ClassBType = classBTypeFromSymbol(requiredClass[java.lang.invoke.LambdaMetafactory])
-  private lazy val jliMethodHandleRef          : ClassBType = classBTypeFromSymbol(defn.MethodHandleClass)
-  private lazy val jliMethodHandlesLookupRef   : ClassBType = classBTypeFromSymbol(defn.MethodHandlesLookupClass)
-  private lazy val jliMethodTypeRef            : ClassBType = classBTypeFromSymbol(requiredClass[java.lang.invoke.MethodType])
-  private lazy val jliStringConcatFactoryRef   : ClassBType = classBTypeFromSymbol(requiredClass("java.lang.invoke.StringConcatFactory")) // since JDK 9
-  private lazy val srLambdaDeserialize         : ClassBType = classBTypeFromSymbol(requiredClass[scala.runtime.LambdaDeserialize]) 
+  lazy val jlStringBuilderRef       : ClassBType = classBTypeFromSymbol(requiredClass[java.lang.StringBuilder])
+  lazy val jlStringBufferRef        : ClassBType = classBTypeFromSymbol(requiredClass[java.lang.StringBuffer])
+  lazy val jlCharSequenceRef        : ClassBType = classBTypeFromSymbol(requiredClass[java.lang.CharSequence])
+  lazy val jlClassRef               : ClassBType = classBTypeFromSymbol(requiredClass[java.lang.Class[_]])
+  lazy val jlThrowableRef           : ClassBType = classBTypeFromSymbol(defn.ThrowableClass)
+  lazy val jlCloneableRef           : ClassBType = classBTypeFromSymbol(defn.JavaCloneableClass)
+  lazy val jiSerializableRef        : ClassBType = classBTypeFromSymbol(requiredClass[java.io.Serializable])
+  lazy val jlClassCastExceptionRef  : ClassBType = classBTypeFromSymbol(requiredClass[java.lang.ClassCastException])
+  lazy val jlIllegalArgExceptionRef : ClassBType = classBTypeFromSymbol(requiredClass[java.lang.IllegalArgumentException])
+  lazy val jliSerializedLambdaRef   : ClassBType = classBTypeFromSymbol(requiredClass[java.lang.invoke.SerializedLambda])
 
-  lazy val jliLambdaMetaFactoryMetafactoryHandle: Handle = new Handle(
+  lazy val srBoxesRuntimeRef: ClassBType = classBTypeFromSymbol(requiredClass[scala.runtime.BoxesRunTime])
+
+  private lazy val jliCallSiteRef            : ClassBType = classBTypeFromSymbol(requiredClass[java.lang.invoke.CallSite])
+  private lazy val jliLambdaMetafactoryRef   : ClassBType = classBTypeFromSymbol(requiredClass[java.lang.invoke.LambdaMetafactory])
+  private lazy val jliMethodHandleRef        : ClassBType = classBTypeFromSymbol(defn.MethodHandleClass)
+  private lazy val jliMethodHandlesLookupRef : ClassBType = classBTypeFromSymbol(defn.MethodHandlesLookupClass)
+  private lazy val jliMethodTypeRef          : ClassBType = classBTypeFromSymbol(requiredClass[java.lang.invoke.MethodType])
+  private lazy val jliStringConcatFactoryRef : ClassBType = classBTypeFromSymbol(requiredClass("java.lang.invoke.StringConcatFactory")) // since JDK 9
+
+  lazy val srLambdaDeserialize               : ClassBType = classBTypeFromSymbol(requiredClass[scala.runtime.LambdaDeserialize])
+
+  lazy val jliLambdaMetaFactoryMetafactoryHandle = new Handle(
     Opcodes.H_INVOKESTATIC,
     jliLambdaMetafactoryRef.internalName,
     "metafactory",
@@ -150,7 +162,7 @@ class CoreBTypes[BTFS <: BTypesFromSymbols[_ <: DottyBackendInterface]](val bTyp
     ).descriptor,
     /* itf = */ false)
 
-  lazy val jliLambdaMetaFactoryAltMetafactoryHandle: Handle = new Handle(
+  lazy val jliLambdaMetaFactoryAltMetafactoryHandle = new Handle(
     Opcodes.H_INVOKESTATIC,
     jliLambdaMetafactoryRef.internalName,
     "altMetafactory",
@@ -159,7 +171,7 @@ class CoreBTypes[BTFS <: BTypesFromSymbols[_ <: DottyBackendInterface]](val bTyp
       jliCallSiteRef
     ).descriptor,
     /* itf = */ false)
-    
+
   lazy val jliLambdaDeserializeBootstrapHandle: Handle = new Handle(
     Opcodes.H_INVOKESTATIC,
     srLambdaDeserialize.internalName,
@@ -179,19 +191,19 @@ class CoreBTypes[BTFS <: BTypesFromSymbols[_ <: DottyBackendInterface]](val bTyp
       jliCallSiteRef
     ).descriptor,
     /* itf = */ false)
-  
+
   /**
    * Methods in scala.runtime.BoxesRuntime
    */
   lazy val asmBoxTo  : Map[BType, MethodNameAndType] = Map(
-    BOOL   -> MethodNameAndType("boxToBoolean",   MethodBType(List(BOOL),   BOXED_BOOLEAN)),
-    BYTE   -> MethodNameAndType("boxToByte",      MethodBType(List(BYTE),   BOXED_BYTE)),
-    CHAR   -> MethodNameAndType("boxToCharacter", MethodBType(List(CHAR),   BOXED_CHAR)),
-    SHORT  -> MethodNameAndType("boxToShort",     MethodBType(List(SHORT),  BOXED_SHORT)),
-    INT    -> MethodNameAndType("boxToInteger",   MethodBType(List(INT),    BOXED_INT)),
-    LONG   -> MethodNameAndType("boxToLong",      MethodBType(List(LONG),   BOXED_LONG)),
-    FLOAT  -> MethodNameAndType("boxToFloat",     MethodBType(List(FLOAT),  BOXED_FLOAT)),
-    DOUBLE -> MethodNameAndType("boxToDouble",    MethodBType(List(DOUBLE), BOXED_DOUBLE))
+    BOOL   -> MethodNameAndType("boxToBoolean",   MethodBType(List(BOOL),   boxedClassOfPrimitive(BOOL))),
+    BYTE   -> MethodNameAndType("boxToByte",      MethodBType(List(BYTE),   boxedClassOfPrimitive(BYTE))),
+    CHAR   -> MethodNameAndType("boxToCharacter", MethodBType(List(CHAR),   boxedClassOfPrimitive(CHAR))),
+    SHORT  -> MethodNameAndType("boxToShort",     MethodBType(List(SHORT),  boxedClassOfPrimitive(SHORT))),
+    INT    -> MethodNameAndType("boxToInteger",   MethodBType(List(INT),    boxedClassOfPrimitive(INT))),
+    LONG   -> MethodNameAndType("boxToLong",      MethodBType(List(LONG),   boxedClassOfPrimitive(LONG))),
+    FLOAT  -> MethodNameAndType("boxToFloat",     MethodBType(List(FLOAT),  boxedClassOfPrimitive(FLOAT))),
+    DOUBLE -> MethodNameAndType("boxToDouble",    MethodBType(List(DOUBLE), boxedClassOfPrimitive(DOUBLE)))
   )
 
   lazy val asmUnboxTo: Map[BType, MethodNameAndType] = Map(
@@ -219,76 +231,4 @@ class CoreBTypes[BTFS <: BTypesFromSymbols[_ <: DottyBackendInterface]](val bTyp
         (List(OARRAY_LENGTH, OARRAY_GET, OARRAY_SET) map (_ -> ObjectRef)) : _*
     )
   }
-}
-
-/**
- * This trait make some core BTypes availalbe that don't depend on a Global instance. Some core
- * BTypes are required to be accessible in the BTypes trait, which does not have access to Global.
- *
- * BTypes cannot refer to CoreBTypesProxy because some of its members depend on global, for example
- * the type Symbol in
- *   def primitiveTypeMap: Map[Symbol, PrimitiveBType]
- */
-trait CoreBTypesProxyGlobalIndependent[BTS <: BTypes] {
-  val bTypes: BTS
-  import bTypes._
-
-  def boxedClasses: Set[ClassBType]
-
-  def srNothingRef : ClassBType
-  def srNullRef    : ClassBType
-
-  def ObjectRef          : ClassBType
-  def jlCloneableRef     : ClassBType
-  def jiSerializableRef  : ClassBType
-}
-
-/**
- * See comment in class [[CoreBTypes]].
- */
-final class CoreBTypesProxy[BTFS <: BTypesFromSymbols[_ <: DottyBackendInterface]](val bTypes: BTFS) extends CoreBTypesProxyGlobalIndependent[BTFS] {
-  import bTypes._
-
-  private var _coreBTypes: CoreBTypes[bTypes.type] = _
-  def setBTypes(coreBTypes: CoreBTypes[BTFS]): Unit = {
-    _coreBTypes = coreBTypes.asInstanceOf[CoreBTypes[bTypes.type]]
-  }
-
-  def primitiveTypeMap: Map[Symbol, PrimitiveBType] = _coreBTypes.primitiveTypeMap
-
-  def boxedClasses: Set[ClassBType] = _coreBTypes.boxedClasses
-
-  def boxedClassOfPrimitive: Map[PrimitiveBType, ClassBType] = _coreBTypes.boxedClassOfPrimitive
-
-  def boxResultType: Map[Symbol, ClassBType] = _coreBTypes.boxResultType
-
-  def unboxResultType: Map[Symbol, PrimitiveBType] = _coreBTypes.unboxResultType
-
-  def srNothingRef : ClassBType = _coreBTypes.srNothingRef
-  def srNullRef    : ClassBType = _coreBTypes.srNullRef
-
-  def ObjectRef                   : ClassBType = _coreBTypes.ObjectRef
-  def StringRef                   : ClassBType = _coreBTypes.StringRef
-  def jlStringBuilderRef          : ClassBType = _coreBTypes.jlStringBuilderRef
-  def jlStringBufferRef           : ClassBType = _coreBTypes.jlStringBufferRef
-  def jlCharSequenceRef           : ClassBType = _coreBTypes.jlCharSequenceRef
-  def jlClassRef                  : ClassBType = _coreBTypes.jlClassRef
-  def jlThrowableRef              : ClassBType = _coreBTypes.jlThrowableRef
-  def jlCloneableRef              : ClassBType = _coreBTypes.jlCloneableRef
-  def jiSerializableRef           : ClassBType = _coreBTypes.jioSerializableRef
-  def jlClassCastExceptionRef     : ClassBType = _coreBTypes.jlClassCastExceptionRef
-  def jlIllegalArgExceptionRef    : ClassBType = _coreBTypes.jlIllegalArgExceptionRef
-  def jliSerializedLambdaRef      : ClassBType = _coreBTypes.jliSerializedLambdaRef
-
-  def srBoxesRuntimeRef: ClassBType = _coreBTypes.srBoxesRunTimeRef
-
-  def jliLambdaMetaFactoryMetafactoryHandle    : Handle = _coreBTypes.jliLambdaMetaFactoryMetafactoryHandle
-  def jliLambdaMetaFactoryAltMetafactoryHandle : Handle = _coreBTypes.jliLambdaMetaFactoryAltMetafactoryHandle
-  def jliLambdaDeserializeBootstrapHandle      : Handle = _coreBTypes.jliLambdaDeserializeBootstrapHandle
-  def jliStringConcatFactoryMakeConcatWithConstantsHandle: Handle = _coreBTypes.jliStringConcatFactoryMakeConcatWithConstantsHandle
-  
-  def asmBoxTo  : Map[BType, MethodNameAndType] = _coreBTypes.asmBoxTo
-  def asmUnboxTo: Map[BType, MethodNameAndType] = _coreBTypes.asmUnboxTo
-
-  def typeOfArrayOp: Map[Int, BType] = _coreBTypes.typeOfArrayOp
 }
