7c13eeee1da4917f580dbee4292701ebdef3c3e4
odersky <odersky@gmail.com>
2022-07-09 20:43:56 +0200

    Re-instantiate accidentally dropped comment

--- tests/semanticdb/expect/i5854.expect.scala
+++ tests/semanticdb/expect/i5854.expect.scala
@@ -1,6 +1,10 @@
 package i5854
 
 class B/*<-i5854::B#*/ {
+  // Known issue: Can't lookup the symbol of `b.A`
+  // we have to register the symbol of `b { type A }` to the refinementSymtab first
+  // then resolve, or assign same semanticdb symbol for both
+  // fake symbol for b.A, and real symbol of A in b
   val a/*<-i5854::B#a.*/: String/*->scala::Predef.String#*/ = (((1: Any/*->scala::Any#*/): b/*->i5854::B#b.*/.A): Nothing/*->scala::Nothing#*/): String/*->scala::Predef.String#*/
   val b/*<-i5854::B#b.*/: { type A/*<-local0*/ >: Any/*->scala::Any#*/ <: Nothing/*->scala::Nothing#*/ } = loop/*->i5854::B#loop().*/() // error
   def loop/*<-i5854::B#loop().*/(): Nothing/*->scala::Nothing#*/ = loop/*->i5854::B#loop().*/()
