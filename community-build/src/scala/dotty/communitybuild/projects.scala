cf7d6fc7f21c409e82b55c2085dc51f63fbb4355
Chris Kipp <ckipp@pm.me>
2022-07-24 13:32:40 +0200

    dep: sync with latest scala-java8-compat
    This also allows us to get rid of the fnGen stuff and simplify
    the sbtTestCommand and sbtPublishCommand

--- community-build/src/scala/dotty/communitybuild/projects.scala
+++ community-build/src/scala/dotty/communitybuild/projects.scala
@@ -506,10 +506,8 @@ object projects:
 
   lazy val scalaJava8Compat = SbtCommunityProject(
     project        = "scala-java8-compat",
-    // the fnGen subproject must be built with 2.12.x
-    sbtTestCommand = s"++2.12.x; ++$compilerVersion!; set fnGen/dependencyOverrides := Nil; test",
-    sbtPublishCommand = s"++2.12.x; ++$compilerVersion!; set fnGen/dependencyOverrides := Nil; publishLocal",
-    scalacOptions = Nil // avoid passing Scala 3 options to Scala 2.12 in fnGen subproject
+    sbtTestCommand = "test",
+    sbtPublishCommand = "publishLocal",
   )
 
   lazy val verify = SbtCommunityProject(
