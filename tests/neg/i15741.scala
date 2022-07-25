7996022a6b4b3ce6a7f4ca7f994a311dbc16405a
odersky <odersky@gmail.com>
2022-07-25 16:07:32 +0200

    Fix test

--- tests/neg/i15741.scala
+++ tests/neg/i15741.scala
@@ -1,14 +1,14 @@
   def get(using Int): String = summon[Int].toString
 
-  def pf2: PartialFunction[String, Int ?=> String] = { // error
-    case "hoge" => get
+  def pf2: PartialFunction[String, Int ?=> String] = {
+    case "hoge" => get // error
     case "huga" => get
   }
 
   type IS = Int ?=> String
 
-  def pf3: PartialFunction[String, IS] = { // error
-    case "hoge" => get
+  def pf3: PartialFunction[String, IS] = {
+    case "hoge" => get  // error
     case "huga" => get
   }
 
