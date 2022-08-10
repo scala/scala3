fed4d4842d189d296590a63ad10561b4ccd3cc44
Yichen Xu <yichen.x@outlook.com>
2022-08-10 15:25:08 +0800

    update pdgadt-wildcard test
    The limitation error in this test is eliminated.

--- tests/pos/pdgadt-wildcard.scala
+++ tests/pos/pdgadt-wildcard.scala
@@ -3,6 +3,6 @@ case class Inv[X](x: X) extends Expr { type T = X }
 case class Inv2[X](x: X) extends Expr { type T >: X }
 
 def eval(e: Expr): e.T = e match
-  case Inv(x) => x  // limitation // error
+  case Inv(x) => x
   case Inv2(x) => x
 
