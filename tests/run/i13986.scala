9c868c656c6b431b8d4ff49aa999b3ba806e9fbb
odersky <odersky@gmail.com>
2022-09-09 09:04:17 +0200

    Fix test

--- tests/run/i13986.scala
+++ tests/run/i13986.scala
@@ -1,15 +1,15 @@
-package example
-
 sealed trait Xa[T]
 sealed trait Mu[T] extends Xa[T]
 object Xa {
   implicit def convertMu[X[x] <: Xa[x], A, B](implicit t: X[A]): X[B] = t.asInstanceOf[X[B]]
 }
 object Mu {
-  implicit def mu: Mu[Int] = new Mu[Int] {}
+  implicit def mu: Mu[Int] = new Mu[Int] {
+    override def toString = "mu"
+  }
 }
 
 object Test extends App {
-  def constrain(a: Mu[Long]): Unit = ()
+  def constrain(a: Mu[Long]): Unit = println(a)
   constrain(Xa.convertMu)
 }
