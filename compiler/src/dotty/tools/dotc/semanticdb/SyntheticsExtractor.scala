49daf3ced9dd4f301e4321f25c5cf38fdf717b32
Rikito Taniguchi <rikiriki1238@gmail.com>
2022-09-13 18:10:59 +0900

    fix: remove duplicated type apply synthetics
    fix: https://github.com/scalameta/metals/issues/4372

--- compiler/src/dotty/tools/dotc/semanticdb/SyntheticsExtractor.scala
+++ compiler/src/dotty/tools/dotc/semanticdb/SyntheticsExtractor.scala
@@ -4,6 +4,7 @@ import dotty.tools.dotc.ast.tpd._
 import dotty.tools.dotc.core.Contexts._
 import dotty.tools.dotc.core.Flags._
 import dotty.tools.dotc.core.StdNames.nme
+import dotty.tools.dotc.core.NameKinds
 import dotty.tools.dotc.{semanticdb => s}
 
 
@@ -27,7 +28,20 @@ class SyntheticsExtractor:
         case tree: TypeApply
           if tree.span.isSynthetic &&
             tree.args.forall(arg => !arg.symbol.isDefinedInSource) &&
-            !tree.span.isZeroExtent =>
+            !tree.span.isZeroExtent &&
+            (tree.fun match {
+              // for `Bar[Int]` of `class Foo extends Bar[Int]`
+              // we'll have `TypeTree(Select(New(AppliedTypeTree(...))), List(Int))`
+              // in this case, don't register `*[Int]` to synthetics as we already have `[Int]` in source.
+              case Select(New(AppliedTypeTree(_, _)), _) => false
+
+              // for `new SomeJavaClass[Int]()`
+              // there will be a synthesized default getter
+              // in addition to the source derived one.
+              case Select(_, name) if name.is(NameKinds.DefaultGetterName) => false
+              case Select(fun, _) if fun.symbol.name.isDynamic => false
+              case _ => true
+            }) =>
           visited.add(tree)
           val fnTree = tree.fun match
             // Something like `List.apply[Int](1,2,3)`
