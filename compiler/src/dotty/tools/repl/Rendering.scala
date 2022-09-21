f57fb8e2945f3f07e10b3f46dd0ec940f6e9326d
Michael Pollmeier <michael@michaelpollmeier.com>
2022-09-21 14:08:16 +0200

    naming the `using` parameter isn't required here (PR comment)

--- compiler/src/dotty/tools/repl/Rendering.scala
+++ compiler/src/dotty/tools/repl/Rendering.scala
@@ -33,7 +33,7 @@ private[repl] class Rendering(parentClassLoader: Option[ClassLoader] = None):
   private var myReplStringOf: Object => String = _
 
   /** Class loader used to load compiled code */
-  private[repl] def classLoader()(using ctx: Context) =
+  private[repl] def classLoader()(using Context) =
     if (myClassLoader != null && myClassLoader.root == ctx.settings.outputDir.value) myClassLoader
     else {
       val parent = Option(myClassLoader).orElse(parentClassLoader).getOrElse {
