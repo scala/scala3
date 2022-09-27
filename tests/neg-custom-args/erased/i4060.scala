11ace0f494986d7cefc650407f305a37d10ff52f
Natsu Kagami <nki@nkagami.me>
2022-09-27 12:51:38 +0200

    Add tests for #4060

--- tests/neg-custom-args/erased/i4060.scala
+++ tests/neg-custom-args/erased/i4060.scala
@@ -1,17 +1,21 @@
 // See https://github.com/lampepfl/dotty/issues/4060#issuecomment-445808377
-import scala.language.experimental.erasedDefinitions
 
 object App {
   trait A { type L >: Any}
-  //def upcast(a: A, x: Any): a.L = x
   def upcast(erased a: A)(x: Any): a.L = x
-  //lazy val p: A { type L <: Nothing } = p
-  erased val p: A { type L <: Nothing } = p // error
+  erased val p: A { type L <: Nothing } = p
   def coerce(x: Any): Int = upcast(p)(x) // error
 
   def coerceInline(x: Any): Int = upcast(compiletime.erasedValue[A {type L <: Nothing}])(x) // error
 
+  trait B { type L <: Nothing }
+  def upcast_dep_parameter(erased a: B)(x: a.L) : Int = x
+  erased val q : B { type L >: Any } = compiletime.erasedValue
+
+  def coerceInlineWithB(x: Any): Int = upcast_dep_parameter(q)(x) // error
+
   def main(args: Array[String]): Unit = {
     println(coerce("Uh oh!"))
+    println(coerceInlineWithB("Uh oh!"))
   }
 }
