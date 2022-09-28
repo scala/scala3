0c966721a331896a0d2e839a9648ad09e93cf5e6
Yichen Xu <yichen.x@outlook.com>
2022-09-28 21:55:39 +0200

    always box args of HKTypeLambda

--- compiler/src/dotty/tools/dotc/core/TypeApplications.scala
+++ compiler/src/dotty/tools/dotc/core/TypeApplications.scala
@@ -11,7 +11,7 @@ import util.Stats._
 import Names._
 import Flags.{Module, Provisional}
 import dotty.tools.dotc.config.Config
-import cc.boxedUnlessFun
+import cc.{boxedUnlessFun, boxed}
 
 object TypeApplications {
 
@@ -346,7 +346,7 @@ class TypeApplications(val self: Type) extends AnyVal {
             }
             if ((dealiased eq stripped) || followAlias)
               try
-                val instantiated = dealiased.instantiate(args.mapConserve(_.boxedUnlessFun(self)))
+                val instantiated = dealiased.instantiate(args.mapConserve(_.boxed))
                 if (followAlias) instantiated.normalized else instantiated
               catch
                 case ex: IndexOutOfBoundsException =>
