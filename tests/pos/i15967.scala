0d7978043175103c91b151a97442b799dedd8791
Dale Wijnand <dale.wijnand@gmail.com>
2022-10-11 10:48:00 +0100

    Fix exhaustivity warning

---
+++ tests/pos/i15967.scala
@@ -0,0 +1,10 @@
+// scalac: -Werror
+sealed trait A[-Z]
+final case class B[Y]() extends A[Y]
+
+class Test:
+  def t1[X](a: A[X]) = a match // was inexhaustive
+    case _: B[X] @unchecked =>
+
+//def t2[X](a: A[X]) = a match // was inexhaustive
+//  case _: B[X] => // expected unchecked warning
