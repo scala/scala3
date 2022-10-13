e46ed94b2f26b1e76f0c89877ea4a3a61476fcbc
Dale Wijnand <dale.wijnand@gmail.com>
2022-10-13 15:49:27 +0100

    Fix redundancy (unreachability) warning

---
+++ tests/pos/i16123.scala
@@ -0,0 +1,12 @@
+// scalac: -Werror
+sealed trait Nat
+case class Zero() extends Nat
+case class Succ[N <: Nat](n: N) extends Nat
+
+class Test:
+  def foo(n: Nat): Int = n match
+    case Zero() => 0
+    case Succ(Zero()) => 1
+    case _ => 2 // was: warning for this line
+
+  def test = foo(Succ(Succ(Zero()))) // evaluates to 2
