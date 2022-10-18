760285ddd3214cf405247a907c8f4dc98adfe03e
Kamil Szewczyk <kamilszewczyk0@gmail.com>
2022-10-18 23:10:23 +0200

    fix clearing filters and filter search optimalization

--- scaladoc/resources/dotty_res/scripts/components/FilterBar.js
+++ scaladoc/resources/dotty_res/scripts/components/FilterBar.js
@@ -3,92 +3,101 @@
  */
 
  class FilterBar extends Component {
-  constructor(props) {
-    super(props);
+   constructor(props) {
+     super(props);
 
-    this.refs = {
-      elements: findRefs(".documentableElement"),
-      filterBar: findRef(".documentableFilter"),
-    };
+     this.refs = {
+       elements: findRefs(".documentableElement"),
+       filterBar: findRef(".documentableFilter"),
+     };
 
-    this.state = {
-      filter: new Filter("", {}, this.refs.elements, true),
-      isVisible: false,
-      selectedPill: '',
-    };
+     this.state = {
+       filter: new Filter("", {}, this.refs.elements, true),
+       isVisible: false,
+       selectedPill: "",
+     };
 
-    this.inputComp = new Input({ onInputChange: this.onInputChange });
-    this.listComp = new DocumentableList({
-      filter: this.state.filter,
-    });
-    this.filterGroupComp = new FilterGroup({
-      filter: this.state.filter,
-      onFilterToggle: this.onFilterToggle,
-      onGroupSelectChange: this.onGroupSelectChange,
-      onFilterVisibilityChange: this.onFilterVisibilityChange,
-      onPillClick: this.onPillClick,
-      onPillCollapse: this.onPillCollapse,
-    });
+     this.inputComp = new Input({ onInputChange: this.onInputChange });
+     this.listComp = new DocumentableList({
+       filter: this.state.filter,
+     });
+     this.filterGroupComp = new FilterGroup({
+       filter: this.state.filter,
+       onFilterToggle: this.onFilterToggle,
+       onGroupSelectChange: this.onGroupSelectChange,
+       onFilterVisibilityChange: this.onFilterVisibilityChange,
+       onPillClick: this.onPillClick,
+       onPillCollapse: this.onPillCollapse,
+     });
 
-    this.render();
-  }
+     this.render();
+   }
 
-  onInputChange = (value) => {
-    this.setState((prevState) => ({
-      filter: prevState.filter.onInputValueChange(value),
-    }));
-  };
+   onInputChange = (value) => {
+     this.setState((prevState) => ({
+       filter: prevState.filter.onInputValueChange(value),
+     }));
+   };
 
-  onGroupSelectChange = (key, isActive) => {
-    this.setState((prevState) => ({
-      filter: prevState.filter.onGroupSelectionChange(key, isActive),
-    }));
-  };
+   onGroupSelectChange = (key, isActive) => {
+     this.setState((prevState) => ({
+       filter: prevState.filter.onGroupSelectionChange(key, isActive),
+     }));
+   };
 
-  onClearFilters = () => {
-    this.setState(() => ({
-      filter: ""
-    }))
-  }
+   onClearFilters = () => {
+     this.inputComp.inputRef.value = "";
+     this.setState((prevState) => ({
+       filter: prevState.filter.onInputValueChange(""),
+     }));
+   };
 
-  onFilterVisibilityChange = () => {
-    this.setState((prevState) => ({ isVisible: !prevState.isVisible }));
-  };
+   onFilterVisibilityChange = () => {
+     this.setState((prevState) => ({ isVisible: !prevState.isVisible }));
+   };
 
-  onFilterToggle = (key, value) => {
-    this.setState((prevState) => ({
-      filter: prevState.filter.onFilterToggle(key, value),
-    }));
-  };
+   onFilterToggle = (key, value) => {
+     this.setState((prevState) => ({
+       filter: prevState.filter.onFilterToggle(key, value),
+     }));
+   };
 
-  onPillClick = (key) => {
-    this.setState((prevState) => ({
-      filter: prevState.filter,
-      selectedPill: key
-    }))
-  }
+   onPillClick = (key) => {
+     this.setState((prevState) => ({
+       filter: prevState.filter,
+       selectedPill: key,
+     }));
+   };
 
-  onPillCollapse = () => {
-    this.setState((prevState) => ({
-      filter: prevState.filter,
-      selectedPill: ""
-    }))
-  }
+   onPillCollapse = () => {
+     this.setState((prevState) => ({
+       filter: prevState.filter,
+       selectedPill: "",
+     }));
+   };
 
-  render() {
-    if (this.refs.filterBar) {
-      if (this.state.isVisible) {
-        this.refs.filterBar.classList.add("active");
-      } else {
-        this.refs.filterBar.classList.remove("active");
-      }
-    }
+   render() {
+     if (this.refs.filterBar) {
+       if (this.state.isVisible) {
+         this.refs.filterBar.classList.add("active");
+       } else {
+         this.refs.filterBar.classList.remove("active");
+       }
+     }
 
-    this.listComp.render({ filter: this.state.filter });
-    this.filterGroupComp.render({ filter: this.state.filter, selectedPill: this.state.selectedPill });
-  }
-}
+     this.listComp.render({ filter: this.state.filter });
+     this.filterGroupComp.render({
+       filter: this.state.filter,
+       selectedPill: this.state.selectedPill,
+     });
+   }
+ }
 
-window.addEventListener("dynamicPageLoad", () => {
-  new FilterBar()
-})
+ window.addEventListener("dynamicPageLoad", () => {
+   new FilterBar();
+ });
+
+ const clearButton = document.querySelector(".clearButton");
+ clearButton.addEventListener("click", () => {
+   new FilterBar().onClearFilters();
+ });
