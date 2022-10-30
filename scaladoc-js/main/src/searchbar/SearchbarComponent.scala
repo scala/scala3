c181b417ce85631c9142ac8fc9d92d9a3a6b22aa
Kamil Szewczyk <kamilszewczyk0@gmail.com>
2022-10-30 14:07:08 +0100

    fixed paddings for no result page in searchbar

--- scaladoc-js/main/src/searchbar/SearchbarComponent.scala
+++ scaladoc-js/main/src/searchbar/SearchbarComponent.scala
@@ -104,9 +104,9 @@ class SearchbarComponent(engine: PageSearchEngine, inkuireEngine: InkuireJSSearc
       if result.isEmpty then
         val noResultsDiv = div(id := "no-results-container")(
           img(src := "./images/no-results.svg", alt := "Sick face"),
-          h2(cls := "h200 no-result-header")("No results match your filter"),
-          p("Try adjusting or clearing your filters", p("to display better result")),
-          button(cls := "clearButton label-only-button")("Clear all filters")
+          h2(cls := "h200 no-result-header")("No results match your filter criteria."),
+          p(cls := "body-small no-result-content")("Try adjusting or clearing your filters", p("to display better result")),
+          button(id := "searchbar-clear-button", cls := "clearButton label-only-button")("Clear all filters")
         )
         resultsDiv.scrollTop = 0
         resultsDiv.appendChild(noResultsDiv)
