73d807a08d0b09eda63c6d150ea628079d06e14e
Szymon Rodziewicz <szymonrodant@gmail.com>
2022-11-15 13:39:53 +0100

    Make lazy vals run on non-fallback graal image - remove dynamic reflection

--- library/src/scala/runtime/LazyVals.scala
+++ library/src/scala/runtime/LazyVals.scala
@@ -9,19 +9,15 @@ import scala.annotation.*
  */
 object LazyVals {
   @nowarn
-  private[this] val unsafe: sun.misc.Unsafe =
-      classOf[sun.misc.Unsafe].getDeclaredFields.nn.find { field =>
-        field.nn.getType == classOf[sun.misc.Unsafe] && {
-          field.nn.setAccessible(true)
-          true
-        }
-      }
-      .map(_.nn.get(null).asInstanceOf[sun.misc.Unsafe])
-      .getOrElse {
-        throw new ExceptionInInitializerError {
-          new IllegalStateException("Can't find instance of sun.misc.Unsafe")
-        }
-      }
+  private[this] val unsafe: sun.misc.Unsafe = {
+    val unsafeField = classOf[sun.misc.Unsafe].getDeclaredField("theUnsafe").nn
+    if unsafeField.getType == classOf[sun.misc.Unsafe] then
+      unsafeField.setAccessible(true)
+    else throw new ExceptionInInitializerError {
+      new IllegalStateException("Can't find instance of sun.misc.Unsafe")
+    }
+    unsafeField.get(null).asInstanceOf[sun.misc.Unsafe]
+  }
 
   private[this] val base: Int = {
     val processors = java.lang.Runtime.getRuntime.nn.availableProcessors()
