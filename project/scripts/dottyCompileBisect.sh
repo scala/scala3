d2f4c12e219635281804310190034bd132b6af7a
Michał Pałka <prolativus@gmail.com>
2022-11-17 16:09:55 +0100

    Improve bisect script:   * Support checking runtime success while bisecting   * Properly report cases when no release was bad   * Get correct results when running the script directly from the repository root

--- project/scripts/dottyCompileBisect.sh
+++ project/scripts/dottyCompileBisect.sh
@@ -2,15 +2,23 @@
 # > git bisect start
 # > git bisect bad <bad-commit>
 # > git bisect good <good-commit>
-# > git bisect run project/scripts/dottyCompileBisect.sh <file.scala>
+# > git bisect run project/scripts/dottyCompileBisect.sh [--run <main.class.name>] [<compiler-option> ...] <file1.scala> [<fileN.scala> ...]
 #
 # Note: Use dottyCompileBisect.scala for faster bisection over commits that spans several days
 
-files=$@
-shift
+if [ "$1" == "--run" ]; then
+  mainClass="$2"
+  shift; shift
+fi
+
+compilerArgs=$@
 
 rm -r out
 mkdir out
 mkdir out/bisect
 
-sbt "clean; scalac -d out/bisect $files"
+if [ -n "$mainClass" ]; then
+  sbtRunCommand="scala -classpath out/bisect $mainClass"
+fi
+
+sbt "clean; scalac -d out/bisect $compilerArgs; $sbtRunCommand"
