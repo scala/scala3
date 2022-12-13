d1f944166d387db5922d79189e7e9fe02cd57337
Paul Coral <paul.coral@epfl.ch>
2022-12-13 18:30:17 +0100

    Fix test i15503j

--- tests/neg-custom-args/fatal-warnings/i15503j.scala
+++ tests/neg-custom-args/fatal-warnings/i15503j.scala
@@ -1,6 +1,6 @@
 // scalac: -Wunused:strict-no-implicit-warn
 
-package unused.test:
+package foo.unused.strict.test:
   package a:
     given x: Int = 0
     implicit val y: Int = 1
@@ -16,10 +16,44 @@ package unused.test:
     import a.f // error
   package c:
     import a.given // OK
-    import a._ // OK
-    import a.* // OK
     import a.x // OK
     import a.y // OK
     import a.z // OK
     import a.f // OK
     def g = f + z + y + x
+
+package foo.implicits.resolution:
+  class X
+  class Y extends X
+  object A { implicit val x: X = new X }
+  object B { implicit val y: Y = new Y }
+  class C {
+    import A._ // OK
+    import B._ // OK
+    def t = implicitly[X]
+  }
+
+package foo.unused.summon.inlines:
+  package lib:
+    trait A
+    trait B
+    trait C
+    trait X
+
+    given willBeUnused: (A & X) = new A with X {}
+    given willBeUsed: (A & B) = new A with B {}
+
+  package use:
+    import lib.{A, B, C, willBeUnused, willBeUsed} //OK
+    import compiletime.summonInline //OK
+
+    transparent inline given conflictInside: C =
+      summonInline[A]
+      new {}
+
+    transparent inline given potentialConflict: C =
+      summonInline[B]
+      new {}
+
+    val b: B = summon[B]
+    val c: C = summon[C]
