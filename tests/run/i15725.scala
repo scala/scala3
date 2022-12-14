ac100532b2fb3cff12d12dd33148a025d9673bea
odersky <odersky@gmail.com>
2022-12-14 19:26:47 +0100

    Make Test ScalaJS friendly
    Doubles print differently on ScalaJS, use Ints instead.

--- tests/run/i15725.scala
+++ tests/run/i15725.scala
@@ -1,8 +1,8 @@
-class ToString(d:Double) extends AnyVal :
+class ToString(d: Int) extends AnyVal :
   inline override def toString():String = s"$d meters"
 
 @main def Test =
-  val ts = ToString(2.0)
+  val ts = ToString(2)
   val a: Any = ts
   println(a)
 
