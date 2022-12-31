9f455a670ea2374d966be997923e5eeb4abc1d89
odersky <odersky@gmail.com>
2022-12-31 19:45:50 +0100

    Add validation to the strawman

--- tests/run/errorhandling/Result.scala
+++ tests/run/errorhandling/Result.scala
@@ -28,6 +28,24 @@ object Result:
       case Ok(x) => f(x)
       case err: Err[_] => err
 
+    /** Validate both `r` and `other`; return a pair of successes or a list of failures. */
+    def * [U](other: Result[U, E]): Result[(T, U), List[E]] = (r, other) match
+      case (Ok(x), Ok(y))     => Ok((x, y))
+      case (Ok(_), Err(e))    => Err(e :: Nil)
+      case (Err(e), Ok(_))    => Err(e :: Nil)
+      case (Err(e1), Err(e2)) => Err(e1 :: e2 :: Nil)
+
+    /** Validate both `r` and `other`; return a tuple of successes or a list of failures.
+     *  Unlike with `*`, the right hand side `other` must be a `Result` returning a `Tuple`,
+     *  and the left hand side is added to it. See `Result.empty` for a convenient
+     *  right unit of chains of `*:`s.
+     */
+    def *: [U <: Tuple](other: Result[U, List[E]]): Result[T *: U, List[E]] = (r, other) match
+      case (Ok(x), Ok(ys))     => Ok(x *: ys)
+      case (Ok(_), es: Err[?]) => es
+      case (Err(e), Ok(_))     => Err(e :: Nil)
+      case (Err(e), Err(es))   => Err(e :: es)
+
   /** Simlar to `Try`: Convert exceptions raised by `body` to `Err`s.
    *  In principle, `Try[T]` should be equivalent to `Result[T, Exception]`.
    *  Note that we do not want to catch and reify all Throwables.
@@ -37,9 +55,12 @@ object Result:
    *  (Generally, the focus on `Throwable` in Scala libraries is a mistake.
    *   Use `Exception` instead, as it was meant to in Java.)
    */
-  def apply[T](body: => T): Result[T, Exception] =
+  inline def apply[T](body: => T): Result[T, Exception] =
     try Ok(body)
     catch case ex: Exception => Err(ex)
+
+  /** Right unit for chains of `*:`s. Returns an `Ok` with an `EmotyTuple` value. */
+  def empty: Result[EmptyTuple, Nothing] = Ok(EmptyTuple)
 end Result
 
 /** A prompt for `_.?`. It establishes a boundary to which `_.?` returns */
