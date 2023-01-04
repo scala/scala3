db484bbfb01021f925fab2c6c3c7173179321f78
Hamza REMMAL <56235032+hamzaremmal@users.noreply.github.com>
2023-01-05 00:03:58 +0100

    Change implicitNotFound message for the CanThrow class to drop the contextual parameter list

--- library/src/scala/CanThrow.scala
+++ library/src/scala/CanThrow.scala
@@ -2,12 +2,13 @@ package scala
 import language.experimental.erasedDefinitions
 import annotation.{implicitNotFound, experimental, capability}
 
-/** A capability class that allows to throw exception `E`. When used with the
+/**
+ * A capability class that allows to throw exception `E`. When used with the
  *  experimental.saferExceptions feature, a `throw Ex()` expression will require
  *  a given of class `CanThrow[Ex]` to be available.
  */
 @experimental @capability
-@implicitNotFound("The capability to throw exception ${E} is missing.\nThe capability can be provided by one of the following:\n - Adding a using clause `(using CanThrow[${E}])` to the definition of the enclosing method\n - Adding `throws ${E}` clause after the result type of the enclosing method\n - Wrapping this piece of code with a `try` block that catches ${E}")
+@implicitNotFound("The capability to throw exception of type ${E} is missing.\nThe capability can be provided by one of the following:\n - Adding `throws ${E}` clause after the parameter list of the enclosing method\n - Adding `throws ${E}` clause after the result type of the enclosing method\n - Wrapping this piece of code with a `try` block that catches ${E}")
 erased class CanThrow[-E <: Exception]
 
 @experimental
