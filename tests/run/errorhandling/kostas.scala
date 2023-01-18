a8f81ddf3c9e69b46c1bcb01ef4d0b8433869a83
odersky <odersky@gmail.com>
2023-01-18 14:53:26 +0100

    Fix test

--- tests/run/errorhandling/kostas.scala
+++ tests/run/errorhandling/kostas.scala
@@ -1,5 +1,5 @@
 package optionMockup:
-  import scala.util.boundary
+  import scala.util.{boundary, break}
   object optional:
     transparent inline def apply[T](inline body: boundary.Label[None.type] ?=> T): Option[T] =
       boundary(Some(body))
@@ -7,7 +7,7 @@ package optionMockup:
   extension [T](r: Option[T])
     transparent inline def ? (using label: boundary.Label[None.type]): T = r match
       case Some(x) => x
-      case None => label.break(None)
+      case None => break(None)
 
 import optionMockup.*
 
