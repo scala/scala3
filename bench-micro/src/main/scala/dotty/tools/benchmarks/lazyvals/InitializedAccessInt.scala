fad1584d1b4c518c5637eca516b4f67c9461a642
Szymon Rodziewicz <szymonrodant@gmail.com>
2023-02-01 17:39:54 +0100

    Fix static lazy field holder for GraalVM

---
+++ bench-micro/src/main/scala/dotty/tools/benchmarks/lazyvals/InitializedAccessInt.scala
@@ -0,0 +1,30 @@
+package dotty.tools.benchmarks.lazyvals
+
+import org.openjdk.jmh.annotations.*
+import org.openjdk.jmh.infra.Blackhole
+import LazyVals.LazyIntHolder
+import java.util.concurrent.TimeUnit
+
+@BenchmarkMode(Array(Mode.AverageTime))
+@Fork(2)
+@Threads(1)
+@Warmup(iterations = 5)
+@Measurement(iterations = 5)
+@OutputTimeUnit(TimeUnit.NANOSECONDS)
+@State(Scope.Benchmark)
+class InitializedAccessInt {
+
+  var holder: LazyIntHolder = _
+
+  @Setup
+  def prepare: Unit = {
+    holder = new LazyIntHolder
+    holder.value
+  }
+
+  @Benchmark
+  def measureInitialized(bh: Blackhole) = {
+    bh.consume(holder)
+    bh.consume(holder.value)
+  }
+}
