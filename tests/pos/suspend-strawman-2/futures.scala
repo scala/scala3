c5aac2ad983215f46fe978170c1d82e9580755ce
odersky <odersky@gmail.com>
2023-02-04 18:50:30 +0100

    Add exception handling
    Collect thrown exceptions in a Try and rethrow on await

--- tests/pos/suspend-strawman-2/futures.scala
+++ tests/pos/suspend-strawman-2/futures.scala
@@ -2,38 +2,59 @@ package futures
 
 import scala.collection.mutable.ListBuffer
 import scala.util.boundary, boundary.Label
+import scala.compiletime.uninitialized
+import scala.util.{Try, Success, Failure}
 import runtime.*
 
 trait Async:
-  def await[T](f: Future[T]): T
+
+  /** Wait for completion of future `f`.
+   */
+  def await[T](f: Future[T]): Try[T]
+
+end Async
 
 class Future[+T](body: Async ?=> T):
-  private var result: Option[T] = None
-  private var waiting: ListBuffer[T => Unit] = ListBuffer()
-  private def addWaiting(k: T => Unit): Unit = waiting += k
+  import Future.Status, Status.*
 
-  def await(using a: Async): T = a.await(this)
+  private var status: Status = Started
+  private var result: Try[T] = uninitialized
+  private var waiting: ListBuffer[Try[T] => Unit] = ListBuffer()
+
+  private def addWaiting(k: Try[T] => Unit): Unit =
+    waiting += k
+
+  /** Wait for this future to be completed, return its value in case of success,
+   *  or rethrow exception in case of failure.
+   */
+  def await(using a: Async): T = a.await(this).get
 
   // a handler for Async
   private def async(body: Async ?=> Unit): Unit =
     boundary [Unit]:
       given Async with
-        def await[T](f: Future[T]): T = f.result match
-          case Some(x) => x
-          case None => suspend[T, Unit](s => f.addWaiting(s.resume))
+        def await[T](f: Future[T]): Try[T] = f.status match
+          case Started   =>
+            suspend[Try[T], Unit](s => f.addWaiting(s.resume))
+          case Completed =>
+            f.result
       body
 
   private def complete(): Unit =
     async:
-      val value = body
-      val result = Some(value)
+      val result =
+        try Success(body)
+        catch case ex: Exception => Failure(ex)
+      status = Completed
       for k <- waiting do
-        Scheduler.schedule(() => k(value))
+        Scheduler.schedule(() => k(result))
       waiting.clear()
-
+      
   Scheduler.schedule(() => complete())
 
-object Future
+object Future:
+  enum Status:
+    case Started, Completed
 end Future
 
 def Test(x: Future[Int], xs: List[Future[Int]]) =
