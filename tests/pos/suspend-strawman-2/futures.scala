7969a3c82162d781f68dececc000821837096b7a
odersky <odersky@gmail.com>
2023-02-05 12:03:37 +0100

    Add simple cancellation

--- tests/pos/suspend-strawman-2/futures.scala
+++ tests/pos/suspend-strawman-2/futures.scala
@@ -17,7 +17,7 @@ trait Async:
 end Async
 
 class Future[+T](body: Async ?=> T):
-  import Future.Status, Status.*
+  import Future.{Status, Cancellation}, Status.*
 
   @volatile private var status: Status = Initial
   private var result: Try[T] = uninitialized
@@ -28,6 +28,14 @@ class Future[+T](body: Async ?=> T):
     if status == Completed then k(result)
     else waiting += k
 
+  private def currentWaiting(): List[Try[T] => Unit] = synchronized:
+    val ws = waiting.toList
+    waiting.clear()
+    ws
+
+  private def checkCancellation(): Unit =
+    if status == Cancelled then throw Cancellation()
+
   /** Wait for this future to be completed, return its value in case of success,
    *  or rethrow exception in case of failure.
    */
@@ -37,18 +45,33 @@ class Future[+T](body: Async ?=> T):
   private def async(body: Async ?=> Unit): Unit =
     boundary [Unit]:
       given Async with
-        def await[T](f: Future[T]): Try[T] = f.status match
-          case Initial   =>
-            f.start()(using scheduler)
-            await(f)
-          case Started   =>
-            suspend[Try[T], Unit]: s =>
-              f.addWaiting: result =>
-                scheduler.schedule: () =>
-                  s.resume(result)
+
+        private def resultOption[T](f: Future[T]): Option[Try[T]] = f.status match
+          case Initial =>
+            f.ensureStarted()(using scheduler)
+            resultOption(f)
+          case Started =>
+            None
           case Completed =>
-            f.result
+            Some(f.result)
+          case Cancelled =>
+            Some(Failure(Cancellation()))
+
+        private inline def cancelChecked[T](op: => T): T =
+          checkCancellation()
+          val res = op
+          checkCancellation()
+          res
+
+        def await[T](f: Future[T]): Try[T] =
+          cancelChecked:
+            resultOption(f).getOrElse:
+              suspend[Try[T], Unit]: s =>
+                f.addWaiting: result =>
+                  scheduler.schedule: () =>
+                    s.resume(result)
       body
+  end async
 
   private def complete(): Unit =
     async:
@@ -56,22 +79,41 @@ class Future[+T](body: Async ?=> T):
         try Success(body)
         catch case ex: Exception => Failure(ex)
       status = Completed
-      waiting.foreach(_(result))
-      waiting.clear()
+      for task <- currentWaiting() do task(result)
 
-  /** Start future's execution */
-  def start()(using scheduler: Scheduler): this.type =
-    this.scheduler = scheduler
+  /** Ensure future's execution has started */
+  def ensureStarted()(using scheduler: Scheduler): this.type =
     synchronized:
-      if status == Initial then
-        scheduler.schedule(() => complete())
-        status = Started
+      if status == Initial then start()
     this
 
-object Future:
-  enum Status:
-    case Initial, Started, Completed
+  /** Start future's execution
+   *  @pre future has not yet started
+   */
+  def start()(using scheduler: Scheduler): this.type =
+    synchronized:
+      assert(status == Initial)
+      this.scheduler = scheduler
+      scheduler.schedule(() => complete())
+      status = Started
+    this
 
+  def cancel(): Unit = synchronized:
+    if status != Completed && status != Cancelled then
+      status = Cancelled
+
+object Future:
+
+  class Cancellation extends Exception
+
+  enum Status:
+    // Transitions always go left to right.
+    // Cancelled --> Completed with Failure(Cancellation()) result
+    case Initial, Started, Cancelled, Completed
+
+  /** Construct a future and start it so that ion runs in parallel with the
+   *  current thread.
+   */
   def spawn[T](body: Async ?=> T)(using Scheduler): Future[T] =
     Future(body).start()
 end Future
@@ -79,11 +121,3 @@ end Future
 def Test(x: Future[Int], xs: List[Future[Int]])(using Scheduler): Future[Int] =
   Future.spawn:
     x.await + xs.map(_.await).sum
-
-
-
-
-
-
-
-
