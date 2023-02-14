9f28641da43ed10aeeaa9aa6b97f572021ed693c
David Hua <d5hua@uwaterloo.ca>
2023-02-14 02:34:42 -0500

    Add tests for clause interleaving to safe initialization test suite.

---
+++ tests/init/pos/interleaving-params.scala
@@ -0,0 +1,8 @@
+import scala.language.experimental.clauseInterleaving
+
+class Params{
+    type U
+    def foo[T](x: T)[U >: x.type <: T](using U)[L <: List[U]](l: L): L = ???
+    def aaa(x: U): U = ???
+    def bbb[T <: U](x: U)[U]: U = ???
+}
