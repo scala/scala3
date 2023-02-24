4e27a3acf829d36f35e37249c52af0b4b60be5b2
Timothée Andres <andres.timothee@gmail.com>
2023-02-24 10:31:38 +0100

    Create clean test for inline Numeric
    Split code into separate files for clarity
    
    Reimplement Numeric and Ordering as inline
    
    Place symbols in the trait

---
+++ tests/run/inline-numeric/Fractional.scala
@@ -0,0 +1,62 @@
+package scala.math
+package inline
+
+trait Fractional[T] extends Numeric[T]:
+  transparent inline def div(inline x: T, inline y: T): T
+  protected transparent inline def isNaN(inline x: T): Boolean
+  protected transparent inline def isNegZero(inline x: T): Boolean
+
+  extension (inline x: T)
+    transparent inline def abs: T =
+      if lt(x, zero) || isNegZero(x) then negate(x) else x
+    transparent inline def sign: T =
+      if isNaN(x) || isNegZero(x) then x
+      else if lt(x, zero) then negate(one)
+      else if gt(x, zero) then one
+      else zero
+    transparent inline def /(inline y: T) = div(x, y)
+
+object Fractional:
+  given BigDecimalIsFractional: BigDecimalIsConflicted with Fractional[BigDecimal] with
+    transparent inline def div(inline x: BigDecimal, inline y: BigDecimal): BigDecimal = x / y
+
+    protected transparent inline def isNaN(inline x: BigDecimal): Boolean = false
+    protected transparent inline def isNegZero(inline x: BigDecimal): Boolean = false
+
+  given DoubleIsFractional: Fractional[Double] with Ordering.DoubleIeeeOrdering with
+    transparent inline def plus(inline x: Double, inline y: Double): Double = x + y
+    transparent inline def minus(inline x: Double, inline y: Double): Double = x - y
+    transparent inline def times(inline x: Double, inline y: Double): Double = x * y
+    transparent inline def div(inline x: Double, inline y: Double): Double = x / y
+    transparent inline def negate(inline x: Double): Double = -x
+
+    transparent inline def fromInt(x: Int): Double = x.toDouble
+    def parseString(str: String): Option[Double] = str.toDoubleOption
+
+    protected transparent inline def isNaN(inline x: Double): Boolean = x.isNaN
+    protected transparent inline def isNegZero(inline x: Double): Boolean = x.equals(-0.0)
+
+    extension (inline x: Double)
+      transparent inline def toInt: Int = x.toInt
+      transparent inline def toLong: Long = x.toLong
+      transparent inline def toFloat: Float = x.toFloat
+      transparent inline def toDouble: Double = x
+
+  given FloatIsFractional: Fractional[Float] with Ordering.FloatIeeeOrdering with
+    transparent inline def plus(inline x: Float, inline y: Float): Float = x + y
+    transparent inline def minus(inline x: Float, inline y: Float): Float = x - y
+    transparent inline def times(inline x: Float, inline y: Float): Float = x * y
+    transparent inline def div(inline x: Float, inline y: Float): Float = x / y
+    transparent inline def negate(inline x: Float): Float = -x
+
+    transparent inline def fromInt(x: Int): Float = x.toFloat
+    def parseString(str: String): Option[Float] = str.toFloatOption
+
+    protected transparent inline def isNaN(inline x: Float): Boolean = x.isNaN
+    protected transparent inline def isNegZero(inline x: Float): Boolean = x.equals(-0f)
+
+    extension (inline x: Float)
+      transparent inline def toInt: Int = x.toInt
+      transparent inline def toLong: Long = x.toLong
+      transparent inline def toFloat: Float = x
+      transparent inline def toDouble: Double = x.toDouble
---
+++ tests/run/inline-numeric/test.scala
@@ -0,0 +1,54 @@
+import scala.math.inline.*
+import scala.math.inline.Ordering.*
+import scala.math.inline.Integral.given
+import scala.math.inline.Fractional.given
+
+object tests:
+  inline def foo[T: Numeric](inline a: T, inline b: T) =
+    a + b * b
+
+  inline def div[T: Integral](inline a: T, inline b: T) =
+    a / b % b
+
+  inline def div[T: Fractional](inline a: T, inline b: T) =
+    a / b + a
+
+  inline def toInt[T: Numeric](inline a: T) =
+    a.toInt
+
+  inline def explicitToInt[T](inline a: T)(using n: Numeric[T]) =
+    n.toInt(a)
+
+  inline def sign[T: Numeric](inline a: T) =
+    a.sign
+
+  inline def explicitPlus[T](inline a: T, inline b: T)(using n: Numeric[T]) =
+    n.plus(a, b)
+
+  @main def Test =
+    def a: Int = 0
+    def b: Int = 1
+
+    val v1 = foo(a, b) // should be a + b * b // can check with -Xprint:inlining
+    val v2 = foo(a.toShort, b.toShort) // should be a + b * b
+
+    val v3 = div(BigDecimal(a), BigDecimal(b))(using BigDecimalAsIfIntegral) // should be BigDecimal(a) quot BigDecimal(b) remainder BigDecimal(b)
+    val v4 = div(BigDecimal(a), BigDecimal(b))(using BigDecimalIsFractional) // should be BigDecimal(a) / BigDecimal(b) + BigDecimal(a)
+
+    val v5 = toInt(a.toFloat) // should be a.toFloat.toInt
+    val v6 = toInt(a) // should be a
+
+    val v7 = sign(a)
+    val v8 = sign(a.toChar)
+    val v9 = sign(-7F)
+
+    val v10 = sign(BigDecimal(a))(using BigDecimalAsIfIntegral)
+    val v11 = sign(BigDecimal(a))(using BigDecimalIsFractional) // the condition with isNan() should be removed, i.e. it should be equivalent to v10
+
+    val v12 = explicitPlus(3, 5) // should be 8
+    val v13 = explicitPlus(a, b) // should be a + b
+
+    val v14 = explicitToInt(3.2) // should be (3.2).toInt
+    val v15 = explicitToInt(3) // should be 3
+    val v16 = explicitToInt(a) // should be a
+    val v17 = explicitToInt(a.toShort) // should be a.toShort.toInt
