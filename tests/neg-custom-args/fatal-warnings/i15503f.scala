432e829d3bdae27a2f62e18fb8878c74c0676ceb
Szymon Rodziewicz <szymonrodant@gmail.com>
2023-03-29 15:51:50 +0200

    Add a failing case with named using to test Wunused:implicits

--- tests/neg-custom-args/fatal-warnings/i15503f.scala
+++ tests/neg-custom-args/fatal-warnings/i15503f.scala
@@ -5,8 +5,9 @@ val default_int = 1
 
 def f1(a: Int) = a // OK
 def f2(a: Int) = 1 // OK
-def f3(a: Int)(using Int) = a // error
-def f4(a: Int)(using Int) = default_int // error
+def f3(a: Int)(using Int) = a // OK
+def f4(a: Int)(using Int) = default_int // OK
 def f6(a: Int)(using Int) = summon[Int] // OK
 def f7(a: Int)(using Int) = summon[Int] + a // OK
+def f8(a: Int)(using foo: Int) = a // error
 
