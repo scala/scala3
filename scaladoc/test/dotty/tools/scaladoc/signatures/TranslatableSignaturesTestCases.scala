fe0f685cde359ec1048e177a432aa6ddf3efe287
Florian3k <mfornal@virtuslab.com>
2023-04-06 10:48:38 +0200

    scaladoc: type rendering fixes and improvements

--- scaladoc/test/dotty/tools/scaladoc/signatures/TranslatableSignaturesTestCases.scala
+++ scaladoc/test/dotty/tools/scaladoc/signatures/TranslatableSignaturesTestCases.scala
@@ -108,3 +108,13 @@ class ImplicitMembers extends SignatureTest(
 )
 
 class NonScala3Parent extends SignatureTest("nonScala3Parent", SignatureTest.all)
+
+class SupertypeParamsSubstitution extends SignatureTest("supertypeParamsSubstitution", SignatureTest.all)
+
+class ThisType extends SignatureTest("thisType", SignatureTest.all)
+
+class PathDependentTypes extends SignatureTest("pathDependentTypes", SignatureTest.all)
+
+class MatchTypeTuple extends SignatureTest("matchTypeTuple", SignatureTest.all)
+
+class InfixTypes extends SignatureTest("infixTypes", SignatureTest.all)
