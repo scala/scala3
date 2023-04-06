fb2186293313cf3b101a65dd4ad0cf01ca08d371
Fengyun Liu <fengyun.liu.cs@gmail.com>
2023-04-06 07:58:12 +0200

    Separate object init tests from class init tests

--- tests/init/pos/i12544.scala
+++ tests/init/pos/i12544.scala
@@ -1,8 +1,5 @@
 enum Enum:
   case Case
-  case Case2(x: Int)
-
-def g(b: Enum.B): Int = b.foo()
 
 object Enum:
   object nested:
@@ -11,9 +8,5 @@ object Enum:
   val b: Enum = f(nested.a)
 
   def f(e: Enum): Enum = e
-
-  class B() { def foo() = n + 1 }
-  g(new B())                       // error
-  val n: Int = 10
 
 @main def main(): Unit = println(Enum.b)
