aae9ec1dfbe695ae481be5d6740898d9bd54d624
Kacper Korban <kacper.f.korban@gmail.com>
2023-04-21 01:35:46 +0200

    Wunused: Include import selector bounds in unused checks
    closes lampepfl#17314

---
+++ tests/pos-special/fatal-warnings/i17314a.scala
@@ -0,0 +1,12 @@
+// scalac: -Wunused:all
+
+package foo:
+  class Foo[T]
+  given Foo[Int] = new Foo[Int]
+
+
+package bar:
+  import foo.{given foo.Foo[Int]}
+  import foo.Foo
+
+  val repro: Foo[Int] = summon[Foo[Int]]
