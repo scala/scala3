b9a81b117922335c636c9bf4efd039d6095e20ff
Timothée Andres <andres.timothee@gmail.com>
2023-04-21 12:10:08 +0200

    Clean up example 4, make version without inner class

--- tests/pos/inline-trait-4-inner-class.scala
+++ tests/pos/inline-trait-4-inner-class.scala
@@ -1,28 +1,20 @@
-inline trait Options[T]:
-  // FIXME: remove original class definition from inline trait to allow public classes
-  private trait Option:
+inline trait Options[+T]:
+  sealed trait Option:
     def get: T
-  // FIXME: support constructor parameter
-  // FIXME: support specialized parents
-  private class Some://(x: T): // extends Option
-    def get: T = ??? // x
-  // FIXME: support specialized modules
-  // private object None // extends Option
-  //   def get: T = throw new NoSuchElementException("None.get")
+    def isEmpty: Boolean
 
-  // FIXME: specialize reference to Option and Some
-  // def option(value: T): Option = new Some//(value)
+  class Some(x: T) extends Option:
+    def get: T = x
+    def isEmpty: Boolean = false
+
+  object None extends Option:
+    def get: T = throw new NoSuchElementException("None.get")
+    def isEmpty: Boolean = true
 end Options
 
-object IntOptions extends Options[Int]:
-  /*
-  <generated> trait Option:
-    def get: Int
-  <generated> class Some(x: Int) extends Option
-    def get: Int = x
-  <generated> object None extends Option
-    def get: Int = throw new NoSuchElementException("None.get")
+object IntOptions extends Options[Int]
+import IntOptions._
 
-  <generated> def option(value: Int): Option = new Some(value)
-  */
-end IntOptions
+val o1: Option = Some(1) // specialized
+val o2: Option = None
+val x1: Int = o1.get // no unboxing
