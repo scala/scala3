1ac2066c6a898928327985121149a14dc3774df1
Lucas <leblanc.lucas29@gmail.com>
2023-04-24 11:21:04 +0200

    Corrections - Change SocialLinks and do a match case - Rephrase ScaladocSetting for custom

--- scaladoc/src/dotty/tools/scaladoc/ScaladocSettings.scala
+++ scaladoc/src/dotty/tools/scaladoc/ScaladocSettings.scala
@@ -43,8 +43,7 @@ class ScaladocSettings extends SettingGroup with AllScalaSettings:
 
   val socialLinks: Setting[List[String]] =
     MultiStringSetting("-social-links", "social-links",
-      "Links to social sites. '[github|twitter|gitter|discord]::link' syntax is used." +
-        "'custom::link::white_icon_name::black_icon_name' is also allowed, in this case icons must be present in 'images/'' directory.")
+      "Links to social sites. '[github|twitter|gitter|discord]::link' or 'custom::link::light_icon_file_name[::dark_icon_file_name]' syntax is used. For custom links, the icons must be present in '_assets/images/'")
 
   val deprecatedSkipPackages: Setting[List[String]] =
     MultiStringSetting("-skip-packages", "packages", "Deprecated, please use `-skip-by-id` or `-skip-by-regex`")
