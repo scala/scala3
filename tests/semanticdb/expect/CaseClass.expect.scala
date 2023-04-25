931cb921bff1796181151e5407a765462b65761a
Katarzyna Marek <kasia@marek.net>
2023-04-25 13:56:14 +0200

    bugfix: in semanticdb make synthetic apply disambiguator consistent with Scala 2 implementation
    [Cherry-picked 7a44b59d8f733356d8050e8d122e933b7cac2813]

---
+++ tests/semanticdb/expect/CaseClass.expect.scala
@@ -0,0 +1,7 @@
+package caseclass
+
+case class CaseClass/*<-caseclass::CaseClass#*/(int1/*<-caseclass::CaseClass#int1.*/: Int/*->scala::Int#*/, int2/*<-caseclass::CaseClass#int2.*/: Int/*->scala::Int#*/)
+
+object CaseClass/*<-caseclass::CaseClass.*/:
+  def apply/*<-caseclass::CaseClass.apply().*/(int/*<-caseclass::CaseClass.apply().(int)*/: Int/*->scala::Int#*/): CaseClass/*->caseclass::CaseClass#*/ = CaseClass/*->caseclass::CaseClass.*/(int/*->caseclass::CaseClass.apply().(int)*/, 0)
+  def apply/*<-caseclass::CaseClass.apply(+1).*/(): CaseClass/*->caseclass::CaseClass#*/ = CaseClass/*->caseclass::CaseClass.*/(0, 0)
