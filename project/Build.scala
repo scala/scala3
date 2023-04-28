1cd9bd263e2fcd557c6779e29ad10f6d8299cf99
Paweł Marks <pmarks@virtuslab.com>
2023-04-28 13:29:31 +0200

    Set reference version to 3.3.0-RC5

--- project/Build.scala
+++ project/Build.scala
@@ -80,7 +80,7 @@ object DottyJSPlugin extends AutoPlugin {
 object Build {
   import ScaladocConfigs._
 
-  val referenceVersion = "3.3.0-RC4"
+  val referenceVersion = "3.3.0-RC5"
 
   val baseVersion = "3.3.1-RC1"
 
