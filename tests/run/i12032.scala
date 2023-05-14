7959821ab55523b14e71178087d10acf3dd54c55
Chris Kipp <ckipp@pm.me>
2023-05-14 13:24:08 +0200

    test: add in a regression test for #12032
    [skip community_build]
    
    closes #12032

---
+++ tests/run/i12032.scala
@@ -0,0 +1,24 @@
+// https://github.com/lampepfl/dotty/issues/12032
+class Foo(val strings: Seq[String]) extends FooLowPriority
+
+trait FooLowPriority { self: Foo =>
+  @scala.annotation.targetName("appendFromProducers")
+  def append(v: String): Foo = new Foo(v +: self.strings)
+}
+
+trait FooBar { self: Foo =>
+  @scala.annotation.targetName("appendFromProducers")
+  final override def append(v: String): Foo = new Foo(self.strings :+ v)
+}
+
+object Foo {
+  type Bar = Foo with FooBar
+
+  def bar(vs: String*): Bar = new Foo(vs) with FooBar
+}
+
+@main def Test() =
+  Foo.bar("foo")
+    .append("bar")
+    .strings
+    .foreach(println)
