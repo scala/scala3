2ca90a512cdd2d1e6d2e04d9ed017c85728536ba
G1ng3r <saynulanude@gmail.com>
2023-05-23 23:14:18 +0300

    handle varargs

--- compiler/src/dotty/tools/dotc/typer/Dynamic.scala
+++ compiler/src/dotty/tools/dotc/typer/Dynamic.scala
@@ -181,6 +181,18 @@ trait Dynamic {
   def handleStructural(tree: Tree)(using Context): Tree = {
     val fun @ Select(qual, name) = funPart(tree): @unchecked
     val vargss = termArgss(tree)
+    val isRepeated = vargss.flatten.exists(_.tpe.widen.isRepeatedParam)
+
+    def handleRepeated(args: List[List[Tree]]): List[Tree] =
+      if isRepeated then List(untpd.TypedSplice(tpd.repeated(args.flatten, TypeTree(defn.AnyType))))
+      else args.flatten.map { t =>
+        val clzSym = t.tpe.resultType.classSymbol.asClass
+        if ValueClasses.isDerivedValueClass(clzSym) then
+          val underlying = ValueClasses.valueClassUnbox(clzSym).asTerm
+          tpd.Select(t, underlying.name)
+        else
+          t
+      }.map(untpd.TypedSplice(_))
 
     def structuralCall(selectorName: TermName, classOfs: => List[Tree]) = {
       val selectable = adapt(qual, defn.SelectableClass.typeRef | defn.DynamicClass.typeRef)
@@ -193,14 +205,7 @@ trait Dynamic {
 
       val scall =
         if (vargss.isEmpty) base
-        else untpd.Apply(base, vargss.flatten.map{ t =>
-          val clzSym = t.tpe.resultType.classSymbol.asClass
-          if ValueClasses.isDerivedValueClass(clzSym) then
-            val x = ValueClasses.valueClassUnbox(clzSym).asTerm
-            tpd.Select(t, x.name)
-          else
-            t
-        }.map(untpd.TypedSplice(_)))
+        else untpd.Apply(base, handleRepeated(vargss))
 
       // If function is an `applyDynamic` that takes a Class* parameter,
       // add `classOfs`.
--- tests/run/i16995.scala
+++ tests/run/i16995.scala
@@ -7,6 +7,8 @@ class Reflective extends reflect.Selectable
     def bar(foo: Foo) = foo.i
     def fun(argument: Argument) = argument
     def manyArgs(argument: Argument, foo: Foo, someInt: Int) = foo.i + someInt + argument.x.length
+    def varargs(x: Int, foo: Foo*) = foo.map(_.i).sum + x
+    def letsHaveSeq(args: Seq[Argument]) = args.map(_.x.length).sum
   }
   
   val i = reflective.bar(Foo(1))
@@ -21,3 +23,18 @@ class Reflective extends reflect.Selectable
   val x = 1
   val j = reflective.manyArgs(length4, foo, x)
   println(j)
+
+  val varargs = List(Foo(1), Foo(2), Foo(3))
+  val m = reflective.varargs(1, varargs:_*)
+  println(m)
+
+  val foo1 = Foo(1)
+  val foo2 = Foo(2)
+  val foo3 = Foo(3)
+  val n = reflective.varargs(2, foo1, foo2)
+  println(n)
+
+  val arg1 = Argument("1")
+  val seq = Seq(arg1, arg1, arg1)
+  val p = reflective.letsHaveSeq(seq)
+  println(p)
