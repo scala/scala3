733eeede22ae02b814fb2d14c26c04fe49f683e4
odersky <odersky@gmail.com>
2023-05-26 10:34:21 +0200

    Don't generate a super accessor for an inline method call
    Inline methods cannot be overridden, so they don't need super accessors.
    
    Fixes #17584
    
    [Cherry-picked 467edd8f8dd8d8b10979b0ab2af04e1e9f1be87d]

--- compiler/src/dotty/tools/dotc/transform/SuperAccessors.scala
+++ compiler/src/dotty/tools/dotc/transform/SuperAccessors.scala
@@ -153,7 +153,9 @@ class SuperAccessors(thisPhase: DenotTransformer) {
         }
     }
 
-    val needAccessor = name.isTermName && (
+    val needAccessor =
+      name.isTermName                // Types don't need super accessors
+      && !sym.isInlineMethod && (    // Inline methods can't be overridden, don't need superaccessors
       clazz != currentClass || !validCurrentClass || mix.name.isEmpty && clazz.is(Trait))
 
     if (needAccessor) atPhase(thisPhase.next)(superAccessorCall(sel, mix.name))
