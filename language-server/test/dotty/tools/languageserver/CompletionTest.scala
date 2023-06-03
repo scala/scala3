c04bb4f6436aaae3cd5f2b6c8b2bd0a01f741c82
Dale Wijnand <dale.wijnand@gmail.com>
2023-06-03 12:18:55 +0100

    Print full package names, even if they're TermRefs
    If you refer to a package by name, e.g. `scala.quoted.Quotes`, the type
    may have a "quoted" prefix that is a TermRef.  Rather than unravel the
    rest of the prefixes, we print the fully package name.

--- language-server/test/dotty/tools/languageserver/CompletionTest.scala
+++ language-server/test/dotty/tools/languageserver/CompletionTest.scala
@@ -39,7 +39,7 @@ class CompletionTest {
     code"class Foo { val foo: BigD${m1} }"
       .completion(
         ("BigDecimal", Field, "BigDecimal"),
-        ("BigDecimal", Method, "=> math.BigDecimal.type"),
+        ("BigDecimal", Method, "=> scala.math.BigDecimal.type"),
       )
   }
 
