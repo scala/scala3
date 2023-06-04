e05078cb50fb1a90f51b6b01533e1ca4e1fb76d8
odersky <odersky@gmail.com>
2023-06-04 20:22:28 +0200

    Address review comments
    [Cherry-picked c93765ee9d9ea85844178d615acbe3b27fc2f1f0]

--- tests/run/i17584a.scala
+++ tests/run/i17584a.scala
@@ -1,5 +1,7 @@
-trait A:
-  inline def g = 1
+trait T:
+  def g = 2
+trait A extends T:
+  inline override def g = 1
 trait B extends A:
   def f = super.g
 class C extends B
@@ -7,4 +9,3 @@ class C extends B
 @main def Test =
   val c = C()
   assert(c.f == 1)
-
