68693855801419940f47c804a0f21b843f43bbab
Lucas <leblanc.lucas29@gmail.com>
2023-06-13 11:52:35 +0200

    Fix: Test with previous version of the file

--- scaladoc/test/dotty/tools/scaladoc/site/SidebarParserTest.scala
+++ scaladoc/test/dotty/tools/scaladoc/site/SidebarParserTest.scala
@@ -1,16 +1,9 @@
 package dotty.tools.scaladoc
 package site
-
 import org.junit.Test
 import org.junit.Assert._
-import dotty.tools.scaladoc.site.Sidebar
-import dotty.tools.scaladoc.site.Sidebar.RawInput
-import java.io.ByteArrayOutputStream
-import java.io.PrintStream
-
-// TODO add negative and more details tests
+// TODO add negaitve and more details tests
 class SidebarParserTest:
-
   private val sidebar =
     """index: index.md
       |subsection:
@@ -38,63 +31,6 @@ class SidebarParserTest:
       |          - page: my-page6/my-page6/my-page6.md
       """.stripMargin
 
-  private val sidebarErrorNoTitle =
-    """index: index.md
-      |subsection:
-      |    page: my-page1.md
-      |  - page: my-page2.md
-      |  - page: my-page3/subsection
-      |  - title: Reference
-      |    subsection:
-      |      - page: my-page3.md
-      |        hidden: true
-      |  - index: my-page4/index.md
-      |    subsection:
-      |      - page: my-page4/my-page4.md
-      |  - title: My subsection
-      |    index: my-page5/index.md
-      |    subsection:
-      |      - page: my-page5/my-page5.md
-      |  - subsection:
-      |        page: my-page7/my-page7.md
-      |  - index: my-page6/index.md
-      |    subsection:
-      |      - index: my-page6/my-page6/index.md
-      |        subsection:
-      |          - page: my-page6/my-page6/my-page6.md
-      """.stripMargin
-
-  private val sidebarErrorNoPage =
-    """index: index.md
-      |subsection:
-      |  - title: My title
-      |  - page: my-page2.md
-      |  - page: my-page3/subsection
-      |  - title: Reference
-      |    subsection:
-      |      - page: my-page3.md
-      |        hidden: true
-      |  - index: my-page4/index.md
-      |    subsection:
-      |      - page: my-page4/my-page4.md
-      |  - title: My subsection
-      |    index: my-page5/index.md
-      |    subsection:
-      |      - page: my-page5/my-page5.md
-      |  - subsection:
-      |      - page: my-page7/my-page7.md
-      |  - index: my-page6/index.md
-      |    subsection:
-      |      - index: my-page6/my-page6/index.md
-      |        subsection:
-      |          - page: my-page6/my-page6/my-page6.md
-      """.stripMargin
-
-  private val msgNoTitle = "`title` property is missing for some page."
-  private val msgNoPage = "Error parsing YAML configuration file: 'index' or 'page' path is missing for title 'My title'."
-
-  private def schemaMessage: String = Sidebar.schemaMessage
-
   @Test
   def loadSidebar(): Unit = assertEquals(
     Sidebar.Category(
@@ -114,24 +50,3 @@ class SidebarParserTest:
     ),
     Sidebar.load(sidebar)(using testContext)
   )
-
-  @Test
-  def loadSidebarNoPageError: Unit =
-    val out = new ByteArrayOutputStream()
-    Console.withErr(new PrintStream(out)) {
-      Sidebar.load(sidebarErrorNoPage)(using testContext)
-    }
-    val error = out.toString().trim()
-
-    assert(error.contains(msgNoPage) && error.contains(schemaMessage))
-
-
-  @Test
-  def loadSidebarNoTitleError(): Unit =
-    val out = new ByteArrayOutputStream()
-    Console.withErr(new PrintStream(out)) {
-      Sidebar.load(sidebarErrorNoTitle)(using testContext)
-    }
-    val error = out.toString().trim()
-
-    assert(error.contains(msgNoTitle) && error.contains(schemaMessage))
