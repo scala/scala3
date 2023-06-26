c21133f1c5b39054031aed2f28a82f1e396a3f2d
Guillaume Martres <smarter@ubuntu.com>
2023-06-26 19:05:46 +0200

    Fix signature computation involving nested type variables
    During typechecking, we sometimes need to compute the signature of a type
    involving uninstantiated type variables or wildcards (in particular, because
    signature matching is doen as part of subtype checking for methods), this is
    handled with `tpnme.Uninstantiated` which is handled specially in `Signature`.
    
    Before this commit, `sigName` only checked for wildcards and type variables
    at the top-level of the type, even though nested types can still have
    an impact on type erasure, in particular when they appear as part of:
    - an intersection
    - a union
    - an underlying type of a derived value class
    - the element type of an array type
    - an element type of a tuple (... *: X *: ...)
    
    We keep track of all these situations by returning `null` in
    `TypeErasure#apply` when encountering a wildcard or uninstantiated type
    variable, which we then propage upwards to the `sigName` call. This propagation
    only happens for `sigName` calls (where `inSigName` is true), otherwise we
    throw an assertion since erasure shouldn't normally be computed for
    underdefined types.

--- compiler/test/dotty/tools/SignatureTest.scala
+++ compiler/test/dotty/tools/SignatureTest.scala
@@ -4,12 +4,19 @@ import vulpix.TestConfiguration
 
 import org.junit.Test
 
-import dotc.ast.Trees._
+import dotc.ast.untpd
 import dotc.core.Decorators._
 import dotc.core.Contexts._
+import dotc.core.Flags._
 import dotc.core.Phases._
 import dotc.core.Types._
 import dotc.core.Symbols._
+import dotc.core.StdNames._
+import dotc.core.Signature
+import dotc.typer.ProtoTypes.constrained
+import dotc.typer.Inferencing.isFullyDefined
+import dotc.typer.ForceDegree
+import dotc.util.NoSourcePosition
 
 import java.io.File
 import java.nio.file._
@@ -38,3 +45,34 @@ class SignatureTest:
              |${ref.denot.signature}""".stripMargin)
       }
     }
+
+  /** Ensure that signature computation returns an underdefined signature when
+   *  the signature depends on uninstantiated type variables.
+   */
+  @Test def underdefined: Unit =
+    inCompilerContext(TestConfiguration.basicClasspath, separateRun = false,
+      """trait Foo
+        |trait Bar
+        |class A[T <: Tuple]:
+        |  def and(x: T & Foo): Unit = {}
+        |  def andor(x: (T | Bar) & Foo): Unit = {}
+        |  def array(x: Array[(T | Bar) & Foo]): Unit = {}
+        |  def tuple(x: Foo *: T): Unit = {}
+        |  def tuple2(x: Foo *: (T | Tuple) & Foo): Unit = {}
+        |""".stripMargin):
+      val cls = requiredClass("A")
+      val tvar = constrained(cls.requiredMethod(nme.CONSTRUCTOR).info.asInstanceOf[TypeLambda], untpd.EmptyTree, alwaysAddTypeVars = true)._2.head.tpe
+      tvar <:< defn.TupleTypeRef
+      val prefix = cls.typeRef.appliedTo(tvar)
+
+      def checkSignatures(expectedIsUnderDefined: Boolean)(using Context): Unit =
+        for decl <- cls.info.decls.toList if decl.is(Method) && !decl.isConstructor do
+          val meth = decl.asSeenFrom(prefix)
+          val sig = meth.info.signature
+          val what = if expectedIsUnderDefined then "underdefined" else "fully-defined"
+          assert(sig.isUnderDefined == expectedIsUnderDefined, i"Signature of `$meth` with prefix `$prefix` and type `${meth.info}` should be $what but is `$sig`")
+
+      checkSignatures(expectedIsUnderDefined = true)
+      assert(isFullyDefined(tvar, force = ForceDegree.all), s"Could not instantiate $tvar")
+      checkSignatures(expectedIsUnderDefined = false)
+
