scala/scala3

commit a019a4e4532ae086c2d223186580c898023bb5e4

Enforce restrictions on typelevel methods
Typelevel methods are transparent methods with toplevel matches. We now
track this property with a flag. To be done: merge with the Macro flag.

Typelevel methods

 - cannot be eta-expanded
 - cannot override anything

Also, fixes the "missing args" error message, which previously claimed that `()`
was missing even for nonempty argument lists.

compiler/src/dotty/tools/dotc/typer/Typer.scala
@@ -2128,7 +2128,9 @@ class Typer extends Namer
     def readaptSimplified(tree: Tree)(implicit ctx: Context) = readapt(simplify(tree, pt, locked))
 
     def missingArgs(mt: MethodType) = {
-      ctx.error(MissingEmptyArgumentList(methPart(tree).symbol), tree.pos)
+      val meth = methPart(tree).symbol
+      if (mt.paramNames.length == 0) ctx.error(MissingEmptyArgumentList(meth), tree.pos)
+      else ctx.error(em"missing arguments for $meth", tree.pos)
       tree.withType(mt.resultType)
     }
 
@@ -2338,10 +2340,12 @@ class Typer extends Namer
 
       // Reasons NOT to eta expand:
       //  - we reference a constructor
+      //  - we reference a typelevel method
       //  - we are in a pattern
       //  - the current tree is a synthetic apply which is not expandable (eta-expasion would simply undo that)
       if (arity >= 0 &&
           !tree.symbol.isConstructor &&
+          !tree.symbol.is(TypeLevel) &&
           !ctx.mode.is(Mode.Pattern) &&
           !(isSyntheticApply(tree) && !isExpandableApply))
         simplify(typed(etaExpand(tree, wtp, arity), pt), pt, locked)

compiler/src/dotty/tools/backend/jvm/DottyBackendInterface.scala
@@ -433,7 +433,7 @@ class DottyBackendInterface(outputDirectory: AbstractFile, val superCallsMap: Ma
   val Flag_METHOD: Flags = Flags.Method.bits
   val ExcludedForwarderFlags: Flags = {
       Flags.Specialized | Flags.Lifted | Flags.Protected | Flags.JavaStatic |
-      Flags.Bridge | Flags.VBridge | Flags.Private | Flags.Macro
+      Flags.Bridge | Flags.Private | Flags.TypeLevel | Flags.Macro
   }.bits
 
   def isQualifierSafeToElide(qual: Tree): Boolean = tpd.isIdempotentExpr(qual)

compiler/src/dotty/tools/dotc/core/Flags.scala
@@ -351,8 +351,8 @@ object Flags {
   /** A bridge method. Set by Erasure */
   final val Bridge = termFlag(34, "<bridge>")
 
-  /** Symbol is a Java varargs bridge */ // (needed?)
-  final val VBridge = termFlag(35, "<vbridge>") // TODO remove
+  /** Symbol is a typelevel method, cannot be used in normal code */
+  final val TypeLevel = termFlag(35, "<type-level>")
 
   /** Symbol is a method which should be marked ACC_SYNCHRONIZED */
   final val Synchronized = termFlag(36, "<synchronized>")

compiler/src/dotty/tools/dotc/core/SymDenotations.scala
@@ -600,7 +600,7 @@ object SymDenotations {
 
     /** Is this a denotation of a stable term (or an arbitrary type)? */
     final def isStable(implicit ctx: Context) =
-      isType || is(Stable) || !(is(UnstableValue) || info.isInstanceOf[ExprType])
+      isType || is(Stable) || !is(UnstableValue) && !info.isInstanceOf[ExprType]
 
     /** Is this a denotation of a class that does not have - either direct or inherited -
      *  initaliazion code?

compiler/src/dotty/tools/dotc/core/tasty/TastyFormat.scala
@@ -182,6 +182,7 @@ Standard-Section: "ASTs" TopLevelStat*
                   LAZY
                   OVERRIDE
                   TRANSPARENT
+                  TYPELEVEL                           // transparent method containing toplevel matches (TODO: merge with MACRO)
                   MACRO                               // transparent method containing toplevel splices
                   STATIC                              // mapped to static Java member
                   OBJECT                              // an object or its class
@@ -295,8 +296,8 @@ object TastyFormat {
   final val IMPLICIT = 13
   final val LAZY = 14
   final val OVERRIDE = 15
-
-  final val TRANSPARENT = 17
+  final val TRANSPARENT = 16
+  final val TYPELEVEL = 17
   final val STATIC = 18
   final val OBJECT = 19
   final val TRAIT = 20
@@ -473,6 +474,7 @@ object TastyFormat {
        | LAZY
        | OVERRIDE
        | TRANSPARENT
+       | TYPELEVEL
        | MACRO
        | STATIC
        | OBJECT
@@ -529,6 +531,7 @@ object TastyFormat {
     case LAZY => "LAZY"
     case OVERRIDE => "OVERRIDE"
     case TRANSPARENT => "TRANSPARENT"
+    case TYPELEVEL => "TYPELEVEL"
     case MACRO => "MACRO"
     case STATIC => "STATIC"
     case OBJECT => "OBJECT"

compiler/src/dotty/tools/dotc/core/tasty/TreePickler.scala
@@ -601,6 +601,7 @@ class TreePickler(pickler: TastyPickler) {
     if (flags is Case) writeByte(CASE)
     if (flags is Override) writeByte(OVERRIDE)
     if (flags is Transparent) writeByte(TRANSPARENT)
+    if (flags is TypeLevel) writeByte(TYPELEVEL)
     if (flags is Macro) writeByte(MACRO)
     if (flags is JavaStatic) writeByte(STATIC)
     if (flags is Module) writeByte(OBJECT)

compiler/src/dotty/tools/dotc/core/tasty/TreeUnpickler.scala
@@ -585,6 +585,7 @@ class TreeUnpickler(reader: TastyReader,
           case LAZY => addFlag(Lazy)
           case OVERRIDE => addFlag(Override)
           case TRANSPARENT => addFlag(Transparent)
+          case TYPELEVEL => addFlag(TypeLevel)
           case MACRO => addFlag(Macro)
           case STATIC => addFlag(JavaStatic)
           case OBJECT => addFlag(Module)

compiler/src/dotty/tools/dotc/core/unpickleScala2/PickleBuffer.scala
@@ -232,7 +232,7 @@ object PickleBuffer {
       EXPANDEDNAME -> Scala2ExpandedName,
       IMPLCLASS -> (Scala2PreSuper, ImplClass),
       SPECIALIZED -> Specialized,
-      VBRIDGE -> VBridge,
+      VBRIDGE -> EmptyFlags,
       VARARGS -> JavaVarargs,
       ENUM -> Enum)
 

commit a7bae7a37e6af82aaff1c4c21b8a57e73ab7e402

Allow eta-expansion of inline defs
The fact that this wasn't allowed before seems to be a bug and not an
intentional restriction: the check was originally introduced in
a019a4e4532ae086c2d223186580c898023bb5e4 for "typelevel methods" which are only
vaguely related to today's inline methods.

compiler/src/dotty/tools/dotc/typer/Typer.scala
@@ -3976,12 +3976,10 @@ class Typer(@constructorOnly nestingLevel: Int = 0) extends Namer
 
       // Reasons NOT to eta expand:
       //  - we reference a constructor
-      //  - we reference a typelevel method
       //  - we are in a pattern
       //  - the current tree is a synthetic apply which is not expandable (eta-expasion would simply undo that)
       if arity >= 0
          && !tree.symbol.isConstructor
-         && !tree.symbol.isAllOf(InlineMethod)
          && !ctx.mode.is(Mode.Pattern)
          && !(isSyntheticApply(tree) && !functionExpected)
       then

tests/neg/i12207.scala
@@ -5,4 +5,4 @@ extension [T](t: T) inline def pi[P <: Tuple](using P): T = ???
 inline def env[P <: Tuple, T](op: P ?=> T): P ?=> T = op
 
 @main def Test =
-  env { pi[String] } // error // error
+  env { pi[String] } // error

tests/neg/i7459.scala
@@ -2,7 +2,7 @@ object Foo {
   inline def summon[T](x: T): T =  x match {
     case t: T => t
   }
-  println(summon)  // error
+  println(summon)
 }
 
 import scala.deriving.*

tests/pos/inline-eta.scala
@@ -1,0 +1,9 @@
+class Foo(x: Int)
+
+object A:
+  inline def bar(x: Int): Int = x
+  val g1 = bar
+  val g2: Int => Int = bar
+
+  def foo(xs: List[Int]) =
+    xs.map(Foo.apply) // use the `inline def apply` constructor proxy