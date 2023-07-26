56251079218efae3d8fc9dd618ad009e7a396a86
Nicolas Stucki <nicolas.stucki@gmail.com>
2023-07-26 13:21:19 +0200

    Support polymorphic functions with erased parameters
    This adds support for
    ```scala
    [T1, ..., Tn] => ([erased] x1: X1, ..., [erased] xm: Xm) => R
    ```
    
    Polymorphic function types with erased parameters are represented as
    using a refinement on `PolyFunction`. `ErasedFunction` is not needed.
    ```scala
    PolyFunction {
      def apply[T1, ..., Tn]([erased] x1: X1, ..., [erased] xm: Xm): R
    }
    ```

---
+++ tests/pos/poly-erased-functions.scala
@@ -0,0 +1,14 @@
+import language.experimental.erasedDefinitions
+
+object Test:
+  type T1 = [X] => (erased x: X, y: Int) => Int
+  type T2 = [X] => (x: X, erased y: Int) => X
+
+  val t1 = [X] => (erased x: X, y: Int) => y
+  val t2 = [X] => (x: X, erased y: Int) => x
+
+  erased class A
+
+  type T3 = [X] => (x: A, y: X) => X
+
+  val t3 = [X] => (x: A, y: X) => y
