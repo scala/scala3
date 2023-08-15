5c072cabf6c7d59e4c866c487c10a1d33a3f910c
Dale Wijnand <dale.wijnand@gmail.com>
2023-08-15 17:55:01 +0100

    Vulpix: implement "// warn" tags
    Also, show compilation errors, if there are any, for a warn test.
    
    [Cherry-picked 3686ba06164d4db25cc12b2d688ea680f8864fab]

--- tests/warn/i16451.scala
+++ tests/warn/i16451.scala
@@ -22,21 +22,21 @@ object Test:
     case x: Wrapper[Color.Green.type] => None    // warn: unreachable // also: unchecked (hidden)
 
   def test_wrong(x: Wrapper[Color]): Option[Wrapper[Color.Red.type]] = x match
-    case x: Wrapper[Color.Red.type] => Some(x) // error: unreachable // error: unchecked
+    case x: Wrapper[Color.Red.type] => Some(x) // warn: unchecked
     case null                       => None
 
   def t2[A1 <: Wrapper[Color]](x: A1): Option[Wrapper[Color.Red.type]] = x match
-    case x: Wrapper[Color.Red.type] => Some(x)
+    case x: Wrapper[Color.Red.type] => Some(x) // warn: unchecked
     case null                       => None
 
   def test_wrong_seq(xs: Seq[Wrapper[Color]]): Seq[Wrapper[Color.Red.type]] =
     xs.collect {
-      case x: Wrapper[Color.Red.type] => x
+      case x: Wrapper[Color.Red.type] => x // warn: unchecked
     }
 
   def test_wrong_seq2(xs: Seq[Wrapper[Color]]): Seq[Wrapper[Color.Red.type]] =
     xs.collect { x => x match
-      case x: Wrapper[Color.Red.type] => x
+      case x: Wrapper[Color.Red.type] => x // warn: unchecked
     }
 
   def main(args: Array[String]): Unit =
