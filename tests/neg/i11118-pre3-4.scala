1b63f543de85d3aff392403edcfa1618b407e36d
Jamie Thompson <bishbashboshjt@gmail.com>
2023-09-28 13:37:52 +0200

    fix error in test

---
+++ tests/neg/i11118-pre3-4.scala
@@ -0,0 +1,4 @@
+import scala.language.`3.3`
+
+// https://github.com/lampepfl/dotty/issues/11118
+val (a,b) = (1,2,3) // error // warning
--- tests/neg/i11118.scala
+++ tests/neg/i11118.scala
@@ -1,2 +1,2 @@
 // https://github.com/lampepfl/dotty/issues/11118
-val (a,b) = (1,2,3) // error // warning
+val (a,b) = (1,2,3) // error
