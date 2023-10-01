eda563be728352e50df89cb547e3bdf3d8b23ac7
odersky <odersky@gmail.com>
2023-10-01 15:23:48 +0200

    Use LooseCaptureRoot checking for checking bounds of applied types
    Temporary measure until we have class capture roots worked out

--- compiler/src/dotty/tools/dotc/cc/CheckCaptures.scala
+++ compiler/src/dotty/tools/dotc/cc/CheckCaptures.scala
@@ -376,7 +376,7 @@ class CheckCaptures extends Recheck, SymTransformer:
         || sym.isTerm && defn.isFunctionType(sym.info) && pt == AnySelectionProto
       if sym.skipConstructor.isLevelOwner && canInstantiate then
         val tpw = tp.widen
-        val tp1 = mapRoots(sym.localRoot.termRef, CaptureRoot.Var(ctx.owner, sym))(tpw)
+        var tp1 = mapRoots(sym.localRoot.termRef, CaptureRoot.Var(ctx.owner, sym))(tpw)
           .showing(i"INST $sym: $tp, ${sym.localRoot} = $result", ccSetup)
         if tpw eq tp1 then tp else tp1
       else
@@ -760,6 +760,8 @@ class CheckCaptures extends Recheck, SymTransformer:
           capt.println(i"fallBack from $actualWide to $actualInst to match $expected1")
           ok = (actualInst ne actualWide)
             && isCompatible(adaptBoxed(actualInst, expected1, tree.srcPos), expected1)
+          // Useful for debugging:  
+          // if !ok then err.typeMismatch(tree.withType(actualInst), expected1, addenda ++ CaptureSet.levelErrors)
         case _ =>
       if !ok then
         capt.println(i"conforms failed for ${tree}: $actual vs $expected")
@@ -1231,6 +1233,6 @@ class CheckCaptures extends Recheck, SymTransformer:
               case tree: New =>
               case tree: TypeTree => checkAppliedTypesIn(tree.withKnownType)
               case _ => traverseChildren(t)
-          checkApplied.traverse(unit)
+          checkApplied.traverse(unit)(using ctx.withProperty(LooseRootChecking, Some(())))
   end CaptureChecker
 end CheckCaptures
