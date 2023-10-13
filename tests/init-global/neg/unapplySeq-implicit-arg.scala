fcec9130ee569dcc48ad897355f3bc0ec3f7ac83
EnzeXing <enzexing12@gmail.com>
2023-10-13 16:54:52 -0400

    Correct argument order in evalPattern and add test

--- tests/init-global/neg/unapplySeq-implicit-arg.scala
+++ tests/init-global/neg/unapplySeq-implicit-arg.scala
@@ -1,11 +1,14 @@
-class Foo
-
 object Bar {
-  def unapplySeq(using Foo)(using Foo)(pair: (Int, Int))(using Foo): Option[Seq[Int]] =
-    if pair._1 == 0 then Some(Seq(pair._1)) else Some(Seq(pair._2))
+  class Foo {
+    def m1(seq: Seq[Int]) = 1 +: seq
+    def m2(seq: Seq[Int]) = 2 +: seq
+  }
+  def unapplySeq(using f1: Foo)(seqi: Seq[Int]): Option[Seq[Int]] =
+    if seqi(0) == 0 then Some(f1.m1(seqi)) else Some(f1.m2(seqi))
+
   given Foo = new Foo
   val i1: Int = 0
-  val i2: Int = (i1, i2) match // error
+  val i2: Int = Seq(i2) match // error
     case Bar(i) => i
     case _ => 0
 }
