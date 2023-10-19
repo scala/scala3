mbovel/dotty

Elide companion defs to a `object` extending `AnyVal`

compiler/src/dotty/tools/dotc/typer/Checking.scala
@@ -738,7 +738,10 @@ object Checking {
               for (p <- params if !p.is(Erased))
                 report.error("value class can only have one non `erased` parameter", p.srcPos)
           case Nil =>
-            report.error(ValueClassNeedsOneValParam(clazz), clazz.srcPos)
+            if clazz.is(Module) then
+              report.error("A module cannot extends AnyVal",clazz.srcPos)
+            else
+              report.error(ValueClassNeedsOneValParam(clazz), clazz.srcPos)
         }
       }
       stats.foreach(checkValueClassMember)

scala/scala3

Co-authored-by: Matt Bovel <matthieu@bovel.net>

compiler/src/dotty/tools/dotc/typer/Checking.scala
@@ -717,6 +717,8 @@ object Checking {
     if (isDerivedValueClass(clazz)) {
       if (clazz.is(Trait))
         report.error(CannotExtendAnyVal(clazz), clazz.srcPos)
+      if clazz.is(Module) then
+        report.error(CannotExtendAnyVal(clazz), clazz.srcPos)
       if (clazz.is(Abstract))
         report.error(ValueClassesMayNotBeAbstract(clazz), clazz.srcPos)
       if (!clazz.isStatic)