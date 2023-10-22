07761aec094af62c4ed7621a0362167cbe66eb1b
i10416 <ito.yo16uh90616@gmail.com>
2023-10-22 17:03:07 +0900

    maybefix: update semanticdb expected output

--- tests/semanticdb/expect/StructuralTypes.expect.scala
+++ tests/semanticdb/expect/StructuralTypes.expect.scala
@@ -16,7 +16,7 @@ object StructuralTypes/*<-example::StructuralTypes.*/:
 
   val V/*<-example::StructuralTypes.V.*/: Object/*->java::lang::Object#*/ {
     def scalameta/*<-local4*/: String/*->scala::Predef.String#*/
-  } = /*<-local6*/new:
-    def scalameta/*<-local5*/ = "4.0"
+  } = new:
+    /*<-local6*/def scalameta/*<-local5*/ = "4.0"
   V/*->example::StructuralTypes.V.*/.scalameta/*->scala::reflect::Selectable#selectDynamic().*/
 end StructuralTypes/*->example::StructuralTypes.*/
