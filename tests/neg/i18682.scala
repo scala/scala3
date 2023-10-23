279efea5c95a04f6b5069c74304a920c96d4c1f5
odersky <odersky@gmail.com>
2023-10-23 11:31:55 +0200

    Improvements to "did you mean ...?" scheme
     1. Only show accessible members
     2. Show several alternatives if they are at same distance. Unlike Scala 2,
        we do not show alternatives at larger distance. I fear that would produce
        more noise than signal.
    
    Fixes #17067
    
    [Cherry-picked 5ce1ac9a72b21e7a09ad88055ef24a83a0bd3b9d]

--- tests/neg/i18682.scala
+++ tests/neg/i18682.scala
@@ -8,9 +8,24 @@ val _ = hellx // error
 object Bar:
   class Baz()
   object App
+  def cool = 1
+  def wool = 2
+  def pool = 3
 
 val bar = Bar
-val _ = bar.Bap // error, App does shown as hint, too far away
+val _ = bar.Bap // error, App does not show as hint, too far away
 val _ = bar.Bap() // error
 
 val _ = error // error, java.lang.Error does not show as hint, since it is not a value
+
+// #17067
+val _ = "123".view.reverse.padTo(5, '0').iterator.reverse // error, no hint since `reversed` is not accessible
+
+val cool = "cool"
+val wool = "wool"
+
+val _ = pool // error
+
+val _ = bar.poodle // error
+
+val _ = bar.ool // error
