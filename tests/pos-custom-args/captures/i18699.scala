041f9ceaf4edf02dbf197d6e636eeed2f09a6693
odersky <odersky@gmail.com>
2023-10-29 17:55:09 +0100

    Make printing of capture sets work also when testing Pickler

--- tests/pos-custom-args/captures/i18699.scala
+++
@@ -1,7 +0,0 @@
-import language.experimental.captureChecking
-trait Cap:
-  def use: Int = 42
-
-def test2(cs: List[Cap^]): Unit =
-  val t0: Cap^{cap[test2]} = cs.head  // error
-  var t1: Cap^{cap[test2]} = cs.head  // error
