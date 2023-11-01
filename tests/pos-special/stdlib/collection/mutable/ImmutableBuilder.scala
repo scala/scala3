20f3a97dd68b0e7bd528a6bb764f0b6b421464c8
odersky <odersky@gmail.com>
2023-11-01 16:30:26 +0100

    Add LongMap + friends to stdlib

---
+++ tests/pos-special/stdlib/collection/mutable/ImmutableBuilder.scala
@@ -0,0 +1,32 @@
+/*
+ * Scala (https://www.scala-lang.org)
+ *
+ * Copyright EPFL and Lightbend, Inc.
+ *
+ * Licensed under Apache License 2.0
+ * (http://www.apache.org/licenses/LICENSE-2.0).
+ *
+ * See the NOTICE file distributed with this work for
+ * additional information regarding copyright ownership.
+ */
+
+package scala
+package collection
+package mutable
+import language.experimental.captureChecking
+
+
+/**
+  * Reusable builder for immutable collections
+  */
+abstract class ImmutableBuilder[-A, C <: IterableOnce[_]](empty: C)
+  extends ReusableBuilder[A, C] {
+
+  protected var elems: C = empty
+
+  def clear(): Unit = { elems = empty }
+
+  def result(): C = elems
+
+  override def knownSize: Int = elems.knownSize
+}
