2c582c3aac703b288c15e22b2fca7d58bd69ca50
EnzeXing <enzexing12@gmail.com>
2023-11-08 16:35:35 -0500

    Update failing minimized test

--- tests/init-global/neg/i18628.scala
+++ tests/init-global/neg/i18628.scala
@@ -2,6 +2,6 @@ object Test:
   class Box(val x: Int)
 
   def recur(a: => Box, b: => Box): Int =
-    a.x + recur(a, b) + b.x // error
+    a.x + recur(a, b) + b.x // error // error
 
   recur(Box(1), Box(2))
--- tests/init-global/neg/i18628_3.scala
+++ tests/init-global/neg/i18628_3.scala
@@ -4,6 +4,6 @@ object Test:
   class Box(val x: Int)
 
   def recur(a: => Box, b: => Box): Int =
-    a.x + recur(a: @widen(5), b: @widen(5)) + b.x // error
+    a.x + recur(a: @widen(5), b: @widen(5)) + b.x // error // error
 
   recur(Box(1), Box(2))
