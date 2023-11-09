a1d705993a8f6416581a1bd69db091f237396c47
Yichen Xu <yichen.x@outlook.com>
2023-11-09 13:55:31 +0100

    Polishments

--- tests/pos-custom-args/captures/cc-setup-impure-classes.scala
+++ tests/pos-custom-args/captures/cc-setup-impure-classes.scala
@@ -2,4 +2,5 @@ import language.experimental.captureChecking
 
 trait Resource
 def id[X](x: X): x.type = x
-def foo[M <: Resource](r: M^): Unit = id(r)
+def foo[M <: Resource](r: M^): Unit = id(r)  // was error, should be ok
+def bar[M](r: M^): Unit = id(r)  // ok
