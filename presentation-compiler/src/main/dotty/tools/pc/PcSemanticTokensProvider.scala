b474209fdf6c56fa6d0e8b0d672541a6463f2f2e
Jakub Ciesluk <323892@uwr.edu.pl>
2023-11-28 11:04:39 +0100

    presentation compiler: Show semantic tokens for vals from predef as classes

--- presentation-compiler/src/main/dotty/tools/pc/PcSemanticTokensProvider.scala
+++ presentation-compiler/src/main/dotty/tools/pc/PcSemanticTokensProvider.scala
@@ -129,7 +129,7 @@ final class PcSemanticTokensProvider(
         if sym.isGetter | sym.isSetter then
           getTypeId(SemanticTokenTypes.Variable)
         else getTypeId(SemanticTokenTypes.Method) // "def"
-      else if isPredefClass(sym) then
+      else if sym.isTerm && sym.info.typeSymbol.is(Flags.Module)  then
         getTypeId(SemanticTokenTypes.Class) // "class"
       else if sym.isTerm &&
         (!sym.is(Flags.Param) || sym.is(Flags.ParamAccessor))
@@ -150,8 +150,5 @@ final class PcSemanticTokensProvider(
 
     TokenNode(pos.start, pos.`end`, typ, mod)
   end makeNode
-
-  def isPredefClass(sym: Symbol)(using Context) =
-    sym.is(Flags.Method) && sym.info.resultType.typeSymbol.is(Flags.Module)
 
 end PcSemanticTokensProvider
