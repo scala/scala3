86f39ea2f27eb587cdf2b7cb4cecfe4019b29884
Hamza Remmal <56235032+hamzaremmal@users.noreply.github.com>
2023-12-05 18:38:44 +0100

    Warn about unchecked type tests in primitive catch cases
    [Cherry-picked dfacadd04160f242b5bedd5cc4430eaf446ebf6a]

---
+++ tests/pos/i19013-a.scala
@@ -0,0 +1,9 @@
+//> using options -Xfatal-warnings
+
+def handle[E <: Exception](f: => Unit): Option[E] =
+  try
+    f
+    None
+  catch case e: E @unchecked => Some(e)
+
+val r: RuntimeException = handle[RuntimeException](throw new Exception()).get
