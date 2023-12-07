f87a634a6e88777dad6b808d2813289d703330c8
Dale Wijnand <dale.wijnand@gmail.com>
2023-12-07 14:02:33 +0000

    Fix provablyDisjoint handling of HK types
    If refineUsingParent/instantiateToSubType is passed a HK then it's
    not possible to instantiate a class to that type (at the moment and
    perhaps ever).  So it's important we guard against that.
    
    This came up while trying to see if Mark[?] and Foo[Int] (from
    pos/i19031.ci-reg1.scala) are provably disjoint - which they should be
    reported not to be.  Because they're not applied types of the same type
    constructor we end up trying to prove that HK type Mark is disjoint from
    HK type Foo.  Because we don't know how to instantiate Foo's subclasses
    (e.g Bar2) such that it's a subtype of higher-kinded type "Mark", we end
    up discarding all of Foo's subclasses, which implies that Foo & Mark is
    uninhabited, thus they are provably disjoint - which is incorrect.
    
    We originally didn't encounter this because we eagerly decomposed in
    Space intersection, while now we've dispatched it to provablyDisjoint.
    
    (edit) We allow for some kindness in provablyDisjoint.
    
    [Cherry-picked 7e472945c7c186140431675fb5b6d46712f1e1ae]

---
+++ tests/pos/i19031.ci-reg1.scala
@@ -0,0 +1,12 @@
+//> using options -Werror
+
+sealed trait Mark[T]
+
+trait Foo[T]
+class Bar1[T] extends Foo[T]
+class Bar2[T] extends Foo[T] with Mark[T]
+
+class Test:
+  def t1(foo: Foo[Int]): Unit = foo match
+    case _: Mark[t] =>
+    case _          =>
