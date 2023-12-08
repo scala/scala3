8d29ccfa3e8c76a8ccc1df0b8415b41d8d4dc864
odersky <odersky@gmail.com>
2023-12-08 11:47:38 +0100

    Fix pattern matching for get matches

--- tests/run/named-patterns.scala
+++ tests/run/named-patterns.scala
@@ -6,6 +6,10 @@ object Test1:
   object Person:
     def unapply(p: Person): (name: String, age: Int) = (p.name, p.age)
 
+  class Person2(val name: String, val age: Int)
+  object Person2:
+    def unapply(p: Person2): Option[(name: String, age: Int)] = Some((p.name, p.age))
+
   case class Address(city: String, zip: Int, street: String, number: Int)
 
   @main def Test =
@@ -20,6 +24,18 @@ object Test1:
       case Person(age = a, name = n) => println(s"age $a, name $n")
     bob match
       case Person(age, name) => println(s"$age, $name")
+
+    val bob2 = Person2("Bob", 22)
+    bob2 match
+      case Person2(name = n, age = a) => println(s"name $n, age $a")
+    bob2 match
+      case Person2(name = n) => println(s"name $n")
+    bob2 match
+      case Person2(age = a) => println(s"age $a")
+    bob2 match
+      case Person2(age = a, name = n) => println(s"age $a, name $n")
+    bob2 match
+      case Person2(age, name) => println(s"$age, $name")
 
     val addr = Address("Lausanne", 1003, "Rue de la Gare", 44)
     addr match
@@ -37,7 +53,3 @@ object Test1:
     addr match
       case Address(c, z, s, number) =>
         println(s"$z $c, $s $number")
-
-
-
-
