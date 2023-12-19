6b621a3bb4770801dab06a140636f72df2bf2faa
odersky <odersky@gmail.com>
2023-12-19 13:38:33 +0100

    Swap two givens in Specs2 to satisfy new restriuction

--- tests/neg/i7294-a.scala
+++ tests/neg/i7294-a.scala
@@ -4,8 +4,10 @@ package foo
 
 trait Foo { def g(x: Int): Any }
 
-inline given f[T <: Foo]: T = ??? match {
-  case x: T => x.g(10) // error // error
-}
+object Test:
 
-@main def Test = f
+  inline given f[T <: Foo]: T = ??? match {
+    case x: T => x.g(10) // error // error
+  }
+
+  @main def Test = f
