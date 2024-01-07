ab7a521bd7e196d0056885318e751af5811eaa09
odersky <odersky@gmail.com>
2024-01-07 23:10:11 +0100

    Fix typing of generated context bound refinements
    Avoid accidental binding to self
    
    # Conflicts:
    #	compiler/src/dotty/tools/dotc/typer/Typer.scala

--- tests/pos/hylolib-extract.scala
+++ tests/pos/hylolib-extract.scala
@@ -1,17 +1,25 @@
 //> using options -language:experimental.modularity -source future
 package hylotest
 
-trait Value[Self]
+trait Value:
+  type Self
+  extension (self: Self) def eq(other: Self): Boolean
 
 /** A collection of elements accessible by their position. */
-trait Collection[Self]:
+trait Collection:
+  type Self
 
   /** The type of the elements in the collection. */
   type Element: Value
 
 class BitArray
 
-given Value[Boolean] {}
+given Boolean is Value:
+  extension (self: Self) def eq(other: Self): Boolean =
+    self == other
 
-given Collection[BitArray] with
+given BitArray is Collection:
   type Element = Boolean
+
+extension [Self: Value](self: Self)
+  def neq(other: Self): Boolean = !self.eq(other)
