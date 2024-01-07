9e5a6be934611808da44e2b53cee16dbb5bc7d2b
odersky <odersky@gmail.com>
2024-01-07 21:30:17 +0100

    Allow types in given definitions to be infix types
    A type implemented in a given definition can now be an infix type, without
    enclosing parens being necessary.
    
    By contrast, it cannot anymore be a refined type. Refined types have to be
    enclosed in parens. This second point aligns the dotty parser with the
    published syntax and the scala meta parser.

--- tests/pos/typeclasses-this.scala
+++ tests/pos/typeclasses-this.scala
@@ -39,7 +39,7 @@ end Common
 
 object Instances extends Common:
 
-  given intOrd: (Int is Ord) with
+  given intOrd: Int is Ord with
     extension (x: Int)
       def compareTo(y: Int) =
         if x < y then -1
@@ -47,7 +47,7 @@ object Instances extends Common:
         else 0
 
 //  given [T](using tracked val ev: Ord { type This = T}): Ord { type This = List[T] } with
-  given [T: Ord]: (List[T] is Ord) with
+  given [T: Ord]: List[T] is Ord with
     extension (xs: List[T]) def compareTo(ys: List[T]): Int = (xs, ys) match
       case (Nil, Nil) => 0
       case (Nil, _) => -1
@@ -56,7 +56,7 @@ object Instances extends Common:
         val fst = x.compareTo(y)
         if (fst != 0) fst else xs1.compareTo(ys1)
 
-  given listMonad: (List is Monad) with
+  given listMonad: List is Monad with
     extension [A](xs: List[A]) def flatMap[B](f: A => List[B]): List[B] =
       xs.flatMap(f)
     def pure[A](x: A): List[A] =
@@ -64,7 +64,7 @@ object Instances extends Common:
 
   type Reader[Ctx] = [X] =>> Ctx => X
 
-  given readerMonad[Ctx]: (Reader[Ctx] is Monad) with
+  given readerMonad[Ctx]: Reader[Ctx] is Monad with
     extension [A](r: Ctx => A) def flatMap[B](f: A => Ctx => B): Ctx => B =
       ctx => f(r(ctx))(ctx)
     def pure[A](x: A): Ctx => A =
@@ -86,7 +86,7 @@ object Instances extends Common:
   def maximum[T: Ord](xs: List[T]): T =
     xs.reduce(_ `max` _)
 
-  given descending[T: Ord]: (T is Ord) with
+  given descending[T: Ord]: T is Ord with
     extension (x: T) def compareTo(y: T) = T.compareTo(y)(x)
 
   def minimum[T: Ord](xs: List[T]) =
@@ -126,7 +126,7 @@ class Sheep(val name: String):
       println(s"$name gets a haircut!")
       isNaked = true
 
-given (Sheep is Animal) with
+given Sheep is Animal with
   def apply(name: String) = Sheep(name)
   extension (self: This)
     def name: String = self.name
