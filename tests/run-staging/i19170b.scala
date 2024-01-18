a7f514a5d92aaf1f46ab867a745eb32370291e46
Nicolas Stucki <nicolas.stucki@gmail.com>
2024-01-18 13:26:30 +0100

    Disable i19170b on Windows

--- tests/run-staging/i19170b.scala
+++ tests/run-staging/i19170b.scala
@@ -8,9 +8,10 @@ class A(i: Int)
 def f(i: Expr[Int])(using Quotes): Expr[A] = { '{ new A($i) } }
 
 @main def Test = {
-  try
-    val g: Int => A = staging.run { '{ (i: Int) => ${ f('{i}) } } }
-    println(g(3))
-  catch case ex: Exception =>
-    assert(ex.getMessage().startsWith("`scala.quoted.staging.run` failed to load a class."))
+  if !System.getProperty("os.name").contains("Windows") then
+    try
+      val g: Int => A = staging.run { '{ (i: Int) => ${ f('{i}) } } }
+      println(g(3))
+    catch case ex: Exception =>
+      assert(ex.getMessage().startsWith("`scala.quoted.staging.run` failed to load a class."), ex.getMessage())
 }
