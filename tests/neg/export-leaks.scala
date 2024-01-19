6aadd94b6fbfafc160489f6d81a6cbcc7b08a51b
Dale Wijnand <dale.wijnand@gmail.com>
2024-01-19 14:53:11 +0000

    Alternatively, make export method synthetic & handle that

--- tests/neg/export-leaks.scala
+++ tests/neg/export-leaks.scala
@@ -6,10 +6,20 @@ object Signature {
   object O1 {
     private[Signature] def bar: T = ???
   }
-  export O1._ // error: non-private method bar refers to private type T
+  export O1._
 
   object O2 {
     private[Signature] val foo: T = ???
   }
-  export O2._ // error: non-private method foo refers to private type T
+  export O2._
+
+  object PosTest:
+    def main(args: Array[String]): Unit =
+      val t1 = bar // ok
+      val t2 = foo // ok
 }
+
+object Test:
+  def main(args: Array[String]): Unit =
+    val t1 = bar // error: Not found: bar
+    val t2 = foo // error: Not found: foo
