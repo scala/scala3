f20ef43aa31c291ac8e852bb74788e2279c28e51
i10416 <ito.yo16uh90616@gmail.com>
2024-01-22 00:04:52 +0900

    fix(#15784): ident rule for pat match  was too strict
    close https://github.com/lampepfl/dotty/issues/15784
    
    Scala 2 allows backticked identifier and capital identifier in pattern
    match, but Scala 3 mistakenly prohibited them.
    
    For example, the following code is valid in Scala 2,
    
    ```scala
    List(42) match {
      case List(_, Rest @ _*) => Rest
      case List(_, `Rest` @ _*) => `Rest`
      _ => ???
    }
    ```
    whereas it resulted in `Not Found Rest` error in Scala 3.
    
    This is because the condition to detect wildcard pattern was so strict
    that it chose the wrong match arm; `case _ => ifExpr`.
    
    [Cherry-picked e236cdd7766263da8e8633beaf46bb24bc85229d]

---
+++ tests/pos/i15784.scala
@@ -0,0 +1,8 @@
+def i15784 = List(42) match
+      case List(_, rest @ _*) => rest
+      case List(_, Rest @ _*) => Rest
+      case List(_, `Rest` @ _*) => Rest
+      case _ => ???
+
+def i15784_auxiliary = 42 match
+      case `type` : Int => `type`
