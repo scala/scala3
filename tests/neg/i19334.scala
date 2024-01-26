2972573a7a5277ef22729ec76d852d9215a121b6
odersky <odersky@gmail.com>
2024-01-26 13:52:12 +0100

    Another bad test, this time add a spurious // error

--- tests/neg/i19334.scala
+++ tests/neg/i19334.scala
@@ -2,5 +2,5 @@
 def foo[T](f: T): T = ???
 
 @main def main = foo:
-  def f() = ()
+  def f() = () // error !!! spurious error marker
   f(_) // error was OOM formatting TypeVar(TypeParamRef(T)) when offering explanations
