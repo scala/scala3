3581bfb69a95c690dcf65fe42f946d3d2c269726
aherlihy <herlihyap@gmail.com>
2024-02-02 17:28:49 +0100

    Fix purity check for val inside of object
    Fixes #17317
    
    [Cherry-picked ea3835e4db798913cc20911ca2c4521e0228e3d5]

---
+++ tests/run/i17317.scala
@@ -0,0 +1,12 @@
+package object foo {
+  object HelloGen {
+    println("hello world")
+  }
+  val Hello = HelloGen
+}
+
+import foo.Hello
+
+object Test {
+  def main(args: Array[String]): Unit = Hello: Unit
+}
