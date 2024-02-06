4c36dc180031ae900b1f1f155e6f6875eb7b060f
Yilin Wei <yilin@kebab-ca.se>
2024-02-06 16:27:20 +0000

    Adding extra passing case.

--- tests/pos/byname-inline.scala
+++ tests/pos/byname-inline.scala
@@ -1,11 +1,23 @@
 final class ByName1(val x: Int) extends AnyVal:
-  inline def isEmpty: Boolean = false
+  inline def isEmpty: Boolean = x == 1
   inline def get: String = x.toString
 
 object ByName1:
   inline def unapply(x: Int): ByName1 = new ByName1(x)
 
-def useByNamePatMatch: String =
+def useByName1PatMatch: String =
   val x = 1
   x match
     case ByName1(s) => s
+
+final class ByName2(val x: Int) extends AnyVal:
+  inline def isEmpty: false = false
+  inline def get: Int = x
+
+object ByName2:
+  inline def unapply(x: Int): ByName2 = new ByName2(x)
+
+def useByName2PatMatch: Int =
+  val x = 1
+  x match
+    case ByName2(s) => s
