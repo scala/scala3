406c039ef35dbfb9e0ea0708e47ef2f313c47588
Yilin Wei <yilin@kebab-ca.se>
2024-02-07 23:01:06 +0000

    Add 0 arity test

--- tests/run-java16+/java-records/Test.scala
+++ tests/run-java16+/java-records/Test.scala
@@ -1,10 +1,14 @@
 // scalajs: --skip
 
-object Test {
-  def main(args: Array[String]): Unit = {
+object Test:
+  def main(args: Array[String]): Unit =
+    val r0 = R0()
+    r0 match
+      case R0() =>
+
     val r1 = R1(42)
-    r1 match {
+    r1 match
       case R1(i) => assert(i == 42)
-    }
-  }
-}
+
+    val R1(i) = r1
+    assert(i == 42)
