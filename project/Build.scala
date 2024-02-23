3ad3acba11552e638a91889b8ff48385b624db08
Nicolas Stucki <nicolas.stucki@gmail.com>
2024-02-23 08:55:25 +0100

    Update reference compiler to 3.4.0

--- project/Build.scala
+++ project/Build.scala
@@ -83,7 +83,7 @@ object DottyJSPlugin extends AutoPlugin {
 object Build {
   import ScaladocConfigs._
 
-  val referenceVersion = "3.3.1"
+  val referenceVersion = "3.4.0"
 
   val baseVersion = "3.4.2-RC1"
 
