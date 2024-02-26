a62b4139c228761d8071885e890aa2b182864850
oronpo <oron.port@cornell.edu>
2024-02-26 22:43:32 +0200

    implement custom show with annotation

---
+++ tests/neg/custom-show.scala
@@ -0,0 +1,30 @@
+import scala.compiletime.{CustomShow, hasCustomShow}
+object Regular:
+  trait Foo[T]
+  type Baz = Foo["To Infinity And Beyond!"]
+  val x: Foo["To Infinity And Beyond!"] = 1 // error
+
+object Custom1:
+  @hasCustomShow
+  trait Foo[T]
+  type Baz = Foo["To Infinity And Beyond!"]
+  object Foo:
+    given CustomShow[Baz] with {
+      type Out = "Baz"
+    }
+  val x: Foo["To Infinity And Beyond!"] = 1 // error
+
+object Custom2:
+  @hasCustomShow
+  opaque type Foo[T] = 0
+  type Baz = Foo["To Infinity And Beyond!"]
+  given CustomShow[Baz] with {
+    type Out = "Baz"
+  }
+  val x: Foo["To Infinity And Beyond!"] = 1 // error
+
+object CustomMissingImplicit:
+  @hasCustomShow
+  trait Foo[T]
+  type Baz = Foo["To Infinity And Beyond!"]
+  val x: Foo["To Infinity And Beyond!"] = 1 // error
