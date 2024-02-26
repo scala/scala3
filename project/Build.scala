15c3f9cfdd98f6351797e8c2ff8b092279e7ebab
Hamza REMMAL <hamza@remmal.net>
2024-02-26 14:21:37 +0100

    Bump JLine to 3.24.1-> 3.25.1
    [Cherry-picked adc2205606bdcff6800a118d8edfe3700b8be439]

--- project/Build.scala
+++ project/Build.scala
@@ -579,9 +579,9 @@ object Build {
       libraryDependencies ++= Seq(
         "org.scala-lang.modules" % "scala-asm" % "9.6.0-scala-1", // used by the backend
         Dependencies.compilerInterface,
-        "org.jline" % "jline-reader" % "3.24.1",   // used by the REPL
-        "org.jline" % "jline-terminal" % "3.24.1",
-        "org.jline" % "jline-terminal-jna" % "3.24.1", // needed for Windows
+        "org.jline" % "jline-reader" % "3.25.1",   // used by the REPL
+        "org.jline" % "jline-terminal" % "3.25.1",
+        "org.jline" % "jline-terminal-jna" % "3.25.1", // needed for Windows
         ("io.get-coursier" %% "coursier" % "2.0.16" % Test).cross(CrossVersion.for3Use2_13),
       ),
 
