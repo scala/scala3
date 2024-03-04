27f020171f34d42fc08b0fc19481ef4f412a20ef
Hamza REMMAL <hamza@remmal.dev>
2024-03-04 12:33:52 +0100

    Add test for #19842
    [Cherry-picked 96e1d4eec8b879e26c120a4a59d28ddaf35ac035]

---
+++ tests/neg-macros/i19842/Macro.scala
@@ -0,0 +1,29 @@
+
+import scala.annotation.{experimental, targetName}
+import scala.quoted.*
+import scala.util.Try
+
+object Macros {
+  def makeSerializer[T: Type](using Quotes): Expr[Serializer[T]] = {
+    import quotes.reflect.*
+
+    val tpe: TypeRepr = TypeRepr.of[T]
+    val name: String = Symbol.freshName("objectSerializer")
+
+    val modSym: Symbol = Symbol.newModule(
+      Symbol.spliceOwner,
+      name,
+      Flags.Implicit,
+      Flags.EmptyFlags,
+      // Without TypeRep.of[Object] it would fail with java.lang.AssertionError: assertion failed: First parent must be a class
+      List(TypeRepr.of[Object], TypeRepr.of[Serializer[T]]),
+      _ => Nil,
+      Symbol.noSymbol
+    )
+
+    val (modValDef: ValDef, modClassDef: ClassDef) =
+      ClassDef.module(modSym, List(TypeTree.of[Serializer[T]]), Nil)
+
+    Block(List(modValDef, modClassDef), Ref(modSym)).asExprOf[Serializer[T]]
+  }
+}
