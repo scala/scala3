49e7b4c8f512a21d4fdef890f13b50965324af9a
Jamie Thompson <bishbashboshjt@gmail.com>
2024-03-13 16:17:04 +0100

    Partial revert of previous commit.
    Instead of avoiding fully qualified names,
    use a different separator in zincMangledName.
    
    [Cherry-picked 0f7de67245a8519842f2ef25b985fe967a24d561]

--- compiler/src/dotty/tools/dotc/sbt/package.scala
+++ compiler/src/dotty/tools/dotc/sbt/package.scala
@@ -4,6 +4,7 @@ import dotty.tools.dotc.core.Contexts.Context
 import dotty.tools.dotc.core.Symbols.Symbol
 import dotty.tools.dotc.core.NameOps.stripModuleClassSuffix
 import dotty.tools.dotc.core.Names.Name
+import dotty.tools.dotc.core.Names.termName
 
 inline val TermNameHash = 1987 // 300th prime
 inline val TypeNameHash = 1993 // 301st prime
@@ -11,10 +12,15 @@ inline val InlineParamHash = 1997 // 302nd prime
 
 extension (sym: Symbol)
 
-  def constructorName(using Context) =
-    sym.owner.name ++ ";init;"
-
-  /** Mangle a JVM symbol name in a format better suited for internal uses by sbt. */
-  def zincMangledName(using Context): Name =
-    if (sym.isConstructor) constructorName
-    else sym.name.stripModuleClassSuffix
+  /** Mangle a JVM symbol name in a format better suited for internal uses by sbt.
+   *  WARNING: output must not be written to TASTy, as it is not a valid TASTy name.
+   */
+  private[sbt] def zincMangledName(using Context): Name =
+    if sym.isConstructor then
+      // TODO: ideally we should avoid unnecessarily caching these Zinc specific
+      // names in the global chars array. But we would need to restructure
+      // ExtractDependencies caches to avoid expensive `toString` on
+      // each member reference.
+      termName(sym.owner.fullName.mangledString.replace(".", ";").nn ++ ";init;")
+    else
+      sym.name.stripModuleClassSuffix
