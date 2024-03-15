b4949f719628d89520c94b3c7702f57f5a0f6c4d
Eugene Flesselle <eugene@flesselle.net>
2024-03-15 13:17:42 +0100

    Update check-files and remove i18488.scala
    i18488.scala was only passing because of the bug in the MatchReducer,
    as we can see in the subtyping trace:
    ```
    ==> isSubType TableQuery[BaseCrudRepository.this.EntityTable] <:< Query[BaseCrudRepository.this.EntityTable, E[Option]]?
      ==> isSubType Query[BaseCrudRepository.this.EntityTable, Extract[BaseCrudRepository.this.EntityTable]] <:<
                    Query[BaseCrudRepository.this.EntityTable, E[Option]] (left is approximated)?
        ==> isSubType E[Option] <:< Extract[BaseCrudRepository.this.EntityTable]?
          ==> isSubType [T[_$1]] =>> Any <:< Extract?
            ==> isSubType Any <:< Extract[T]?
              ==> isSubType Any <:< T match { case AbstractTable[t] => t } <: t (right is approximated)?
                ==> isSubType Any <:< <error Match type reduction failed since selector T
                                       matches none of the cases
                                       case AbstractTable[t] => t> (right is approximated)?
                <== isSubType Any <:< <error Match type reduction failed since selector T
                                       matches none of the cases
                                       case AbstractTable[t] => t> (right is approximated) = true
              <== isSubType Any <:< T match { case AbstractTable[t] => t } <: t (right is approximated) = true
            <== isSubType Any <:< Extract[T] = true
          <== isSubType [T[_$1]] =>> Any <:< Extract = true
          ...
        <== isSubType Extract[BaseCrudRepository.this.EntityTable] <:< E[Option] = true
      <== isSubType Query[BaseCrudRepository.this.EntityTable, Extract[BaseCrudRepository.this.EntityTable]] <:<
                    Query[BaseCrudRepository.this.EntityTable, E[Option]] (left is approximated) = true
    <== isSubType TableQuery[BaseCrudRepository.this.EntityTable] <:< Query[BaseCrudRepository.this.EntityTable, E[Option]] = true
    ```
    
    [Cherry-picked 5becaace6495a8e1af69951d9ffef54406f722d1]

--- tests/neg/i13757-match-type-anykind.scala
+++ tests/neg/i13757-match-type-anykind.scala
@@ -1,11 +1,11 @@
 object Test:
-  type AnyKindMatchType1[X <: AnyKind] = X match // error: the scrutinee of a match type cannot be higher-kinded // error
+  type AnyKindMatchType1[X <: AnyKind] = X match // error: the scrutinee of a match type cannot be higher-kinded
     case Option[a] => Int
 
   type AnyKindMatchType2[X <: AnyKind] = X match // error: the scrutinee of a match type cannot be higher-kinded
     case Option => Int // error: Missing type parameter for Option
 
-  type AnyKindMatchType3[X <: AnyKind] = X match // error: the scrutinee of a match type cannot be higher-kinded // error
+  type AnyKindMatchType3[X <: AnyKind] = X match // error: the scrutinee of a match type cannot be higher-kinded
     case _ => Int
 
   type AnyKindMatchType4[X <: Option] = X match // error // error: the scrutinee of a match type cannot be higher-kinded // error
