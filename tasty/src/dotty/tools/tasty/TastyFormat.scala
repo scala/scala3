4029577068587ddc523e84fa39849446f83c0ea6
Paweł Marks <pmarks@virtuslab.com>
2024-03-28 12:10:26 +0100

    Release 3.4.2-RC1

--- tasty/src/dotty/tools/tasty/TastyFormat.scala
+++ tasty/src/dotty/tools/tasty/TastyFormat.scala
@@ -318,7 +318,7 @@ object TastyFormat {
    *  compatibility, but remains backwards compatible, with all
    *  preceding `MinorVersion`.
    */
-  final val MinorVersion: Int = 5
+  final val MinorVersion: Int = 4
 
   /** Natural Number. The `ExperimentalVersion` allows for
    *  experimentation with changes to TASTy without committing
@@ -334,7 +334,7 @@ object TastyFormat {
    *  is able to read final TASTy documents if the file's
    *  `MinorVersion` is strictly less than the current value.
    */
-  final val ExperimentalVersion: Int = 1
+  final val ExperimentalVersion: Int = 0
 
   /**This method implements a binary relation (`<:<`) between two TASTy versions.
    *
