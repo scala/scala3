f2b7c12e6941b472086debef420e4eb3578c178a
odersky <odersky@gmail.com>
2024-03-31 11:21:32 +0200

    Plug unsoundness for reach capabilities
    Enforce an analogous restriction to the one for creating reach capabilities
    for all values. The type of a value cannot both have a reach capability with variance
    >= 0 and at the same time a universal capability with variance <= 0.
    Plug soundness hole for reach capabilities

---
+++ tests/neg/unsound-reach-3.scala
@@ -0,0 +1,21 @@
+import language.experimental.captureChecking
+trait File:
+  def close(): Unit
+
+def withFile[R](path: String)(op: File^ => R): R = ???
+
+trait Foo[+X]:
+  def use(x: File^): X
+class Bar extends Foo[File^]:
+  def use(x: File^): File^ = x
+
+def bad(): Unit =
+  val backdoor: Foo[File^] = new Bar
+  val boom: Foo[File^{backdoor*}] = backdoor
+
+  var escaped: File^{backdoor*} = null
+  withFile("hello.txt"): f =>
+    escaped = boom.use(f) // error
+      // boom.use: (x: File^) -> File^{backdoor*}, it is a selection so reach capabilities are allowed
+      // f: File^, so there is no reach capabilities
+
