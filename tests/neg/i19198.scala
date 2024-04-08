f73c99068d3cf32080f3e9d2fe8f8e172c5efe17
Guillaume Martres <smarter@ubuntu.com>
2024-04-08 20:43:52 +0200

    Do not assume the superType of a match type gives us a sound mirror
    [Cherry-picked 4699140dcdc76e00d3a85f3eeab25d843b938e5a]

--- tests/neg/i19198.scala
+++ tests/neg/i19198.scala
@@ -4,6 +4,10 @@ import compiletime.summonInline
 type DoesNotReduce[T] = T match
   case String => Any
 
+type DoesNotReduce2[T] <: T = T match
+  case String => T
+
 class Foo
 @main def Test: Unit =
   summonInline[Mirror.Of[DoesNotReduce[Option[Int]]]] // error
+  summonInline[Mirror.Of[DoesNotReduce2[Option[Int]]]] // error
