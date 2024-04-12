c66be47d86b6675143086f16951cf9f9a7a71bbd
Eugene Flesselle <eugene@flesselle.net>
2024-04-12 11:12:58 +0200

    Allow SAM types to contain multiple refinements

--- tests/run/i18315.scala
+++ tests/run/i18315.scala
@@ -7,9 +7,16 @@ trait Sam2:
   type T
   def apply(x: T): T
 
+trait Sam3:
+  type T
+  type U
+  def apply(x: T): U
+
 object Test:
   def main(args: Array[String]): Unit =
     val s1: Sam1 { type T = String } = x => x.trim
     s1.apply("foo")
     val s2: Sam2 { type T = Int } = x => x + 1
     s2.apply(1)
+    val s3: Sam3 { type T = Int; type U = String } = x => x.toString
+    s3.apply(2)
