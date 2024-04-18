7539fb51b9897816013262b54bd9e98114dcd24c
odersky <odersky@gmail.com>
2024-04-18 13:40:56 +0200

    Piggy-back some unrelated tests

---
+++ tests/pos/into-bigint.scala
@@ -0,0 +1,21 @@
+import language.experimental.into
+
+class BigInt(x: Int):
+  def + (other: into BigInt): BigInt = ???
+  def * (other: into BigInt): BigInt = ???
+
+object BigInt:
+  given Conversion[Int, BigInt] = BigInt(_)
+
+  extension (x: into BigInt)
+    def + (other: BigInt): BigInt = ???
+    def * (other: BigInt): BigInt = ???
+
+@main def Test =
+  val x = BigInt(2)
+  val y = 3
+  val a1 = x + y
+  val a2 = y * x
+  val a3 = x * x
+  val a4 = y + y
+
