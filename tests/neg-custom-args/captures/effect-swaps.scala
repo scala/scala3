6e080775f9849b6d532940b2c972bfef6ba4f00b
odersky <odersky@gmail.com>
2024-04-23 17:22:42 +0200

    Update test to show where the difference lies

--- tests/neg-custom-args/captures/effect-swaps.scala
+++ tests/neg-custom-args/captures/effect-swaps.scala
@@ -14,55 +14,57 @@ end boundary
 
 import boundary.{Label, break}
 
-class Async
+@capability trait Async
+object Async:
+  def blocking[T](body: Async ?=> T): T = ???
+
 class Future[+T]:
   this: Future[T]^ =>
-  def await(using Async^): T = ???
+  def await(using Async): T = ???
 object Future:
-  def apply[T](op: Async^ ?=> T)(using Async): Future[T]^{op} = ???
+  def apply[T](op: Async ?=> T)(using Async): Future[T]^{op} = ???
 
-abstract class Result[+T, +E]
-case class Ok[+T](value: T)  extends Result[T, Nothing]
-case class Err[+E](value: E) extends Result[Nothing, E]
+enum Result[+T, +E]:
+  case Ok[+T](value: T) extends Result[T, Nothing]
+  case Err[+E](error: E) extends Result[Nothing, E]
+
 
 object Result:
-  extension [T, E](r: Result[T, E])
+  extension [T, E](r: Result[T, E]^)(using Label[Err[E]])
 
     /** `_.ok` propagates Err to current Label */
-    inline def ok(using Label[Result[Nothing, E]]): T = r match
-      case r: Ok[_] => r.value
-      case err => break(err.asInstanceOf[Err[E]])
+    def ok: T = r match
+      case Ok(value) => value
+      case Err(value) => break[Err[E]](Err(value))
 
   transparent inline def apply[T, E](inline body: Label[Result[T, E]] ?=> T): Result[T, E] =
-    boundary:
-      val result = body
-      Ok(result)
+    boundary(Ok(body))
 
   // same as apply, but not an inline method
   def make[T, E](body: Label[Result[T, E]] ?=> T): Result[T, E] =
-    boundary:
-      val result = body
-      Ok(result)
+    boundary(Ok(body))
 
 end Result
 
 def test[T, E](using Async) =
-  val good1: List[Future[Result[T, E]]] => Future[Result[List[T], E]] = frs =>
-    Future:
-      Result:
-        frs.map(_.await.ok) // OK
-
-  val good2: Result[Future[T], E] => Future[Result[T, E]] = rf =>
-    Future:
-      Result:
-        rf.ok.await  // OK, Future argument has type Result[T]
-
-  def fail3(fr: Future[Result[T, E]]^) =
-    Result:
-      Future: // error, escaping label from Result
-        fr.await.ok
-
-  def fail4(fr: Future[Result[T, E]]^) =
-    Result.make: // error, escaping label from Result
+  import Result.*
+  Async.blocking: async ?=>
+    val good1: List[Future[Result[T, E]]] => Future[Result[List[T], E]] = frs =>
       Future:
-        fr.await.ok
+        Result:
+          frs.map(_.await.ok) // OK
+
+    val good2: Result[Future[T], E] => Future[Result[T, E]] = rf =>
+      Future:
+        Result:
+          rf.ok.await  // OK, Future argument has type Result[T]
+
+    def fail3(fr: Future[Result[T, E]]^) =
+      Result:
+        Future: // error, escaping label from Result
+          fr.await.ok
+
+    def fail4[T, E](fr: Future[Result[T, E]]^) =
+        Result.make: //lbl ?=> // error, escaping label from Result
+          Future: fut ?=>
+            fr.await.ok
