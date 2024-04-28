561b46e220f978c730f662666523926ba8bb357d
Matt Bovel <matthieu@bovel.net>
2024-04-28 18:10:23 +0200

    Remove left-over line
    [Cherry-picked dd527fc120aa6e7819b40869f7537a5c88aa657c]

--- compiler/src/dotty/tools/dotc/typer/Applications.scala
+++ compiler/src/dotty/tools/dotc/typer/Applications.scala
@@ -1100,7 +1100,6 @@ trait Applications extends Compatibility {
                               case msg: TypeMismatch =>
                                 msg.inTree match
                                   case Some(arg) if tree.args.exists(_.span == arg.span) =>
-                                    val Select(qualifier, _) = fun1: @unchecked
                                     val noteText =
                                       i"""The required type comes from a parameter of the automatically
                                          |inserted `apply` method of `${qualifier.tpe}`,
