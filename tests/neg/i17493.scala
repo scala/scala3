68c30d1efb51f745ef6994ebac55189584c431df
Yoonjae Jeon <yoonjae.jeon@devsisters.com>
2024-05-01 11:30:44 +0900

    Move the test file to tests/warn directory

--- tests/neg/i17493.scala
+++
@@ -1,5 +0,0 @@
-//> using options -explain -Xfatal-warnings
-class A(val s: String) extends AnyVal {
-  def g = synchronized { println("hello, world") } // warn
-}
-// nopos-error: No warnings can be incurred under -Werror (or -Xfatal-warnings)
