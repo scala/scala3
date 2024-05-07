9c14610802bb24031401eeca0e7641a0c84cd731
Pascal Weisenburger <pascal.weisenburger@web.de>
2024-05-07 22:07:01 +0200

    update for #19677

--- tests/pos-macros/i20353/Macro_1.scala
+++ tests/pos-macros/i20353/Macro_1.scala
@@ -16,7 +16,7 @@ end ImplicitValue
 
 @experimental
 class Test extends MacroAnnotation:
-  def transform(using Quotes)(tree: quotes.reflect.Definition) =
+  def transform(using Quotes)(definition: quotes.reflect.Definition, companion: Option[quotes.reflect.Definition]) =
     import quotes.reflect.*
     Implicits.search(TypeRepr.of[ImplicitValue])
-    List(tree)
+    List(definition)
