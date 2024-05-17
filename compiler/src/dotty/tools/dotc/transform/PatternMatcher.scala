83d46ad3c73e18517017bfc8065f6a7f912df2aa
Derek Wickern <dwickern@gmail.com>
2024-05-16 21:03:57 -0700

    replace widen with <:<
    [Cherry-picked 7279bf7cb0368b6abef68a8664e63505b22dc7ce]

--- compiler/src/dotty/tools/dotc/transform/PatternMatcher.scala
+++ compiler/src/dotty/tools/dotc/transform/PatternMatcher.scala
@@ -805,11 +805,11 @@ object PatternMatcher {
      */
     private def collectSwitchCases(scrutinee: Tree, plan: SeqPlan): List[(List[Tree], Plan)] = {
       def isSwitchableType(tpe: Type): Boolean =
-        (tpe isRef defn.IntClass) ||
-        (tpe isRef defn.ByteClass) ||
-        (tpe isRef defn.ShortClass) ||
-        (tpe isRef defn.CharClass) ||
-        (tpe isRef defn.StringClass)
+        (tpe <:< defn.IntType) ||
+        (tpe <:< defn.ByteType) ||
+        (tpe <:< defn.ShortType) ||
+        (tpe <:< defn.CharType) ||
+        (tpe <:< defn.StringType)
 
       val seen = mutable.Set[Any]()
 
@@ -859,7 +859,7 @@ object PatternMatcher {
           (Nil, plan) :: Nil
       }
 
-      if (isSwitchableType(scrutinee.tpe.widen.widenSingletons())) recur(plan)
+      if (isSwitchableType(scrutinee.tpe)) recur(plan)
       else Nil
     }
 
@@ -880,9 +880,8 @@ object PatternMatcher {
        */
 
       val (primScrutinee, scrutineeTpe) =
-        val tpe = scrutinee.tpe.widen.widenSingletons()
-        if (tpe.isRef(defn.IntClass)) (scrutinee, defn.IntType)
-        else if (tpe.isRef(defn.StringClass)) (scrutinee, defn.StringType)
+        if (scrutinee.tpe <:< defn.IntType) (scrutinee, defn.IntType)
+        else if (scrutinee.tpe <:< defn.StringType) (scrutinee, defn.StringType)
         else (scrutinee.select(nme.toInt), defn.IntType)
 
       def primLiteral(lit: Tree): Tree =
