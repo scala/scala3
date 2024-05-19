4487eebad7be7ffcbfafcfa2a4b1d6851a6c0071
odersky <odersky@gmail.com>
2024-05-19 10:36:57 +0200

    Two fixes to make tests pass as before
     - Avoid creating capture sets of untrackable references
     - Refine disallowRootCapability to consider only explicit captures

--- tests/pos-custom-args/captures/nested-classes.scala
+++ tests/pos-custom-args/captures/nested-classes.scala
@@ -2,21 +2,21 @@ import language.experimental.captureChecking
 import language.experimental.modularity
 import annotation.{capability, constructorOnly}
 
-class IO // does not work with extends caps.Capability
+class IO extends caps.Capability
 class Blah
-class Pkg(using io: IO^):
+class Pkg(using io: IO):
   class Foo:
     def m(foo: Blah^{io}) = ???
-class Pkg2(using io: IO^):
+class Pkg2(using io: IO):
   class Foo:
     def m(foo: Blah^{io}): Any = io; ???
 
-def main(using io: IO^) =
+def main(using io: IO) =
   val pkg = Pkg()
   val f = pkg.Foo()
-  f.m(???)
+  val x1 = f.m(???)
   val pkg2 = Pkg2()
   val f2 = pkg2.Foo()
-  f2.m(???)
+  val x2 = f2.m(???)
 
 
