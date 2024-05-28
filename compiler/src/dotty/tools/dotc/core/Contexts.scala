00ef3f30713a9c1b8d0df47d2bbdbf1d0cae0026
odersky <odersky@gmail.com>
2024-05-28 20:38:39 +0200

    Avoid stacked thisCall contexts
    AddImplicitArgs can recursively add several implicit parameter lists.
    We need to make sure we don't perform a thisCallContext search in another
    thisCall context in this case.
    
    Fixes #20483
    
    The original code would back out further and further in the context chain for every
    implicit parameter section on the secondary constructor. Eventually (in this case
    after 3 times) bad things happen.

--- compiler/src/dotty/tools/dotc/core/Contexts.scala
+++ compiler/src/dotty/tools/dotc/core/Contexts.scala
@@ -477,7 +477,7 @@ object Contexts {
 
     /** Is the flexible types option set? */
     def flexibleTypes: Boolean = base.settings.YexplicitNulls.value && !base.settings.YnoFlexibleTypes.value
-    
+
     /** Is the best-effort option set? */
     def isBestEffort: Boolean = base.settings.YbestEffort.value
 
