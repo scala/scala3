9ee8c5d253caa0efd6a7a77025585e928be34075
noti0na1 <noti0na1@users.noreply.github.com>
2024-06-06 14:24:15 +0200

    Fix error related to reaches

--- tests/neg/i20503.scala
+++ tests/neg/i20503.scala
@@ -1,6 +1,16 @@
 import language.experimental.captureChecking
+
+class List[+A]:
+  def head: A = ???
+  def tail: List[A] = ???
+  def map[B](f: A => B): List[B] = ???
+  def foreach[U](f: A => U): Unit = ???
+  def nonEmpty: Boolean = ???
+
 def runOps(ops: List[() => Unit]): Unit =
-  ops.foreach(op => op())
+  // See i20156, due to limitation in expressiveness of current system,
+  // we cannot map over the list of impure elements.
+  ops.foreach(op => op()) // error
 
 def main(): Unit =
   val f: List[() => Unit] -> Unit = runOps  // error
