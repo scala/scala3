9b415d8a44c675207701c1908d30fd36199c9805
odersky <odersky@gmail.com>
2024-06-06 19:47:28 +0200

    Generalize isAlwaysEmpty and streamline isBoxedCaptured
     - isBoxedCaptured no longer requires the construction of intermediate capture sets.
     - isAlwaysEmpty is also true for solved variables that have no elements

--- compiler/src/dotty/tools/dotc/cc/CaptureSet.scala
+++ compiler/src/dotty/tools/dotc/cc/CaptureSet.scala
@@ -455,7 +455,7 @@ object CaptureSet:
     var deps: Deps = emptySet
 
     def isConst = isSolved
-    def isAlwaysEmpty = false
+    def isAlwaysEmpty = isSolved && elems.isEmpty
 
     def isMaybeSet = false // overridden in BiMapped
 
