7e0bef566ad39319c1e1befbcd1ad3086559ec99
Dale Wijnand <dale.wijnand@gmail.com>
2024-06-11 21:28:25 +0100

    Fix EnclosingMethod for lifted anonfun
    The anonfun "() => new TB {.." code is lifted to a static method, in the
    original class (A), but the GenBCode logic was still returning the TA
    anon class.
    
    [Cherry-picked 5c295f1626c6361cb702fbe95bcf280cbc889990]

---
+++ tests/run/i18701.fixed.scala
@@ -0,0 +1,15 @@
+abstract class TA { def tb(): TB }
+abstract class TB { def chk(): Unit }
+class A:
+  def a(): TA =
+    new TA {
+      def tb(): TB =
+        val fn: () => TB = new Function0[TB]:
+          def apply(): TB = new TB {
+            def chk() = println(getClass.getEnclosingMethod())
+          }
+        fn()
+    }
+
+object Test:
+  def main(args: Array[String]): Unit = new A().a().tb().chk()
