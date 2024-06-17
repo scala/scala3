dddd6bcb9740322c3b0ddb14a3b1124424e476c0
odersky <odersky@gmail.com>
2024-06-17 18:08:45 +0200

    Drop checkReachCapsIsolated restriction

---
+++ tests/neg-custom-args/captures/widen-reach.scala
@@ -0,0 +1,14 @@
+import language.experimental.captureChecking
+
+trait IO
+
+trait Foo[+T]:
+  val foo: IO^ -> T
+
+trait Bar extends Foo[IO^]:
+  val foo: IO^ -> IO^ = x => x
+
+def test(x: Foo[IO^]): Unit =
+  val y1: Foo[IO^{x*}] = x
+  val y2: IO^ -> IO^ = y1.foo      // error
+  val y3: IO^ -> IO^{x*} = y1.foo  // error
