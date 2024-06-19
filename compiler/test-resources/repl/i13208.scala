12a3aabeabe3ca9ee29c14c0134feb5a1fff0854
Wojciech Mazur <wmazur@virtuslab.com>
2024-06-19 14:45:39 +0200

    use directives instead of scalac options in tests
    [Cherry-picked c16fb4e13c9fbddce0d7541f7c566955a6f65023][modified]

--- compiler/test-resources/repl/i13208.scala
+++ compiler/test-resources/repl/i13208.scala
@@ -1,4 +1,4 @@
-// scalac: -source:future -deprecation
+//> using options -source:future -deprecation
 scala> type M[X] = X match { case Int => String case _ => Int }
 scala> type N[X] = X match { case List[_] => Int }
 1 warning found
