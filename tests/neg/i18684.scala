d53cbac2778307c32e6fee8b5f01578fc26831cb
Wojciech Mazur <wmazur@virtuslab.com>
2024-06-21 19:58:23 +0200

    Better error message when a pattern match extractor is not found.
    Fixes #18684
    
    [Cherry-picked 7c308d6a7a3cef4ea804623e821250065cfae444][modified]

---
+++ tests/neg/i18684.scala
@@ -0,0 +1,12 @@
+//> using option -explain
+object Test:
+  val s(): String = "hello, world" // error
+
+  val i() = 22 // error
+
+  def foo(): String = "22"
+
+  object inner:
+    val foo() = "33" // error
+
+  val inner(x) = 3 // error // error
