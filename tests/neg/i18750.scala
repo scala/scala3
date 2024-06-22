442876b7a43ea2d8337f6f7f777e5162b57d0926
Wojciech Mazur <wmazur@virtuslab.com>
2024-06-22 15:43:15 +0200

    Better error for definition followed by keyword
    Fixes #18750
    
    [Cherry-picked dc1090290812d30975ee1ea2d7abb117136119e3][modified]

---
+++ tests/neg/i18750.scala
@@ -0,0 +1,13 @@
+//> using options -explain
+
+val do = 23 // error
+val if = 23 // error
+val val = 23 // error
+val a, if = 23 // error
+
+def do; // error
+var do; // error
+class do; // error
+object if; // error // error
+trait else; // error
+type for; // error
