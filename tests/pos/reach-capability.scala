38cf302f57b042657082cfc7525b70e9058aeaf6
odersky <odersky@gmail.com>
2024-06-27 13:40:20 +0200

    Reclassify maximal capabilities
    Don't treat user-defined capabilities deriving from caps.Capability as
    maximal. That was a vestige from when we treated capability classes natively.
    It caused code that should compile to fail because if `x extends Capability` then
    `x` could not be widened to `x*`.
    
    As a consequence we have one missed error in effect-swaps again, which re-establishes
    the original (faulty) situation.

---
+++ tests/pos/reach-capability.scala
@@ -0,0 +1,17 @@
+import language.experimental.captureChecking
+import annotation.experimental
+import caps.{Capability}
+
+@experimental object Test2:
+
+  class List[+A]:
+    def map[B](f: A => B): List[B] = ???
+
+  class Label extends Capability
+
+  class Listener
+
+  def test2(lbls: List[Label]) =
+    def makeListener(lbl: Label): Listener^{lbl} = ???
+    val listeners = lbls.map(makeListener) // should work
+
