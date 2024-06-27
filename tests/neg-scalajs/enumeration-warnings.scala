523b2c51bbe174d0a4bc09a138fe619ca05f65a5
Wojciech Mazur <wmazur@virtuslab.com>
2024-06-27 14:51:22 +0200

    Fix tests after rebase
    [Cherry-picked bfe0c702795833d7877b145b581cc84f3a39d8d4][modified]

--- tests/neg-scalajs/enumeration-warnings.scala
+++ tests/neg-scalajs/enumeration-warnings.scala
@@ -3,37 +3,35 @@
 class UnableToTransformValue extends Enumeration {
   val a = {
     println("oh, oh!")
-    Value // warn
+    Value // error
   }
   val b = {
     println("oh, oh!")
-    Value(4) // warn
+    Value(4) // error
   }
 }
 
 class ValueWithNullName extends Enumeration {
-  val a = Value(null) // warn
-  val b = Value(10, null) // warn
+  val a = Value(null) // error
+  val b = Value(10, null) // error
 }
 
 class NewValWithNoName extends Enumeration {
-  val a = new Val // warn
-  val b = new Val(10) // warn
+  val a = new Val // error
+  val b = new Val(10) // error
 }
 
 class NewValWithNullName extends Enumeration {
-  val a = new Val(null) // warn
-  val b = new Val(10, null) // warn
+  val a = new Val(null) // error
+  val b = new Val(10, null) // error
 }
 
 class ExtendsValWithNoName extends Enumeration {
-  protected class Val1 extends Val // warn
-  protected class Val2 extends Val(1) // warn
+  protected class Val1 extends Val // error
+  protected class Val2 extends Val(1) // error
 }
 
 class ExtendsValWithNullName extends Enumeration {
-  protected class Val1 extends Val(null) // warn
-  protected class Val2 extends Val(1, null) // warn
+  protected class Val1 extends Val(null) // error
+  protected class Val2 extends Val(1, null) // error
 }
-
-// nopos-error: No warnings can be incurred under -Werror.
