f4a66d4ff577520a7b4e926356346102238a2010
Florian3k <mfornal@virtuslab.com>
2024-07-01 13:51:43 +0200

    Add sources of synthetic classes to sources jar

---
+++ library-aux/src/scala/AnyKind.scala
@@ -0,0 +1,7 @@
+package scala
+
+/** The super-type of all types.
+ *
+ *   See [[https://docs.scala-lang.org/scala3/reference/other-new-features/kind-polymorphism.html]].
+ */
+final abstract class AnyKind
