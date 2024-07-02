59ab8494bcf9f4f1206b80e92a6f5116d8ad12a4
Wojciech Mazur <wmazur@virtuslab.com>
2024-07-02 23:45:47 +0200

    Fix run-macros/type-show
    [Cherry-picked db1dcdc0e11516a61c4edc25776a6ea0d5dacf78][modified]

--- tests/run-macros/type-show/Test_2.scala
+++ tests/run-macros/type-show/Test_2.scala
@@ -1,18 +1,34 @@
 
 object Test {
   import TypeToolbox.*
+
+  def assertEql[A](obt: A, exp: A): Unit =
+    assert(obt == exp, s"\nexpected: $exp\nobtained: $obt")
+
   def main(args: Array[String]): Unit = {
     val x = 5
-    assert(show[x.type] == "x.type")
-    assert(show[Nil.type] == "scala.Nil.type")
-    assert(show[Int] == "scala.Int")
-    assert(show[Int => Int] == "scala.Function1[scala.Int, scala.Int]")
-    assert(show[(Int, String)] == "scala.Tuple2[scala.Int, scala.Predef.String]")
-    assert(show[[X] =>> X match { case Int => Int }] ==
+    assertEql(show[x.type], "x.type")
+    assertEql(show[Nil.type], "scala.Nil.type")
+    assertEql(show[Int], "scala.Int")
+    assertEql(show[Int => Int], "scala.Function1[scala.Int, scala.Int]")
+    assertEql(show[(Int, String)], "scala.Tuple2[scala.Int, scala.Predef.String]")
+    assertEql(show[[X] =>> X match { case Int => Int }],
       """[X >: scala.Nothing <: scala.Any] => X match  {
         |  case scala.Int => scala.Int
         |}""".stripMargin)
-    assert(showStructure[[X] =>> X match { case Int => Int }] == """TypeLambda(List(X), List(TypeBounds(TypeRef(ThisType(TypeRef(NoPrefix(), "scala")), "Nothing"), TypeRef(ThisType(TypeRef(NoPrefix(), "scala")), "Any"))), MatchType(TypeRef(ThisType(TypeRef(NoPrefix(), "scala")), "Any"), ParamRef(binder, 0), List(MatchCase(TypeRef(TermRef(ThisType(TypeRef(NoPrefix(), "<root>")), "scala"), "Int"), TypeRef(TermRef(ThisType(TypeRef(NoPrefix(), "<root>")), "scala"), "Int")))))""")
+    assertEql(showStructure[[X] =>> X match { case Int => Int }],
+      """TypeLambda("""+
+        """List(X), """+
+        """List(TypeBounds("""+
+        """TypeRef(ThisType(TypeRef(NoPrefix(), "scala")), "Nothing"), """+
+        """TypeRef(ThisType(TypeRef(NoPrefix(), "scala")), "Any"))), """+
+        """MatchType("""+
+        """TypeRef(TermRef(ThisType(TypeRef(NoPrefix(), "<root>")), "scala"), "Int"), """+ // match type bound
+        """ParamRef(binder, 0), """+
+        """List("""+
+        """MatchCase("""+
+        """TypeRef(TermRef(ThisType(TypeRef(NoPrefix(), "<root>")), "scala"), "Int"), """+
+        """TypeRef(TermRef(ThisType(TypeRef(NoPrefix(), "<root>")), "scala"), "Int")))))""")
 
     // TODO: more complex types:
     //  - implicit function types
