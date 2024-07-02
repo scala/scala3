532a9daab3cfda88ee11a7fa2df6eb50551b7ad7
odersky <odersky@gmail.com>
2024-07-02 14:21:28 +0200

    Re-use isConcrete checking in match types for NamedTyple.From
     - Move isConcrete to a new object `MatchTypes`. We should also move other MatchType-related
       stuff from Types and TypeComparer here. Type and TypeComparer are already unconfortably big,
       and MatchTypes are a coherent topic where everything should work together.
     - Streamline isConcrete a bit.
     - Re-use isConcrete for a similar test in CheckRealizable.
     - Re-use isConcrete for evaluating NamedTuple.From
    
    Fixes #20517

---
+++ compiler/src/dotty/tools/dotc/core/MatchTypes.scala
@@ -0,0 +1,59 @@
+package dotty.tools
+package dotc
+package core
+
+import Types.*, Contexts.*, Symbols.*, Flags.*, Decorators.*
+
+object MatchTypes:
+
+  /* Concreteness checking
+   *
+   * When following a baseType and reaching a non-wildcard, in-variant-pos type capture,
+   * we have to make sure that the scrutinee is concrete enough to uniquely determine
+   * the values of the captures. This comes down to checking that we do not follow any
+   * upper bound of an abstract type.
+   *
+   * See notably neg/wildcard-match.scala for examples of this.
+   *
+   * See neg/i13780.scala, neg/i13780-1.scala and neg/i19746.scala for
+   * ClassCastException reproducers if we disable this check.
+   */
+  def isConcrete(tp: Type)(using Context): Boolean =
+    val tp1 = tp.normalized
+
+    tp1 match
+      case tp1: TypeRef =>
+        if tp1.symbol.isClass then true
+        else
+          tp1.info match
+            case info: AliasingBounds => isConcrete(info.alias)
+            case _                    => false
+      case tp1: AppliedType =>
+        isConcrete(tp1.tycon) && isConcrete(tp1.superType)
+      case tp1: HKTypeLambda =>
+        true
+      case tp1: TermRef =>
+        !tp1.symbol.is(Param) && isConcrete(tp1.underlying)
+      case tp1: TermParamRef =>
+        false
+      case tp1: SingletonType =>
+        isConcrete(tp1.underlying)
+      case tp1: ExprType =>
+        isConcrete(tp1.underlying)
+      case tp1: AnnotatedType =>
+        isConcrete(tp1.parent)
+      case tp1: RefinedOrRecType =>
+        isConcrete(tp1.underlying)
+      case tp1: AndOrType =>
+        isConcrete(tp1.tp1) && isConcrete(tp1.tp2)
+      case tp1: TypeVar =>
+        isConcrete(tp1.underlying)
+      case tp1: LazyRef =>
+        isConcrete(tp1.ref)
+      case tp1: FlexibleType =>
+        isConcrete(tp1.hi)
+      case _ =>
+        false
+  end isConcrete
+
+end MatchTypes
