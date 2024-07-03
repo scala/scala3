09c075ea63dd0990cd7ae2ea29c98e02d1a8e333
Florian3k <mfornal@virtuslab.com>
2024-07-03 20:38:00 +0200

    apply fixes from review
    [Cherry-picked 32b5843eee864d166ce78a1031504bc5429dd082]

--- library-aux/src/scala/Matchable.scala
+++ library-aux/src/scala/Matchable.scala
@@ -2,6 +2,6 @@ package scala
 
 /** The base trait of types that can be safely pattern matched against.
  *
- *   See [[https://docs.scala-lang.org/scala3/reference/other-new-features/matchable.html]].
+ *  See [[https://docs.scala-lang.org/scala3/reference/other-new-features/matchable.html]].
  */
 trait Matchable
--- library-aux/src/scala/andType.scala
+++ library-aux/src/scala/andType.scala
@@ -2,6 +2,6 @@ package scala
 
 /** The intersection of two types.
  *
- *   See [[https://docs.scala-lang.org/scala3/reference/new-types/intersection-types.html]].
+ *  See [[https://docs.scala-lang.org/scala3/reference/new-types/intersection-types.html]].
  */
-type &[A, B] = A & B
+type &[A, B]
