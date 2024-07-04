27fe00e9e19a1596f3ab40dfbec0747986832ab7
Eugene Flesselle <eugene@flesselle.net>
2024-07-04 19:15:59 +0200

    Fix isomorphism tests of `AndOrType`s under non-empty `BinderPairs` (#21017)
    Before the changes, when comparing two `HKTypeLambda` result types, the
    list of binding pairs was lost when entering comparison of `AndOrType`s,
    which caused the `equals` to fail, and hence prevented hash-consing.
    
    Even though `M1` and `M2` in pos/i20858-min should still conform to
    one-another, we entered a deep-subtype comparison because of the order
    in which the TypeComparer does dealiasing of AppliedTypes, and
    comparison of MatchCases and AndTypes.
    
    Fix #20858
    [Cherry-picked f2829c3fab28cc6ab47a5627abda855884476572]

---
+++ tests/pos/i20858/defns_1.scala
@@ -0,0 +1,27 @@
+import scala.compiletime.*
+import scala.deriving.*
+
+sealed trait ZIO[-R, +E, +A]
+sealed abstract class ZLayer[-RIn, +E, +ROut]
+object ZLayer:
+  def apply[RIn, E, ROut](zio: => ZIO[RIn, E, ROut]): ZLayer[RIn, E, ROut] = ???
+type URIO[-R, +A] = ZIO[R, Nothing, A]
+type IAnyType[T <: Tuple] = Tuple.Fold[T, Any, [x, y] =>> x & y]
+type UAnyType[T <: Tuple] = Tuple.Fold[T, Any, [x, y] =>> x | y]
+
+
+trait AutoLayer[A]:
+  def zlayer(using
+             p: Mirror.ProductOf[A]
+            ): ZLayer[IAnyType[p.MirroredElemTypes], Nothing, A]
+
+object AutoLayer:
+  inline given derived[A](using p: Mirror.ProductOf[A]): AutoLayer[A] = {
+    val a: ZIO[IAnyType[p.MirroredElemTypes], Nothing, A] = ???
+    new AutoLayer[A]:
+      override def zlayer(using
+                          pp: Mirror.ProductOf[A]
+                         ): ZLayer[IAnyType[pp.MirroredElemTypes], Nothing, A] = ZLayer {
+        a.asInstanceOf[ZIO[IAnyType[pp.MirroredElemTypes], Nothing, A]]
+      }
+  }
