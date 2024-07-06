6397a0d07f145a6d7cf5ec5b32cd119934b84902
Wojciech Mazur <wmazur@virtuslab.com>
2024-07-06 16:13:54 +0200

    Support src filter in -WConf (Closes #17635) (#18783)
    Fixes #18782
    [Cherry-picked 86e2fe5ee6d3431375f14de2563f6822f79c6bdc][modified]

--- compiler/test/dotty/tools/dotc/config/ScalaSettingsTests.scala
+++ compiler/test/dotty/tools/dotc/config/ScalaSettingsTests.scala
@@ -5,8 +5,14 @@ import CommandLineParser.tokenize
 import Settings._
 
 import org.junit.Test
+import org.junit.Ignore
 import org.junit.Assert._
 import core.Decorators.toMessage
+import dotty.tools.io.{Path, PlainFile}
+
+import java.net.URI
+import java.nio.file.Files
+import scala.util.Using
 
 class ScalaSettingsTests:
 
@@ -82,5 +88,116 @@ class ScalaSettingsTests:
     assertEquals(Action.Warning, sut.action(warn))
     val nowr = new Diagnostic.Warning("This is a problem.".toMessage, util.NoSourcePosition)
     assertEquals(Action.Silent, sut.action(nowr))
+
+  @Ignore("LTS backport rejected: https://github.com/scala/scala3/pull/18503")
+  @Test def `i18367 rightmost WConf flags take precedence over flags to the left`: Unit =
+    import reporting.{Action, Diagnostic}
+    val sets = new ScalaSettings
+    val args = List("-Wconf:cat=deprecation:e", "-Wconf:cat=deprecation:s")
+    val sumy = ArgsSummary(sets.defaultState, args, errors = Nil, warnings = Nil)
+    val proc = sets.processArguments(sumy, processAll = true, skipped = Nil)
+    val conf = sets.Wconf.valueIn(proc.sstate)
+    val msg  = "Don't use that!".toMessage
+    val depr = new Diagnostic.DeprecationWarning(msg, util.NoSourcePosition)
+    val sut  = reporting.WConf.fromSettings(conf).getOrElse(???)
+    assertEquals(Action.Silent, sut.action(depr))
+
+
+  private def wconfSrcFilterTest(argsStr: String,
+                                 warning: reporting.Diagnostic.Warning): Either[List[String], reporting.Action] =
+    import reporting.Diagnostic
+    val settings = new ScalaSettings
+    val args = ArgsSummary(settings.defaultState, List(argsStr), errors = Nil, warnings = Nil)
+    val proc = settings.processArguments(args, processAll = true, skipped = Nil)
+    val wconfStr = settings.Wconf.valueIn(proc.sstate)
+    val wconf = reporting.WConf.fromSettings(wconfStr)
+    wconf.map(_.action(warning))
+
+  @Test def `WConf src filter silences warnings from a matching path for virtual file`: Unit =
+    val result = wconfSrcFilterTest(
+      argsStr = "-Wconf:src=path/.*:s",
+      warning = reporting.Diagnostic.Warning(
+        "A warning".toMessage,
+        util.SourcePosition(
+          source = util.SourceFile.virtual(new URI("file:///some/path/file.scala"), ""),
+          span = util.Spans.Span(1L)
+        )
+      )
+    )
+    assertEquals(result, Right(reporting.Action.Silent))
+
+  @Test def `WConf src filter doesn't silence warnings from a non-matching path`: Unit =
+    val result = wconfSrcFilterTest(
+      argsStr = "-Wconf:src=another/.*:s",
+      warning = reporting.Diagnostic.Warning(
+        "A warning".toMessage,
+        util.SourcePosition(
+          source = util.SourceFile.virtual(new URI("file:///some/path/file.scala"), ""),
+          span = util.Spans.Span(1L)
+        )
+      )
+    )
+    assertEquals(result, Right(reporting.Action.Warning))
+
+  @Test def `WConf src filter silences warnings from a matching path for real file`: Unit =
+    val result = Using.resource(Files.createTempFile("myfile", ".scala").nn) { file =>
+      wconfSrcFilterTest(
+        argsStr = "-Wconf:src=myfile.*?\\.scala:s",
+        warning = reporting.Diagnostic.Warning(
+          "A warning".toMessage,
+          util.SourcePosition(
+            source = util.SourceFile(new PlainFile(Path(file)), "UTF-8"),
+            span = util.Spans.Span(1L)
+          )
+        )
+      )
+    }(Files.deleteIfExists(_))
+    assertEquals(result, Right(reporting.Action.Silent))
+
+  @Test def `WConf src filter doesn't silence warnings from a non-matching path for real file`: Unit =
+    val result = Using.resource(Files.createTempFile("myfile", ".scala").nn) { file =>
+      wconfSrcFilterTest(
+        argsStr = "-Wconf:src=another.*?\\.scala:s",
+        warning = reporting.Diagnostic.Warning(
+          "A warning".toMessage,
+          util.SourcePosition(
+            source = util.SourceFile(new PlainFile(Path(file)), "UTF-8"),
+            span = util.Spans.Span(1L)
+          )
+        )
+      )
+    }(Files.deleteIfExists(_))
+    assertEquals(result, Right(reporting.Action.Warning))
+
+  @Test def `WConf src filter reports an error on an invalid regex`: Unit =
+    val result = wconfSrcFilterTest(
+      argsStr = """-Wconf:src=\:s""",
+      warning = reporting.Diagnostic.Warning(
+        "A warning".toMessage,
+        util.SourcePosition(
+          source = util.SourceFile.virtual(new URI("file:///some/path/file.scala"), ""),
+          span = util.Spans.Span(1L)
+        )
+      ),
+    )
+    assertTrue(
+      result.left.exists(errors =>
+        errors.sizeIs == 1 && errors.headOption.exists(_.startsWith("invalid pattern"))
+      )
+    )
+
+  @Ignore("LTS backport rejected: https://github.com/scala/scala3/pull/18503")
+  @Test def `WConf src filter can be mixed with other filters with rightmost taking precedence`: Unit =
+    val result = wconfSrcFilterTest(
+      argsStr = "-Wconf:src=.*:s,cat=deprecation:e",
+      warning = reporting.Diagnostic.DeprecationWarning(
+        "A warning".toMessage,
+        util.SourcePosition(
+          source = util.SourceFile.virtual(new URI("file:///some/path/file.scala"), ""),
+          span = util.Spans.Span(1L)
+        )
+      )
+    )
+    assertEquals(result, Right(reporting.Action.Error))
 
 end ScalaSettingsTests
