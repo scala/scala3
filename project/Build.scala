94f76b4028991f7ed2598f4eb0ae69762dae1a08
Wojciech Mazur <wmazur@virtuslab.com>
2024-07-09 23:43:39 +0200

    chore: update to mtags `1.3.2`
    [Cherry-picked 4d7b2a4ecfa21347a350dc38187847ab6a0e9887][modified]

--- project/Build.scala
+++ project/Build.scala
@@ -1131,9 +1131,8 @@ object Build {
       BuildInfoPlugin.buildInfoDefaultSettings
 
   lazy val presentationCompilerSettings = {
-    val mtagsVersion = "1.3.1+63-1a8f4659-SNAPSHOT"
+    val mtagsVersion = "1.3.2"
     Seq(
-      resolvers ++= Resolver.sonatypeOssRepos("snapshots"),
       libraryDependencies ++= Seq(
         "org.lz4" % "lz4-java" % "1.8.0",
         "io.get-coursier" % "interface" % "1.0.18",
