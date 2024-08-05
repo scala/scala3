35b5f7cccbef0dfacc8c48e2fbd6c4f864c0bb7e
odersky <odersky@gmail.com>
2024-08-05 11:28:18 +0200

    A left-biased variant for implicit/given pairs
    We now use a left-biased scheme, as follows.
    
    From 3.6 on:
    
     - A given x: X is better than a given or implicit y: Y if y can be instantiated/widened to X.
     - An implicit x: X is better than a given or implicit y: Y if y can be instantiated to a supertype of X.
     - Use owner score for givens as a tie breaker if after all other tests we still have an ambiguity.
    
    This is not transitive, so we need a separate scheme to work around that.
    
    Other change:
    
     - Drop special handling of NotGiven in prioritization. The previous logic pretended to do so,
       but was ineffective.

--- tests/pos/i21212.scala
+++ tests/pos/i21212.scala
@@ -20,14 +20,3 @@ class UsingArguments[F[_]](using Temporal[F])(using err: MonadError[F, Throwable
   val bool: F[Boolean] = ???
   def works = toFunctorOps(bool).map(_ => ()) // warns under -source:3.5
 
-
-object Minimization:
-
-  trait A
-  trait B extends A
-
-  def test1(using a1: A)(using    b1: B) = summon[A] // picks (most general) a1
-  def test2(using a2: A)(implicit b2: B) = summon[A] // picks (most general) a2, was ambiguous
-  def test3(implicit       a3: A, b3: B) = summon[A] // picks (most specific) b3
-
-end Minimization
