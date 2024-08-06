ed6b6b6737375275a71622e41bd85301fb2bf9ff
Hamza REMMAL <hamza@remmal.dev>
2024-08-06 19:21:46 +0200

    Bump scala-cli to 1.4.3

--- project/Build.scala
+++ project/Build.scala
@@ -118,7 +118,7 @@ object Build {
   val mimaPreviousLTSDottyVersion = "3.3.0"
 
   /** Version of Scala CLI to download */
-  val scalaCliLauncherVersion = "1.4.1"
+  val scalaCliLauncherVersion = "1.4.3"
   /** Version of Coursier to download for initializing the local maven repo of Scala command */
   val coursierJarVersion = "2.1.10"
 
