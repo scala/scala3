b2292a8c67eaf09f8604b3c1e6a1fe5dffc145e3
odersky <odersky@gmail.com>
2024-08-10 14:21:53 +0200

    Unrelated test: A Provider variant

--- tests/run/Providers.scala
+++ tests/run/Providers.scala
@@ -65,6 +65,8 @@ end Providers
   Explicit().test()
   println(s"\nInjected")
   Injected().test()
+  println(s"\nInjected2")
+  Injected2().test()
 
 /** Demonstrator for explicit dependency construction */
 class Explicit:
@@ -172,6 +174,56 @@ class Injected:
     given Provider[UserSubscription] = provide(subscriptionService)
   end explicit
 end Injected
+
+/** Injected with builders in companion objects */
+class Injected2:
+  import Providers.*
+
+  case class User(name: String, email: String)
+
+  class UserSubscription(emailService: EmailService, db: UserDatabase):
+    def subscribe(user: User) =
+      emailService.email(user)
+      db.insert(user)
+  object UserSubscription:
+    def apply()(using Provider[(EmailService, UserDatabase)]): UserSubscription =
+      new UserSubscription(provided[EmailService], provided[UserDatabase])
+
+  class EmailService:
+    def email(user: User) =
+      println(s"You've just been subscribed to RockTheJVM. Welcome, ${user.name}")
+
+  class UserDatabase(pool: ConnectionPool):
+    def insert(user: User) =
+      pool.get().runQuery(s"insert into subscribers(name, email) values ${user.name} ${user.email}")
+  object UserDatabase:
+    def apply()(using Provider[(ConnectionPool)]): UserDatabase =
+      new UserDatabase(provided[ConnectionPool])
+
+  class ConnectionPool(n: Int):
+    def get(): Connection =
+      println(s"Acquired connection")
+      Connection()
+
+  class Connection():
+    def runQuery(query: String): Unit =
+      println(s"Executing query: $query")
+
+  def test() =
+    given Provider[EmailService]     = provide(EmailService())
+    given Provider[ConnectionPool]   = provide(ConnectionPool(10))
+    given Provider[UserDatabase]     = provide(UserDatabase())
+    given Provider[UserSubscription] = provide(UserSubscription())
+
+    def subscribe(user: User)(using Provider[UserSubscription]) =
+      val sub = UserSubscription()
+      sub.subscribe(user)
+
+    subscribe(User("Daniel", "daniel@RocktheJVM.com"))
+    subscribe(User("Martin", "odersky@gmail.com"))
+  end test
+end Injected2
+
 
 
 
