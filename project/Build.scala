f0a5da4c82c62b48fea80e7bcc0085513ba27555
Hamza REMMAL <hamza@remmal.dev>
2024-08-12 15:12:21 +0100

    Bump MiMa check to 3.5.0

--- project/Build.scala
+++ project/Build.scala
@@ -113,7 +113,7 @@ object Build {
    *   - `3.M.0`     if `P > 0`
    *   - `3.(M-1).0` if `P = 0`
    */
-  val mimaPreviousDottyVersion = "3.5.0-RC1"
+  val mimaPreviousDottyVersion = "3.5.0"
 
   /** LTS version against which we check binary compatibility.
    *
