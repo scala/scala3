929e7eb814f7236f51b383ea1717a04e93bfa6e5
Som Snytt <som.snytt@gmail.com>
2024-08-24 14:39:14 -0700

    Restore help msg for other than language

--- compiler/src/dotty/tools/dotc/config/CompilerCommand.scala
+++ compiler/src/dotty/tools/dotc/config/CompilerCommand.scala
@@ -9,7 +9,8 @@ abstract class CompilerCommand extends CliCommand:
 
   final def helpMsg(using settings: ConcreteSettings)(using SettingsState, Context): String =
     settings.allSettings.find(isHelping) match
-      case Some(s) => availableOptionsMsg(_ == s, showArgFileMsg = false)
+      case Some(s @ settings.language) => availableOptionsMsg(_ == s, showArgFileMsg = false)
+      case Some(s) => s.description
       case _ =>
         if (settings.help.value) usageMessage
         else if (settings.Vhelp.value) vusageMessage
