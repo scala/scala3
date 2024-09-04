1e166cbee71b9d52783f0a7ac03e028089c92b00
Jan Chyb <jan.chyb.99@gmail.com>
2024-09-04 16:29:22 +0200

    Ignore best effort settings in repl

--- compiler/test/dotty/tools/repl/ReplCompilerTests.scala
+++ compiler/test/dotty/tools/repl/ReplCompilerTests.scala
@@ -456,6 +456,17 @@ class ReplCompilerTests extends ReplTest:
       assertTrue(last, last.startsWith("val res0: tpolecat.type = null"))
       assertTrue(last, last.endsWith("""// result of "res0.toString" is null"""))
 
+  @Test def `i21431 filter out best effort options`: Unit =
+    initially:
+      run(":settings -Ybest-effort -Ywith-best-effort-tasty")
+    .andThen:
+      run("0") // check for crash
+      val last = lines()
+      println(last)
+      assertTrue(last(0), last(0) == ("Options incompatible with repl will be ignored: -Ybest-effort, -Ywith-best-effort-tasty"))
+      assertTrue(last(1), last(1) == ("val res0: Int = 0"))
+
+
 object ReplCompilerTests:
 
   private val pattern = Pattern.compile("\\r[\\n]?|\\n");
