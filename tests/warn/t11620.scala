8d3b3193689899e08f0c80b251a9e6902b6940b1
Wojciech Mazur <wmazur@virtuslab.com>
2024-09-10 12:20:20 +0200

    Revert Backport "Fix a bundle of patmat issues" to 3.3.4

--- tests/warn/t11620.scala
+++
@@ -1,9 +0,0 @@
-sealed trait A[+T0]
-case class A1[+T1](t1: T1) extends A[T1]
-case class A2[+T2](t2: T2) extends A[T2]
-sealed trait B[+T3] { type AA[+U] <: A[U] ; def a: AA[T3] }
-object B { def unapply[T4](b: B[T4]): Some[b.AA[T4]] = Some(b.a) }
-class Test:
-  def m1[X](b: B[X]): X = b match
-    case B(A1(v1)) => v1
-    case B(A2(v2)) => v2
