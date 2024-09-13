45728afddd3d1f5ffca185a302959f3f39dddc08
odersky <odersky@gmail.com>
2024-09-13 13:12:01 +0200

    Embed accountsFor info in regular explain traces

--- compiler/src/dotty/tools/dotc/cc/CaptureSet.scala
+++ compiler/src/dotty/tools/dotc/cc/CaptureSet.scala
@@ -158,9 +158,13 @@ sealed abstract class CaptureSet extends Showable:
    *                 as frozen.
    */
   def accountsFor(x: CaptureRef)(using Context): Boolean =
-    reporting.trace(i"$this accountsFor $x, ${x.captureSetOfInfo}?", show = true):
+    def debugInfo(using Context) = i"$this accountsFor $x, which has capture set ${x.captureSetOfInfo}"
+    def test(using Context) = reporting.trace(debugInfo):
       elems.exists(_.subsumes(x))
       || !x.isMaxCapability && x.captureSetOfInfo.subCaptures(this, frozen = true).isOK
+    comparer match
+      case comparer: ExplainingTypeComparer => comparer.traceIndented(debugInfo)(test)
+      case _ => test
 
   /** A more optimistic version of accountsFor, which does not take variable supersets
    *  of the `x` reference into account. A set might account for `x` if it accounts
