4bc5d2427c91c00034a553f862de31dee1314ff9
Kacper Korban <kacper.f.korban@gmail.com>
2024-09-23 14:47:29 +0200

    Check for parameter references in type bounds when infering tracked

--- tests/pos/infer-tracked.scala
+++ tests/pos/infer-tracked.scala
@@ -10,13 +10,37 @@ class F(val x: C):
 
 class G(override val x: C) extends F(x)
 
+class H(val x: C):
+  type T1 = x.T
+  val result: T1 = x.foo
+
+class I(val c: C, val t: c.T)
+
+case class J(c: C):
+  val result: c.T = c.foo
+
+case class K(c: C):
+  def result[B >: c.T]: B = c.foo
+
 def Test =
   val c = new C:
     type T = Int
     def foo = 42
 
   val f = new F(c)
-  val i: Int = f.result
+  val _: Int = f.result
 
   // val g = new G(c)
-  // val j: Int = g.result
+  // val _: Int = g.result
+
+  val h = new H(c)
+  val _: Int = h.result
+
+  val i = new I(c, c.foo)
+  val _: Int = i.t
+
+  val j = J(c)
+  val _: Int = j.result
+
+  val k = K(c)
+  val _: Int = k.result
