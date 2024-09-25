64bd03eff0d4a901de85b563e681e1ffc5d8eb24
Kacper Korban <kacper.f.korban@gmail.com>
2024-09-25 09:49:23 +0200

    Add more test cases

--- tests/pos/contextbounds-for-poly-functions.scala
+++ tests/pos/contextbounds-for-poly-functions.scala
@@ -4,6 +4,9 @@ import scala.language.future
 trait Ord[X]:
   def compare(x: X, y: X): Int
 
+trait Show[X]:
+  def show(x: X): String
+
 val less1 = [X: Ord] => (x: X, y: X) => summon[Ord[X]].compare(x, y) < 0
 
 val less2 = [X: Ord as ord] => (x: X, y: X) => ord.compare(x, y) < 0
@@ -18,3 +21,11 @@ val less3: Comparer = [X: Ord as ord] => (x: X, y: X) => ord.compare(x, y) < 0
 // val less4: Comparer2 = [X: Ord] => (x: X, y: X) => summon[Ord[X]].compare(x, y) < 0
 
 val less5 = [X: [X] =>> Ord[X]] => (x: X, y: X) => summon[Ord[X]].compare(x, y) < 0
+
+val less6 = [X: {Ord, Show}] => (x: X, y: X) => summon[Ord[X]].compare(x, y) < 0
+
+val less7 = [X: {Ord as ord, Show}] => (x: X, y: X) => ord.compare(x, y) < 0
+
+val less8 = [X: {Ord, Show as show}] => (x: X, y: X) => summon[Ord[X]].compare(x, y) < 0
+
+val less9 = [X: {Ord as ord, Show as show}] => (x: X, y: X) => ord.compare(x, y) < 0
