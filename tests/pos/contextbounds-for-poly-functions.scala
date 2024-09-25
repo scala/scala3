7da22701313d1b955652f3db26661f2e303f12e5
Kacper Korban <kacper.f.korban@gmail.com>
2024-09-25 09:43:30 +0200

    Small cleanup
    [Cherry-picked 309034e5579dd5dcf2667d5cd096c1067681b724]

--- tests/pos/contextbounds-for-poly-functions.scala
+++ tests/pos/contextbounds-for-poly-functions.scala
@@ -16,3 +16,5 @@ val less3: Comparer = [X: Ord as ord] => (x: X, y: X) => ord.compare(x, y) < 0
 // type Cmp[X] = (x: X, y: X) => Boolean
 // type Comparer2 = [X: Ord] => Cmp[X]
 // val less4: Comparer2 = [X: Ord] => (x: X, y: X) => summon[Ord[X]].compare(x, y) < 0
+
+val less5 = [X: [X] =>> Ord[X]] => (x: X, y: X) => summon[Ord[X]].compare(x, y) < 0
