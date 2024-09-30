63119f792bda315f168b4fd731b97f1d6299ce56
Jamie Thompson <bishbashboshjt@gmail.com>
2024-09-30 12:48:56 +0200

    SIP 61 - add compilation test, fix missing symbols

---
+++ tests/run/unroll-caseclass/Test_4.scala
@@ -0,0 +1,11 @@
+//> using options -experimental
+import example.*
+// !! IMPORTANT: If you remove this test, also remove unroll-caseclass.check
+@main def Test(): Unit = {
+  println("=== Unrolled Test V1 ===")
+  UnrollTestMainV1.main(Array.empty)
+  println("=== Unrolled Test V2 ===")
+  UnrollTestMainV2.main(Array.empty)
+  println("=== Unrolled Test V3 ===")
+  UnrollTestMainV3.main(Array.empty)
+}
