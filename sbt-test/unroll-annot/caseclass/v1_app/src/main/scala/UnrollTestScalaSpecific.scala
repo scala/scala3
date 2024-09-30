97497741cc976e6f7c5cdfb19f66b3dc365fe394
Jamie Thompson <bishbashboshjt@gmail.com>
2024-09-30 15:26:42 +0200

    SIP 61 - add sbt scripted test template

---
+++ sbt-test/unroll-annot/caseclass/v1_app/src/main/scala/UnrollTestScalaSpecific.scala
@@ -0,0 +1,20 @@
+package unroll
+
+import unroll.TestUtils.logAssertStartsWith
+
+object UnrollTestScalaSpecificV1{
+  def test() = {
+    val unrolled = Unrolled.fromProduct(
+      new Product{
+        def canEqual(that: Any) = true
+        def productArity = 2
+        def productElement(n: Int) = n match{
+          case 0 => "hello"
+          case 1 => 31337
+        }
+      }
+    )
+
+    logAssertStartsWith(unrolled.foo, "hello31337")
+  }
+}
