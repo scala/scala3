ac7619451afd79fb1bb8299e6875dcad0b909ed4
Fengyun Liu <fengyun.liu.cs@gmail.com>
2024-10-06 22:20:20 +0200

    Add test

---
+++ tests/init-global/warn/of-class-unsound.scala
@@ -0,0 +1,25 @@
+class Box[T](val value: T)
+
+abstract class Base[T]:
+  def update(n: T): Unit
+
+class A[T](var a: T) extends Base[T]:
+  def update(n: T) =
+    a = n
+
+class B[T](var b: T) extends Base[T]:
+  def update(n: T) =
+    println(O.x)       // warn
+    b = n
+
+object O:
+  val m: Int = 10
+  f(if m > 5 then Box(A(3)) else Box(B(4)))
+
+  val x: Int = 10
+
+  def f(a: Box[Base[Int]]): Unit =
+    h(a.value)
+
+  def h(a: Base[Int]): Unit =
+    a.update(10)
