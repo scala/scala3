882cb84ea303405e3a1a6a7de4a880b37d2ae033
Som Snytt <som.snytt@gmail.com>
2024-10-14 14:06:43 -0700

    Don't ignore params of public methods

--- tests/warn/i15503e.scala
+++ tests/warn/i15503e.scala
@@ -1,4 +1,4 @@
-//> using options  -Wunused:explicits
+//> using options -Wunused:explicits
 
 object Foo {
   /* This goes around the "trivial method" detection */
@@ -31,7 +31,7 @@ package scala3main:
 package foo.test.lambda.param:
   val default_val = 1
   val a = (i: Int) => i // OK
-  val b = (i: Int) => default_val // OK
+  val b = (i: Int) => default_val // warn
   val c = (_: Int) => default_val // OK
 
 package foo.test.trivial:
