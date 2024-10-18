3a98a1cd3efd52fdb7e92f03d7d9ddc7f8ed3282
noti0na1 <noti0na1@users.noreply.github.com>
2024-10-18 10:44:58 +0200

    Remove extra if

--- compiler/src/dotty/tools/dotc/parsing/Parsers.scala
+++ compiler/src/dotty/tools/dotc/parsing/Parsers.scala
@@ -4696,9 +4696,11 @@ object Parsers {
           stats += closure(in.offset, Location.InBlock, modifiers(BitSet(IMPLICIT)))
         else if isIdent(nme.extension) && followingIsExtension() then
           stats += extension()
-        else if outermost && ctx.mode.is(Mode.Interactive) && isDefIntro(localModifierTokens) then
-          stats +++= localDef(in.offset)
-        else if isDefIntro(localModifierTokens, excludedSoftModifiers = Set(nme.`opaque`)) then
+        else if isDefIntro(localModifierTokens,
+            excludedSoftModifiers =
+              // Allow opaque definitions at outermost level in REPL.
+              if outermost && ctx.mode.is(Mode.Interactive)
+              then Set.empty else Set(nme.`opaque`)) then
           stats +++= localDef(in.offset)
         else
           empty = true
