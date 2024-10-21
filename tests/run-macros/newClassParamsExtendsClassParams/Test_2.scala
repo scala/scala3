b8cd0844b39126ff80b41b8d374db0907f65f34f
Jan Chyb <jan.chyb.99@gmail.com>
2024-10-21 14:20:36 +0200

    Add class term parameters, flags, and privateWithin to newClass in reflect API

---
+++ tests/run-macros/newClassParamsExtendsClassParams/Test_2.scala
@@ -0,0 +1,10 @@
+//> using options -experimental
+
+@main def Test: Unit = {
+  val foo: Foo = makeClass("foo")
+  foo.foo()
+  println(foo.getClass)
+  val bar: Foo = makeClass("bar")
+  bar.foo()
+  println(bar.getClass)
+}
