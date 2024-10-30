69352518330fddbed9e33e58604a7889c97fa34b
Dale Wijnand <dale.wijnand@gmail.com>
2024-10-30 14:04:44 +0000

    Simplify transformIsInstanceOf check
    [Cherry-picked 7b721f0fdc032e871737d11ed1841c9fdc4ef98d]

--- tests/pos/i21544.scala
+++ tests/pos/i21544.scala
@@ -1,2 +1,4 @@
 class Test():
   def m1(xs: List[Boolean]) = for (x: Any) <- xs yield x
+  def m2(xs: List[Boolean]) = for (x: AnyVal) <- xs yield x
+  def m3(xs: List[Boolean]) = for (x: Matchable) <- xs yield x
