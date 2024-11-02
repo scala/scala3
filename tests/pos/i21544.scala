aea7d9e1562a7a967b42078b44199a65c4b8a450
Dale Wijnand <dale.wijnand@gmail.com>
2024-11-02 18:09:27 +0000

    Add extra isInstanceOf test cases
    [Cherry-picked 49d876a87851b78a63f3027811b41791a2fe583c]

--- tests/pos/i21544.scala
+++ tests/pos/i21544.scala
@@ -2,3 +2,12 @@ class Test():
   def m1(xs: List[Boolean]) = for (x: Any) <- xs yield x
   def m2(xs: List[Boolean]) = for (x: AnyVal) <- xs yield x
   def m3(xs: List[Boolean]) = for (x: Matchable) <- xs yield x
+
+  def v1(xs: List[AnyVal])    = for (x: Any) <- xs yield x
+  def v2(xs: List[AnyVal])    = for (x: AnyVal) <- xs yield x
+  def v3(xs: List[AnyVal])    = for (x: Matchable) <- xs yield x
+
+  def t1(xs: List[Matchable]) = for (x: Any) <- xs yield x
+  def t2(xs: List[Matchable]) = for (x: Matchable) <- xs yield x
+
+  def a1(xs: List[Any])       = for (x: Any) <- xs yield x
