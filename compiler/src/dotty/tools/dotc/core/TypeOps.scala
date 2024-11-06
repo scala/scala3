cecd05356beecd232053d4c593af54bcc12cad0e
Hamza Remmal <hamza@remmal.net>
2024-11-06 18:16:33 +0100

    Revert "Drop inaccessible subclasses from refineUsingParent"

--- compiler/src/dotty/tools/dotc/core/TypeOps.scala
+++ compiler/src/dotty/tools/dotc/core/TypeOps.scala
@@ -930,11 +930,7 @@ object TypeOps:
       for tp <- mixins.reverseIterator do
         protoTp1 <:< tp
       maximizeType(protoTp1, NoSpan)
-      val inst = wildApprox(protoTp1)
-      if !inst.classSymbol.exists then
-        // E.g. i21790, can't instantiate S#CA as a subtype of O.A, because O.CA isn't accessible
-        NoType
-      else inst
+      wildApprox(protoTp1)
     }
 
     if (protoTp1 <:< tp2) instantiate()
