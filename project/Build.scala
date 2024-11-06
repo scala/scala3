360f19ad6e42d9ceb6acc57a71212269271bddf8
Wojciech Mazur <wmazur@virtuslab.com>
2024-11-06 16:15:00 +0100

    Set baseVersion to 3.6.2

--- project/Build.scala
+++ project/Build.scala
@@ -93,7 +93,7 @@ object Build {
 
   val referenceVersion = "3.5.2"
 
-  val baseVersion = "3.6.1"
+  val baseVersion = "3.6.2"
   // Will be required by some automation later
   // val prereleaseVersion = s"$baseVersion-RC1"
 
