74920d38d0e9943a8c7f1550d17d2eec97e03d6d
Kacper Korban <kacper.f.korban@gmail.com>
2024-11-06 13:33:23 +0100

    Don't typeAhead erroneous parsing trees related to export statements

--- tests/neg/i20511-1.scala
+++ tests/neg/i20511-1.scala
@@ -4,4 +4,4 @@ def toppingPrice(size: Int): Double = ???
 
 def crustPrice(crustType: Double): Double = ???
 
-export toppingPrice.apply, crustPrice.unlift // error // error // error
+export toppingPrice.apply, crustPrice.apply, crustPrice.unlift // error // error // error // error // error
