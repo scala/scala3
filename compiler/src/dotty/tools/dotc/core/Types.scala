6cb2d018d16b383ba45bf156a6bdd3d7779e70b7
Matt Bovel <matthieu@bovel.net>
2024-11-13 15:24:57 +0100

    Rename `InlineCopier` to `ConservativeTreeCopier`, use it in `TypeMap`s
    [Cherry-picked bb63e31a8384e6e3127ee679ea0033e8437ef9ce]

--- compiler/src/dotty/tools/dotc/core/Types.scala
+++ compiler/src/dotty/tools/dotc/core/Types.scala
@@ -5936,7 +5936,14 @@ object Types extends TypeUtils {
       }
     }
 
-    private def treeTypeMap = new TreeTypeMap(typeMap = this)
+    private def treeTypeMap = new TreeTypeMap(
+      typeMap = this,
+      // Using `ConservativeTreeCopier` is needed when copying dependent annoted
+      // types, where we can refer to a previous parameter represented as
+      // `TermParamRef` that has no underlying type yet.
+      // See tests/pos/annot-17242.scala.
+      cpy = ConservativeTreeCopier()
+    )
 
     def mapOver(syms: List[Symbol]): List[Symbol] = mapSymbols(syms, treeTypeMap)
 
