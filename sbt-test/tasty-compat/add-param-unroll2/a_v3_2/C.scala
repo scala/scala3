78fbf043bc421f45e70b622178009a7454ceb728
Jamie Thompson <bishbashboshjt@gmail.com>
2024-11-14 19:12:10 +0100

    address review: part 3

--- sbt-test/tasty-compat/add-param-unroll2/a_v3_2/C.scala
+++
@@ -1,9 +0,0 @@
-package a
-import b.*
-
-// C is a separate compilation unit to A, and inlines B.caller, so its TASTy will try to resolve
-// the generated A.foo forwarder, which will not exist yet in typer phase.
-// The unit will suspend, and in the second run, A.foo will be generated, so resolution will succeed.
-object C {
-  val res: String = B.caller
-}
