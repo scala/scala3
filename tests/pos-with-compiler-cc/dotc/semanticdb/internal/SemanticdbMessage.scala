7b4597fea9c5d409e769d3ce8fe22a26028eb7c9
Dale Wijnand <dale.wijnand@gmail.com>
2024-11-15 09:59:27 +0000

    Remove tests/pos-with-compiler-cc

--- tests/pos-with-compiler-cc/dotc/semanticdb/internal/SemanticdbMessage.scala
+++
@@ -1,7 +0,0 @@
-package dotty.tools.dotc.semanticdb.internal
-
-trait SemanticdbMessage[A] {
-  def serializedSize: Int
-  def writeTo(out: SemanticdbOutputStream): Unit
-  def mergeFrom(in: SemanticdbInputStream): A
-}
