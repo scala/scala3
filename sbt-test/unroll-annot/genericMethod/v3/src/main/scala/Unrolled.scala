d02d8ce4a49c0a68977b8b8516b5a132728894ec
Jamie Thompson <bishbashboshjt@gmail.com>
2024-11-15 20:55:10 +0100

    mechanically copy all sbt-test for unroll
    still leave tasty-compat/add-param-unroll, as that specifically tests
    reading from tasty

--- sbt-test/unroll-annot/genericMethod/v3/src/main/scala/Unrolled.scala
+++
@@ -1,7 +0,0 @@
-package unroll
-
-import scala.annotation.unroll
-
-class Unrolled{
-  final def foo[T](s: T, @unroll n: Int = 1, b: Boolean = true, @unroll l: Long = 0) = s.toString + n + b + l
-}
--- sbt-test/unroll-annot/primaryConstructor/v2/src/main/scala/Unrolled.scala
+++
@@ -1,7 +0,0 @@
-package unroll
-
-import scala.annotation.unroll
-
-class Unrolled(s: String, n: Int = 1, @unroll b: Boolean = true){
-  final def foo = s + n + b
-}
