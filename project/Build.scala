236cb8ae058bc859c0aaa865072f046ab2a7fe29
Wojciech Mazur <wmazur@virtuslab.com>
2024-11-19 13:06:08 +0100

    Release Scala 3.6.2-RC1

--- project/Build.scala
+++ project/Build.scala
@@ -143,8 +143,9 @@ object Build {
    *  For a developedVersion `3.M.P` the mimaPreviousDottyVersion should be set to:
    *   - `3.M.0`     if `P > 0`
    *   - `3.(M-1).0` if `P = 0`
+   *  3.6.1 is an exception from this rule - 3.6.0 was a broken release
    */
-  val mimaPreviousDottyVersion = "3.6.0"
+  val mimaPreviousDottyVersion = "3.6.1"
 
   /** LTS version against which we check binary compatibility.
    *
