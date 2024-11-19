20917c7a95f004ac5caad5097d5d06eb5120da77
noti0na1 <noti0na1@users.noreply.github.com>
2024-11-19 18:04:51 +0100

    Add some tests

---
+++ tests/neg-custom-args/captures/i21868.scala
@@ -0,0 +1,14 @@
+import caps.
+
+trait AbstractWrong:
+    type C <: CapSet
+    def boom(): Unit^{C^} // error
+
+trait Abstract:
+    type C <: CapSet^
+    def boom(): Unit^{C^}
+
+class Concrete extends Abstract:
+    type C = Nothing
+    def boom() = () // error
+
