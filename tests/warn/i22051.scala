bc1e435325f89420932902f5993e0bdcbf5329a9
noti0na1 <noti0na1@users.noreply.github.com>
2024-12-03 16:11:36 +0100

    Move test to warn
    [Cherry-picked a7b23d6f1890ce31cf52b72848b559d7873c4213]

--- tests/warn/i22051.scala
+++ tests/warn/i22051.scala
@@ -1,9 +1,7 @@
-//> using options -Werror
-
 def boundary[T](body: (T => RuntimeException) => T): T =
   case class Break(value: T) extends RuntimeException
   try body(Break.apply)
-  catch case Break(t) => t // error: pattern matching on local classes is unsound currently
+  catch case Break(t) => t // warn: pattern matching on local classes is unsound currently
 
 def test =
   boundary[Int]: EInt =>
@@ -16,4 +14,4 @@ def boundaryCorrectBehaviour[T](body: (T => RuntimeException) => T): T =
     // A correct implementation, but is still treated as a local class right now
     case class Break(value: T) extends RuntimeException
   try body(local.Break.apply)
-  catch case local.Break(t) => t // error
+  catch case local.Break(t) => t // warn
