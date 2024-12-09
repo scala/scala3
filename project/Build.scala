ad3f69867b4600a3550bf9e76a1fc58091cb5eaf
Wojciech Mazur <wmazur@virtuslab.com>
2024-12-09 12:05:18 +0100

    Release 3.6.2

--- project/Build.scala
+++ project/Build.scala
@@ -109,7 +109,7 @@ object Build {
    *  During release candidate cycle incremented by the release officer before publishing a subsequent RC version;
    *  During final, stable release is set exactly to `developedVersion`.
   */
-  val baseVersion = s"$developedVersion-RC3"
+  val baseVersion = developedVersion
 
   /** Final version of Scala compiler, controlled by environment variables. */
   val dottyVersion = {
--- tasty/src/dotty/tools/tasty/TastyFormat.scala
+++ tasty/src/dotty/tools/tasty/TastyFormat.scala
@@ -340,7 +340,7 @@ object TastyFormat {
    *  is able to read final TASTy documents if the file's
    *  `MinorVersion` is strictly less than the current value.
    */
-  final val ExperimentalVersion: Int = 2
+  final val ExperimentalVersion: Int = 0
 
   /**This method implements a binary relation (`<:<`) between two TASTy versions.
    *
