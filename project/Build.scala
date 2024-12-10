aa7bf6bbdd990eba524b73ec1a7ff4675dac7b4e
Wojciech Mazur <wmazur@virtuslab.com>
2024-12-10 23:43:02 +0100

    Update mima previous dotty version

--- project/Build.scala
+++ project/Build.scala
@@ -149,9 +149,9 @@ object Build {
    *  For a developedVersion `3.M.P` the mimaPreviousDottyVersion should be set to:
    *   - `3.M.0`     if `P > 0`
    *   - `3.(M-1).0` if `P = 0`
-   *  3.6.1 is an exception from this rule - 3.6.0 was a broken release
+   *  3.6.2 is an exception from this rule - 3.6.0 was a broken release, 3.6.1 was hotfix (unstable) release
    */
-  val mimaPreviousDottyVersion = "3.6.1"
+  val mimaPreviousDottyVersion = "3.6.2"
 
   /** LTS version against which we check binary compatibility.
    *
