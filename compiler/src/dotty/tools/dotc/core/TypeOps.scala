3ac5f2def9222cdd25436f387ed6883d2d8687f4
Dale Wijnand <dale.wijnand@gmail.com>
2024-12-12 16:51:27 +0000

    Teach AvoidMap to strip opaque alias refinements

--- compiler/src/dotty/tools/dotc/core/TypeOps.scala
+++ compiler/src/dotty/tools/dotc/core/TypeOps.scala
@@ -511,7 +511,26 @@ object TypeOps:
         tp
       else (if pre.isSingleton then NoType else tryWiden(tp, tp.prefix)).orElse {
         if (tp.isTerm && variance > 0 && !pre.isSingleton)
-          apply(tp.info.widenExpr)
+          tp.prefix match
+            case prefix: TermRef if prefix.name.is(InlineBinderName) && prefix.symbol.denot.is(Synthetic, butNot=InlineProxy) =>
+              prefix.info.widenExpr.dealias match
+                case info: RefinedType =>
+                  // Strip refinements on an opaque alias proxy
+                  // Using pos/i22068 as an example,
+                  // Inliner#addOpaqueProxies add the following opaque alias proxy:
+                  //     val $proxy1: foos.type { type Foo[T] = String } =
+                  //       foos.$asInstanceOf[foos.type { type Foo[T] = String }]
+                  // Then when InlineCall#expand creates a typed Inlined,
+                  // we type avoid any local bindings, which includes that opaque alias proxy.
+                  // To avoid that the replacement is a non-singleton RefinedType,
+                  // we drop the refinements too and return foos.type.
+                  // That way, when we inline `def m1` and we calculate the asSeenFrom
+                  // of `b1.and(..)` b1 doesn't have an unstable prefix.
+                  derivedSelect(tp, info.stripRefinement)
+                case _ =>
+                  apply(tp.info.widenExpr)
+            case _ =>
+              apply(tp.info.widenExpr)
         else if (upper(pre).member(tp.name).exists)
           super.derivedSelect(tp, pre)
         else
