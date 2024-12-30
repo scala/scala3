9ba16020ba1c831a94e370b8703d87ffda38302f
Wojciech Mazur <wmazur@virtuslab.com>
2024-12-30 23:45:22 +0100

    Set referenceVersion to 3.6.3-RC2

--- project/Build.scala
+++ project/Build.scala
@@ -98,7 +98,7 @@ object Build {
    *
    *  Warning: Change of this variable needs to be consulted with `expectedTastyVersion`
    */
-  val referenceVersion = "3.6.3-RC1"
+  val referenceVersion = "3.6.3-RC2"
 
   /** Version of the Scala compiler targeted in the current release cycle
    *  Contains a version without RC/SNAPSHOT/NIGHTLY specific suffixes
