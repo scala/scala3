87212138d67a7ed8bc32f09ad7d6bceffe66b2bd
Jan Chyb <jan.chyb.99@gmail.com>
2025-01-09 15:41:39 +0100

    Change parameter names, add tests

---
+++ tests/run-macros/newClassExtendsJavaClass/JavaClass.java
@@ -0,0 +1,9 @@
+class JavaClass<T> {
+  T value;
+  public JavaClass(T value) {
+    this.value = value;
+  }
+  public T getT() {
+    return value;
+  }
+}
--- tests/run-macros/newClassTypeParams/Macro_1.scala
+++ tests/run-macros/newClassTypeParams/Macro_1.scala
@@ -8,7 +8,7 @@ private def makeClassExpr(nameExpr: Expr[String])(using Quotes): Expr[Any] = {
 
   val name = nameExpr.valueOrAbort
   def decls(cls: Symbol): List[Symbol] = Nil
-  val constrType =
+  val conMethodType =
     (classType: TypeRepr) => PolyType(List("A", "B"))(
       _ => List(TypeBounds.empty, TypeBounds.upper(TypeRepr.of[Int])),
       polyType => MethodType(List("param1", "param2"))((_: MethodType) => List(polyType.param(0), polyType.param(1)), (_: MethodType) => classType)
@@ -20,19 +20,23 @@ private def makeClassExpr(nameExpr: Expr[String])(using Quotes): Expr[Any] = {
     parents = _ => List(TypeRepr.of[Object]),
     decls,
     selfType = None,
-    constrType,
-    Flags.EmptyFlags,
-    Symbol.noSymbol,
-    Flags.EmptyFlags,
-    Symbol.noSymbol,
-    List(List(Flags.EmptyFlags, Flags.EmptyFlags), List(Flags.EmptyFlags, Flags.EmptyFlags))
+    clsFlags = Flags.EmptyFlags,
+    clsPrivateWithin = Symbol.noSymbol,
+    conMethodType,
+    conFlags = Flags.EmptyFlags,
+    conPrivateWithin = Symbol.noSymbol,
+    conParamFlags = List(List(Flags.EmptyFlags, Flags.EmptyFlags), List(Flags.EmptyFlags, Flags.EmptyFlags))
   )
 
   val clsDef = ClassDef(cls, List(TypeTree.of[Object]), body = Nil)
   val newCls =
-    cls.typeRef.asType match
-      case '[t] =>
-        Typed(Apply(TypeApply(Select(New(TypeIdent(cls)), cls.primaryConstructor), List(TypeTree.of[String], TypeTree.of[Int])), List(Expr("test").asTerm, Expr(1).asTerm)), TypeTree.of[Any])
+    Typed(
+      Apply(
+        TypeApply(Select(New(TypeIdent(cls)), cls.primaryConstructor), List(TypeTree.of[String], TypeTree.of[Int])),
+        List(Expr("test").asTerm, Expr(1).asTerm)
+      ),
+      TypeTree.of[Any]
+    )
 
   val res = Block(List(clsDef), newCls).asExpr
 
