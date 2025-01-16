4d1e2b02d0b66fc397b1dd2ddafa55c39f0e0295
aherlihy <herlihyap@gmail.com>
2025-01-15 22:13:47 -0500

    Handle out of sync imports after require + tests

---
+++ compiler/test-resources/jars/MyLibrary.scala
@@ -0,0 +1,10 @@
+// MyLibrary.scala
+package mylibrary
+
+object Utils:
+  def greet(name: String): String = s"Hello, $name!"
+
+class Calculator:
+  def add(x: Int, y: Int): Int = x + y
+  def subtract(x: Int, y: Int): Int = x - y
+
---
+++ compiler/test-resources/jars/MyLibrary2.scala
@@ -0,0 +1,10 @@
+// MyLibrary2.scala
+package mylibrary2
+
+object Utils2:
+  def greet(name: String): String = s"Hello, $name!"
+
+class Calculator2:
+  def add(x: Int, y: Int): Int = x + y
+  def subtract(x: Int, y: Int): Int = x - y
+
