b4ae9e6ee98ae04a8162c5649a6217f5c982cd0c
Wojciech Mazur <wmazur@virtuslab.com>
2025-01-18 00:00:03 +0100

    Set reference version to 3.6.4-RC1

--- project/Build.scala
+++ project/Build.scala
@@ -98,7 +98,7 @@ object Build {
    *
    *  Warning: Change of this variable needs to be consulted with `expectedTastyVersion`
    */
-  val referenceVersion = "3.6.3-RC2"
+  val referenceVersion = "3.6.4-RC1"
 
   /** Version of the Scala compiler targeted in the current release cycle
    *  Contains a version without RC/SNAPSHOT/NIGHTLY specific suffixes
