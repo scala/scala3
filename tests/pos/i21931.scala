08442e883b0dacdbc60c6f697e2467bfdf1643cd
Kacper Korban <kacper.f.korban@gmail.com>
2025-01-20 12:03:28 +0100

    Only check logicalOwners for methods, and not for classes, when looking for proxies (#22356)
    possible fix for #21931

---
+++ tests/pos/i21931.scala
@@ -0,0 +1,13 @@
+def f() =
+  val NotFound: Char = 'a'
+  class crashing() {
+    class issue() {
+      NotFound
+    }
+    class Module() {
+      val obligatory =
+        class anonIssue() {
+          issue()
+        }
+    }
+  }
