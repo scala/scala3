4ca490e762f2a9df21ef710dbb9053e3002776ee
Piotr Chabelski <ged.subfan@gmail.com>
2025-01-24 09:36:12 +0100

    Update Scala CLI to 1.6.1 (was 1.5.4) & `coursier` to 2.1.24 (was 2.1.18)

--- project/Build.scala
+++ project/Build.scala
@@ -184,9 +184,9 @@ object Build {
   val mimaPreviousLTSDottyVersion = "3.3.0"
 
   /** Version of Scala CLI to download */
-  val scalaCliLauncherVersion = "1.5.4"
+  val scalaCliLauncherVersion = "1.6.1"
   /** Version of Coursier to download for initializing the local maven repo of Scala command */
-  val coursierJarVersion = "2.1.18"
+  val coursierJarVersion = "2.1.24"
 
   object CompatMode {
     final val BinaryCompatible = 0
