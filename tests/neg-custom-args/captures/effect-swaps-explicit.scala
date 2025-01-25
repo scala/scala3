3adf4b06de5338d4371d3e0fd03b8a0487548999
odersky <odersky@gmail.com>
2025-01-25 19:27:54 +0100

    Check that hidden parameters are annotated @consume
    TODO:
    
     - check that only @consume parameters flow to @consume parameters

--- tests/neg-custom-args/captures/effect-swaps-explicit.scala
+++ tests/neg-custom-args/captures/effect-swaps-explicit.scala
@@ -1,4 +1,4 @@
-
+import language.future // sepchecks on
 
 object boundary:
 
@@ -14,7 +14,7 @@ end boundary
 
 import boundary.{Label, break}
 
-trait Async extends caps.Capability
+trait Async extends caps.SharedCapability
 object Async:
   def blocking[T](body: Async ?=> T): T = ???
 
--- tests/neg-custom-args/captures/i22005.scala
+++ tests/neg-custom-args/captures/i22005.scala
@@ -1,3 +1,4 @@
+import language.future // sepchecks on
 import caps.*
 
 class IO
@@ -5,4 +6,4 @@ class File(io: IO^)
 
 class Handler[C^]:
   def f(file: File^): File^{C^} = file // error
-  def g(file: File^{C^}): File^ = file // ok
+  def g(@consume file: File^{C^}): File^ = file // ok
