b4ff2fa9c9566bd73a23be3eb17a443b8b26c0a2
odersky <odersky@gmail.com>
2025-02-04 23:53:19 +0100

    Revise path handling
     - Don't widen prefix type if followed by a path selection
     - When applying VAR with a prefix path p on a boxed capture set,
       replace cap with p* instead of p. This makes it clear that we
       need to charge deep capture sets on parameters and avoids
       paradoxes like this one:
    
          case class Box[+T](get: T)
          val b: Box[C^]
    
       Here, b.get <:< b, but b.get's underlying capture set is {cap} whereas
       b's underlying capture set is {}. By using b.get* instead of b.get, we
       make sure that we compare against the dcs of `b`, which is also {cap}.

--- tests/run-custom-args/captures/colltest5/CollectionStrawManCC5_1.scala
+++ tests/run-custom-args/captures/colltest5/CollectionStrawManCC5_1.scala
@@ -455,12 +455,14 @@ object CollectionStrawMan5 {
     object Filter:
       def apply[A](underlying: Iterable[A]^, pp: A => Boolean, isFlipped: Boolean): Filter[A]^{underlying, pp} =
         underlying match
-          case filter: Filter[A]^{underlying} =>
-            unsafeAssumeSeparate:
+          case filter: Filter[A] =>
+            new Filter(filter.underlying, a => filter.p(a) && pp(a))
+              .asInstanceOf[Filter[A]^{underlying, pp}]
+            //unsafeAssumeSeparate:
               // See filter-iterable.scala for a test where a variant of Filter
               // works without the unsafeAssumeSeparate. But it requires significant
               // changes compared to the version here.
-              new Filter(filter.underlying, a => filter.p(a) && pp(a))
+              //new Filter(filter.underlying, a => filter.p(a) && pp(a))
           case _ => new Filter(underlying, pp)
 
     case class Partition[A](val underlying: Iterable[A]^, p: A => Boolean) {
