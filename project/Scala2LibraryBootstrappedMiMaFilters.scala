78c74736fb800cc38a7fe67cab65b630f4cb925d
Hamza Remmal <hamza@remmal.net>
2025-02-06 13:53:08 +0100

    chore: Allow to write tasty for Scala2 symbols under -Ycompile-scala2-library

--- project/Scala2LibraryBootstrappedMiMaFilters.scala
+++ project/Scala2LibraryBootstrappedMiMaFilters.scala
@@ -66,8 +66,6 @@ object Scala2LibraryBootstrappedMiMaFilters {
         ProblemFilters.exclude[FinalMethodProblem]("scala.io.Source.NoPositioner"),
         ProblemFilters.exclude[FinalMethodProblem]("scala.io.Source.RelaxedPosition"),
         ProblemFilters.exclude[FinalMethodProblem]("scala.io.Source.RelaxedPositioner"),
-        ProblemFilters.exclude[IncompatibleResultTypeProblem]("scala.reflect.ManifestFactory#NothingManifest.newArray"),
-        ProblemFilters.exclude[IncompatibleResultTypeProblem]("scala.reflect.ManifestFactory#NullManifest.newArray"),
         ProblemFilters.exclude[MissingFieldProblem]("scala.collection.ArrayOps#ReverseIterator.xs"),
         ProblemFilters.exclude[MissingFieldProblem]("scala.runtime.NonLocalReturnControl.value"),
       ) ++
