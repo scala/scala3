6fc147ed2eabafcf97671406f4ebaf348132187d
Tomasz Godzik <tgodzik@virtuslab.com>
2025-02-06 16:58:35 +0100

    chore: Fix test to pass, since community build doesn't break more

--- tests/patmat/i13931.scala
+++ tests/patmat/i13931.scala
@@ -1,7 +1,7 @@
 class Test:
   def test = Vector() match
     case Seq() => println("empty")
-    case _ => println("non-empty")
+    case null => println("non-empty")
 
   def test2 = IndexedSeq() match { case IndexedSeq() => case null => }
   def test3 = IndexedSeq() match { case IndexedSeq(1) => case _ => }
