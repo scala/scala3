a23b62854f41e2914ae0592229e42a2e1621a34a
Kacper Korban <kacper.f.korban@gmail.com>
2025-02-07 18:13:01 +0100

    More applied constructor types fixes
    - only type applied constructor types in Typer
    - handle multiple parameter lists
    - add tests for mixed tracked and not tracked parameters

--- tests/pos/applied_constructor_types.scala
+++ tests/pos/applied_constructor_types.scala
@@ -8,8 +8,12 @@ class F[A](tracked val a: Int)
 class G[A](tracked val a: A)
 class NF[A](tracked val f: F[A])
 
+class Person(val name: String, tracked val age: Int)
+class PersonPrime(val name: String)(tracked val age: Int)
+class PersonBis(tracked val name: String)(val age: Int)
+
 object O:
-  val m: Int = 42
+  val m: Int = 27
 
   class InnerClass(tracked val x: Int)
 
@@ -23,11 +27,23 @@ object Test extends App {
   val f3: F(42) = F(42)
   val g: G(42) = G(42)
 
-  val n: Int = 42
+  val n: Int = 27
   val c2: C(n) = C(n)
   val c3: C(O.m) = C(O.m)
 
   val box: Box(O.InnerClass(42)) = Box(O.InnerClass(42))
   val box2: Box(O.InnerClass(n)) = Box(O.InnerClass(n))
   val box3: Box(O.InnerClass(O.m)) = Box(O.InnerClass(O.m))
+
+  val person: Person("Kasia", 27) = Person("Kasia", 27)
+  val person1: Person("Kasia", n) = Person("Kasia", n)
+  val person2: Person("Kasia", O.m) = Person("Kasia", O.m)
+
+  val personPrime: PersonPrime("Kasia")(27) = PersonPrime("Kasia")(27)
+  val personPrime1: PersonPrime("Kasia")(n) = PersonPrime("Kasia")(n)
+  val personPrime2: PersonPrime("Kasia")(O.m) = PersonPrime("Kasia")(O.m)
+
+  val personBis: PersonBis("Kasia")(27) = PersonBis("Kasia")(27)
+  val personBis1: PersonBis("Kasia")(n) = PersonBis("Kasia")(n)
+  val personBis2: PersonBis("Kasia")(O.m) = PersonBis("Kasia")(O.m)
 }
