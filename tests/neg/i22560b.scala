b6ad24f21798fe0d148deea276e23c3226fd68f1
Som Snytt <som.snytt@gmail.com>
2025-02-13 09:30:58 -0800

    Reference on access of ctor proxy

--- tests/neg/i22560b.scala
+++ tests/neg/i22560b.scala
@@ -7,3 +7,12 @@ class Enumeration:
 class Test extends Enumeration:
   val Hearts = Val(27) // error
   val Diamonds = Val() // error
+
+package p:
+  private[p] class C(i: Int) // ctor proxy gets privateWithin of class
+  private[p] class D(i: Int)
+  object D
+
+package q:
+  def f() = p.C(42) // error
+  def g() = p.D(42) // error
