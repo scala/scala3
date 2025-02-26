9d7f439403feb3c87b98bf26892600a5038c92ac
Wojciech Mazur <wmazur@virtuslab.com>
2025-02-26 10:42:08 +0100

    Release 3.6.4-RC2

--- project/Build.scala
+++ project/Build.scala
@@ -118,7 +118,7 @@ object Build {
    *  During release candidate cycle incremented by the release officer before publishing a subsequent RC version;
    *  During final, stable release is set exactly to `developedVersion`.
   */
-  val baseVersion = s"$developedVersion-RC1"
+  val baseVersion = s"$developedVersion-RC2"
 
   /** The version of TASTY that should be emitted, checked in runtime test
    *  For defails on how TASTY version should be set see related discussions:
