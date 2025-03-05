ab3c8e90b5795e7bc7d27119b8257e9859a54732
odersky <odersky@gmail.com>
2025-03-05 11:42:28 +0100

    Make CanThrow a SharedCapability
    [Cherry-picked 641dffa2412093ab0c858f83c54eb9d4b9242cfc]

--- library/src/scala/CanThrow.scala
+++ library/src/scala/CanThrow.scala
@@ -8,7 +8,7 @@ import annotation.{implicitNotFound, experimental, capability}
  */
 @experimental
 @implicitNotFound("The capability to throw exception ${E} is missing.\nThe capability can be provided by one of the following:\n - Adding a using clause `(using CanThrow[${E}])` to the definition of the enclosing method\n - Adding `throws ${E}` clause after the result type of the enclosing method\n - Wrapping this piece of code with a `try` block that catches ${E}")
-erased class CanThrow[-E <: Exception] extends caps.Capability
+erased class CanThrow[-E <: Exception] extends caps.SharedCapability
 
 @experimental
 object unsafeExceptions:
