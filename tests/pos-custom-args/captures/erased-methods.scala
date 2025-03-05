641dffa2412093ab0c858f83c54eb9d4b9242cfc
odersky <odersky@gmail.com>
2025-03-05 11:42:28 +0100

    Make CanThrow a SharedCapability

--- tests/pos-custom-args/captures/erased-methods.scala
+++ tests/pos-custom-args/captures/erased-methods.scala
@@ -7,14 +7,14 @@ class Ex2 extends Exception("Ex2")
 class Ex3 extends Exception("Ex3")
 
 def foo8a(i: Int) =
-  (erased xx1: CanThrow[Ex2]^) ?=> throw new Ex2
+  (erased xx1: CanThrow[Ex2]) ?=> throw new Ex2
 
 def foo9a(i: Int)
-  : (erased x$0: CanThrow[Ex3]^)
-    ?=> (erased x$1: CanThrow[Ex2]^)
-    ?=> (erased x$2: CanThrow[Ex1]^)
+  : (erased x$0: CanThrow[Ex3])
+    ?=> (erased x$1: CanThrow[Ex2])
+    ?=> (erased x$2: CanThrow[Ex1])
     ?=> Unit
-  = (erased x$1: CanThrow[Ex3]^)
-     ?=> (erased x$2: CanThrow[Ex2]^)
-     ?=> (erased x$3: CanThrow[Ex1]^)
+  = (erased x$1: CanThrow[Ex3])
+     ?=> (erased x$2: CanThrow[Ex2])
+     ?=> (erased x$3: CanThrow[Ex1])
      ?=> throw new Ex3
