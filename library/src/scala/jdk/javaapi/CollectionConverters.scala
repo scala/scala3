ce1dab9480289cc5f131cf568a3315658ea87d6e
Seth Tisue <seth@tisue.net>
2025-03-05 12:03:34 -0800

    collections Scaladoc: fix an awkward wording

--- library/src/scala/jdk/javaapi/CollectionConverters.scala
+++ library/src/scala/jdk/javaapi/CollectionConverters.scala
@@ -34,7 +34,7 @@ import scala.collection.convert.{AsJavaConverters, AsScalaConverters}
   * }}}
   *
   * The conversions return adapters for the corresponding API, i.e., the collections are wrapped,
-  * not converted. Changes to the original collection are reflected in the view, and vice versa.
+  * not copied. Changes to the original collection are reflected in the view, and vice versa.
   *
   * The following conversions are supported via `asScala` and `asJava`:
   *
