6785da26a167126e207e3da6f835b94780522e71
kasiaMarek <kasia@marek.net>
2025-03-11 15:44:06 +0100

    fix: go to def should lead to all: apply, object and class

--- presentation-compiler/test/dotty/tools/pc/tests/definition/PcDefinitionSuite.scala
+++ presentation-compiler/test/dotty/tools/pc/tests/definition/PcDefinitionSuite.scala
@@ -512,3 +512,75 @@ class PcDefinitionSuite extends BasePcDefinitionSuite:
          |val foo_name = foo.na@@me
          |""".stripMargin
     )
+
+  @Test def `object` =
+    check(
+      """|package a
+         |object <<Bar>> {
+         |  def foo = 42
+         |}
+         |val m = B@@ar.foo
+         |""".stripMargin
+    )
+
+  @Test def i7267 =
+    check(
+      """|package a
+        |trait Foo {
+        |  def someNum: Int
+        |  def <<apply>>(i: Int): Unit = println(someNum)
+        |}
+        |object <<Bar>> extends Foo {
+        |  def someNum = 42
+        |}
+        |
+        |object Test {
+        |  B@@ar(2)
+        |}
+        |""".stripMargin
+    )
+
+  @Test def `i7267-2` =
+    check(
+      """|package b
+        |trait Foo {
+        |  def someNum: Int
+        |  def <<unapply>>(i: Int): Option[Int] = Some(i)
+        |}
+        |object <<Bar>> extends Foo {
+        |  def someNum = 42
+        |}
+        |
+        |object Test {
+        |  Bar.someNum match {
+        |    case B@@ar(1) => ???
+        |    case _ =>
+        |  }
+        |}
+        |""".stripMargin
+    )
+
+  @Test def `i7267-3` =
+    check(
+      """|package c
+        |case class <<Bar>>()
+        |object <<Bar>>
+        |object O {
+        |  val a = B@@ar()
+        |}
+        |""".stripMargin
+    )
+
+  @Test def `i7267-4` =
+    check(
+      """|package d
+        |class <<Bar>>()
+        |object <<Bar>> {
+        |  def <<apply>>(): Bar = new Bar()
+        |}
+        |object O {
+        |  val a = B@@ar()
+        |}
+        |""".stripMargin
+    )
+
