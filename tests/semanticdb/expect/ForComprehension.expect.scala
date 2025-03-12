49bc04dfe5f9b93312be58d6fd7b15aa280ffa8a
Wojciech Mazur <wmazur@virtuslab.com>
2025-03-12 09:22:02 +0100

    Adjust failing SemanticdbTests when compiling without -preview

--- tests/semanticdb/expect/ForComprehension.expect.scala
+++ tests/semanticdb/expect/ForComprehension.expect.scala
@@ -3,43 +3,43 @@ package example
 class ForComprehension/*<-example::ForComprehension#*/ {
   for {
     a/*<-local0*/ <- List/*->scala::package.List.*/(1)
-    b/*<-local1*/ <- List/*->scala::package.List.*/(1)
+    b/*<-local1*//*->local1*/ <- List/*->scala::package.List.*/(1)
     if b/*->local1*/ >/*->scala::Int#`>`(+3).*/ 1
-    c/*<-local2*/ = a/*->local0*/ +/*->scala::Int#`+`(+4).*/ b/*->local1*/
+    c/*<-local2*//*->local2*/ = a/*->local0*/ +/*->scala::Int#`+`(+4).*/ b/*->local1*/
   } yield (a/*->local0*/, b/*->local1*/, c/*->local2*/)
   for {
-    a/*<-local3*/ <- List/*->scala::package.List.*/(1)
-    b/*<-local4*/ <- List/*->scala::package.List.*/(a/*->local3*/)
+    a/*<-local4*/ <- List/*->scala::package.List.*/(1)
+    b/*<-local5*/ <- List/*->scala::package.List.*/(a/*->local4*/)
     if (
-      a/*->local3*/,
-      b/*->local4*/
+      a/*->local4*/,
+      b/*->local5*/
     ) ==/*->scala::Any#`==`().*/ (1, 2)
     (
-      c/*<-local6*/,
-      d/*<-local7*/
-    ) <- List/*->scala::package.List.*/((a/*->local3*/, b/*->local4*/))
+      c/*<-local7*/,
+      d/*<-local8*/
+    ) <- List/*->scala::package.List.*/((a/*->local4*/, b/*->local5*/))
     if (
-      a/*->local3*/,
-      b/*->local4*/,
-      c/*->local6*/,
-      d/*->local7*/
+      a/*->local4*/,
+      b/*->local5*/,
+      c/*->local7*/,
+      d/*->local8*/
     ) ==/*->scala::Any#`==`().*/ (1, 2, 3, 4)
-    e/*<-local8*//*->local8*/ = (
-      a/*->local3*/,
-      b/*->local4*/,
-      c/*->local6*/,
-      d/*->local7*/
+    e/*<-local9*//*->local9*/ = (
+      a/*->local4*/,
+      b/*->local5*/,
+      c/*->local7*/,
+      d/*->local8*/
     )
-    if e/*->local8*/ ==/*->scala::Any#`==`().*/ (1, 2, 3, 4)
-    f/*<-local9*/ <- List/*->scala::package.List.*/(e/*->local8*/)
+    if e/*->local9*/ ==/*->scala::Any#`==`().*/ (1, 2, 3, 4)
+    f/*<-local10*/ <- List/*->scala::package.List.*/(e/*->local9*/)
   } yield {
     (
-      a/*->local3*/,
-      b/*->local4*/,
-      c/*->local6*/,
-      d/*->local7*/,
-      e/*->local8*/,
-      f/*->local9*/
+      a/*->local4*/,
+      b/*->local5*/,
+      c/*->local7*/,
+      d/*->local8*/,
+      e/*->local9*/,
+      f/*->local10*/
     )
   }
 }
