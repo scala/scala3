56cc6b724bd62e9eb7229f752ba1b255c959c821
noti0na1 <noti0na1@users.noreply.github.com>
2025-03-31 17:10:58 +0200

    Fix some test

--- tests/pos-custom-args/captures/cc-expand.scala
+++ tests/pos-custom-args/captures/cc-expand.scala
@@ -11,12 +11,12 @@ object Test:
 
     def x0: A -> B^{ct} = ???
 
-    def x1: A -> B @retains(ct) = ???
-    def x2: A -> B -> C @retains(ct) = ???
-    def x3: A -> () -> B -> C @retains(ct) = ???
+    def x1: A -> B @retains[ct.type] = ???
+    def x2: A -> B -> C @retains[ct.type] = ???
+    def x3: A -> () -> B -> C @retains[ct.type] = ???
 
-    def x4: (x: A @retains(ct)) -> B -> C = ???
+    def x4: (x: A @retains[ct.type]) -> B -> C = ???
 
-    def x5: A -> (x: B @retains[ct.type]) -> () -> C @retains(dt) = ???
+    def x5: A -> (x: B @retains[ct.type]) -> () -> C @retains[dt.type] = ???
     def x6: A -> (x: B @retains[ct.type]) -> (() -> C @retains[dt.type]) @retains[x.type | dt.type] = ???
     def x7: A -> (x: B @retains[ct.type]) -> (() -> C @retains[dt.type]) @retains[x.type] = ???
