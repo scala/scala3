f5207d32f5490dfa6ab979a2c8677ee756b2d286
Jan Chyb <jan.chyb.99@gmail.com>
2025-04-11 16:34:40 +0200

    Fix issue with certain synthetics missing in compiletime.typechecks
    Previously, instead of using the PostTyper phase directly, a typed tree
    Transformer was used. However, it did not transform the ClassInfo
    values using PostTyper.transformInfo, which lead to RefChecks not
    finding certain synthesized methods, throwing an incorrect error about
    a missing implementation. This is fixed by running the full suite of
    PostTyper transforms. A minor refactor is included, to limit
    the repeated CompilationUnit construction and error checking.
    
    [Cherry-picked 8f48a3befb3b23ec6cb9fbfca1bc3e148c0d893b]

--- compiler/src/dotty/tools/dotc/CompilationUnit.scala
+++ compiler/src/dotty/tools/dotc/CompilationUnit.scala
@@ -134,7 +134,7 @@ object CompilationUnit {
   /** Create a compilation unit corresponding to an in-memory String.
    *  Used for `compiletime.testing.typeChecks`.
    */
-  def apply(name: String, source: String)(using Context): CompilationUnit = {
+  def apply(name: String, source: String): CompilationUnit = {
     val src = SourceFile.virtual(name = name, content = source, maybeIncomplete = false)
     new CompilationUnit(src)
   }
