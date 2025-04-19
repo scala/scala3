3367e0d44d6d5c458659d7b74976faaa8e3bd8b7
Som Snytt <som.snytt@gmail.com>
2025-04-19 08:13:09 -0700

    Check inline expansion for exclusion
    Preserve attachments of literal constant in `42: Unit`.
    
    [Cherry-picked 8f7b6b7b914c51c61c3455fda72051f5dbcebbff]

--- tests/warn/i23250.scala
+++ tests/warn/i23250.scala
@@ -1,4 +1,4 @@
-//> using options -Wunused:all -Werror
+//> using options -Wunused:all
 
 trait MonadError[F[_], E]
 type MonadThrow[F[_]] = MonadError[F, Throwable]
@@ -10,7 +10,7 @@ trait WriteResult
 trait MetaStreamsSyntax:
   extension [F[_]](ms: MetaStreams[F])(using MonadThrow[F])
     def setMaxAge(): F[WriteResult] =
-      summon[MonadThrow[F]]
+      summon[MonadThrow[F]] // warn pure expr
       ms.use[WriteResult]
 
     def setTruncateBefore(): F[WriteResult] =
