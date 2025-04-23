fa115526960cf1088d75b1ab0aab2360e9a09397
Tomasz Godzik <tgodzik@virtuslab.com>
2025-04-23 11:23:13 +0200

    Remove repl bin; rename flag
    [Cherry-picked 28c877f74b919348f6d715ff01570aee8ee89822][modified]

--- compiler/test/dotty/tools/scripting/BashExitCodeTests.scala
+++ compiler/test/dotty/tools/scripting/BashExitCodeTests.scala
@@ -30,7 +30,6 @@ class BashExitCodeTests:
   def scala(args: String*)     = verifyExit(scalaPath, args*)
   def scalacRaw(args: String*) = verifyExit(scalacPath, args*)
   def scalac(args: String*)    = scalacRaw(("-d" +: tmpDir +: args)*)
-  def repl(args: String*)      = verifyExit(scalaPath, "--power", "repl", "--offline", "--", args*)
 
   /** The path to the test file for this class. */
   def f(body: String, suffix: String = ".scala"): String =
@@ -62,8 +61,6 @@ class BashExitCodeTests:
   @Test def version       = scala("--version")(0)
   @Test def xPluginList   = scala("-Xplugin-list")(0)
   @Test def vPhases       = scala("-Vphases")(0)
-
-  @Test def replEval      = repl("--repl-quit-after-init", "--repl-init-script", "\'println(\"Hello from init script!\"); val i = 2 * 2\'")(0)
 
   /** A utility for running two commands in a row, like you do in bash. */
   extension (inline u1: Unit) inline def & (inline u2: Unit): Unit = { u1; u2 }
