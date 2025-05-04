6056ae2f08d8a7b611de4c28e414e2fe0a41d756
odersky <odersky@gmail.com>
2025-05-04 19:01:41 +0200

    Implement level checking for Fresh roots

--- tests/pos-custom-args/captures/skolems2.scala
+++ tests/pos-custom-args/captures/skolems2.scala
@@ -13,6 +13,17 @@ def Test(@consume c: Object^, @consume f: Object^ => Object^) =
   val x4: Object^ =
     { unsafeAssumeSeparate(f)(cc) }
 
+def Test2(@consume c: Object^, @consume f: Object^ => Object^) =
+  def cc(): Object^ = unsafeAssumeSeparate(c)
+  val x1 =
+    { f(cc()) }
+  val x2 =
+    f(cc())
+  val x3: Object^ =
+    f(cc())
+  val x4: Object^ =
+    { unsafeAssumeSeparate(f)(cc()) }
+
 
 
 
