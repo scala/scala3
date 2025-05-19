733e2533567e8a2c9e4d2fdcbe88042351ceb1cf
Dale Wijnand <dale.wijnand@gmail.com>
2025-05-19 09:46:29 +0100

    Initialize with null not "uninitialized"
    Co-authored-by: Sébastien Doeraene <sjrdoeraene@gmail.com>

--- compiler/src/dotty/tools/dotc/sbt/ExtractDependencies.scala
+++ compiler/src/dotty/tools/dotc/sbt/ExtractDependencies.scala
@@ -577,7 +577,7 @@ class DependencyRecorder {
     clazz
   }
 
-  private[dotc] var _responsibleForImports: Symbol | Null = uninitialized
+  private[dotc] var _responsibleForImports: Symbol | Null = null
 
   /** Top level import dependencies are registered as coming from a first top level
    *  class/trait/object declared in the compilation unit. If none exists, issue a warning and return NoSymbol.
