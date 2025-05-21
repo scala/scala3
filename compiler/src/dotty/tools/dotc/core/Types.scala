8e14a348cdbf056e23584e7440b51eb7c48c6155
Matt Bovel <matthieu@bovel.net>
2025-05-21 14:01:19 +0000

    Only keep denotation for methods in IntegrateMap

--- compiler/src/dotty/tools/dotc/core/Types.scala
+++ compiler/src/dotty/tools/dotc/core/Types.scala
@@ -3862,7 +3862,7 @@ object Types extends TypeUtils {
         if tp.prefix eq pre then tp
         else
           pre match
-            case ref: ParamRef if (ref.binder eq self) && tp.symbol.exists =>
+            case ref: ParamRef if (ref.binder eq self) && tp.symbol.exists && tp.symbol.is(Method) =>
               NamedType(pre, tp.name, tp.denot.asSeenFrom(pre))
             case _ =>
               tp.derivedSelect(pre)
