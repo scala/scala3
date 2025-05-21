2b859acf299b87fa8447bab9ad84fd61d625678e
Hamza Remmal <hamza@remmal.net>
2025-05-21 11:57:54 +0200

    chore: add `3.1-migration` in the compiler
    [Cherry-picked acacffe872ea679aaa9b80fb3de9a24d44f8d2b3]

--- compiler/src/dotty/tools/dotc/config/SourceVersion.scala
+++ compiler/src/dotty/tools/dotc/config/SourceVersion.scala
@@ -6,7 +6,8 @@ import core.Decorators.*
 import util.Property
 
 enum SourceVersion:
-  case `3.0-migration`, `3.0`, `3.1` // Note: do not add `3.1-migration` here, 3.1 is the same language as 3.0.
+  case `3.0-migration`, `3.0` 
+  case `3.1-migration`, `3.1`
   case `3.2-migration`, `3.2`
   case `3.3-migration`, `3.3`
   case `future-migration`, `future`
@@ -25,10 +26,10 @@ object SourceVersion extends Property.Key[SourceVersion]:
   def defaultSourceVersion = `3.3`
 
   /* Illegal source versions that may not appear in the settings `-source:<...>` */
-  val illegalInSettings = List(`never`)
+  val illegalInSettings = List(`3.1-migration`, `never`)
 
   /* Illegal source versions that may not appear as an import `import scala.language.<...>` */
-  val illegalInImports  = List(`never`)
+  val illegalInImports  = List(`3.1-migration`, `never`)
 
   /** language versions that may appear in a language import, are deprecated, but not removed from the standard library. */
   val illegalSourceVersionNames = "3.1-migration" :: illegalInImports.map(_.toString.toTermName)
