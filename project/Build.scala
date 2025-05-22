23726396a9a1f6ea13ad7c907f92ec38a1a82991
Wojciech Mazur <wmazur@virtuslab.com>
2025-05-22 20:53:16 +0200

    Release 3.7.1-RC2

--- project/Build.scala
+++ project/Build.scala
@@ -123,7 +123,7 @@ object Build {
    *  During release candidate cycle incremented by the release officer before publishing a subsequent RC version;
    *  During final, stable release is set exactly to `developedVersion`.
   */
-  val baseVersion = s"$developedVersion-RC1"
+  val baseVersion = s"$developedVersion-RC2"
 
   /** The version of TASTY that should be emitted, checked in runtime test
    *  For defails on how TASTY version should be set see related discussions:
@@ -1238,7 +1238,7 @@ object Build {
     dependsOn(dottyCompiler(Bootstrapped) % "provided; compile->runtime; test->test").
     settings(scala2LibraryBootstrappedSettings).
     settings(moduleName := "scala2-library-cc")
-  
+
   lazy val scala2LibraryBootstrappedSettings = Seq(
       javaOptions := (`scala3-compiler-bootstrapped` / javaOptions).value,
       Compile / scalacOptions ++= {
