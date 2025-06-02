4c20f079119dbff66babb9ba5af4abcac24b09ca
Yichen Xu <yichen.x@outlook.com>
2025-06-02 11:39:02 +0200

    Charge deep capture set for use class params

--- compiler/src/dotty/tools/dotc/cc/CheckCaptures.scala
+++ compiler/src/dotty/tools/dotc/cc/CheckCaptures.scala
@@ -834,10 +834,14 @@ class CheckCaptures extends Recheck, SymTransformer:
             initCs ++ FreshCap(Origin.NewCapability(core)).readOnly.singletonCaptureSet
           else initCs
         for (getterName, argType) <- mt.paramNames.lazyZip(argTypes) do
+          val paramSym = cls.primaryConstructor.paramNamed(getterName)
           val getter = cls.info.member(getterName).suchThat(_.isRefiningParamAccessor).symbol
           if !getter.is(Private) && getter.hasTrackedParts then
             refined = refined.refinedOverride(getterName, argType.unboxed) // Yichen you might want to check this
-            allCaptures ++= argType.captureSet
+            if paramSym.isUseParam then
+              allCaptures ++= argType.deepCaptureSet
+            else
+              allCaptures ++= argType.captureSet
         (refined, allCaptures)
 
       /** Augment result type of constructor with refinements and captures.
