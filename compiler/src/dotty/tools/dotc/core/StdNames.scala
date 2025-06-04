621551cf2f34cddfd8acf1bb72a3e8621f348028
Matt Bovel <matthieu@bovel.net>
2025-06-04 13:33:43 +0000

    Add syntax for qualified types
    Co-Authored-By: Quentin Bernet <28290641+Sporarum@users.noreply.github.com>

--- compiler/src/dotty/tools/dotc/core/StdNames.scala
+++ compiler/src/dotty/tools/dotc/core/StdNames.scala
@@ -587,6 +587,7 @@ object StdNames {
     val productElementName: N   = "productElementName"
     val productIterator: N      = "productIterator"
     val productPrefix: N        = "productPrefix"
+    val qualified : N           = "qualified"
     val quotes : N              = "quotes"
     val raw_ : N                = "raw"
     val rd: N                   = "rd"
