9e3f0351e83b5de0374da5fe1057eb863f06708e
Matt Bovel <matthieu@bovel.net>
2025-06-04 14:51:05 +0000

    Allow nested qualified types with implicit argument name

--- tests/printing/qualified-types.scala
+++ tests/printing/qualified-types.scala
@@ -31,7 +31,21 @@ def test() =
     x > 0
   } = 1
   val x5: Int with x > 0 = 1
-  val x6: Int = id[{x: Int with x < 0}](1) + id[Neg](-1)
+  val x6: Int = id[{x: Int with x < 0}](-1) + id[Neg](-1)
+
+// `val x: Int with x > 0` is desugared to `val x: {x: Int with x > 0}`: if the
+// name of a qualifier argument is not specified, it is assumed to be the same
+// as the parent `val` definition.
+def implicitArgumentName() =
+  val x0: (Int with x0 > 0) | (String with x0 == "foo") = ???
+  val x1: Int with x1 > 0 = ???
+  val x2: (Int with x2 > 0) = ???
+  val x3: (Int with x3 > 0) & (Int with x3 < 10) = ???
+  val x4: (Int with x4 > 0) & Int with x4 < 10 = ???
+  val x5: Int & String with false = ???
+  val x6: ((Int with x6 > 0) & Int) with x5 < 10 = ???
+  val x7: (Int with x7 > 0) with x6 < 10 = ???
+  val x8: ((Int with x8 > 0) with x7 < 10) = ???
 
 def bar(x: Int with x > 0) = ???
 def secondGreater1(x: Int, y: Int)(z: {w: Int with x > y}) = ???
