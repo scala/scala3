8d1425cb272aea2c91f85358a68258ae7f4b7d1a
Matt Bovel <matthieu@bovel.net>
2025-06-12 09:46:13 +0000

    Add E-Graph-based rewriting to the QualifierSolver

--- compiler/src/dotty/tools/dotc/qualified_types/QualifierSolver.scala
+++ compiler/src/dotty/tools/dotc/qualified_types/QualifierSolver.scala
@@ -28,21 +28,21 @@ class QualifierSolver(using Context):
           val rhs = defDef1.rhs
           val lhs = defDef2.rhs
           if tree1ArgSym.info frozen_<:< tree2ArgSym.info then
-            impliesRec(rhs, lhs.subst(List(tree2ArgSym), List(tree1ArgSym)))
+            impliesRec1(rhs, lhs.subst(List(tree2ArgSym), List(tree1ArgSym)))
           else if tree2ArgSym.info frozen_<:< tree1ArgSym.info then
-            impliesRec(rhs.subst(List(tree1ArgSym), List(tree2ArgSym)), lhs)
+            impliesRec1(rhs.subst(List(tree1ArgSym), List(tree2ArgSym)), lhs)
           else
             false
         case _ =>
           throw IllegalArgumentException("Qualifiers must be closures")
 
-  private def impliesRec(tree1: Tree, tree2: Tree): Boolean =
+  private def impliesRec1(tree1: Tree, tree2: Tree): Boolean =
     // tree1 = lhs || rhs
     tree1 match
       case Apply(select @ Select(lhs, name), List(rhs)) =>
         select.symbol match
           case d.Boolean_|| =>
-            return impliesRec(lhs, tree2) && impliesRec(rhs, tree2)
+            return impliesRec1(lhs, tree2) && impliesRec1(rhs, tree2)
           case _ => ()
       case _ => ()
 
@@ -51,32 +51,64 @@ class QualifierSolver(using Context):
       case Apply(select @ Select(lhs, name), List(rhs)) =>
         select.symbol match
           case d.Boolean_&& =>
-            return impliesRec(tree1, lhs) && impliesRec(tree1, rhs)
+            return impliesRec1(tree1, lhs) && impliesRec1(tree1, rhs)
           case d.Boolean_|| =>
-            return impliesRec(tree1, lhs) || impliesRec(tree1, rhs)
-          case _ => ()
-      case _ => ()
-
-    // tree1 = lhs && rhs
-    tree1 match
-      case Apply(select @ Select(lhs, name), List(rhs)) =>
-        select.symbol match
-          case d.Boolean_&& =>
-            return impliesRec(lhs, tree2) || impliesRec(rhs, tree2)
+            return impliesRec1(tree1, lhs) || impliesRec1(tree1, rhs)
           case _ => ()
       case _ => ()
 
     val tree1Normalized = QualifierNormalizer.normalize(QualifierEvaluator.evaluate(tree1))
     val tree2Normalized = QualifierNormalizer.normalize(QualifierEvaluator.evaluate(tree2))
 
-    tree2Normalized match
-      case Literal(Constant(true)) =>
-        return true
+    val eqs = topLevelEqualities(tree1Normalized)
+    if !eqs.isEmpty then
+      val (tree1Rewritten, tree2Rewritten) = rewriteEquivalences(tree1Normalized, tree2Normalized, eqs)
+      return impliesRec2(QualifierNormalizer.normalize(tree1Rewritten), QualifierNormalizer.normalize(tree2Rewritten))
+
+    impliesRec2(tree1Normalized, tree2Normalized)
+
+  def impliesRec2(tree1: Tree, tree2: Tree): Boolean =
+    // tree1 = lhs && rhs
+    tree1 match
+      case Apply(select @ Select(lhs, name), List(rhs)) =>
+        select.symbol match
+          case d.Boolean_&& =>
+            return impliesRec2(lhs, tree2) || impliesRec2(rhs, tree2)
+          case _ => ()
       case _ => ()
 
-    tree1Normalized match
+    tree1 match
       case Literal(Constant(false)) =>
         return true
       case _ => ()
 
-    QualifierAlphaComparer().iso(tree1Normalized, tree2Normalized)
+    tree2 match
+      case Literal(Constant(true)) =>
+        return true
+      case _ => ()
+
+    QualifierAlphaComparer().iso(tree1, tree2)
+
+  private def topLevelEqualities(tree: Tree): List[(Tree, Tree)] =
+    trace(i"topLevelEqualities $tree", Printers.qualifiedTypes):
+      topLevelEqualitiesImpl(tree)
+
+  private def topLevelEqualitiesImpl(tree: Tree): List[(Tree, Tree)] =
+    val d = defn
+    tree match
+      case Apply(select @ Select(lhs, name), List(rhs)) =>
+        select.symbol match
+          case d.Int_== | d.Any_== | d.Boolean_== => List((lhs, rhs))
+          case d.Boolean_&& => topLevelEqualitiesImpl(lhs) ++ topLevelEqualitiesImpl(rhs)
+          case _ => Nil
+      case _ =>
+        Nil
+
+  private def rewriteEquivalences(tree1: Tree, tree2: Tree, eqs: List[(Tree, Tree)]): (Tree, Tree) =
+    trace(i"rewriteEquivalences $tree1, $tree2, $eqs", Printers.qualifiedTypes):
+      val egraph = QualifierEGraph()
+      for (lhs, rhs) <- eqs do
+        egraph.union(lhs, rhs)
+      egraph.repair()
+      (egraph.rewrite(tree1), egraph.rewrite(tree2))
+
---
+++ tests/pos-custom-args/qualified-types/subtyping_equalities.scala
@@ -0,0 +1,31 @@
+def f(x: Int): Int = ???
+def g(x: Int): Int = ???
+def f2(x: Int, y: Int): Int = ???
+def g2(x: Int, y: Int): Int = ???
+
+def test: Unit =
+  val a: Int = ???
+  val b: Int = ???
+  val c: Int = ???
+  val d: Int = ???
+
+  // Equality is reflexive, symmetric and transitive
+  summon[{v: Int with v == v} <:< {v: Int with true}]
+  summon[{v: Int with v == a} <:< {v: Int with v == a}]
+  summon[{v: Int with v == a} <:< {v: Int with a == v}]
+  summon[{v: Int with a == b} <:< {v: Int with b == a}]
+  summon[{v: Int with v == a && a > 3} <:< {v: Int with v > 3}]
+  summon[{v: Int with v == a && a == b} <:< {v: Int with v == b}]
+  summon[{v: Int with a == b && b == c} <:< {v: Int with a == c}]
+  summon[{v: Int with a == b && c == b} <:< {v: Int with a == c}]
+  summon[{v: Int with a == b && c == d && b == d} <:< {v: Int with b == d}]
+  summon[{v: Int with a == b && c == d && b == d} <:< {v: Int with a == c}]
+
+  // Equality is congruent over functions
+  summon[{v: Int with a == b} <:< {v: Int with f(a) == f(b)}]
+  summon[{v: Int with a == b} <:< {v: Int with f(f(a)) == f(f(b))}]
+  summon[{v: Int with c == f(a) && d == f(b) && a == b} <:< {v: Int with c == d}]
+  // the two first equalities in the premises are just used to test the behavior
+  // of the e-graph when `f(a)` and `f(b)` are inserted before `a == b`.
+  summon[{v: Int with c == f(a) && d == f(b) && a == b} <:< {v: Int with f(a) == f(b)}]
+  summon[{v: Int with c == f(a) && d == f(b) && a == b} <:< {v: Int with f(f(a)) == f(f(b))}]
