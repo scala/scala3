som-snytt/dotty

Warn if -Vprint is not a phase

compiler/src/dotty/tools/dotc/transform/Recheck.scala
@@ -180,7 +180,7 @@ abstract class Recheck extends Phase, SymTransformer:
      *  can be retrieved with `knownType`
      */
     private val keepAllTypes = inContext(ictx) {
-      ictx.settings.Xprint.value.containsPhase(thisPhase)
+      ictx.settings.Vprint.value.containsPhase(thisPhase)
     }
 
     /** Should type of `tree` be kept in an attachment so that it can be retrieved with

scala/scala3

compiler/src/dotty/tools/dotc/transform/Recheck.scala
@@ -183,10 +183,10 @@ abstract class Recheck extends Phase, SymTransformer:
 
     /** If true, remember the new types of nodes in this compilation unit
      *  as an attachment in the unit's tpdTree node. By default, this is
-     *  enabled when -Xprint:cc is set. Can be overridden.
+     *  enabled when -Vprint:cc is set. Can be overridden.
      */
     def keepNuTypes(using Context): Boolean =
-      ctx.settings.Xprint.value.containsPhase(thisPhase)
+      ctx.settings.Vprint.value.containsPhase(thisPhase)
 
     def resetNuTypes()(using Context): Unit =
       nuTypes.clear(resetToInitial = false)