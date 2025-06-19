b1da8fc156823c8f79cc2f12d79065f4b46caf94
Wojciech Mazur <wmazur@virtuslab.com>
2025-06-19 13:59:46 +0200

    [chore]: Setup development for 3.7.3 cycle (#23395)
    We're starting developement cycle for 3.7.3 
    We're starting to use 3.7.2-RC1 as reference compiler

--- project/Build.scala
+++ project/Build.scala
@@ -102,7 +102,7 @@ object Build {
    *
    *  Warning: Change of this variable needs to be consulted with `expectedTastyVersion`
    */
-  val referenceVersion = "3.7.1"
+  val referenceVersion = "3.7.2-RC1"
 
   /** Version of the Scala compiler targeted in the current release cycle
    *  Contains a version without RC/SNAPSHOT/NIGHTLY specific suffixes
@@ -113,7 +113,7 @@ object Build {
    *
    *  Warning: Change of this variable might require updating `expectedTastyVersion`
    */
-  val developedVersion = "3.7.2"
+  val developedVersion = "3.7.3"
 
   /** The version of the compiler including the RC prefix.
    *  Defined as common base before calculating environment specific suffixes in `dottyVersion`
