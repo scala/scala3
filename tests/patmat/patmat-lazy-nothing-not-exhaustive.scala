0696aeca8bfeeab62cd85cf99a93e6480b4c82bd
Alexander <azazf93@gmail.com>
2025-06-22 19:36:25 +0300

    move test to patmat

--- tests/patmat/patmat-lazy-nothing-not-exhaustive.scala
+++ tests/patmat/patmat-lazy-nothing-not-exhaustive.scala
@@ -4,9 +4,7 @@ case class Bar() extends Adt {
   lazy val x: Nothing = throw new Exception()
 }
 
-inline def id[A](a: A): A = a
-
 def shouldThrowAWarning(x: Adt) =
-  id(x match { // warn
-    case Foo() => "Bar"
-  })
+  x match { // warn
+    case Foo() => "Foo"
+  }
