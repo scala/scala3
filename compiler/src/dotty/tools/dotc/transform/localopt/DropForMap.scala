240df1c471df7aa18554ea48e39a3df021e34eed
Som Snytt <som.snytt@gmail.com>
2025-06-24 09:02:53 -0700

    Elide conversion of receiver in DropForMap

--- compiler/src/dotty/tools/dotc/transform/localopt/DropForMap.scala
+++ compiler/src/dotty/tools/dotc/transform/localopt/DropForMap.scala
@@ -2,8 +2,9 @@ package dotty.tools.dotc
 package transform.localopt
 
 import dotty.tools.dotc.ast.tpd.*
-import dotty.tools.dotc.core.Decorators.*
 import dotty.tools.dotc.core.Contexts.*
+import dotty.tools.dotc.core.Decorators.*
+import dotty.tools.dotc.core.Flags.*
 import dotty.tools.dotc.core.StdNames.*
 import dotty.tools.dotc.core.Symbols.*
 import dotty.tools.dotc.core.Types.*
@@ -25,14 +26,20 @@ class DropForMap extends MiniPhase:
   override def description: String = DropForMap.description
 
   override def transformApply(tree: Apply)(using Context): Tree =
-    if !tree.hasAttachment(desugar.TrailingForMap) then tree
-    else tree match
-      case aply @ Apply(MapCall(f), List(Lambda(List(param), body)))
-      if f.tpe =:= aply.tpe => // make sure that the type of the expression won't change
-        f // drop the map call
+    tree.removeAttachment(desugar.TrailingForMap) match
+    case Some(_) =>
+      tree match
+      case aply @ Apply(MapCall(f), List(Lambda(List(param), body))) =>
+        if f.tpe =:= aply.tpe then // make sure that the type of the expression won't change
+          return f // drop the map call
+        else
+          f match
+          case Converted(r) if r.tpe =:= aply.tpe =>
+            return r // drop the map call and the conversion
+          case _ =>
       case _ =>
-        tree.removeAttachment(desugar.TrailingForMap)
-        tree
+    case _ =>
+    tree
 
   private object Lambda:
     def unapply(tree: Tree)(using Context): Option[(List[ValDef], Tree)] =
@@ -46,6 +53,15 @@ class DropForMap extends MiniPhase:
     def unapply(tree: Tree)(using Context): Option[Tree] = tree match
       case Select(f, nme.map) => Some(f)
       case Apply(fn, _) => unapply(fn)
+      case TypeApply(fn, _) => unapply(fn)
+      case _ => None
+
+  private object Converted:
+    def unapply(tree: Tree)(using Context): Option[Tree] = tree match
+      case Apply(fn @ Apply(_, _), _) => unapply(fn)
+      case Apply(fn, r :: Nil)
+      if fn.symbol.is(Implicit) || fn.symbol.name == nme.apply && fn.symbol.owner.derivesFrom(defn.ConversionClass)
+      => Some(r)
       case TypeApply(fn, _) => unapply(fn)
       case _ => None
 
