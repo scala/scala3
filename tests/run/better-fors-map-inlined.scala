c18243e1102533c2365a3f0df16ad3771deee5ca
Som Snytt <som.snytt@gmail.com>
2025-06-24 09:02:53 -0700

    DropForMap conversion, extension, inline, args

--- tests/run/better-fors-map-inlined.scala
+++ tests/run/better-fors-map-inlined.scala
@@ -1,8 +1,10 @@
+//> using options -preview
+
 enum MyOption[+A]:
   case MySome(x: A)
   case MyNone
 
-  def map[B](f: A => B): MyOption[B] =
+  inline def map[B](f: A => B): MyOption[B] =
     this match
     case MySome(x) => ???  //MySome(f(x))
     case MyNone => ??? //MyNone
@@ -38,7 +40,3 @@ object MyOption:
       a <- MyOption(1)
       (b, (c, d)) <- MyOption((2, (3, 4)))
     yield (b, (c, d))
-
-  extension (i: Int) def map[A](f: Int => A): A = ???
-
-  val _ = for j <- 42 yield j
