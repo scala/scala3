fcf87926a8898da1fbc8e7e38c6b2e135c349e19
Matt Bovel <matthieu@bovel.net>
2025-06-25 12:23:26 +0000

    Move normalization and comparison logic to the E-Graph

--- tests/pos-custom-args/qualified-types/subtyping_comparisons.scala
+++ tests/pos-custom-args/qualified-types/subtyping_comparisons.scala
@@ -2,12 +2,17 @@ def tp[T](): Boolean = ???
 
 class Outer:
   class Inner:
-    class D
+    type D
     summon[{v: Boolean with tp[Inner.this.D]()} =:= {v: Boolean with tp[D]()}]
 
 object OuterO:
   object InnerO:
-    class D
+    type D
     summon[{v: Boolean with tp[InnerO.this.D]()} =:= {v: Boolean with tp[D]()}]
+
+    // Before normalization:
+    // lhs: <empty>.this.OuterO$.this.InnerO$.this.D
+    // rhs: <empty>.this.OuterO$.this.InnerO.D
     summon[{v: Boolean with tp[InnerO.D]()} =:= {v: Boolean with tp[D]()}]
+
     summon[{v: Boolean with tp[OuterO.InnerO.D]()} =:= {v: Boolean with tp[D]()}]
