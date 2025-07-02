2a6e409c904628e6750a284b13c47d0582b4c798
Seyon Sivatharan <sivatharanseyon@gmail.com>
2025-07-01 20:17:24 -0400

    Add quick fix to remove unnecessary .nn

--- compiler/src/dotty/tools/dotc/reporting/ErrorMessageID.scala
+++ compiler/src/dotty/tools/dotc/reporting/ErrorMessageID.scala
@@ -229,6 +229,7 @@ enum ErrorMessageID(val isActive: Boolean = true) extends java.lang.Enum[ErrorMe
   case PointlessAppliedConstructorTypeID // errorNumber: 213
   case IllegalContextBoundsID // errorNumber: 214
   case NamedPatternNotApplicableID // errorNumber: 215
+  case UnnecessaryNN // errorNumber: 216
 
   def errorNumber = ordinal - 1
 
