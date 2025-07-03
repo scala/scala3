65cf6036dd9a2c88cabd4640f74016aaff464345
Matt Bovel <matthieu@bovel.net>
2025-07-03 14:55:05 +0000

    Add support for field accesses and constructors

--- tests/neg-custom-args/qualified-types/adapt_neg.scala
+++ tests/neg-custom-args/qualified-types/adapt_neg.scala
@@ -12,11 +12,11 @@ def test: Unit =
   val v3: {v: Int with v == x + 1} = x + 2 // error
   val v4: {v: Int with v == f(x)} = g(x) // error
   val v5: {v: Int with v == g(x)} = f(x) // error
-  //val v6: {v: Int with v == IntBox(x)} = IntBox(x) // Not implemented
-  //val v7: {v: Int with v == Box(x)} = Box(x) // Not implemented
+  val v6: {v: IntBox with v == IntBox(x)} = IntBox(x + 1) // error
+  val v7: {v: Box[Int] with v == Box(x)} = Box(x + 1) // error
   val v8: {v: Int with v == x + f(x)} = x + g(x) // error
   val v9: {v: Int with v == x + g(x)} = x + f(x) // error
   val v10: {v: Int with v == f(x + 1)} = f(x + 2) // error
   val v11: {v: Int with v == g(x + 1)} = g(x + 2) // error
-  //val v12: {v: Int with v == IntBox(x + 1)} = IntBox(x + 1) // Not implemented
-  //val v13: {v: Int with v == Box(x + 1)} = Box(x + 1) // Not implemented
+  val v12: {v: IntBox with v == IntBox(x + 1)} = IntBox(x) // error
+  val v13: {v: Box[Int] with v == Box(x + 1)} = Box(x) // error
---
+++ tests/neg-custom-args/qualified-types/subtyping_objects_neg.scala
@@ -0,0 +1,52 @@
+class Box[T](val x: T)
+
+class BoxMutable[T](var x: T)
+
+class Foo(val id: String):
+  def this(x: Int) = this(x.toString)
+
+class Person(val name: String, val age: Int)
+
+class PersonCurried(val name: String)(val age: Int)
+
+class PersonMutable(val name: String, var age: Int)
+
+case class PersonCaseMutable(name: String, var age: Int)
+
+case class PersonCaseSecondary(name: String, age: Int):
+  def this(name: String) = this(name, 0)
+
+case class PersonCaseEqualsOverriden(name: String, age: Int):
+  override def equals(that: Object): Boolean = this eq that
+
+def test: Unit =
+  summon[{b: Box[Int] with b == Box(1)} =:= {b: Box[Int] with b == Box(1)}] // error
+
+  summon[{b: BoxMutable[Int] with b == BoxMutable(1)} =:= {b: BoxMutable[Int] with b == BoxMutable(1)}] // error
+  // TODO(mbovel): restrict selection to stable members
+  //summon[{b: BoxMutable[Int] with b.x == 3} =:= {b: BoxMutable[Int] with b.x == 3}]
+
+  summon[{f: Foo with f == Foo("hello")} =:= {f: Foo with f == Foo("hello")}] // error
+  summon[{f: Foo with f == Foo(1)} =:= {f: Foo with f == Foo(1)}] // error
+  summon[{s: String with Foo("hello").id == s} =:= {s: String with s == "hello"}] // error
+
+  summon[{p: Person with p == Person("Alice", 30)} =:= {p: Person with p == Person("Alice", 30)}] // error
+  summon[{s: String with Person("Alice", 30).name == s} =:= {s: String with s == "Alice"}] // error
+  summon[{n: Int with Person("Alice", 30).age == n} =:= {n: Int with n == 30}] // error
+
+  summon[{p: PersonCurried with p == PersonCurried("Alice")(30)} =:= {p: PersonCurried with p == PersonCurried("Alice")(30)}] // error
+  summon[{s: String with PersonCurried("Alice")(30).name == s} =:= {s: String with s == "Alice"}] // error
+  summon[{n: Int with PersonCurried("Alice")(30).age == n} =:= {n: Int with n == 30}] // error
+
+  summon[{p: PersonMutable with p == PersonMutable("Alice", 30)} =:= {p: PersonMutable with p == PersonMutable("Alice", 30)}] // error
+  summon[{s: String with PersonMutable("Alice", 30).name == s} =:= {s: String with s == "Alice"}] // error
+  summon[{n: Int with PersonMutable("Alice", 30).age == n} =:= {n: Int with n == 30}] // error
+
+  summon[{n: Int with PersonCaseMutable("Alice", 30).age == n} =:= {n: Int with n == 30}] // error
+
+  summon[{s: String with new PersonCaseSecondary("Alice").name == s} =:= {s: String with s == "Alice"}] // error
+  summon[{n: Int with new PersonCaseSecondary("Alice").age == n} =:= {n: Int with n == 0}] // error
+
+  summon[{p: PersonCaseEqualsOverriden with PersonCaseEqualsOverriden("Alice", 30) == p} =:= {p: PersonCaseEqualsOverriden with p == PersonCaseEqualsOverriden("Alice", 30)}] // error
+  summon[{s: String with PersonCaseEqualsOverriden("Alice", 30).name == s} =:= {s: String with s == "Alice"}] // error
+  summon[{n: Int with PersonCaseEqualsOverriden("Alice", 30).age == n} =:= {n: Int with n == 30}] // error
