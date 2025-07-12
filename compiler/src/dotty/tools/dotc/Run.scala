lampepfl/dotty

-Vphases takes names to mark, -Vprint is typer
Distinguish whether a setting was "set" by the user.
-Vphases takes names to append an asterisk for easy scanning,
and defaults to "none" to mark; -Vprint defaults to "typer".
Both options are used if the user used them: setting.isPresent.
If the setting value is not supplied, use a nonempty default.
An "empty" default, such as an empty string or list, would
require an explicit arg from the user.

compiler/src/dotty/tools/dotc/Run.scala
@@ -245,7 +245,7 @@ class Run(comp: Compiler, ictx: Context) extends ImplicitRunInfo with Constraint
             val profileBefore = profiler.beforePhase(phase)
             units = phase.runOn(units)
             profiler.afterPhase(phase, profileBefore)
-            if (ctx.settings.Xprint.value.containsPhase(phase))
+            for (printAt <- ctx.settings.Xprint.userValue if printAt.containsPhase(phase))
               for (unit <- units)
                 lastPrintedTree =
                   printTree(lastPrintedTree)(using ctx.fresh.setPhase(phase.next).setCompilationUnit(unit))

scala/scala3

compiler/src/dotty/tools/dotc/Run.scala
@@ -367,7 +367,7 @@ extends ImplicitRunInfo, ConstraintRunInfo, cc.CaptureRunInfo {
             profiler.onPhase(phase):
               try units = phase.runOn(units)
               catch case _: InterruptedException => cancelInterrupted()
-            if (ctx.settings.Vprint.value.containsPhase(phase))
+            for (printAt <- ctx.settings.Vprint.userValue if printAt.containsPhase(phase))
               for (unit <- units)
                 def printCtx(unit: CompilationUnit) = phase.printingContext(
                   ctx.fresh.setPhase(phase.next).setCompilationUnit(unit))