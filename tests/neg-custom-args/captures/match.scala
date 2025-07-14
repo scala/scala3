e0256bdc0d8cb473b670a350d8263c8ee9cafe32
noti0na1 <noti0na1@users.noreply.github.com>
2025-07-14 16:24:26 +0200

    Enhance pattern matching with capturing types; fix stdlib-cc

---
+++ tests/neg-custom-args/captures/match.scala
@@ -0,0 +1,21 @@
+import language.experimental.captureChecking
+
+trait A
+
+case class B(x: AnyRef^) extends A
+
+def test =
+  val x: AnyRef^ = new AnyRef
+  val a: A^{x} = B(x)
+
+  val y1: A = a match
+    case b: B => b // error: (b: B) becomes B^{x} implicitly
+
+  val y2: A^{x} = a match
+    case b: B => b // ok
+
+  val x3: AnyRef = a match
+    case B(x2: AnyRef) => x2 // error: we lose some information about field x, but it still cannot be pure
+
+  val x4: AnyRef = a match
+    case b: B => b.x // error
