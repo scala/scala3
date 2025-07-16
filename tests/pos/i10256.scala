e064a1c07fd4e838fb691266ddfa6996301ecd36
Eugene Flesselle <eugene@flesselle.net>
2025-07-16 16:49:41 +0200

    Update tests specifically about distributing intersections

--- tests/pos/i10256.scala
+++ tests/pos/i10256.scala
@@ -2,6 +2,6 @@ trait Foo[T <: Foo[T]] {
   type I <: Foo[I]
 }
 
-trait Bar[T <: Foo[T]] extends Foo[T] { // error: cyclic
+trait Bar[T <: Foo[T]] extends Foo[T] { // was error: cyclic
   self: T =>
 }
