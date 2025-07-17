6b61b01209044c8faded133018ac9e1bf04eed35
odersky <odersky@gmail.com>
2025-07-17 17:26:55 +0200

    Update test

--- tests/neg-custom-args/captures/i23207.scala
+++ tests/neg-custom-args/captures/i23207.scala
@@ -13,6 +13,16 @@ def leak(io: AnyRef^): A =
   val b = new B
   val box = b.getBox
   val a: A = box.x // error
-  val c = b.getBox.x // now OK
-  val _: B^{b} = c
+  val c = b.getBox.x
+  val _: B^{b} = c // ok
+  val _: A = c // error
+  c // no error here since we don't propagate expected type into the last expression of a block
+    // and the whole block's span overlaps with previous errors
+
+def leak2(io: AnyRef^): A =
+  class B extends A:   // error, now we see the error for the whole block since there are no nested errors
+    val hide: AnyRef^{io} = io
+
+  val b = new B
+  val c = b.getBox.x
   c
