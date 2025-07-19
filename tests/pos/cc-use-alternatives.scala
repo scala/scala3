5a440b4a1ba7dd1cc03e9528d259de9e6c04ff7b
odersky <odersky@gmail.com>
2025-07-19 10:55:27 +0200

    Unrelated test

---
+++ tests/pos/cc-use-alternatives.scala
@@ -0,0 +1,24 @@
+import language.experimental.captureChecking
+// no separation checking
+import caps.{cap, use}
+
+def foo1(@use xs: List[() => Unit]): Unit =
+  var x: () ->{xs*} Unit = xs.head
+  var ys = xs
+  while ys.nonEmpty do
+    ys = ys.tail
+    x = ys.head
+
+def foo2(@use xs: List[() => Unit]): Unit =
+  var x: () => Unit = xs.head // note: this would fail separation checking
+  var ys = xs
+  while ys.nonEmpty do
+    ys = ys.tail
+    x = ys.head
+
+def foo3[@use C^](xs: List[() ->{C} Unit]): Unit =
+  var x: () ->{C} Unit = xs.head
+  var ys = xs
+  while ys.nonEmpty do
+    ys = ys.tail
+    x = ys.head
