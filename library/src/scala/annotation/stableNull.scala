2d74c8ca5f70ed3049d4fa55963172dacd0fe517
noti0na1 <noti0na1@users.noreply.github.com>
2025-07-20 20:28:50 +0800

    Fix documentation typo and update MiMaFilters

--- library/src/scala/annotation/stableNull.scala
+++ library/src/scala/annotation/stableNull.scala
@@ -1,7 +1,7 @@
 package scala.annotation
 
 /** An annotation that can be used to mark a mutable field as trackable for nullability.
- *  With explicit nulls, a normal mutable field can be tracked for nullability by flow typing,
+ *  With explicit nulls, a normal mutable field cannot be tracked for nullability by flow typing,
  *  since it can be updated to a null value at the same time.
  *  This annotation will force the compiler to track the field for nullability, as long as the
  *  prefix is a stable path.
