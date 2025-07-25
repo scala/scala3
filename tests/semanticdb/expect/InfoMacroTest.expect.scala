00d19dfd0906d2b4a8c076ded8af15cc3a6f2dc9
Tomasz Godzik <tgodzik@users.noreply.github.com>
2025-07-25 09:11:02 +0200

    bugfix: Also save infos in semanticdb (#23587)
    Previously, when user changed a warning to info, we would not save it
    and miss information if that warning was about unused. Now, we also save
    infos.
    
    https://github.com/scalacenter/scalafix/issues/2269

---
+++ tests/semanticdb/expect/InfoMacroTest.expect.scala
@@ -0,0 +1,7 @@
+
+object InfoMacroTest/*<-_empty_::InfoMacroTest.*/ {
+  def main/*<-_empty_::InfoMacroTest.main().*/(): Unit/*->scala::Unit#*/ = {
+    InfoMacro/*->_empty_::InfoMacro.*/.reportInfo/*->_empty_::InfoMacro.reportInfo().*/("This is a test info message")
+    InfoMacro/*->_empty_::InfoMacro.*/.reportInfo/*->_empty_::InfoMacro.reportInfo().*/("Another info message")
+  }
+}
