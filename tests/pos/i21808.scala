70733c76e9840a0bfd791f020f28751be8da26e9
TheDrawingCoding-Gamer <bulbyvr@gmail.com>
2025-07-28 15:25:51 -0400

    update tupled test

--- tests/pos/i21808.scala
+++ tests/pos/i21808.scala
@@ -7,4 +7,11 @@ object Test {
   type T
 
   summon[TupledFunction[(x: T, y: T) => x.type, (x: (T, T)) => x._1.type]]
+
+  // arity > 22
+  summon[TupledFunction[
+    (a: T, b: T, c: T, d: T, e: T, f: T, g: T, h: T, i: T, j: T, k: T, l: T, m: T, n: T, o: T, p: T,
+    q: T, r: T, s: T, t: T, u: T, v: T, w: T) => a.type,
+    (a: (T, T, T, T, T, T, T, T, T, T, T, T, T, T, T, T, T, T, T, T, T, T, T)) => Tuple.Elem[a.type, 0]
+    ]]
 }
