207604b03e824a4c223f81c2740e19ce6ae6b38e
Wojciech Mazur <wmazur@virtuslab.com>
2025-07-31 12:06:15 +0200

    Reenable better-fors under '-source:3.7 -preview' settings (#23636)
    Addendum to #23630 with missing adjustment for older source verisons

--- compiler/src/dotty/tools/dotc/config/SourceVersion.scala
+++ compiler/src/dotty/tools/dotc/config/SourceVersion.scala
@@ -43,7 +43,7 @@ enum SourceVersion:
   def enablesClauseInterleaving = isAtLeast(`3.6`)
   def enablesNewGivens = isAtLeast(`3.6`)
   def enablesNamedTuples = isAtLeast(`3.7`)
-  def enablesBetterFors = isAtLeast(`3.8`)
+  def enablesBetterFors(using Context) = isAtLeast(`3.8`) || (isAtLeast(`3.7`) && isPreviewEnabled)
 
   def requiresNewSyntax = isAtLeast(future)
 
