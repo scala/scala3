1be0e67dd60e6b865906d0fc3ad4e2a4f2bd327a
odersky <odersky@gmail.com>
2025-08-05 14:41:50 +0200

    Handle assertion error in TyperState
    #23609 triggers an assertion error in TyperState. The relevant explanation seems to be
    in ProtoTypes.scala:
    ```scala
                // To respect the pre-condition of `mergeConstraintWith` and keep
                // `protoTyperState` committable we must ensure that it does not
                // contain any type variable which don't already exist in the passed
                // TyperState. This is achieved by instantiating any such type
                // variable. NOTE: this does not suffice to discard type variables
                // in ancestors of `protoTyperState`, if this situation ever
                // comes up, an assertion in TyperState will trigger and this code
                // will need to be generalized.
    ```
    We should go to the bottom of it and fix the assertion. But before that's done this PR
    offers a temporary hack to catch the exception when it is triggered from a new code path
    created by PR #23532. This should fix the regression reported in #23609. We should leave
    the issue open as a reminder that we still need a better fix.
    
    Also: handle crash to missing span in a migration helper.

--- compiler/src/dotty/tools/dotc/typer/Typer.scala
+++ compiler/src/dotty/tools/dotc/typer/Typer.scala
@@ -4368,11 +4368,20 @@ class Typer(@constructorOnly nestingLevel: Int = 0) extends Namer
               || !isFullyDefined(formal, ForceDegree.none)
                     // more context might constrain type variables which could make implicit scope larger
 
+            // Try to constrain the result using `pt1`, but back out if a BadTyperStateAssertion
+            // is thrown. TODO Find out why the bad typer state arises and prevent it. The try-catch
+            // is a temporary hack to keep projects compiling that would fail otherwise due to
+            // searching more arguments to instantiate implicits (PR #23532). A failing project
+            // is described in issue #23609.
+            def tryConstrainResult(pt: Type): Boolean =
+              try constrainResult(tree.symbol, wtp, pt)
+              catch case ex: TyperState.BadTyperStateAssertion => false
+
             arg.tpe match
               case failed: SearchFailureType if canProfitFromMoreConstraints =>
                 val pt1 = pt.deepenProtoTrans
-                if (pt1 `ne` pt) && (pt1 ne sharpenedPt) && constrainResult(tree.symbol, wtp, pt1)
-                then return implicitArgs(formals, argIndex, pt1)
+                if (pt1 `ne` pt) && (pt1 ne sharpenedPt) && tryConstrainResult(pt1) then
+                  return implicitArgs(formals, argIndex, pt1)
               case _ =>
 
             arg.tpe match
