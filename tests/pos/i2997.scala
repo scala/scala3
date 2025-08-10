99e9408ad712b9bc269b6b601da389c9b912a383
Kacper Korban <kacper.f.korban@gmail.com>
2025-08-10 17:19:34 +0200

    Cleanup

--- tests/pos/i2997.scala
+++ tests/pos/i2997.scala
@@ -1,19 +1,9 @@
-// case class Foo[T <: Int with Singleton](t : T)
-
-// object Test {
-//   val one = 1
-//   final val final_one = 1
-//   val a : 1 = Foo(1).t
-//   val b : one.type = Foo(one).t
-//   val c : 1 = Foo(final_one).t
-// }
-
-case class Foo(t : Int)
-
-object Foo {
-  def apply(t0: Int) : Foo {val t : t0.type} = ???
-}
+case class Foo[T <: Int with Singleton](t : T)
 
 object Test {
+  val one = 1
+  final val final_one = 1
   val a : 1 = Foo(1).t
+  val b : one.type = Foo(one).t
+  val c : 1 = Foo(final_one).t
 }
