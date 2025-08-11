3b957f213c778b6ba591f9c5f87315abae78bf97
Natsu Kagami <nki@nkagami.me>
2025-08-11 02:12:27 +0200

    Update test outputs

--- tests/neg-custom-args/captures/boundary.scala
+++ tests/neg-custom-args/captures/boundary.scala
@@ -1,9 +1,8 @@
-import language.experimental.captureChecking
-
 import scala.util.boundary
 
 object test:
-  boundary[boundary.Label[Unit]]: l1 ?=> // error
-    boundary[Unit]: l2 ?=>
-      boundary.break(l2)(using l1) // error
-    ???
+  boundary[AnyRef^]:
+    l1 ?=> // error // error
+      boundary[Unit]: l2 ?=>
+        boundary.break(l2)(using l1)
+      ???
