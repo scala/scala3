4c289542d84d7672f45b84594ef217993543923c
Eugene Flesselle <eugene@flesselle.net>
2025-08-13 18:33:41 +0200

    Add pickling of match sub cases

--- tasty/src/dotty/tools/tasty/TastyFormat.scala
+++ tasty/src/dotty/tools/tasty/TastyFormat.scala
@@ -511,6 +511,7 @@ object TastyFormat {
   final val EMPTYCLAUSE = 45
   final val SPLITCLAUSE = 46
   final val TRACKED = 47
+  final val WITH = 48
 
   // Tree Cat. 2:    tag Nat
   final val firstNatTreeTag = SHAREDterm
