25ce3ec8bf85aeebefadc1fa817b44e2f3739417
Yichen Xu <yichen.x@outlook.com>
2025-08-14 17:16:52 +0200

    Capture check mutable collections done

--- library/src/scala/collection/mutable/IndexedSeq.scala
+++ library/src/scala/collection/mutable/IndexedSeq.scala
@@ -14,6 +14,7 @@ package scala.collection
 package mutable
 
 import scala.language.`2.13`
+import language.experimental.captureChecking
 
 trait IndexedSeq[T] extends Seq[T]
   with scala.collection.IndexedSeq[T]
