8f2fd148dd889e38da4174a512b03244d5008a85
Natsu Kagami <nki@nkagami.me>
2025-08-14 21:36:32 +0200

    Fix partial function test

--- tests/neg-custom-args/captures/partial-function.scala
+++ tests/neg-custom-args/captures/partial-function.scala
@@ -2,5 +2,5 @@ class A extends caps.SharedCapability:
   def even(x: Int) = x % 2 == 0
 
 def f(a: A): List[Int] -> List[Int] =
-  (xs: List[Int]) => xs.collect:
+  (xs: List[Int]) => xs.collect: // error
     case x if a.even(x) => 1
