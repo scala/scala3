4f4aa5b627897d15121cad2b7e2386fbae939d17
Natsu Kagami <nki@nkagami.me>
2025-08-15 18:00:20 +0200

    Remove TODO

--- mk_todo.fish
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env fish
-
-for file in (ls library/src/**.scala | sort)
-  set checkbox (if rg -q 'import (scala.)?language.experimental.captureChecking' $file; echo "[x]"; else; echo "[ ]"; end)
-  printf "- %s %s\n" $checkbox $file
-end
