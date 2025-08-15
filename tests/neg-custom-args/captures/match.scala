80eea30dea030b93418019a47ccb517eef9ca1b0
noti0na1 <noti0na1@users.noreply.github.com>
2025-08-15 15:02:28 +0200

    Update test
    [Cherry-picked d140083bb8ba3605499aa8fbd3215d7dba4c0184]

--- tests/neg-custom-args/captures/match.scala
+++ tests/neg-custom-args/captures/match.scala
@@ -12,7 +12,10 @@ def test =
     case b: B => b // error: (b: B) becomes B^{x} implicitly
 
   val y2: A^{x} = a match
-    case b: B => b // ok
+    case b: B =>
+      val bb: B^{b} = b
+      val aa: A^{a} = bb
+      b // ok
 
   val x3: AnyRef = a match
     case B(x2: AnyRef) => x2 // error: we lose some information about field x, but it still cannot be pure
