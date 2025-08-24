5778bea2f37a08eb5a8fdea4592164a92b50be2b
Matthias Berndt <matthias_berndt@gmx.de>
2025-08-24 23:26:54 +0200

    Implement SIP-67: strictEquality pattern matching (Github issue #22732)

---
+++ compiler/test/dotty/tools/dotc/typer/SIP67Tests.scala
@@ -0,0 +1,52 @@
+// filepath: /home/mberndt/scala3/compiler/test/dotty/tools/dotc/typer/SIP67Tests.scala
+package dotty.tools.dotc.typer
+
+import dotty.tools.DottyTest
+import dotty.tools.dotc.core.Contexts.*
+
+import org.junit.Test
+import org.junit.Assert.fail
+
+class SIP67Tests extends DottyTest {
+
+
+  @Test
+  def sip67test1: Unit = {
+    val source = """
+    import scala.language.strictEquality
+    enum Foo {
+      case Bar
+    }
+
+    val _ =
+      (??? : Foo) match {
+        case Foo.Bar =>
+      }
+    """
+    val ctx = checkCompile("typer", source) { (_, ctx) => }
+    if ctx.reporter.hasErrors then
+      fail("Unexpected compilation errors were reported")
+  }
+
+  @Test
+  def sip67test2: Unit = {
+    val source = """
+    import scala.language.strictEquality
+
+    sealed trait Foo
+
+    object Foo {
+      case object Bar extends Foo
+    }
+
+    val _ =
+      (??? : Foo) match {
+        case Foo.Bar =>
+      }
+    """
+    val ctx = checkCompile("typer", source) { (_, ctx) => }
+    if ctx.reporter.hasErrors then
+      fail("Unexpected compilation errors were reported")
+  }
+
+}
