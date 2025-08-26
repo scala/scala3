0a565d0caa6552646330e403ac386bbe568da908
Matthias Berndt <matthias_berndt@gmx.de>
2025-08-26 22:56:35 +0200

    use braceless syntax in SIP67Tests

--- compiler/test/dotty/tools/dotc/typer/SIP67Tests.scala
+++ compiler/test/dotty/tools/dotc/typer/SIP67Tests.scala
@@ -1,4 +1,3 @@
-// filepath: /home/mberndt/scala3/compiler/test/dotty/tools/dotc/typer/SIP67Tests.scala
 package dotty.tools.dotc.typer
 
 import dotty.tools.DottyTest
@@ -7,46 +6,38 @@ import dotty.tools.dotc.core.Contexts.*
 import org.junit.Test
 import org.junit.Assert.fail
 
-class SIP67Tests extends DottyTest {
-
+class SIP67Tests extends DottyTest:
 
   @Test
-  def sip67test1: Unit = {
+  def sip67test1: Unit =
     val source = """
     import scala.language.strictEquality
-    enum Foo {
+    enum Foo:
       case Bar
-    }
 
     val _ =
-      (??? : Foo) match {
+      (??? : Foo) match
         case Foo.Bar =>
-      }
     """
-    val ctx = checkCompile("typer", source) { (_, ctx) => }
+    val ctx = checkCompile("typer", source)((_, ctx) => ())
+
     if ctx.reporter.hasErrors then
       fail("Unexpected compilation errors were reported")
-  }
 
   @Test
-  def sip67test2: Unit = {
+  def sip67test2: Unit =
     val source = """
     import scala.language.strictEquality
 
     sealed trait Foo
 
-    object Foo {
+    object Foo:
       case object Bar extends Foo
-    }
 
     val _ =
-      (??? : Foo) match {
+      (??? : Foo) match
         case Foo.Bar =>
-      }
     """
-    val ctx = checkCompile("typer", source) { (_, ctx) => }
+    val ctx = checkCompile("typer", source)((_, ctx) => ())
     if ctx.reporter.hasErrors then
       fail("Unexpected compilation errors were reported")
-  }
-
-}
