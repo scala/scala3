1cc636dbaf36595894a08898362f8da381225cfe
Jan Chyb <jan.chyb.99@gmail.com>
2025-08-27 12:35:50 +0200

    Stabilise Quotes reflect methods used for creating new classes/objects

--- tests/run-macros/newClassAnnotation/Test_2.scala
+++ tests/run-macros/newClassAnnotation/Test_2.scala
@@ -1,5 +1,3 @@
-//> using options -experimental
-
 @main def Test =
   val (cls, str) = makeClass("name")
   println(str)
--- tests/run-macros/newClassParams/Test_2.scala
+++ tests/run-macros/newClassParams/Test_2.scala
@@ -1,5 +1,3 @@
-//> using options -experimental
-
 @main def Test: Unit = {
   makeClassAndCall("bar", 10, "test")
 }
