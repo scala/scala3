6570fb2b3f47fd42d47c41b78510c9d169deeda2
Matthias Berndt <matthias_berndt@gmx.de>
2025-09-01 17:31:50 +0200

    minor refactoring of SIP67Tests

--- compiler/test/dotty/tools/dotc/typer/SIP67Tests.scala
+++ compiler/test/dotty/tools/dotc/typer/SIP67Tests.scala
@@ -8,38 +8,38 @@ import org.junit.Assert.fail
 
 class SIP67Tests extends DottyTest:
 
+  private def checkNoErrors(source: String): Unit =
+    val ctx = checkCompile("typer", source)((_, _) => ())
+    if ctx.reporter.hasErrors then
+      fail("Unexpected compilation errors were reported")
+  
   @Test
   def sip67test1: Unit =
-    val source = """
-    import scala.language.strictEquality
-    import scala.language.experimental.strictEqualityPatternMatching
-    enum Foo:
-      case Bar
+    checkNoErrors:
+      """
+      import scala.language.strictEquality
+      import scala.language.experimental.strictEqualityPatternMatching
+      enum Foo:
+        case Bar
 
-    val _ =
-      (??? : Foo) match
-        case Foo.Bar =>
-    """
-    val ctx = checkCompile("typer", source)((_, ctx) => ())
-
-    if ctx.reporter.hasErrors then
-      fail("Unexpected compilation errors were reported")
-
+      val _ =
+        (??? : Foo) match
+          case Foo.Bar =>
+      """
   @Test
   def sip67test2: Unit =
-    val source = """
-    import scala.language.strictEquality
-    import scala.language.experimental.strictEqualityPatternMatching
+    checkNoErrors:
+      """
+      import scala.language.strictEquality
+      import scala.language.experimental.strictEqualityPatternMatching
 
-    sealed trait Foo
+      sealed trait Foo
 
-    object Foo:
-      case object Bar extends Foo
+      object Foo:
+        case object Bar extends Foo
 
-    val _ =
-      (??? : Foo) match
-        case Foo.Bar =>
-    """
-    val ctx = checkCompile("typer", source)((_, ctx) => ())
-    if ctx.reporter.hasErrors then
-      fail("Unexpected compilation errors were reported")
+      val _ =
+        (??? : Foo) match
+          case Foo.Bar =>
+      """
+  
