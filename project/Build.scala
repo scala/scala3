2562be11633c27d440a47192782f3cd312cb874b
Li Haoyi <haoyi.sg@gmail.com>
2025-09-01 10:39:19 +0800

    .

--- project/Build.scala
+++ project/Build.scala
@@ -675,6 +675,49 @@ object Build {
     recur(lines)
   }
 
+  val shadedSourceGenerator = (Compile / sourceGenerators) += Def.task {
+    val downloads = Seq(
+      "https://repo1.maven.org/maven2/com/lihaoyi/pprint_3/0.9.3/pprint_3-0.9.3-sources.jar",
+      "https://repo1.maven.org/maven2/com/lihaoyi/fansi_3/0.5.1/fansi_3-0.5.1-sources.jar",
+      "https://repo1.maven.org/maven2/com/lihaoyi/sourcecode_3/0.4.3-M5/sourcecode_3-0.4.3-M5-sources.jar",
+    )
+    val sourceManagedPath = os.Path((Compile / sourceManaged).value / "downloaded")
+    os.remove.all(sourceManagedPath)
+    os.makeDir.all(sourceManagedPath)
+    for(download <- downloads) {
+      os.unzip.stream(requests.get.stream(download), dest = sourceManagedPath)
+    }
+    for {
+      file <- os.walk(sourceManagedPath)
+      if file.ext == "scala"
+      if file.last != "CollectionName.scala"
+    } yield {
+      val text = os.read(file)
+      os.write.over(
+        file,
+        "package dotty.shaded\n" +
+          text
+            .replace("import scala", "import _root_.scala")
+            .replace(" scala.collection.", " _root_.scala.collection.")
+            .replace("_root_.pprint", "_root_.dotty.shaded.pprint")
+            .replace("_root_.fansi", "_root_.dotty.shaded.fansi")
+            .replace("def apply(c: Char): Trie[T]", "def apply(c: Char): Trie[T] | Null")
+            .replace("var head: Iterator[T] = null", "var head: Iterator[T] | Null = null")
+            .replace("if (head != null && head.hasNext) true", "if (head != null && head.nn.hasNext) true")
+            .replace("head.next()", "head.nn.next()")
+            .replace(
+              "_root_.scala.collection.internal.pprint.CollectionName.get(i)",
+              "import scala.reflect.Selectable.reflectiveSelectable\n" +
+                "    i.asInstanceOf[{def collectionClassName: String}].collectionClassName"
+            )
+            .replace("abstract class Walker", "@scala.annotation.nowarn abstract class Walker")
+            .replace("object TPrintLowPri", "@scala.annotation.nowarn object TPrintLowPri")
+        //                .replace("_root_.scala.collection.internal.pprint.CollectionName.get(i)", "head.nn.next()")
+        //                .replace("_root_.scala.collection.internal.pprint.CollectionName.get(i)", "head.nn.next()")
+      )
+      file.toIO
+    }
+  }.taskValue
   // Settings shared between scala3-compiler and scala3-compiler-bootstrapped
   lazy val commonDottyCompilerSettings = Seq(
       // Note: bench/profiles/projects.yml should be updated accordingly.
@@ -721,49 +764,7 @@ object Build {
         ("io.get-coursier" %% "coursier" % "2.0.16" % Test).cross(CrossVersion.for3Use2_13),
       ),
 
-      (Compile / sourceGenerators) += Def.task {
-        val downloads = Seq(
-          "https://repo1.maven.org/maven2/com/lihaoyi/pprint_3/0.9.3/pprint_3-0.9.3-sources.jar",
-          "https://repo1.maven.org/maven2/com/lihaoyi/fansi_3/0.5.1/fansi_3-0.5.1-sources.jar",
-          "https://repo1.maven.org/maven2/com/lihaoyi/sourcecode_3/0.4.3-M5/sourcecode_3-0.4.3-M5-sources.jar",
-        )
-        val sourceManagedPath = os.Path((Compile / sourceManaged).value / "downloaded")
-        os.remove.all(sourceManagedPath)
-        os.makeDir.all(sourceManagedPath)
-        for(download <- downloads) {
-          os.unzip.stream(requests.get.stream(download), dest = sourceManagedPath)
-        }
-        for {
-          file <- os.walk(sourceManagedPath)
-          if file.ext == "scala"
-          if file.last != "CollectionName.scala"
-        } yield {
-          val text = os.read(file)
-          os.write.over(
-            file,
-            "package dotty.shaded\n" +
-              text
-                .replace("import scala", "import _root_.scala")
-                .replace(" scala.collection.", " _root_.scala.collection.")
-                .replace("_root_.pprint", "_root_.dotty.shaded.pprint")
-                .replace("_root_.fansi", "_root_.dotty.shaded.fansi")
-                .replace("def apply(c: Char): Trie[T]", "def apply(c: Char): Trie[T] | Null")
-                .replace("var head: Iterator[T] = null", "var head: Iterator[T] | Null = null")
-                .replace("if (head != null && head.hasNext) true", "if (head != null && head.nn.hasNext) true")
-                .replace("head.next()", "head.nn.next()")
-                .replace(
-                  "_root_.scala.collection.internal.pprint.CollectionName.get(i)",
-                  "import scala.reflect.Selectable.reflectiveSelectable\n" +
-                    "    i.asInstanceOf[{def collectionClassName: String}].collectionClassName"
-                )
-                .replace("abstract class Walker", "@scala.annotation.nowarn abstract class Walker")
-                .replace("object TPrintLowPri", "@scala.annotation.nowarn object TPrintLowPri")
-//                .replace("_root_.scala.collection.internal.pprint.CollectionName.get(i)", "head.nn.next()")
-//                .replace("_root_.scala.collection.internal.pprint.CollectionName.get(i)", "head.nn.next()")
-          )
-          file.toIO
-        }
-      }.taskValue,
+      shadedSourceGenerator,
 
       // For convenience, change the baseDirectory when running the compiler
       Compile / forkOptions := (Compile / forkOptions).value.withWorkingDirectory((ThisBuild / baseDirectory).value),
@@ -2185,6 +2186,7 @@ object Build {
 
         Seq(file)
       }.taskValue,
+      shadedSourceGenerator,
       // sbt adds all the projects to scala-tool config which breaks building the scalaInstance
       // as a workaround, I build it manually by only adding the compiler
       scalaInstance := {
