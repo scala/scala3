85f73ce84ae08461ce336f8a36bb01545ded35c2
Li Haoyi <haoyi.sg@gmail.com>
2025-09-01 11:03:05 +0800

    .

--- project/Build.scala
+++ project/Build.scala
@@ -681,42 +681,96 @@ object Build {
       "https://repo1.maven.org/maven2/com/lihaoyi/fansi_3/0.5.1/fansi_3-0.5.1-sources.jar",
       "https://repo1.maven.org/maven2/com/lihaoyi/sourcecode_3/0.4.3-M5/sourcecode_3-0.4.3-M5-sources.jar",
     )
-    val sourceManagedPath = os.Path((Compile / sourceManaged).value / "downloaded")
-    os.remove.all(sourceManagedPath)
-    os.makeDir.all(sourceManagedPath)
-    for(download <- downloads) {
-      os.unzip.stream(requests.get.stream(download), dest = sourceManagedPath)
+    val dest = ((Compile / sourceManaged).value / "downloaded").toPath
+    if (Files.exists(dest)) {
+      Files.walk(dest)
+        .sorted(java.util.Comparator.reverseOrder()) // delete children before parents
+        .forEach(p => Files.delete(p));
     }
-    for {
-      file <- os.walk(sourceManagedPath)
-      if file.ext == "scala"
-      if file.last != "CollectionName.scala"
-    } yield {
-      val text = os.read(file)
-      os.write.over(
-        file,
-        "package dotty.shaded\n" +
-          text
-            .replace("import scala", "import _root_.scala")
-            .replace(" scala.collection.", " _root_.scala.collection.")
-            .replace("_root_.pprint", "_root_.dotty.shaded.pprint")
-            .replace("_root_.fansi", "_root_.dotty.shaded.fansi")
-            .replace("def apply(c: Char): Trie[T]", "def apply(c: Char): Trie[T] | Null")
-            .replace("var head: Iterator[T] = null", "var head: Iterator[T] | Null = null")
-            .replace("if (head != null && head.hasNext) true", "if (head != null && head.nn.hasNext) true")
-            .replace("head.next()", "head.nn.next()")
-            .replace(
-              "_root_.scala.collection.internal.pprint.CollectionName.get(i)",
-              "import scala.reflect.Selectable.reflectiveSelectable\n" +
-                "    i.asInstanceOf[{def collectionClassName: String}].collectionClassName"
-            )
-            .replace("abstract class Walker", "@scala.annotation.nowarn abstract class Walker")
-            .replace("object TPrintLowPri", "@scala.annotation.nowarn object TPrintLowPri")
-        //                .replace("_root_.scala.collection.internal.pprint.CollectionName.get(i)", "head.nn.next()")
-        //                .replace("_root_.scala.collection.internal.pprint.CollectionName.get(i)", "head.nn.next()")
-      )
-      file.toIO
+    Files.createDirectories(dest)
+
+    for(url <- downloads) {
+      import java.io._
+      import java.net.{HttpURLConnection, URL}
+      import java.nio.file._
+      import java.nio.file.attribute.FileTime
+      import java.util.zip.{ZipEntry, ZipInputStream}
+
+
+
+
+      val conn = new URL(url).openConnection().asInstanceOf[HttpURLConnection]
+      conn.setInstanceFollowRedirects(true)
+      conn.setConnectTimeout(15000)
+      conn.setReadTimeout(60000)
+      conn.setRequestMethod("GET")
+
+      var in: InputStream = null
+      var zis: ZipInputStream = null
+      try {
+        in = new BufferedInputStream(conn.getInputStream)
+        zis = new ZipInputStream(in)
+
+        var entry: ZipEntry = zis.getNextEntry
+        val buffer = new Array[Byte](8192)
+
+        while (entry != null) {
+          val target = dest.resolve(entry.getName).normalize()
+          if (entry.isDirectory) Files.createDirectories(target)
+          else {
+            Files.createDirectories(target.getParent)
+            var out: OutputStream = null
+            try {
+              out = new BufferedOutputStream(Files.newOutputStream(target, StandardOpenOption.CREATE, StandardOpenOption.TRUNCATE_EXISTING))
+              var n = zis.read(buffer)
+              while (n != -1) {
+                out.write(buffer, 0, n)
+                n = zis.read(buffer)
+              }
+            } finally if (out != null) out.close()
+          }
+
+          zis.closeEntry()
+          entry = zis.getNextEntry
+        }
+      } finally {
+        if (zis != null) zis.close()
+        if (in != null) in.close()
+        conn.disconnect()
+      }
     }
+
+    import collection.JavaConverters._
+    Files.walk(dest)
+        .filter(p => p.toString().endsWith(".scala"))
+        .filter(p => !p.getFileName().toString().equals("CollectionName.scala"))
+        .map[java.io.File] { (file: java.nio.file.Path) =>
+          val text = Files.readString(file)
+          Files.write(
+            file,
+            ("package dotty.shaded\n" +
+              text
+                .replace("import scala", "import _root_.scala")
+                .replace(" scala.collection.", " _root_.scala.collection.")
+                .replace("_root_.pprint", "_root_.dotty.shaded.pprint")
+                .replace("_root_.fansi", "_root_.dotty.shaded.fansi")
+                .replace("def apply(c: Char): Trie[T]", "def apply(c: Char): Trie[T] | Null")
+                .replace("var head: Iterator[T] = null", "var head: Iterator[T] | Null = null")
+                .replace("if (head != null && head.hasNext) true", "if (head != null && head.nn.hasNext) true")
+                .replace("head.next()", "head.nn.next()")
+                .replace(
+                  "_root_.scala.collection.internal.pprint.CollectionName.get(i)",
+                  "import scala.reflect.Selectable.reflectiveSelectable\n" +
+                    "    i.asInstanceOf[{def collectionClassName: String}].collectionClassName"
+                )
+                .replace("abstract class Walker", "@scala.annotation.nowarn abstract class Walker")
+                .replace("object TPrintLowPri", "@scala.annotation.nowarn object TPrintLowPri")).getBytes
+          )
+          file.toFile
+
+        }
+        .collect(java.util.stream.Collectors.toList()).asScala.toSeq
+
   }.taskValue
   // Settings shared between scala3-compiler and scala3-compiler-bootstrapped
   lazy val commonDottyCompilerSettings = Seq(
--- project/build.sbt
+++ project/build.sbt
@@ -1,6 +1,4 @@
 // Used by VersionUtil to get gitHash and commitDate
 libraryDependencies += "org.eclipse.jgit" % "org.eclipse.jgit" % "4.11.0.201803080745-r"
-libraryDependencies += "com.lihaoyi" %% "requests" % "0.8.3"
-libraryDependencies += "com.lihaoyi" %% "os-lib" % "0.11.5"
 
 libraryDependencies += Dependencies.`jackson-databind`
