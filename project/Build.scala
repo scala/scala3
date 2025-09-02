4f1b861b68d9e3cd84dd8b31f4ba4692b5f9c1fc
Wojciech Mazur <wmazur@virtuslab.com>
2025-09-02 07:28:00 +0200

    [chore] Update scala-cli to 1.9.0 (was 1.8.5) (#23856)
    Unblocks of Scala 3.8 nightlies using scala-cli
    [Cherry-picked e44993518bde0c2eaa700ff68ce67492c302b569]

--- project/Build.scala
+++ project/Build.scala
@@ -137,7 +137,7 @@ object Build {
   val mimaPreviousLTSDottyVersion = "3.3.0"
 
   /** Version of Scala CLI to download */
-  val scalaCliLauncherVersion = "1.8.5"
+  val scalaCliLauncherVersion = "1.9.0"
   /** Version of Coursier to download for initializing the local maven repo of Scala command */
   val coursierJarVersion = "2.1.24"
 
