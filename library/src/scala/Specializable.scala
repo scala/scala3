81c69add371ae89520a61e2e82e7dfeff881f481
noti0na1 <noti0na1@users.noreply.github.com>
2025-09-02 13:04:15 +0200

    Always apply asInstanceOf with arguments

--- library/src/scala/Specializable.scala
+++ library/src/scala/Specializable.scala
@@ -26,15 +26,15 @@ object Specializable {
   // Smuggle a list of types by way of a tuple upon which Group is parameterized.
   class Group[T](value: T) extends SpecializedGroup
 
-  final val Primitives:  Group[(Byte, Short, Int, Long, Char, Float, Double, Boolean, Unit)] = null.asInstanceOf
-  final val Everything:  Group[(Byte, Short, Int, Long, Char, Float, Double, Boolean, Unit, AnyRef)] = null.asInstanceOf
-  final val Bits32AndUp: Group[(Int, Long, Float, Double)] = null.asInstanceOf
-  final val Integral:    Group[(Byte, Short, Int, Long, Char)] = null.asInstanceOf
-  final val AllNumeric:  Group[(Byte, Short, Int, Long, Char, Float, Double)] = null.asInstanceOf
-  final val BestOfBreed: Group[(Int, Double, Boolean, Unit, AnyRef)] = null.asInstanceOf
-  final val Unit:        Group[Tuple1[Unit]] = null.asInstanceOf
+  final val Primitives:  Group[(Byte, Short, Int, Long, Char, Float, Double, Boolean, Unit)] = null.asInstanceOf[Group[(Byte, Short, Int, Long, Char, Float, Double, Boolean, Unit)]]
+  final val Everything:  Group[(Byte, Short, Int, Long, Char, Float, Double, Boolean, Unit, AnyRef)] = null.asInstanceOf[Group[(Byte, Short, Int, Long, Char, Float, Double, Boolean, Unit, AnyRef)]]
+  final val Bits32AndUp: Group[(Int, Long, Float, Double)] = null.asInstanceOf[Group[(Int, Long, Float, Double)]]
+  final val Integral:    Group[(Byte, Short, Int, Long, Char)] = null.asInstanceOf[Group[(Byte, Short, Int, Long, Char)]]
+  final val AllNumeric:  Group[(Byte, Short, Int, Long, Char, Float, Double)] = null.asInstanceOf[Group[(Byte, Short, Int, Long, Char, Float, Double)]]
+  final val BestOfBreed: Group[(Int, Double, Boolean, Unit, AnyRef)] = null.asInstanceOf[Group[(Int, Double, Boolean, Unit, AnyRef)]]
+  final val Unit:        Group[Tuple1[Unit]] = null.asInstanceOf[Group[Tuple1[Unit]]]
 
-  final val Arg:         Group[(Int, Long, Float, Double)] = null.asInstanceOf
-  final val Args:        Group[(Int, Long, Double)] = null.asInstanceOf
-  final val Return:      Group[(Int, Long, Float, Double, Boolean, Unit)] = null.asInstanceOf
+  final val Arg:         Group[(Int, Long, Float, Double)] = null.asInstanceOf[Group[(Int, Long, Float, Double)]]
+  final val Args:        Group[(Int, Long, Double)] = null.asInstanceOf[Group[(Int, Long, Double)]]
+  final val Return:      Group[(Int, Long, Float, Double, Boolean, Unit)] = null.asInstanceOf[Group[(Int, Long, Float, Double, Boolean, Unit)]]
 }
