a18711015c8a1b0584ec3e5690af764c00cdce58
EnzeXing <enzexing12@gmail.com>
2025-09-01 20:10:50 -0400

    Cache result during thunk evaluation

--- tests/init-global/pos/i18628_2.scala
+++ tests/init-global/pos/i18628_2.scala
@@ -2,6 +2,6 @@ object Test:
   class Box(val x: Int)
 
   def recur(a: => Box, b: Box): Int =
-    a.x + recur(a, b) + b.x // warn
+    a.x + recur(a, b) + b.x
 
   recur(Box(1), Box(2))
--- tests/init-global/pos/i18628_3.scala
+++ tests/init-global/pos/i18628_3.scala
@@ -6,6 +6,6 @@ object Test:
   class Box(val x: Int)
 
   def recur(a: => Box, b: => Box): Int =
-    a.x + recur(a: @widen(5), b: @widen(5)) + b.x // warn // warn
+    a.x + recur(a: @widen(5), b: @widen(5)) + b.x
 
   recur(Box(1), Box(2))
