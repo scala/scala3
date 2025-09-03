60e421b0723fdb1e564070ee1556c3a55fa91b79
Wojciech Mazur <wmazur@virtuslab.com>
2025-09-03 16:03:55 +0200

    [chore] Set reference compiler version to 3.7.3-RC3 (#23868)
    Signed-off-by: Wojciech Mazur <wmazur@virtuslab.com>

--- project/Build.scala
+++ project/Build.scala
@@ -55,7 +55,7 @@ object Build {
    *
    *  Warning: Change of this variable needs to be consulted with `expectedTastyVersion`
    */
-  val referenceVersion = "3.7.3-RC2"
+  val referenceVersion = "3.7.3-RC3"
 
   /** Version of the Scala compiler targeted in the current release cycle
    *  Contains a version without RC/SNAPSHOT/NIGHTLY specific suffixes
