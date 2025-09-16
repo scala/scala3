8a0bbdf1ce04add1d864f52f2a88c001509bd0bd
Emil Ejbyfeldt <emil.ejbyfeldt@choreograph.com>
2025-09-16 15:18:26 +0200

    Only track types in from LazyRef

--- compiler/src/dotty/tools/dotc/core/Types.scala
+++ compiler/src/dotty/tools/dotc/core/Types.scala
@@ -7046,7 +7046,6 @@ object Types extends TypeUtils {
   class TypeSizeAccumulator(using Context) extends TypeAccumulator[Int] {
     var seen = util.HashSet[Type](initialCapacity = 8)
     def apply(n: Int, tp: Type): Int =
-      seen += tp
       tp match {
         case tp: AppliedType =>
           val tpNorm = tp.tryNormalize
@@ -7058,8 +7057,11 @@ object Types extends TypeUtils {
           apply(n, tp.superType)
         case tp: TypeParamRef =>
           apply(n, TypeComparer.bounds(tp))
-        case tp: LazyRef if seen.contains(tp) =>
-          n
+        case tp: LazyRef =>
+          if seen.contains(tp) then n
+          else
+            seen += tp
+            foldOver(n, tp)
         case _ =>
           foldOver(n, tp)
       }
