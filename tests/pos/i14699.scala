99c3a1675476f16e6f1f4a934b6e44018942914d
Som Snytt <som.snytt@gmail.com>
2025-09-15 21:38:07 -0700

    Status quo overload taking context function

--- tests/pos/i14699.scala
+++ tests/pos/i14699.scala
@@ -1,7 +1,21 @@
-def always(block: => Unit): Unit = {}
-def always(args: Int*)(block: => Unit): Unit ={}
 
-def test =
-  val x = always{}
-  val xc: Unit = x
-  always(1,2,3) {}
+package i14699:
+  def always(block: => Unit): Unit = ()
+  def always(args: Int*)(block: => Unit): Unit = ()
+
+  def test =
+    val x = always{}
+    val xc: Unit = x
+    always(1,2,3) {}
+
+package i15163:
+  trait Ctx
+  object Ctx:
+    given Ctx()
+  def always(block: Ctx ?=> Unit): Unit = block
+  def always(args: Int*)(block: Ctx ?=> Unit): Unit = block
+
+  def test =
+    //val _ = always {}
+    val _ = always { (x: Ctx) ?=> () }
+    always(1,2,3) {}
