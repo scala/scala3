8b2ca89c6c1928fa3379c9697310a3754edf47bc
noti0na1 <noti0na1@users.noreply.github.com>
2025-09-24 15:10:14 +0200

    Keep Yflexify-tasty tests disabled

--- compiler/test/dotty/tools/dotc/CompilationTests.scala
+++ compiler/test/dotty/tools/dotc/CompilationTests.scala
@@ -213,15 +213,16 @@ class CompilationTests {
       compileFilesInDir("tests/explicit-nulls/unsafe-common", explicitNullsOptions `and` "-Yno-flexible-types", FileFilter.exclude(TestSources.negExplicitNullsScala2LibraryTastyExcludelisted)),
     ).checkExpectedErrors()
 
-    locally {
-      val unsafeFile = compileFile("tests/explicit-nulls/flexible-unpickle/neg/Unsafe_1.scala", explicitNullsOptions without "-Yexplicit-nulls")
-      val flexibleFile = compileFile("tests/explicit-nulls/flexible-unpickle/neg/Flexible_2.scala",
-          explicitNullsOptions.and("-Yflexify-tasty").withClasspath(defaultOutputDir + testGroup + "/Unsafe_1/neg/Unsafe_1"))
+    // locally {
+    //   val unsafeFile = compileFile("tests/explicit-nulls/flexible-unpickle/neg/Unsafe_1.scala", explicitNullsOptions without "-Yexplicit-nulls")
+    //   val flexibleFile = compileFile("tests/explicit-nulls/flexible-unpickle/neg/Flexible_2.scala",
+    //       explicitNullsOptions.and("-Yflexify-tasty").withClasspath(defaultOutputDir + testGroup + "/Unsafe_1/neg/Unsafe_1"))
 
-      flexibleFile.keepOutput.checkExpectedErrors()
+    //   unsafeFile.keepOutput.checkCompile()
+    //   flexibleFile.keepOutput.checkExpectedErrors()
 
-      List(unsafeFile, flexibleFile).foreach(_.delete())
-    }
+    //   List(unsafeFile, flexibleFile).foreach(_.delete())
+    // }
   }
 
   @Test def explicitNullsPos: Unit = {
@@ -232,15 +233,15 @@ class CompilationTests {
       compileFilesInDir("tests/explicit-nulls/unsafe-common", explicitNullsOptions `and` "-language:unsafeNulls" `and` "-Yno-flexible-types"),
     ).checkCompile()
 
-    locally {
-      val tests = List(
-        compileFile("tests/explicit-nulls/flexible-unpickle/pos/Unsafe_1.scala", explicitNullsOptions `without` "-Yexplicit-nulls"),
-        compileFile("tests/explicit-nulls/flexible-unpickle/pos/Flexible_2.scala",
-        explicitNullsOptions.and("-Yflexify-tasty").withClasspath(defaultOutputDir + testGroup + "/Unsafe_1/pos/Unsafe_1")),
-      ).map(_.keepOutput.checkCompile())
+    // locally {
+    //   val tests = List(
+    //     compileFile("tests/explicit-nulls/flexible-unpickle/pos/Unsafe_1.scala", explicitNullsOptions without "-Yexplicit-nulls"),
+    //     compileFile("tests/explicit-nulls/flexible-unpickle/pos/Flexible_2.scala",
+    //     explicitNullsOptions.and("-Yflexify-tasty").withClasspath(defaultOutputDir + testGroup + "/Unsafe_1/pos/Unsafe_1")),
+    //   ).map(_.keepOutput.checkCompile())
 
-      tests.foreach(_.delete())
-    }
+    //   tests.foreach(_.delete())
+    // }
   }
 
   @Test def explicitNullsWarn: Unit = {
