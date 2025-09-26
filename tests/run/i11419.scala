61d0a781ce6db85d801928d5c0f43d3ee3fb933e
odersky <odersky@gmail.com>
2025-09-26 18:28:24 +0200

    Drop stray `// error`

--- tests/run/i11419.scala
+++ tests/run/i11419.scala
@@ -3,7 +3,7 @@ import language.experimental.multiSpreads
 object Test {
   def main(args: Array[String]): Unit = {
     val arr: Array[String] = Array("foo")
-    val lst = List("x", arr*) // error
+    val lst = List("x", arr*)
     println(lst)
   }
 }
