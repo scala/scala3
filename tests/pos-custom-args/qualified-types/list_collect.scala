099ba60c051332b861cdbfa57b3874a686f38d05
Matt Bovel <matthieu@bovel.net>
2025-10-14 03:20:10 +0000

    Add Scala Workshop 25 examples

---
+++ tests/pos-custom-args/qualified-types/list_collect.scala
@@ -0,0 +1,5 @@
+type Pos = { v: Int with v >= 0 }
+
+@main def main =
+  val xs = List(-1,2,-2,1)
+  xs.collect { case x: Pos => x } : List[Pos]
--- tests/pos-custom-args/qualified-types/list_map.scala
+++ tests/pos-custom-args/qualified-types/list_map.scala
@@ -1,9 +1,9 @@
-type PosInt = {v: Int with v > 0}
+type Pos = {v: Int with v > 0}
 
-
-def inc(x: PosInt): PosInt = (x + 1).runtimeChecked
+def inc(x: Pos): Pos = (x + 1).runtimeChecked
 
 @main def Test =
-  val l: List[PosInt] = List(1,2,3)
-  val l2: List[PosInt] = l.map(inc)
+  val l: List[Pos] = List(1,2,3)
+  val l2 = l.map(inc)
+  l2: List[Pos]
   ()
