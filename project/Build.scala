1a5e0c97aea15299286ae9a4e992214e80d596f0
Wojciech Mazur <wmazur@virtuslab.com>
2025-10-15 19:07:25 +0200

    Upgrade Scala 2 standard library to 2.13.17

--- project/Build.scala
+++ project/Build.scala
@@ -161,8 +161,8 @@ object Build {
    *  scala-library.
    */
   def stdlibVersion(implicit mode: Mode): String = mode match {
-    case NonBootstrapped => "2.13.16"
-    case Bootstrapped => "2.13.16"
+    case NonBootstrapped => "2.13.17"
+    case Bootstrapped => "2.13.17"
   }
 
   /** Version of the scala-library for which we will generate TASTy.
@@ -172,7 +172,7 @@ object Build {
    *  We can use nightly versions to tests the future compatibility in development.
    *  Nightly versions: https://scala-ci.typesafe.com/ui/native/scala-integration/org/scala-lang
    */
-  val stdlibBootstrappedVersion = "2.13.16"
+  val stdlibBootstrappedVersion = "2.13.17"
 
   val dottyOrganization = "org.scala-lang"
   val dottyGithubUrl = "https://github.com/scala/scala3"
@@ -2597,7 +2597,7 @@ object Build {
           .exclude("org.eclipse.lsp4j","org.eclipse.lsp4j.jsonrpc"),
         "org.eclipse.lsp4j" % "org.eclipse.lsp4j" % "0.20.1",
       ),
-      libraryDependencies += ("org.scalameta" % "mtags-shared_2.13.16" % mtagsVersion % SourceDeps),
+      libraryDependencies += ("org.scalameta" % "mtags-shared_2.13.17" % mtagsVersion % SourceDeps),
       ivyConfigurations += SourceDeps.hide,
       transitiveClassifiers := Seq("sources"),
       scalacOptions ++= Seq("-source", "3.3"), // To avoid fatal migration warnings
