ab9a589d02fffa6185d4d13fba4a5ca3f321bd68
Li Haoyi <haoyi.sg@gmail.com>
2025-10-15 17:56:43 +0800

    .

--- tests/neg/dedented-string-literals.scala
+++ tests/neg/dedented-string-literals.scala
@@ -1,21 +1,21 @@
 // Test error cases for dedented string literals
 
-object DedentedStringErrors {
+object DedentedStringErrors { // nopos-error // nopos-error // nopos-error
   // Error: No newline after opening quotes
   val noNewlineAfterOpen = '''content on same line // error
 
-  // Error: Content not indented enough // error
+  // Error: Content not indented enough
   val notIndented = '''
 content
   '''
 
-  // Error: Mixed tabs and spaces // error
+  // Error: Mixed tabs and spaces
   val mixedTabsSpaces = '''
 	tab line
   space line
 	'''
 
-  // Error: Unclosed literal // error
+  // Error: Unclosed literal
   val unclosed = '''
 some content
 }
