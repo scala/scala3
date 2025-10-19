00120c7a8e9f6e4e8d465861bf3cefe151f3ba02
Wojciech Mazur <wmazur@virtuslab.com>
2025-10-19 22:27:35 +0200

    [community-build] Fix failing mill builds (#24208)
    * Stop setting explicit MILL_VERSION as explicit variable - version
    should be set at the project level
    * Don't start mill build server

--- community-build/src/scala/dotty/communitybuild/projects.scala
+++ community-build/src/scala/dotty/communitybuild/projects.scala
@@ -74,8 +74,7 @@ final case class MillCommunityProject(
   override val docCommand = null
     // uncomment once mill is released
     // if ignoreDocs then null else s"$baseCommand.docJar"
-  override val runCommandsArgs = List("-i", "-D", s"dottyVersion=$compilerVersion")
-  override val environment = Map("MILL_VERSION" -> "0.10.5")
+  override val runCommandsArgs = List("-i", "-D", s"dottyVersion=$compilerVersion", "--no-server")
 
 final case class SbtCommunityProject(
     project: String,
