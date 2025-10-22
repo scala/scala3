3d8aecc3a1098a7d84012ea556940c9b0bac9e0e
Hamza Remmal <hamza@remmal.net>
2025-10-22 08:17:29 +0100

    chore: extract repl to be its own artifact
    [Cherry-picked ff05ec8abe06403bb8d438287deb8e663ae3a26c]

---
+++ compiler/src/dotty/tools/io/AbstractFileClassLoader.scala
@@ -0,0 +1,61 @@
+/*
+ * Scala (https://www.scala-lang.org)
+ *
+ * Copyright EPFL and Lightbend, Inc.
+ *
+ * Licensed under Apache License 2.0
+ * (http://www.apache.org/licenses/LICENSE-2.0).
+ *
+ * See the NOTICE file distributed with this work for
+ * additional information regarding copyright ownership.
+ */
+
+package dotty.tools
+package io
+
+import scala.language.unsafeNulls
+
+import dotty.tools.io.AbstractFile
+
+import java.net.{URL, URLConnection, URLStreamHandler}
+import java.util.Collections
+
+class AbstractFileClassLoader(val root: AbstractFile, parent: ClassLoader) extends ClassLoader(parent):
+  private def findAbstractFile(name: String) = root.lookupPath(name.split('/').toIndexedSeq, directory = false)
+
+  // on JDK 20 the URL constructor we're using is deprecated,
+  // but the recommended replacement, URL.of, doesn't exist on JDK 8
+  @annotation.nowarn("cat=deprecation")
+  override protected def findResource(name: String): URL | Null =
+    findAbstractFile(name) match
+      case null => null
+      case file => new URL(null, s"memory:${file.path}", new URLStreamHandler {
+        override def openConnection(url: URL): URLConnection = new URLConnection(url) {
+          override def connect() = ()
+          override def getInputStream = file.input
+        }
+      })
+  override protected def findResources(name: String): java.util.Enumeration[URL] =
+    findResource(name) match
+      case null => Collections.enumeration(Collections.emptyList[URL])  //Collections.emptyEnumeration[URL]
+      case url  => Collections.enumeration(Collections.singleton(url))
+
+  override def findClass(name: String): Class[?] = {
+    var file: AbstractFile | Null = root
+    val pathParts = name.split("[./]").toList
+    for (dirPart <- pathParts.init) {
+      file = file.lookupName(dirPart, true)
+      if (file == null) {
+        throw new ClassNotFoundException(name)
+      }
+    }
+    file = file.lookupName(pathParts.last+".class", false)
+    if (file == null) {
+      throw new ClassNotFoundException(name)
+    }
+    val bytes = file.toByteArray
+    defineClass(name, bytes, 0, bytes.length)
+  }
+
+  override def loadClass(name: String): Class[?] = try findClass(name) catch case _: ClassNotFoundException => super.loadClass(name)
+end AbstractFileClassLoader
