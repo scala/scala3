4c73265035166301308d86bf9fe2bdf8eb471966
noti0na1 <noti0na1@users.noreply.github.com>
2025-10-22 15:59:17 +0200

    Revert nullable Option.apply

---
+++ tests/pos/i24206.scala
@@ -0,0 +1,16 @@
+class DispatchQuery:
+  type CompareFunction = (AnyRef, AnyRef) => Boolean
+  private var _sortedWith: Option[CompareFunction] = Option.empty
+
+  def sortedWith(f: PartialFunction[(AnyRef, AnyRef), Boolean]): DispatchQuery = {
+    // fails
+    _sortedWith = Option { case (a, b) => f.applyOrElse((a, b), _ => false) }
+    // workaround
+    _sortedWith = Option[CompareFunction] { case (a, b) => f.applyOrElse((a, b), _ => false) }
+    this
+  }
+
+trait Result
+def getAll(nameFilter: Option[String => Boolean]): List[Result] = ???
+def get(collectionName: String): List[Result] =
+    getAll(Option(_.startsWith(collectionName)))
