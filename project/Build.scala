ed4edf0c7b2fe68f9df44b77a54c6c812fcf372d
Wojciech Mazur <wmazur@virtuslab.com>
2025-10-23 10:37:09 +0200

    Set reference compiler version to 3.7.4-RC2

--- project/Build.scala
+++ project/Build.scala
@@ -59,7 +59,7 @@ object Build {
    *
    *  Warning: Change of this variable needs to be consulted with `expectedTastyVersion`
    */
-  val referenceVersion = "3.7.4-RC1"
+  val referenceVersion = "3.7.4-RC2"
 
   /** Version of the Scala compiler targeted in the current release cycle
    *  Contains a version without RC/SNAPSHOT/NIGHTLY specific suffixes
