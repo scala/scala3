d6d41e2fb90d9a46de5694bffeec5ad9ed450c83
Matt Bovel <matthieu@bovel.net>
2025-10-30 14:29:47 +0000

    Fix handling of `null` in qualifiers

--- tests/pos-custom-args/qualified-types/class_constraints.scala
+++ tests/pos-custom-args/qualified-types/class_constraints.scala
@@ -4,4 +4,6 @@ class Multi(x: Int with x > 0)(y: String with y.length > 2, z: Double with z >= 
 
 case class MultiCase(x: Int with x > 0)(val y: String with y.length > 2, val z: Double with z >= 0.0)
 
+class TParam[T](x: T with x != null)
+
 @main def Test = ()
