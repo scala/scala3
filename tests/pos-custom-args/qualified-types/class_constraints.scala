e652179fcac841f41cd56aa1579d875441f264f0
Matt Bovel <matthieu@bovel.net>
2025-10-30 12:16:01 +0000

    Fix desugaring of fields with qualified types

--- tests/pos-custom-args/qualified-types/class_constraints.scala
+++ tests/pos-custom-args/qualified-types/class_constraints.scala
@@ -1,3 +1,7 @@
-/*class foo(elem: Int with elem > 0)*/
+class foo(elem: Int with elem > 0)
+
+class Multi(x: Int with x > 0)(y: String with y.length > 2, z: Double with z >= 0.0)
+
+case class MultiCase(x: Int with x > 0)(val y: String with y.length > 2, val z: Double with z >= 0.0)
 
 @main def Test = ()
