6fc91e597e70acd8a6dd2a7503517e9fd8dbce10
Lukas Rytz <lukas.rytz@gmail.com>
2025-11-03 11:07:57 +0100

    Add `@uncheckedOverride` annotation for definitions that may override
    Upstream: scala/scala@7dcf3a6007659f0e835d751bb0564c1fc267c95c

---
+++ library/src/scala/annotation/unchecked/uncheckedOverride.scala
@@ -0,0 +1,21 @@
+/*
+ * Scala (https://www.scala-lang.org)
+ *
+ * Copyright EPFL and Lightbend, Inc. dba Akka
+ *
+ * Licensed under Apache License 2.0
+ * (http://www.apache.org/licenses/LICENSE-2.0).
+ *
+ * See the NOTICE file distributed with this work for
+ * additional information regarding copyright ownership.
+ */
+
+package scala.annotation.unchecked
+
+import scala.annotation.StaticAnnotation
+
+/**
+ * Marking a definition `@uncheckedOverride` is equivalent to the `override` keyword, except that overriding is not
+ * enforced. A definition marked `@uncheckedOverride` is allowed to override nothing.
+ */
+final class uncheckedOverride extends StaticAnnotation
