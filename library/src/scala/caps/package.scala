7b32bb75128a46ad3ba25b0e1952744f57a9ac91
odersky <odersky@gmail.com>
2025-11-12 13:24:37 +0100

    Add caps.freeze wrapper

--- library/src/scala/caps/package.scala
+++ library/src/scala/caps/package.scala
@@ -123,6 +123,7 @@ final class reserve extends annotation.StaticAnnotation
  *  environment.
  */
 @experimental
+@deprecated(since = "3.8.0")
 final class use extends annotation.StaticAnnotation
 
 /** A trait that used to allow expressing existential types. Replaced by
@@ -131,6 +132,12 @@ final class use extends annotation.StaticAnnotation
 @experimental
 @deprecated
 sealed trait Exists extends Capability
+
+/** A wrapper that strips all covariant capture sets from Mutable types in the
+ *  result of pure operation `op`, turning them into immutable types.
+ */
+@experimental
+def immutable[T](op: -> T): T = op
 
 @experimental
 object internal:
