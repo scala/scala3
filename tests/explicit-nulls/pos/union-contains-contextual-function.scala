504068c2d7b0a70ba08e1893b50e33b6509fe4e5
noti0na1 <noti0na1@users.noreply.github.com>
2025-11-13 17:18:59 +0100

    Update tests/explicit-nulls/pos/union-contains-contextual-function.scala
    Co-authored-by: Ondřej Lhoták <olhotak@uwaterloo.ca>

--- tests/explicit-nulls/pos/union-contains-contextual-function.scala
+++ tests/explicit-nulls/pos/union-contains-contextual-function.scala
@@ -1,6 +1,8 @@
 class Test:
   val x: ((a: String) ?=> Int) | Null = null
 
+  val x2: ((a: String) ?=> Int) | Null = (s: String) ?=> 42
+
   val y: ((a: String) ?=> Int) | String = "hello"
 
   def f(g: ((a: String) ?=> Int) | Null = null) = ???
