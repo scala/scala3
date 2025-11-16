b89b2fc83bfaf06c60b8638a51da8e29307716c9
Hamza Remmal <hamza@remmal.net>
2025-11-16 23:04:40 +0100

    mark some annotations as `@exportable`

--- library/src/scala/annotation/targetName.scala
+++ library/src/scala/annotation/targetName.scala
@@ -1,10 +1,12 @@
 package scala.annotation
 
 import language.experimental.captureChecking
+import meta.exportable
 
 /** An annotation that defines an external name for a definition.
  *  If an `targetName(extname)` annotation is given for a method or some other
  *  definition, its implementation will use the name `extname` instead of
  *  the regular name.
  */
+@exportable
 final class targetName(name: String) extends StaticAnnotation
