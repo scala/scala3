a04c07a0948adc409721b393a89a1b9bd83e52b3
Li Haoyi <haoyi.sg@gmail.com>
2025-11-20 10:40:55 -0800

    cleanup

--- repl-embedded/src/scala/tools/repl/EmbeddedReplMain.scala
+++ repl-embedded/src/scala/tools/repl/EmbeddedReplMain.scala
@@ -25,30 +25,21 @@ class UnshadingClassLoader(parent: ClassLoader) extends ClassLoader(parent) {
                         !name.startsWith(SHADED_PREFIX)
 
     if (shouldUnshade) {
-      // Try to find the shaded version
-      val shadedName = SHADED_PREFIX + name
-
-      // First check if we've already loaded this class
       val loaded = findLoadedClass(name)
       if (loaded != null) return loaded
 
       try {
         // Load the shaded class bytes from parent
-        val resourceName = shadedName.replace('.', '/') + ".class"
-        val is = getParent.getResourceAsStream(resourceName)
+        val is = getParent.getResourceAsStream((SHADED_PREFIX + name).replace('.', '/') + ".class")
 
         if (is != null) {
           try {
-            // Read the class bytes
-            val bytes = readAllBytes(is)
-
+            val bytes = is.readAllBytes()
             // Define the class with the unshaded name
             val clazz = defineClass(name, bytes, 0, bytes.length)
             if (resolve) resolveClass(clazz)
             return clazz
-          } finally {
-            is.close()
-          }
+          } finally is.close()
         }
       } catch {
         case _: Exception => // Fall through to parent
@@ -57,18 +48,6 @@ class UnshadingClassLoader(parent: ClassLoader) extends ClassLoader(parent) {
 
     // For everything else (scala.* and already shaded classes), delegate to parent
     super.loadClass(name, resolve)
-  }
-
-  private def readAllBytes(is: InputStream): Array[Byte] = {
-    val buffer = new Array[Byte](8192)
-    var bytesRead = 0
-    val baos = new java.io.ByteArrayOutputStream()
-
-    while ({bytesRead = is.read(buffer); bytesRead != -1}) {
-      baos.write(buffer, 0, bytesRead)
-    }
-
-    baos.toByteArray()
   }
 }
 
@@ -82,17 +61,13 @@ object EmbeddedReplMain {
   def main(args: Array[String]): Unit = {
     // Get the location of the current jar to use as classpath
     val codeSource = getClass.getProtectionDomain.getCodeSource
-    val jarPath = if (codeSource != null) {
-      val location = codeSource.getLocation
-      if (location.getProtocol == "file") {
-        new java.io.File(location.toURI).getAbsolutePath
-      } else {
-        location.toString
+    val jarPath =
+      if (codeSource == null) System.getProperty("java.class.path") // Fallback: try to extract from classpath
+      else  {
+        val location = codeSource.getLocation
+        if (location.getProtocol == "file") new java.io.File(location.toURI).getAbsolutePath
+        else location.toString
       }
-    } else {
-      // Fallback: try to extract from classpath
-      System.getProperty("java.class.path")
-    }
 
     // Add -classpath argument pointing to the shaded jar itself
     // This allows the ReplDriver's compiler to find scala.* classes
@@ -104,31 +79,16 @@ object EmbeddedReplMain {
 
     // Create the unshading classloader with the current classloader as parent
     // This ensures it has access to all dependencies in the shaded jar
-    val unshadingClassLoader = new UnshadingClassLoader(
-      getClass.getClassLoader // Use current classloader to access all dependencies
-    )
+    val unshadingClassLoader = new UnshadingClassLoader(getClass.getClassLoader)
 
-    // Load the ReplDriver class through the unshading classloader
     val replDriverClass = unshadingClassLoader.loadClass("dotty.tools.repl.ReplDriver")
-
-    // Get the constructor: ReplDriver(Array[String], PrintStream, Option[ClassLoader], String)
-    val constructor = replDriverClass.getConstructors()(0)
-
-    // Create an Option[ClassLoader] containing the external classloader
-    val scalaOptionClass = unshadingClassLoader.loadClass("scala.Option")
-    val scalaOptionModule = unshadingClassLoader.loadClass("scala.Option$")
-    val someMethod = scalaOptionModule.getField("MODULE$").get(null)
-      .asInstanceOf[Object].getClass.getMethod("apply", classOf[Object])
-    val classLoaderOption = someMethod.invoke(
-      scalaOptionModule.getField("MODULE$").get(null),
-      getClass.getClassLoader // Pass the external classloader
-    )
+    val constructor = replDriverClass.getConstructors().head
 
     // Create the ReplDriver instance with classpath argument
     val replDriver = constructor.newInstance(
       argsWithClasspath,        // settings: Array[String] (now includes -classpath)
       System.out,               // out: PrintStream
-      classLoaderOption,        // classLoader: Option[ClassLoader]
+      Option(getClass.getClassLoader),        // classLoader: Option[ClassLoader]
       ""                        // extraPredef: String
     )
 
