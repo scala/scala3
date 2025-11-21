d45911f3592abbc3d10add00c7a945871c3fa78b
Yichen Xu <yichen.x@outlook.com>
2025-11-21 17:25:03 +0100

    Refactor: move multi-span handling logic to Message

--- compiler/src/dotty/tools/dotc/cc/SepCheck.scala
+++ compiler/src/dotty/tools/dotc/cc/SepCheck.scala
@@ -454,14 +454,31 @@ class SepCheck(checker: CheckCaptures.CheckerAPI) extends tpd.TreeTraverser:
             |No clashing definitions were found. This might point to an internal error.""",
         tree.srcPos)
 
-  class UseAfterConsume(ref: Capability, consumedLoc: SrcPos, useLoc: SrcPos)(using Context) extends reporting.Diagnostic.Error(
-    em"""Separation failure: Illegal access to $ref, which was passed to a
-        |consume parameter or was used as a prefix to a consume method
-        |and therefore is no longer available.""",
-    useLoc.sourcePos
-  ):
-      addSubdiag(em"The capability was consumed here.", consumedLoc.sourcePos)
-      addPrimaryNote(em"Then, it was used here")
+  class UseAfterConsume(ref: Capability, consumedLoc: SrcPos, useLoc: SrcPos)(using Context) extends reporting.Message(reporting.ErrorMessageID.NoExplanationID):
+    def kind = reporting.MessageKind.NoKind
+
+    protected def msg(using Context): String = ""
+
+    protected def explain(using Context): String = ""
+
+    override def leading(using Context): Option[String] = Some(
+      em"""Separation failure: Illegal access to $ref, which was passed to a
+          |consume parameter or was used as a prefix to a consume method
+          |and therefore is no longer available.""".message
+    )
+
+    override def parts(using Context): List[reporting.Message.MessagePart] = List(
+      reporting.Message.MessagePart(
+        "The capability was consumed here.",
+        consumedLoc.sourcePos,
+        isPrimary = false
+      ),
+      reporting.Message.MessagePart(
+        "Then, it was used here",
+        useLoc.sourcePos,
+        isPrimary = true
+      )
+    )
 
   /** Report a failure where a previously consumed capability is used again,
    *  @param ref     the capability that is used after being consumed
@@ -474,8 +491,6 @@ class SepCheck(checker: CheckCaptures.CheckerAPI) extends tpd.TreeTraverser:
       em"""Separation failure: Illegal access to $ref, which was ${role.howConsumed}
           |on line ${locPos.line + 1} and therefore is no longer available.""",
       pos)
-  // def consumeError(ref: Capability, loc: SrcPos, pos: SrcPos)(using Context): Unit =
-  //   ctx.reporter.report(UseAfterConsume(ref, loc, pos))
 
   /** Report a failure where a capability is consumed in a loop.
    *  @param ref     the capability
