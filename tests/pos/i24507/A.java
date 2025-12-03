scala/scala3

commit e7d479f80a29c6ae21d0755047e563963b87ac82

Fixes IllegalAccessError with Java package protected class
This is a backport of Scala 2.x scala/scala 6023
Fixes 13841
Fixes 13897

**Problem**
When compiling `builder.call1().call2()` where both are Java-defined
package-protected class through a public subsclass, Scala 3 does not properly
cast the receiver to the public class, and results in an IllegalAccessError.

**Solution**
This backports the casting fix from the Scala 2.x compiler.

compiler/src/dotty/tools/dotc/transform/Erasure.scala
@@ -36,6 +36,7 @@ import ExplicitOuter.*
 import core.Mode
 import util.Property
 import reporting.*
+import scala.annotation.tailrec
 
 class Erasure extends Phase with DenotTransformer {
 
@@ -764,7 +765,8 @@ object Erasure {
           (ctx.owner.enclosingPackageClass eq boundary)
         }
 
-      def recur(qual: Tree): Tree = {
+      @tailrec
+      def recur(qual: Tree): Tree =
         val qualIsPrimitive = qual.tpe.widen.isPrimitiveValueType
         val symIsPrimitive = sym.owner.isPrimitiveValueClass
 
@@ -773,33 +775,34 @@ object Erasure {
             inContext(preErasureCtx):
               tree.qualifier.typeOpt.widen.finalResultType)
 
-        if (qualIsPrimitive && !symIsPrimitive || qual.tpe.widenDealias.isErasedValueType)
+        if qualIsPrimitive && !symIsPrimitive || qual.tpe.widenDealias.isErasedValueType then
           recur(box(qual))
-        else if (!qualIsPrimitive && symIsPrimitive)
+        else if !qualIsPrimitive && symIsPrimitive then
           recur(unbox(qual, sym.owner.typeRef))
-        else if (sym.owner eq defn.ArrayClass)
+        else if sym.owner eq defn.ArrayClass then
           selectArrayMember(qual, originalQual)
-        else {
-          val qual1 = adaptIfSuper(qual)
-          if (qual1.tpe.derivesFrom(sym.owner) || qual1.isInstanceOf[Super])
-            select(qual1, sym)
-          else
-            val castTarget = // Avoid inaccessible cast targets, see i8661
-              if isJvmAccessible(sym.owner) && sym.owner.isType
-              then
-                sym.owner.typeRef
-              else
-                // If the owner is inaccessible, try going through the qualifier,
-                // but be careful to not go in an infinite loop in case that doesn't
-                // work either.
-                val tp = originalQual
-                if tp =:= qual1.tpe.widen then
-                  return errorTree(qual1,
-                    em"Unable to emit reference to ${sym.showLocated}, ${sym.owner} is not accessible in ${ctx.owner.enclosingClass}")
-                tp
-            recur(cast(qual1, castTarget))
-        }
-      }
+        else
+          adaptIfSuper(qual) match
+            case qual1: Super =>
+              select(qual1, sym)
+            case qual1 if !isJvmAccessible(qual1.tpe.typeSymbol)
+                || !qual1.tpe.derivesFrom(sym.owner) =>
+              val castTarget = // Avoid inaccessible cast targets, see i8661
+                if isJvmAccessible(sym.owner) && sym.owner.isType then
+                  sym.owner.typeRef
+                else
+                  // If the owner is inaccessible, try going through the qualifier,
+                  // but be careful to not go in an infinite loop in case that doesn't
+                  // work either.
+                  val tp = originalQual
+                  if tp =:= qual1.tpe.widen then
+                    return errorTree(qual1,
+                      em"Unable to emit reference to ${sym.showLocated}, ${sym.owner} is not accessible in ${ctx.owner.enclosingClass}")
+                  tp
+              recur(cast(qual1, castTarget))
+            case qual1 =>
+              select(qual1, sym)
+      end recur
 
       checkNotErased(recur(qual1))
     }

tests/run/java-package-protected/A.java
@@ -1,0 +1,21 @@
+// filter: unchecked
+package a;
+
+/** This is package protected. */
+class B<T extends B<T>> {
+    private int connectTimeout = 10000;
+    private int failedAttempts = 3;
+
+    public T setConnectTimeout(int connectTimeout) {
+        this.connectTimeout = connectTimeout;
+        return (T) this;
+    }
+
+    public T setFailedAttempts(int failedAttempts) {
+        this.failedAttempts = failedAttempts;
+        return (T) this;
+    }
+}
+
+/** This is public. */
+public class A extends B<A> { }

tests/run/java-package-protected/Test.scala
@@ -1,0 +1,5 @@
+// scalajs: --skip
+
+object Test extends App:
+  assert(b.C.m == 0)
+end Test

tests/run/java-package-protected/C.scala
@@ -1,0 +1,10 @@
+package b
+
+import a.*
+
+object C:
+  def m: Int =
+    val a = new A()
+      .setConnectTimeout(1)
+      .setFailedAttempts(1)
+    0

commit 5f791a7fd8cb82bde0ce021b275bd8baeccf4bc8

Fix isJvmAccessible to handle protected Java classes #24507
Fixes #24507

The previous implementation of `isJvmAccessible` failed to
recognize protected Java nested classes as accessible.
And it causes compilation errors when extending them from subclasses.

Previous implementation:

```scala
def isJvmAccessible(cls: Symbol): Boolean =
  !cls.is(Flags.JavaDefined) || {
    val boundary = cls.accessBoundary(cls.owner)(using preErasureCtx)
    (boundary eq defn.RootClass) ||
    (ctx.owner.enclosingPackageClass eq boundary)
  }
```

For protected nested classes like B below, the access boundary is the enclosing class (`A` in this case), not a `package a` or root.
However, `B` should be accessible through a public class `A`.

```java
package a;

public class A {
  protected class B {}
}
```

This commit replace the manual boundary checks with `cls.isAccessibleFrom`, which properly handles all Java access modifiers.
This aligns with the Scala 2 implementation which uses the
equivalent `context.isAccessible(cls, cls.owner.thisType)`.
https://github.com/scala/scala/blob/efb71845113639bd1da231c917e18af33519fc07/src/compiler/scala/tools/nsc/transform/Erasure.scala#L1451-L1455

For example,
- `cls` will be `protected class B`
- `cls.owner.thisType` will be `public class A`
- And, `cls.isAccessibleFrom(cls.owner.thisType)` is true.

On the other hand, `isJvmAccessible` returns false for Java-defined package protected class case like `java-package-protected` case.
https://github.com/tanishiking/scala3/tree/b2850be4c79996d8ebd9afaf36ed12f46febb0d1/tests/run/java-package-protected

In this case,
- `cls` = `class B` (package protected under `a`)
- `cls.owner.thisType` = `package a`
- And, `cls.isAccessibleFrom(cls.owner.thisType)` is false.

because `B` is package protected and, we cannot access B through `a.B` from another package. In this case, cast to `A` happens.

---

Note that, this regression starts from e7d479f but the true commit is https://github.com/scala/scala3/pull/9364/commits/d3ce551b3a74c03cb28a1c77cd6ed1b0517bcb6a

compiler/src/dotty/tools/dotc/transform/Erasure.scala
@@ -748,13 +748,8 @@ object Erasure {
         // Scala classes are always emitted as public, unless the
         // `private` modifier is used, but a non-private class can never
         // extend a private class, so such a class will never be a cast target.
-        !cls.is(Flags.JavaDefined) || {
-          // We can't rely on `isContainedWith` here because packages are
-          // not nested from the JVM point of view.
-          val boundary = cls.accessBoundary(cls.owner)(using preErasureCtx)
-          (boundary eq defn.RootClass) ||
-          (ctx.owner.enclosingPackageClass eq boundary)
-        }
+        !cls.is(Flags.JavaDefined) ||
+          cls.isAccessibleFrom(cls.owner.thisType)(using preErasureCtx)
 
       @tailrec
       def recur(qual: Tree): Tree =

tests/pos/i24507-jframe.scala
@@ -1,0 +1,2 @@
+class Test extends javax.swing.JFrame { class Acc extends AccessibleJFrame }
+

tests/pos/i24507/A.java
@@ -1,0 +1,5 @@
+package a;
+
+public class A {
+  protected class B {}
+}

tests/pos/i24507/Test.scala
@@ -1,0 +1,3 @@
+import a._
+
+class Test extends A { class Acc extends B }