0d480029b2bd2ef30221324509aa4f326fba6560
Piotr Chabelski <ged.subfan@gmail.com>
2025-12-09 11:33:44 +0100

    Work around https://github.com/sbt/sbt/issues/8376 and re-enable `shapeless-3` community build (#24709)
    - works around https://github.com/sbt/sbt/issues/8376 /
    https://github.com/coursier/coursier/issues/3520
    - kudos to @eed3si9n for suggesting this, as per
    https://github.com/coursier/coursier/issues/3520#issuecomment-3630791062
    - re-enables tests disabled in
    https://github.com/scala/scala3/pull/24477#discussion_r2583852505
    [Cherry-picked 577721f395350461499c3a71a7a0c934aa09109c]

--- project/Build.scala
+++ project/Build.scala
@@ -3028,7 +3028,7 @@ object Build {
     publishConfiguration ~= (_.withOverwrite(true)),
     publishLocalConfiguration ~= (_.withOverwrite(true)),
     projectID ~= {id =>
-      val line = "scala.versionLine" -> versionLine
+      val line = "info.scala.versionLine" -> versionLine
       id.withExtraAttributes(id.extraAttributes + line)
     },
     Test / publishArtifact := false,
