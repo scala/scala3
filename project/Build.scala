035cdc3cf5247ba983c2c516cbff7da5a9895f04
Piotr Chabelski <ged.subfan@gmail.com>
2025-12-12 15:23:15 +0100

    Bump `coursier` to 2.1.25-M21 (was 2.1.25-M19)

--- project/Build.scala
+++ project/Build.scala
@@ -136,7 +136,7 @@ object Build {
   /** Version of Scala CLI to download */
   val scalaCliLauncherVersion = "1.11.0"
   /** Version of Coursier to download for initializing the local maven repo of Scala command */
-  val coursierJarVersion = "2.1.25-M19"
+  val coursierJarVersion = "2.1.25-M21"
 
   object CompatMode {
     final val BinaryCompatible = 0
