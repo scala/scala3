5fcd43b34b3a58989bc867657476b71d8ea812c3
Natsu Kagami <nki@nkagami.me>
2025-12-16 15:02:33 +0100

    Use a LinkedHashSet to store keys in a strict KeySet
    We need a data structure that both acts like a Set (queries membership efficiently), and has a stable iteration order.
    LinkedHashSet seems to fit this criteria.

--- library/src/scala/collection/Map.scala
+++ library/src/scala/collection/Map.scala
@@ -217,7 +217,7 @@ transparent trait MapOps[K, +V, +CC[_, _] <: IterableOps[?, AnyConstr, ?], +C]
   protected trait GenKeySet @retains[MapOps.this.type] () { // todo change @retains to uses_init when we bootstrap with 3.8.1
     this: Set[K] =>
     // CC note: this is unavoidable to make the KeySet pure.
-    private[MapOps] val allKeys = MapOps.this.keysIterator.toList
+    private[MapOps] val allKeys = MapOps.this.keysIterator.to(mutable.LinkedHashSet)
     // We restore the lazy behavior in LazyKeySet
     def iterator: Iterator[K] =
       allKeys.iterator
