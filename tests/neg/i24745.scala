a37f089cb34e4b19255e6bc6a786e47cd71deca8
Som Snytt <som.snytt@gmail.com>
2025-12-16 10:45:22 -0800

    Test arg order

--- tests/neg/i24745.scala
+++ tests/neg/i24745.scala
@@ -3,10 +3,19 @@ extension (s: String)
   def g_::(x: (suffix: String, n: Int)): String = s"$s${x.suffix * x.n}"
   def ok_::(using suffix: String, n: Int): String = s"$s${suffix * n}"
   def no_::(suffix: String, n: Int): String = s"$s${suffix * n}" // error
+  def huh_::(using DummyImplicit)(suffix: String, n: Int): String = s"$s${suffix * n}"
+
+def local =
+  extension (s: String) def f_::(using DummyImplicit): String = s.reverse
+  "hello, world".f_::
 
 @main def Test =
   println:
     "hello, world".f_::
+  println:
+    f_::
+      ("hello, world")
+      (using DummyImplicit.dummyImplicit)
   println:
     (suffix = "s", n = 3).g_::("hello, world")
   println:
@@ -15,3 +24,5 @@ extension (s: String)
     given String = "s"
     given Int = 3
     "hello, world".ok_::
+  println:
+    "hello, world".huh_::("s", 3)
