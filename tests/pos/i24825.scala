60a06a6aaa814321d2092355f5ad43b64b088ee7
Som Snytt <som.snytt@gmail.com>
2025-12-24 12:25:04 -0800

    Permit override enum, improve checks

---
+++ tests/pos/i24825.scala
@@ -0,0 +1,26 @@
+sealed trait A:
+  type Blah <: reflect.Enum
+
+case object B extends A:
+  override class Blah extends reflect.Enum:
+    // Members declared in scala.reflect.Enum
+    def ordinal: Int = ???
+
+    // Members declared in scala.Equals
+    def canEqual(that: Any): Boolean = ???
+
+    // Members declared in scala.Product
+    def productArity: Int = ???
+    def productElement(n: Int): Any = ???
+
+case object C extends A:
+  override enum Blah:
+    case Testing
+
+sealed trait Alt:
+  type Blah <: reflect.Enum
+  val Blah: AnyRef
+
+case object Alternatively extends Alt:
+  override enum Blah:
+    case Testing
