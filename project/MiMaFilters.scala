fab225afb91b3c45b9f70e1aa543bd4cdccfbb3a
Wojciech Mazur <wmazur@virtuslab.com>
2025-12-30 19:39:47 +0100

    Move LazyList$State from forward to backward breaking changes filters
    [Cherry-picked a9aecfac2ad56c2c652686221cbb7faf0f544fc5][modified]

--- project/MiMaFilters.scala
+++ project/MiMaFilters.scala
@@ -34,8 +34,6 @@ object MiMaFilters {
         ProblemFilters.exclude[MissingClassProblem]("scala.caps.Control"),
         // scala/scala3#24545 / scala/scala3#24788
         ProblemFilters.exclude[MissingClassProblem]("scala.annotation.unchecked.uncheckedOverride"),
-        // scala/scala3#24855 - copied from Scala 2.13.16 by ScalaLibraryPlugin, to be removed when Scala 3.8.0 is released
-        ProblemFilters.exclude[MissingClassProblem]("scala.collection.immutable.LazyList$State*"),
       ),
 
     )
@@ -497,7 +495,10 @@ object MiMaFilters {
 
           // NO IDEA FOR NOW :)
           ProblemFilters.exclude[ReversedMissingMethodProblem]("scala.collection.mutable.ArrayDequeOps.scala$collection$mutable$ArrayDequeOps$$super$sliding"),
-        ),
+
+          // scala/scala3#24855 - copied from Scala 2.13.16 by ScalaLibraryPlugin, to be removed when Scala 3.8.0 is released
+          ProblemFilters.exclude[MissingClassProblem]("scala.collection.immutable.LazyList$State*"),
+      )
     )
   }
 
