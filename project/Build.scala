29b448534623a3f6746252187755998ce484a378
Wojciech Mazur <wmazur@virtuslab.com>
2025-12-31 14:34:04 +0100

    Release Scala 3.8.0-RC5

--- project/Build.scala
+++ project/Build.scala
@@ -80,7 +80,7 @@ object Build {
    *  During release candidate cycle incremented by the release officer before publishing a subsequent RC version;
    *  During final, stable release is set exactly to `developedVersion`.
   */
-  val baseVersion = s"$developedVersion-RC4"
+  val baseVersion = s"$developedVersion-RC5"
 
   /** The version of TASTY that should be emitted, checked in runtime test
    *  For defails on how TASTY version should be set see related discussions:
