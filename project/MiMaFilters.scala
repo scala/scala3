0c6cc15157513694bf5461a8ba6acafd997aad83
Ruslan Shevchenko <ruslan@shevchenko.kiev.ua>
2026-01-02 12:38:04 +0200

    Fix exhaustivity checking for Tuple.unapplySeq with empty patterns
    When matching on abstract Tuple with case Tuple(), the unapplySeq
    extractor is used. The exhaustivity checker didn't understand that
    Tuple() via unapplySeq covers EmptyTuple.
    
    This adds special handling in the pattern space projection to map
    Tuple.unapplySeq with empty patterns directly to Typ(EmptyTuple),
    allowing the exhaustivity checker to correctly determine that
    case Tuple() covers the EmptyTuple case.

--- project/MiMaFilters.scala
+++ project/MiMaFilters.scala
@@ -34,7 +34,6 @@ object MiMaFilters {
         ProblemFilters.exclude[DirectMissingMethodProblem]("scala.Tuple.unapply20"),
         ProblemFilters.exclude[DirectMissingMethodProblem]("scala.Tuple.unapply21"),
         ProblemFilters.exclude[DirectMissingMethodProblem]("scala.Tuple.unapply22"),
-        ProblemFilters.exclude[DirectMissingMethodProblem]("scala.Tuple.unapplySeq"),
         ProblemFilters.exclude[MissingClassProblem]("scala.TupleMacros"),
         ProblemFilters.exclude[MissingClassProblem]("scala.TupleMacros$"),
       ),
