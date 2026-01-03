5a3f6a1866109d7c6f6cc8db77ecea6611bb5bb2
xuwei-k <6b656e6a69@gmail.com>
2026-01-03 19:42:51 +0900

    Fix scala-library scaladoc

--- library/src/scala/annotation/internal/MappedAlternative.scala
+++ library/src/scala/annotation/internal/MappedAlternative.scala
@@ -7,8 +7,8 @@ import language.experimental.captureChecking
  *  explore deeper into the types of the opverloaded alternatives.
  *  Its tree is a TypeTree with two parameters which are both needed to
  *  fine default getters in later parameter sections.
- *  @param  Prefix    the prefix field of the original alternative TermRef
- *  @param  SkipCount a ConstantType referring to the number of skipped term parameters
+ *  @tparam  Prefix    the prefix field of the original alternative TermRef
+ *  @tparam  SkipCount a ConstantType referring to the number of skipped term parameters
  *  The annotation is short-lived since mapped symbols are discarded immediately
  *  once an overloading resolution step terminates.
  */
