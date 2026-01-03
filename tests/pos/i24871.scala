878da476f99be1dbf052c978029c67dd2bb9b57a
krrish175-byte <krrishbiswas175@gmail.com>
2026-01-03 13:35:24 +0530

    Fix test file: wrap for-yield expression in object method

--- tests/pos/i24871.scala
+++ tests/pos/i24871.scala
@@ -6,7 +6,9 @@ object X:
   def apply()(d: Double) = List(List(d))
   def apply[T]()(t: C[T])(d: Double) = List(List(d))
 
-for
-  List(x) <- X()(.0)
-yield
-  x
+object Test:
+  def test =
+    for
+      List(x) <- X()(.0)
+    yield
+      x
