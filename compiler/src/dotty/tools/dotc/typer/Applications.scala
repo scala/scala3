48a81e5485cb9a2ce6d2227582d56b7d640422f7
krrish175-byte <krrishbiswas175@gmail.com>
2026-01-04 15:53:57 +0530

    Fix bootstrap test failures in overload resolution between empty params and varargs

--- compiler/src/dotty/tools/dotc/typer/Applications.scala
+++ compiler/src/dotty/tools/dotc/typer/Applications.scala
@@ -2039,7 +2039,13 @@ trait Applications extends Compatibility {
         case tp1: MethodType => // (1)
           tp1.paramInfos.isEmpty
           && tp2.isInstanceOf[LambdaType]
-          && !tp2.isVarArgsMethod
+          && {
+            // When tp1 is nullary (empty parameter list), it's as good as tp2 only if
+            // tp2 is not a varargs method (which is less specific than an explicit empty list)
+            tp2 match
+              case tp2: MethodType => !tp2.isVarArgsMethod
+              case _ => true
+          }
           || {
             if tp1.isVarArgsMethod then
               tp2.isVarArgsMethod
