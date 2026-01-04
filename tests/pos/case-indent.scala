db31bfbe2ebf04f6f3a0896f722323e706174355
Som Snytt <som.snytt@gmail.com>
2026-01-04 15:21:24 -0800

    More examples of relaxed colon case

--- tests/pos/case-indent.scala
+++ tests/pos/case-indent.scala
@@ -1,14 +1,35 @@
 import language.experimental.relaxedColonSyntax
+import language.experimental.relaxedLambdaSyntax
 
 def f[A](xs: List[A]): List[String] =
   xs.map:
   case s: String => s
   case x => x.toString
+  .map:
+  case s: String if s.length > 4 => s
+  case s => f"$s%.04s"
+
+def g(xs: List[Int]): List[String] =
+  xs.map: case i: Int => i.toString // "relaxed lambda"
+  .map:
+  case s: String if s.length > 4 => s
+  case s => f"$s%.04s"
 
 class Extra:
   val pf: PartialFunction[String, Int] =
   case "foo" => 1
   case "bar" => 2
+
+  def tryit(xs: List[String]) = xs.collect(pf)
+
+class Possibly(b: Boolean):
+  val pf: PartialFunction[String, Int] =
+    if b then
+    case "foo" => 1
+    case "bar" => 2
+    else
+    case "foo" => 42
+    case "bar" => 27
 
   def tryit(xs: List[String]) = xs.collect(pf)
 
@@ -21,4 +42,8 @@ class Functional:
   println:
     f(List(42))
   println:
+    g(List(42))
+  println:
     Extra().tryit("baz" :: "bar" :: "foo" :: Nil)
+  println:
+    Possibly(false).tryit("baz" :: "bar" :: "foo" :: Nil)
