97c904f7eb7f093ec65f32f0049a8505f4e4eb49
Li Haoyi <haoyi.sg@gmail.com>
2026-01-10 21:30:32 +0800

    .

---
+++ tests/pos/i24707/MockLayout_1.java
@@ -0,0 +1,10 @@
+public class MockLayout {
+    public int marginTop;
+    public int marginBottom;
+    public int marginLeft;
+    public int marginRight;
+    public int spacing;
+    public boolean wrap;
+
+    public MockLayout(int style) {}
+}
