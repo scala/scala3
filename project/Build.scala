73ecd48aca775085ec0b71db22a638c7fe189996
Wojciech Mazur <wmazur@virtuslab.com>
2026-01-13 09:26:29 +0100

    Release Scala 3.8.0
    Signed-off-by: Wojciech Mazur <wmazur@virtuslab.com>

--- project/Build.scala
+++ project/Build.scala
@@ -80,7 +80,7 @@ object Build {
    *  During release candidate cycle incremented by the release officer before publishing a subsequent RC version;
    *  During final, stable release is set exactly to `developedVersion`.
   */
-  val baseVersion = s"$developedVersion-RC6"
+  val baseVersion = developedVersion
 
   /** The version of TASTY that should be emitted, checked in runtime test
    *  For defails on how TASTY version should be set see related discussions:
@@ -98,7 +98,7 @@ object Build {
    *      - in release candidate branch is experimental if {patch == 0}
    *      - in stable release is always non-experimetnal
    */
-  val expectedTastyVersion = "28.8-experimental-1"
+  val expectedTastyVersion = "28.8"
   checkReleasedTastyVersion()
 
   /** Final version of Scala compiler, controlled by environment variables. */
