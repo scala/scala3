47283a5a8197a9e8ec55021b320e155cc7d2064d
Solal Pirelli <solal.pirelli@gmail.com>
2026-01-13 09:41:12 +0100

    more

--- compiler/test/dotty/tools/backend/jvm/OptimizationBytecodeTests.scala
+++ compiler/test/dotty/tools/backend/jvm/OptimizationBytecodeTests.scala
@@ -30,22 +30,54 @@ class OptimizationBytecodeTests extends DottyBytecodeTest {
   }
 
 
-  @Test def inlineTuple2Creation =
+  @Test def inlineTuple2Specialized =
     testEquivalence(
       "1",
       "val t = (1, 2); t._1"
     )
 
-  @Test def inlineTuple2CreationTwice =
+  @Test def inlineTuple2SpecializedTwice =
     testEquivalence(
       "1",
       "val t = (1, 2); val t2 = (3, t._1); t2._2"
     )
 
-  @Test def inlineTuple3Creation =
+  @Test def inlineTuple2OfTuple2Specialized =
+    testEquivalence(
+      "1 + 2 + 3",
+      "val ((a, b), c) = ((1, 2), 3); a + b + c"
+    )
+
+  @Test def inlineTuple2SpecializedCastToGeneral =
+    testEquivalence(
+      "3",
+      "val t = (3, 4); (t: Tuple2[Any, Any])._1.asInstanceOf[Int]"
+    )
+
+  @Test def inlineTuple3Specialized =
     testEquivalence(
       "1",
       "val t = (3, 1, 2); t._2"
+    )
+
+  @Test def inlineTuple2General =
+    testEquivalence(
+      "x",
+      "val a: Any = x; val t = (a, 42); t._1.asInstanceOf[Int]",
+      params = List("x: Int"),
+    )
+
+  @Test def inlineTuple2OfTuple2General =
+    testEquivalence(
+      "1 + 2 + 3",
+      "val ((a, b), c) = ((1, 2), Integer.valueOf(3)); a + b + c"
+    )
+
+  @Test def inlineTuple2GeneralAndSpecialized =
+    testEquivalence(
+      "x",
+      "val t1 = (x, 0); val a: Any = t1._1; val t2 = (a, 42); t2._1.asInstanceOf[Int]",
+      params = List("x: Int")
     )
 
 
@@ -73,10 +105,132 @@ class OptimizationBytecodeTests extends DottyBytecodeTest {
     )
 
 
-  @Test def inlineBoxesRunTimeUnbox =
+  @Test def inlineBoxUnbox =
+    testEquivalence(
+      "x",
+      "val a: Any = x; a.asInstanceOf[Int]",
+      params = List("x: Int")
+    )
+
+  @Test def inlineBoxUnboxMixedArithmetic =
+    testEquivalence(
+      "x + y",
+      "val a: java.lang.Integer = x; val b: java.lang.Long = y; a + b",
+      params = List("x: Int", "y: Long"),
+      returnType = "Long"
+    )
+
+  @Test def inlineBoxUnboxValEscaped =
+    testEquivalence(
+      "escape(x); x",
+      "val a: Any = x; escape(a); a.asInstanceOf[Int]",
+      params = List("x: Int"),
+      extraMemberSources = List("@noinline def escape(a: Any): Unit = ???")
+    )
+
+  @Test def inlineBoxUnboxValEscapedBranch =
+    testEquivalence(
+      "escape(if b then 10 else 32); if b then 10 else 32",
+      "val i = Integer.valueOf(10); val j = Integer.valueOf(32); escape(if b then i else j); if b then i.toInt else j.toInt",
+      params = List("b: Boolean"),
+      extraMemberSources = List("@noinline def escape(a: Any): Unit = ???")
+    )
+
+  @Test def inlineBoxUnboxVarClosure =
+    testEquivalence(
+      "x",
+      "var a: Any = x; def c() = a.asInstanceOf[Int]; c()",
+      params = List("x: Int")
+    )
+
+  @Test def inlineBoxUnboxBranch =
+    testEquivalence(
+      "if x >= 0 then 1 else 2",
+      "var a = if x >= 0 then (1: Any) else 2; a.asInstanceOf[Int]",
+      params = List("x: Int")
+    )
+
+  @Test def inlineBoxUnboxBranchContrived =
+    testEquivalence(
+      "String.valueOf(x)",
+      "val wat: Any = if (x > 0) x else -x; wat match { case x: Int => String.valueOf(x); case _ => \"?\" }",
+      params = List("x: Int"),
+      returnType = "String"
+    )
+
+  @Test def inlineBoxUnboxJava =
+    testEquivalence(
+      "x",
+      "val a = java.lang.Integer.valueOf(x); a.asInstanceOf[Int]",
+      params = List("x: Int")
+    )
+
+  @Test def inlineBoxUnboxJavaBranch =
+    testEquivalence(
+      "10",
+      "val b = java.lang.Boolean.valueOf(true); val a = if b then java.lang.Integer.valueOf(10) else 0; a.asInstanceOf[Int]"
+    )
+
+  @Test def inlineUnboxNull =
     testEquivalence(
       "0",
       "scala.runtime.BoxesRunTime.unboxToInt(null)"
+    )
+
+  @Test def inlineUnboxNull2 =
+    testEquivalence(
+      "0",
+      "null.asInstanceOf[Int]"
+    )
+
+
+  @Test def inlineRef =
+    testEquivalence(
+      "0",
+      "val r = scala.runtime.IntRef(0); r.elem"
+    )
+
+  @Test def inlineRefBranch =
+    testEquivalence(
+      "if b then 0 else 1",
+      "val r1 = scala.runtime.IntRef.zero(); val r2 = scala.runtime.IntRef.create(1); val r = if b then r1 else r2; r.elem",
+      params = List("b: Boolean")
+    )
+
+  @Test def inlineRefWrite =
+    testEquivalence(
+      "var l = 10L; l += 3; l",
+      "val r = scala.runtime.LongRef.create(10); r.elem += 3; r.elem",
+      returnType = "Long"
+    )
+
+  @Test def inlineRefUsedAsBranch =
+    testEquivalence(
+      "var x = false; if b then x = true; if b then 1 else 2",
+      "val x = scala.runtime.BooleanRef.create(false); if b then x.elem = true; if x.elem then 1 else 2",
+      params = List("b: Boolean")
+    )
+
+
+  @Test def inlineMatch =
+    testEquivalence(
+      "a + b",
+      "(a, b) match { case (a: Int, b: Int) => a + b; case _ => -1 }",
+      params = List("a: Int", "b: Int")
+    )
+
+  @Test def inlineMatchBoxed =
+    testEquivalence(
+      "a + b",
+      "(a: Any, b: Any) match { case (a: Int, b: Int) => a + b; case _ => -1 }",
+      params = List("a: Int", "b: Int")
+    )
+
+  @Test def inlineMatchResult =
+    testEquivalence(
+      "if n == 0 then 1 + 2 else 3 + 4",
+      "val (a, b) = n match { case 0 => (1, 2); case _ => (3, 4) }; a + b",
+      params = List("n: Int")
     )
 
 
@@ -167,19 +321,50 @@ class OptimizationBytecodeTests extends DottyBytecodeTest {
     )
 
 
-  @Test def deadCastPrimitive =
+  @Test def elidedCastPrimitive =
     testEquivalence(
       "x",
       "x.asInstanceOf[Int]",
       params = List("x: Int")
     )
 
-  @Test def deadCastClass =
+  @Test def elidedCastClass =
     testEquivalence(
       "x",
       "x.asInstanceOf[String]",
       params = List("x: String"),
       returnType = "String"
+    )
+
+  @Test def elidedIsInstanceTop =
+    testEquivalence(
+      "true",
+      "val b = (x: Integer); b.isInstanceOf[Object]",
+      params = List("x: Int"),
+      returnType = "Boolean"
+    )
+
+  @Test def elidedIsInstanceInterface =
+    testEquivalence(
+      "true",
+      "val b = (x: Integer); b.isInstanceOf[Number]",
+      params = List("x: Int"),
+      returnType = "Boolean"
+    )
+
+
+  @Test def deadBoxJava =
+    testEquivalence(
+      "x",
+      "java.lang.Integer.valueOf(x); x",
+      params = List("x: Int")
+    )
+
+  @Test def deadBoxScala =
+    testEquivalence(
+      "x",
+      "scala.runtime.BoxesRunTime.boxToInteger(x); x",
+      params = List("x: Int")
     )
 
 
