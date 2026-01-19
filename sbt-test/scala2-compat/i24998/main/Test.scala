6b39f2d648b911cd86f7928d24ff91265d36e445
Wojciech Mazur <wmazur@virtuslab.com>
2026-01-20 00:25:17 +0100

    Inline all scripted tests

--- sbt-test/scala2-compat/i24998/main/Test.scala
+++ sbt-test/scala2-compat/i24998/main/Test.scala
@@ -1,11 +1,11 @@
 object TestRuntimeCompat  {
   def main(args: Array[String]): Unit =  {
-    // unoptimized
+    // Failed in unoptimized
     ArraysOpsTests()
     RangeTests()
     EnumerationTests()
 
-    // optimized
+    // Failed only in optimized and heavily inlined
     scala.collection.immutable.test.ReadBlackTreeTests()
     OrderingTests()
     MurmurHash3Tests()
