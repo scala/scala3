675476cb5e4900b3d4a362cca8864a9ecd4963d2
Wojciech Mazur <wmazur@virtuslab.com>
2026-01-20 12:49:22 +0100

    Fetch and compile Scala 2.13 library directly in the Scala 3 build
    [Cherry-picked e07b7269310ed300418bccef42ec6350679b8bcc][modified]

--- project/ScalaLibraryPlugin.scala
+++ project/ScalaLibraryPlugin.scala
@@ -5,6 +5,7 @@ import sbt.Keys.*
 import sbt.io.Using
 import scala.jdk.CollectionConverters.*
 import scala.collection.mutable
+import java.io.File
 import java.nio.file.Files
 import java.nio.ByteBuffer
 import xsbti.VirtualFileRef
@@ -40,15 +41,13 @@ object ScalaLibraryPlugin extends AutoPlugin {
     Scala2PickleAnnotations.contains(descriptor)
 
   object autoImport {
-    val keepSJSIR = settingKey[Boolean]("Should we patch .sjsir too?")
+    val scala2LibraryClasspath = taskKey[Vector[File]]("Jars containing Scala library artifacts to be used when patching")
   }
 
   import autoImport._
 
-  override def projectSettings = Seq (
-    // Custom resolver providing access to custom scala-library artifacts
-    // Currently using stub, available until Feb 19 2026
-    externalResolvers +=  "Scala 2 no-inlined artifacts" at "https://scala3.westeurope.cloudapp.azure.com/maven2/scala2-noinline/",
+
+  override def projectSettings = Seq(
     // Settings to validate that JARs don't contain Scala 2 pickle annotations and have valid TASTY attributes
     Compile / packageBin := (Compile / packageBin)
       .map{ jar =>
@@ -60,25 +59,11 @@ object ScalaLibraryPlugin extends AutoPlugin {
       .value,
     (Compile / manipulateBytecode) := {
       val stream = streams.value
-      val target = (Compile / classDirectory).value
-      val lm = dependencyResolution.value
       val log = stream.log
-      val cache  = stream.cacheDirectory
-      val retrieveDir = cache / "scalajs-scalalib" / scalaVersion.value
-
-      val scalalibArtifact =
-        if (keepSJSIR.value) "org.scala-js" % "scalajs-scalalib_2.13" % s"$scala2Version+$scalaJSVersion"
-        else "org.scala-lang" % "scala-library" % scala2NonOptimized
-
-      val comp = lm.retrieve(scalalibArtifact, scalaModuleInfo = None, retrieveDir, log)
-          .fold(w => throw w.resolveException, identity)
-          .filterNot(_.getPath().contains("javalib"))
-          .filter(!_.getPath().contains("scalajs-scalalib_2.13") || keepSJSIR.value)
-          .distinct
+      val classDir = (Compile / classDirectory).value
 
       // Fetch classfiles and sjsir files
-      val patches: Seq[(Set[File], File)] = comp.map(fetch(stream, _))
-
+      val patches: Seq[(Set[File], File)] = scala2LibraryClasspath.value.map(fetch(stream, _))
       val previous = (Compile / manipulateBytecode).value
 
       val analysis = previous.analysis match {
@@ -87,18 +72,17 @@ object ScalaLibraryPlugin extends AutoPlugin {
       }
       var stamps = analysis.stamps
 
-      val classDir = (Compile / classDirectory).value
-
       // Patch the files that are in the list
       for {
         (files, reference) <- patches
-        file <- files
+        file <- files.toSeq.sorted
         id <- file.relativeTo(reference)
         path = id.toString().replace("\\", "/").stripSuffix(".class").stripSuffix(".sjsir")
         if filesToCopy.exists(s => path == s || path.startsWith(s + '$')) // Only Override Some Very Specific Files
-        dest = target / (id.toString)
+        dest = classDir / (id.toString)
         ref <- dest.relativeTo((LocalRootProject / baseDirectory).value)
       } {
+        log.debug(s"Replacing generated .class file with Scala 2.13 patch: ${id}")
         patchFile(input = file, output = dest, classDirectory = classDir)
         // Update the timestamp in the analysis
         stamps = stamps.markProduct(
@@ -116,9 +100,11 @@ object ScalaLibraryPlugin extends AutoPlugin {
 
       for ((files, reference) <- patches) {
         val diff = files.filterNot(file => overwrittenBinaries.contains(file.relativeTo(reference).get))
+        log.debug(s"Found ${diff.size} .class files specific to Scala 2 in scala-library")
         // Copy all the specialized classes in the stdlib
         // no need to update any stamps as these classes exist nowhere in the analysis
-        for (orig <- diff; dest <- orig.relativeTo(reference)) {
+        for (orig <- diff.toSeq.sorted; dest <- orig.relativeTo(reference)) {
+          log.debug(s"Adding Scala 2.13 specific .class file: ${dest}")
           patchFile(
             input = orig,
             output = classDir / dest.toString(),
@@ -130,7 +116,6 @@ object ScalaLibraryPlugin extends AutoPlugin {
       previous
         .withAnalysis(analysis.copy(stamps = stamps)) // update the analysis with the correct stamps
         .withHasModified(true)  // mark it as updated for sbt to update its caches
-
     }
   )
 
@@ -381,7 +366,7 @@ object ScalaLibraryPlugin extends AutoPlugin {
     "scala/runtime/NonLocalReturnControl",
     "scala/util/hashing/MurmurHash3",
     "scala/util/Sorting",
-    )
+  )
 
   /** Extract the SourceFile attribute from class file bytecode */
   private def extractSourceFile(bytes: Array[Byte]): Option[String] = {
@@ -510,5 +495,64 @@ object ScalaLibraryPlugin extends AutoPlugin {
       // Scala attribute is empty (length = 0x0)
       new ByteVector(0)
     }
+  }
+
+  def fetchScalaJsScalaLibrary: Def.Initialize[Task[Vector[File]]] = Def.task {
+    val stream = streams.value
+    val target = (Compile / classDirectory).value
+    val lm = dependencyResolution.value
+    val log = stream.log
+    val cache  = stream.cacheDirectory
+    val retrieveDir = cache / "scalajs-scalalib" / scalaVersion.value
+
+    val scalalibArtifact = "org.scala-js" % "scalajs-scalalib_2.13" % s"$scala2Version+$scalaJSVersion"
+    lm.retrieve(scalalibArtifact, scalaModuleInfo = None, retrieveDir, log)
+        .fold(w => throw w.resolveException, identity)
+        .filterNot(_.getPath().contains("javalib"))
+        .distinct
+    }
+
+  def fetchScala2LibrarySources(targetDirectory: SettingKey[File]): Def.Initialize[Task[File]] = Def.task {
+    val version = scala2Version
+
+    val targetDir = targetDirectory.value
+    val stream = streams.value
+    val log = stream.log
+    val cache  = stream.cacheDirectory
+    val scalaStdLibraryDep = "org.scala-lang" % "scala-library" % version
+    lazy val scalaLibSourcesJar =
+      dependencyResolution.value
+        .retrieve(
+          scalaStdLibraryDep.classifier("sources"),
+          scalaModuleInfo = None, retrieveDirectory = cache, log = log
+        )
+        .getOrElse(sys.error(s"Could not fetch ${scalaStdLibraryDep} sources for version $version"))
+        .find(_.name.endsWith(s"scala-library-$version-sources.jar"))
+        .getOrElse(sys.error(s"Not expected .jar file in retrived dependencies"))
+
+    // Auxilary source files that cannot be compield
+    val excludedSourceFiles = Set(
+        "scala/AnyRef.scala",
+        "scala/Any.scala",
+        "scala/Nothing.scala",
+        "scala/Null.scala",
+        "scala/Singleton.scala",
+    )
+    FileFunction.cached(
+      cache / s"scala-library-sources-$version",
+      FilesInfo.lastModified,
+      FilesInfo.exists
+    ) { _ =>
+      log.debug(s"Unpacking Scala ${version} library sources to $targetDir...")
+      if (targetDir.exists)
+        IO.delete(targetDir)
+      IO.createDirectory(targetDir)
+      IO.unzip(
+        from = scalaLibSourcesJar,
+        toDirectory = targetDir,
+        filter = new SimpleFilter(path => !excludedSourceFiles.contains(path.replace(File.separatorChar, '/')))
+      )
+    }(Set(scalaLibSourcesJar))
+    targetDir
   }
 }
