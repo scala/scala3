16d9036b906b5eca3c4aff0ca7027478384fb9f8
Wojciech Mazur <wmazur@virtuslab.com>
2026-01-22 01:54:05 +0100

    correct the list of files to copy from Scala 2
    [Cherry-picked b287ca1bcb0ebd06a43261e257f6e51aeb9a940e][modified]

--- project/MiMaFilters.scala
+++ project/MiMaFilters.scala
@@ -9,6 +9,11 @@ object MiMaFilters {
       // Additions that require a new minor version of the library
       Build.mimaPreviousDottyVersion -> Seq(
         ProblemFilters.exclude[DirectMissingMethodProblem]("scala.caps.package#package.freeze"),
+        ProblemFilters.exclude[DirectMissingMethodProblem]("scala.caps.package#package.cap"),
+        ProblemFilters.exclude[FinalClassProblem]("scala.Function1$UnliftOps$"),
+        ProblemFilters.exclude[FinalClassProblem]("scala.jdk.Accumulator$AccumulatorFactoryShape$"),
+        ProblemFilters.exclude[DirectMissingMethodProblem]("scala.collection.ArrayOps.iterateUntilEmpty$extension"),
+        ProblemFilters.exclude[DirectMissingMethodProblem]("scala.collection.ArrayOps.scala$collection$ArrayOps$$elemTag$extension"),
       ),
 
     )
@@ -19,7 +24,34 @@ object MiMaFilters {
 
       // Breaking changes since last reference version
       Build.mimaPreviousDottyVersion -> Seq(
-        ProblemFilters.exclude[MissingTypesProblem]("scala.util.control.NonLocalReturns$ReturnThrowable")
+        ProblemFilters.exclude[MissingTypesProblem]("scala.util.control.NonLocalReturns$ReturnThrowable"),
+        // THIS IS FINE, IT SHOULD HAVE BEEN THIS WAY
+        ProblemFilters.exclude[MissingTypesProblem]("scala.Function1$"),
+        ProblemFilters.exclude[MissingTypesProblem]("scala.Function1$UnliftOps$"),
+        ProblemFilters.exclude[MissingTypesProblem]("scala.Product1$"),
+        ProblemFilters.exclude[MissingTypesProblem]("scala.Product2$"),
+        ProblemFilters.exclude[MissingTypesProblem]("scala.collection.ArrayOps$"),
+        ProblemFilters.exclude[MissingTypesProblem]("scala.jdk.Accumulator$"),
+        ProblemFilters.exclude[MissingTypesProblem]("scala.jdk.Accumulator$AccumulatorFactoryShape$"),
+        ProblemFilters.exclude[DirectMissingMethodProblem]("scala.Function1#UnliftOps.equals$extension"),
+        ProblemFilters.exclude[DirectMissingMethodProblem]("scala.Function1#UnliftOps.hashCode$extension"),
+        ProblemFilters.exclude[DirectMissingMethodProblem]("scala.Function1#UnliftOps.unlift$extension"),
+        ProblemFilters.exclude[DirectMissingMethodProblem]("scala.collection.ArrayOps.iterateUntilEmpty$extension"),
+        ProblemFilters.exclude[DirectMissingMethodProblem]("scala.collection.ArrayOps.scala$collection$ArrayOps$$$iterateUntilEmpty$extension"),
+        ProblemFilters.exclude[DirectMissingMethodProblem]("scala.collection.ArrayOps.scala$collection$ArrayOps$$$elemTag$extension"),
+        ProblemFilters.exclude[DirectMissingMethodProblem]("scala.jdk.Accumulator#AccumulatorFactoryShape.anyAccumulatorFactoryShape"),
+        ProblemFilters.exclude[DirectMissingMethodProblem]("scala.jdk.Accumulator#AccumulatorFactoryShape.doubleAccumulatorFactoryShape"),
+        ProblemFilters.exclude[DirectMissingMethodProblem]("scala.jdk.Accumulator#AccumulatorFactoryShape.intAccumulatorFactoryShape"),
+        ProblemFilters.exclude[DirectMissingMethodProblem]("scala.jdk.Accumulator#AccumulatorFactoryShape.jDoubleAccumulatorFactoryShape"),
+        ProblemFilters.exclude[DirectMissingMethodProblem]("scala.jdk.Accumulator#AccumulatorFactoryShape.jIntegerAccumulatorFactoryShape"),
+        ProblemFilters.exclude[DirectMissingMethodProblem]("scala.jdk.Accumulator#AccumulatorFactoryShape.jLongAccumulatorFactoryShape"),
+        ProblemFilters.exclude[DirectMissingMethodProblem]("scala.jdk.Accumulator#AccumulatorFactoryShape.longAccumulatorFactoryShape"),
+        ProblemFilters.exclude[MissingFieldProblem]("scala.Function1.UnliftOps"),
+        ProblemFilters.exclude[MissingFieldProblem]("scala.jdk.Accumulator.AccumulatorFactoryShape"),
+        ProblemFilters.exclude[FinalMethodProblem]("scala.jdk.Accumulator.++:"),
+        ProblemFilters.exclude[FinalMethodProblem]("scala.jdk.Accumulator.:++"),
+        ProblemFilters.exclude[FinalMethodProblem]("scala.jdk.Accumulator.concat"),
+        ProblemFilters.exclude[FinalMethodProblem]("scala.jdk.Accumulator.sizeHint$default$2"),
       )
     )
   }
