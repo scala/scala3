5206e81532f5ef1ca052fc0e6acfe13739b6e237
Matt Dziuban <mrdziuban@gmail.com>
2026-01-27 08:41:14 -0500

    Treat synthetic casts as pure.
    This aims to fix an issue with erased definitions in the presence of `opaque type`s. The expression generated in the test here is:
    
    ```scala
    {
      val $proxy1: test.type{type Foo = Int} =
        test.$asInstanceOf[test.type{type Foo = Int}]
      val test$_this: ($proxy1 : test.type{type Foo = Int}) = $proxy1
      new test$_this.Bar[Int]():test$_this.Bar[Int]
    }
    ```
    
    This is not considered pure due to the creation of `val $proxy1`, an opaque proxy. Specifically the call to `asInstanceOf` is not considered pure.
    
    This updates `TypedTreeInfo#exprPurity` to consider calls to `defn.Any_typeCast` pure as long as the call is synthetic, which allows the whole expression to be considered pure.
    
    Closes #24990

--- compiler/src/dotty/tools/dotc/ast/TreeInfo.scala
+++ compiler/src/dotty/tools/dotc/ast/TreeInfo.scala
@@ -596,6 +596,7 @@ trait TypedTreeInfo extends TreeInfo[Type] { self: Trees.Instance[Type] =>
           || sym == defn.Predef_classOf
           || sym == defn.Compiletime_erasedValue && tree.tpe.dealias.isInstanceOf[ConstantType]
           || defn.capsErasedValueMethods.contains(sym)
+          || sym == defn.Any_typeCast && sym.is(Synthetic)
       then Pure
       else Impure
     case Apply(fn, args) =>
