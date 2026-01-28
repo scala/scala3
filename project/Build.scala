c0aba9f8afaa2b41bcb1cd2c2caf4d532b35d024
Wojciech Mazur <wmazur@virtuslab.com>
2026-01-28 21:25:35 +0100

    Link to Java 17 JavaDoc
    [Cherry-picked b86520a848c4a6d825724f10296cccfaad7c9cf0]

--- project/Build.scala
+++ project/Build.scala
@@ -2849,7 +2849,7 @@ object ScaladocConfigs {
   private lazy val currentYear: String = java.util.Calendar.getInstance().get(java.util.Calendar.YEAR).toString
 
   def dottyExternalMapping = ".*scala/.*::scaladoc3::https://dotty.epfl.ch/api/"
-  def javaExternalMapping = ".*java/.*::javadoc::https://docs.oracle.com/javase/8/docs/api/"
+  def javaExternalMapping = ".*java/.*::javadoc::https://docs.oracle.com/en/java/javase/17/docs/api/"
   def defaultSourceLinks(version: String, allowGitSHA: Boolean = true) = {
     def dottySrcLink(v: String) = sys.env.get("GITHUB_SHA") match {
       case Some(sha) if allowGitSHA => s"github://scala/scala3/$sha"
