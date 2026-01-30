844106a5045d843f329d7d97572281aedf2acb89
Emil Ejbyfeldt <emil.ejbyfeldt@choreograph.com>
2026-01-30 14:45:55 +0100

    fixes #24946

---
+++ scaladoc-testcases/src/tests/snippetTestcaseMacro.scala
@@ -0,0 +1,22 @@
+package tests.snippetTestcaseMacro
+
+import scala.quoted.*
+
+/** Regression test for https://github.com/scala/scala3/issues/24946
+  * Scaladoc snippet compilation fails when the snippet calls a macro
+  * defined in the same compilation unit.
+  */
+object MacroLib:
+  inline def hello: String = ${ helloImpl }
+
+  def helloImpl(using Quotes): Expr[String] = Expr("Hello")
+
+class SnippetTestcaseMacro:
+  /** This snippet should compile successfully but currently fails.
+    *
+    * SNIPPET(OUTERLINEOFFSET:18,OUTERCOLUMNOFFSET:6,INNERLINEOFFSET:4,INNERCOLUMNOFFSET:2)
+    * ```scala sc:compile
+    * MacroLib.hello
+    * ```
+    */
+  def a = 3
