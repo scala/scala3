1497d261a8e7ba53a737839a0a5415d6763f155b
Said BOUDJELDA <bmscomp@gmail.com>
2026-02-03 16:01:03 +0100

    Fix XML element span to include opening '<' character
    - Change curOffset to curOffset - 1 in element() and xPattern()
    - Update tests/neg/i19100.check with corrected error positions

--- compiler/src/dotty/tools/dotc/parsing/xml/MarkupParsers.scala
+++ compiler/src/dotty/tools/dotc/parsing/xml/MarkupParsers.scala
@@ -277,7 +277,7 @@ object MarkupParsers {
      *                | xmlTag1 '/' '>'
      */
     def element: Tree = {
-      val start = curOffset // FIXME should be `curOffset - 1` (scalatest and tests/neg/i19100.scala must be updated)
+      val start = curOffset - 1 // Include the '<' character in the span
       val (qname, attrMap) = xTag(())
       if (ch == '/') { // empty element
         xToken("/>")
@@ -435,7 +435,7 @@ object MarkupParsers {
      *                  | Name [S] '/' '>'
      */
     def xPattern: Tree = {
-      var start = curOffset // FIXME should be `curOffset - 1` (scalatest and tests/neg/i19100.scala must be updated)
+      var start = curOffset - 1 // Include the '<' character in the span
       val qname = xName
       debugLastStartElement = (start, qname) :: debugLastStartElement
       xSpaceOpt()
