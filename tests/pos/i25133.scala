d59ff33e9816a3a65ca12a6e4b95edb513598048
odersky <odersky@gmail.com>
2026-02-06 11:19:59 +0100

    Add variant to test

--- tests/pos/i25133.scala
+++ tests/pos/i25133.scala
@@ -2,6 +2,8 @@ class C extends Object:
   override def toString = "C"
 
 inline def foo(inline x: Object): String = x.toString
+inline def bar(inline x: Object): String = x.toString()
 
 val c = C()
 val s = foo(c)
+val t = bar(c)
