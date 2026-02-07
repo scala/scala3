f4af902415f03c01dc6dffd89271535f35a2e226
Oscar Boykin <oscar.boykin@gmail.com>
2026-02-07 10:22:44 -1000

    Add pending i25208 macro crash reproducer

---
+++ tests/pending/run-macros/i25208/Test_2.scala
@@ -0,0 +1,11 @@
+//> using options -Yexplicit-nulls
+
+package i25208
+
+import NonNullFold.*
+
+object Test:
+  def main(args: Array[String]): Unit =
+    val x: String | Null = "not null"
+    val y = x.foldNN("it's null")(x => x)
+    println(y)
