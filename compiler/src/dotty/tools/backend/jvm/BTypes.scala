e0f8da1339555d19195104280d1fdae0edd4536b
Solal Pirelli <solal.pirelli@gmail.com>
2026-02-09 14:39:47 +0100

    Port BTypes additions for inliner structures

--- compiler/src/dotty/tools/backend/jvm/BTypes.scala
+++ compiler/src/dotty/tools/backend/jvm/BTypes.scala
@@ -4,13 +4,28 @@ package jvm
 
 import scala.language.unsafeNulls
 import java.util.concurrent.ConcurrentHashMap
+import scala.jdk.CollectionConverters.*
 import scala.tools.asm
+import scala.collection.{SortedMap, mutable}
+import scala.collection.immutable.ArraySeq
+import dotty.tools.backend.jvm.BTypes.{EmptyInlineInfo, InlineInfo, InternalName, MethodInlineInfo}
 import dotty.tools.backend.jvm.PostProcessorFrontendAccess.Lazy
-import dotty.tools.backend.jvm.BTypes.InternalName
 import dotty.tools.backend.jvm.BackendReporting.*
+import dotty.tools.backend.jvm.opt.InlineInfoAttribute
+import dotty.tools.dotc.core.Contexts.*
+import dotty.tools.dotc.core.Decorators.*
+import dotty.tools.dotc.core.Flags.{Final, JavaDefined, Method, Sealed}
+import dotty.tools.dotc.core.Phases.picklerPhase
+import dotty.tools.dotc.core.Symbols.{requiredClass as _, *}
+import DottyBackendInterface.given
 
+import dotty.tools.dotc.core.StdNames.nme
+import dotty.tools.dotc.core.Types.abstractTermNameFilter
+
+import scala.annotation.tailrec
 import scala.tools.asm.Opcodes
-import scala.tools.asm.Opcodes.{ACC_PRIVATE, ACC_PROTECTED, ACC_PUBLIC, ACC_STATIC}
+import scala.tools.asm.Opcodes.{ACC_ABSTRACT, ACC_PRIVATE, ACC_PROTECTED, ACC_PUBLIC, ACC_STATIC}
+import scala.tools.asm.tree.ClassNode
 
 
 /**
@@ -556,6 +571,11 @@ sealed trait RefBType extends BType {
  * TODO: innerclass attributes on mirror class
  */
 
+enum InlineInfoSource:
+  case Symbol(classSym: ClassSymbol, internalName: InternalName)
+  case Classfile(classNode: ClassNode)
+  case Missing
+
 /**
  * The type info for a class. Used for symboltable-independent subtype checks in the backend.
  *
@@ -570,9 +590,143 @@ sealed trait RefBType extends BType {
  *                      InnerClass table, see the InnerClass spec summary above.
  *
  * @param nestedInfo    If this describes a nested class, information for the InnerClass table.
+ * @param inlineInfoSource    Information about this class for the inliner.
  */
 final case class ClassInfo(superClass: Option[ClassBType], interfaces: List[ClassBType], flags: Int,
-                           nestedClasses: Lazy[List[ClassBType]], nestedInfo: Lazy[Option[NestedInfo]])
+                           nestedClasses: Lazy[List[ClassBType]], nestedInfo: Lazy[Option[NestedInfo]],
+                           inlineInfoSource: InlineInfoSource) {
+  private var _inlineInfo: InlineInfo | Null = null
+
+  /**
+   * Build the InlineInfo for a ClassBType.
+   *
+   * Note that the InlineInfo is only built from the symbolic information for classes that are being
+   * compiled. For all other classes we delegate to inlineInfoFromClassfile. The reason is that
+   * mixed-in methods are only added to class symbols being compiled, but not to other classes
+   * extending traits. Creating the InlineInfo from the symbol would prevent these mixins from being
+   * inlined.
+   *
+   * So for classes being compiled, the InlineInfo is created here and stored in the ScalaInlineInfo
+   * classfile attribute.
+   */
+  def inlineInfo(bCodeRepository: BCodeRepository, primitives: DottyPrimitives, ts: CoreBTypes)(using Context): InlineInfo =
+    if _inlineInfo eq null then
+      _inlineInfo = inlineInfoSource match {
+        case InlineInfoSource.Symbol(classSym, internalName) =>
+          // phase travel required (or at least it was in Scala 2). for nested classes, it checks if the
+          // enclosingTopLevelClass is being compiled. after flatten, all classes are considered top-level,
+          // so it would return `false`.
+          if atPhase(picklerPhase.next) { classSym.isDefinedInCurrentRun } then buildInlineInfoFromClassSymbol(classSym, primitives, ts) // // InlineInfo required for classes being compiled, we have to create the classfile attribute
+          // For classes not being compiled, the InlineInfo is read from the classfile attribute. This
+          // fixes an issue with mixed-in methods: the mixin phase enters mixin methods only to class
+          // symbols being compiled. For non-compiled classes, we could not build MethodInlineInfos
+          // for those mixin members, which prevents inlining.
+          else bCodeRepository.classNode(internalName) match {
+            case Right(classNode) =>
+              inlineInfoFromClassfile(classNode)
+            case Left(missingClass) =>
+              EmptyInlineInfo.copy(warning = Some(ClassNotFoundWhenBuildingInlineInfoFromSymbol(missingClass)))
+          }
+        case InlineInfoSource.Classfile(classNode) => inlineInfoFromClassfile(classNode)
+        case InlineInfoSource.Missing => EmptyInlineInfo
+      }
+    _inlineInfo.nn
+
+  /**
+   * Build the [[InlineInfo]] for a class symbol.
+   */
+  private def buildInlineInfoFromClassSymbol(classSym: ClassSymbol, primitives: DottyPrimitives, ts: CoreBTypes)(using Context): InlineInfo = {
+    // We only want an approximation of SAMs for inlining heuristics, no need to check FunctionalInterface annotations or such
+    val abstractMembers = classSym.memberNames(abstractTermNameFilter).iterator.map(classSym.classInfo.member).map(_.symbol).filter(_.is(Method)).toList
+    val sam = abstractMembers match
+      case List(single) =>
+        val btype = ts.asmMethodType(single)
+        Some(single.javaSimpleName + btype.descriptor)
+      case _ => None
+
+    def keepMember(sym: Symbol) = sym.is(Method) && !primitives.isPrimitive(sym)
+
+    val classMethods = classSym.info.decls.iterator.filter(keepMember)
+    val methods = if classSym.is(JavaDefined) then
+      // Phase travel important for nested classes (scala-dev#402). When a java class symbol A$B
+      // is compiled from source, this ensures that `companionModule` doesn't return the `A$B`
+      // symbol created for the `A$B.class` file on the classpath, which might be different.
+      val companion = atPhase(picklerPhase.next) { classSym.companionModule }
+      val staticMethods = companion.info.decls.iterator.filter(m => !m.isConstructor && keepMember(m))
+      staticMethods ++ classMethods
+    else
+      val staticForwarders = if classSym.isInterface then
+        classSym.info.decls.filter(s => !s.isPrivate && !s.isStaticMember && s.name != nme.TRAIT_CONSTRUCTOR).map(s => {
+          BackendUtils.makeStatifiedDefSymbol(s.asTerm, BackendUtils.traitSuperAccessorName(s).toTermName)
+        })
+      else Nil
+      classMethods ++ staticForwarders
+
+    // Primitive methods cannot be inlined, so there's no point in building a MethodInlineInfo. Also, some
+    // primitive methods (e.g., `isInstanceOf`) have non-erased types, which confuses [[typeToBType]].
+    val methodInlineInfos = new collection.mutable.TreeMap[(String, String), MethodInlineInfo]()
+    methods.foreach {
+      methodSym =>
+        val name = methodSym.javaSimpleName // same as in genDefDef
+        val signature = (name, ts.asmMethodType(methodSym).descriptor)
+
+        val info = MethodInlineInfo(
+          effectivelyFinal = methodSym.isEffectivelyFinal,
+          annotatedInline = methodSym.hasAnnotation(defn.InlineAnnot),
+          annotatedNoInline = methodSym.hasAnnotation(defn.NoInlineAnnot))
+
+        methodInlineInfos(signature) = info
+    }
+
+    InlineInfo(classSym.is(Final), sam, methodInlineInfos, None)
+  }
+
+
+  /**
+   * Build the InlineInfo for a class. For Scala classes, the information is stored in the
+   * ScalaInlineInfo attribute. If the attribute is missing, the InlineInfo is built using the
+   * metadata available in the classfile (ACC_FINAL flags, etc.).
+   */
+  def inlineInfoFromClassfile(classNode: ClassNode): InlineInfo = {
+    def fromClassfileAttribute: Option[InlineInfo] = {
+      if (classNode.attrs == null) None
+      else classNode.attrs.asScala.collectFirst{ case a: InlineInfoAttribute => a.inlineInfo}
+    }
+
+    def fromClassfileWithoutAttribute = {
+      val warning = {
+        val isScala = classNode.attrs != null && classNode.attrs.asScala.exists(a => a.`type` == BTypes.ScalaAttributeName || a.`type` == BTypes.ScalaSigAttributeName)
+        if (isScala) Some(NoInlineInfoAttribute(classNode.name))
+        else None
+      }
+      // when building MethodInlineInfos for the members of a ClassSymbol, we exclude those methods
+      // in scalaPrimitives. This is necessary because some of them have non-erased types, which would
+      // require special handling. Excluding is OK because they are never inlined.
+      // Here we are parsing from a classfile and we don't need to do anything special. Many of these
+      // primitives don't even exist, for example Any.isInstanceOf.
+      val methodInfos = new mutable.TreeMap[(String, String), MethodInlineInfo]()
+      classNode.methods.forEach(methodNode => {
+        val info = MethodInlineInfo(
+          effectivelyFinal                    = BCodeUtils.isFinalMethod(methodNode),
+          annotatedInline                     = false,
+          annotatedNoInline                   = false)
+        methodInfos((methodNode.name, methodNode.desc)) = info
+      })
+
+      // We only want an approximation of SAMs for inlining heuristics, no need to check FunctionalInterface annotations or such
+      val abstractMethods = classNode.methods.asScala.filter(m => (m.access & ACC_ABSTRACT) != 0)
+      val sam = if abstractMethods.size == 1 then Some(abstractMethods.head.name + abstractMethods.head.desc) else None
+
+      InlineInfo(
+        isEffectivelyFinal = BCodeUtils.isFinalClass(classNode),
+        sam = sam,
+        methodInfos = methodInfos,
+        warning)
+    }
+
+    fromClassfileAttribute.getOrElse(fromClassfileWithoutAttribute)
+  }
+}
 
 /**
  * Information required to add a class to an InnerClass table.
@@ -665,18 +819,18 @@ final class ClassBType private(val internalName: String, val fromSymbol: Boolean
 
     assert(!ClassBType.isInternalPhantomType(internalName), s"Cannot create ClassBType for phantom type $this")
 
-    assert(
-      if (info.get.superClass.isEmpty) { isJLO(this) || ClassBType.hasNoSuper(internalName) }
-      else if (isInterface.get) isJLO(info.get.superClass.get)
-      else !isJLO(this) && ifInit(info.get.superClass.get)(!_.isInterface.get),
-      s"Invalid superClass in $this: ${info.get.superClass}"
-    )
-    assert(
-      info.get.interfaces.forall(c => ifInit(c)(_.isInterface.get)),
-      s"Invalid interfaces in $this: ${info.get.interfaces}"
-    )
-
-    assert(info.get.nestedClasses.get.forall(c => ifInit(c)(_.isNestedClass.get)), info.get.nestedClasses.get)
+    if info.isRight then
+      assert(
+        if (info.get.superClass.isEmpty) { isJLO(this) || ClassBType.hasNoSuper(internalName) }
+        else if (isInterface.get) isJLO(info.get.superClass.get)
+        else !isJLO(this) && ifInit(info.get.superClass.get)(!_.isInterface.get),
+        s"Invalid superClass in $this: ${info.get.superClass}"
+      )
+      assert(
+        info.get.interfaces.forall(c => ifInit(c)(_.isInterface.get)),
+        s"Invalid interfaces in $this: ${info.get.interfaces}"
+      )
+      assert(info.get.nestedClasses.forall(c => ifInit(c)(_.isNestedClass.get)), info.get.nestedClasses)
   }
 
   /**
@@ -924,4 +1078,147 @@ object BTypes {
    * But that would create overhead in a Collection[InternalName].
    */
   type InternalName = String
+
+
+  /**
+   * Check if a type is accessible to some class, as defined in JVMS 5.4.4.
+   * (A1) C is public
+   * (A2) C and D are members of the same run-time package
+   */
+  @tailrec
+  final def classIsAccessible(accessed: BType, from: ClassBType): Either[OptimizerWarning, Boolean] = (accessed: @unchecked) match {
+    // TODO: A2 requires "same run-time package", which seems to be package + classloader (JVMS 5.3.). is the below ok?
+    case c: ClassBType => c.isPublic.map(_ || c.packageInternalName == from.packageInternalName)
+    case a: ArrayBType => classIsAccessible(a.elementType, from)
+    case _: PrimitiveBType => Right(true)
+  }
+
+  /**
+   * Check if a member reference is accessible from the `destinationClass`, as defined in the
+   * JVMS 5.4.4. Note that the class name in a field / method reference is not necessarily the
+   * class in which the member is declared:
+   *
+   * class A { def f = 0 }; class B extends A { f }
+   *
+   * The INVOKEVIRTUAL instruction uses a method reference "B.f ()I". Therefore this method has
+   * two parameters:
+   *
+   * @param memberDeclClass The class in which the member is declared (A)
+   * @param memberRefClass  The class used in the member reference (B)
+   *
+   *                        (B0) JVMS 5.4.3.2 / 5.4.3.3: when resolving a member of class C in D, the class C is resolved
+   *                        first. According to 5.4.3.1, this requires C to be accessible in D.
+   *
+   *                        JVMS 5.4.4 summary: A field or method R is accessible to a class D (destinationClass) iff
+   *                        (B1) R is public
+   *                        (B2) R is protected, declared in C (memberDeclClass) and D is a subclass of C.
+   *                        If R is not static, R must contain a symbolic reference to a class T (memberRefClass),
+   *                        such that T is either a subclass of D, a superclass of D, or D itself.
+   *                        Also (P) needs to be satisfied.
+   *                        (B3) R is either protected or has default access and declared by a class in the same
+   *                        run-time package as D.
+   *                        If R is protected, also (P) needs to be satisfied.
+   *                        (B4) R is private and is declared in D.
+   *
+   *                        (P) When accessing a protected instance member, the target object on the stack (the receiver)
+   *                        has to be a subtype of D (destinationClass). This is enforced by classfile verification
+   *                        (https://docs.oracle.com/javase/specs/jvms/se8/html/jvms-4.html#jvms-4.10.1.8).
+   *
+   *                        TODO: we cannot currently implement (P) because we don't have the necessary information
+   *                        available. Once we have a type propagation analysis implemented, we can extract the receiver
+   *                        type from there (https://github.com/scala-opt/scala/issues/13).
+   */
+  def memberIsAccessible(memberFlags: Int, memberDeclClass: ClassBType, memberRefClass: ClassBType, from: ClassBType): Either[OptimizerWarning, Boolean] = {
+    // TODO: B3 requires "same run-time package", which seems to be package + classloader (JVMS 5.3.). is the below ok?
+    def samePackageAsDestination = memberDeclClass.packageInternalName == from.packageInternalName
+
+    def targetObjectConformsToDestinationClass = false // needs type propagation analysis, see above
+
+    def memberIsAccessibleImpl = {
+      val key = (ACC_PUBLIC | ACC_PROTECTED | ACC_PRIVATE) & memberFlags
+      key match {
+        case ACC_PUBLIC => // B1
+          Right(true)
+
+        case ACC_PROTECTED => // B2
+          val isStatic = (ACC_STATIC & memberFlags) != 0
+          tryEither {
+            val condB2 = from.isSubtypeOf(memberDeclClass).orThrow && {
+              isStatic || memberRefClass.isSubtypeOf(from).orThrow || from.isSubtypeOf(memberRefClass).orThrow
+            }
+            Right(
+              (condB2 || samePackageAsDestination /* B3 (protected) */) &&
+                (isStatic || targetObjectConformsToDestinationClass) // (P)
+            )
+          }
+
+        case 0 => // B3 (default access)
+          Right(samePackageAsDestination)
+
+        case ACC_PRIVATE => // B4
+          Right(memberDeclClass == from)
+      }
+    }
+
+    classIsAccessible(memberDeclClass, from) match { // B0
+      case Right(true) => memberIsAccessibleImpl
+      case r => r
+    }
+  }
+
+  /**
+   * Metadata about a ClassBType, used by the inliner.
+   *
+   * More information may be added in the future to enable more elaborate inline heuristics.
+   * Note that this class should contain information that can only be obtained from the ClassSymbol.
+   * Information that can be computed from the ClassNode should be added to the call graph instead.
+   *
+   * @param isEffectivelyFinal     True if the class cannot have subclasses: final classes, module
+   *                               classes.
+   *
+   * @param sam                    If this class is a SAM type, the SAM's "\$name\$descriptor".
+   *
+   * @param methodInfos            The [[MethodInlineInfo]]s for the methods declared in this class.
+   *                               The map is indexed by the string s"\$name\$descriptor" (to
+   *                               disambiguate overloads).
+   *
+   * @param warning                Contains a warning message if an error occurred when building this
+   *                               InlineInfo, for example if some classfile could not be found on
+   *                               the classpath. This warning can be reported later by the inliner.
+   */
+  final case class InlineInfo(isEffectivelyFinal: Boolean,
+                              sam: Option[String],
+                              methodInfos: collection.SortedMap[(String, String), MethodInlineInfo],
+                              warning: Option[BackendReporting.ClassInlineInfoWarning]) {
+    lazy val methodInfosSorted: IndexedSeq[((String, String), MethodInlineInfo)] = {
+      val result = new Array[((String, String), MethodInlineInfo)](methodInfos.size)
+      var i = 0
+      methodInfos.foreachEntry { (ownerAndName, info) =>
+        result(i) = (ownerAndName, info)
+        i += 1
+      }
+      scala.util.Sorting.quickSort(result)(using Ordering.by(_._1))
+      ArraySeq.unsafeWrapArray(result)
+    }
+  }
+
+  val EmptyInlineInfo = InlineInfo(isEffectivelyFinal = false, sam = None, methodInfos = SortedMap.empty, warning = None)
+
+  /**
+   * Metadata about a method, used by the inliner.
+   *
+   * @param effectivelyFinal                    True if the method cannot be overridden (in Scala)
+   * @param annotatedInline                     True if the method is annotated `@inline`
+   * @param annotatedNoInline                   True if the method is annotated `@noinline`
+   */
+  final case class MethodInlineInfo(effectivelyFinal: Boolean = false,
+                                    annotatedInline: Boolean = false,
+                                    annotatedNoInline: Boolean = false)
+
+  // no static way (without symbol table instance) to get to nme.ScalaATTR / ScalaSignatureATTR
+  val ScalaAttributeName    = "Scala"
+  val ScalaSigAttributeName = "ScalaSig"
+  
+  // when inlining, local variable names of the callee are prefixed with the name of the callee method
+  val InlinedLocalVariablePrefixMaxLength = 128
 }
