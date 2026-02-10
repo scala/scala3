cea3f8e22cc00fcc2922d9747eea9a97175e6b23
Wojciech Mazur <wmazur@virtuslab.com>
2026-02-10 01:15:24 +0100

    Bump Scala CLI to v1.12.2 (was v1.11.0) & coursier to 2.1.25-M23 (was 2.1.25-M21) (#25217)
    https://github.com/VirtusLab/scala-cli/releases/tag/v1.12.2
    https://github.com/coursier/coursier/releases/tag/v2.1.25-M23
    [Cherry-picked ead18e6d1c296b9fdcac168a9442eacee45a9b6d][modified]

--- project/Build.scala
+++ project/Build.scala
@@ -135,9 +135,9 @@ object Build {
   val mimaPreviousDottyVersion = "3.8.0"
 
   /** Version of Scala CLI to download */
-  val scalaCliLauncherVersion = "1.11.0"
+  val scalaCliLauncherVersion = "1.12.2"
   /** Version of Coursier to download for initializing the local maven repo of Scala command */
-  val coursierJarVersion = "2.1.25-M21"
+  val coursierJarVersion = "2.1.25-M23"
 
   object CompatMode {
     final val BinaryCompatible = 0
