1a71f1c2c6f8e2559813ce3d6f23bd519dddf959
Lukas Rytz <lukas.rytz@gmail.com>
2026-02-26 15:31:22 +0100

    Don't infer implicits from non-accessible companion
    Implicits found in the companion object of a type should not
    be inferred if the companion object is not accessible.

---
+++ tests/neg/i25347.scala
@@ -0,0 +1,17 @@
+trait T {
+  object i {
+    class C
+    private object C {
+      import scala.language.implicitConversions
+      given c2i: Conversion[C, Int] = _ => 42
+    }
+  }
+}
+
+object O {
+  def foo(x: Int): String = "hi"
+}
+
+object P extends T {
+  def bar = O.foo(new i.C) // error
+}
