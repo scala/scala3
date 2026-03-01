b47ffbdab64089f9272a2f23fa366d73be5efe71
Ozair Faizan <ahmad.faizan@epfl.ch>
2026-03-01 21:07:25 +0100

    Add structural recursion termination checks
    In recursive calls, at least on argument should decrease, and all others
    should be nonincreasing.
    
    An argument is decreasing if it is a case class whose `unapply` method
    is not overridden, whose fields are immutable and the recursive call is
    on a subpart of it.

---
+++ tests/pos/ycheck-termination-decreasing.scala
@@ -0,0 +1,31 @@
+//> using options -Ycheck-termination
+
+class C {
+  import scala.annotation.terminates
+
+  sealed trait MyList[+A]
+  case object MyNil extends MyList[Nothing]
+  case class MyCons[+A](val head: A, val tail: MyList[A]) extends MyList[A]
+
+  @terminates
+  def sum(l: MyList[Int]): Int =
+    l match {
+      case MyNil => 0
+      case MyCons(x, xs) => x + sum(xs)
+    }
+
+  @terminates
+  def merge(l1: MyList[Int], l2: MyList[Int]): MyList[Int] =
+    l1 match {
+      case MyNil => l2
+      case MyCons(x, xs) =>
+        l2 match {
+          case MyNil => l1
+          case MyCons(y, ys) =>
+            if (x < y) then MyCons(x, merge(xs, l2))
+            else MyCons(y, merge(l1, ys))
+        }
+    }
+
+}
+
