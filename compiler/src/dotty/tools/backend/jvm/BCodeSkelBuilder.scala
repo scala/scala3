bbbb8b10a77fa05bb65cf3a4dc31d6c90e53ac6c
lidaisy <li.daisy2003@gmail.com>
2026-03-03 21:21:34 -0500

    Add valhalla specific flags.

--- compiler/src/dotty/tools/backend/jvm/BCodeSkelBuilder.scala
+++ compiler/src/dotty/tools/backend/jvm/BCodeSkelBuilder.scala
@@ -414,6 +414,7 @@ trait BCodeSkelBuilder(using ctx: Context) extends BCodeHelpers {
         .addFlagIf(sym.hasAnnotation(TransientAttr), ACC_TRANSIENT)
         .addFlagIf(sym.hasAnnotation(VolatileAttr), ACC_VOLATILE)
         .addFlagIf(!sym.is(Mutable), ACC_FINAL)
+        .addFlagIf(sym.denot.owner.isValhallaValueClass, ACC_STRICT)
     }
 
     def addClassFields(): Unit = {
