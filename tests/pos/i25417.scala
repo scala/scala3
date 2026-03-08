scala/scala3

commit 816fc6cb34a2ee430ada1c40072a9635e0b673a5

Cast inline call results to result type, not method type (#25111)
Fixes #25091.

When casting inline call results to keep opaque types, we were using the
method type as the target. I suspect `widenTermRefExpr` was probably
just missing from the original code: we always want to cast to what the
method returns, not to the method reference itself.

Investigated with @jan-pieter and @bracevac during the Compiler Issue
Spree of January 26, 2026.

compiler/src/dotty/tools/dotc/inlines/Inlines.scala
@@ -623,12 +623,13 @@ object Inlines:
             val withAdjustedThisTypes = if call.symbol.is(Macro) then fixThisTypeModuleClassReferences(unpacked) else unpacked
             (call.tpe & withAdjustedThisTypes, withAdjustedThisTypes != unpacked)
           else (call.tpe, false)
+        val resultType = target.widenIfUnstable
         if forceCast then
           // we need to force the cast for issues with ThisTypes, as ensureConforms will just
           // check subtyping and then choose not to cast, leaving the previous, incorrect type
-          inlined.cast(target)
+          inlined.cast(resultType)
         else
-          inlined.ensureConforms(target)
+          inlined.ensureConforms(resultType)
           // Make sure that the sealing with the declared type
           // is type correct. Without it we might get problems since the
           // expression's type is the opaque alias but the call's type is

tests/pos/i25091.scala
@@ -1,0 +1,22 @@
+transparent trait VersionComponent[T]:
+    protected def minimumValue: Int
+    inline def minimum: T = wrap(minimumValue)
+    inline def reset: T = minimum
+    inline def wrap(value: Int): T
+    inline def unwrap(value: T): Int
+
+opaque type MajorVersion = Int
+object MajorVersion extends  VersionComponent[MajorVersion]:
+    protected inline def minimumValue: Int = 0
+    inline def wrap(value: Int): MajorVersion = value
+    inline def unwrap(mv: MajorVersion): Int = mv
+
+case class Version(major: MajorVersion)
+object Version:
+  trait Increment[F]:
+    extension (v: Version) def increment: Version
+
+  object Increment:
+    given Increment[MajorVersion]:
+      extension (v: Version)
+        inline def increment: Version = Version(MajorVersion.reset)

tests/pos/i25091b.scala
@@ -1,0 +1,15 @@
+object Test:
+  trait VersionComponent[T]:
+      inline def minimum: T = wrap(42)
+      inline def wrap(value: Int): T
+
+  opaque type MajorVersionType = Int
+  object MajorVersion extends VersionComponent[MajorVersionType]:
+      inline def wrap(value: Int): MajorVersionType = value
+      inline def reset: MajorVersionType = minimum
+
+  trait Increment[F]:
+    def increment: MajorVersionType
+
+  class MyIncrement extends Increment[MajorVersionType]:
+    inline def increment: MajorVersionType = MajorVersion.reset

commit cdb609fb46e8a89a6470617562064453070ac7cc

Inlines: decide sealing against original target, while using widened type as cast destination
Fixes #25417 #25427
Regression introduced by 816fc6cb34 (#25111), which fixed #25091.

Background:
- #25091 showed that inline sealing could cast target contains  method-reference that results in `notype` at erasure.
- #25111 fixed this by using `target.widenIfUnstable` to remove  method-reference from cast destination, and use it's return type instead.
- However, deciding adaptation against the widened type is too weak and could skip a required cast, and leak projected opaque-proxy types (#25417).

For example:
```scala
import scala.compiletime.summonInline

trait MyEvidence[T]

object Scope:
  opaque type T = String
  inline given MyEvidence[T] with {}

inline def summonInlineNoOpProxy: MyEvidence[Scope.T] =
  summonInline[MyEvidence[Scope.T]]

val smoke = (
  summonInline[MyEvidence[Scope.T]],
  summonInlineNoOpProxy,
)
```

For `summonInline[MyEvidence[Scope.T]]`:

- `inlined.tpe`:
  `Scope.type{type T = String}#given_MyEvidence_T`
- `target` (before widening):
  `(Scope.given_MyEvidence_T : => Scope.given_MyEvidence_T)`
- `resultType = target.widenIfUnstable`:
  `Scope.given_MyEvidence_T`

Previously
- `inlined.tpe <:< resultType` hold, so adaptation is skipped.
- Therefore, no cast is inserted, inlined tree type remains`Scope.type{type T = String}#given_MyEvidence_T`
- This is seen as `MyEvidence[String]`
- Later typing fails for required `MyEvidence[Scope.T]` vs `MyEvidence[String]` (path dependence infor is dropped).

---

This commit fixes the problem by: decide whether adaptation is needed against original `target` (the real call-site contract), while using `target.widenIfUnstable` as a cast destination, so that #25091 fix)

`target.widenIfUnstable` is an upper approximation of `target`. Conformance to widened type is weaker than conformance to original `target`.

compiler/src/dotty/tools/dotc/inlines/Inlines.scala
@@ -623,13 +623,24 @@ object Inlines:
             val withAdjustedThisTypes = if call.symbol.is(Macro) then fixThisTypeModuleClassReferences(unpacked) else unpacked
             (call.tpe & withAdjustedThisTypes, withAdjustedThisTypes != unpacked)
           else (call.tpe, false)
+        // target might contain method reference, which is invalid cast target. Use it's return type instead
+        // see https://github.com/scala/scala3/issues/25091
         val resultType = target.widenIfUnstable
         if forceCast then
           // we need to force the cast for issues with ThisTypes, as ensureConforms will just
           // check subtyping and then choose not to cast, leaving the previous, incorrect type
           inlined.cast(resultType)
         else
-          inlined.ensureConforms(resultType)
+          // Here we can't just use `inlined.ensureConforms(resultType)`
+          // `target.widenIfUnstable` is an upper approximation of `target`.
+          // a tree may conform to `target.widenIfUnstable` while still not conforming to `target`.
+          // This can happen when widening drops path-/prefix-sensitive information (e.g. projected opaque-proxy types).
+          // So adaptation must be decided against the original `target`, while we cast to the widened type.
+          // see https://github.com/scala/scala3/issues/25417
+          if inlined.ensureConforms(target) eq inlined then
+            inlined
+          else
+            inlined.cast(resultType)
           // Make sure that the sealing with the declared type
           // is type correct. Without it we might get problems since the
           // expression's type is the opaque alias but the call's type is

tests/pos/i25417.scala
@@ -1,0 +1,24 @@
+import scala.compiletime.summonFrom
+
+trait Decoder[A]
+
+given Decoder[String] with {}
+
+trait Mirror[T]:
+  type IronType
+
+given [T](using mirror: Mirror[T], ev: Decoder[mirror.IronType]): Decoder[T] =
+  ev.asInstanceOf[Decoder[T]]
+
+object Foo:
+  opaque type T = String
+
+  inline given Mirror[T] with
+    type IronType = String
+
+inline def summonFooDecoder: Decoder[Foo.T] =
+  summonFrom {
+    case decodeA: Decoder[Foo.T] => decodeA
+  }
+
+val fooDecoder: Decoder[Foo.T] = summonFooDecoder

tests/pos/i25427.scala
@@ -1,0 +1,15 @@
+import scala.compiletime.summonInline
+
+trait MyEvidence[T]
+
+object Scope:
+  opaque type T = String
+
+  inline given MyEvidence[T] with {}
+
+inline def summonInlineNoOpProxy: MyEvidence[Scope.T] = summonInline[MyEvidence[Scope.T]]
+
+val smoke = (
+  summonInline[MyEvidence[Scope.T]],
+  summonInlineNoOpProxy,
+)