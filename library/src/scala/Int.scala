63cb28f183df094b4ee6ebe4d8dadfedb2fc400c
Natsu Kagami <nki@nkagami.me>
2026-03-13 20:28:25 +0100

    Lots of capture checking on simple files

--- library/src/scala/Int.scala
+++ library/src/scala/Int.scala
@@ -12,6 +12,7 @@
 
 package scala
 
+import language.experimental.captureChecking
 import scala.language.`2.13`
 
 /** `Int`, a 32-bit signed integer (equivalent to Java's `int` primitive type) is a
