d4c978fd955af963d20649f7f3c2b1961e174718
HarrisL2 <harris.yh.lau@gmail.com>
2026-03-14 16:45:53 -0400

    Add safeNulls language option and no-explicit-nulls flag

--- compiler/src/dotty/tools/dotc/Run.scala
+++ compiler/src/dotty/tools/dotc/Run.scala
@@ -615,8 +615,8 @@ extends ImplicitRunInfo, ConstraintRunInfo, cc.CaptureRunInfo {
       .setTyper(new Typer)
       .addMode(Mode.ImplicitsEnabled)
       .setTyperState(ctx.typerState.fresh(ctx.reporter))
-    // if ctx.settings.YexplicitNulls.value && !Feature.enabledBySetting(nme.unsafeNulls) then
-    //   start = start.addMode(Mode.SafeNulls)
+    if ctx.settings.YexplicitNulls.value || Feature.enabledBySetting(nme.safeNulls) then
+      start = start.addMode(Mode.SafeNulls)
     ctx.initialize()(using start) // re-initialize the base context with start
 
     // Cache unpickled quote templates for the whole run (keyed by the pickled TASTY
