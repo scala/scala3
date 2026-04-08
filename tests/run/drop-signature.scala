27bf28a5c91693b1c9dfe499e7e7019e9e48e1dc
Solal Pirelli <solal.pirelli@gmail.com>
2026-04-08 11:32:01 +0200

    hmmm

--- tests/run/drop-signature.scala
+++ tests/run/drop-signature.scala
@@ -7,14 +7,21 @@ transparent trait IterableOps[+A, +CC[_], +C] extends Any {
   def drop(n: Int): C = ???
 }
 
+type AnyConstr[X] = Any
+
+transparent trait MapOps[K, +V, +CC[_, _] <: IterableOps[?, AnyConstr, ?], +C] extends IterableOps[(K, V), Iterable, C]
+
 trait Iterable[+A] extends IterableOps[A, Iterable, Iterable[A]]
 
 abstract class AbstractIterable[+A] extends Iterable[A]
 
-class Map[K, +V] extends AbstractIterable[(K, V)]
+trait Map[K, +V] extends Iterable[(K, V)] with MapOps[K, V, Map, Map[K, V]]
 
+abstract class AbstractMap[K, +V] extends AbstractIterable[(K, V)] with Map[K, V]
+
+object MMM extends AbstractMap[Int, String]
 
 object Test:
   def main(args: Array[String]): Unit =
-    val m = new Map[Int, String]()
+    val m: Map[Int, String] = MMM
     m.getClass.getMethods.filter(_.getName == "drop").map(_.toGenericString).foreach(println)
