9d4ac2a1c86448ea2674a72012661d04c8ac5144
Solal Pirelli <solal.pirelli@gmail.com>
2026-04-08 16:37:14 +0200

    Enable old primitive-sigs-2 tests

--- tests/run/primitive-sigs-2-old.scala
+++ tests/run/primitive-sigs-2-old.scala
@@ -1,6 +1,5 @@
 
 import scala.language.{ postfixOps }
-import scala.reflect.{ClassTag, classTag}
 import java.{ lang => jl }
 
 trait T[A] {
@@ -13,7 +12,15 @@ class Arr {
   def arr1(xs: Array[Int]): List[Int] = xs.toList
   def arr2(xs: Array[jl.Character]): List[jl.Character] = xs.toList
   def arr3(xss: Array[Array[Float]]): Array[Float] = xss map (_.sum)
-  def arr4[T: ClassTag](xss: Array[Array[T]]): Array[T] = xss map (_.head)
+  // This gets a signature like
+  // public <T> java.lang.Object Arr.arr4(java.lang.Object[],scala.reflect.Manifest<T>)
+  //
+  // instead of the more appealing version from the past
+  // public <T> T[] Arr.arr4(T[][],scala.reflect.Manifest<T>)
+  //
+  // because java inflict's its reference-only generic-arrays on us.
+  //
+  def arr4[T: Manifest](xss: Array[Array[T]]): Array[T] = xss map (_.head)
 }
 
 object Test {
@@ -28,7 +35,7 @@ object Test {
   def main(args: Array[String]): Unit = {
     println(c2.getGenericInterfaces.map(_.toString).sorted mkString " ")
     println(c1m ++ c2m sorted)
-    println(new C f)
+    println((new C()).f())
     c3m.sorted foreach println
   }
 }
