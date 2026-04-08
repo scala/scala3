6b881fba2c68e9c68345b29dc0ba2324a097dde9
Solal Pirelli <solal.pirelli@gmail.com>
2026-04-08 16:44:09 +0200

    Move one disabled test and one from scala2

---
+++ tests/run/t1461.scala
@@ -0,0 +1,17 @@
+object Test {
+  def main(args: Array[String]): Unit = {
+    val jl = classOf[Foo].getMethod("jl", classOf[Baz[_]])
+    jl.getGenericParameterTypes // works fine
+
+    val l = classOf[Foo].getMethod("l", classOf[Baz[_]])
+    // By debugger inspection l.signature is (Ltest/Baz<J>;)V
+    l.getGenericParameterTypes // throws GenericSignatureFormatError
+  }
+}
+
+class Baz[T]
+
+class Foo {
+  def l(b: Baz[Long]): Unit = { }
+  def jl(b: Baz[java.lang.Long]): Unit = { }
+}
