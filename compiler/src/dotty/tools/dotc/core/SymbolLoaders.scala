3088acc230c772b58f13dc1ae1220cc9be6449e8
Solal Pirelli <solal.pirelli@gmail.com>
2026-04-16 13:35:07 +0200

    tweak

--- compiler/src/dotty/tools/dotc/core/SymbolLoaders.scala
+++ compiler/src/dotty/tools/dotc/core/SymbolLoaders.scala
@@ -153,7 +153,7 @@ object SymbolLoaders {
 
         def checkPathMatches(path: List[TermName], what: String, tree: NameTree): Boolean = {
           // Ignore empty packages so we don't warn on top-level package objects
-          // (such as `package object scala` in the top-level "package.scala" of the compiler itself)
+          // (such as `package object scala` in the top-level "package.scala" of the standard library)
           val ok = filePath == path.filter(_ != nme.EMPTY_PACKAGE)
           if (!ok)
             report.warning(i"""$what ${tree.name} is in the wrong directory.
