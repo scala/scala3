10522c88625027104542ce20f917de812854dad7
Piotr Chabelski <ged.subfan@gmail.com>
2026-04-16 10:23:27 +0200

    Bump Scala CLI to v1.13.0 (was v1.12.5) (#25810)
    https://github.com/VirtusLab/scala-cli/releases/tag/v1.13.0
    https://github.com/VirtusLab/scala-cli-setup/releases/tag/v1.13.0
    
    ## How much have you relied on LLM-based tools in this contribution?
    Not at all
    
    ## How was the solution tested?
    Covered by existing tests
    [Cherry-picked ff19e1656a16ac47bf8d23f3e66cf551387fcb10]

--- project/Build.scala
+++ project/Build.scala
@@ -136,7 +136,7 @@ object Build {
   val mimaPreviousDottyVersion = "3.8.0"
 
   /** Version of Scala CLI to download */
-  val scalaCliLauncherVersion = "1.12.5"
+  val scalaCliLauncherVersion = "1.13.0"
   /** Version of Coursier to download for initializing the local maven repo of Scala command */
   val coursierJarVersion = "2.1.25-M24"
 
