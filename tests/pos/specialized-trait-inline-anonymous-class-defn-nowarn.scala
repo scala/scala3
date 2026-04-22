e52001e48cc20249b917a26ab9ca1d697176e4f9
Hamish <hamishstarling@hotmail.co.uk>
2026-04-22 17:19:16 +0200

    Fix syntax error in test

--- tests/pos/specialized-trait-inline-anonymous-class-defn-nowarn.scala
+++ tests/pos/specialized-trait-inline-anonymous-class-defn-nowarn.scala
@@ -1,5 +1,7 @@
 //> using options -language:experimental.specializedTraits -Werror
 inline trait T1[T: Specialized]
+inline trait T2[T: Specialized, E]
 
 inline def foo[F: Specialized] = new T1[F]() {}       // By default this warns Anonymous class will be defined at each use site, which may lead to a larger number of classfiles. This is not true for Specialized traits as we share the $impl$ class instances.
-inline def foo[F: Specialized, E] = new T1[F, E]() {} // This should also not warn even though E is not specialized 
+
+inline def foo[F: Specialized, E] = new T2[F, E]() {} // This should also not warn even though E is not specialized 
