0e46775c96dfea901af7b5825d654ec8f9289025
Som Snytt <som.snytt@gmail.com>
2026-05-01 08:24:43 -0700

    Nobody prints arrays

--- tests/neg/t750/AO_1.java
+++ tests/neg/t750/AO_1.java
@@ -1,5 +1,7 @@
 public class AO_1 {
     public static <T> void f(T[] ar0) {
-        System.out.println(ar0);
+        if (ar0.length > 0) {
+            System.out.println("hello, world");
+        }
     }
 }
