0046c0a6ebf3c0abc3dba9fd660255d0168181ef
Hamish <hamishstarling@hotmail.co.uk>
2026-05-11 11:05:00 +0200

    Special case treechecker for duplicate impl and sp classes as discussed with Solal

--- compiler/src/dotty/tools/dotc/transform/TreeChecker.scala
+++ compiler/src/dotty/tools/dotc/transform/TreeChecker.scala
@@ -75,7 +75,11 @@ class TreeChecker extends Phase with SymTransformer {
         sym.isRefinementClass
 
       assert(validSuperclass, i"$sym has no superclass set")
-      testDuplicate(sym, seenClasses, "class")
+
+       // Multiple references to specialized traits will specialize multiple times, but they lead to the same
+       // interface and implementation classes every time, so we can allow duplicates and pick one arbitrarily.  
+      if !(sym.isSpecializedTraitInterface || sym.isSpecializedTraitImplementationClass) then
+        testDuplicate(sym, seenClasses, "class")
     }
 
     val badDeferredAndPrivate =
