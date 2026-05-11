f155e6488e400d63b5d64529cd47d8bc92d92314
Hamish <hamishstarling@hotmail.co.uk>
2026-05-11 12:35:05 +0200

    Add specialized-trait-list-of-specialized-trait.scala

---
+++ tests/run/specialized-trait-list-of-specialized-trait.scala
@@ -0,0 +1,9 @@
+//> using options -language:experimental.specializedTraits
+inline trait Foo[T: Specialized]:
+    def foo = Thread.currentThread.getStackTrace()(1).getClassName()
+
+def bar(xs: List[Foo[Int]]) = xs.head
+
+@main def Test =
+    val myList = List(new Foo[Int]() {}, new Foo[Int]() {})
+    assert(bar(myList).foo == "Foo$impl$Int")
