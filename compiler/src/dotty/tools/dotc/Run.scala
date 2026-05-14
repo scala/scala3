a87ec3a79d4647d00bce41c1303bda553b6d8099
odersky <odersky@gmail.com>
2026-05-14 10:18:40 +0200

    Optimize handling of -Yexplain-cycles
    Use a single slot in the `Run` class instead of a context property.
    
    [Cherry-picked 55b60adb0d0b690ae503b81f3a1ae6a4de595256]

--- compiler/src/dotty/tools/dotc/Run.scala
+++ compiler/src/dotty/tools/dotc/Run.scala
@@ -276,6 +276,9 @@ class Run(comp: Compiler, ictx: Context) extends ImplicitRunInfo with Constraint
    */
   var ccImportEncountered = false
 
+  /** If -explain-cycles is set, a trace of cyclic reference dependencies, otherwise null */
+  var cyclicReferenceTrace: CyclicReference.Trace | Null = null
+
   private var myEnrichedErrorMessage = false
 
   def compile(files: List[AbstractFile]): Unit =
@@ -378,7 +381,7 @@ class Run(comp: Compiler, ictx: Context) extends ImplicitRunInfo with Constraint
     unfusedPhases.foreach(_.initContext(runCtx))
     val fusedPhases = runCtx.base.allPhases
     if ctx.settings.explainCyclic.value then
-      runCtx.setProperty(CyclicReference.Trace, new CyclicReference.Trace())
+      cyclicReferenceTrace = new CyclicReference.Trace()
     runCtx.withProgressCallback: cb =>
       _progress = Progress(cb, this, fusedPhases.map(_.traversals).sum)
     runPhases(allPhases = fusedPhases)(using runCtx)
