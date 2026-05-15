405785311ecd2a4a32b18022f08fe7dc89df3de9
Matt Bovel <matthieu@bovel.net>
2026-05-15 07:16:11 +0000

    Avoid warning in `list_map`

--- tests/pos-custom-args/qualified-types/list_map.scala
+++ tests/pos-custom-args/qualified-types/list_map.scala
@@ -2,8 +2,7 @@ type Pos = {v: Int with v > 0}
 
 def inc(x: Pos): Pos = (x + 1).runtimeChecked
 
-@main def Test =
+def test: List[Pos] =
   val l: List[Pos] = List(1,2,3)
   val l2 = l.map(inc)
-  l2: List[Pos]
-  ()
+  l2
