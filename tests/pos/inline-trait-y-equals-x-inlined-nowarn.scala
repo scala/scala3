49ac34e12621ce37071b30d3a5f20c0416be02ef
Hamish <hamishstarling@hotmail.co.uk>
2026-05-16 16:41:22 +0200

    Fix inline-trait-y-equals-x-inlined-nowarn by special case in error rules

--- tests/pos/inline-trait-y-equals-x-inlined-nowarn.scala
+++ tests/pos/inline-trait-y-equals-x-inlined-nowarn.scala
@@ -1,7 +1,8 @@
 //> using options -Werror -Wsafe-init
 
 inline trait A(val x: Int):
-  val y = x // We need to be careful not to warn on this when we inline it but leave the body in the parent. Pruning early enough avoids this.
+  val y = x // We need to be careful not to warn on this when we inline it but leave the body in the parent, because in the child the access is fine as we inline the parameter value,
+            // while the rhs of val y in the parent inline trait is going to be pruned out later so the illegal access won't be possible.
 class C extends A(10)
 
 @main def Test = 
