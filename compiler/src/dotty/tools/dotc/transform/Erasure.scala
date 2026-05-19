cd2952e0c1d13a079711e4d869497b54f795b659
Solal Pirelli <SolalPirelli@users.noreply.github.com>
2026-05-19 13:43:05 +0200

    Do not overwrite the supertype of non-trait super in Erasure (#26081)
    Fixes #26071 
    
    ## How much have you relied on LLM-based tools in this contribution?
    
    Not at all (but check out https://github.com/scala/scala3-lts/pull/906
    for an LLM-provided alternative 🙃 )
    
    ## How was the solution tested?
    
    New automated tests (including the issue's reproducer, if applicable)
    [Cherry-picked f5e208bac337f4697da66f0feb09b0c4afb98672]

--- compiler/src/dotty/tools/dotc/transform/Erasure.scala
+++ compiler/src/dotty/tools/dotc/transform/Erasure.scala
@@ -702,13 +702,10 @@ object Erasure {
           assignType(untpd.cpy.Select(tree)(qual, tree.name.primitiveArrayOp), qual)
 
       def adaptIfSuper(qual: Tree): Tree = qual match {
-        case Super(thisQual, untpd.EmptyTypeIdent) =>
-          val SuperType(thisType, supType) = qual.tpe: @unchecked
-          if (sym.owner.is(Flags.Trait))
-            cpy.Super(qual)(thisQual, untpd.Ident(sym.owner.asClass.name))
-              .withType(SuperType(thisType, sym.owner.typeRef))
-          else
-            qual.withType(SuperType(thisType, thisType.firstParent.typeConstructor))
+        case Super(thisQual, untpd.EmptyTypeIdent) if sym.owner.is(Flags.Trait) =>
+          val SuperType(thisType, _) = qual.tpe: @unchecked
+          cpy.Super(qual)(thisQual, untpd.Ident(sym.owner.asClass.name))
+            .withType(SuperType(thisType, sym.owner.typeRef))
         case _ =>
           qual
       }
