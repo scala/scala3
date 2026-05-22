c4b055ccb68ef70b465eb8aebea2fc898cc85a03
Natsu Kagami <nki@nkagami.me>
2026-05-23 01:28:17 +0200

    Capture check large part of runtime

--- library/src/scala/runtime/RichLong.scala
+++ library/src/scala/runtime/RichLong.scala
@@ -13,6 +13,7 @@
 package scala
 package runtime
 
+import language.experimental.captureChecking
 import scala.language.`2.13`
 
 final class RichLong(val self: Long) extends AnyVal with IntegralProxy[Long] {
