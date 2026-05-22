7e827208f1c2829468b3858fcba504ddddb8ce08
Jamie Thompson <bishbashboshjt@gmail.com>
2026-05-22 17:54:19 +0200

    fix automated tests

--- tests/neg-custom-args/captures/boundary.scala
+++ tests/neg-custom-args/captures/boundary.scala
@@ -2,7 +2,7 @@ import scala.util.boundary
 
 object test:
   boundary[AnyRef^]:
-    l1 ?=> // error // error
+    l1 ?=> // error
       boundary[Unit]: l2 ?=>
         boundary.break(l2)(using l1) // error
       ???
