b1faf9a96b71870665bd3a555c81abc87e011ac1
Hamish <hamishstarling@hotmail.co.uk>
2026-05-25 18:22:03 +0200

    Add bridges for methods with specialized trait parameters; unlocks Vec[?]

---
+++ tests/run/specialized-trait-check-bridge-exists-hidden-by-inline.scala
@@ -0,0 +1,16 @@
+//> using options -language:experimental.specializedTraits
+inline trait Trait[T: Specialized]:
+    def do_something() = println("Good morning")
+  
+inline trait A[T: Specialized]:
+    def foo(x: Trait[T]): Unit
+
+inline trait B extends A[Int]:
+    override def foo(x: Trait[Int]): Unit = x.do_something()
+
+class C extends B // Should have a bridge for the specialized foo
+
+@main def Test = 
+    val fooMethods = classOf[C].getDeclaredMethods.filter(_.getName == "foo")
+    assert(fooMethods.length == 2, s"expected 2 `foo` methods on Bar (real + bridge), found ${fooMethods.length}: ${fooMethods.mkString(", ")}")
+
