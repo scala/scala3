44b4d74ee1fb8809365e9f623c5491f6eb0aedf8
Hamish <hamishstarling@hotmail.co.uk>
2026-05-26 16:19:14 +0200

    Fix self type test error checking

--- tests/neg/specialized-trait-self-type-problems.scala
+++ tests/neg/specialized-trait-self-type-problems.scala
@@ -8,18 +8,18 @@ class Test
 inline trait A[T: Specialized]:
     this: T1 => 
 
-inline trait D extends A[Int] // error: self type of D does not conform to that of A // error: self type of A$sp$Int does not conform to that of A
+inline trait D extends A[Int] // error: self type of D does not conform to that of A
 inline trait E extends D
 
 inline trait B[T: Specialized]:
     this: T2 & T1 =>
 
-inline trait F extends A[Int], B[Int] // error: self type of F does not conform to that of A // error: self type of B$sp$Int does not conform to self type
+inline trait F extends A[Int], B[Int] // error: self type of F does not conform to that of A
 
 inline trait C[T: Specialized]:
     this: T =>
 
-inline trait H extends C[Test]: // error self type of H does not conform to that of C // error: self type of C$sp$Int does not conform to self type
+inline trait H extends C[Test]: // error self type of H does not conform to that of C
     this: T3 =>
 
 class Cl2 extends Test with H with T3
