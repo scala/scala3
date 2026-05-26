75c2a36c15e9e4d7d67666a77e18aad4bed9f82b
Hamish <hamishstarling@hotmail.co.uk>
2026-05-26 12:23:15 +0200

    Fix inline-trait-self-type-problems to use inline traits

--- tests/neg/inline-trait-self-type-problems.scala
+++ tests/neg/inline-trait-self-type-problems.scala
@@ -3,21 +3,21 @@ trait T2
 trait T3
 class Test
 
-trait A[T]:
+inline trait A[T]:
     this: T1 => 
 
-trait D extends A[Int] // error: self type of D does not conform to that of A
-trait E extends D
+inline trait D extends A[Int] // error: self type of D does not conform to that of A
+inline trait E extends D
 
-trait B[T]:
+inline trait B[T]:
     this: T2 & T1 =>
 
-trait F extends A[Int], B[Int] // error: self type of F does not conform to that of A
+inline trait F extends A[Int], B[Int] // error: self type of F does not conform to that of A
 
-trait C[T]:
+inline trait C[T]:
     this: T =>
 
-trait H extends C[Test]: // error self type of H does not conform to that of C
+inline trait H extends C[Test]: // error self type of H does not conform to that of C
     this: T3 =>
 
 class Cl2 extends Test with H with T3
