0c3b953d484510879bddca804fa6d0ac0f1c2525
Solal Pirelli <SolalPirelli@users.noreply.github.com>
2026-05-29 13:35:21 +0200

    Relax typer position assert in interactive mode (#26130)
    Fixes #25890
    
    This is fixing a symptom, but we already liberally check for interactive
    mode in the codebase to accept dubious trees. And it fixes a regression;
    perhaps we can have an issue to remove as many of those interactive
    checks as possible at some point...
    
    I also added a clearer indication that something went wrong when
    completions throw an exception.
    
    ## How much have you relied on LLM-based tools in this contribution?
    
    Not at all
    
    ## How was the solution tested?
    
    New automated testing
    
    Also manually tested: `buildQuick` then run `bin/replQ` with the issue's
    reproducer and see that it works

--- repl/src/dotty/tools/repl/ReplDriver.scala
+++ repl/src/dotty/tools/repl/ReplDriver.scala
@@ -354,7 +354,10 @@ class ReplDriver(settings: Array[String],
           unit.tpdTree = tpdTree
           given Context = state.context.fresh.setCompilationUnit(unit)
           val srcPos = SourcePosition(file, Span(cursor))
-          try Completion.completions(srcPos)._2 catch case NonFatal(_) => Nil
+          try
+            Completion.completions(srcPos)._2
+          catch case NonFatal(_) =>
+            List(Completion("<Error while fetching completions. Please report it to the Scala 3 maintainers at https://github.com/scala/scala3/issues>", "", Nil))
         }
         .getOrElse(Nil)
   end completions
