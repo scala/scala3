cbc4d63b631874ed75b5037262fc375a79bbe52b
Hamish <hamishstarling@hotmail.co.uk>
2026-06-01 18:41:24 +0200

    Fix variant warning test

--- tests/neg/specialized-trait-variant-warns.scala
+++ tests/neg/specialized-trait-variant-warns.scala
@@ -1,7 +1,6 @@
 //> using options -language:experimental.specializedTraits -Werror
 
-// nopos-error: (warning): Type parameter is both Specialized and avariant. This imposes additional typing restrictions.
-// nopos-error: (warning): Type parameter is both Specialized and variant. This imposes additional typing restrictions.
+// nopos-error: no warnings under Werror
 
-inline trait Bin[-T: Specialized]
-inline trait List[+T: Specialized]
+inline trait Bin[-T: Specialized] // warn
+inline trait List[+T: Specialized] // warn
