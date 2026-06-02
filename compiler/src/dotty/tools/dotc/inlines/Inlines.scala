787873850b631c5c21df87258909390b123c956d
Hamish <hamishstarling@hotmail.co.uk>
2026-06-02 17:03:18 +0200

    Also check if we are in an inline val before checking post condition in YCheck

--- compiler/src/dotty/tools/dotc/inlines/Inlines.scala
+++ compiler/src/dotty/tools/dotc/inlines/Inlines.scala
@@ -58,6 +58,10 @@ object Inlines:
   def inInlineMethod(using Context): Boolean =
     ctx.owner.ownersIterator.exists(_.isInlineMethod)
 
+  /** Are we in an val body? */
+  def inInlineVal(using Context): Boolean =
+    ctx.owner.ownersIterator.exists(_.isInlineVal)
+
   /** Can a call to method `meth` be inlined? */
   def isInlineable(meth: Symbol)(using Context): Boolean =
     meth.is(Inline) && meth.hasAnnotation(defn.BodyAnnot) && !inInlineMethod
