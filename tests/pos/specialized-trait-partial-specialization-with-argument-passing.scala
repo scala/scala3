151ee4a15bda0edcf59b90d23524502dca79f0d3
Hamish <hamishstarling@hotmail.co.uk>
2026-06-04 10:40:06 +0200

    Upgrade partial specialization tests

---
+++ tests/pos/specialized-trait-partial-specialization-with-argument-passing.scala
@@ -0,0 +1,8 @@
+inline trait Foo[T: Specialized, S: Specialized] 
+inline trait Bar[T: Specialized] extends Foo[T, Int]
+class Baz extends Bar[Boolean] 
+
+inline def receiver[S: Specialized](x: Foo[S, Int]) = 
+    println("Good Morning")
+
+val x = receiver(Baz())
