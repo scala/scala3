f9402ed26e1f757533761c0da5cc1cd68a5e6ea8
HarrisL2 <harris.yh.lau@gmail.com>
2026-06-05 16:19:14 -0400

    Refactor FlexibleType to use TypeBounds instead of case class

--- compiler/src/dotty/tools/dotc/typer/RefChecks.scala
+++ compiler/src/dotty/tools/dotc/typer/RefChecks.scala
@@ -1241,7 +1241,7 @@ object RefChecks {
         && !sym.owner.name.isReplWrapperName
         && !sym.isOneOf(JavaOrPrivateOrSynthetic | InlineProxy | Param | Exported) then
       val resTp = sym.info.finalResultType
-      if resTp.existsPart(_.isInstanceOf[FlexibleType], StopAt.Static) then
+      if resTp.existsPart(FlexibleType.isInstance(_), StopAt.Static) then
         report.warning(
           em"${sym.show} exposes a flexible type in its inferred result type ${resTp}. Consider annotating the type explicitly",
           sym.srcPos
