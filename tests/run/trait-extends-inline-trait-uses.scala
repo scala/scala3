75bf972880e6774efc2542a9b04a2ef9da19bafa
Hamish <hamishstarling@hotmail.co.uk>
2026-06-11 17:04:07 +0200

    Add another failing test

---
+++ tests/run/trait-extends-inline-trait-uses.scala
@@ -0,0 +1,14 @@
+inline trait Foo:
+  val x = 10
+  var y = 100
+  def z = 5
+
+trait Bar extends Foo
+
+class Baz extends Bar
+
+@main def Test = 
+  val b = Baz()
+  assert(b.y == 100)
+  assert(b.x == 10)
+  assert(b.z == 5)
