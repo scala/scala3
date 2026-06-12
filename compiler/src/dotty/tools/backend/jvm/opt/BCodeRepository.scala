64df5db4501988ad812aa566ae7a3cf34e0d7f95
Solal Pirelli <solal.pirelli@gmail.com>
2026-06-12 13:58:12 +0200

    Remove dead bits of BCodeRepository

--- compiler/src/dotty/tools/backend/jvm/opt/BCodeRepository.scala
+++ compiler/src/dotty/tools/backend/jvm/opt/BCodeRepository.scala
@@ -33,13 +33,13 @@ import scala.tools.asm.{Attribute, ClassReader, Type}
  */
 class BCodeRepository(classPath: ClassPath, indyTracker: IndyLambdaImplTracker) {
 
-  type ClassAndModuleNodes = (ClassNode, Option[ModuleNode])
+  private type ClassAndModuleNodes = (ClassNode, Option[ModuleNode])
 
   /**
    * Contains ClassNodes and the canonical path of the source file path of classes being compiled in
    * the current compilation run.
    */
-  val compilingClasses: concurrent.Map[InternalName, (ClassAndModuleNodes, String)] = concurrent.TrieMap.empty
+  val compilingClasses: concurrent.Map[InternalName, (ClassNode, String)] = concurrent.TrieMap.empty
 
   /**
   * Prevent the code repository from growing too large. Profiling reveals that the average size
@@ -57,10 +57,8 @@ class BCodeRepository(classPath: ClassPath, indyTracker: IndyLambdaImplTracker) 
   private val parsedClasses: mutable.Map[InternalName, Either[ClassNotFound, ClassAndModuleNodes]] =
     FifoCache[InternalName, Either[ClassNotFound, ClassAndModuleNodes]](maxCacheSize, threadsafe = true)
 
-  def add(classNode: ClassNode, sourceFilePath: Option[String]): Unit = sourceFilePath match {
-    case Some(path) if path != "<no file>" => compilingClasses(classNode.name) = ((classNode, None), path)
-    case _                                 => parsedClasses(classNode.name) = Right(classNode, None)
-  }
+  def add(classNode: ClassNode, sourceFilePath: String): Unit =
+    compilingClasses(classNode.name) = (classNode, sourceFilePath)
 
   private def parsedClassNode(internalName: InternalName): Either[ClassNotFound, ClassAndModuleNodes] = {
     parsedClasses.getOrElseUpdate(internalName, parseClass(internalName))
@@ -72,7 +70,7 @@ class BCodeRepository(classPath: ClassPath, indyTracker: IndyLambdaImplTracker) 
    */
   def classNodeAndSourceFilePath(internalName: InternalName): Either[ClassNotFound, (ClassAndModuleNodes, Option[String])] = {
     compilingClasses.get(internalName) match {
-      case Some((c, p)) => Right((c, Some(p)))
+      case Some((c, p)) => Right(((c, None), Some(p)))
       case _            => parsedClassNode(internalName).map((_, None))
     }
   }
@@ -83,7 +81,7 @@ class BCodeRepository(classPath: ClassPath, indyTracker: IndyLambdaImplTracker) 
    */
   def classNode(internalName: InternalName): Either[ClassNotFound, ClassAndModuleNodes] = {
     compilingClasses.get(internalName) match {
-      case Some((c, _)) => Right(c)
+      case Some((c, _)) => Right((c, None))
       case None         => parsedClassNode(internalName)
     }
   }
