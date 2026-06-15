a2a4e5ea8d09973f034fcdd4e4f7d29dea6840e4
Tomasz Godzik <tgodzik@virtuslab.com>
2026-06-15 16:34:36 +0200

    [tools] Improvements and cleanups to bisect script (#25698)
    * Print errors from compiler build only on failure (reduce spam) 
    * Better handling of errnouns builds
    [Cherry-picked a16b497cedf841f4192ec254affc4fd1dff30e22][modified]

--- project/scripts/bisect.scala
+++ project/scripts/bisect.scala
@@ -248,11 +248,28 @@ class CommitBisect(validationScript: File, shouldFail: Boolean, bootstrapped: Bo
     val scala3CompilerProject = if bootstrapped then "scala3-compiler-bootstrapped" else "scala3-compiler"
     val scala3Project = if bootstrapped then "scala3-bootstrapped" else "scala3"
     val validationCommandStatusModifier = if shouldFail then "! " else "" // invert the process status if failure was expected
+    val sbtPublishRecipe = Seq(
+      "clean",
+      """set every doc := new File("unused")""",
+      s"set scaladoc/Compile/resourceGenerators := (`$scala3Project`/Compile/resourceGenerators).value",
+      s"$scala3Project/publishLocal",
+    ).mkString("; ")
+
     val bisectRunScript = raw"""
       |scalaVersion=$$(sbt "print ${scala3CompilerProject}/version" | tail -n1)
-      |rm -r out
-      |sbt "clean; ${scala3Project}/publishLocal"
-      |${validationCommandStatusModifier}${validationScript.getAbsolutePath} "$$scalaVersion"
+      |rm -rf out
+      |export JAVA_HOME=${sys.props("java.home")}
+      |sbt_build_log=$$(mktemp)
+      |echo 'Running sbt publish recipe: sbt "$sbtPublishRecipe"'
+      |if sbt '$sbtPublishRecipe' >"$$sbt_build_log" 2>&1; then
+      |  rm -f "$$sbt_build_log"
+      |  ${validationCommandStatusModifier}${validationScript.getAbsolutePath} "$$scalaVersion"
+      |else
+      |  echo "Failed to build compiler, skip $$scalaVersion"
+      |  cat "$$sbt_build_log"
+      |  rm -f "$$sbt_build_log"
+      |  git bisect skip
+      |fi
     """.stripMargin
     "git bisect start".!
     s"git bisect bad $fistBadHash".!
