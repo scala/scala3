4f00bf890579759acb15d882829f1cacf8dd043a
Hamish <hamishstarling@hotmail.co.uk>
2026-06-17 16:53:31 +0200

    Add phase ordering edge case

---
+++ tests/run/specialized-trait-inline-trait-ordering-edge-case.scala
@@ -0,0 +1,16 @@
+//> using options -language:experimental.specializedTraits
+
+inline trait T1[A: Specialized]
+
+inline trait T2[B: Specialized]:
+    def foo(x: T1[B]) = "bungle"
+
+class Bar extends T2[Int] // Inlined by specializeInlineTraits
+                          // DesugarSpecializedTraits then needs to see the inlined T1[Int] and generate the specialization
+                          // This works ok right now because of the miniphase processing but it's perhaps not ideal.         
+@main def Test = 
+    val fooMethods = classOf[Bar].getDeclaredMethods.iterator
+        .filter(_.getName.startsWith("foo"))
+        .map(m => m.getName -> m.getParameterTypes.head.getName)
+        .toList
+    assert(fooMethods.exists(_._2 == "T1$$sp$scala$Int"))
