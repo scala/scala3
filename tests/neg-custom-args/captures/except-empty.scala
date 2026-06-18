849dae6d5ac689420b6f27640626f5ab82ec828d
Oliver Bračevac <bracevac@users.noreply.github.com>
2026-06-18 19:21:33 +0200

    Permit `Any`/`Capability` as classifier top

--- tests/neg-custom-args/captures/except-empty.scala
+++
@@ -1,7 +0,0 @@
-import caps.{any, Classifier, SharedCapability}
-
-trait C extends SharedCapability, Classifier
-trait C1 extends C, Classifier
-
-def test(c: Object^): Unit =
-  val x: Object^{c.only[C1].except[C]} = ??? // error: the capture set is empty
