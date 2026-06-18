4bcb25fcfc4fdce7e370c1ce76933ac598a687eb
Oliver Bračevac <bracevac@users.noreply.github.com>
2026-06-18 19:21:33 +0200

    Permit `Any`/`Capability` as classifier top

--- compiler/src/dotty/tools/dotc/cc/CheckCaptures.scala
+++ compiler/src/dotty/tools/dotc/cc/CheckCaptures.scala
@@ -120,14 +120,14 @@ object CheckCaptures:
       case ReadOnlyCapability(ref) =>
         check(ref)
       case OnlyCapability(ref, cls) =>
-        if !cls.isClassifiedCapabilityClass then
+        if !cls.isClassifiedCapabilityClass && !cls.isTopClassifier then
           report.error(
             em"""${ref.showRef}.only[${cls.name}] is not well-formed since $cls is not a classifier class.
                 |A classifier class is a class extending `caps.Capability` and directly extending `caps.Classifier`.""",
             ann.srcPos)
         check(ref)
       case ExceptCapability(ref, cls) =>
-        if !cls.isClassifiedCapabilityClass then
+        if !cls.isClassifiedCapabilityClass && !cls.isTopClassifier then
           // `ref` can itself carry an `.only` qualifier; show it as a capability
           // if possible instead of in its raw annotated form.
           val refStr =
