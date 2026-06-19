0f9997751aefc335d1cfc8e0256e0ddedad18caf
Oliver Bračevac <bracevac@users.noreply.github.com>
2026-06-19 19:27:19 +0200

    Flat representation of only/except
    Merges Restricted and Except into a single representation.
    This representation supports multiple except clauses.
    So far, this is only an internal representation change.
    The user-facing syntax still supports only a single
    except clause.

--- compiler/src/dotty/tools/dotc/core/Types.scala
+++ compiler/src/dotty/tools/dotc/core/Types.scala
@@ -6382,14 +6382,12 @@ object Types extends TypeUtils {
                 skolem
         c.derivedLocalCap(ensurePath(apply(prefix)))
       case c: RootCapability => c
-      case Restricted(c1, cls) =>
+      case Classified(c1, only, except) =>
         mapCapability(c1) match
-          case c2: Capability => c2.restrict(cls)
-          case (cs: CaptureSet, exact) => (cs.restrict(cls), exact)
-      case Excluded(c1, cls) =>
-        mapCapability(c1) match
-          case c2: Capability => c2.exclude(cls)
-          case (cs: CaptureSet, exact) => (cs.exclude(cls), exact)
+          case c2: Capability =>
+            except.foldLeft(if only == defn.AnyClass then c2 else c2.restrict(only))((c, e) => c.exclude(e))
+          case (cs: CaptureSet, exact) =>
+            (except.foldLeft(if only == defn.AnyClass then cs else cs.restrict(only))((s, e) => s.exclude(e)), exact)
       case ReadOnly(c1) =>
         mapCapability(c1) match
           case c2: Capability => c2.readOnly
