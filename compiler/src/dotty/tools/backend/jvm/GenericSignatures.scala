6b633b8ab86705d4052911d48151d2a9b67cdaff
Solal Pirelli <SolalPirelli@users.noreply.github.com>
2026-06-22 15:38:19 +0200

    Fix generic signature of trait val setter (#26374)
    Fixes #26372
    
    ## Have you relied on LLM-based tools in this contribution?
    
    No
    
    ## How was the solution tested?
    
    New automated tests (including the issue's reproducer, if applicable)
    [Cherry-picked 208dc7e77ad70fcf00e68e5f4607bbe83be25612]

--- compiler/src/dotty/tools/backend/jvm/GenericSignatures.scala
+++ compiler/src/dotty/tools/backend/jvm/GenericSignatures.scala
@@ -228,9 +228,13 @@ object GenericSignatures {
               case res =>
                 jsig(res)
           case _ =>
-            boxedSig(tp.widenDealias.widenNullaryMethod)
+            val typeSym = tp.typeSymbol
+            if typeSym.isTypeParam then
+              typeParamSig(typeSym.name)
+            else
               // `tp` might be a singleton type referring to a getter.
               // Hence the widenNullaryMethod.
+              boxedSig(tp.widenDealias.widenNullaryMethod)
         }
 
       pre.widenDealias match {
