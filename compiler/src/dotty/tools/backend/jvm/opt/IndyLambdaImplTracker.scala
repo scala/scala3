4299eb5180166451c2bbfc921600e55ebd549e3e
Solal Pirelli <solal.pirelli@gmail.com>
2026-06-25 15:09:53 +0200

    fix again

--- compiler/src/dotty/tools/backend/jvm/opt/IndyLambdaImplTracker.scala
+++ compiler/src/dotty/tools/backend/jvm/opt/IndyLambdaImplTracker.scala
@@ -3,6 +3,7 @@ package dotty.tools.backend.jvm.opt
 import dotty.tools.backend.jvm.BTypes.InternalName
 
 import java.util.concurrent.ConcurrentHashMap
+import scala.collection.immutable.Map
 import scala.collection.mutable
 import scala.tools.asm
 import scala.tools.asm.Handle
@@ -10,34 +11,34 @@ import scala.tools.asm.tree.{InvokeDynamicInsnNode, MethodNode}
 
 final class IndyLambdaImplTracker {
   private val indyLambdaImplMethods =
-    new ConcurrentHashMap[InternalName, mutable.Map[MethodNode, mutable.Map[InvokeDynamicInsnNode, asm.Handle]]]
+    new ConcurrentHashMap[InternalName, mutable.Map[MethodNode, Map[InvokeDynamicInsnNode, asm.Handle]]]
 
-  private def onMethods[T](hostClass: InternalName)(action: mutable.Map[MethodNode, mutable.Map[InvokeDynamicInsnNode, asm.Handle]] => T): T = {
+  private def onMethods[T](hostClass: InternalName)(action: mutable.Map[MethodNode, Map[InvokeDynamicInsnNode, asm.Handle]] => T): T = {
     val methods = indyLambdaImplMethods.computeIfAbsent(hostClass, _ => mutable.Map.empty)
     methods.synchronized(action(methods))
   }
 
   def add(hostClass: InternalName, method: MethodNode, indy: InvokeDynamicInsnNode, handle: asm.Handle): Unit = {
-    onMethods(hostClass)(_.getOrElseUpdate(method, mutable.Map.empty)(indy) = handle)
+    onMethods(hostClass)(_.updateWith(method)(v => v.map(_.updated(indy, handle))))
   }
 
   def remove(hostClass: InternalName, method: MethodNode, indy: InvokeDynamicInsnNode): Unit = {
-    onMethods(hostClass)(_.get(method).foreach(_.remove(indy)))
+    onMethods(hostClass)(_.updateWith(method)(v => v.map(_.removed(indy))))
   }
 
-  def reset(hostClass: InternalName, method: MethodNode, values: collection.Map[InvokeDynamicInsnNode, Handle]): Unit = {
+  def reset(hostClass: InternalName, method: MethodNode, values: Map[InvokeDynamicInsnNode, Handle]): Unit = {
     onMethods(hostClass)(ms => {
       if values.isEmpty then
         ms.remove(method)
       else
-        ms(method) = mutable.Map.from(values)
+        ms(method) = values
     })
   }
 
   /**
    * The methods used as lambda bodies for IndyLambda instructions within `method` of `hostClass`.
    */
-  def get(hostClass: InternalName, method: MethodNode): collection.Map[InvokeDynamicInsnNode, Handle] = {
-    onMethods(hostClass)(ms => ms.getOrElseUpdate(method, mutable.Map.empty))
+  def get(hostClass: InternalName, method: MethodNode): Map[InvokeDynamicInsnNode, Handle] = {
+    onMethods(hostClass)(ms => ms.getOrElseUpdate(method, Map.empty))
   }
 }
