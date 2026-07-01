7c9816ef7d261bb345297a4fcc7ba07043f7aadb
Solal Pirelli <solal.pirelli@gmail.com>
2026-07-01 10:55:12 +0200

    Regression test for #26436

---
+++ tests/generic-java-signatures/nested-generic-same-name.scala
@@ -0,0 +1,17 @@
+trait Outer[A] {
+  case class Same[A]()
+  case class Diff[B]()
+
+  val same: Same[A] = Same()
+  val diff: Diff[A] = Diff()
+}
+
+object Test:
+  def main(arguments: Array[String]): Unit =
+    classOf[Outer[?]].getDeclaredMethods
+      .filter(_.getName.contains("_setter_"))
+      .foreach(setter => {
+        println(setter.getName)
+        println(setter.getGenericParameterTypes().mkString(","))
+        println(setter.toGenericString)
+      })
