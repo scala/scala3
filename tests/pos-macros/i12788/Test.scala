9ba83cf3ab6ffa8525f6cb921784cd777a43e736
Solal Pirelli <SolalPirelli@users.noreply.github.com>
2026-07-07 10:20:32 +0200

    Do not emit unnecessary fields for nested objects (#25538)
    Fixes #3950
    
    ## How much have your relied on LLM-based tools in this contribution?
    
    not
    
    ## How was the solution tested?
    
    new automated test

--- tests/pos-macros/i12788/Test.scala
+++ tests/pos-macros/i12788/Test.scala
@@ -1,4 +1,4 @@
-// nopos-error
+// it's also OK if this errors due to a cycle, what matters is the compiler shouldn't crash
 trait DFBitsCompanion:
   type Token[W <: Int] = DFToken[DFType.DFBits[W]]
   extension [LW <: Int](lhs: DFType.DFBits.Token[LW]) def foo: Unit = lhs.width
