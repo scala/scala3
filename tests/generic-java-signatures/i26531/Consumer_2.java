scala/scala3

commit 208dc7e77ad70fcf00e68e5f4607bbe83be25612

Fix generic signature of trait val setter (#26374)
Fixes #26372

## Have you relied on LLM-based tools in this contribution?

No

## How was the solution tested?

New automated tests (including the issue's reproducer, if applicable)

compiler/src/dotty/tools/backend/jvm/GenericSignatures.scala
@@ -226,9 +226,13 @@ object GenericSignatures {
                 // value classes cannot appear as generic arguments
                 jsig(res, vcBoxing = ValueClassBoxing.Box)
           case _ =>
-            boxedSig(tp.widenDealias.widenNullaryMethod)
+            val typeSym = tp.typeSymbol
+            if typeSym.isTypeParam then
+              typeParamSig(typeSym.name)
+            else
               // `tp` might be a singleton type referring to a getter.
               // Hence the widenNullaryMethod.
+              boxedSig(tp.widenDealias.widenNullaryMethod)
         }
 
       pre.widenDealias match {

tests/generic-java-signatures/t6344.check
@@ -71,6 +71,8 @@ public int C5.f4(java.lang.Integer)
 public int C5.f4(java.lang.Integer)
 public int C5.f5(java.lang.Integer)
 public int C5.f5(java.lang.Integer)
+public int C5.f5(java.lang.Integer,int)
+public int C5.f5(java.lang.Integer,int)
 
 C6
 public java.lang.Object C6.f1(int,java.lang.Object,java.lang.String,java.lang.Object)

tests/generic-java-signatures/t6344.scala
@@ -28,6 +28,7 @@ class ValA[Q](val value: Q) extends AnyVal {
 }
 class ValB[Q, Q0 <: Q](val value: Q) extends AnyVal {
   def f: Q0 = ???
+  type PQ0 = Q0
 }
 
 class C0 {
@@ -72,6 +73,7 @@ class C5 {
   def f3(x: ValA[Int]) = x.f
   def f4(x: ValB[Int, Int]) = x.f
   def f5(x: ValB[Int, _ <: Int]) = x.f
+  def f5(x: ValB[Int, _ <: Int], y: x.PQ0) = 0
 }
 class C6[A] {
   def f1(x1: Val[A], x2: ValAny[A], x3: ValStr[A], x4: ValA[A]) = x4

tests/generic-java-signatures/trait-field-generic-signature-parameter.check
@@ -1,0 +1,4 @@
+public void MyAbstract.MyTrait$_setter_$foo_$eq(scala.collection.mutable.Map)
+public void MyAbstract.MyTrait$_setter_$foo_$eq(scala.collection.mutable.Map<S, java.lang.String>)
+public scala.collection.mutable.Map MyAbstract.foo()
+public scala.collection.mutable.Map<S, java.lang.String> MyAbstract.foo()

tests/generic-java-signatures/trait-field-generic-signature-parameter.scala
@@ -1,0 +1,11 @@
+trait MyTrait[S]:
+  val foo: scala.collection.mutable.Map[S, String] = scala.collection.mutable.Map.empty
+
+abstract class MyAbstract[S] extends MyTrait[S]
+
+object Test:
+  def main(args: Array[String]): Unit =
+    classOf[MyAbstract[Any]].getMethods.sortBy(_.getName).filter(_.getName.contains("foo")).foreach(m => {
+      println(m)
+      println(m.toGenericString)
+    })

commit 046cb686515ad28c005b86e9f02b8423d9409810

Fix regression #26374: generic signatures of mixed-in trait setters
Fixes #26531
Fixes #26533

This is the fix for the regression introduced by #26374

**original issue #26372**

The original issue #26372 was, given:

```scala
trait MyTrait[S0]:
  val foo: Map[S0, String]

abstract class MyAbstract[S1] extends MyTrait[S1]
```

`Mixin` copies the setter of `MyTrait` into `MyAbstract`, and it's
expected type is like `def foo_=(value: Map[S1, String]): Unit`.

However, `mixinGenericInfos` in `Mixin.scala`, construct generic
information from trait as-is.
`def foo_=(value: Map[S0, String]): Unit` (insttead of `S1`),
where `S0` is not declared in `MyAbstract[S1]` scope.

As a result, `GenericSignatures` tells that `S1` isn't referenceable
from `MyAbstract`, and widen to it's upper bound `Any`.
In the end, generic signature for generated setter for `MyTrait` became
`Map<Object, String>` that isn't compatible with `MyTrait`'s setter
and fails with

```java
MyJavaImpl.java:3: error: MyJavaImpl is not abstract and does not override abstract method test$MyTrait$_setter_$test$MyTrait$$myMap_$eq(scala.collection.mutable.Map<java.lang.Object,java.lang.String>) in MyTrait
public class MyJavaImpl extends MyAbstract<String> {
       ^
```

---

**How #26374 address the issue**

the fix by #26374 was "if it's a type parameter,
don't widen it, just output the name as-is" by:

```scala
if typeSym.isTypeParam then
  typeParamSig(typeSym.name)
```

result in `Map<S1, String>`

---

**Why #26374 introduced regressions**

However, in a JVM signature can only be written when the enclosing
class or method actually declares tparam `S1`.

For #26533

```scala
class Wrap[A](val value: G[A]) extends AnyVal
class FirstList(val cache: Cache[Wrap])
```

`A` is declared only within `Wrap`. `FirstList` does not declare `A`.
However, `A` is still type parameter and `isTypeParam` returns true.
Therefore, it ended up emitting `Cache<G<TA;>>` where A isn't declared
in `FirstList` scope.

For #26351

compiler/src/dotty/tools/backend/jvm/GenericSignatures.scala
@@ -226,13 +226,9 @@ object GenericSignatures {
                 // value classes cannot appear as generic arguments
                 jsig(res, vcBoxing = ValueClassBoxing.Box)
           case _ =>
-            val typeSym = tp.typeSymbol
-            if typeSym.isTypeParam then
-              typeParamSig(typeSym.name)
-            else
+            boxedSig(tp.widenDealias.widenNullaryMethod)
               // `tp` might be a singleton type referring to a getter.
               // Hence the widenNullaryMethod.
-              boxedSig(tp.widenDealias.widenNullaryMethod)
         }
 
       pre.widenDealias match {

compiler/src/dotty/tools/dotc/transform/Mixin.scala
@@ -317,7 +317,11 @@ class Mixin extends MiniPhase with SymTransformer { thisPhase =>
       mixinSetters.map(setter => {
         val copied = transformFollowing(DefDef(mkForwarderSym(setter.asTerm), unitLiteral.withSpan(cls.span)))
         mixinGenericInfos.get(setter) match
-          case Some(gi) => mixinGenericInfos(copied.symbol) = gi
+          case Some(gi) =>
+            mixinGenericInfos(copied.symbol) = atPhase(erasurePhase) {
+              val mixinArgs = mixin.typeParams.map(_.typeRef.asSeenFrom(cls.thisType, mixin))
+              gi.subst(mixin.typeParams, mixinArgs)
+            }
           case None => ()
         copied
       })

tests/generic-java-signatures/i26531.check
@@ -1,0 +1,1 @@
+ok

tests/generic-java-signatures/i26531/Builder_1.scala
@@ -1,0 +1,6 @@
+package i26531
+
+import java.util.function.Consumer
+
+final class Builder[T]:
+  def accept[M <: T](clazz: Class[M], consumer: Consumer[M]): Unit = ()

tests/generic-java-signatures/i26531/Consumer_2.java
@@ -1,0 +1,11 @@
+package i26531;
+
+public final class Consumer_2 {
+    interface Command {}
+    static final class Create implements Command {}
+
+    public static void test() {
+        Builder<Command> builder = new Builder<>();
+        builder.accept(Create.class, (Create command) -> {});
+    }
+}

tests/generic-java-signatures/i26531/Test_3.scala
@@ -1,0 +1,6 @@
+import i26531.Consumer_2
+
+object Test:
+  def main(args: Array[String]): Unit =
+    Consumer_2.test()
+    println("ok")

tests/generic-java-signatures/i26533.check
@@ -1,0 +1,1 @@
+ok

tests/generic-java-signatures/i26533/Consumer_2.java
@@ -1,0 +1,7 @@
+package i26533;
+
+public final class Consumer_2 {
+    public static void test() {
+        new FirstList(null);
+    }
+}

tests/generic-java-signatures/i26533/Definitions_1.scala
@@ -1,0 +1,6 @@
+package i26533
+
+class G[A]
+class Wrap[A](val value: G[A]) extends AnyVal
+class Cache[F[_]]
+class FirstList(val cache: Cache[Wrap])

tests/generic-java-signatures/i26533/Test_3.scala
@@ -1,0 +1,6 @@
+import i26533.Consumer_2
+
+object Test:
+  def main(args: Array[String]): Unit =
+    Consumer_2.test()
+    println("ok")