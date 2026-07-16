11a755ff2a91b3de3300e13ac97ac953051292d8
odersky <odersky@gmail.com>
2026-07-16 19:41:32 +0200

    Distinguish more precisely between kinds of boxed status
    Two possibilities:
    
     - `hasBoxedCapsets`: contains boxed capsets (even under unboxed ones)
     - `isBoxed`: Has a boxed capset that can only be wrapped by empty capsets

--- compiler/src/dotty/tools/dotc/cc/Setup.scala
+++ compiler/src/dotty/tools/dotc/cc/Setup.scala
@@ -433,7 +433,7 @@ class Setup extends PreRecheck, SymTransformer, SetupAPI:
      */
     def stripImpliedCaptureSet(tp: Type): Type = tp match
       case tp @ CapturingType(parent, refs)
-      if refs.isInstanceOf[CaptureSet.CSImpliedByCapability] && !tp.isBoxedCapturing =>
+      if refs.isInstanceOf[CaptureSet.CSImpliedByCapability] && !tp.isBoxed =>
         parent
       case tp: AliasingBounds =>
         tp.derivedAlias(stripImpliedCaptureSet(tp.alias))
