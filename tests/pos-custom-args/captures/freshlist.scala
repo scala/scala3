bf6c17e619b0bfd1f202cd4aa1a03f4e12cfeb9e
odersky <odersky@gmail.com>
2026-07-17 20:03:38 +0200

    Convert _i case selectors to the original case accessors
     - More precise typing
     - Avoids an unsoundness because _i accessor functions return pure results.
       This causes an additional test failure in caseclass/Test_2.scala.

---
+++ tests/pos-custom-args/captures/freshlist.scala
@@ -0,0 +1,47 @@
+import caps.*
+
+class Ref extends Mutable:
+    var x = 0
+    def get: Int = x
+    update def put(y: Int): Unit = x = y
+
+trait FreshList[+A] { this: FreshList[A]^ =>
+  def isEmpty: Boolean
+  consume def decons: FreshCons[A]^
+}
+
+case class FreshCons[+A](val head: A^, val tail: FreshList[A]^) extends FreshList[A] {
+  def isEmpty = false
+  consume def decons: FreshCons[A]^ = this
+}
+
+object FreshNil extends FreshList[Nothing] {
+  def isEmpty = true
+  consume def decons = ???
+}
+
+def Test =
+    val r1 = Ref()
+    val r2 = Ref()
+    val xs0 = FreshNil
+    val xs1 = FreshCons(r1, xs0)
+    val xs = FreshCons(r2, xs1)
+    val c = xs.decons
+    val h = c.head
+    val t = c.tail
+    val xs2 = FreshCons(h, t)
+    val xs3: FreshList[Ref^{}]^ = xs2
+
+def Test2 =
+    val xs = FreshCons(Ref(), FreshCons(Ref(), FreshNil))
+    val c = xs.decons
+    val (h, t) = (c.head, c.tail)
+    val xs2 = FreshCons(h, t)
+    val xs3: FreshList[Ref^{}]^ = xs2
+
+def Test3 =
+    val xs = FreshCons(Ref(), FreshCons(Ref(), FreshNil))
+    val FreshCons(h, t) = xs.decons
+    val xs2 = FreshCons(h, t)
+    val xs3: FreshList[Ref^{}]^ = xs2
+
