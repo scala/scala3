999e9c107a84c50f23d434496be28ff20a74862e
Jakub Warchoł <warcholljakub@gmail.com>
2026-07-20 17:31:48 +0200

    fix test

--- tests/pos/i24537.scala
+++ tests/pos/i24537.scala
@@ -1,39 +1,32 @@
-// Regression test for scala/scala3#24537
-// The compiler used to hang in `TypeComparer.compareAppliedTypeParamRef` when
-// resolving chained extension methods on an opaque refinement type combined
-// with a higher-kinded lower-bounded type parameter, as exercised by the
-// Iron library (`:|`) together with `fs2.Stream.evalMapFilter`.
-
 import scala.language.implicitConversions
 
-opaque type IronType[A, C] <: A = A
-type :|[A, C] = IronType[A, C]
+object iron:
+  opaque type IronType[A, C] <: A = A
+  type :|[A, C] = IronType[A, C]
 
-final class Implication[C1, C2]
-type ==>[C1, C2] = Implication[C1, C2]
-object Implication:
-  given [C]: (C ==> C) = Implication()
+  final class Implication[C1, C2]
+  type ==>[C1, C2] = Implication[C1, C2]
   given [C1, C2](using C1 <:< C2): (C1 ==> C2) = Implication()
 
-implicit inline def autoCastIron[A, C1, C2](inline v: A :| C1)(using C1 ==> C2): A :| C2 =
-  v.asInstanceOf[A :| C2]
+  implicit inline def autoCastIron[A, C1, C2](inline value: A :| C1)(using C1 ==> C2): A :| C2 =
+    value.asInstanceOf
+  implicit inline def autoFactorize[A, I[_] <: Iterable[?], C1, C2](inline iterable: I[A :| C1])(using C1 ==> C2): I[A] :| C2 =
+    iterable.asInstanceOf
+import iron.*
 
-class Stream[+F[_], +O]:
-  def evalMapFilter[F2[x] >: F[x], O2](f: O => F2[Option[O2]]): Stream[F2, O2] = ???
+trait Path
+trait Stream[F[_], A]:
+  def evalMapFilter[F2[_], O2](f: A => F2[O2]): Stream[F2, O2]
 
-class Path
 final class A
 final class D
 type AD = D & A
 
-object FExt:
-  extension (p: Path)
-    def asD[F[_]]: F[Option[Path :| D]] = ???
+extension (p: Path)
+  def asD[F[_]]: F[Path :| D] = ???
 
-  extension (p: Path :| D)
-    def ls[F[_]]: Stream[F, Path] = ???
+extension (p: Path :| D)
+  def ls[F[_]]: Stream[F, Path] = ???
 
-  // This must compile without the compiler hanging.
-  extension (p: Path :| AD)
-    def lsD[F[_]]: Stream[F, Path :| D] =
-      p.ls.evalMapFilter(_.asD)
+extension (p: Path :| AD)
+  def lsDA[F[_]]: Stream[F, Path :| AD] = p.ls.evalMapFilter(_.asD)
