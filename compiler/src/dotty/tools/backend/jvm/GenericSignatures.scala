55bcaa1e54be71c8b748d001516952a32caffeed
Solal Pirelli <solal.pirelli@gmail.com>
2026-07-28 09:33:03 +0200

    Revert original change

--- compiler/src/dotty/tools/backend/jvm/GenericSignatures.scala
+++ compiler/src/dotty/tools/backend/jvm/GenericSignatures.scala
@@ -467,9 +467,15 @@ object GenericSignatures {
   }
 
 
-  // only refer to type params that will actually make it into the sig, i.e., where `sym` is declared by `initialSymbol` or by one of its owners
+  // only refer to type params that will actually make it into the sig, this excludes:
+  // * type parameters appearing in method parameters
+  // * type members not visible in an enclosing template
   private def isTypeParameterInSig(sym: Symbol, initialSymbol: Symbol)(using Context) =
-    sym.isTypeParam && initialSymbol.ownersIterator.exists(_.typeRef <:< sym.owner.typeRef)
+    !sym.maybeOwner.isTypeParam &&
+      sym.isTypeParam && (
+      sym.isContainedIn(initialSymbol.topLevelClass) ||
+        (initialSymbol.is(Method) && initialSymbol.typeParams.contains(sym))
+      )
 
   // @M #2585 when generating a java generic signature that includes
   // a selection of an inner class p.I, (p = `pre`, I = `cls`) must
