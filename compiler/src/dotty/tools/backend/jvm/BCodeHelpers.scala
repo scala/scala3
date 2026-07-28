feba7d976645042abb1180a436dc8e35df2f1728
Solal Pirelli <solal.pirelli@gmail.com>
2026-07-28 10:04:47 +0200

    Lazy

--- compiler/src/dotty/tools/backend/jvm/BCodeHelpers.scala
+++ compiler/src/dotty/tools/backend/jvm/BCodeHelpers.scala
@@ -606,7 +606,7 @@ trait BCodeHelpers(val bTypeLoader: BTypeLoader) extends BCodeIdiomatic {
       // We must ensure all classes used in generic signatures are known to the loader so they can later be resolved
       // if necessary; and to do so, we must have a context with flattened names, because the callback is called with an erasure-time context.
       // The one exception is `scala.Array`, which can end up in a signature like `class C extends T[Array]` with `trait T[C[_]]`.
-      val loadingCtx = ctx.withPhase(flattenPhase.next)
+      lazy val loadingCtx = ctx.withPhase(flattenPhase.next)
       val jsOpt = GenericSignatures.javaSig(sym, memberTpe, c => {
         if c != defn.ArrayClass then bTypeLoader.classBTypeFromSymbol(c)(using loadingCtx)
       })
