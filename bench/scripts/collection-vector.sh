89a81c638d36bd4f700de7f72bcc304b6bcd856d
Matt Bovel <matthieu@bovel.net>
2026-07-29 14:45:38 +0000

    Remove unused and outdated bench project

--- bench/scripts/collection-vector.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-sbt "scala3-bench-bootstrapped/jmh:run 40 40 3 bench/tests/Vector.scala"
--- bench/scripts/library.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-find library/src/ -type f \( -name "*.scala" -or -name "*.java" \) -exec echo "scala3-bench-bootstrapped/jmh:run 40 30" {} + | sbt
