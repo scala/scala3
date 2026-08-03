192ccc736befbcfc3fe2022725d3a0153802b7b5
Tomasz Godzik <tgodzik@virtuslab.com>
2026-08-03 17:06:40 +0200

    feat: Add actionable diagnostic for missing members (#23572)
    This closes #23469
    
    LLMs were used to a moderate degree while working on this PR.
    
    ---------
    
    Co-authored-by: ghostbuster91 <ghostbuster91@users.noreply.github.com>
    [Cherry-picked c101b01b41f8780122caffcc03e0f395edc8016e][modified]

--- compiler/src/dotty/tools/dotc/reporting/ErrorMessageID.scala
+++ compiler/src/dotty/tools/dotc/reporting/ErrorMessageID.scala
@@ -243,6 +243,8 @@ enum ErrorMessageID(val isActive: Boolean = true) extends java.lang.Enum[ErrorMe
   case PrivateShadowsTypeID // errorNumber: 227
   case AmbiguousTemplateNameID // errorNumber: 228
   case IndentationWarningID // errorNumber: 229
+  case IllegalIdentifierID // errorNumber: 230
+  case ConcreteClassHasUnimplementedMethodsID // errorNumer: 231
 
   def errorNumber = ordinal - 1
 
