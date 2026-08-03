d954d59a0fb642856434cbd088828633ecc5be53
Solal Pirelli <solal.pirelli@gmail.com>
2026-08-03 13:08:01 +0200

    Emit `synchronized` methods explicitly on the JVM

---
+++ tests/pos/tailrec-synchronized.scala
@@ -0,0 +1,14 @@
+// scalajs: --skip
+// (JVM-only simplification)
+
+import scala.annotation.tailrec
+
+class TailrecProblem {
+  @tailrec
+  private def create(x: Int): Unit = {
+    this.synchronized {
+      if (x == 0) ()
+      else create(x - 1)
+    }
+  }
+}
