3d0e54d0f0ae50637320eafa6e8e6b3e70323f44
Solal Pirelli <solal.pirelli@gmail.com>
2026-08-04 13:49:06 +0200

    Simplify Profiler

--- compiler/src/dotty/tools/dotc/core/SymbolLoaders.scala
+++ compiler/src/dotty/tools/dotc/core/SymbolLoaders.scala
@@ -415,10 +415,10 @@ abstract class SymbolLoader extends LazyType { self =>
 
   private inline def profileCompletion[T](root: SymDenotation)(inline body: T)(using Context): T = {
     val sym = root.symbol
-    def associatedFile = root.symbol.associatedFile match
-      case file: AbstractFile => file
-      case null => NoAbstractFile
-    ctx.profiler.onCompletion(sym, associatedFile)(body)
+    def associatedFileName = root.symbol.associatedFile match
+      case file: AbstractFile => Some(file.name)
+      case null => None
+    ctx.profiler.onCompletion(sym, associatedFileName)(body)
   }
 
   override def complete(root: SymDenotation)(using Context): Unit = profileCompletion(root) {
