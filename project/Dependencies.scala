f4f94df30d5594cabcc10082c1a6fdd1f77a70f3
Solal Pirelli <SolalPirelli@users.noreply.github.com>
2026-08-05 08:54:31 +0200

    Update lz4 dependency (#26707)
    I hadn't noticed this before but Maven Central shows this dependency was
    renamed. So use the new name + latest version.

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -49,7 +49,7 @@ object Dependencies {
 
   val lsp4j = "org.eclipse.lsp4j" % "org.eclipse.lsp4j" % "1.0.0"
 
-  val lz4 = "org.lz4" % "lz4-java" % "1.8.1"
+  val lz4 = "at.yawk.lz4" % "lz4-java" % "1.11.1"
 
   private val mtagsVersion = "1.6.8"
   val mtagsInterfaces = "org.scalameta" % "mtags-interfaces" % mtagsVersion
