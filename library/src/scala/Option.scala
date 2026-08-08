8ea5b1a46066eaf3be86a3519cbbe79985d6d03e
Wojciech Mazur <wmazur@virtuslab.com>
2026-08-08 14:23:23 +0200

    Make Option.orNull public in binary
    [Cherry-picked 716d6989a98bb0e7dbff93fa246d93cfb6f66dce]

--- library/src/scala/Option.scala
+++ library/src/scala/Option.scala
@@ -13,6 +13,7 @@
 package scala
 
 import scala.language.`2.13`
+import scala.annotation.publicInBinary
 
 object Option {
 
@@ -249,8 +250,9 @@ sealed abstract class Option[+A] extends IterableOnce[A] with Product with Seria
    *  @return the option's value if nonempty, or `null` if empty
    */
   @inline final def orNull[A1 >: A | Null]: A1 = this.getOrElse(null)
-  
-  // for binary and TASTy backwards compatibility 
+
+  // for binary and TASTy backwards compatibility
+  @publicInBinary
   @deprecated @inline protected final def orNull[A1 >: A](implicit ev: Null <:< A1): A1 = this getOrElse ev(null)
 
   /** Returns a $some containing the result of applying $f to this $option's
