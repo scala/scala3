b93f80fc927a044f554e134dc8ec7f911ac76672
Solal Pirelli <solal.pirelli@gmail.com>
2026-08-12 09:33:13 +0200

    Add tests

--- scaladoc/test-resources/special-last-chars/special-last-chars.scala
+++ scaladoc/test-resources/special-last-chars/special-last-chars.scala
@@ -9,13 +9,13 @@ def ok_+ : Int = ???
 
 /** [[class_!]] */
 class class_! {
-  /** [[class_!.`bad$`]] */
+  /** [[class_!.`bad$`]] (or [[this.`bad$`]], or [[class_!#`bad$`]]) */
   def `bad$`: Int = ???
 
-  /** [[class_!.bad_!]] */
+  /** [[class_!.bad_!]] (or [[this.bad_!]], or [[class_!#bad_!]]) */
   def bad_! : Int = ???
 
-  /** [[class_!.ok_+]] */
+  /** [[class_!.ok_+]] (or [[this.ok_+]], or [[class_!#ok_+]]) */
   def ok_+ : Int = ???
 }
 
@@ -28,5 +28,16 @@ class `class_$` {
   def bad_! : Int = ???
 
   /** [[`class_$`.ok_+]] */
+  def ok_+ : Int = ???
+}
+
+package inPackage {
+  /** [[`bad$`]] (or [[package.`bad$`]]) */
+  def `bad$`: Int = ???
+
+  /** [[bad_!]] (or [[package.bad_!]]) */
+  def bad_! : Int = ???
+
+  /** [[ok_+]] (or [[package.ok_+]]) */
   def ok_+ : Int = ???
 }
