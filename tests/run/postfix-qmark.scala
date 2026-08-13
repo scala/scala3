513610f4dbda79fcba8bcd63d7d08f811a118265
odersky <odersky@gmail.com>
2026-08-13 15:07:00 +0200

    Allow `?` postfix operator
     - Enabled by experimental.magic
     - Independent of language.postfixOps
     - Highest precedence

---
+++ tests/run/postfix-qmark.scala
@@ -0,0 +1,21 @@
+import language.experimental.magic
+
+case class C(elem: Int):
+  def ? (y: Int) = C(elem + y)
+  def ? = C(elem)
+
+def foo(x: C, y: C) = ()
+
+@main def Test =
+  val c = C(1)
+  val x = c ? 1
+  assert(x.elem == 2)
+  val y = c ?
+  identity(1)
+  assert(y.elem == 1)
+  val z = c
+    ? 1
+  assert(z.elem == 2)
+  foo(c?, c?)
+  val xx = c?
+  println("done")
