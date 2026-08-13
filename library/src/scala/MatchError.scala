579634ee69eb4c03607fccba230ee553d0357d59
Solal Pirelli <solal.pirelli@gmail.com>
2026-08-13 12:29:36 +0200

    Fix warnings in library

--- library/src/scala/MatchError.scala
+++ library/src/scala/MatchError.scala
@@ -13,6 +13,7 @@
 package scala
 
 import scala.language.`2.13`
+import scala.util.control.NonFatal
 
 /** This class implements errors which are thrown whenever an
  *  object doesn't match any pattern of a pattern matching
@@ -30,7 +31,7 @@ final class MatchError(@transient obj: Any) extends RuntimeException {
     else
       try s"$obj ($ofClass)"
       catch {
-        case _: Throwable => "an instance " + ofClass
+        case NonFatal(_) => "an instance " + ofClass
       }
   }
 
