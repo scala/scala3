30c4f49a83409eb021a09ba602c3d41c37cb9397
Patryk Zieliński <patrykzielinskiti@gmail.com>
2026-08-16 20:00:48 +0200

    handle another case

---
+++ tests/warn/i25929d.scala
@@ -0,0 +1,12 @@
+sealed trait Ev[F[_]]
+case class IsList() extends Ev[List]
+
+sealed trait X[A]
+case class ListIntX() extends X[List[Int]]
+case class OptionIntX() extends X[Option[Int]]
+
+def test[F[_]](x: X[F[Int]], ev: Ev[F]) =
+  ev match
+    case IsList() =>
+      x match
+        case ListIntX() => ()
