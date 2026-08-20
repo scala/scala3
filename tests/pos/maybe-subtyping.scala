aa01be8b5317c86c35e59941c02fdb1ba305707f
odersky <odersky@gmail.com>
2026-08-20 11:43:25 +0200

    Make T? a subtype of T | Null if T is not nullable
    So for T not nullable, we now have T? =:= T | Null.

---
+++ tests/pos/maybe-subtyping.scala
@@ -0,0 +1,14 @@
+//> using options -Yexplicit-nulls
+import language.experimental.magic
+
+var x: String? = "hi"
+var y: String | Null = "ho"
+
+def Test =
+  x = y
+  y = x
+
+  val z1 = if ??? then x else y
+  val _: String? = z1
+  val z2 = if ??? then y else x
+  val _: String? = z2
