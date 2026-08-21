2158165137dcb384475eb75272f45a927a00b9fb
odersky <odersky@gmail.com>
2026-08-21 13:08:14 +0200

    Drop mostly redundant tests
    To reduce added linecount we drop a bunch of tests that are straightforward Option -> ?
    substitutions.

--- tests/pos/i8997-maybe.scala
+++
@@ -1,9 +0,0 @@
-//> using options -Yexplicit-nulls
-import language.experimental.magic
-object Foo:
-  def unapply(n: Int)(using x: DummyImplicit)(using y: Int): Int? = ???
-
-def test =
-  given Int = 3
-  1 match
-    case Foo(_) =>
