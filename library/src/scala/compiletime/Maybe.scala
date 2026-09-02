0e30b4daa160b278729317950f6dd8a8fa989a1c
odersky <odersky@gmail.com>
2026-09-02 16:46:24 +0200

    More extension methods on Maybe

--- library/src/scala/compiletime/Maybe.scala
+++ library/src/scala/compiletime/Maybe.scala
@@ -12,31 +12,46 @@ sealed trait Maybe[+T, +E] extends Any, Matchable:
   def get: T
 
 object Maybe {
-  extension [T, E](x: Maybe[T, E])
-    transparent inline def ? (using maybe.CanErr[E]): T =
+  extension [A, E](x: Maybe[A, E])
+    transparent inline def ? (using maybe.CanErr[E]): A =
       x.runtimeChecked match  // runtime checked needed for non-bootstrapped compilation
       case Ok(y) => y
       case Err(e) => break(Err(e))
 
-    def withErr[E1](e: E1): Maybe[T, E1] = x.runtimeChecked match
+    def withErr[E1](e: E1): Maybe[A, E1] = x.runtimeChecked match
       case Ok(y) => Ok(y)
       case Err(_) => Err(e)
 
-    def mapErr[E1](f: E => E1): Maybe[T, E1] = x.runtimeChecked match
+    def mapErr[E1](f: E => E1): Maybe[A, E1] = x.runtimeChecked match
       case Ok(y) => Ok(y)
       case Err(e) => Err(f(e))
 
-    def map[U](f: T => U): Maybe[U, E] = x.runtimeChecked match
+    def map[B](f: A => B): Maybe[B, E] = x.runtimeChecked match
       case Ok(y) => Ok(f(y))
       case Err(e) => Err(e)
 
-    def flatMap[U](f: T => Maybe[U, E]): Maybe[U, E] = x.runtimeChecked match
+    def flatMap[B](f: A => Maybe[B, E]): Maybe[B, E] = x.runtimeChecked match
       case Ok(y) => f(y)
       case Err(e) => Err(e)
 
-    // TODO Add withFilter
+    def foreach[U](f: A => U): Unit = x.runtimeChecked match
+      case Ok(y) => f(y)
+      case Err(_) =>
 
+  extension [A](x: Maybe[A, Unit])
+    def filter(p: A => Boolean): Maybe[A, Unit] = x.runtimeChecked match
+      case Ok(y) if p(y) => x
+      case _ => null.asInstanceOf[Maybe[A, Unit]]
 
+    def filterNot(p: A => Boolean): Maybe[A, Unit] = x.filter(!p(_))
+
+    inline def withFilter(p: A => Boolean): WithFilter[A] = WithFilter(x, p)
+
+  class WithFilter[A](x: Maybe[A, Unit], p: A => Boolean):
+    def map[B](f: A => B): Maybe[B, Unit] = x.filter(p).map(f)
+    def flatMap[B](f: A => Maybe[B, Unit]): Maybe[B, Unit] = x.filter(p).flatMap(f)
+    def foreach[U](f: A => U): Unit = x.filter(p).foreach(f)
+    def withFilter(q: A => Boolean): WithFilter[A] = new WithFilter(x, x => p(x) && q(x))
 }
 
 
--- tests/run-bootstrapped/parse-dates-monadic.scala
+++ tests/run-bootstrapped/parse-dates-monadic.scala
@@ -31,54 +31,56 @@ def parseDate2(str: String): Date ? String =
         Date(day, month, year)
     case _ =>
       Err("Date not in format day/month/year")
-/*
-def parseDate3(str: String): Date ? String =
-  str.split("/") match
-    case Array(d, m, y) =>
-      maybe:
-        val day   = d.parseInt.withErr(s"malformed day: $d")?
-        val month = m.parseInt.withErr(s"malformed month: $m")?
-        val year  = y.parseInt.withErr(s"malformed year: $y")?
-        if 1 <= day && day <= 31 else s"day $day outside allowed range 1..31"
-        if 1 <= month && month <= 12 else s"month $month outside allowed range 1..12"
-        Date(day, month, year)
-    case _ =>
-      Err("Date not in format day/month/year")
 
 def parseDate4(str: String): Date? =
   str.split("/") match
     case Array(d, m, y) =>
-      maybe:
-        val day   = d.parseInt?
-        val month = m.parseInt?
-        val year  = y.parseInt?
+      for
+        day <- d.parseInt
         if 1 <= day && day <= 31
+        month <- m.parseInt
+        year <- y.parseInt
         if 1 <= month && month <= 12
+      yield
         Date(day, month, year)
     case _ =>
       null
-*/
+
+def printDate4(str: String): Unit =
+  str.split("/") match
+    case Array(d, m, y) =>
+      for
+        day <- d.parseInt
+        if 1 <= day && day <= 31
+        month <- m.parseInt
+        year <- y.parseInt
+        if 1 <= month && month <= 12
+      do
+        println(Date(day, month, year))
+    case _ =>
+      println(s"malformed date: $str")
+
 @main def Test =
-  println("1/1/2000")
+  println("1/1/2000:")
   println(parseDate("1/1/2000"))
   println(parseDate2("1/1/2000"))
-  //println(parseDate3("1/1/2000"))
-  //println(parseDate4("1/1/2000"))
+  println(parseDate4("1/1/2000"))
+  printDate4("1/1/2000")
 
-  println("1/1-2000")
+  println("1/1-2000:")
   println(parseDate("1/1-2000"))
   println(parseDate2("1/1-2000"))
-  //println(parseDate3("1/1-2000"))
-  //println(parseDate4("1/1-2000"))
+  println(parseDate4("1/1-2000"))
+  printDate4("1/1-2000")
 
-  println("1/jan/2000")
+  println("1/jan/2000:")
   println(parseDate("1/jan/2000"))
   println(parseDate2("1/jan/2000"))
-  //println(parseDate3("1/jan/2000"))
-  //println(parseDate4("1/jan/2000"))
+  println(parseDate4("1/jan/2000"))
+  printDate4("1/jan/2000")
 
-  println("1/13/2000")
+  println("1/13/2000:")
   println(parseDate("1/13/2000"))
   println(parseDate2("1/13/2000"))
-  //println(parseDate3("1/13/2000"))
-  //println(parseDate4("1/13/2000"))
+  println(parseDate4("1/13/2000"))
+  printDate4("1/13/2000")
