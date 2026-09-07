0d1cf608de573c841e083d396437823746a4b436
Solal Pirelli <solal.pirelli@gmail.com>
2026-09-07 13:18:16 +0200

    and now?

--- tests/neg/stackoverflowmacro/Macro_1.scala
+++ tests/neg/stackoverflowmacro/Macro_1.scala
@@ -1,7 +1,3 @@
-//> using options -Xmax-fuel:30
-// (because we store the errors caused by a recursive overflow, so if the fuel is too high,
-//  the error is too much text for a JVM string constant)
-
 import scala.quoted.*
 
 transparent inline def stackOverflowMacro: Int = ${ StackOverflowMacro.impl }
