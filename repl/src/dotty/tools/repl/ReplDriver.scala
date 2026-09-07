041f47ef7ca6feb40236a1c5acea8aa67305278a
Jakub Warchoł <warcholljakub@gmail.com>
2026-09-07 17:39:31 +0200

    fix(repl): close every class loader a worksheet session created

--- repl/src/dotty/tools/repl/ReplDriver.scala
+++ repl/src/dotty/tools/repl/ReplDriver.scala
@@ -173,6 +173,9 @@ class ReplDriver(settings: Array[String],
 
   private[repl] def replShouldStart: Boolean = shouldStart
 
+  private[repl] def replRenderingClassLoader: Option[ClassLoader] =
+    Option(rendering).flatMap(loaded => Option(loaded.myClassLoader))
+
   private var rootCtx: Context = uninitialized
   private var shouldStart: Boolean = uninitialized
   private var compiler: ReplCompiler = uninitialized
