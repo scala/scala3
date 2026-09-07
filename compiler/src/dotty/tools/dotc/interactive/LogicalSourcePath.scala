dcc910fe0b34275e372bfef60e239156c144ddfb
Solal Pirelli <SolalPirelli@users.noreply.github.com>
2026-09-07 10:33:52 +0200

    Simplify classpath code (#26712)
    * Move `ClassPath` to the `classpath` package where it should be (it was
    in `io`)
    * Delete dead code and types
    * Delete wrapper type `PackageEntry` (makes it harder to cache packages,
    since you need to create a PackageEntry just to test whether a string is
    in a collection)
    * Simplify code
      * Stop using arrays / vectors / ... when all we need is iterables
    * Use caches more in `JrtClassPath` (turns out `Files.exists` in
    `jrt://` internally throws an exception and catches it...)
    
    Makes some benchmarks 1% faster, others are noise.
    
    ## Have you relied on LLM-based tools in this contribution?
    
    No
    
    ## How was the solution tested?
    
    Covered by existing tests (this is a refactoring)

--- compiler/src/dotty/tools/dotc/interactive/LogicalSourcePath.scala
+++ compiler/src/dotty/tools/dotc/interactive/LogicalSourcePath.scala
@@ -1,8 +1,7 @@
 package dotty.tools.dotc.interactive
 
-import dotty.tools.dotc.classpath.PackageEntry
 import dotty.tools.dotc.classpath.SourceFileEntry
-import dotty.tools.io.ClassPath
+import dotty.tools.dotc.classpath.ClassPath
 
 import java.io.File
 import java.net.URL
@@ -17,25 +16,25 @@ class LogicalSourcePath(val sourcepath: String, rootPackage: LogicalPackage)
     findPackage(inPackage).isDefined
 
   /** Return all packages contained inside `inPackage`. Package entries contain the *full name* of the package. */
-  override def packages(inPackage: String): Seq[PackageEntry] =
+  override def packages(inPackage: String): Iterable[String] =
     findPackage(inPackage) match
       case Some(pkg) => packagesIn(pkg, inPackage)
-      case None => Seq.empty[PackageEntry]
+      case None => Iterable.empty
 
 
   /** Return all sources contained directly inside `inPackage` */
-  override def sources(inPackage: String): Seq[SourceFileEntry] =
+  override def sources(inPackage: String): Iterable[SourceFileEntry] =
     findPackage(inPackage) match
       case Some(pkg) =>
         sourcesIn(pkg)
-      case None => Seq.empty[SourceFileEntry]
+      case None => Iterable.empty
 
   private def sourcesIn(pkg: LogicalPackage) =
     pkg.sources.map(p => SourceFileEntry(p))
 
   private def packagesIn(pkg: LogicalPackage, prefix: String) =
     val pre = if (prefix.isEmpty) prefix else s"$prefix."
-    pkg.packages.map(p => PackageEntry(pre + p.name))
+    pkg.packages.map(p => pre + p.name)
 
   override def asURLs: Seq[URL] = sourcepath.split(File.pathSeparator).toIndexedSeq.map(new File(_)).map(_.toURI.toURL)
 
