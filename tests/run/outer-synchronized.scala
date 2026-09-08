scala/scala3

commit 6efe202244b4225a99e58ad7716c87a124760be2

Emit `synchronized` methods explicitly on the JVM (#26697)
Fixes #5933 
Fixes #14308

Note that the `Synchronized` flag already existed, as did JVM backend
support, it just wasn't used.

## Have you relied on LLM-based tools in this contribution?

No

## How was the solution tested?

New automated tests (including the issue's reproducer, if applicable)

compiler/src/dotty/tools/dotc/transform/SimplifySynchronized.scala
@@ -1,0 +1,47 @@
+package dotty.tools.dotc.transform
+
+import scala.annotation.tailrec
+import dotty.tools.dotc.ast.tpd.*
+import dotty.tools.dotc.core.Contexts.Context
+import dotty.tools.dotc.core.Flags.{Inline, Synchronized}
+import dotty.tools.dotc.core.StdNames.nme
+import dotty.tools.dotc.core.Symbols.*
+import dotty.tools.dotc.transform.MegaPhase.MiniPhase
+
+class SimplifySynchronized extends MiniPhase:
+  override def phaseName: String = SimplifySynchronized.name
+  override def description: String = SimplifySynchronized.description
+
+  // Turn non-inline methods whose body is contained within a `this.synchronized` call into synchronized methods
+  // Besides being more efficient, this also allows tail recursion in such methods.
+  override def transformDefDef(tree: DefDef)(using Context): Tree = {
+    @tailrec
+    def extractSynchronized(rhs: Tree): Option[Tree] = rhs match
+      case Apply(TypeApply(Ident(nme.synchronized_) | Select(This(_), nme.synchronized_), _), synchronizedBody :: Nil) =>
+        Some(synchronizedBody)
+      case Block(Nil, expr) =>
+        extractSynchronized(expr)
+      case _ =>
+        None
+
+    if canBeSynchronized(tree.symbol) then
+      extractSynchronized(tree.rhs) match
+        case Some(newRhs) =>
+          tree.symbol.denot.setFlag(Synchronized)
+          cpy.DefDef(tree)(rhs = newRhs)
+        case None =>
+          tree
+    else
+      tree
+  }
+
+  // However, this is only feasible for methods that are not inline
+  private def canBeSynchronized(sym: Symbol)(using Context) =
+    !sym.is(Inline)
+      && (sym.owner match
+            case cs: ClassSymbol => !cs.parentSyms.contains(defn.AnyValClass)
+            case _ => false)
+
+object SimplifySynchronized:
+  val name: String = "simplifySynchronized"
+  val description: String = "simplify synchronized blocks when they surround a method body"

compiler/src/dotty/tools/backend/jvm/BCodeBodyBuilder.scala
@@ -772,10 +772,12 @@ trait BCodeBodyBuilder(val primitives: ScalaPrimitives, val bTypes: KnownBTypes)
 
           generatedType = bTypeLoader.bTypeFromType(c.typeValue)
           mkArrayConstructorCall(generatedType.asArrayBType, app, av.elems)
-        case Apply(t :TypeApply, _) =>
+        case Apply(t @ TypeApply(fun, _), args) =>
           generatedType =
             if (t.symbol ne defn.Object_synchronized) genTypeApply(t)
-            else genSynchronized(app, expectedType)
+            else
+              genLoadQualifier(fun)
+              genSynchronized(app, args, expectedType)
 
         case Apply(fun @ DesugaredSelect(superRef @ Super(superQual, _), _), args) =>
           // 'super' call: Note: since constructors are supposed to
@@ -1779,7 +1781,6 @@ trait BCodeBodyBuilder(val primitives: ScalaPrimitives, val bTypes: KnownBTypes)
     }
 
 
-    def genSynchronized(tree: Apply, expectedType: BType)(using Context): BType
     def genLoadTry(tree: Try)(using Context): BType
 
     def genInvokeDynamicLambda(lambdaTarget: Symbol, environmentSize: Int, functionalInterface: Symbol)(using Context): BType = {

compiler/src/dotty/tools/backend/jvm/BCodeIdiomatic.scala
@@ -277,6 +277,7 @@ trait BCodeIdiomatic(callGraph: Option[CallGraph]) {
     final def iinc( idx: Int, increment: Int): Unit = jmethod.visitIincInsn(idx, increment) // can-multi-thread
 
     final def aload( tk: BType): Unit = { emitTypeBased(JCodeMethodN.aloadOpcodes,  tk) } // can-multi-thread
+    final def aloadThis(): Unit = { jmethod.visitVarInsn(Opcodes.ALOAD, 0) } // can-multi-thread
     final def astore(tk: BType): Unit = { emitTypeBased(JCodeMethodN.astoreOpcodes, tk) } // can-multi-thread
 
     final def neg(tk: BType): Unit = { emitPrimitive(JCodeMethodN.negOpcodes, tk) } // can-multi-thread

compiler/src/dotty/tools/backend/jvm/BCodeSkelBuilder.scala
@@ -903,7 +903,13 @@ trait BCodeSkelBuilder extends BCodeHelpers {
               ctx.source.atSpan(NoSpan)
             )
           else
-            genLoadTo(trimmedRhs, returnType, LoadDestination.Return)
+            // The JVM doesn't support `synchronized` methods on interfaces so we must implement that ourselves
+            if dd.symbol.is(Synchronized) && dd.symbol.owner.is(Trait) then
+              bc.aloadThis()
+              val generatedType = genSynchronized(trimmedRhs, trimmedRhs :: Nil, returnType)
+              genAdaptAndSendToDest(generatedType, returnType, LoadDestination.Return)
+            else
+              genLoadTo(trimmedRhs, returnType, LoadDestination.Return)
 
           if (emitVars) {
             // add entries to LocalVariableTable JVM attribute
@@ -942,6 +948,8 @@ trait BCodeSkelBuilder extends BCodeHelpers {
       }
     }
 
+    def genSynchronized(tree: Tree, args: List[Tree], expectedType: BType)(using Context): BType
+    def genAdaptAndSendToDest(generatedType: BType | Null, expectedType: BType, dest: LoadDestination)(using Context): Unit
     def genLoadTo(tree: Tree, expectedType: BType, dest: LoadDestination)(using Context): Unit
 
   } // end of class PlainSkelBuilder

compiler/src/dotty/tools/backend/jvm/BCodeSyncAndTry.scala
@@ -22,8 +22,8 @@ trait BCodeSyncAndTry extends BCodeBodyBuilder {
    */
   class SyncAndTryBuilder extends PlainBodyBuilder {
 
-    def genSynchronized(tree: Apply, expectedType: BType)(using Context): BType = (tree: @unchecked) match {
-      case Apply(TypeApply(fun, _), args) =>
+    /** Precondition: the target of the 'synchronized' call must already be on the stack. (`tree` is only used for positions) */
+    def genSynchronized(tree: Tree, args: List[Tree], expectedType: BType)(using Context): BType = {
       val monitor = locals.makeLocal(bTypes.ObjectRef, "monitor", defn.ObjectType, tree.span)
       val monCleanup = new asm.Label
 
@@ -32,8 +32,9 @@ trait BCodeSyncAndTry extends BCodeBodyBuilder {
       val hasResult = expectedType != UNIT
       val monitorResult: Symbol | Null = if (hasResult) locals.makeLocal(tpeTK(args.head), "monitorResult", defn.ObjectType, tree.span) else null
 
-      /* ------ (1) pushing and entering the monitor, also keeping a reference to it in a local var. ------ */
-      genLoadQualifier(fun)
+      /* ------ (0) the monitor itself has been pushed to stack already, precondition of this method. ------ */
+
+      /* ------ (1) entering the monitor, also keeping a reference to it in a local var. ------ */
       bc.dup(bTypes.ObjectRef)
       locals.store(monitor)
       emit(asm.Opcodes.MONITORENTER)

compiler/src/dotty/tools/backend/jvm/BCodeUtils.scala
@@ -502,7 +502,7 @@ object BCodeUtils {
         // Primitives are "abstract final" to prohibit instantiation
         // without having to provide any implementations, but that is an
         // illegal combination of modifiers at the bytecode level so
-        // suppress final if abstract if present.
+        // suppress final if abstract is present.
         && !sym.isOneOf(AbstractOrTrait)
         // Bridges can be final, but final bridges confuse some frameworks
         && !sym.is(Bridge), ACC_FINAL)
@@ -512,7 +512,8 @@ object BCodeUtils {
       .addFlagIf(sym.isClass && !sym.is(Trait), ACC_SUPER)
       .addFlagIf(sym.isAllOf(JavaEnum), ACC_ENUM)
       .addFlagIf(sym.is(JavaVarargs), ACC_VARARGS)
-      .addFlagIf(sym.is(Synchronized), ACC_SYNCHRONIZED)
+      // The JVM does not support synchronized interface methods, we implement those ourselves
+      .addFlagIf(sym.is(Synchronized) && !sym.owner.is(Trait), ACC_SYNCHRONIZED)
       .addFlagIf(sym.isDeprecated, ACC_DEPRECATED)
       .addFlagIf(sym.is(Enum), ACC_ENUM)
   }

compiler/src/dotty/tools/dotc/Compiler.scala
@@ -110,7 +110,8 @@ class Compiler {
          new ParamForwarding,        // Add forwarders for aliases of superclass parameters
          new TupleOptimizations,     // Optimize generic operations on tuples
          new LetOverApply,           // Lift blocks from receivers of applications
-         new ArrayConstructors) ::   // Intercept creation of (non-generic) arrays and intrinsify.
+         new ArrayConstructors,      // Intercept creation of (non-generic) arrays and intrinsify.
+         new SimplifySynchronized) ::// Simplify `synchronized` blocks when they surround a method body
     List(new Erasure) ::             // Rewrite types to JVM model, erasing all type parameters, abstract types and refinements.
     List(new ElimErasedValueType,    // Expand erased value types to their underlying implementation types
          new PureStats,              // Remove pure stats from blocks

compiler/src/dotty/tools/dotc/core/Flags.scala
@@ -453,7 +453,7 @@ object Flags {
 
   /** Flags representing source modifiers */
   private val CommonSourceModifierFlags: FlagSet =
-    commonFlags(Private, Protected, Final, Case, Implicit, Given, Override, JavaStatic, Transparent, Erased)
+    commonFlags(Private, Protected, Final, Case, Implicit, Given, Override, JavaStatic, Transparent, Erased, Synchronized)
 
   val TypeSourceModifierFlags: FlagSet =
     CommonSourceModifierFlags.toTypeFlags | Abstract | Sealed | Opaque | Open | Into

commit 07fb1898838a3a150ae8f8588b6b680dff0a86c4

Do not emit a SYNCHRONIZED method for `Outer.this.synchronized` calls (#26998)
Reported by @natsukagami offline. Regression from #26697

## Have you relied on LLM-based tools in this contribution?

No

## How was the solution tested?

New automated tests (including the issue's reproducer, if applicable)

compiler/src/dotty/tools/dotc/transform/SimplifySynchronized.scala
@@ -17,7 +17,10 @@ class SimplifySynchronized extends MiniPhase:
   override def transformDefDef(tree: DefDef)(using Context): Tree = {
     @tailrec
     def extractSynchronized(rhs: Tree): Option[Tree] = rhs match
-      case Apply(TypeApply(Ident(nme.synchronized_) | Select(This(_), nme.synchronized_), _), synchronizedBody :: Nil) =>
+      case Apply(TypeApply(Ident(nme.synchronized_), _), synchronizedBody :: Nil) =>
+        Some(synchronizedBody)
+      // `This(X)` means `this` inside `X` but `X.this` inside a nested class of `X`!
+      case Apply(TypeApply(Select(t: This, nme.synchronized_), _), synchronizedBody :: Nil) if t.tpe == tree.symbol.owner.thisType =>
         Some(synchronizedBody)
       case Block(Nil, expr) =>
         extractSynchronized(expr)

compiler/test/dotc/scoverage-ignore.excludelist
@@ -43,3 +43,4 @@ spurious-overload.scala
 tailrec-synchronized.scala
 tailrec.scala
 traitParams.scala
+outer-synchronized.scala

tests/run/outer-synchronized.check
@@ -1,0 +1,3 @@
+32
+32
+0

tests/run/outer-synchronized.scala
@@ -1,0 +1,24 @@
+// scalajs: --skip
+// (JVM-only reflection-based test)
+
+class Outer:
+  var x = 0
+  class Inner:
+    def f(): Unit = Outer.this.synchronized { x += 1 }
+  def own(): Unit = this.synchronized { x += 1 }
+  def ownImplicit(): Unit = synchronized { x += 1 }
+
+object Test:
+  def main(args: Array[String]): Unit =
+    val o = new Outer()
+    val i = new o.Inner()
+
+    val om = o.getClass.getDeclaredMethods.find(_.getName == "own").get
+    println(om.getModifiers & java.lang.reflect.Modifier.SYNCHRONIZED)
+
+    val om2 = o.getClass.getDeclaredMethods.find(_.getName == "ownImplicit").get
+    println(om2.getModifiers & java.lang.reflect.Modifier.SYNCHRONIZED)
+
+    val fm = i.getClass.getDeclaredMethods.find(_.getName == "f").get
+    println(fm.getModifiers & java.lang.reflect.Modifier.SYNCHRONIZED)
+