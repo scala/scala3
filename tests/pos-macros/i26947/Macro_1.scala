scala/scala3

commit 69f7ab1d9eacc9ade77d5d9f4732eff1bc5ffc2a

Check macro expansion type

compiler/src/dotty/tools/dotc/inlines/Inliner.scala
@@ -1015,7 +1015,25 @@ class Inliner(val call: tpd.Tree)(using Context):
         case tree1 @ Splice(expr) if level == 0 && !hasInliningErrors && !ctx.usedBestEffortTasty =>
           val expanded = expandMacro(expr, tree1.srcPos)
           transform.TreeChecker.checkMacroGeneratedTree(tree1, expanded)
-          typedExpr(expanded) // Inline calls and constant fold code generated by the macro
+          val res = typedExpr(expanded) // Inline calls and constant fold code generated by the macro
+
+          // We dealias opaque types because their aliases might not be visible
+          // at the expansion site. See `tests/run-macros/opaque-inline`.
+          val dealiasOpaques = new TypeMap:
+            def apply(tp: Type): Type = tp match
+              case tp: TypeRef if tp.typeSymbol.isOpaqueAlias =>
+                val sym = tp.typeSymbol
+                apply(sym.opaqueAlias.asSeenFrom(tp.prefix, sym.owner))
+              case _ =>
+                mapOver(tp)
+
+          val actualTp = dealiasOpaques(res.tpe)
+          val expectedTp = dealiasOpaques(tree1.tpe)
+          if actualTp frozen_<:< expectedTp then
+            res
+          else
+            errorTree(tree1, em"""Macro expansion has type $actualTp, which does not conform to the expected type $expectedTp""")
+
         case tree1 => tree1
 
     override def typedMatch(tree: untpd.Match, pt: Type)(using Context): Tree =

tests/neg-macros/expr-asInstanceOf/Macro_1.scala
@@ -1,0 +1,9 @@
+import scala.quoted.*
+
+object Macro:
+  inline def foo(): Int =
+    ${fooImpl()}
+
+  def fooImpl()(using Quotes): Expr[Int] =
+    import quotes.reflect.*
+    Expr("hello").asInstanceOf[Expr[Int]]

tests/neg-macros/expr-asInstanceOf/Test_2.scala
@@ -1,0 +1,2 @@
+@main def test =
+  println(Macro.foo()) // error

tests/neg-macros/monocle-transparent-invariant-neg/Macro_1.scala
@@ -1,0 +1,12 @@
+import scala.quoted.*
+
+// Invariant type constructor
+final class Inv[A](val value: A)
+
+object Inv:
+  transparent inline def make: Inv[Tuple] =
+    ${ makeImpl }
+
+  def makeImpl(using Quotes): Expr[Inv[Tuple]] =
+    val e: Expr[Inv[EmptyTuple]] = '{ new Inv[EmptyTuple](EmptyTuple) }
+    e.asInstanceOf[Expr[Inv[Tuple]]]

tests/neg-macros/monocle-transparent-invariant-neg/Test_2.scala
@@ -1,0 +1,1 @@
+val inv: Inv[EmptyTuple] = Inv.make // error

tests/pos-macros/monocle-transparent-invariant-pos/Macro_1.scala
@@ -1,0 +1,11 @@
+import scala.quoted.*
+
+// Invariant type constructor
+final class Inv[A](val value: A)
+
+object Inv:
+  transparent inline def make: Inv[? <: Tuple] =
+    ${ makeImpl }
+
+  def makeImpl(using Quotes): Expr[Inv[? <: Tuple]] =
+    '{ new Inv[EmptyTuple](EmptyTuple) }

tests/pos-macros/monocle-transparent-invariant-pos/Test_2.scala
@@ -1,0 +1,1 @@
+val inv: Inv[EmptyTuple] = Inv.make

tests/run-macros/i7887/Macro_1.scala
@@ -1,4 +1,4 @@
-def myMacroImpl(a: quoted.Expr[_])(using qctx: quoted.Quotes) = {
+def myMacroImpl[S](a: quoted.Expr[S])(using qctx: quoted.Quotes): quoted.Expr[S] = {
   import scala.quoted.quotes.reflect.*
   def typed[A] = {
     implicit val t: quoted.Type[A] = a.asTerm.tpe.widen.asType.asInstanceOf[quoted.Type[A]]

commit 74a90ae23aecc3acfa8c8a4730ae28097b4ca5d6

Also dealias transparent types when dealiasing opaques in typedSplice (#26984)
Fixes #26947 

The issue was introduced in
https://github.com/scala/scala3/commit/69f7ab1d9eacc9ade77d5d9f4732eff1bc5ffc2a,
with the opaque dealiasing introduced when typechecking a splice. The
problem in the reproduction was that, without also dealiasing
transparent types, we would not handle a chain like `MyType
(transparent) -> Wrapper.Impl (opaque) -> Int` correctly, when comparing
to `Wrapper.Impl (opaque) -> Int`.

## Have you relied on LLM-based tools in this contribution?

No

## How was the solution tested?

New automated tests (including the issue's reproducer, if applicable)

compiler/src/dotty/tools/dotc/inlines/Inliner.scala
@@ -1112,6 +1112,9 @@ class Inliner(val call: tpd.Tree)(using Context):
               case tp: TypeRef if tp.typeSymbol.isOpaqueAlias =>
                 val sym = tp.typeSymbol
                 apply(sym.opaqueAlias.asSeenFrom(tp.prefix, sym.owner))
+              case tp: TypeRef if tp.typeSymbol.isAliasType =>
+                val tp1 = tp.dealias
+                if tp1 eq tp then tp else apply(tp1)
               case _ =>
                 mapOver(tp)
 

tests/pos-macros/i26947/Macro_1.scala
@@ -1,0 +1,18 @@
+package lib
+
+import scala.quoted.*
+
+object Wrapper:
+  opaque type Impl = Int
+  object Impl:
+    def apply(x: Int): Impl = x
+
+type MyType = Wrapper.Impl
+object MyType:
+  def apply(x: Int): MyType = Wrapper.Impl(x)
+
+object MyMacro:
+  def createImpl(using Quotes): Expr[MyType] =
+    '{ MyType(42) }
+
+  inline def create: MyType = ${ createImpl }

tests/pos-macros/i26947/Test_2.scala
@@ -1,0 +1,5 @@
+package lib
+
+@main def test =
+  val x: MyType = MyMacro.create
+  println(x)