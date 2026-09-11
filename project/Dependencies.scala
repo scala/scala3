d37feb1637e165910e92e6af9c4875c8264ec2a5
Piotr Chabelski <ged.subfan@gmail.com>
2026-09-11 09:48:56 +0200

    Bump Scala CLI to v1.17.0 (was v1.16.0)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -71,7 +71,7 @@ object Dependencies {
   val scalaCheck = "org.scalacheck" %% "scalacheck" % "1.20.0"
 
   /** Version of Scala CLI to download */
-  val scalaCliLauncherVersion = "1.16.0"
+  val scalaCliLauncherVersion = "1.17.0"
 
   val scalaJsDomVersion = "2.8.1" // needs %%% which isn't usable within a val here
   val scalaJsEnvNodeJs = "org.scala-js" %% "scalajs-env-nodejs" % "1.6.0"
