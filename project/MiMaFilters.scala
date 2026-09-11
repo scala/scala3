ed440bf33ee5ab735b370a5db54774f193cfe521
Solal Pirelli <solal.pirelli@gmail.com>
2026-09-11 09:33:55 +0200

    Update MiMaFilters.scala

--- project/MiMaFilters.scala
+++ project/MiMaFilters.scala
@@ -713,13 +713,13 @@ object MiMaFilters {
   object Interfaces {
     val ForwardsBreakingChanges: Map[String, Seq[ProblemFilter]] = Map(
       // Additions that require a new minor version of interfaces
-      Versions.mimaPreviousVersion -> Seq.empty,
-
-      // Additions since last LTS
-      Versions.mimaPreviousLTSVersion -> Seq(
+      Versions.mimaPreviousVersion -> Seq(
         // SourceFile now has the (preferred) textContent to avoid copying to a char array
         ProblemFilters.exclude[DirectMissingMethodProblem]("dotty.tools.dotc.interfaces.SourceFile.textContent"),
-      )
+      ),
+
+      // Additions since last LTS
+      Versions.mimaPreviousLTSVersion -> Seq.empty,
     )
 
     val BackwardsBreakingChanges: Map[String, Seq[ProblemFilter]] = Map(
