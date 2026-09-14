d32a1452d251b551f5438bf8603be57bf372db1c
Solal Pirelli <solal.pirelli@gmail.com>
2026-09-14 14:28:08 +0200

    WIP

--- scaladoc/src/dotty/tools/scaladoc/snippets/SnippetCompiler.scala
+++ scaladoc/src/dotty/tools/scaladoc/snippets/SnippetCompiler.scala
@@ -4,65 +4,28 @@ package snippets
 import dotty.tools.io
 import dotty.tools.io.AbstractFile
 import dotty.tools.dotc.Driver
-import dotty.tools.dotc.core.Contexts.Context
 import dotty.tools.dotc.core.Mode
-import dotty.tools.dotc.core.MacroClassLoader
-import dotty.tools.dotc.config.Settings.Setting._
+import dotty.tools.dotc.config.Settings.Setting.*
 import dotty.tools.dotc.reporting.StoreReporter
-import dotty.tools.dotc.{ Compiler, Run }
 import dotty.tools.dotc.util.{SourceFile, SourcePosition}
 import dotty.tools.dotc.util.Spans.NoSpan
+import java.nio.charset.StandardCharsets
 
 class SnippetCompiler(
   val snippetCompilerSettings: Seq[SnippetCompilerSetting[?]],
   target: AbstractFile = io.virtualDirectory("(memory)")
 ):
-  object SnippetDriver extends Driver:
-    val currentCtx =
-      val rootCtx = initCtx.fresh.addMode(Mode.ReadPositions).addMode(Mode.Interactive)
-      rootCtx.setSetting(rootCtx.settings.experimental, true)
-      rootCtx.setSetting(rootCtx.settings.YretainTrees, true)
-      rootCtx.setSetting(rootCtx.settings.XcookComments, true)
-      rootCtx.setSetting(rootCtx.settings.XreadComments, true)
-      rootCtx.setSetting(rootCtx.settings.color, "never")
-      rootCtx.setSetting(rootCtx.settings.XimportSuggestionTimeout, 0)
-      for scSetting <- snippetCompilerSettings do
-        rootCtx.setSetting(scSetting.setting, scSetting.value)
-
-      val ctx = setup(Array(""), rootCtx) match
-        case Some((_, ctx)) =>
-          ctx
-        case None => rootCtx
-      ctx.initialize()(using ctx)
-      MacroClassLoader.init(ctx.fresh)
-
-  private val scala3Compiler = new Compiler
-
-  private def newRun(using ctx: Context): Run = scala3Compiler.newRun
-
   def compile(
     snippet: SnippetSource,
     wrappedSnippet: WrappedSnippet,
     arg: SnippetCompilerArg,
     sourceFile: SourceFile
   ): SnippetCompilationResult =
-    val baseContext = SnippetDriver.currentCtx.fresh
-      .setSetting(
-        SnippetDriver.currentCtx.settings.outputDir,
-        target
-      )
-      .setReporter(new StoreReporter)
-    val context =
-      if arg.scalacOptions.isEmpty then baseContext
-      else
-        val args = arg.scalacOptions.toArray
-        SnippetDriver.setup(args, baseContext) match
-          case Some((_, ctx)) => ctx
-          case None => baseContext
-    val run = newRun(using context)
-    run.compileFromStrings(List(wrappedSnippet.snippet))
-
-    val diagnostics = context.reporter.pendingMessages(using context)
+    val driver = new SnippetDriver(snippetCompilerSettings, target)
+    val files = List(new VirtualFile("(snippet)", wrappedSnippet.snippet.getBytes(StandardCharsets.UTF_8)))
+    val reporter = driver.process(arg.scalacOptions, files)
+    reporter.flush()
+    val diagnostics = reporter.allErrors ++ reporter.allWarnings ++ reporter.allInfos
     val observed = SnippetExpectations.observe(diagnostics, wrappedSnippet, sourceFile)
 
     val (messages, succeeded) =
@@ -70,7 +33,7 @@ class SnippetCompiler(
         val expected = SnippetExpectations.parse(snippet, sourceFile)
         val validation = SnippetExpectations.validate(expected, observed, sourceFile)
         val failCheck =
-          if arg.flag == SCFlags.Fail && expected.expectedErrors == 0 && !context.reporter.hasErrors then
+          if arg.flag == SCFlags.Fail && expected.expectedErrors == 0 && !reporter.hasErrors then
             List(SnippetCompilerMessage(None, s"No errors found when compiling snippet in $sourceFile:\n${wrappedSnippet.snippet}", MessageLevel.Error))
           else Nil
         val errors = validation ++ failCheck
@@ -79,19 +42,40 @@ class SnippetCompiler(
           if hasMismatches then errors
           else observed.map(_.message) ++ errors
         val ok = !hasMismatches
-          && (arg.flag != SCFlags.Fail || context.reporter.hasErrors || expected.expectedErrors > 0)
+          && (arg.flag != SCFlags.Fail || reporter.hasErrors || expected.expectedErrors > 0)
         (msgs, ok)
       else
-        val failMsg = Option.when(arg.flag == SCFlags.Fail && !context.reporter.hasErrors)(
+        val failMsg = Option.when(arg.flag == SCFlags.Fail && !reporter.hasErrors)(
           SnippetCompilerMessage(
             Some(Position(SourcePosition(sourceFile, NoSpan), wrappedSnippet.outerLineOffset)),
-            s"Snippet should not compile but compiled successfully in $sourceFile:\n${wrappedSnippet.snippet}\n(with arg: $arg)", MessageLevel.Error)
+            s"Snippet should not compile but compiled successfully in $sourceFile:\n${wrappedSnippet.snippet}", MessageLevel.Error)
         )
         val msgs = observed.map(_.message) ++ failMsg
-        val ok =
-          if arg.flag == SCFlags.Fail then context.reporter.hasErrors
-          else !context.reporter.hasErrors
+        val ok = (arg.flag == SCFlags.Fail) == reporter.hasErrors
         (msgs, ok)
 
-    val t = Option.when(!context.reporter.hasErrors)(target)
+    val t = Option.when(!reporter.hasErrors)(target)
     SnippetCompilationResult(wrappedSnippet, succeeded, t, messages)
+
+final class SnippetDriver(snippetCompilerSettings: Seq[SnippetCompilerSetting[?]], target: AbstractFile) extends Driver:
+  protected override def sourcesRequired: Boolean = false
+
+  protected override def initCtx =
+    val rootCtx = super.initCtx.fresh.addMode(Mode.ReadPositions).addMode(Mode.Interactive)
+    rootCtx.setSetting(rootCtx.settings.experimental, true)
+    rootCtx.setSetting(rootCtx.settings.YretainTrees, true)
+    rootCtx.setSetting(rootCtx.settings.XcookComments, true)
+    rootCtx.setSetting(rootCtx.settings.XreadComments, true)
+    rootCtx.setSetting(rootCtx.settings.color, "never")
+    rootCtx.setSetting(rootCtx.settings.XimportSuggestionTimeout, 0)
+    for scSetting <- snippetCompilerSettings do
+      rootCtx.setSetting(scSetting.setting, scSetting.value)
+    rootCtx.setSetting(rootCtx.settings.outputDir, target)
+
+  def process(args: Array[String], files: List[AbstractFile]): Reporter = {
+    setup(args, initCtx.fresh) match
+      case Some((_, compileCtx)) =>
+        doCompile(newCompiler(using compileCtx), files)(using compileCtx)
+      case None =>
+        initCtx.reporter
+  }
