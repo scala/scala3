090eb8754b9b9e7665fa64a81665816bb42cd522
Solal Pirelli <solal.pirelli@gmail.com>
2026-09-17 13:20:23 +0200

    Add some tests

--- tests/warn/i16899.scala
+++ tests/warn/i16899.scala
@@ -3,3 +3,14 @@ sealed trait Unset
 def foo(v: Unset|Option[Int]): Unit = v match
   case v: Unset => ()
   case v: Option[Int] => () // ok
+
+def foo2(v: Unset|Option[Int]): Unit = v match
+  case _: Unset | _: Option[Int] => () // ok
+
+def foo3(v: Unset|Option[?]): Unit = v match
+  case v: Unset => ()
+  case v: Option[Int] => () // warn
+
+def foo4(v: Unset|Option[?]): Unit = v match
+  case v: Unset => ()
+  case v @ (_: Option[Int] | _: Option[String]) => () // warn // warn
