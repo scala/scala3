9a563820a8c1fb9fb7c900d9e587d954bd699266
Rikito Taniguchi <rikitotaniguchi@proton.me>
2026-09-24 22:43:45 +0900

    Workaround for sbt client mode output loss
    Cherry-pick https://github.com/scala/scala3/pull/25973
    Which is fixed in sbt 1.x by https://github.com/sbt/sbt/pull/9265

--- project/Build.scala
+++ project/Build.scala
@@ -1532,6 +1532,9 @@ object Build {
       ),
       // Specify the default entry point of the compiler
       Compile / mainClass := Some("dotty.tools.dotc.Main"),
+      // Disable connectInput to avoid losing stdout/stderr in sbt client mode
+      // Workaround for https://github.com/sbt/sbt/issues/9185
+      run / connectInput := false,
       // Add entry's to the MANIFEST
       packageOptions += ManifestAttributes(("Git-Hash", VersionUtil.gitHash)), // Used by the REPL
       // Packaging configuration of the stdlib
@@ -1662,6 +1665,9 @@ object Build {
       ),
       // Specify the default entry point of the compiler
       Compile / mainClass := Some("dotty.tools.dotc.Main"),
+      // Disable connectInput to avoid losing stdout/stderr in sbt client mode
+      // Workaround for https://github.com/sbt/sbt/issues/9185
+      run / connectInput := false,
       // Add entry's to the MANIFEST
       packageOptions += ManifestAttributes(("Git-Hash", VersionUtil.gitHash)), // Used by the REPL
       // Packaging configuration of the stdlib
